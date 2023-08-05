b2f5cce85fcdd33166ddf939c396027ba3d5e969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:02 +0000

    Add package itkwidgets/0.13.1/itkwidgets-0.13.1-py2.py3-none-any.whl

---
+++ packages/itkwidgets/itkwidgets-0.13.1-py2.py3-none-any.whl/itkwidgets/__init__.py
@@ -0,0 +1,12 @@
+from ._version import version_info, __version__
+
+from .widget_viewer import Viewer, view
+from . import cm
+
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'static',
+        'dest': 'itk-jupyter-widgets',
+        'require': 'itk-jupyter-widgets/extension'
+    }]
