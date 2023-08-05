b0adb46a0b7e6e63b3a2cb58c08fff72067c2871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:43 +0000

    Add package Minetorch/0.1.6/Minetorch-0.1.6.tar.gz

---
+++ packages/Minetorch/Minetorch-0.1.6.tar.gz/Minetorch-0.1.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Minetorch',
+    description='A tools collection for pytorch users',
+    version='0.1.6',
+    packages=['minetorch'],
+    url="https://github.com/louis-she/minetorch",
+    author='louis',
+    author_email='chenglu.she@gmail.com',
+    keywords='pytorch minecraft'
+)
+
