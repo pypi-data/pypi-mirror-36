213c7743c93e091c44e0a731fdedadb1829e7b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:52 +0000

    Add package wordkit/2.0.1/wordkit-2.0.1.tar.gz

---
+++ packages/wordkit/wordkit-2.0.1.tar.gz/wordkit-2.0.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""Setup file."""
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='wordkit',
+      version='2.0.1',
+      description='Word featurization',
+      author='Stéphan Tulkens',
+      author_email='stephan.tulkens@uantwerpen.be',
+      url='https://github.com/clips/wordkit',
+      license='GPLv3',
+      packages=find_packages(exclude=['examples', 'images']),
+      install_requires=['numpy>=1.11.0',
+                        'ipapy',
+                        'pandas',
+                        'scikit_learn'],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 3'],
+      keywords='machine learning',
+      zip_safe=True,
+      python_requires='>=3')
---
+++ packages/wordkit/wordkit-2.0.1.tar.gz/wordkit-2.0.1/wordkit/samplers/samplers.py
@@ -0,0 +1,83 @@
+"""Sampler class."""
+import numpy as np
+from sklearn.base import TransformerMixin
+
+
+class Sampler(TransformerMixin):
+    """
+    Sample from a list of words, based on the frequency of those words.
+
+    Parameters
+    ----------
+    X : numpy array
+        Your vectorized data.
+
+    words : list of strings.
+        The identities of the words.
+
+    frequencies : numpy array or list of integers, default None.
+        The frequencies of your input data. If this is None, the Sampler
+        will sample uniformly.
+
+    replacement : bool
+        Whether to sample with or without replacement.
+
+    Example
+    -------
+    >>> import numpy as np
+    >>> np.random.seed(44)
+    >>> from wordkit.samplers import Sampler
+    >>> words = ["dog", "cat"]
+    >>> frequencies = [10, 30]
+    >>> s = Sampler(words, frequencies)
+    >>> num_to_sample = 6
+    >>> sampled_data = s.sample(num_to_sample)
+    >>> sampled_data
+    array(['cat', 'dog', 'cat', 'cat', 'cat', 'cat'], dtype='<U3')
+
+    """
+
+    def __init__(self,
+                 X,
+                 frequencies=None,
+                 replacement=True):
+        """Sample from a distribution over words."""
+        if frequencies is None:
+            frequencies = np.ones(len(X))
+        else:
+            frequencies = np.asarray(frequencies)
+        assert(len(frequencies) == len(X))
+
+        self.X = X
+        self.frequencies = frequencies / np.sum(frequencies)
+        self.replacement = replacement
+
+    def sample(self, num_to_sample):
+        """
+        Sample from the featurized data.
+
+        Parameters
+        ----------
+        num_to_sample : int
+            The number of items to sample.
+
+        Returns
+        -------
+        features : np.array
+            A matrix of sampled data.
+
+        """
+        if not self.replacement and num_to_sample > len(self.X):
+            raise ValueError("You tried to sample without replacement from "
+                             "a set which is smaller than your sample size "
+                             ": sample size: {} set size: {}"
+                             "".format(num_to_sample, len(self.X)))
+
+        samples = np.random.choice(np.arange(len(self.X)),
+                                   size=num_to_sample,
+                                   p=self.frequencies,
+                                   replace=self.replacement)
+
+        # We can't use smart indexing because we don't know whether our
+        # base data is an array.
+        return np.take(self.X, samples, axis=0)
