29b93b6e5dcb617c590ad09dae59062c73b909f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:17 +0000

    Add package gp-emulator/1.6.1/gp_emulator-1.6.1.tar.gz

---
+++ packages/gp-emulator/gp_emulator-1.6.1.tar.gz/gp_emulator-1.6.1/gp_emulator/__init__.py
@@ -0,0 +1,7 @@
+
+from .GaussianProcess import GaussianProcess, k_fold_cross_validation
+from .multivariate_gp import MultivariateEmulator
+from .lhd import lhd
+from .emulation_helpers import create_training_set, create_validation_set
+from .emulation_helpers import create_emulator_validation
+from .emulation_helpers import create_single_band_emulators
---
+++ packages/gp-emulator/gp_emulator-1.6.1.tar.gz/gp_emulator-1.6.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gp_emulator',
+      version='1.6.1',
+      description='A Python GaussianProcess emulator software package',
+      classifiers=[
+	'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Environment :: Console'],
+      author='J Gomez-Dans',
+      author_email='j.gomez-dans@ucl.ac.uk',
+      url='http://github.com/jgomezdans/gp_emulator',
+      packages=['gp_emulator'],
+     )
