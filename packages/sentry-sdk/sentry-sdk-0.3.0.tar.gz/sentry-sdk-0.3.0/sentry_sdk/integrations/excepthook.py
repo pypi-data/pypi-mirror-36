5d1de9c13da61c783fb6e9c37c4eb3099ad5a713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:12 +0000

    Add package sentry-sdk/0.3.0/sentry-sdk-0.3.0.tar.gz

---
+++ packages/sentry-sdk/sentry-sdk-0.3.0.tar.gz/sentry-sdk-0.3.0/sentry_sdk/integrations/excepthook.py
@@ -0,0 +1,27 @@
+import sys
+
+from sentry_sdk import capture_exception
+from sentry_sdk.utils import capture_internal_exceptions
+from sentry_sdk.integrations import Integration
+
+
+class ExcepthookIntegration(Integration):
+    identifier = "excepthook"
+
+    def install(self):
+        if hasattr(sys, "ps1"):
+            # Disable the excepthook for interactive Python shells, otherwise
+            # every typo gets sent to Sentry.
+            return
+
+        sys.excepthook = _make_excepthook(sys.excepthook)
+
+
+def _make_excepthook(old_excepthook):
+    def sentry_sdk_excepthook(exctype, value, traceback):
+        with capture_internal_exceptions():
+            capture_exception((exctype, value, traceback))
+
+        return old_excepthook(exctype, value, traceback)
+
+    return sentry_sdk_excepthook
