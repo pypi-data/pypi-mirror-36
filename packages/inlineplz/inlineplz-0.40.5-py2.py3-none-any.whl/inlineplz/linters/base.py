89cc7f81c16bf52716b23b7f416a8117351b4ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:32 +0000

    Add package inlineplz/0.40.5/inlineplz-0.40.5-py2.py3-none-any.whl

---
+++ packages/inlineplz/inlineplz-0.40.5-py2.py3-none-any.whl/inlineplz/linters/base.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+class ParserBase:
+    """Abstract base class for parsers"""
+
+    def parse(self, lint_data):
+        """
+        Parse linter output and return a list of messages.
+        :param str lint_data: linter output in the form of a string
+        :return: a list of Messages
+        :rtype: list
+        """
+        raise NotImplementedError()
