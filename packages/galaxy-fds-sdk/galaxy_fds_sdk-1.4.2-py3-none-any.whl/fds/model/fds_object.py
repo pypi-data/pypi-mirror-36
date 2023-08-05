5d42cf2a5a50d5d5a332a9489602566b90e092f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:37 +0000

    Add package galaxy-fds-sdk/1.4.2/galaxy_fds_sdk-1.4.2-py3-none-any.whl

---
+++ packages/galaxy-fds-sdk/galaxy_fds_sdk-1.4.2-py3-none-any.whl/fds/model/fds_object.py
@@ -0,0 +1,22 @@
+from sys import version_info
+IS_PY3 = version_info[0] >= 3
+
+if IS_PY3:
+  from urllib.request import urlopen
+else:
+  from urllib2 import urlopen
+
+class FDSObject(object):
+  '''
+  The FDS Object class.
+  '''
+  def __init__(self):
+    self.summary = None
+    self.metadata = None
+    self.stream = None
+
+  def get_next_chunk_as_string(self, encoding="UTF-8"):
+    if IS_PY3:
+      return next(self.stream).decode(encoding=encoding)
+    else:
+      return self.stream.next()
