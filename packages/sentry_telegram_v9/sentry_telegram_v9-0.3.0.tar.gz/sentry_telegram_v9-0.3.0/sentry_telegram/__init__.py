83b91048ac1e4685d93999baf35f644b8e1112bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:11 +0000

    Add package sentry_telegram_v9/0.3.0/sentry_telegram_v9-0.3.0.tar.gz

---
+++ packages/sentry_telegram_v9/sentry_telegram_v9-0.3.0.tar.gz/sentry_telegram_v9-0.3.0/sentry_telegram/__init__.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+"""
+Plugin for Sentry which allows sending notification via Telegram messenger.
+"""
+from django.conf import settings
+
+
+__version__ = '0.3.0'
+
+
+if settings.configured:
+    from sentry.plugins import plugins, register
+
+    from plugin import TelegramNotificationsPlugin
+
+    if TelegramNotificationsPlugin.slug not in [plugin.slug for plugin in plugins.all()]:
+        register(TelegramNotificationsPlugin)
---
+++ packages/sentry_telegram_v9/sentry_telegram_v9-0.3.0.tar.gz/sentry_telegram_v9-0.3.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup
+
+import sentry_telegram
+
+
+setup(
+    name='sentry_telegram_v9',
+    version=sentry_telegram.__version__,
+    packages=['sentry_telegram'],
+    url='https://github.com/butorov/sentry-telegram',
+    author='Viacheslav Butorov',
+    author_email='butorovv@gmail.com',
+    description='Plugin for Sentry which allows sending notification via Telegram messenger.',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    include_package_data=True,
+)
