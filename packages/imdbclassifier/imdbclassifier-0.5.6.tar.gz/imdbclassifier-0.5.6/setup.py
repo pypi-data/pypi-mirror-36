c665232495025cb6eba5e9915af1777c0a668af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:29 +0000

    Add package imdbclassifier/0.5.6/imdbclassifier-0.5.6.tar.gz

---
+++ packages/imdbclassifier/imdbclassifier-0.5.6.tar.gz/imdbclassifier-0.5.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='imdbclassifier',
+      version='0.5.6',
+      description='MLflow modules for binary classification in Keras',
+      url='http://github.com/dmatrix/jsd-mflow-examples/keras/imdbclassifier',
+      author='Jules S. Damji',
+      author_email='jules@databricks.com',
+      license='None',
+      packages=['imdbclassifier'],
+      zip_safe=False)
