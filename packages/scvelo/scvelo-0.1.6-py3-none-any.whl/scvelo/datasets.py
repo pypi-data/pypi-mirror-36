ee1cd967f64eb6b93cd6f17ca0ed7ae54b8954ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:21 +0000

    Add package scvelo/0.1.6/scvelo-0.1.6-py3-none-any.whl

---
+++ packages/scvelo/scvelo-0.1.6-py3-none-any.whl/scvelo/datasets.py
@@ -0,0 +1,38 @@
+"""Builtin Datasets.
+"""
+
+from scanpy.api import read
+import numpy as np
+
+
+def dentategyrus():
+    """Dentate Gyrus dataset from Hochgerner et al. (2018).
+    Dentate gyrus is part of the hippocampus involved in learning, episodic memory formation and spatial coding.
+    It is measured using 10X Genomics Chromium and described in Hochgerner et al. (2018).
+    The data consists of 25,919 genes across 3,396 cells and provides several interesting characteristics.
+    """
+    filename = 'data/DentateGyrus/10X43_1.loom'
+    url = 'http://pklab.med.harvard.edu/velocyto/DG1/10X43_1.loom'
+    adata = read(filename, backup_url=url, cleanup=True, sparse=True, cache=True)
+    return adata
+
+
+def forebrain():
+    """Developing human forebrain.
+    Forebrain tissue of a week 10 embryo, focusing on the glutamatergic neuronal lineage.
+    """
+    filename = 'data/ForebrainGlut/hgForebrainGlut.loom'
+    url = 'http://pklab.med.harvard.edu/velocyto/hgForebrainGlut/hgForebrainGlut.loom'
+    adata = read(filename, backup_url=url, cleanup=True, sparse=True, cache=True)
+    adata.var_names_make_unique()
+    return adata
+
+
+def toy_data(n_obs):
+    """Random samples from Dentate Gyrus.
+    """
+    adata = dentategyrus()
+    indices = np.random.choice(adata.n_obs, n_obs)
+    adata = adata[indices]
+    adata.obs_names = np.array(range(adata.n_obs), dtype='str')
+    return adata
---
+++ packages/scvelo/scvelo-0.1.6-py3-none-any.whl/scvelo/plotting/velocity.py
@@ -0,0 +1,84 @@
+from ..preprocessing.moments import second_order_moments
+from .scatter import scatter
+from matplotlib.ticker import MaxNLocator
+import matplotlib.pyplot as pl
+import numpy as np
+
+
+def velocity(adata, var_names=None, basis='umap', mode='deterministic', fits='all', layers='all', color=None,
+             perc=[2,98], fontsize=8, color_map='RdBu_r', size=.2, alpha=.5, dpi=120, ax=None, **kwargs):
+    """Phase and velocity plot for set of genes.
+
+    The phase plot shows pliced against unspliced expressions with steady-state fit.
+    Further the embedding is shown colored by velocity and expression.
+
+    Arguments
+    ---------
+    adata: :class:`~anndata.AnnData`
+        Annotated data matrix.
+    var_names: `str` or list of `str` (default: `None`)
+        Which variables to show.
+    basis: `str` (default: `'umap'`)
+        Key for embedding coordinates.
+    """
+    var_names = [var_names] if isinstance(var_names, str) else var_names
+    var_names = [var for var in var_names if var in adata.var_names[adata.var.velocity_genes]] \
+        if isinstance(var_names, list) else adata.var_names[(adata.layers['spliced'] > 0).sum(0).argsort()[::-1][:4]]
+
+    layers = ['velocity', 'Ms', 'variance_velocity'] if layers == 'all' else layers
+    layers = [layer for layer in layers if layer in adata.layers.keys()]
+
+    fits = adata.layers.keys() if fits == 'all' else fits
+    fits = [fit for fit in fits if all(['velocity' in fit, fit + '_gamma' in adata.var.keys()])]
+
+    n_row, n_col = len(var_names), (1 + len(layers) + (mode == 'stochastic')*2)
+
+    ax = pl.figure(figsize=(3*n_col, 2*n_row), dpi=dpi) if ax is None else ax
+    gs = pl.GridSpec(n_row, n_col, wspace=0.3, hspace=0.5)
+
+    for v, var in enumerate(var_names):
+        ix = np.where(adata.var_names == var)[0][0]
+        s, u = adata.layers['Ms'][:, ix], adata.layers['Mu'][:, ix]
+
+        # spliced/unspliced phase portrait with steady-state estimate
+        ax = pl.subplot(gs[v * n_col])
+        scatter(adata, x=s, y=u, color=color, frameon=True, title=var, xlabel='spliced', ylabel='unspliced',
+                show=False, ax=ax, fontsize=fontsize, size=size, alpha=alpha, **kwargs)
+
+        xnew = np.linspace(0, s.max() * 1.02)
+        for fit in fits:
+            linestyle = '--' if 'stochastic' in fit else '-'
+            pl.plot(xnew, adata.var[fit + '_gamma'][ix] / adata.var[fit + '_beta'][ix] * xnew
+                    + adata.var[fit + '_offset'][ix] / adata.var[fit + '_beta'][ix], c='k', linestyle=linestyle)
+        if v == len(var_names)-1: pl.legend(fits, loc='lower right', prop={'size': .5*fontsize})
+
+        ax.xaxis.set_major_locator(MaxNLocator(nbins=3))
+        ax.yaxis.set_major_locator(MaxNLocator(nbins=3))
+        ax.tick_params(axis='both', which='major', labelsize=.7*fontsize)
+
+        # velocity and expression plots
+        for l, layer in enumerate(layers):
+            ax = pl.subplot(gs[v*n_col + l + 1])
+            title = 'expression' if layer == 'Ms' else layer
+            scatter(adata, basis=basis, color=var, layer=layer, color_map=color_map, title=title,
+                    perc=perc, fontsize=fontsize, size=size, alpha=alpha, show=False, ax=ax, **kwargs)
+
+        if mode == 'stochastic' is not None:
+            ss, us = second_order_moments(adata)
+
+            ax = pl.subplot(gs[v*n_col + len(layers) + 1])
+            x = 2 * (ss - s**2) - s
+            y = 2 * (us - u * s) + u + 2 * s * \
+                adata.var['stochastic_velocity_offset'][ix] / adata.var['stochastic_velocity_beta'][ix]
+
+            scatter(adata, x=x, y=y, color=color, title=var, fontsize=40/n_col, show=False, ax=ax,
+                    perc=perc, xlabel=r'2 $\Sigma_s - \langle s \rangle$', ylabel=r'2 $\Sigma_{us} + \langle u \rangle$', **kwargs)
+
+            xnew = np.linspace(x.min(), x.max() * 1.02)
+            fits = adata.layers.keys() if fits == 'all' else fits
+            fits = [fit for fit in fits if 'velocity' in fit]
+            for fit in fits:
+                linestyle = '--' if 'stochastic' in fit else '-'
+                pl.plot(xnew, adata.var[fit + '_gamma'][ix] / adata.var[fit + '_beta'][ix] * xnew +
+                        adata.var[fit + '_offset2'][ix] / adata.var[fit + '_beta'][ix], c='k', linestyle=linestyle)
+            if v == len(var_names) - 1: pl.legend(fits, loc='lower right', prop={'size': 34/n_col})
