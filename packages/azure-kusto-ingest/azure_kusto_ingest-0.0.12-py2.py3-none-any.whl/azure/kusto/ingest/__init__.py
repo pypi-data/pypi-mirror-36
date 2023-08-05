34a07a59c83530d2d07f1c737d57e684d51d455c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:00 +0000

    Add package azure-kusto-ingest/0.0.12/azure_kusto_ingest-0.0.12-py2.py3-none-any.whl

---
+++ packages/azure-kusto-ingest/azure_kusto_ingest-0.0.12-py2.py3-none-any.whl/azure/kusto/ingest/__init__.py
@@ -0,0 +1,15 @@
+from ._ingest_client import KustoIngestClient
+from ._descriptors import BlobDescriptor, FileDescriptor
+from ._ingestion_properties import (
+    DataFormat,
+    ValidationPolicy,
+    ValidationImplications,
+    ValidationOptions,
+    ReportLevel,
+    ReportMethod,
+    CsvColumnMapping,
+    JsonColumnMapping,
+    IngestionProperties,
+)
+
+from ._version import VERSION as __version__
