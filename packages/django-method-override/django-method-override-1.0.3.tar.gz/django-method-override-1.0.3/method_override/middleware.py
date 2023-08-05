c987ad31037d5f066282f86f631d84f890600591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:09 +0000

    Add package django-method-override/1.0.3/django-method-override-1.0.3.tar.gz

---
+++ packages/django-method-override/django-method-override-1.0.3.tar.gz/django-method-override-1.0.3/method_override/middleware.py
@@ -0,0 +1,24 @@
+from method_override import settings
+
+
+class MethodOverrideMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        if request.method != 'POST':
+            return None
+        method = self._get_method_override(request)
+        if method in settings.ALLOWED_HTTP_METHODS:
+            request.method = method
+
+    def _get_method_override(self, request):
+        method = (
+            request.POST.get(settings.PARAM_KEY) or
+            request.META.get(settings.HTTP_HEADER)
+        )
+        return method and method.upper()
