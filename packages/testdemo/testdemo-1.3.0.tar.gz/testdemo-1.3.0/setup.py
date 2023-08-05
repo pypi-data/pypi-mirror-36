05572af02bbb21bb92bb32626302533508b833e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:22 +0000

    Add package testdemo/1.3.0/testdemo-1.3.0.tar.gz

---
+++ packages/testdemo/testdemo-1.3.0.tar.gz/testdemo-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name        = 'testdemo',
+        version     = '1.3.0',
+        author      = 'huangqihao',
+        url         = '',
+        author_email= 'huangqihao8@gmail.com,',
+        description = 'A simple demo of nested lists',
+    )
