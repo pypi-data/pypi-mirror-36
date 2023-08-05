8d7511f99d3bfd2a908ec12adaf7fe40004a93fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:33 +0000

    Add package curlnagios/0.6.0/curlnagios-0.6.0-py3-none-any.whl

---
+++ packages/curlnagios/curlnagios-0.6.0-py3-none-any.whl/curlnagios-0.6.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,130 @@
+Title1
+======
+
+Checks an url raise an alert if some problem is found.
+Uses curl with all its power, so you can extend your check with all curl options.
+
+`VERSION  <burp_reports/VERSION>`__
+
+Install
+=======
+
+Linux::
+
+    sudo pip3 install curlnagios --upgrade
+
+Also is possible to use::
+
+    sudo python3 -m pip install curlnagios --upgrade
+
+On windows with python3.5::
+
+    pip install curlnagios --upgrade
+
+For proxies add::
+
+    --proxy='http://user:passw@server:port'
+
+Usage
+=====
+
+Use the command line::
+
+    > curlnagios --help
+      usage: curlnagios [-h] [-u [URL]] [-e [EXTRA_ARGS]]
+
+        optional arguments:
+        -h, --help            show this help message and exit
+        -u [URL], --url [URL]
+                                url to check 
+        -e [EXTRA_ARGS], --extra_args [EXTRA_ARGS]
+                                extra args to add to curl, see `curl manpage <https://curl.haxx.se/docs/manpage.html>`_.
+
+
+Example usage
+=============
+
+Example use with proxy::
+
+    > curlnagios --url='http://xx/dd' --extra_args='--proxy http://user:pass@host:8080'
+
+Example using proxy, and authentication with ntlm for the website:
+
+    > curlnagios --url='http://xx/dd' --extra_args='--proxy http://user:pass@host:8080 --user user:pass --ntlm'
+
+All the extra_args are options directly comming from curl manpage, you can use almost any 
+with exception of -s, -o, -w as these are
+implicit added on the curl command line argument to format the output for this plugin.
+
+Example usage with AzureAD oauth2
+=================================
+
+When creating authentication with AzureAD oauth2, you need to create a client Application and Azure AD only applications:
+
+https://apps.dev.microsoft.com/#/appList
+
+Then use:
+
+    > curlnagios --url 'https://{yoururltotest}/api/path' --client_id 'unique-client-id' --scope 'https://{tenant}/unique-id-here-for-the-app/.default' --client_secret 'theclientoken' --grant_type 'client_credentials' --auth_url 'https://login.microsoftonline.com/{tenant}/oauth2/v2.0/token' --oauth2
+
+Change {tenant} with your identifier and the unique id for client and for application.
+Add your client secret
+
+This comes from documentation using token: https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow#request-an-access-token
+
+Nagios config
+=============
+
+Example command::
+
+    define command{
+        command_name  check_http_curl
+        command_line  /usr/local/bin/curlnagios --url='$ARG1$' --extra_args='$ARG2$'
+    }
+
+    define command{
+        command_name  check_http_curl_azuread
+        command_line  /usr/local/bin/curlnagios --url='$ARG1$' --client_id '$ARG2$' --scope '$ARG3$' --client_secret '$ARG4$' --auth_url 'https://login.microsoftonline.com/$ARG5$/oauth2/v2.0/token' --oauth2 --extra_args='$ARG6$'
+    }
+
+Example service::
+
+    define service {
+            host_name                       SERVERX
+            service_description             service_name
+            check_command                   check_http_curl!http://url/path!--proxy http://user:name@host:8080 --user user:name --ntlm
+            use				                generic-service
+            notes                           some useful notes
+    }
+
+Example service bypassing reverse proxy and dns and proxy server::
+
+    define service {
+            host_name                       SERVERY
+            service_description             fqdn.backend1
+            check_command                   check_http_curl!http://192.168.10.10:80!--noproxy "192.168.10.10" -H "Host: fqdn.site.name"
+            use				                generic-service
+            notes                           Monitoring backend1 de of site fqdn.site.name
+    }
+
+     ## In this way you can connect to some backend and pass with -H the host header to get and also ensure no proxy used to connect to url.
+
+Example using azuread oauth2::
+
+    define service {
+            host_name                       SERVERY
+            service_description             fqdn.backend1
+            check_command                   check_http_curl_azuread!http://fqdn.site.name/api/xx!client-unique-id!https://{tenant}/unique-id-here-for-the-app/.default!client-secret-unique!tenant!some extra args if desired
+            use				                generic-service
+            notes                           Monitoring backend1 de of site fqdn.site.name
+    }
+
+You can use ansible role that already has the installation and command: https://github.com/CoffeeITWorks/ansible_nagios4_server_plugins
+
+TODO
+====
+
+* Use hash passwords
+* Add Unit tests?
+
+
---
+++ packages/curlnagios/curlnagios-0.6.0-py3-none-any.whl/curlnagios/__main__.py
@@ -0,0 +1,154 @@
+#!python3
+# https://nagios-plugins.org/doc/guidelines.html
+
+# Import required libs for sharepointhealth
+from .plugin_check import curlCheck # Change this to your own class
+from .oauth2_token import GetOauth2Token as Azureoauth
+import argparse
+import sys
+
+
+# Return codes expected by Nagios
+OK = 0
+WARNING = 1
+CRITICAL = 2
+UNKNOWN = 3
+
+# Return message
+message = {
+    'status': OK,
+    'summary': 'Example summary',
+    'perfdata': 'label1=0;;;; '  # 'label'=value[UOM];[warn];[crit];[min];[max] 
+}
+
+# For multiple perdata, ensure to add space after each perfdata
+# message['perfdata'] = 'label1=x;;;; '
+# message['perfdata'] += 'label2=x;;;; '
+
+# Function to parse arguments
+def parse_args(args):
+    """
+    Information extracted from: https://mkaz.com/2014/07/26/python-argparse-cookbook/
+     https://docs.python.org/3/library/argparse.html
+    :return: parse.parse_args(args) object
+    You can use obj.option, example:
+    options = parse_args(args)
+    options.user # to read username
+    """
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, 
+                                     description='nagios plugin to check some url using curl and some other code')
+
+    parser.add_argument('-u', '--url', dest='url', nargs='?', default=None, const=None,
+                        help='url to check \n')
+    parser.add_argument('-e', '--extra_args', dest='extra_args', nargs='?', default='', const=None,
+                            help='extra args to add to curl, see curl manpage  \n')
+    
+    # Arguments to check using OAuth2
+    parser.add_argument('--client_id', dest='client_id', nargs='?', default=None, const=None,
+                            help='oauth2 client_id example client id: 6731de76-14a6-49ae-97bc-6eba6914391e \n')
+    parser.add_argument('--scope', dest='scope', nargs='?', default=None, const=None,
+                            help='oauth2 scope  https://company.onmicrosoft.com/some-unique-number-for-scope/.default \n')
+    parser.add_argument('--client_secret', dest='client_secret', nargs='?', default=None, const=None,
+                            help='oauth2 client_secret client password \n')
+    parser.add_argument('--grant_type', dest='grant_type', nargs='?', default='client_credentials', const=None,
+                            help='oauth2 grant_type \n')
+    parser.add_argument('--auth_url', dest='auth_url', nargs='?', default=None, const=None,
+                            help='oauth2 auth_url example: https://login.microsoftonline.com/company.onmicrosoft.com/oauth2/v2.0/token \n')
+    parser.add_argument('--oauth2', action='store_true',
+                            help='''Flag to use or not token for oauth2 before creating the request, used to check published services that uses azure oauth2 \n
+                                    See https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow#refresh-the-access-token \n''')
+
+    if not args:
+        raise SystemExit(parser.print_help())
+
+    return parser.parse_args(args)
+
+# Function to execute cli commands
+def cli_execution(options):
+    """
+    : param: options: arguments from parse.parse_args(args) (see parse_args function)
+    """
+    auth_args = ''
+    # Creating access_token to authenticate with azure oauth2
+    if options.oauth2:
+        if not options.client_id:
+            sys.exit('param client_id is required  when using oauth2')
+        if not options.scope:
+            sys.exit('param scope is required  when using oauth2')
+        if not options.client_secret:
+            sys.exit('param client_secret is required  when using oauth2')
+        if not options.auth_url:
+            sys.exit('param auth_url is required  when using oauth2')
+        
+        oauth_obj = Azureoauth(client_id = options.client_id,
+                               scope = options.scope,
+                               client_secret = options.client_secret,
+                               grant_type = options.grant_type,
+                               auth_url = options.auth_url)
+        auth_tuple = oauth_obj.get_token()
+        auth_http_code = auth_tuple[1]
+        if auth_http_code != 200:
+            sys.exit('Error getting access_token, http_code != 200, http_code: {}'.format(auth_http_code))
+        
+        access_token = auth_tuple[0]['access_token']
+        header_token = {"Authorization": "Bearer {}".format(access_token)}
+        # https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow#use-the-access-token
+        auth_args = "--header 'Authorization: {}'".format(header_token['Authorization'])
+    
+    curlnagiosobj = curlCheck(URL=options.url,
+                              extra_args=options.extra_args,
+                              auth_args=auth_args)
+
+    def collect_data():
+        # use new object class HealthMonitor
+        retrcode, retroutput, retrdict = curlnagiosobj.collect_data()
+        return retrcode, retroutput, retrdict
+    
+    def format_message():
+        return 'url: {} '.format(options.url)
+
+    def check(retrcode):
+        if retrcode >= 2:
+            status = CRITICAL
+            message['summary'] = 'CRITICAL: '
+        elif retrcode == 1:
+            status = WARNING
+            message['summary'] = 'WARNING: '
+        else:
+            status = OK
+            message['summary'] = 'OK: '
+        return status
+
+    # Check logic starts here
+    data = collect_data()
+    message['status'] = check(data[0])
+    # Add summary
+    message['summary'] += format_message()
+    # Add perfdata
+    # total = len(data)
+    message['perfdata'] = curlnagiosobj.format_perfdata()
+
+    # Print the message
+    print("{summary}|{perfdata}".format(
+        summary=message.get('summary'),
+        perfdata=message.get('perfdata')
+    ))
+
+    # Exit with status code
+    raise SystemExit(message['status'])
+
+# Argument parser
+# https://docs.python.org/3.5/library/argparse.html
+
+def main():
+    """
+    Main function
+    """
+    # Get options with argparse
+    options = parse_args(sys.argv[1:])
+    # Execute program functions passing the options collected
+    cli_execution(options)
+
+
+if __name__ == "__main__":
+    main()
