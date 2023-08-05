e1497775c832c1aac5e87f69e5512c45de03638b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:12 +0000

    Add package telebaka-faq/0.1.7/telebaka_faq-0.1.7-py3-none-any.whl

---
+++ packages/telebaka-faq/telebaka_faq-0.1.7-py3-none-any.whl/telebaka_faq/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from bots.models import TelegramBot
+
+
+class FAQSection(models.Model):
+    hidden = models.BooleanField(default=False)
+    command = models.CharField(max_length=64, unique=True, db_index=True)
+    title = models.CharField(max_length=128, unique=True, db_index=True)
+    text = models.TextField()
+    bot = models.ForeignKey(TelegramBot, on_delete=models.SET_NULL, null=True,
+                            limit_choices_to={'plugin_name': 'telebaka_faq'})
+
+    def __str__(self):
+        return self.title
+
+
+
+
