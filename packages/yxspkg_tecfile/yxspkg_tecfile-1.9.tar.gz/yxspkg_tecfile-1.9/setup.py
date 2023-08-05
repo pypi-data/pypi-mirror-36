86a9043a6861f1fe93aeae8386b54a5fbff01dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:12 +0000

    Add package yxspkg_tecfile/1.9/yxspkg_tecfile-1.9.tar.gz

---
+++ packages/yxspkg_tecfile/yxspkg_tecfile-1.9.tar.gz/yxspkg_tecfile-1.9/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup 
+setup(name='yxspkg_tecfile',   
+      version='1.9',    
+      description='A simple API to prase some CFD data file such as ".prof(fluent)", ".txt" etc.',    
+      author='Blacksong',    
+      install_requires=[],
+      py_modules=['yxspkg_tecfile'],
+      platforms='any',
+      author_email='blacksong@yeah.net',       
+      url='https://github.com/blacksong',
+      classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+)   
