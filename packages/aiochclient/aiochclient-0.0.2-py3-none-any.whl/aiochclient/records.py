c925815f6bbb4c69e85d8049f9c820fa8f69cc94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:14 +0000

    Add package aiochclient/0.0.2/aiochclient-0.0.2-py3-none-any.whl

---
+++ packages/aiochclient/aiochclient-0.0.2-py3-none-any.whl/aiochclient/records.py
@@ -0,0 +1,18 @@
+from aiochclient.types import what_py_type
+
+
+__all__ = ["RecordsFabric"]
+
+
+class RecordsFabric:
+
+    __slots__ = ("tps",)
+
+    def __init__(self, tps: bytes):
+        self.tps = [what_py_type(tp) for tp in tps.decode().strip().split("\t")]
+
+    def new(self, vls: bytes):
+        vls = vls[:-1]
+        if not vls:  # In case of empty row
+            return ()
+        return tuple(typ.convert(val) for typ, val in zip(self.tps, vls.split(b"\t")))
