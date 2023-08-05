2d3c0caf70d57bba110b1c972ddca5f1204f251a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:12 +0000

    Add package bamboo-ipa-sync/2.0.0/bamboo_ipa_sync-2.0.0-py2.py3-none-any.whl

---
+++ packages/bamboo-ipa-sync/bamboo_ipa_sync-2.0.0-py2.py3-none-any.whl/bamboo_ipa_sync/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Python Template
+
+Author: Peter Pakos <peter.pakos@wandisco.com>
+
+Copyright (C) 2018 WANdisco
+"""
+
+from .__version__ import __version__
---
+++ packages/bamboo-ipa-sync/bamboo_ipa_sync-2.0.0-py2.py3-none-any.whl/bamboo_ipa_sync/__main__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Python Template
+
+Author: Peter Pakos <peter.pakos@wandisco.com>
+
+Copyright (C) 2018 WANdisco
+"""
+
+from __future__ import absolute_import
+
+from bamboo_ipa_sync.bamboo_ipa_sync import main
+
+if __name__ == '__main__':
+    main()
