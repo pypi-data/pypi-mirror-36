43c99b8a3ac72d073f5edc0b26fdb53a24fd9235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:15 +0000

    Add package pyMd2Doc/0.1.2/pyMd2Doc-0.1.2.tar.gz

---
+++ packages/pyMd2Doc/pyMd2Doc-0.1.2.tar.gz/pyMd2Doc-0.1.2/README.rst
@@ -0,0 +1,30 @@
+# 1. Introduction of pyMd2Doc
+
+
+
+
+# 2. use method
+
+
+
+## 2.1 prepare Markdown document
+
+Copy and paste the prepared Markdown file that needs to be documented into the pyMd2Doc / APP / title. MD file and save it.
+
+
+
+## 2.2 conversion
+
+Double click to execute the start.bat file, which does two operations.
+
+Installation dependent needs
+
+Converting Markdown files into documents with directory structure
+
+
+
+## 2.3 view document
+
+Open the newly generated title.html file with browser.
+
+Click the directory to jump to the contents of the corresponding document.
---
+++ packages/pyMd2Doc/pyMd2Doc-0.1.2.tar.gz/pyMd2Doc-0.1.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+ 
+setup(
+    name="pyMd2Doc",
+    version="0.1.2",
+    author="Yule Meng",
+    author_email="88914511@qq.com",
+    description="add whl file",
+    long_description=open("README.rst").read(),
+    license="MIT",
+    url="https://github.com/yuleMeng/pyMarkdown",
+    packages=['app'],
+    install_requires=[
+        "Markdown"
+        ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"
+    ],
+)
