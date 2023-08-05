4dd01f51c523dd11c44da875af0ea469c8407627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:46 +0000

    Add package restmagic/0.5.1/restmagic-0.5.1.tar.gz

---
+++ packages/restmagic/restmagic-0.5.1.tar.gz/restmagic-0.5.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, convert_path
+
+main_ns = {}
+with open(convert_path('restmagic/version.py')) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+with open(convert_path('README.rst')) as readme_file:
+    long_description = readme_file.read()
+
+
+setup(
+    name='restmagic',
+    version=main_ns['__version__'],
+    packages=['restmagic'],
+    description='HTTP REST magic for IPython',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author='b3b',
+    author_email='ash.b3b@gmail.com',
+    install_requires=[
+        'ipython>=1.0',
+        'requests-toolbelt>=0.8.0',
+        'six',
+    ],
+    url='https://github.com/b3b/ipython-restmagic',
+    # https://pypi.org/classifiers/
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Framework :: IPython',
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+    ],
+    keywords='magic rest http ipython jupyter',
+    license='MIT',
+    zip_safe=False,
+)
