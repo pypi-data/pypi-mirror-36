28006254fd6d4fad590cd64559cb6b12624df06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:32 +0000

    Add package nimbus-chart/0.1.9/nimbus_chart-0.1.9-py2.py3-none-any.whl

---
+++ packages/nimbus-chart/nimbus_chart-0.1.9-py2.py3-none-any.whl/nimbus_chart/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from datetime import date
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__author_email__",
+    "__url__",
+    "__platforms__",
+    "__license__",
+    "__copyright__",
+]
+
+__version__ = '0.1.9'
+__author__ = "william"
+__author_email__ = "william.ren@live.cn"
+__url__ = "https://github.com/williamren"
+__platforms__ = "Any"
+__license__ = "Apache License 2.0"
+__copyright__ = "Copyright 2001-2017 william william.ren@live.cn"
+
