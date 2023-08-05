abf9c8b6efbb8ed91f9686b199031d4daf683235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:06 +0000

    Add package mysql_autoxtrabackup/1.5.3/mysql-autoxtrabackup-1.5.3.tar.gz

---
+++ packages/mysql_autoxtrabackup/mysql-autoxtrabackup-1.5.3.tar.gz/mysql-autoxtrabackup-1.5.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from general_conf import path_config
+
+datafiles = [(path_config.config_path, ['general_conf/autoxtrabackup.cnf'])]
+
+setup(
+    name='mysql-autoxtrabackup',
+    version='1.5.3',
+    packages=['general_conf', 'backup_prepare', 'partial_recovery', 'master_backup_script', 'prepare_env_test_mode'],
+    package_data={
+        'prepare_env_test_mode': ['*.sh', '*.sql']
+    },
+    py_modules=['autoxtrabackup'],
+    url='https://github.com/ShahriyarR/MySQL-AutoXtraBackup',
+    download_url='https://github.com/ShahriyarR/MySQL-AutoXtraBackup/archive/v1.5.3.zip',
+    license='MIT',
+    author='Shahriyar Rzayev',
+    author_email='rzayev.shahriyar@yandex.com',
+    description='Commandline tool written in Python 3 for using Percona XtraBackup',
+    install_requires=[
+        'click>=3.3',
+        'mysql-connector==2.1.4',
+        'pid>=2.0',
+        'humanfriendly>=2.0',
+        'pytest'
+    ],
+    dependency_links=['https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.4.tar.gz'],
+    entry_points='''
+        [console_scripts]
+        autoxtrabackup=autoxtrabackup:all_procedure
+    ''',
+    data_files=datafiles,
+)
