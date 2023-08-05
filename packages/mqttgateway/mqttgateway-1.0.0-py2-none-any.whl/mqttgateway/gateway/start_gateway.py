aa25204f5e235d4bb4cc81df9662ace4c9e07d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:18 +0000

    Add package mqttgateway/1.0.0/mqttgateway-1.0.0-py2-none-any.whl

---
+++ packages/mqttgateway/mqttgateway-1.0.0-py2-none-any.whl/mqttgateway/gateway/start_gateway.py
@@ -0,0 +1,157 @@
+''' Defines the function that starts the gateway.
+
+.. Reviewed 30 May 2018.
+'''
+
+import json
+import traceback
+
+import mqttgateway.gateway.mqtt_client as mqtt
+import mqttgateway.utils.app_properties as app
+import mqttgateway.gateway.mqtt_map as mqtt_map
+
+from mqttgateway.utils.load_config import loadconfig
+from mqttgateway.utils.init_logger import initlogger
+
+from mqttgateway.gateway.configuration import CONFIG
+
+_logger = app.Properties.get_logger(__name__)
+
+def startgateway(gateway_interface):
+    ''' Entry point.'''
+    try:
+        _startgateway(gateway_interface)
+    except:
+        _logger.error(''.join(('Fatal error: ', traceback.format_exc())))
+        raise
+
+def _startgateway(gateway_interface):
+    '''
+    Initialisation of the application and main loop.
+
+    Initialises the configuration and the logger, starts the interface,
+    starts the MQTT communication then starts the main loop.
+
+    The data files are:
+
+    - the configuration file (compulsory), which is necessary at least to define the MQTT broker;
+      a path to it can be provided as first argument of the command line, or the default path
+      will be used;
+    - the map file (optional), if the mapping option is enabled.
+
+    The rules for providing paths of files are available in the configuration file template as
+    a comment. The same rules apply to the command line argument and to the paths provided in the
+    configuration file.
+
+    Args:
+        gateway_interface (class (not an instance of it!)): the interface class
+    '''
+
+    # Load the configuration ======================================================================
+    cfg = loadconfig(CONFIG, app.Properties.config_file_path)
+
+    # Initialise the logger handlers ==============================================================
+    logfilename = cfg.get('LOG', 'logfilename')
+    if not logfilename: logfilepath = None
+    else: logfilepath = app.Properties.get_path(logfilename, extension='.log')
+    # create the tuple to send the arguments to initlogger
+    logfiledata = (cfg.get('LOG', 'consolelevel'),
+                   logfilepath,
+                   cfg.get('LOG', 'filelevel'),
+                   cfg.get('LOG', 'filenum'),
+                   cfg.get('LOG', 'filesize')
+                    )
+    emaildata = (cfg.get('LOG', 'emailhost'), cfg.get('LOG', 'emailport'),
+                 cfg.get('LOG', 'emailaddress'), app.Properties.name)
+    initlogger(app.Properties.root_logger, logfiledata, emaildata)
+    #logger = app.Properties.get_logger(__name__)
+
+    # Log the configuration used ==================================================================
+    _logger.info('=== APPLICATION STARTED ===')
+    _logger.info('Configuration:')
+    for section in cfg.sections():
+        for option in cfg.options(section):
+            _logger.info(''.join(('   [', section, '].', option, ' : <',
+                                 str(cfg.get(section, option)), '>.')))
+
+    # Exit in case of error processing the configuration file.
+    if cfg.has_section('CONFIG') and cfg.has_option('CONFIG', 'error'):
+        _logger.critical(''.join(('Error while processing the configuration file:\n\t',
+                                 cfg.get('CONFIG', 'error'))))
+        raise SystemExit
+
+    # Instantiate the gateway interface ===========================================================
+    # Create the dictionary of the parameters for the interface from the configuration file
+    interfaceparams = {}
+    for option in cfg.options('INTERFACE'):
+        interfaceparams[option] = str(cfg.get('INTERFACE', option))
+    # Create 2 message lists, one incoming, the other outgoing
+    msglist_in = mqtt_map.MsgList()
+    msglist_out = mqtt_map.MsgList()
+    gatewayinterface = gateway_interface(interfaceparams, msglist_in, msglist_out)
+
+    # Load the map data ===========================================================================
+    mapping_flag = cfg.getboolean('MQTT', 'mapping')
+    mapfilename = cfg.get('MQTT', 'mapfilename')
+    if mapping_flag and mapfilename:
+        mapfilepath = app.Properties.get_path(mapfilename, extension='.map')
+        try:
+            with open(mapfilepath, 'r') as mapfile:
+                map_data = json.load(mapfile)
+        except (OSError, IOError) as err:
+            _logger.critical(''.join(('Error loading map file <', mapfilepath, '>:/n/t', str(err))))
+            raise SystemExit
+        except ValueError as err:
+            _logger.critical(''.join(('Error reading JSON file <', mapfilepath, '>:/n/t', str(err))))
+            raise SystemExit
+    else: # use default map - take root and topics from configuration file
+        mqtt_map.NO_MAP['root'] = cfg.get('MQTT', 'root')
+        mqtt_map.NO_MAP['topics'] = [topic.strip() for topic in cfg.get('MQTT', 'topics').split(',')]
+        map_data = None
+    try: messagemap = mqtt_map.msgMap(map_data) # will raise ValueErrors if problems
+    except ValueError as err:
+        _logger.critical(''.join(('Error processing map file <', mapfilepath, '>:/n/t', str(err))))
+
+    # Initialise the MQTT client and connect ======================================================
+    # Define the function that will be called by the on_message MQTT call-back
+    def process_mqttmsg(mqtt_msg):
+        ''' Converts a MQTT message into an internal message and pushes it on the message list.
+
+        Args:
+            mqtt_msg (:class:`mqtt.Message`): incoming MQTT message.
+        '''
+        try: internal_msg = messagemap.mqtt2internal(mqtt_msg)
+        except ValueError as err:
+            _logger.info(str(err))
+            return
+        msglist_in.push(internal_msg)
+        return
+
+    timeout = cfg.getfloat('MQTT', 'timeout') # for the MQTT loop() method
+    client_id = cfg.get('MQTT', 'clientid')
+    if not client_id: client_id = app.Properties.name
+    mqttclient = mqtt.mgClient(host=cfg.get('MQTT', 'host'),
+                               port=cfg.getint('MQTT', 'port'),
+                               keepalive=cfg.getint('MQTT', 'keepalive'),
+                               client_id=client_id,
+                               on_msg_func=process_mqttmsg,
+                               topics=messagemap.topics)
+
+    # Main loop ===================================================================================
+    while True:
+
+        # Call the MQTT loop.
+        mqttclient.loop(timeout)
+
+        # Call the interface loop.
+        gatewayinterface.loop()
+
+        # Publish the messages returned, if any.
+        while True:
+            internal_msg = msglist_out.pull()
+            if internal_msg is None: break
+            try: mqtt_msg = messagemap.internal2mqtt(internal_msg)
+            except ValueError as err:
+                _logger.info(str(err))
+                continue
+            mqttclient.publish(mqtt_msg.topic, mqtt_msg.payload, qos=0, retain=False)
---
+++ packages/mqttgateway/mqttgateway-1.0.0-py2-none-any.whl/mqttgateway/utils/app_properties.py
@@ -0,0 +1,109 @@
+''' Application wide properties.
+
+.. reviewed 30 May 2018
+
+This module is an alternative to a singleton.
+It keeps some application variables in the module namespace making them effectively global.
+At startup, the module should be imported straight away so that it creates an AppProperties object
+called Properties that is global, and initialised with mostly empty values, except the ``init``
+attribute which is essentially the constructor.
+'''
+
+from collections import namedtuple
+import logging
+import os.path
+import sys
+
+_THIS = sys.modules[__name__]
+
+AppProperties = namedtuple('AppProperties', ('name',
+                                             'directories',
+                                             'config_file_path',
+                                             'root_logger',
+                                             'init',
+                                             'get_path',
+                                             'get_logger'))
+
+def _dummy(*args, **kwargs):
+    ''' Do nothing.'''
+    pass
+
+def _get_logger(fullmodulename):
+    ''' Returns the logger for the module in argument.
+
+    Resolves the case where the module name is ``__main__``.
+
+    Args:
+        fullmodulename (string): normally it is the __name__ of the calling module.
+    '''
+    if fullmodulename == '__main__' or fullmodulename == _THIS.Properties.name:
+        logname = _THIS.Properties.name
+    else:
+        modulename = fullmodulename.split('.')[-1]
+        if not modulename: logname = _THIS.Properties.name
+        else: logname = '.'.join((_THIS.Properties.name, modulename))
+    return logging.getLogger(logname)
+
+def _get_path(path_given, extension=None, app_name=None, app_dirs=None):
+    ''' Returns the absolute path of a file following the application rules.
+
+    Rules:
+
+   - the default name is app_name + extension;
+   - the default directories are provided by the app_dirs argument;
+   - file paths can be directory only (ends with a '/') and are appended with the default name;
+   - file paths can be absolute or relative; absolute start with a '/' and
+     relative are prepended with the default directory;
+   - file paths can be file only (no '/' whatsoever) and are prepended with the default directory;
+   - use forward slashes '/' in any case, even for Windows systems, it should work;
+   - however for Windows systems, use of the drive letter might be an issue and has not been tested.
+
+    Args:
+        path_given (string): any type pf path; see rules
+        extension (string): the default extension of the file
+        app_name (string): the application name for the defaults
+        app_dirs (list of strings): the default directories where to look for the files
+    '''
+    if app_name is None: app_name = _THIS.Properties.name
+    if app_dirs is None: app_dirs = _THIS.Properties.directories
+    if not extension: extension = ''
+    elif extension[0] != '.': extension = '.' + extension
+    if not path_given or path_given == '.': path_given = './'
+    if path_given == '..': path_given = '../'
+    dirname, filename = os.path.split(path_given.strip())
+    if filename == '': filename = ''.join((app_name, extension)) # default name
+    dirname = os.path.normpath(dirname) # not sure it is necessary
+    if os.path.isabs(dirname):
+        return os.path.normpath(os.path.join(dirname, filename))
+    else: # dirname is relative
+        paths = [os.path.normpath(os.path.join(pth, dirname, filename)) for pth in app_dirs]
+        for pth in paths:
+            if os.path.exists(pth): return pth
+        return paths[0] # even if it will fail, return the first path in the list
+
+def _init_properties(app_path=None, app_name=None):
+    ''' Initialisation of the properties object.
+
+    This is supposed to work like a constructor of the *class-like* object ``AppProperties``.
+    Once called, it replaces the ``Properties`` instance with another instance initialised with
+    the right values.
+    The ``Properties`` object is a namedtuple to emphasize the fact that these are variables that
+    should not change.  As usual, you can still mess with those but hopefully the barrier to mess
+    is higher.
+     '''
+    if not app_name:
+        app_name = os.path.splitext(os.path.basename(app_path))[0] # first part of the filename, without extension
+    script_dir = os.path.realpath(os.path.dirname(app_path)) # full path of directory of launching script
+    current_working_dir = os.getcwd()
+    # Find configuration file
+    if len(sys.argv) >= 2: pathgiven = sys.argv[1].strip()
+    else: pathgiven = '' # default location in case no file name or path is given
+    config_file_path = _get_path(pathgiven, extension='.conf', app_name=app_name,
+                                 app_dirs=(current_working_dir, script_dir))
+    config_file_dir = os.path.dirname(config_file_path)
+    dirs = (config_file_dir, current_working_dir, script_dir)
+    root_logger = logging.getLogger(app_name)
+    _THIS.Properties = AppProperties(app_name, dirs, config_file_path, root_logger,
+                                     _dummy, _get_path, _get_logger)
+
+Properties = AppProperties('', [], '', None, _init_properties, _dummy, _dummy)
