d8c85e146405be0385fc99278e1a1aacb2174a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:07 +0000

    Add package pyclickup/0.0.5/pyclickup-0.0.5.tar.gz

---
+++ packages/pyclickup/pyclickup-0.0.5.tar.gz/pyclickup-0.0.5/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+pip setup file
+"""
+from pyclickup.globals import __version__, LIBRARY
+from setuptools import setup, find_packages
+from m2r import convert
+
+
+with open("README.md") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name=LIBRARY,
+    version=__version__,
+    description="A python wrapper for the ClickUp API",
+    long_description=convert(long_description),
+    author="Jacobi Petrucciani",
+    author_email="jacobi@mimirhq.com",
+    keywords="clickup python",
+    url="https://github.com/jpetrucciani/{}.git".format(LIBRARY),
+    download_url="https://github.com/jpetrucciani/{}.git".format(LIBRARY),
+    license="LICENSE",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    zip_safe=False,
+)
