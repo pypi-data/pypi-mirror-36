2cbbdb665de658370e99f488d66cd27f1645e6a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:36 +0000

    Add package clove/1.2.8/clove-1.2.8-py3-none-any.whl

---
+++ packages/clove/clove-1.2.8-py3-none-any.whl/clove/network/bitcoin/__init__.py
@@ -0,0 +1,53 @@
+from clove.block_explorer.blockcypher import BlockcypherAPI
+from clove.network.bitcoin.base import BitcoinBaseNetwork
+
+
+class Bitcoin(BlockcypherAPI, BitcoinBaseNetwork):
+    """
+    Class with all the necessary BTC network information based on
+    https://github.com/bitcoin/bitcoin/blob/master/src/chainparams.cpp
+    (date of access: 01/18/2018)
+    """
+    name = 'bitcoin'
+    symbols = ('BTC', 'XBT')
+    seeds = (
+        'seed.bitcoin.sipa.be',
+        'dnsseed.bluematt.me',
+        'dnsseed.bitcoin.dashjr.org',
+        'seed.bitcoinstats.com',
+        'seed.bitcoin.jonasschnelli.ch',
+        'seed.btc.petertodd.org',
+    )
+    port = 8333
+    message_start = b'\xf9\xbe\xb4\xd9'
+    base58_prefixes = {
+        'PUBKEY_ADDR': 0,
+        'SCRIPT_ADDR': 5,
+        'SECRET_KEY': 128
+    }
+    source_code_url = 'https://github.com/bitcoin/bitcoin/blob/master/src/chainparams.cpp'
+    blockexplorer_tx = 'https://live.blockcypher.com/btc/tx/{0}/'
+
+
+class BitcoinTestNet(Bitcoin):
+    """
+    Class with all the necessary BTC testing network information based on
+    https://github.com/bitcoin/bitcoin/blob/master/src/chainparams.cpp
+    (date of access: 01/18/2018)
+    """
+    name = 'test-bitcoin'
+    seeds = (
+        'testnet-seed.bitcoin.jonasschnelli.ch',
+        # 'seed.tbtc.petertodd.org', Last check: 2018-02-21
+        'seed.testnet.bitcoin.sprovoost.nl',
+        'testnet-seed.bluematt.me',
+    )
+    port = 18333
+    message_start = b'\x0b\x11\x09\x07'
+    base58_prefixes = {
+        'PUBKEY_ADDR': 111,
+        'SCRIPT_ADDR': 196,
+        'SECRET_KEY': 239
+    }
+    testnet = True
+    blockexplorer_tx = 'https://live.blockcypher.com/btc-testnet/tx/{0}/'
---
+++ packages/clove/clove-1.2.8-py3-none-any.whl/clove/network/bitcoin_based/dash.py
@@ -0,0 +1,48 @@
+from clove.block_explorer.blockcypher import BlockcypherAPI
+from clove.network.bitcoin.base import BitcoinBaseNetwork, NoAPI
+
+
+class Dash(BlockcypherAPI, BitcoinBaseNetwork):
+    """
+    Class with all the necessary DASH network information based on
+    https://github.com/dashpay/dash/blob/master/src/chainparams.cpp
+    (date of access: 01/18/2018)
+    """
+    name = 'dash'
+    symbols = ('DASH', )
+    seeds = (
+        'dnsseed.dash.org',
+        'dnsseed.dashdot.io',
+        'dnsseed.masternode.io',
+        # 'dnsseed.dashpay.io', Last check: 2018-02-21
+    )
+    port = 9999
+    message_start = b'\xbf\x0c\x6b\xbd'
+    base58_prefixes = {
+        'PUBKEY_ADDR': 76,
+        'SCRIPT_ADDR': 16,
+        'SECRET_KEY': 204
+    }
+    source_code_url = 'https://github.com/dashpay/dash/blob/master/src/chainparams.cpp'
+    blockexplorer_tx = 'https://live.blockcypher.com/dash/tx/{0}/'
+
+
+class DashTestNet(NoAPI, Dash):
+    """
+    Class with all the necessary DASH testing network information based on
+    https://github.com/dashpay/dash/blob/master/src/chainparams.cpp
+    (date of access: 01/18/2018)
+    """
+    name = 'test-dash'
+    seeds = (
+        # 'testnet-seed.dashdot.io', Last check: 2018-02-21
+        'test.dnsseed.masternode.io',
+    )
+    port = 19999
+    message_start = b'\xce\xe2\xca\xff'
+    base58_prefixes = {
+        'PUBKEY_ADDR': 140,
+        'SCRIPT_ADDR': 19,
+        'SECRET_KEY': 239
+    }
+    testnet = True
