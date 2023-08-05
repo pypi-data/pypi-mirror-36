f8daa41733e1ea038e98c69a37b21028bf3a9fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:52 +0000

    Add package sjg/1.0.0/sjg-1.0.0.tar.gz

---
+++ packages/sjg/sjg-1.0.0.tar.gz/sjg-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+               name='sjg',
+               version='1.0.0',
+               py_modules=['sjg'],
+               author='hfpython',
+               author_mail='hfpython@headfirstlabs.com',
+               url='http://www.headfirstlabs.com',
+               description='A simple printer of nested lists',
+      )
