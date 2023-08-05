029902b4df8e292e03756f974c3490ea27cb59c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:38 +0000

    Add package uperations-base/0.0.3/uperations-base-0.0.3.tar.gz

---
+++ packages/uperations-base/uperations-base-0.0.3.tar.gz/uperations-base-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='uperations-base',
+    version='0.0.3',
+    packages=find_packages(),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'README.rst')).read(),
+    author='Brice Aminou',
+    author_email='brice.aminou@gmail.com',
+    keywords='workflow tool',
+    url='https://github.com/baminou/Uperations-Base'
+)
---
+++ packages/uperations-base/uperations-base-0.0.3.tar.gz/uperations-base-0.0.3/uperation_base/operations/make_operation/__init__.py
@@ -0,0 +1,25 @@
+
+
+from uperations.operation import Operation
+from libraries.uperation_base.uperation_base.operations.utils import operation_create
+from uperations.kernel import Kernel
+import os
+
+class MakeOperation(Operation):
+
+    @staticmethod
+    def name():
+        return "make_operation"
+
+    @staticmethod
+    def description():
+        return "Create a new operation"
+
+    def _parser(self, main_parser):
+        main_parser.add_argument('library', help="Name of the library to add the operation")
+        main_parser.add_argument('operation', help="Name of the operation")
+        return
+
+    def _run(self):
+        operation_create(Kernel.get_instance().find_library(self.args.library),self.args.library,self.args.operation)
+        return
