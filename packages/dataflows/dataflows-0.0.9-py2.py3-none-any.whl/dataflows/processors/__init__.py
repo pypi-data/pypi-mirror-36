dc959aae19f91fa130453f90c6e92f336e3c173b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:47 +0000

    Add package dataflows/0.0.9/dataflows-0.0.9-py2.py3-none-any.whl

---
+++ packages/dataflows/dataflows-0.0.9-py2.py3-none-any.whl/dataflows/processors/__init__.py
@@ -0,0 +1,16 @@
+from .load import load
+from .printer import printer
+from .set_type import set_type
+from .validate import validate
+from .dumpers import dump_to_path, dump_to_zip
+
+from .add_computed_field import add_computed_field
+from .add_metadata import add_metadata
+from .concatenate import concatenate
+from .delete_fields import delete_fields
+from .duplicate import duplicate
+from .filter_rows import filter_rows
+from .find_replace import find_replace
+from .sort_rows import sort_rows
+from .unpivot import unpivot
+from .cache import cache
