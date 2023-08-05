9a651a595205b8648bc351eddfe1cc4687230a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:19 +0000

    Add package geflex-db/0.10.1/geflex_db-0.10.1.tar.gz

---
+++ packages/geflex-db/geflex_db-0.10.1.tar.gz/geflex_db-0.10.1/geflex_db/__init__.py
@@ -0,0 +1,12 @@
+"""
+@author: geflex
+@contact: https://vk.com/python273
+@license: Apache 2.0, see LICENSE file
+
+Copyright (C) 2018
+"""
+
+__author__ = 'geflex'
+__version__ = '0.9.8'
+__email__ = 'geflikh.d.s@gmail.com'
+__contact__ = 'https://vk.com/geflex'
---
+++ packages/geflex-db/geflex_db-0.10.1.tar.gz/geflex_db-0.10.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='geflex_db',
+    version='0.10.1',
+    packages=['geflex_db'],
+    url='https://github.com/geflex/geflex_db',
+    license='Apache 2.0, see LICENSE file',
+    author='geflex',
+    author_email='geflikh.d.s@gmail.com',
+    description='Simple tables and file objects for data storage and processing.'
+)
