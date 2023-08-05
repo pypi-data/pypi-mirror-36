ce23a4501d3ec99f400c12b7cdb93f5a0c1f5373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:44 +0000

    Add package ybc-trans/1.0.2/ybc_trans-1.0.2.tar.gz

---
+++ packages/ybc-trans/ybc_trans-1.0.2.tar.gz/ybc_trans-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'ybc_trans',
+  packages = ['ybc_trans'],
+  version = '1.0.2',
+  description = 'translate dictionary',
+  long_description='translate dictionary zh to en ,en to zh',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'trans', 'python3','python','translate'],
+  license='MIT',
+)
