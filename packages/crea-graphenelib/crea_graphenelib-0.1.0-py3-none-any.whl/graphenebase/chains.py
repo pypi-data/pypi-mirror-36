e3491cdc97b9609ec62d3c0c321c35ec2d6c8e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:06 +0000

    Add package crea-graphenelib/0.1.0/crea_graphenelib-0.1.0-py3-none-any.whl

---
+++ packages/crea-graphenelib/crea_graphenelib-0.1.0-py3-none-any.whl/graphenebase/chains.py
@@ -0,0 +1,9 @@
+default_prefix = "CREA"
+
+known_chains = {"CREA": {"chain_id": "38f14b346eb697ba04ae0f5adcfaa0a437ed3711197704aa256a14cb9b4a8f26",
+                        "core_symbol": "CREA",
+                        "prefix": "CREA"},
+                "TEST": {"chain_id": "39f5e2ede1f8bc1a3a54a7914414e3779e33193f1f5693510e73cb7a87617447",
+                         "core_symbol": "TEST",
+                         "prefix": "TEST"}
+                }
