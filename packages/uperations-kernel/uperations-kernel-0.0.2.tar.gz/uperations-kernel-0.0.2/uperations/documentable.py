0a67a9ee3998241c509906548577373a7d31a05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:31 +0000

    Add package uperations-kernel/0.0.2/uperations-kernel-0.0.2.tar.gz

---
+++ packages/uperations-kernel/uperations-kernel-0.0.2.tar.gz/uperations-kernel-0.0.2/uperations/documentable.py
@@ -0,0 +1,10 @@
+
+class Documentable:
+
+    @classmethod
+    def name(cls):
+        return cls
+
+    @classmethod
+    def description(cls):
+        return str(cls)
---
+++ packages/uperations-kernel/uperations-kernel-0.0.2.tar.gz/uperations-kernel-0.0.2/uperations/event.py
@@ -0,0 +1,15 @@
+
+from .documentable import Documentable
+
+
+class Event(Documentable):
+
+    def __init__(self, operation):
+        self._operation = operation
+        self.trigger()
+        return
+
+    def trigger(self):
+        #for listener_type in kernel.EVENTS[self.__class__]:
+        #    listener_type(self).handle()
+        return
