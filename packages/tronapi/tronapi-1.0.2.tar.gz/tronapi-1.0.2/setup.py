74a97e5c17782213ac3b14f41800e781ea2f0bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:43 +0000

    Add package tronapi/1.0.2/tronapi-1.0.2.tar.gz

---
+++ packages/tronapi/tronapi-1.0.2.tar.gz/tronapi-1.0.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import platform
+
+try:
+    import setuptools
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    import setuptools
+
+py_version = platform.python_version()
+
+_TRON_VERSION = '1.0.2'
+
+REQUIRED = [
+    'urllib3'
+]
+
+with open('README.md') as fileobj:
+    README = fileobj.read()
+
+setuptools.setup(
+    name='tronapi',
+    version=_TRON_VERSION,
+    description='A Python API for interacting with Tron (TRX)',
+    long_description=README,
+    url='https://github.com/iexbase/tron-api-python',
+    author='Shamsudin Serderov',
+    author_email='steein.shamsudin@gmail.com',
+    install_requires=REQUIRED,
+    license='MIT',
+    keywords=['tron-api', 'tron-api-python', 'iexbase']
+)
