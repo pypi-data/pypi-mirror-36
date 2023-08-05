255ad7a04424229fc6901342e5ffd08c2bccad51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:34 +0000

    Add package webyPy/0.0.1/webyPy-0.0.1.tar.gz

---
+++ packages/webyPy/webyPy-0.0.1.tar.gz/webyPy-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="webyPy",
+    version="0.0.1",
+    author="Takshan Gowda",
+    author_email="takshan.gowdii@gmail.com",
+    description="A light-weight web-design package for Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TakshanGowda/webEpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
