2c054d803a2378432ee108f4a2bd410e1968c9f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:49 +0000

    Add package clustergrammer-widget2/0.5.0/clustergrammer_widget2-0.5.0-py2.py3-none-any.whl

---
+++ packages/clustergrammer-widget2/clustergrammer_widget2-0.5.0-py2.py3-none-any.whl/clustergrammer_widget2/example.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Nicolas Fernandez.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+TODO: Add module docstring
+"""
+
+from ipywidgets import DOMWidget
+from traitlets import Unicode
+from ._version import EXTENSION_SPEC_VERSION
+
+module_name = "clustergrammer_widget2"
+
+
+class ExampleWidget(DOMWidget):
+    """TODO: Add docstring here
+    """
+    _model_name = Unicode('ExampleModel').tag(sync=True)
+    _model_module = Unicode(module_name).tag(sync=True)
+    _model_module_version = Unicode(EXTENSION_SPEC_VERSION).tag(sync=True)
+    _view_name = Unicode('ExampleView').tag(sync=True)
+    _view_module = Unicode(module_name).tag(sync=True)
+    _view_module_version = Unicode(EXTENSION_SPEC_VERSION).tag(sync=True)
+
+    value = Unicode('Hello World')
+
+    network = Unicode('').tag(sync=True)
