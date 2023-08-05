8549183a2b7a76a89162cf9ff8571a854b86c5c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:05 +0000

    Add package sas7bdat-converter/0.2.6/sas7bdat_converter-0.2.6.tar.gz

---
+++ packages/sas7bdat-converter/sas7bdat_converter-0.2.6.tar.gz/sas7bdat_converter-0.2.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from setuptools import find_packages
+
+long_description = '''
+Converts proprietary sas7bdat files from SAS into formats such as csv and XML useable by other programs. Currently supported conversiaions are csv, Excel (xlsx format), json, Pandas DataFrame, and XML.
+
+Conversions can be done on a single file, a batch of files, or a whole directory.
+'''
+
+setup(
+    name='sas7bdat_converter',
+    version='0.2.6',
+    author='Paul Sanders',
+    author_email='psanders1@gmail.com',
+    license='Apache 2.0',
+    description='Convert sas7bdat files into other formats',
+    long_description=long_description,
+    url='https://github.com/sanders41/sas7bdat_converter',
+    download_url='https://github.com/sanders41/sas7bdat_converter/archive/v0.2.6.tar.gz',
+    install_requires=['pandas>=0.23.0',
+                      'XlsxWriter>=1.0.5'],
+    extras_require={
+        'test': ['xlrd>=1.1.0'],
+    },
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent'
+    ],
+)
