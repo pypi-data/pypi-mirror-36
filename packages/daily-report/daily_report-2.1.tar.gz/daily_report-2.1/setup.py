687c5950e00e3ccabf59895b15d6a44b6f415715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:42 +0000

    Add package daily-report/2.1/daily_report-2.1.tar.gz

---
+++ packages/daily-report/daily_report-2.1.tar.gz/daily_report-2.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="daily_report",
+    version="2.1",
+    author="Albert Font",
+    author_email="albertf80@gmail.com",
+    description="A package for the production of daily and cumulative reports attached to PyBpod tasks.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/AFont33/daily_reports/",
+    packages=setuptools.find_packages()
+    )
