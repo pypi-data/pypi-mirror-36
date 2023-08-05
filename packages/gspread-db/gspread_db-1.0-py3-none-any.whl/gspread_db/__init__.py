3cd7330ccdb6e2bd123da2c5fb7d50e922cb94f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:21 +0000

    Add package gspread-db/1.0/gspread_db-1.0-py3-none-any.whl

---
+++ packages/gspread-db/gspread_db-1.0-py3-none-any.whl/gspread_db/__init__.py
@@ -0,0 +1,10 @@
+import gspread
+
+from .client import Client
+from .models import Database, Table, GSpreadDbError, HeaderError, RecordError
+
+
+def authorize(credentials, client_class=gspread.client.Client):
+    """Login to Google API using OAuth2 credentials."""
+    client = gspread.authorize(credentials, client_class)
+    return Client(client.auth, client.session)
