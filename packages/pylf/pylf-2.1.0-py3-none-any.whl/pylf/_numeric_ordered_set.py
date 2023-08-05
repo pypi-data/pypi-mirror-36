94b644a01ad777b40935829b1088c87c711561df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:19 +0000

    Add package pylf/2.1.0/pylf-2.1.0-py3-none-any.whl

---
+++ packages/pylf/pylf-2.1.0-py3-none-any.whl/pylf/_numeric_ordered_set.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+"""A customized data structure for perturbation algorithm"""
+import array
+
+
+class NumericOrderedSet(object):
+    """This data structure only support storing numeric elements"""
+
+    def __init__(self, typecode: str, flag):
+        """More info about typecode: https://docs.python.org/3/library/array.html#module-array
+        The value of flag must be within the typecode's range. Flag is invisible in set operations, but it will appear
+        in the ordered sequence."""
+        self._typecode = typecode
+        self._flag = flag
+        self._array = array.array(typecode)
+        self._set = set()
+
+    def add(self, item) -> bool:
+        """This has no effect and would return False if the item is already present"""
+        if item == self._flag:
+            raise ValueError("'item' cannot be equal to 'flag'")
+        if item in self._set:
+            return False
+        self._set.add(item)
+        self._array.append(item)
+        return True
+
+    def add_flag(self) -> None:
+        self._array.append(self._flag)
+
+    def __contains__(self, item) -> bool:
+        return item in self._set
+
+    def __iter__(self):
+        return iter(self._array)
+
+    def clear(self) -> None:
+        self._set.clear()
+        self._array = array.array(self._typecode)
+
+    def __len__(self):
+        return len(self._array)
+
+    @property
+    def typecode(self) -> str:
+        return self._typecode
+
+    @property
+    def flag(self):
+        return self._flag
---
+++ packages/pylf/pylf-2.1.0-py3-none-any.whl/pylf/_page.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+"""A wrapper for Pillow Image Object"""
+from PIL import Image as image
+from PIL import ImageDraw as image_draw
+
+
+class Page(object):
+    """A simple wrapper for Pillow Image Object"""
+
+    def __init__(self, mode: str, size: tuple, color, num: int):
+        self.image = image.new(mode, size, color)
+        self.num = num
+
+    @property
+    def draw(self):
+        return image_draw.Draw(self.image)
+
+    @property
+    def matrix(self):
+        return self.image.load()
+
+    @property
+    def size(self) -> tuple:
+        return self.image.size
+
+    @property
+    def width(self) -> int:
+        return self.image.size[0]
+
+    @property
+    def height(self) -> int:
+        return self.image.size[1]
