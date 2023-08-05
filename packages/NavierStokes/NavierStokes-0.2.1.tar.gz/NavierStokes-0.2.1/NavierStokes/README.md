bacd796a24bcf79d41bbc2a61c8397f7edaaf274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:12 +0000

    Add package NavierStokes/0.2.1/NavierStokes-0.2.1.tar.gz

---
+++ packages/NavierStokes/NavierStokes-0.2.1.tar.gz/NavierStokes-0.2.1/NavierStokes/README.md
@@ -0,0 +1,11 @@
+# NavierStokes Command Line
+## Mode
+Alias|Mode
+---|---
+-td|TDMA solver
+### Optional Variables
+#### TDMA solver
+Still in development
+
+#### Example
+
---
+++ packages/NavierStokes/NavierStokes-0.2.1.tar.gz/NavierStokes-0.2.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from NavierStokes import __version__
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='NavierStokes',
+    version=__version__,
+    author='Kyu Mok (Ricky) Kim',
+    author_email='rickykim93@hotmail.com',
+    url='http://rickykim.net',
+    description='Navier Stokes Calculator',
+    long_description=open(path.join(here, 'README.md'), encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+    ],
+    test_suite='tests',
+    entry_points={
+        'console_scripts': [
+            'NavierStokes = NavierStokes.__main__:main',
+        ]
+    }
+)
