339816f135fd0be5f155d1e207659c231946abac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:14 +0000

    Add package prynter/0.2/prynter-0.2-py3-none-any.whl

---
+++ packages/prynter/prynter-0.2-py3-none-any.whl/prynter/interpreter.py
@@ -0,0 +1,27 @@
+import re
+
+__all__ = ['run']
+
+MODULE_RE = re.compile(r'^(\w+)(?=\.)|(?<![.\w])(\w+)(?=\.)')
+
+def get_modules(s):
+    """
+    Extracts the module from a given command. E.g.:
+        'string.digits' -> 'string'
+        'print(3)' -> None
+    """
+    return [x or y for x,y in re.findall(MODULE_RE,s)]
+
+def run(*args, **options):
+    # get all (unique) modules
+    instructions = list(args)
+    modules = set(module for mods in map(get_modules,instructions) for module in mods)
+
+    globs = { module:__import__(module) for module in modules } 
+
+    if options.get('-v'):
+        instructions[-1] = 'print({})'.format(instructions[-1])
+
+    src = ';'.join(instructions)
+    cmd = compile(src,'cmd','exec')
+    exec(cmd,globs)
