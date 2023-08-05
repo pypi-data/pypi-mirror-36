6ebb81fc51636cd970951d41866d0a807f1408fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:17 +0000

    Add package django-simple-pages/0.2/django-simple-pages-0.2.tar.gz

---
+++ packages/django-simple-pages/django-simple-pages-0.2.tar.gz/django-simple-pages-0.2/simple_pages/apps.py
@@ -0,0 +1,10 @@
+"""Application configurations for the simple_pages app."""
+
+from django.apps import AppConfig
+
+
+class SimplePagesConfig(AppConfig):
+    """SimplePages application config."""
+
+    name = "simple_pages"
+    verbose_name = "Simple Pages"
