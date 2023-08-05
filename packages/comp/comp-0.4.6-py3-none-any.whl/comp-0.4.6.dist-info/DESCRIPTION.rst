aca54b7e48842746e0220fbad03fcae4542907d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:57 +0000

    Add package comp/0.4.6/comp-0.4.6-py3-none-any.whl

---
+++ packages/comp/comp-0.4.6-py3-none-any.whl/comp-0.4.6.dist-info/DESCRIPTION.rst
@@ -0,0 +1,299 @@
+===============================
+comp - Curses Omni Media Player
+===============================
+
+**comp** is a `mpv <https://mpv.io/>`_ front-end using curses. It has basic
+media player functions and can to extract playlists from multiple sources such
+as media sites supported by `youtube-dl <https://rg3.github.io/youtube-dl/>`_,
+local and direct URL to video/audio and its own JSON playlist format.
+
+.. image:: https://github.com/McSinyx/comp/raw/master/doc/screenshot.png
+
+Installation
+------------
+
+**comp** requires Python 3.5+ with ``curses`` module (only available on
+Unix-like OSes such as GNU/Linux and the BSDs) and ``libmpv`` (available as
+``libmpv1`` in Debian/Ubuntu, openSUSE; and as ``mpv`` in Arch Linux, Gentoo,
+macOS Homebrew repository). It also depends on ``python-mpv`` and
+``youtube-dl`` but the setup program will automatically install them if they
+are missing.
+
+As ``setuptools`` will `install in an egg and cause breakage
+<https://github.com/McSinyx/comp/issues/5>`_, using ``pip`` (Python 3 version) 
+is a must.  After `installing it <https://pip.pypa.io/en/latest/installing/>`_,
+run ``pip3 install comp`` (you might want to add ``--user`` flag to use the
+`User Scheme <https://pip.pypa.io/en/stable/user_guide/#user-installs>`_).
+
+For developers, clone the `Github repo <https://github.com/McSinyx/comp>`_ then
+simply run the ``comp`` executable to test the program. If you insist on
+installing it, still use ``pip3``: ``pip3 install .``. Note that **comp** is
+distibuted in a ``wheel`` created via ``./setup.py bdist_wheel``.
+
+Command line options
+--------------------
+
+::
+
+   usage: comp [-h] [-v] [-e {json,mpv,youtube-dl}] [-c CONFIG] [--vid VID]
+               [--vo DRIVER] [-f YTDL_FORMAT]
+               playlist
+
+   Curses Omni Media Player
+
+   positional arguments:
+     playlist              path or URL to the playlist
+
+   optional arguments:
+     -h, --help            show this help message and exit
+     -v, --version         show program's version number and exit
+     -e {json,mpv,youtube-dl}, --extractor {json,mpv,youtube-dl}
+                           playlist extractor, default is youtube-dl
+     -c CONFIG, --config CONFIG
+                           path to the configuration file
+     --vid VID             initial video channel. auto selects the default, no
+                           disables video
+     --vo DRIVER           specify the video output backend to be used. See
+                           VIDEO OUTPUT DRIVERS in mpv(1) for details and
+                           descriptions of available drivers
+     -f YTDL_FORMAT, --format YTDL_FORMAT
+                           video format/quality to be passed to youtube-dl
+
+Examples
+^^^^^^^^
+
+Open a JSON playlist::
+
+   comp -e json test/playlist.json
+
+Open a Youtube playlist with video height lower than 720::
+
+   comp -f '[height<720]' https://www.youtube.com/watch?list=PLnk14Iku8QM7R3ARnrj1TwYSZleF-i7jT
+
+Keyboard control
+----------------
+
+Bindings inherited from mpv
+^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+For convenience purpose, I try to mimic **mpv** default keybindings, but many
+are slightly different from **mpv** exact behaviour (mainly because of the lack
+of keys which are unsupported by ``curses``). So I will list all of them here
+for you to `compare <https://github.com/mpv-player/mpv/blob/master/DOCS/man/mpv.rst#keyboard-control>`_:
+
+Left and Right
+   Seek backward/forward 5 seconds. Shifted arrow does a 1 second seek.
+
+Up and Down
+   Seek backward/forward 1 minute.
+
+``[`` and ``]``
+   Decrease/increase current playback speed by 10%.
+
+``{`` and ``}``
+   Halve/double current playback speed.
+
+Backspace
+   Reset playback speed to normal.
+
+``<`` and ``>``
+   Go backward/forward in the playlist.
+
+Return
+   Start playing.
+
+Space / ``p``
+   Pause (pressing again unpauses).
+
+``.``
+   Step forward. Pressing once will pause, every consecutive press will play
+   one frame and then go into pause mode again.
+
+``,``
+   Step backward. Pressing once will pause, every consecutive press will play
+   one frame in reverse and then go into pause mode again.
+
+``q``
+   Stop playing and quit.
+
+``/`` / ``9`` and ``*`` / ``0``
+   Decrease/increase volume.
+
+``m``
+   Mute sound.
+
+``_``
+   Cycle through the available video tracks.
+
+``#``
+   Cycle through the available audio tracks.
+
+``f``
+   Toggle fullscreen.
+
+``T``
+   Toggle stay-on-top.
+
+``w`` and ``e``
+   Decrease/increase pan-and-scan range.
+
+``o`` / ``P``
+   Show progression bar, elapsed time and total duration on the OSD.
+
+``O``
+   Toggle OSD states between normal and playback time/duration.
+
+``v``
+   Toggle subtitle visibility.
+
+``j`` and ``J``
+   Cycle through the available subtitles.
+
+``x`` and ``z``
+   Adjust subtitle delay by +/- 0.1 seconds.
+
+``l``
+   Set/clear A-B loop points.
+
+``L``
+   Toggle infinite looping.
+
+Ctrl-``+`` and Ctrl-``-``
+   Adjust audio delay (A/V sync) by +/- 0.1 seconds.
+
+``u``
+   Switch between applying no style overrides to SSA/ASS subtitles, and
+   overriding them almost completely with the normal subtitle style.
+
+``V``
+   Toggle subtitle VSFilter aspect compatibility mode.
+
+``r`` and ``t``
+   Move subtitles up/down.
+
+``s``
+   Take a screenshot.
+
+``S``
+   Take a screenshot, without subtitles.
+
+Alt-``s``
+   Take a screenshot each frame.
+
+Page Up and Page Down
+   Seek to the beginning of the previous/next chapter.
+
+``d``
+   Activate/deactivate deinterlacer.
+
+``A``
+   Cycle aspect ratio override.
+
+``1`` and ``2``
+   Adjust contrast.
+
+``3`` and ``4``
+   Adjust brightness.
+
+``5`` and ``6``
+   Adjust gamma.
+
+``7`` and ``8``
+   Adjust saturation.
+
+Alt-``0``
+   Resize video window to half its original size.
+
+Alt-``1``
+   Resize video window to its original size.
+
+Alt-``2``
+   Resize video window to double its original size.
+
+``E``
+   Cycle through editions.
+
+Movements and selections
+^^^^^^^^^^^^^^^^^^^^^^^^
+
+The following keybindings are Emacs-like since most characters are taken by
+**mpv**.
+
+Ctrl-``p`` and Ctrl-``n``
+   Move a single line up/down.
+
+Alt-``v`` and Ctrl-``v``
+   Move a single page up/down.
+
+Home / Ctrl-``<`` and End / Ctrl-``>``
+   Move to the beginning/end of the playlist.
+
+Ctrl-Space
+   Deselect/reselect the current entry and move down a line.
+
+Playlist manipulation
+^^^^^^^^^^^^^^^^^^^^^
+
+Ctrl-``o``
+   Open playlist.
+
+Ctrl-``i``
+   Insert playlist.
+
+Ctrl-``f`` and Alt-``f``
+   Search forward/backward for a pattern.
+
+Alt-``m``
+   Cycle through playing modes.
+
+Delete
+   Delete the current entry.
+
+``W``
+   Save the current playlist under JSON format.
+
+F5
+   Redraw the screen content.
+
+``:``
+   Execute a **mpv** command.
+
+Configuration files
+-------------------
+
+If not specified by the ``--config``, (user-specific) configuration file is
+``~/.config/comp/settings.ini``. Default configurations
+are listed below::
+
+   [comp]
+   # Initial playing mode, which can be one of these 8 modes: play-current,
+   # play-all, play-selected, repeat-current, repeat-all, repeat-selected,
+   # shuffle-all and shuffle-selected.
+   play-mode = play-current
+
+   [mpv]
+   # Options to be parsed to mpv. See OPTIONS section on mpv(1) man pages for
+   # its complete list of available options.
+   # For example:
+   #vo = xv
+   #ontop = yes
+   #border = no
+   #force-window = yes
+   #autofit = 500x280
+   #geometry = -15-50
+
+   [youtube-dl]
+   # Video format/quality to be passed to youtube-dl. See FORMAT SELECTION in
+   # youtube-dl(1) man page for more details and descriptions.
+   format = bestvideo+bestaudio
+
+
+Bugs
+----
+
+Media durations are not extracted from online playlists as
+``youtube-dl.YoutubeDL`` option ``extract_flat`` is set to ``'in_playlist'``.
+This is rather a feature to save up bandwidth than a bug because a track's
+duration is updated when it's played.
+
+
---
+++ packages/comp/comp-0.4.6-py3-none-any.whl/omp/omp.py
@@ -0,0 +1,221 @@
+# omp.py - Omni Media Player meta object
+# This file is part of comp
+#
+# comp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# comp program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with comp.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2017  Nguyễn Gia Phong <vn.mcsinyx@gmail.com>
+
+import curses
+import json
+import re
+from bisect import bisect_left as bisect
+from collections import deque
+from gettext import bindtextdomain, gettext as _, textdomain
+from itertools import cycle
+from os import makedirs
+from os.path import abspath, dirname, expanduser, expandvars
+from random import choice
+from sys import exc_info
+
+from pkg_resources import resource_filename
+from mpv import MPV
+
+from .ie import JSON_KEYS
+
+# Init gettext
+bindtextdomain('omp', resource_filename('omp', 'locale'))
+textdomain('omp')
+
+
+def shuffle_init(a):
+    """Return in iterator which yield random elements from a,
+    and always begin with its first element.
+    """
+    if a:
+        yield a[0]
+        while True: yield choice(a)
+
+
+class Omp(object):
+    """Omni Media Player meta object.
+
+    Attributes:
+        entries (list): list of all tracks
+        json_file (str): path to save JSON playlist
+        mode (str): the mode to pick and play tracks
+        mp (MPV): an mpv instance
+        play_backward (bool): flag show if to play the previous track
+        play_list (list): list of tracks according to mode
+        played (list): list of previously played tracks
+        playing (int): index of playing track in played
+        playlist (iterator): iterator of tracks according to mode
+        search_res (iterator):  title-searched results
+
+    I/O handlers (defined by front-end):
+        print_msg(message, error=False): print a message
+        property_handler(name, val): called when a mpv property updated
+        read_input(prompt): prompt for user input
+        refresh(): update interface content
+    """
+    def __new__(cls, entries, json_file, mode, mpv_args, ytdlf):
+        self = object.__new__(cls)
+        self.play_backward, self.reading = False, False
+        self.playing = -1
+        self.json_file, self.mode = json_file, mode
+        self.entries, self.played = entries, []
+        self.playlist, self.search_res = iter(()), deque()
+        self.mp = MPV(input_default_bindings=True, input_vo_keyboard=True,
+                      ytdl=True, ytdl_format=ytdlf)
+        return self
+
+    def __init__(self, entries, json_file, mode, mpv_args, ytdlf):
+        for arg, val in mpv_args.items():
+            try:
+                self.mp[arg] = val
+            except:
+                self.__exit__(*exc_info())
+        @self.mp.property_observer('mute')
+        @self.mp.property_observer('pause')
+        @self.mp.property_observer('time-pos')
+        def observer(name, value): self.property_handler(name, value)
+        self.mp.register_key_binding('q', lambda state, key: None)
+
+    def __enter__(self): return self
+
+    def seek(self, amount, reference='relative', precision='default-precise'):
+        """Wrap a try clause around mp.seek to avoid crashing when
+        nothing is being played.
+        """
+        try:
+            self.mp.seek(amount, reference, precision)
+        except:
+            self.print_msg(_("Failed to seek"), error=True)
+
+    def add(self, name, value=1):
+        """Wrap a try clause around mp.property_add."""
+        try:
+            self.mp.property_add(name, value)
+        except:
+            self.print_msg(
+                _("Failed to add {} to '{}'").format(value, name), error=True)
+
+    def multiply(self, name, factor):
+        """Wrap a try clause around mp.property_multiply."""
+        try:
+            self.mp.property_multiply(name, factor)
+        except:
+            self.print_msg(
+                _("Failed to multiply '{}' with {}").format(name, factor),
+                error=True)
+
+    def cycle(self, name, direction='up'):
+        """Wrap a try clause around mp.cycle."""
+        try:
+            self.mp.cycle(name, direction='up')
+        except:
+            self.print_msg(
+                _("Failed to cycle {} '{}'").format(direction, name),
+                error=True)
+
+    def idx(self, entry=None):
+        """Return the index of the current entry."""
+        if entry is None:
+            return self.start + self.y - 1
+        return self.entries.index(entry)
+
+    def current(self):
+        """Return the current entry."""
+        try:
+            return self.entries[self.idx()]
+        except:
+            return {}
+
+    def update_playlist(self):
+        """Update the playlist to be used by play function."""
+        action, pick = self.mode.split('-')
+        if pick == 'current':
+            self.play_list = deque([self.current()])
+        elif pick == 'all':
+            self.play_list = deque(self.entries)
+            self.play_list.rotate(-self.idx())
+        elif pick == 'selected':
+            self.play_list = deque([entry for entry in self.entries
+                                    if entry.get('selected')])
+            indexes = [i for i, entry in enumerate(self.entries)
+                       if entry.get('selected')]
+            idx = indexes[bisect(indexes, self.idx())]
+            self.play_list.rotate(-self.play_list.index(self.entries[idx]))
+
+        if action == 'play':
+            self.playlist = iter(self.play_list)
+        elif action == 'repeat':
+            self.playlist = cycle(self.play_list)
+        elif action == 'shuffle':
+            self.playlist = shuffle_init(self.play_list)
+        if self.playing < -1: self.played = self.played[:self.playing+1]
+
+    def next(self, force=False, backward=False):
+        """Go forward/backward in the playlist.
+
+        If forced, this will also unpause the player.
+        """
+        self.play_backward = backward
+        if self.mp.idle_active:
+            self.play(force)
+        else:
+            self.mp.time_pos = self.mp.duration
+            if force: self.mp.pause = False
+
+    def search(self, backward=False):
+        """Prompt then search for a pattern."""
+        p = re.compile(self.gets('/'), re.IGNORECASE)
+        entries = deque(self.entries)
+        entries.rotate(-self.idx())
+        self.search_res = deque(filter(
+            lambda entry: p.search(entry['title']) is not None, entries))
+        if backward: self.search_res.reverse()
+        if self.search_res:
+            self.move(self.idx(self.search_res[0]) - self.idx())
+        else:
+            self.update_status(_("Pattern not found"), curses.color_pair(1))
+
+    def next_search(self, backward=False):
+        """Repeat previous search."""
+        if self.search_res:
+            self.search_res.rotate(1 if backward else -1)
+            self.move(self.idx(self.search_res[0]) - self.idx())
+        else:
+            self.update_status(_("Pattern not found"), curses.color_pair(1))
+
+    def dump_json(self):
+        """Read user input needed to save the playlist."""
+        s = self.read_input(
+            _("Save playlist to [{}]: ").format(self.json_file))
+        self.json_file = abspath(expanduser(expandvars(s or self.json_file)))
+        entries = [{k: v for k, v in entry.items() if k in JSON_KEYS}
+                   for entry in self.entries]
+        try:
+            makedirs(dirname(self.json_file), exist_ok=True)
+            with open(self.json_file, 'w') as f:
+                json.dump(entries, f, ensure_ascii=False, indent=2,
+                          sort_keys=True)
+        except:
+            errmsg = _("'{}': Can't open file for writing").format(
+                self.json_file)
+            self.print_msg(errmsg, error=True)
+        else:
+            self.print_msg(_("'{}' written").format(self.json_file))
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.mp.quit()
