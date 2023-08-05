957c69e6c66a41135c5e63af3e6b30dda31f5acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:23 +0000

    Add package lidar/0.1.11/lidar-0.1.11.tar.gz

---
+++ packages/lidar/lidar-0.1.11.tar.gz/lidar-0.1.11/lidar/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for lidar."""
+
+__author__ = """Qiusheng Wu"""
+__email__ = 'giswqs@gmail.com'
+__version__ = '0.1.11'
+
+from .filling import ExtractSinks
+from .slicing import DelineateDepressions
+from .filtering import MeanFilter, MedianFilter, GaussianFilter
+# from .mounts import DelineateMounts
