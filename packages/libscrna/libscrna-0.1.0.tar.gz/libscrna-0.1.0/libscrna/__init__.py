580ddc67ef442d12080d9483438d64b3f21819e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:02 +0000

    Add package libscrna/0.1.0/libscrna-0.1.0.tar.gz

---
+++ packages/libscrna/libscrna-0.1.0.tar.gz/libscrna-0.1.0/libscrna/__init__.py
@@ -0,0 +1,4 @@
+name = 'libscrna'
+import libscrna.libcluster
+import libscrna.lib10x
+import libscrna.libtsne
---
+++ packages/libscrna/libscrna-0.1.0.tar.gz/libscrna-0.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name='libscrna',
+    version='0.1.0',
+    author='Antony B Holmes',
+    author_email='antony.b.holmes@gmail.com',
+    description='A library for dealing with single cell RNA-seq data.',
+    url='https://github.com/antonybholmes/libscrna',
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'libplot',
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
