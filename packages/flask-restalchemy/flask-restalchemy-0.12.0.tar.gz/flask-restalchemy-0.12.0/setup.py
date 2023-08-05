09dca022fcb9aa5d312dc72322c50a3d9f040bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:45 +0000

    Add package flask-restalchemy/0.12.0/flask-restalchemy-0.12.0.tar.gz

---
+++ packages/flask-restalchemy/flask-restalchemy-0.12.0.tar.gz/flask-restalchemy-0.12.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+install_requires=[
+    'flask-restful>=0.3.0',
+    'flask-sqlalchemy >= 2.3.0',
+    'flask',
+    'sqlalchemy>=1.1',
+]
+
+setup(
+    name='flask-restalchemy',
+    version='0.12.0',
+    packages=find_packages(exclude=['*.tests']),
+    url='https://github.com/ESSS/flask-restalchemy',
+    license='MIT',
+    author='ESSS',
+    author_email='dev@esss.co',
+    description='Flask extension to build REST APIs based on SQLAlchemy models ',
+    keywords='flask sqlalchemy orm',
+    data_files=[("", ["LICENSE"])],
+    install_requires=install_requires,
+    python_requires='>=3.4',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
