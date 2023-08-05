e0e0d8911d25de4a556b34d86208f3c0e1ed83e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:09 +0000

    Add package rundeck-resources/0.3.1/rundeck_resources-0.3.1.tar.gz

---
+++ packages/rundeck-resources/rundeck_resources-0.3.1.tar.gz/rundeck_resources-0.3.1/rundeck_resources/plugins.py
@@ -0,0 +1,57 @@
+import logging
+import pkg_resources
+from .errors import ConfigError
+
+# Setup logging
+logger = logging.getLogger(__name__)
+
+
+def load_plugins(config: dict, resource_types: str) -> dict:
+    """
+    Method to load plugins defined in the *entry_point*
+    by *resource_type*.
+
+    :param config: The configuration file content.
+    :type config: dict
+    :param resource_types: The resource type to load (Input, Output).
+    :type resource_types: str
+    :returns: The plugins loaded.
+    :rtype: dict
+    """
+    logger.debug("Loading plugins configured to be used")
+    raised_error = 0
+    entry_points = get_plugins(resource_types)
+    plugins = {}
+    for section in config:
+        _section = section.split(":")
+        try:
+            if _section[0] in entry_points:
+                plugins.update({_section[0]: {
+                    'plugin': entry_points[_section[0]].load(),
+                    'title': _section[1]
+                }})
+        except IndexError as e:
+            raised_error = e
+
+        if isinstance(raised_error, IndexError):
+            raise ConfigError("No plugin title found, in section '{}'."
+                              " Configuration sections need to be in"
+                              " the format 'PluginName:Title'"
+                              .format(section))
+    return plugins
+
+
+def get_plugins(resource_types: str) -> dict:
+    """
+    Method to get the plugins defined in the *entry_point*.
+
+    :param resource_types: The resource type to get (Input, Output).
+    :type resource_types: str
+    :returns: The entry points by name.
+    :rtype: dict
+    """
+    logger.debug("Importing plugins from list available")
+    entry_points = {}
+    for entry_point in pkg_resources.iter_entry_points(resource_types):
+        entry_points.update({entry_point.name: entry_point})
+    return entry_points
