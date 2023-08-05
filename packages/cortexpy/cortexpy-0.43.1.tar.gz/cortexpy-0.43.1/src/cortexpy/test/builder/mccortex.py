5085c118816eec2a88fdd8652c5333d341270396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:18 +0000

    Add package cortexpy/0.43.1/cortexpy-0.43.1.tar.gz

---
+++ packages/cortexpy/cortexpy-0.43.1.tar.gz/cortexpy-0.43.1/src/cortexpy/test/builder/mccortex.py
@@ -0,0 +1,48 @@
+import logging
+from collections import OrderedDict
+
+import attr
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from attr import Factory
+
+import cortexpy.test.runner as runner
+
+logger = logging.getLogger(__name__)
+
+
+@attr.s
+class Mccortex(object):
+    kmer_size = attr.ib(3)
+    sequences = attr.ib(Factory(OrderedDict))
+    mccortex_bin = attr.ib(None)
+
+    def with_kmer_size(self, kmer_size):
+        self.kmer_size = kmer_size
+        return self
+
+    def with_dna_sequence(self, sequence, *, name='sample_0'):
+        if name not in self.sequences:
+            self.sequences[name] = []
+        self.sequences[name].append(sequence)
+        return self
+
+    def build(self, tmpdir):
+        mccortex_args = ['build', '--force', '--sort', '--kmer', str(self.kmer_size)]
+        for name, dna_sequences in self.sequences.items():
+            input_fasta = str(tmpdir.join('input.{}.fasta'.format(name)))
+            with open(input_fasta, 'w') as fh:
+                for sequence in dna_sequences:
+                    fh.write(SeqRecord(Seq(sequence)).format('fasta'))
+            mccortex_args.extend(['--sample', name, '-1', input_fasta])
+
+        output_graph = str(tmpdir.join('output.ctx'))
+        mccortex_args.append(output_graph)
+
+        ret = runner.Mccortex(self.kmer_size, mccortex_bin=self.mccortex_bin).run(mccortex_args)
+        logger.debug('\n' + ret.stderr.decode())
+
+        ret = runner.Mccortex(self.kmer_size, mccortex_bin=self.mccortex_bin).view(output_graph)
+        logger.debug('\n' + ret.stdout.decode())
+
+        return output_graph
---
+++ packages/cortexpy/cortexpy-0.43.1.tar.gz/cortexpy-0.43.1/tests/test_unit/test_graph/test_interactor/test_all_simple_paths.py
@@ -0,0 +1,98 @@
+from cortexpy.graph.interactor import Interactor, Contigs
+from cortexpy.test.builder.graph.cortex import (
+    CortexGraphBuilder,
+    get_cortex_builder,
+)
+
+
+class TestCortex(object):
+    def test_emits_one_single_color_unitig(self):
+        # given
+        b = CortexGraphBuilder()
+        b.with_colors(0)
+        b.add_edge('AAA', 'AAT', color=0)
+        b.make_consistent('AAA')
+        graph = b.build()
+
+        # when
+        paths = list(Contigs(graph, color=0).all_simple_paths())
+
+        # then
+        assert ['AAAT'] == [str(p.seq) for p in paths]
+
+    def test_only_follows_one_color_with_color_specified(self):
+        # given
+        b = CortexGraphBuilder()
+        b.with_colors(0, 1)
+        b.add_edge('AAA', 'AAT', color=0)
+        b.add_edge('AAT', 'ATA', color=1)
+        b.make_consistent('AAA')
+        graph = b.build()
+
+        # when
+        paths = list(Contigs(graph, color=0).all_simple_paths())
+
+        # then
+        assert ['AAAT'] == [str(p.seq) for p in paths]
+
+    def test_follows_two_colors_with_no_color_specified(self):
+        # given
+        b = CortexGraphBuilder()
+        b.with_colors(0, 1)
+        b.add_edge('AAA', 'AAT', color=0)
+        b.add_edge('AAT', 'ATA', color=1)
+        b.make_consistent('AAA')
+        graph = b.build()
+
+        # when
+        paths = list(Contigs(graph).all_simple_paths())
+
+        # then
+        assert {'AAATA'} == set([str(p.seq) for p in paths])
+
+    def test_follows_three_colors_with_no_color_specified(self):
+        # given
+        b = CortexGraphBuilder()
+        b.with_colors(0, 1, 2)
+        b.add_edge('AAA', 'AAT', color=0)
+        b.add_edge('AAT', 'ATA', color=1)
+        b.add_edge('AAT', 'ATC', color=2)
+        b.make_consistent('AAA')
+        graph = b.build()
+
+        # when
+        paths = list(Contigs(graph).all_simple_paths())
+
+        # then
+        assert {'AAATA', 'AAATC'} == set([str(p.seq) for p in paths])
+
+    def test_in_y_graph_finds_two_paths(self):
+        # given
+        b = CortexGraphBuilder()
+        b.add_path('CAA', 'AAA')
+        b.add_path('TAA', 'AAA')
+        b.make_consistent('AAA')
+        cdb = b.build()
+
+        # when
+        paths = list(Contigs(cdb).all_simple_paths())
+
+        # then
+        assert {'CAAA', 'TAAA'} == set([str(p.seq) for p in paths])
+
+    def test_in_y_graph_finds_two_paths_of_revcomp(self):
+        # given
+        b = get_cortex_builder()
+        b.with_kmer('CGC 1 .......T')
+        b.with_kmer('AGC 1 a....CG.')
+        b.with_kmer('AAG 1 .....C..')
+        b.with_kmer('GCC 1 a.......')
+        cdb = b.build()
+
+        cdb = Interactor(cdb).make_graph_nodes_consistent(['AAG']).graph
+
+        # when
+        paths = list(Contigs(cdb).all_simple_paths())
+
+        # then
+        assert {'AAGCG', 'AAGCC'} == set([str(p.seq) for p in paths])
