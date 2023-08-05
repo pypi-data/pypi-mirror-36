017f8bcc3ae37c131d6705c6f7bf5cdc988c0f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:07 +0000

    Add package network-analyzer/0.0.1/network-analyzer-0.0.1.tar.gz

---
+++ packages/network-analyzer/network-analyzer-0.0.1.tar.gz/network-analyzer-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='network-analyzer',
+    version="0.0.1",
+    author="Ashutosh Mishra <ashutoshdtu@gmail.com>, Saurabh Shandilya <saurabhshandilya.1991@gmail.com>",
+    author_email="ashutoshdtu@gmail.com, saurabhshandilya.1991@gmail.com",
+    description="Analyze your network traffic",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ashutoshdtu/network-analyzer",
+    packages=['network_analyzer', 'rpc_apiserver', 'socketio_server'],
+    include_package_data=True,
+    install_requires=required,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Framework :: Flask",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: POSIX",
+        "Topic :: System :: Monitoring",
+        "Topic :: System :: Networking"
+    ]
+)
