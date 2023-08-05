83575e767b7197aca41cbf4b7670630e2f1ea26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:32 +0000

    Add package stellargraph/0.4.0b0/stellargraph-0.4.0b0.tar.gz

---
+++ packages/stellargraph/stellargraph-0.4.0b0.tar.gz/stellargraph-0.4.0b0/stellargraph/__init__.py
@@ -0,0 +1,10 @@
+"""
+Stellar Machine Learning Library
+
+"""
+
+__all__ = ["data", "layer", "mapper", "StellarDiGraph", "StellarGraph", "GraphSchema"]
+
+
+# Top-level imports
+from .data.stellargraph import StellarGraph, StellarDiGraph, GraphSchema
