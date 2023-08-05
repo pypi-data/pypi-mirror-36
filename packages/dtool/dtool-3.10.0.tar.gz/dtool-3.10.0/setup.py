0a8a489909315e225bc41d0ba9b9788fc7c94715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:58 +0000

    Add package dtool/3.10.0/dtool-3.10.0.tar.gz

---
+++ packages/dtool/dtool-3.10.0.tar.gz/dtool-3.10.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+url = "https://github.com/jic-dtool/dtool"
+version = "3.10.0"
+readme = open('README.rst').read()
+
+setup(
+    name="dtool",
+    packages=["dtool"],
+    version=version,
+    description="Package files and metadata into self contained datasets",
+    long_description=readme,
+    include_package_data=True,
+    author="Tjelvar Olsson",
+    author_email="tjelvar.olsson@jic.ac.uk",
+    url=url,
+    install_requires=[
+        "dtoolcore==3.7.0",
+        "dtool-cli==0.7.0",
+        "dtool-create==0.20.0",
+        "dtool-info==0.10.3",
+        "dtool-symlink==0.3.0",
+        "dtool-http==0.2.0",
+    ],
+    download_url="{}/tarball/{}".format(url, version),
+    license="MIT"
+)
