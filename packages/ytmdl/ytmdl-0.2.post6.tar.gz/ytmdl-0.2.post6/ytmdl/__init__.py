1d439db776625cd4d76bed0d66935d1f80fda473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:42 +0000

    Add package ytmdl/0.2.post6/ytmdl-0.2.post6.tar.gz

---
+++ packages/ytmdl/ytmdl-0.2.post6.tar.gz/ytmdl-0.2.post6/ytmdl/__init__.py
@@ -0,0 +1,12 @@
+name = "ytmdl"
+
+__all__ = ['ytmdl',
+           'defaults',
+           'dir',
+           'prepend',
+           'setupConfig',
+           'song',
+           'yt',
+           'cache',
+           'stringutils',
+           'utility']
