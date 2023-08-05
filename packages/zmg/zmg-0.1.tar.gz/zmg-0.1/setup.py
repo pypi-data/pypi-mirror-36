3a20a96db7a79b518f5f39d4d038a1b12dd2b150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:57 +0000

    Add package zmg/0.1/zmg-0.1.tar.gz

---
+++ packages/zmg/zmg-0.1.tar.gz/zmg-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='zmg',
+      version='0.1',
+      description='zmg desc',
+      url='https://github.com/jackgithup/FileLibrary',
+      author='jack',
+      author_email='18513102876@163.com',
+      license='MIT',
+      packages=['zmg'],
+      zip_safe=False)
