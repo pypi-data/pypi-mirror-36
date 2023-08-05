9e0e0970ce7de327de201dff2d505cae6bee5280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:48 +0000

    Add package wpexport/0.0.2a1/wpexport-0.0.2a1.tar.gz

---
+++ packages/wpexport/wpexport-0.0.2a1.tar.gz/wpexport-0.0.2a1/wpexport/__main__.py
@@ -0,0 +1,43 @@
+import os
+import argparse
+from wpexport import WPExport
+
+def main():
+    parser = argparse.ArgumentParser(description='Wordpress to GitHub')
+    parser.add_argument('--setup', default=False, action='store_true',  help='Create the config file')
+    parser.add_argument('--config', type=str, default=WPExport.DEFAULT_CONFIG, help='The config file.')
+    parser.add_argument('--output', type=str, help='Export the post to a folder')
+    parser.add_argument('--format', choices=WPExport.FORMATS, default='markdown_github', help='The format')
+
+    for name, info in WPExport.PARAMETER.items():
+        parser.add_argument('--'+name, type=str, help=info['help'])
+
+    args = parser.parse_args()
+    app = WPExport()
+
+    if args.setup:
+        app.cli()
+        app.save(args.config)
+        exit()
+
+    # load config if exists
+    if os.path.exists(args.config): app.load(args.config)
+
+    # load the param from the command line. It ovverid the other parameters
+    app.setup(**vars(args))
+
+    # check the parameters
+    if not app.check:
+        parser.print_help()
+
+    # if the output is set, only export to the folder
+    if args.output:
+        app.export(args.output)
+        exit()
+
+    # create the backup
+    app.backup()
+
+
+if __name__ == '__main__':
+    main()
