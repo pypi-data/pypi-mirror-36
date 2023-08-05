83f31423507d85a5781bec3a6548223cd9f84d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:25 +0000

    Add package shimao-nosidebar-frontend/20180914.0/shimao-nosidebar-frontend-20180914.0.tar.gz

---
+++ packages/shimao-nosidebar-frontend/shimao-nosidebar-frontend-20180914.0.tar.gz/shimao-nosidebar-frontend-20180914.0/hass_frontend/service_worker.js
@@ -0,0 +1,5 @@
+importScripts("/frontend_latest/precache-manifest.983109e773aa206aedcfacc5c813f141.js", "/frontend_latest/workbox-v3.5.0/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/frontend_latest/workbox-v3.5.0"});
+/* global importScripts */
+importScripts('/static/service-worker-hass.js');
+
