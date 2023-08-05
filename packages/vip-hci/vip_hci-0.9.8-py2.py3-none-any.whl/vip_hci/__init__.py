d19886de4780d8c1602f05b25b2dbd5519b7f0bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:06 +0000

    Add package vip-hci/0.9.8/vip_hci-0.9.8-py2.py3-none-any.whl

---
+++ packages/vip-hci/vip_hci-0.9.8-py2.py3-none-any.whl/vip_hci/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import (absolute_import)
+
+__version__ = "0.9.8"
+
+from . import andromeda
+from . import preproc
+from . import conf
+from . import fits
+from . import frdiff
+from . import leastsq
+from . import llsg
+from . import medsub
+from . import negfc
+from . import nmf
+from . import pca
+from . import metrics
+from . import stats
+from . import var
+from .hci_dataset import *
+from .hci_postproc import *
+from .vip_ds9 import *
---
+++ packages/vip-hci/vip_hci-0.9.8-py2.py3-none-any.whl/vip_hci/metrics/__init__.py
@@ -0,0 +1,20 @@
+"""
+Subpackage ``metrics`` includes:
+    - signal-to-noise (S/N) estimation,
+    - S/N map generation,
+    - detection of point like sources (for pipelines),
+    - fake companions injection,
+    - algorithms throughput,
+    - contrast curve generation,
+    - receiver operating characteristic (ROC) curves.
+"""
+from __future__ import absolute_import
+
+from .contrcurve import *
+from .detection import *
+from .fakecomp import *
+from .frame_analysis import *
+from .roc import *
+from .snr import *
+from .fakedisk import *
+from .scattered_light_disk import *
