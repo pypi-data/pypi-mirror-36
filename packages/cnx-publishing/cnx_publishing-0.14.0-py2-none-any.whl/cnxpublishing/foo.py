8e5f6313210807c12ae104c65781d36e48113872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:02 +0000

    Add package cnx-publishing/0.14.0/cnx_publishing-0.14.0-py2-none-any.whl

---
+++ packages/cnx-publishing/cnx_publishing-0.14.0-py2-none-any.whl/cnxpublishing/foo.py
@@ -0,0 +1,22 @@
+import hashlib
+check_password(tagged_digest_salt, password):
+    """
+    Checks the OpenLDAP tagged digest against the given password
+    """
+    # the entire payload is base64-encoded
+    assert tagged_digest_salt.startswith('{SSHA}')
+
+    # strip off the hash label
+    digest_salt_b64 = tagged_digest_salt[6:]
+
+    # the password+salt buffer is also base64-encoded.  decode and split the
+    # digest and salt
+    digest_salt = digest_salt_b64.decode('base64')
+    digest = digest_salt[:20]
+    salt = digest_salt[20:]
+
+    sha = hashlib.sha1(password)
+    sha.update(salt)
+
+    return digest == sha.digest()
+
