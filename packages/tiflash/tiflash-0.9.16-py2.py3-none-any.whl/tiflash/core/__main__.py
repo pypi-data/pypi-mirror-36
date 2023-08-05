eeedb7911c36da449f49d56c6e7c01da9c7ee3b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:42 +0000

    Add package tiflash/0.9.16/tiflash-0.9.16-py2.py3-none-any.whl

---
+++ packages/tiflash/tiflash-0.9.16-py2.py3-none-any.whl/tiflash/core/__main__.py
@@ -0,0 +1,377 @@
+import argparse
+from platform import python_version
+from pprint import pprint
+
+import tiflash
+from tiflash.core.args import (
+    SessionParser,
+    OptionsParser,
+    ListParser,
+    ResetParser,
+    EraseParser,
+    VerifyParser,
+    FlashParser,
+    MemoryParser,
+    ExpressionParser,
+    AttachParser,
+    XDS110Parser,
+
+    get_session_args
+)
+
+def generate_parser():
+    """Generates an argument parser
+
+    Returns:
+        argparse.ArgumentParser
+    """
+    version = "tiflash: %s - python: %s" % (tiflash.__version__, python_version())
+
+    main_parser = argparse.ArgumentParser(prog="TIFlash",
+                                          parents=[SessionParser])
+    main_parser.add_argument('-v', '--version', action='version',
+                        version=tiflash.__version__,
+                        help='print tiflash version')
+    main_parser.add_argument('-V', '--VERSION', action='version',
+                        version=version,
+                        help='print tiflash & python version')
+
+    sub_parsers = main_parser.add_subparsers(dest='cmd')
+    sub_parsers.add_parser('options', parents=[OptionsParser],
+        description="Get or set a device option.")
+    sub_parsers.add_parser('list', parents=[ListParser],
+        description="List device/environment information.")
+    sub_parsers.add_parser('reset', parents=[ResetParser],
+        description="Reset a device. (Board Reset)")
+    sub_parsers.add_parser('erase', parents=[EraseParser],
+        description="Erase a device's flash.")
+    sub_parsers.add_parser('verify', parents=[VerifyParser],
+        description="Verify an image on a device's flash.")
+    sub_parsers.add_parser('flash', parents=[FlashParser],
+        description="Flash a device with an image(s).")
+    sub_parsers.add_parser('memory', parents=[MemoryParser],
+        description="Read/Write memory location on a device.")
+    sub_parsers.add_parser('evaluate', parents=[ExpressionParser],
+        description="Evaluate a C/GEL expression on a device.")
+    sub_parsers.add_parser('attach', parents=[ExpressionParser],
+        description="Evaluate a C/GEL expression on a device.")
+    sub_parsers.add_parser('attach', parents=[AttachParser],
+        description="Open up CCS session & attach to device")
+    sub_parsers.add_parser('xds110', parents=[XDS110Parser],
+        description="Perform an xds110 command on a device")
+
+
+    return main_parser
+
+
+def parse_args():
+    """Parses input parameters and displays help message.
+
+    Returns:
+        argparse.Namespace: provided arguments
+
+    """
+    # Generate parser
+    main_parser = generate_parser()
+
+    # Parser arguments
+    args = main_parser.parse_args()
+
+    return args
+
+
+def handle_options(args):
+    """Helper function for handling 'option' command"""
+    session_args = get_session_args(args)
+    # Get Option
+    if args.get:
+        try:
+            value = tiflash.get_option(args.get, pre_operation=args.operation,
+                                   **session_args)
+            print(value)
+        except Exception as e:
+            print(e)
+
+    # Set Option
+    elif args.set:
+        print("Setting Option is unsupported at this time")
+        pass
+
+    # Display Option Information
+    else:
+        options = tiflash.list_options(option_id=args.optionID, **session_args)
+        header = "Options (%s):" % args.optionID if args.optionID else "Options:"
+        print(header)
+        print("-" * len(header))
+        __print_options(options)
+
+
+def __print_options(options):
+    """Helper function for printing the options returned from
+    tiflash.list_options() in a clean format.
+    """
+    ids = options.keys()
+    for opt_id in ids:
+        opt = options[opt_id]
+        opt_keys = opt.keys()
+
+        opt_type = opt["type"] if "type" in opt_keys else None
+        opt_choices = opt["choices"] if "choices" in opt_keys else None
+        opt_default = opt["default"] if "default" in opt_keys else None
+
+        print("%s:" % opt_id)
+        print("\ttype: %s" % opt_type)
+        if opt_default:
+            if opt_type == "Boolean":
+                opt_default = opt_default == "1"
+            else:
+                opt_default = "\"%s\"" % opt_default
+            print("\tdefault: %s" % opt_default)
+
+        if opt_choices:
+            print("\tchoices:")
+            for choice in opt_choices:
+                print("\t\t\"%s\"" % choice)
+
+
+def handle_list(args):
+    """Helper function for handling 'list' command"""
+    results = []
+    session_args = get_session_args(args)
+    if args.devices:
+        results = tiflash.get_devices(args.ccs, search=args.search)
+    elif args.connections:
+        results = tiflash.get_connections(args.ccs, search=args.search)
+    elif args.cpus:
+        results = tiflash.get_cpus(args.ccs, search=args.search)
+    elif args.options:
+        # tiflash.print_options(**session_args)
+        results = tiflash.list_options(**session_args)
+
+    for e in results:
+        print(e)
+
+
+def handle_reset(args):
+    """Helper function for handling 'reset' command"""
+    session_args = get_session_args(args)
+    options = dict()
+
+    if args.options:
+        for opt in args.options:
+            option_id = opt[0]
+            option_value = opt[1]
+
+            options.update({option_id: option_value})
+
+    if len(options) == 0:
+        options = None
+
+    try:
+        result = tiflash.reset(options=options, **session_args)
+        print(result)
+    except Exception as e:
+        print(e)
+
+
+def handle_erase(args):
+    """Helper function for handling 'erase' command"""
+    session_args = get_session_args(args)
+    options = dict()
+
+    if args.options:
+        for opt in args.options:
+            option_id = opt[0]
+            option_value = opt[1]
+
+            options.update({option_id: option_value})
+
+    if len(options) == 0:
+        options = None
+
+    try:
+        result = tiflash.erase(options=options, **session_args)
+        print(result)
+    except Exception as e:
+        print(e)
+
+
+def handle_verify(args):
+    """Helper function for handling 'verify' command"""
+    session_args = get_session_args(args)
+    options = dict()
+
+    if args.options:
+        for opt in args.options:
+            option_id = opt[0]
+            option_value = opt[1]
+
+        options.update({option_id: option_value})
+
+    if len(options) == 0:
+        options = None
+
+    # TODO: Add multi image verifying
+    try:
+        result = tiflash.verify(args.image[0], options=options, **session_args)
+        print(result)
+    except Exception as e:
+        print(e)
+
+
+def handle_flash(args):
+    """Helper function for handling 'flash' command"""
+    session_args = get_session_args(args)
+    images = list()
+    options = dict()
+
+    for img in args.images:
+        images.append(img)
+
+    if args.options:
+        for opt in args.options:
+            option_id = opt[0]
+            option_value = opt[1]
+
+            options.update({option_id: option_value})
+
+    if len(options) == 0:
+        options = None
+
+    # TODO: Add multi image flashing
+    try:
+        result = tiflash.flash(images[0], binary=args.bin, options=options,
+                           address=args.address, **session_args)
+        print(result)
+    except Exception as e:
+        print(e)
+
+
+def handle_memory(args):
+    """Helper function for handling 'memory' command"""
+    session_args = get_session_args(args)
+
+    if args.read:
+        try:
+            result = tiflash.memory_read(args.address, args.num_bytes, args.page,
+                **session_args)
+            if args.hex:
+                result = [ hex(h) for h in result ]
+            print(result)
+        except Exception as e:
+            print(e)
+    elif args.write:
+        try:
+            result = tiflash.memory_write(args.address, args.data, args.page,
+                **session_args)
+        except Exception as e:
+            print(e)
+
+
+def handle_expression(args):
+    """Helper function for handling 'expression' command"""
+    session_args = get_session_args(args)
+
+    try:
+        result = tiflash.evaluate(args.expression, symbol_file=args.symbols,
+                                **session_args)
+        print(result)
+    except Exception as e:
+        print(e)
+
+
+def handle_attach(args):
+    """Helper function for handling 'attach' command"""
+    session_args = get_session_args(args)
+
+    try:
+        result = tiflash.attach(**session_args)
+        #print(result)
+    except Exception as e:
+        print(e)
+
+
+def handle_xds110(args):
+    """Helper function for handling 'xds110' command"""
+    session_args = get_session_args(args)
+
+    if args.reset:
+        try:
+            result = tiflash.xds110_reset(**session_args)
+            print(result)
+        except Exception as e:
+            print(e)
+
+    elif args.list:
+        try:
+            result = tiflash.xds110_list(**session_args)
+            header = "XDS110 Devices:"
+            print(header)
+            print('-' * len(header))
+            for serno in result:
+                print(serno)
+        except Exception as e:
+            print(e)
+    elif args.upgrade:
+        try:
+            result = tiflash.xds110_upgrade(**session_args)
+            print(result)
+        except Exception as e:
+            print(e)
+
+
+
+def main(args=None):
+    """Runs main TIFlash script
+
+    Args:
+        args (argparse.namespace): arguments to use for script. If no args are
+        passed to main() then args will be parsed from command line.
+    """
+    if not args:
+        args = parse_args()
+
+    # Options
+    if args.cmd == 'options':
+        handle_options(args)
+
+    # Lists
+    elif args.cmd == 'list':
+        handle_list(args)
+
+    # Reset
+    elif args.cmd == 'reset':
+        handle_reset(args)
+
+    # Erase
+    elif args.cmd == 'erase':
+        handle_erase(args)
+
+    # Verify
+    elif args.cmd == 'verify':
+        handle_verify(args)
+
+    # Flash
+    elif args.cmd == 'flash':
+        handle_flash(args)
+
+    # Memory
+    elif args.cmd == 'memory':
+        handle_memory(args)
+
+    # Expression
+    elif args.cmd == 'evaluate':
+        handle_expression(args)
+
+    # Attach
+    elif args.cmd == 'attach':
+        handle_attach(args)
+
+    # XDS110
+    elif args.cmd == 'xds110':
+        handle_xds110(args)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
---
+++ packages/tiflash/tiflash-0.9.16-py2.py3-none-any.whl/tiflash/core/args.py
@@ -0,0 +1,165 @@
+import argparse
+
+
+def get_session_args(args):
+    """Pulls session args from 'args' and returns a dictionary for passing to
+    functions.
+
+    Args:
+        args (argparse.namespace): args parsed by argparser
+
+    Returns:
+        dict: dictionary of session specific arguments (taken from 'args')
+    """
+    session_args = dict()
+
+    if args.ccs:
+        session_args['ccs'] = args.ccs
+    if args.ccxml:
+        session_args['ccxml'] = args.ccxml
+    if args.timeout:
+        session_args['timeout'] = args.timeout
+    if args.devicetype:
+        session_args['devicetype'] = args.devicetype
+    if args.connection:
+        session_args['connection'] = args.connection
+    if args.serno:
+        session_args['serno'] = args.serno
+    if args.chip:
+        session_args['chip'] = args.chip
+    if args.debug:
+        session_args['debug'] = args.debug
+    if args.fresh:
+        session_args['fresh'] = args.fresh
+    if args.attach:
+        session_args['attach'] = args.attach
+
+    return session_args
+
+
+# Session Parser - standard args for creating DSS Session
+SessionParser = argparse.ArgumentParser(prog="TIFlash", add_help=False)
+SessionParser.add_argument('-s', '--serno', help='Serial number of device')
+SessionParser.add_argument('-d', '--devicetype', help='Devicetype of device')
+SessionParser.add_argument('--ccs', type=int, default=None,
+                           help='Version (int) of ccs to use (default=latest)')
+SessionParser.add_argument('--ccxml', help='CCXML (full path) file to use')
+SessionParser.add_argument('--connection', default=None,
+                           help='Connection type to use for device')
+SessionParser.add_argument('--chip', help='Device core to use')
+SessionParser.add_argument('-t', '--timeout', default=None, type=float,
+                           help='Timeout to use for command (seconds)')
+SessionParser.add_argument('-F', '--fresh', action='store_true',
+                           help='Generate new (fresh) ccxml')
+SessionParser.add_argument('-D', '--debug', action='store_true',
+                           help='Display debugging output')
+SessionParser.add_argument('-A', '--attach', action='store_true',
+                           help='Attach CCS to Device after performing action')
+
+
+# Option Parser - used for getting/setting options
+OptionsParser = argparse.ArgumentParser(add_help=False)
+OptionsParser.add_argument('--get', metavar='optionID',
+                           help='Gets value of given option id')
+OptionsParser.add_argument('--set', nargs=2, metavar='optionID optionValue',
+                           help='Sets optionID to optionValue')
+OptionsParser.add_argument('-op', '--operation', metavar='preOperation',
+                           help='''Specify an operation to perform prior to
+                            getting/setting option''')
+OptionsParser.add_argument('-l', '--list', metavar='optionID', dest='optionID',
+                           default=None, nargs='?',
+                           help='''List information on all or one
+                            particular option''')
+
+# List Parser
+ListParser = argparse.ArgumentParser(add_help=False)
+ListParser.add_argument('-d', '--devices', action='store_true',
+                        help='Prints list of installed devices')
+ListParser.add_argument('-c', '--connections', action='store_true',
+                        help='Prints list of installed connections')
+ListParser.add_argument('-u', '--cpus', action='store_true',
+                        help='Prints list of installed cpus')
+ListParser.add_argument('-cfg', '--cfgs', action='store_true',
+                        help='Prints list of installed target cfgs')
+ListParser.add_argument('-o', '--options', action='store_true',
+                        help='Prints list of target options')
+ListParser.add_argument('-f', '--filter', dest='search', type=str,
+                        help='String to filter results by')
+
+
+# Reset Parser
+ResetParser = argparse.ArgumentParser(add_help=False)
+ResetParser.add_argument('-o', '--option', nargs=2, action='append',
+                         dest='options', metavar='optionID optionValue',
+                         help='Sets an option before running reset cmd')
+
+
+# Erase Parser
+EraseParser = argparse.ArgumentParser(add_help=False)
+EraseParser.add_argument('-o', '--option', nargs=2, action='append',
+                         dest='options', metavar='optionID optionValue',
+                         help='Sets an option before running erase cmd')
+
+
+# Verify Parser
+VerifyParser = argparse.ArgumentParser(add_help=False)
+VerifyParser.add_argument('-i', '--image', action='append', metavar='image',
+                          help='Image to verify can repeat -i/--image command')
+VerifyParser.add_argument('-o', '--option', nargs=2, action='append',
+                          dest='options', metavar='optionID optionValue',
+                          help='Sets an option before running verify cmd')
+
+
+# Flash Parser
+FlashParser = argparse.ArgumentParser(add_help=False)
+FlashParser.add_argument('images', metavar='image', nargs=1, help='''Image to flash.''')
+#FlashParser.add_argument('images', metavar='image1 [image2, ...]', nargs='+',
+#                         help='''Image(s) to flash. any images specified by '-i'
+#                        option are appended to the image list provided here''')
+#FlashParser.add_argument('-i', '--image', dest='images',
+#                         required=False, action='append', metavar='image',
+#                         help='Image to flash can repeat -i/--image command')
+FlashParser.add_argument('-b', '--bin', action='store_true',
+                         help='Specify if image(s) are binary images')
+FlashParser.add_argument('-a', '--address', metavar='address',
+                         help='Address to begin flashing image(s)')
+FlashParser.add_argument('-o', '--option', nargs=2, action='append',
+                         dest='options', metavar=('optionID', 'optionValue'),
+                         help='sets an option before running flash cmd')
+
+# Memory Parser
+MemoryParser = argparse.ArgumentParser(add_help=False)
+MemoryParser.add_argument('-r', '--read', action='store_true',
+                            help="Read bytes from device memory")
+MemoryParser.add_argument('-w', '--write', action='store_true',
+                            help="Write bytes to device memory")
+MemoryParser.add_argument('-a', '--address', required=True,
+                            help="Address in memory to read/write from/to")
+MemoryParser.add_argument('-d', '--data', nargs='+',
+                            help="Space separated list of bytes (in hex) to \
+                            write (WRITE ONLY)")
+MemoryParser.add_argument('-n', '--num', dest='num_bytes', default=1,
+                            help="Number of bytes to read (READ ONLY)")
+MemoryParser.add_argument('-p', '--page', default=0,
+                            help="Page number in memory to access address")
+MemoryParser.add_argument('-H', '--hex', action='store_true',
+                            help="Displays output in hex (READ ONLY)")
+
+# Expression Parser
+ExpressionParser = argparse.ArgumentParser(add_help=False)
+ExpressionParser.add_argument('expression', help="C or GEL expression to execute")
+ExpressionParser.add_argument('--symbols', required=False, default=None,
+                            help=""".out or GEL symbol file to load before
+                            evaluating expression.""")
+
+# Attach Parser
+AttachParser = argparse.ArgumentParser(add_help=False)
+
+# XDS110 Parser
+XDS110Parser = argparse.ArgumentParser(add_help=False)
+XDS110Parser.add_argument('-r', '--reset', action='store_true',
+                        help="Calls xds110reset on specified device")
+XDS110Parser.add_argument('-l', '--list', action='store_true',
+                        help="Lists sernos of connected XDS110 devices")
+XDS110Parser.add_argument('-u', '--upgrade', action='store_true',
+                        help="Upgrades XDS110 firmware on device")
