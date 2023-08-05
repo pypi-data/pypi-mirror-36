a01b28463f3a29e1bcbfee64ea9be7f68181b218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:15 +0000

    Add package gardena-smart/0.2/gardena_smart-0.2.tar.gz

---
+++ packages/gardena-smart/gardena_smart-0.2.tar.gz/gardena_smart-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='gardena_smart',
+      version='0.2',
+      description='Library to access the gardena smart platform',
+      long_description="""This is an attempt at creating a library for the
+      gardena smart system in python. At the moment it is a work in progress and not
+      all components are supported. At the moment we only have read only access
+      to the mower component. Please see github page for more information""",
+      url='http://github.com/rcastberg/gardena_smart',
+      author='Rene Castberg',
+      author_email='rene@castberg.org',
+      license='GPL',
+      packages=['gardena_smart'],
+      zip_safe=False)
