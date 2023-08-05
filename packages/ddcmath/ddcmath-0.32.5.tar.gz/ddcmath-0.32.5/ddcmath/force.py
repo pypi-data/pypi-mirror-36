3ea58403b75dd311ef71918f62edf28e35a16f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:59 +0000

    Add package ddcmath/0.32.5/ddcmath-0.32.5.tar.gz

---
+++ packages/ddcmath/ddcmath-0.32.5.tar.gz/ddcmath-0.32.5/ddcmath/force.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 by Christian Tremblay, P.Eng <christian.tremblay@servisys.com>
+#
+# Licensed under GPLv3, see file LICENSE in this source tree.
+from .temperature import delta_c2f, delta_f2c
+from .airflow import cfm2ls
+
+NMLBINRATIO = 8.850745792
+
+def nm_2_inlb(nm = None):   
+    return nm * NMLBINRATIO;
+    
+def inlb_2_nm(inlb = None):   
+    return inlb / NMLBINRATIO;
