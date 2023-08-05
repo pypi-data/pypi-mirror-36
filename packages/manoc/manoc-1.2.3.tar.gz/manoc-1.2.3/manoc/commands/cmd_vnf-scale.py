20eef6dd2e8d187fa2586580ce96c1d88dba49db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:27 +0000

    Add package manoc/1.2.3/manoc-1.2.3.tar.gz

---
+++ packages/manoc/manoc-1.2.3.tar.gz/manoc-1.2.3/manoc/commands/cmd_vnf-scale.py
@@ -0,0 +1,39 @@
+import sys
+
+import click
+import requests
+
+from manoc.cli import pass_context
+from manoc.utils.logging import log_request
+
+
+@click.command('vnf-scale')
+@click.argument('id')
+@click.option('--inputs', help='File with scale inputs.')
+@pass_context
+def cli(ctx, id, inputs):
+    """Command on scale vnf"""
+    url = "http://api-gateway-mano." + ctx.base_url \
+          + "/vnf-manager/" + ctx.tenant + "/vnflcm/v1/vnf_instances/" + id + "/scale/"
+
+    headers = {
+        'Content-Type': "application/json",
+        'Authorization': "Bearer " + ctx.access_token
+    }
+
+    if inputs is None:
+        click.echo('Inputs file not specified.')
+        sys.exit(1)
+
+    file = open(inputs, 'rb')
+    payload = file.read()
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+    log_request(ctx, response)
+
+    if response.status_code != 202:
+        click.echo("Operation failed")
+        sys.exit(1)
+
+    operation_url = response.headers['Location']
+    click.echo(operation_url[operation_url.rfind('/')+1:])
