4c55645843c7dca5d1254ba26774f5220595885b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:58 +0000

    Add package yahoo-finance-pynterface/1.0.2/yahoo_finance_pynterface-1.0.2.tar.gz

---
+++ packages/yahoo-finance-pynterface/yahoo_finance_pynterface-1.0.2.tar.gz/yahoo_finance_pynterface-1.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yahoo_finance_pynterface",
+    version="1.0.2",
+    author="Andrea del Monaco",
+    author_email="and.delmonaco@gmail.com",
+    description="A Python Interface to the Yahoo Finance API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andrea-dm/yahoo-finance-pynterface",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Financial and Insurance Industry",],
+)
