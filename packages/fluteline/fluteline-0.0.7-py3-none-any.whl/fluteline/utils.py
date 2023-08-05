9d68397b5dbe507020fbbe9b848387b2ba85ec44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:00 +0000

    Add package fluteline/0.0.7/fluteline-0.0.7-py3-none-any.whl

---
+++ packages/fluteline/fluteline-0.0.7-py3-none-any.whl/fluteline/utils.py
@@ -0,0 +1,31 @@
+from . import nodes
+from . import queues
+
+
+def connect(nodes):
+    '''
+    Connect a list of nodes.
+
+    Connected nodes have an ``output`` member which is the following node in
+    the line. The last node's ``output`` is a :class:`fluteline.Queue` for
+    easy plumbing.
+    '''
+    for a, b in zip(nodes[:-1], nodes[1:]):
+        a.output = b
+    b.output = queues.Queue()
+
+
+def start(nodes):
+    '''
+    Start multiple nodes.
+    '''
+    for node in nodes:
+        node.start()
+
+
+def stop(nodes):
+    '''
+    Stop multiple nodes.
+    '''
+    for node in nodes:
+        node.stop()
