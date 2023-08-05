6b32484c8b6b0b71939f320f50f0b23dafd8611e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:36 +0000

    Add package OLCTools/0.4.0/OLCTools-0.4.0.tar.gz

---
+++ packages/OLCTools/OLCTools-0.4.0.tar.gz/OLCTools-0.4.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="OLCTools",
+    version="0.4.0",
+    packages=find_packages(),
+    author="Andrew Low",
+    author_email="andrew.low@inspection.gc.ca",
+    url="https://github.com/lowandrew/OLCTools",
+    install_requires=['biopython', 'interop']
+)
