ea25a462f8913f0aa28f038f9beb3922b8c2651c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:14 +0000

    Add package wrfhydropy/0.0.15.dev3/wrfhydropy-0.0.15.dev3.tar.gz

---
+++ packages/wrfhydropy/wrfhydropy-0.0.15.dev3.tar.gz/wrfhydropy-0.0.15.dev3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='wrfhydropy',
+    version='0.0.15.dev3',
+    packages=find_packages(),
+    package_data={'wrfhydropy': ['core/data/*']},
+    url='https://github.com/NCAR/wrf_hydro_py',
+    license='MIT',
+    install_requires=['pandas',
+                      'f90nml',
+                      'netCDF4',
+                      'deepdiff',
+                      'pathlib',
+                      'xarray',
+                      'datetime',
+                      'pytest',
+                      'pytest-html',
+                      'pytest-datadir-ng',
+                      'boltons'],
+    author='Joe Mills',
+    author_email='jmills@ucar.edu',
+    description='Crude API for the WRF-Hydro model',
+)
