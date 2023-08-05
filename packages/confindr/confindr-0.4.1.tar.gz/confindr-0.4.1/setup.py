a0e4d55549a7a632a4077aa0d4289b998c8fc604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:35 +0000

    Add package confindr/0.4.1/confindr-0.4.1.tar.gz

---
+++ packages/confindr/confindr-0.4.1.tar.gz/confindr-0.4.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="confindr",
+    version="0.4.1",
+    packages=find_packages(),
+    scripts=['confindr/confindr.py'],
+    author="Andrew Low",
+    author_email="andrew.low@inspection.gc.ca",
+    url="https://github.com/lowandrew/ConFindr",
+    install_requires=['biopython', 'pysam', 'pytest']
+)
