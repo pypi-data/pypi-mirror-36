ed17fd9035eca972ad0bea92907f64d0a84262c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:08 +0000

    Add package wtylistdir/1.0/wtylistdir-1.0.tar.gz

---
+++ packages/wtylistdir/wtylistdir-1.0.tar.gz/wtylistdir-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'wtylistdir',
+    version = '1.0',
+    py_modules = ['wtylistdir'],
+    author = 'wangty',
+    author_email = '1002814312@qq.com',
+    url = 'https://test.pypi.org/manage/account/',
+    description = 'test'
+    )
