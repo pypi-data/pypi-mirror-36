ae6648d0425692386227a347edc06a781f6c7403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:14 +0000

    Add package zzhfun/0.3/zzhfun-0.3.tar.gz

---
+++ packages/zzhfun/zzhfun-0.3.tar.gz/zzhfun-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='zzhfun',
+      version='0.3',
+      description='zzh model function and data function',
+      url='https://github.com/FlashSnail/zzhfun',
+      author='Zzh',
+      author_email='zzh_0729@foxmail.com',
+      license='MIT',
+      zip_safe=False)
