04ae900dd82ad650ff53281a0cf8a9bc9f9de8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:04 +0000

    Add package ccstudiodss/0.0.0/ccstudiodss-0.0.0.tar.gz

---
+++ packages/ccstudiodss/ccstudiodss-0.0.0.tar.gz/ccstudiodss-0.0.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name='ccstudiodss',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'dss = ccstudiodss.cli:cli',
+        ],
+        'pytest11': [
+            'ccstudiodss = ccstudiodss.pytest [test]',
+        ]
+    },
+    install_requires=[
+        'attrs',
+        'click',
+        'javabridge',
+    ],
+    extras_require={
+        'dev': [
+            'gitignoreio',
+        ],
+        'test': [
+            'pytest',
+        ],
+    },
+)
