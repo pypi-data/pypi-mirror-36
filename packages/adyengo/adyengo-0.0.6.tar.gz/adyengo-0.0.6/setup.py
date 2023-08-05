8ee774970cd8a25acc31491c4f222a9748a7a8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:24 +0000

    Add package adyengo/0.0.6/adyengo-0.0.6.tar.gz

---
+++ packages/adyengo/adyengo-0.0.6.tar.gz/adyengo-0.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='adyengo',
+    keywords='django adyen payment integration',
+    version='0.0.6',
+    author='gitaarik',
+    author_email='gitaarik@gmail.com',
+    description='Django app for easy Adyen integration.',
+    url='https://github.com/gitaarik/adyengo',
+    license='LGPL licence, see LICENCE.txt',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=('ipaddress==1.0.4', 'python-dateutil==2.1', 'requests==2.9.1'),
+    zip_safe=False,
+)
