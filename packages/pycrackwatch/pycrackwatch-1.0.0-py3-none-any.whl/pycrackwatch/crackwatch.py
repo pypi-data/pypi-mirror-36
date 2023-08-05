d924467a392d37fd1d03dc3174e56074dc782098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:04 +0000

    Add package pycrackwatch/1.0.0/pycrackwatch-1.0.0-py3-none-any.whl

---
+++ packages/pycrackwatch/pycrackwatch-1.0.0-py3-none-any.whl/pycrackwatch/crackwatch.py
@@ -0,0 +1,20 @@
+import requests
+from .models import Crack
+
+_base_url = "http://api.crackwatch.com/api/{}"
+
+
+class Crackwatch:
+
+    def getCracks():
+        url = _base_url.format("cracks")
+        req = requests.get(url)
+        json_list = req.json()
+
+        crack_list = []
+
+        for j in json_list:
+            crack = Crack(j)
+            crack_list.append(crack)
+
+        return crack_list
