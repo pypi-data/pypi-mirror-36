e4b3f92dff883d28553262d5749caeab538746ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:52 +0000

    Add package yamata/1.0.0/yamata-1.0.0.tar.gz

---
+++ packages/yamata/yamata-1.0.0.tar.gz/yamata-1.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='yamata',
+      version='1.0.0',
+      description='a web frameware',
+      author='edward Zhang',
+      author_email='dingding@edwardzhang.cn',
+      maintainer_email='dingding@edwardzhang.cn',
+      url='https://github.com/yamata-web/yamata/',
+      packages=['yamata'],
+      long_description="Think about the ideal way to write a web app. Write the code to make it happen.",
+      license="MIT",
+      platforms=["linux"],
+      )
