686a849c9c7c4f1310db035f7d70b73b6cb2ba62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:18 +0000

    Add package weplot/0.0.9/weplot-0.0.9-py3-none-any.whl

---
+++ packages/weplot/weplot-0.0.9-py3-none-any.whl/weplot/__init__.py
@@ -0,0 +1,24 @@
+from .figure import (
+    FigureBuilder,
+    FigureHolder,
+)
+from .layout import (
+    Annotation,
+    Shape,
+    XAxis,
+    YAxis,
+    ZAxis,
+)
+from .plot import (
+    Scatter,
+    Line,
+    Box,
+    Bar,
+    Histogram,
+    Heatmap,
+    Scatter3D,
+    Line3D,
+    Surface,
+    Pie,
+)
+from .version import __version__
