7c227f04c59928b818e6b1eaf1ae4e3c379db72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:29 +0000

    Add package rowgenerators/0.8.13/rowgenerators-0.8.13.tar.gz

---
+++ packages/rowgenerators/rowgenerators-0.8.13.tar.gz/rowgenerators-0.8.13/rowgenerators/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .appurl.url import parse_app_url, Url
+from .appurl.util import get_cache
+from .core import get_generator
+from .source import  Source, RowGenerator
+from .appurl.web.download import Downloader
+from .exceptions import SourceError
+
+
