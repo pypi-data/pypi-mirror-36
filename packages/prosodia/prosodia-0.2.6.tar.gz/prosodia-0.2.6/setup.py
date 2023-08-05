d072bfcc1d7ccf3224b18bb31bdc810e72eb3ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:39 +0000

    Add package prosodia/0.2.6/prosodia-0.2.6.tar.gz

---
+++ packages/prosodia/prosodia-0.2.6.tar.gz/prosodia-0.2.6/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+test_requirements = [
+    'pytest-cov',
+    'pytest'
+]
+
+setup(
+    name='prosodia',
+    version='0.2.6',
+    author='macbeth322',
+    author_email='chrisp533@gmail.com',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=[
+        'typing'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    extras_require=dict(test=test_requirements),
+    package_data={
+        'prosodia': ['py.typed'],
+        '': ['*.grammar']
+    },
+    zip_safe=False
+)
