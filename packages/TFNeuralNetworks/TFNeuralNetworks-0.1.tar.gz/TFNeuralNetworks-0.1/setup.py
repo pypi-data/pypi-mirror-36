ba73d14b5317606a255fe05e7bc8447a53a3f048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:03 +0000

    Add package TFNeuralNetworks/0.1/TFNeuralNetworks-0.1.tar.gz

---
+++ packages/TFNeuralNetworks/TFNeuralNetworks-0.1.tar.gz/TFNeuralNetworks-0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="TFNeuralNetworks",
+    version="0.1",
+    author="Kevin O'Brien",
+    author_email="kevin.vincent.obrien@gmail.com",
+    description="A custom wrapper library for building highly encapsulated TensorFlow neural networks.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KevOBrien/TFNeuralNetworks",
+    packages=['TFNeuralNetworks'],
+    licence='MIT'
+)
