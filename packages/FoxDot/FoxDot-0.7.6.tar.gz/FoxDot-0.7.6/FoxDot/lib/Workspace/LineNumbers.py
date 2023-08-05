f90960e4597e14bb37499c31023d6b253eeb259f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:59 +0000

    Add package FoxDot/0.7.6/FoxDot-0.7.6.tar.gz

---
+++ packages/FoxDot/FoxDot-0.7.6.tar.gz/FoxDot-0.7.6/FoxDot/lib/Workspace/LineNumbers.py
@@ -0,0 +1,58 @@
+from __future__ import absolute_import, division, print_function
+
+try:
+    import Tkinter as Tk
+except ImportError:
+    import tkinter as Tk
+
+from ..Settings import LINE_NUMBER_MARKER_OFFSET
+
+class LineNumbers(Tk.Canvas):
+    def __init__(self, master, *args, **kwargs):
+        Tk.Canvas.__init__(self, *args, **kwargs)
+        self.textwidget = master
+        
+        self.redraw()
+
+    def redraw(self, *args):
+        '''redraw line numbers'''
+        self.delete("all")
+
+        # Draw a line
+
+        w = self.winfo_width() - 1
+        h = self.winfo_height()
+
+        self.create_line(w, 0, w, h, fill="gray")
+
+        i = self.textwidget.index("@0,0")
+        
+        while True:
+
+            dline=self.textwidget.dlineinfo(i)
+
+            if dline is None:
+                break
+
+            y = dline[1]
+            h = dline[3]
+
+            linenum  = int(str(i).split(".")[0])
+            curr_row = int(self.textwidget.index(Tk.INSERT).split(".")[0])
+
+            if linenum == curr_row:
+
+                x1, y1 = 0, y + LINE_NUMBER_MARKER_OFFSET
+                x2, y2 = w - 2, y + h
+
+                self.create_rectangle(x1, y1, x2, y2, fill="gray30", outline="gray30")
+
+            self.create_text(w - 4, y, anchor="ne",
+                             justify=Tk.RIGHT,
+                             text=linenum,
+                             font="CodeFont",
+                             fill="#c9c9c9")
+
+            i = self.textwidget.index("{}+1line".format(i))
+
+        self.after(30, self.redraw)
