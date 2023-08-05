27da210021ff91e11b29dccff3921d875fc89f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:21 +0000

    Add package option/0.1.2/option-0.1.2.tar.gz

---
+++ packages/option/option-0.1.2.tar.gz/option-0.1.2/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['option']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'option',
+    'version': '0.1.2',
+    'description': 'Rust like Option type in Python',
+    'long_description': '# Option\n',
+    'author': 'Peijun Ma',
+    'author_email': 'peijun.ma@protonmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
