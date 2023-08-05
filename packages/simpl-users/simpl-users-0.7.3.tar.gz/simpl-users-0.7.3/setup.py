50640b190ed1ab8d392b14a838b6cf848c1b631d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:42 +0000

    Add package simpl-users/0.7.3/simpl-users-0.7.3.tar.gz

---
+++ packages/simpl-users/simpl-users-0.7.3.tar.gz/simpl-users-0.7.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simpl-users',
+    version="0.7.3",
+    description='simpl-users provides Simpl Users',
+    long_description='readme',
+    author='Jeff Triplett',
+    author_email='jeff@revsys.com',
+    url='https://github.com/simplworld/simpl-users',
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        "django-username-email==2.1.3",
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+    ],
+)
+
