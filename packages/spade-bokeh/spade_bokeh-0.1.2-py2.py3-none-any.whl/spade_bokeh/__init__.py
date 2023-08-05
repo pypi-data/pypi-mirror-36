dad8adff39723eae85f687593edf5ac9906501d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:01 +0000

    Add package spade-bokeh/0.1.2/spade_bokeh-0.1.2-py2.py3-none-any.whl

---
+++ packages/spade-bokeh/spade_bokeh-0.1.2-py2.py3-none-any.whl/spade_bokeh/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for SPADE Bokeh Server."""
+
+__author__ = """Javi Palanca"""
+__email__ = 'jpalanca@gmail.com'
+__version__ = '0.1.2'
+
+from .spade_bokeh import BokehServerMixin  # noqa: F401
