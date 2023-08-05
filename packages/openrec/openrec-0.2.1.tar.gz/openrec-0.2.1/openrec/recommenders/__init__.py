d1377960e97effd56d0c35a2147c911b70efd889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:17 +0000

    Add package openrec/0.2.1/openrec-0.2.1.tar.gz

---
+++ packages/openrec/openrec-0.2.1.tar.gz/openrec-0.2.1/openrec/recommenders/__init__.py
@@ -0,0 +1,8 @@
+from openrec.recommenders.recommender import Recommender
+from openrec.recommenders.bpr import BPR
+from openrec.recommenders.pmf import PMF
+from openrec.recommenders.ucml import UCML
+from openrec.recommenders.vbpr import VBPR
+from openrec.recommenders.drr import DRR
+from openrec.recommenders.rnn_rec import RNNRec
+
