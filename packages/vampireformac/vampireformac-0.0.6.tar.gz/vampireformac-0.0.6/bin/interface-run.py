ffb778a52c86665fba4d8b4a83bc3f373efb1448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:47 +0000

    Add package vampireformac/0.0.6/vampireformac-0.0.6.tar.gz

---
+++ packages/vampireformac/vampireformac-0.0.6.tar.gz/vampireformac-0.0.6/bin/interface-run.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+import vampireformac
+print vampireformac.interface()
---
+++ packages/vampireformac/vampireformac-0.0.6.tar.gz/vampireformac-0.0.6/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vampireformac",
+    version="0.0.6",
+    author="Kyu Sang Han",
+    author_email="khan21@jhu.edu",
+    description="Vampire Image Analysis Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://wirtzlab.johnshopkins.edu",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'scipy',
+        'pandas',
+        'numpy',
+        'pillow',
+        'matplotlib',
+        'scikit-learn',
+        'imageio',
+        'opencv-python',
+    ],
+    scripts=['bin/interface-run.py'],
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+    ),
+)
