6c2f340d834af9b146cacf19ea21f9c81dd60b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:14 +0000

    Add package LibTextClassification/1.3/LibTextClassification-1.3-py2-none-any.whl

---
+++ packages/LibTextClassification/LibTextClassification-1.3-py2-none-any.whl/model/__init__.py
@@ -0,0 +1,5 @@
+from model.averagePooling import AveragePooling
+from model.textCNN import TextCNN
+from model.textRNN import TextRNN
+from model.train import Trainer
+from model.predict import Predicter
