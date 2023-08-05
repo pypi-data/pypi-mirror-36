73d0d40f454c4722aeaecf58b57184b8fb37d6da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:43 +0000

    Add package quickemail/0.1.1/quickemail-0.1.1.tar.gz

---
+++ packages/quickemail/quickemail-0.1.1.tar.gz/quickemail-0.1.1/setup.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+import setuptools
+from distutils.core import setup
+
+setup(name='quickemail',
+      version='0.1.1',
+      description='Quick send email use python3.',
+      long_description=open('README').read(),
+      author='Clayboy',
+      author_email = 'clayboy@foxmail.com',
+      url='http://nmla.cn',
+      py_modules=['quickemail']
+    )
+
+
