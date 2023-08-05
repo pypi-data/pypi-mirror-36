5134c8e2682d006c0f20ca7d74580ca1fa7fbd45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:36 +0000

    Add package exetby/0.2/exetby-0.2.tar.gz

---
+++ packages/exetby/exetby-0.2.tar.gz/exetby-0.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'exetby',
+  packages = ['exetby'],
+  version = '0.2',
+  license='MIT',
+  description = 'Nothing Special',
+  author = 'Bogdan Kortnov',
+  author_email = 'bogdankortnov@gmail.com',
+  url = 'https://github.com/Aponace',
+  download_url = 'https://github.com/Aponace/ExetBy/archive/0.2.tar.gz',
+  include_package_data=True,
+  keywords = ['Nothing', 'Special'],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
