548baae803e305c0b3c332f82ce172d6caea7c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package django-rest-framework-simplify/1.2.5.dev1/django-rest-framework-simplify-1.2.5.dev1.tar.gz

---
+++ packages/django-rest-framework-simplify/django-rest-framework-simplify-1.2.5.dev1.tar.gz/django-rest-framework-simplify-1.2.5.dev1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='django-rest-framework-simplify',
+    version='1.2.5.dev1',
+    description='Django Rest Framework Simplify',
+    author='Skyler Cain',
+    author_email='skylercain@gmail.com',
+    url='https://github.com/Skylude/django-rest-framework-simplify',
+    packages=['rest_framework_simplify', 'rest_framework_simplify.services', 'rest_framework_simplify.services.sql_executor']
+)
