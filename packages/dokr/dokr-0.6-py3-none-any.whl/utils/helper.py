0355fd70e6447de10ec39f324f8f89f936ea6a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:54 +0000

    Add package dokr/0.6/dokr-0.6-py3-none-any.whl

---
+++ packages/dokr/dokr-0.6-py3-none-any.whl/utils/helper.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+
+def cmd_exec(str):
+    print("\n------------------------ Executing Command: Start ------------------------")
+    print("\n$>>"+str);
+    output = os.popen(str).read().strip();
+    #print("\n$>>" + output);
+    print("\n------------------------ Executing Command: End ------------------------")
+    return os.popen(str).read().strip()
+
+
+def join_me(stringList):
+    return "".join(string for string in stringList)
+
+def helper():
+    print('\n------------------------ Command Options ------------------------')
+    print('\ndokr clean searchPattern # delete old images matching pattern')
+    print('\ndokr clean-all searchPattern # clean docker')
+    print('\ndokr lecs # ECS login')
+    print('\ndokr tag searchPattern tagName # add a tag *tagName*on a image matching *searchPattern* ')
+    print('\ndokr push searchPattern  # push all images matching the pattern')
+    print('\n')
