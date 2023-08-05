d093db4a53b5630506de4d109ac05e5eae28d56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:02 +0000

    Add package ben-cogs/0.0.22/ben_cogs-0.0.22.tar.gz

---
+++ packages/ben-cogs/ben_cogs-0.0.22.tar.gz/ben_cogs-0.0.22/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3.6
+# encoding: utf-8
+
+import re
+from setuptools import setup
+
+
+with open('ben_cogs/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+
+setup(
+	name='ben_cogs',
+	author='bmintz',
+	author_email='bmintz@protonmail.com',
+	url='https://github.com/bmintz/cogs',
+	download_url='https://github.com/bmintz/cogs/archive/{}.tar.gz'.format(version),
+	version=version,
+	packages=['ben_cogs'],
+	install_requires=[
+		'discord.py>=1.0.0a1430',
+		'humanize',
+		'objgraph',
+		'psutil'],
+	python_requires='>=3.6.0',
+	license='MIT',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Framework :: AsyncIO',
+		'License :: OSI Approved :: MIT License',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3.6',
+		'Topic :: Internet',
+		'Topic :: Utilities'])
