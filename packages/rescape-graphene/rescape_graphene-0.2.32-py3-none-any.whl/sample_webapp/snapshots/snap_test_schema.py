a8554c883c81d7c67b006bf51eea6fcba230ff06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:42 +0000

    Add package rescape-graphene/0.2.32/rescape_graphene-0.2.32-py3-none-any.whl

---
+++ packages/rescape-graphene/rescape_graphene-0.2.32-py3-none-any.whl/sample_webapp/snapshots/snap_test_schema.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['GenaralTypeCase::test_create 1'] = {
+    'email': '',
+    'firstName': 'T',
+    'id': '6',
+    'isActive': True,
+    'isStaff': False,
+    'isSuperuser': False,
+    'lastName': 'Rex',
+    'username': 'dino'
+}
+
+snapshots['GenaralTypeCase::test_update 1'] = {
+    'email': '',
+    'firstName': 'Al',
+    'id': '11',
+    'isActive': True,
+    'isStaff': False,
+    'isSuperuser': False,
+    'lastName': 'Lissaurus',
+    'password': 'pbkdf2_sha256$100000$not_random$4HHNXbLL1N0D4FgkwM5xhtJfUAlK9XVQgx3rIqTT628=',
+    'username': 'dino'
+}
