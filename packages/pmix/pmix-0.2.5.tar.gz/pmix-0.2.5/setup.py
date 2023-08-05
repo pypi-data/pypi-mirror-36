a4b311b38463a96dbf75400ce57d58d880a07ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:50 +0000

    Add package pmix/0.2.5/pmix-0.2.5.tar.gz

---
+++ packages/pmix/pmix-0.2.5.tar.gz/pmix-0.2.5/setup.py
@@ -0,0 +1,23 @@
+"""Setup"""
+from setuptools import setup, find_packages
+
+
+from pmix import __version__
+
+packages = find_packages(exclude=['test'])
+
+setup(
+    name='pmix',
+    version=__version__,
+    author='James K. Pringle',
+    author_email='jpringle@jhu.edu',
+    url='http://www.pma2020.org',
+    packages=packages,
+    license='LICENSE.txt',
+    description='Smattering of Python3 tools for PMA workflow',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'XlsxWriter>=0.7.0',
+        'xlrd>=0.9.3',
+    ],
+)
