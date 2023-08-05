be78170e58e425d719b38a76da26c17c1954c473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:39 +0000

    Add package PySwitchmate/0.3/PySwitchmate-0.3.tar.gz

---
+++ packages/PySwitchmate/PySwitchmate-0.3.tar.gz/PySwitchmate-0.3/switchmate/__init__.py
@@ -0,0 +1,62 @@
+"""Library to handle connection with Switchmate"""
+
+import logging
+
+import bluepy
+
+HANDLE = 0x2e
+ON_KEY = b'\x00'
+OFF_KEY = b'\x01'
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Switchmate:
+    """Representation of a Switchmate."""
+
+    def __init__(self, mac) -> None:
+        self._mac = mac
+        self.state = False
+        self._device = None
+        self._connect()
+
+    def _connect(self) -> bool:
+        if self._device:
+            try:
+                self._device.disconnect()
+            except bluepy.btle.BTLEException:
+                pass
+        try:
+            self._device = bluepy.btle.Peripheral(self._mac,
+                                                  bluepy.btle.ADDR_TYPE_RANDOM)
+        except bluepy.btle.BTLEException:
+            _LOGGER.error("Failed to connect to switchmate")
+            return False
+        return True
+
+    def _sendpacket(self, key, retry=2) -> bool:
+        try:
+            self._device.writeCharacteristic(HANDLE, key, True)
+        except bluepy.btle.BTLEException:
+            _LOGGER.error("Cannot connect to switchmate. Retrying")
+            if retry < 1:
+                return False
+            if not self._connect():
+                return False
+            self._sendpacket(key, retry-1)
+        return True
+
+    def update(self) -> None:
+        """Synchronize state with switch."""
+        try:
+            self.state = self._device.readCharacteristic(HANDLE) == ON_KEY
+        except bluepy.btle.BTLEException:
+            self._connect()
+
+    def turn_on(self) -> bool:
+        """Turn the switch on."""
+        return self._sendpacket(ON_KEY)
+
+    def turn_off(self) -> bool:
+        """Turn the switch off."""
+        return self._sendpacket(OFF_KEY)
