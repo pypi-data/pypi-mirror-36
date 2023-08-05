772f27a729f0c00d63040dd162ac33afa6bb20b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package extcats/2.2/extcats-2.2.tar.gz

---
+++ packages/extcats/extcats-2.2.tar.gz/extcats-2.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# setup script for the extcats package.
+#
+# Author: M. Giomi (matteo.giomi@desy.de)
+
+from setuptools import setup
+
+setup(
+    name='extcats',
+    version='2.2',
+    description='Tools to organize and query astronomical catalogs',
+    author='Matteo Giomi',
+    author_email='matteo.giomi@desy.de',
+    packages=['extcats'],
+    url = 'https://github.com/MatteoGiomi/extcats',
+    download_url = 'https://github.com/MatteoGiomi/extcats/archive/1.3.tar.gz',
+    install_requires=['pymongo', 'healpy', 'astropy', 'pandas', 'tqdm'],
+    )
