b525afc9001a7e42b1488d6c6e7b141543e42e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:54 +0000

    Add package shortesttrack-sdk/1.1.4/shortesttrack-sdk-1.1.4.tar.gz

---
+++ packages/shortesttrack-sdk/shortesttrack-sdk-1.1.4.tar.gz/shortesttrack-sdk-1.1.4/shortesttrack/client/iss_connector.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+from shortesttrack.utils import getLogger
+
+logger = getLogger()
+
+
+class ISSConnector:
+    def __init__(self, url: str, auth_custom_token: str = None) -> None:
+        self._url = url
+        logger.info(f'CONNECT ISERVICE: {url}')
+        self._auth_custom_token = auth_custom_token
+        logger.info(f'AUTH CUSTOM TOKEN: {auth_custom_token}')
+
+    def send(self, msg: dict) -> bytes:
+        header = {}
+        if self._auth_custom_token is not None:
+            header = {'Auth-Custom-Token': self._auth_custom_token}
+
+        r = requests.post(self._url, json=msg, headers=header)
+
+        logger.info(f'SEND MESSAGE(RESPONSE): {r.status_code}')
+        if r.status_code != 200:
+            raise Exception(r.status_code)
+
+        logger.info(f'SEND MESSAGE(CONTENT): {r.content}')
+        return r.content
+
+    def is_health(self) -> bool:
+        try:
+            r = requests.get(self._url.replace('iter', 'healthz'))
+            return json.loads(r.content.decode())['message'] == 'OK'
+        except:
+            return False
+
+    def is_ready(self) -> bool:
+        try:
+            r = requests.get(self._url.replace('iter', 'readyz'))
+            return json.loads(r.content.decode())['message'] == 'OK'
+        except:
+            return False
