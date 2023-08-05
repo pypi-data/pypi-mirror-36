5e235fee46e011d7f2df97d32f6386cfa3212d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:06 +0000

    Add package wagtail-react-streamfield/0.8.5/wagtail_react_streamfield-0.8.5-py3-none-any.whl

---
+++ packages/wagtail-react-streamfield/wagtail_react_streamfield-0.8.5-py3-none-any.whl/wagtail_react_streamfield/blocks/field_block.py
@@ -0,0 +1,29 @@
+from wagtail.core.blocks import (
+    FieldBlock, CharBlock, TextBlock, FloatBlock, DecimalBlock, RegexBlock,
+    URLBlock, DateBlock, TimeBlock, DateTimeBlock, EmailBlock, IntegerBlock,
+)
+
+from ..constants import FIELD_NAME_TEMPLATE
+
+
+class NewFieldBlock(FieldBlock):
+    def get_definition(self):
+        definition = super(FieldBlock, self).get_definition()
+        definition['html'] = self.render_form(self.get_default(),
+                                              prefix=FIELD_NAME_TEMPLATE)
+        title_template = self.get_title_template()
+        if title_template is not None:
+            definition['titleTemplate'] = title_template
+        return definition
+
+    def get_title_template(self):
+        if isinstance(self, (CharBlock, TextBlock, FloatBlock,
+                             DecimalBlock, RegexBlock, URLBlock,
+                             DateBlock, TimeBlock, DateTimeBlock,
+                             EmailBlock, IntegerBlock)):
+            return '${%s}' % self.name
+
+    def value_from_datadict(self, data, files, prefix):
+        return self.value_from_form(
+            self.field.widget.value_from_datadict(
+                {'value': data['value']}, files, 'value'))
