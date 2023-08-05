98046762a16d1ddc156e7a68e1bd0567ad178302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:12 +0000

    Add package swine/2.5.0/swine-2.5.0.tar.gz

---
+++ packages/swine/swine-2.5.0.tar.gz/swine-2.5.0/swine/component/physics/collider/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .base_collider import BaseCollider
+from .polygon_collider import PolygonCollider
+from .box_collider import BoxCollider
---
+++ packages/swine/swine-2.5.0.tar.gz/swine-2.5.0/swine/object/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .anchors import Anchor
+from .component import Component
+from .game_object import GameObject
