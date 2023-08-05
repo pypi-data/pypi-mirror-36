3344f7ac73495f91a969000122b3d3538a224036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:44 +0000

    Add package scikit-datasets/0.1.14/scikit-datasets-0.1.14.tar.gz

---
+++ packages/scikit-datasets/scikit-datasets-0.1.14.tar.gz/scikit-datasets-0.1.14/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+
+setup(name='scikit-datasets',
+      packages=find_packages(),
+      version='0.1.14',
+      description='Scikit-learn-compatible datasets',
+      author='David Diaz Vico',
+      author_email='david.diaz.vico@outlook.com',
+      url='https://github.com/daviddiazvico/scikit-datasets',
+      download_url='https://github.com/daviddiazvico/scikit-datasets/archive/v0.1.14.tar.gz',
+      keywords=['scikit-learn', 'pandas'],
+      classifiers=['Intended Audience :: Science/Research',
+                   'Intended Audience :: Developers',
+                   'Topic :: Software Development',
+                   'Topic :: Scientific/Engineering',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6'],
+      install_requires=['scikit-learn', 'pandas'])
