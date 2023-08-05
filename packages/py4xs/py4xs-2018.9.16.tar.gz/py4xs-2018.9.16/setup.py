2525e89aeb2cf4efaf8488a052b41b999acee743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:10 +0000

    Add package py4xs/2018.9.16/py4xs-2018.9.16.tar.gz

---
+++ packages/py4xs/py4xs-2018.9.16.tar.gz/py4xs-2018.9.16/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='py4xs',
+    description="""python package for processing x-ray scattering data""",
+    version="2018.09.16",
+    author='Lin Yang',
+    author_email='lyang@bnl.gov',
+    license="MIT",
+    url="",
+    packages=['py4xs'],
+    install_requires=['fabio', 'h5py', 'pillow', 
+                      'matplotlib', 'numpy', 'scipy'],
+    python_requires='>=3',
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords='x-ray scattering',
+)
