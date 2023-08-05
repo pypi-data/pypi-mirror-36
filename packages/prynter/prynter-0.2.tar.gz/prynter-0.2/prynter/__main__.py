f796fe5ddab0b876718a895c05791d86176396a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:14 +0000

    Add package prynter/0.2/prynter-0.2.tar.gz

---
+++ packages/prynter/prynter-0.2.tar.gz/prynter-0.2/prynter/__main__.py
@@ -0,0 +1,18 @@
+import sys
+from . import run
+
+def main():
+    if len(sys.argv) < 2:
+        # show help
+        print('Help')
+        return
+
+    parameters = sys.argv[-1]
+    # Separate command in structions
+    instructions = parameters.split(';')
+
+    # verbose by default
+    options = {'-v': True}
+    run(*instructions, **options)
+
+main()
