452006c074887039f7f0dc2a601eef1c955f5609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:06 +0000

    Add package typepy/0.2.2/typepy-0.2.2.tar.gz

---
+++ packages/typepy/typepy-0.2.2.tar.gz/typepy-0.2.2/test/checker/test_checker_datetime.py
@@ -0,0 +1,47 @@
+# encoding: utf-8
+
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import unicode_literals
+
+import itertools
+from datetime import datetime
+
+import pytest
+import six
+from dateutil.tz import tzoffset
+from typepy import DateTime, StrictLevel, Typecode
+
+
+nan = float("nan")
+inf = float("inf")
+
+
+class Test_DateTime_is_type(object):
+    @pytest.mark.parametrize(
+        ["value", "strict_level", "expected"],
+        list(
+            itertools.product(
+                [datetime(2017, 3, 22, 10, 0, tzinfo=tzoffset(None, 32400))],
+                [StrictLevel.MIN, StrictLevel.MIN + 1, StrictLevel.MAX],
+                [True],
+            )
+        )
+        + list(
+            itertools.product(
+                [None, "invalid time string", six.MAXSIZE, "100-0004"],
+                [StrictLevel.MIN, StrictLevel.MIN + 1, StrictLevel.MAX],
+                [False],
+            )
+        )
+        + list(itertools.product(["2017-03-22T10:00:00+0900"], [StrictLevel.MIN], [True]))
+        + list(itertools.product(["2017-03-22T10:00:00+0900"], [StrictLevel.MAX], [False])),
+    )
+    def test_normal(self, value, strict_level, expected):
+        type_object = DateTime(value, strict_level)
+
+        assert type_object.is_type() == expected
+        assert type_object.typecode == Typecode.DATETIME
