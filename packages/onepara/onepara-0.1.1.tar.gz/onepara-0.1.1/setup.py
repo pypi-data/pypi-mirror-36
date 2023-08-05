5ba64745bb56bce2fa1c10a7a0274cbe9456b966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:34 +0000

    Add package onepara/0.1.1/onepara-0.1.1.tar.gz

---
+++ packages/onepara/onepara-0.1.1.tar.gz/onepara-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+def read(fname):
+    import os
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='onepara',
+      version='0.1.1',
+      description='One-Parameter Correlation Matrix',
+      long_description=read('README.md'),
+      long_description_content_type='text/markdown',
+      url='http://github.com/kmedian/onepara',
+      author='Ulf Hamster',
+      author_email='554c46@gmail.com',
+      license='MIT',
+      packages=['onepara'],
+      install_requires=['setuptools', 'nose', 'numpy', 'scikit-learn'],
+      python_requires='>=3',
+      zip_safe=False)
