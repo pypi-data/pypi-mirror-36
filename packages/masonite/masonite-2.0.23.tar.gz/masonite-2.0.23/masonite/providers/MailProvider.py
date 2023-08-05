2b0127d004cd9a2040ddc0230d89e85de789e93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:38 +0000

    Add package masonite/2.0.23/masonite-2.0.23.tar.gz

---
+++ packages/masonite/masonite-2.0.23.tar.gz/masonite-2.0.23/masonite/providers/MailProvider.py
@@ -0,0 +1,20 @@
+""" A Mail Service Provider """
+
+from config import mail
+from masonite.drivers import MailMailgunDriver, MailSmtpDriver
+from masonite.managers import MailManager
+from masonite.provider import ServiceProvider
+
+
+class MailProvider(ServiceProvider):
+
+    wsgi = False
+
+    def register(self):
+        self.app.bind('MailConfig', mail)
+        self.app.bind('MailSmtpDriver', MailSmtpDriver)
+        self.app.bind('MailMailgunDriver', MailMailgunDriver)
+        self.app.bind('MailManager', MailManager(self.app))
+
+    def boot(self, MailConfig, MailManager):
+        self.app.bind('Mail', MailManager.driver(MailConfig.DRIVER))
