db7ba1d1d2f183b0c2840f53824a650098610e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:13 +0000

    Add package git-fingerprint/0.0.3/git-fingerprint-0.0.3.tar.gz

---
+++ packages/git-fingerprint/git-fingerprint-0.0.3.tar.gz/git-fingerprint-0.0.3/git_fingerprint/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+__version__ = "0.0.3"
+__author__ = "Luke Paris (Paradoxis) <luke@paradoxis.nl>"
+
+from git_fingerprint.scanner import Scanner
+from git_fingerprint.exceptions import GitFingerprintException
