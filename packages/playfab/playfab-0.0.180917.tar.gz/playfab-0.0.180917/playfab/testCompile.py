727767a0090bdeb4374f4b7c9e73402e172d88a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:31 +0000

    Add package playfab/0.0.180917/playfab-0.0.180917.tar.gz

---
+++ packages/playfab/playfab-0.0.180917.tar.gz/playfab-0.0.180917/playfab/testCompile.py
@@ -0,0 +1,24 @@
+import py_compile
+
+files = [
+"PlayFabAdminAPI.py",
+"PlayFabClientAPI.py",
+"PlayFabMatchmakerAPI.py",
+"PlayFabServerAPI.py",
+"PlayFabAuthenticationAPI.py",
+"PlayFabCloudScriptAPI.py",
+"PlayFabDataAPI.py",
+"PlayFabEventsAPI.py",
+"PlayFabGroupsAPI.py",
+"PlayFabLocalizationAPI.py",
+"PlayFabProfilesAPI.py",
+
+"__init__.py",
+"PlayFabErrors.py",
+"PlayFabHTTP.py",
+"PlayFabSettings.py",
+"PlayFabManualTest.py"
+]
+
+for eachFile in files:
+    py_compile.compile(eachFile)
