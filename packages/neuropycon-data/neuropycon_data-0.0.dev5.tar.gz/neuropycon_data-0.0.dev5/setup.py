1b48d4bdf22a916db1ca81727b072b01c12a300d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:35 +0000

    Add package neuropycon-data/0.0.dev5/neuropycon_data-0.0.dev5.tar.gz

---
+++ packages/neuropycon-data/neuropycon_data-0.0.dev5.tar.gz/neuropycon_data-0.0.dev5/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name="neuropycon_data",
+    version='0.0.dev5',
+    packages=find_packages(),
+    author="David Meunier",
+    description="Data for neuropycon testing",
+    include_package_data=True,
+    install_requires=[]
+)
