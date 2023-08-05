29a47e0d8b54b8739ad2b9c31cf99ca7cd08d2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:03 +0000

    Add package Crosscap/0.3.0/Crosscap-0.3.0.tar.gz

---
+++ packages/Crosscap/Crosscap-0.3.0.tar.gz/Crosscap-0.3.0/setup.py
@@ -0,0 +1,31 @@
+from inspect import cleandoc
+
+from setuptools import setup
+
+_version = {}
+exec(open('crosscap/_version.py').read(), _version)
+
+setup(
+    name = 'Crosscap',
+    packages = ['crosscap',],
+    version = _version['__version__'],
+    description = 'idk fix later',
+    author = 'Cory Dodt',
+    author_email = 'corydodt@gmail.com',
+    url = 'https://github.com/corydodt/Crosscap',
+    keywords = [],
+    classifiers = [],
+    scripts = ['bin/urltool'],
+    install_requires=cleandoc('''
+        click>=6.7
+        ftfy==4.4.3
+        future>=0.16.0
+        pytest>=3.1.0
+        pytest-cov>=2.5.1
+        pytest-flakes>=2.0.0
+        pytest-twisted>=1.5
+        pyyaml>=3.12,<4.0
+        twisted>=17.1.0
+        werkzeug>=0.13
+        ''').split()
+)
