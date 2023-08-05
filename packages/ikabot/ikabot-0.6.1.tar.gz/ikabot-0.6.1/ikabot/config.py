3405f4595821da9f535050dc5ab1dc8a78ab08cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:06 +0000

    Add package ikabot/0.6.1/ikabot-0.6.1.tar.gz

---
+++ packages/ikabot/ikabot-0.6.1.tar.gz/ikabot-0.6.1/ikabot/config.py
@@ -0,0 +1,10 @@
+
+ids = None
+ciudades = None
+infoUser = ''
+cookieFile = '.cookies.txt'
+telegramFile = '.telegram.txt'
+urlCiudad = 'view=city&cityId='
+urlIsla = 'view=island&islandId='
+prompt = ' >>  '
+tipoDeBien = ['Madera', 'Vino', 'Marmol', 'Cristal', 'Azufre']
