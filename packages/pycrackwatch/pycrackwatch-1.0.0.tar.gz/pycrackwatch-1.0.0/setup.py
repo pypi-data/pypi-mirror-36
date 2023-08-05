bb9cd80eacec17d6d6fca375928713a5a73075a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:04 +0000

    Add package pycrackwatch/1.0.0/pycrackwatch-1.0.0.tar.gz

---
+++ packages/pycrackwatch/pycrackwatch-1.0.0.tar.gz/pycrackwatch-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='pycrackwatch',
+    version='1.0.0',
+    author='cominixo',
+    author_email='lucasdutraluza@gmail.com',
+    long_description=long_description,
+    description='A Python wrapper for the CrackWatch API',
+    packages=[
+        'pycrackwatch',
+    ],
+)
