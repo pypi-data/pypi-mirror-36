21320d7c5d234ce248ec6ea75b3666409d8c3870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:46 +0000

    Add package django-sorcery/0.6.4/django-sorcery-0.6.4.tar.gz

---
+++ packages/django-sorcery/django-sorcery-0.6.4.tar.gz/django-sorcery-0.6.4/django_sorcery/db/url.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+import os
+
+import sqlalchemy as sa
+
+from django.conf import settings
+
+
+DIALECT_MAP = {
+    "django.db.backends.sqlite3": "sqlite",
+    "django.db.backends.postgresql": "postgresql",
+    "django.db.backends.mysql": "mysql",
+    "django.db.backends.oracle": "oracle",
+    "sqlserver_ado": "mssql",
+}
+
+ENGINE_OPTIONS_NORMALIZATION = {"echo": lambda x: x in ["True", True]}
+
+
+def get_settings(alias):
+    """
+    Returns database settings from either ``SQLALCHEMY_CONNECTIONS`` setting or ``DATABASES`` setting.
+    """
+
+    if hasattr(settings, "SQLALCHEMY_CONNECTIONS") and alias in settings.SQLALCHEMY_CONNECTIONS:
+        return settings.SQLALCHEMY_CONNECTIONS[alias]
+
+    return settings.DATABASES[alias]
+
+
+def _options_from_url(url, base_options):
+    options = base_options.copy()
+    options.update(
+        {
+            "engine_options": {
+                k.replace("engine_", ""): ENGINE_OPTIONS_NORMALIZATION.get(k.replace("engine_", ""), lambda x: x)(
+                    url.query.pop(k)
+                )
+                for k in list(url.query)
+                if k.startswith("engine_")
+            }
+        }
+    )
+    return options
+
+
+def make_url(alias_or_url):
+    """
+    Generates a URL either from a url string, environment variable, SQLALCHEMY_CONNECTIONS or DATABASES settings
+    ---------------------------------------------------------------------
+    alias_or_url: str
+        name of the alias or url as string
+    """
+    settings_url, settings_kwargs = None, {}
+    try:
+        settings_url, settings_kwargs = make_url_from_settings(alias_or_url)
+    except KeyError:
+        pass
+
+    try:
+        url = sa.engine.url.make_url(alias_or_url)
+
+    except sa.exc.ArgumentError:
+        pass
+
+    else:
+        return url, _options_from_url(url, settings_kwargs)
+
+    alias = alias_or_url
+
+    url = sa.engine.url.make_url(os.environ.get(alias.upper() + "_URL", None))
+    if url:
+        return url, _options_from_url(url, settings_kwargs)
+
+    return make_url_from_settings(alias)
+
+
+def make_url_from_settings(alias):
+    """
+    Generates a URL using the alias in settings
+    -------------------------------------------
+    alias: str
+        name of the alias
+
+    Overall settings are very similar with django database settings with a few extra keys.
+
+    ``USER`` - database user
+
+    ``PASSWORD`` - database user password
+
+    ``HOST`` - database host
+
+    ``NAME`` - database name
+
+    ``PORT`` - database name
+
+    ``DIALECT`` - dialect to be used in url, if not provided, will use the ``DIALECT_MAP`` to figure out a dialect to
+    be used in sqlalchemy url
+
+    ``DRIVER`` - If provided, will be used as the driver in sqlalchemy url
+
+    ``SQLALCHEMY`` - If provided, a custom :py:class:`..sqlalchemy.SQLAlchemy` class to be used
+
+    ``QUERY`` - querystring arguments for sqlalchemy url
+
+    ``OPTIONS`` - Optional arguments to be used to initialize the :py:class:`..sqlalchemy.SQLAlchemy` instance
+
+        * ``session_class`` - a custom session class to be used
+        * ``registry_class`` - a custom registy class to be used for scoping
+        * ``model_class`` - a custom base model class to be used for declarative base models.
+        * ``metadata_class`` - a custom metadata class used in delclarative models.
+        * ``metadata_options`` - custom options to use in metadata creation such as specifying naming conventions.
+        * ``engine_options`` - arguments for sqlalchemy ``create_engine``
+        * ``session_options`` - arguments for sqlalchemy ``sessionmaker``
+
+    Other options are ignored.
+    """
+
+    data = get_settings(alias)
+
+    if "DIALECT" not in data:
+        data["DIALECT"] = DIALECT_MAP.get(data["ENGINE"]) or data["ENGINE"].split(".")[-1]
+
+    names = [data["DIALECT"].lower()]
+
+    if "DRIVER" in data:
+        names.append(data["DRIVER"].lower())
+
+    drivername = "+".join(names)
+
+    url = sa.engine.url.URL(drivername, username=data.get("USER") or None, password=data.get("PASSWORD") or None)
+
+    url.host = data.get("HOST") or None
+    url.database = data.get("NAME") or None
+    try:
+        url.port = int(data.get("PORT"))
+    except Exception:
+        pass
+
+    url.query.update(data.get("QUERY", {}))
+
+    return url, data.get("OPTIONS", {})
---
+++ packages/django-sorcery/django-sorcery-0.6.4.tar.gz/django-sorcery-0.6.4/tests/db/test_url.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+import os
+
+from django.test import TestCase, override_settings
+
+from django_sorcery.db.url import make_url
+
+
+@override_settings(
+    SQLALCHEMY_CONNECTIONS={
+        "bad": {},
+        "minimal": {"DIALECT": "sqlite"},
+        "from_env_preserve": {"DIALECT": "sqlite", "OPTIONS": {"foo": "bar"}},
+        "default": {
+            "DIALECT": "postgresql",
+            "DRIVER": "psycopg2",
+            "USER": "usr",
+            "PASSWORD": "hunter2",
+            "HOST": "pghost",
+            "NAME": "pgdb",
+            "PORT": "5432",
+        },
+        "ora": {"DIALECT": "oracle", "HOST": "orahost", "NAME": "oradb"},
+    },
+    DATABASES={
+        "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"},
+        "dummy": {"ENGINE": "django.db.backends.sqlite3", "DRIVER": "sqlite3", "NAME": "dummy.sqlite3"},
+    },
+)
+class TestMakeUrl(TestCase):
+    def setUp(self):
+        super(TestMakeUrl, self).setUp()
+        os.environ["FROM_ENV_URL"] = "postgresql://usr:hunter2@awesomedomain/db?engine_echo=True"
+        os.environ["FROM_ENV_PRESERVE_URL"] = "postgresql://usr:hunter2@awesomedomain/db?engine_echo=True"
+
+    def tearDown(self):
+        super(TestMakeUrl, self).tearDown()
+        os.environ.pop("FROM_ENV_URL", None)
+        os.environ.pop("FROM_ENV_PRESERVE_URL", None)
+
+    def test_handles_url(self):
+        url, options = make_url("postgresql://usr:hunter2@awesomedomain/db?engine_echo=True")
+        self.assertEqual(url.database, "db")
+        self.assertEqual(url.drivername, "postgresql")
+        self.assertEqual(url.host, "awesomedomain")
+        self.assertEqual(url.password, "hunter2")
+        self.assertEqual(url.port, None)
+        self.assertEqual(url.query, {})
+        self.assertEqual(url.username, "usr")
+        self.assertDictEqual(options, {"engine_options": {"echo": True}})
+
+    def test_requires_dialect(self):
+        with self.assertRaises(KeyError):
+            make_url("bad")
+
+    def test_can_override_from_env(self):
+        url, options = make_url("from_env")
+        self.assertEqual(url.database, "db")
+        self.assertEqual(url.drivername, "postgresql")
+        self.assertEqual(url.host, "awesomedomain")
+        self.assertEqual(url.password, "hunter2")
+        self.assertEqual(url.port, None)
+        self.assertEqual(url.query, {})
+        self.assertEqual(url.username, "usr")
+        self.assertDictEqual(options, {"engine_options": {"echo": True}})
+
+    def test_can_override_from_env_preserve(self):
+        url, options = make_url("from_env_preserve")
+        self.assertEqual(url.database, "db")
+        self.assertEqual(url.drivername, "postgresql")
+        self.assertEqual(url.host, "awesomedomain")
+        self.assertEqual(url.password, "hunter2")
+        self.assertEqual(url.port, None)
+        self.assertEqual(url.query, {})
+        self.assertEqual(url.username, "usr")
+        self.assertDictEqual(options, {"engine_options": {"echo": True}, "foo": "bar"})
+
+    def test_can_generate_minimal(self):
+        url, _ = make_url("minimal")
+        self.assertEqual(url.database, None)
+        self.assertEqual(url.drivername, "sqlite")
+        self.assertEqual(url.host, None)
+        self.assertEqual(url.password, None)
+        self.assertEqual(url.port, None)
+        self.assertEqual(url.query, {})
+        self.assertEqual(url.username, None)
+
+    def test_can_generate_full(self):
+        url, _ = make_url("default")
+        self.assertEqual(url.database, "pgdb")
+        self.assertEqual(url.drivername, "postgresql+psycopg2")
+        self.assertEqual(url.host, "pghost")
+        self.assertEqual(url.password, "hunter2")
+        self.assertEqual(url.port, 5432)
+        self.assertEqual(url.query, {})
+        self.assertEqual(url.username, "usr")
+
+    def test_can_handle_oracle_tns(self):
+        url, _ = make_url("ora")
+        self.assertEqual(url.database, "oradb")
+        self.assertEqual(url.drivername, "oracle")
+        self.assertEqual(url.host, "orahost")
+        self.assertEqual(url.password, None)
+        self.assertEqual(url.port, None)
+        self.assertEqual(url.query, {})
+        self.assertEqual(url.username, None)
+
+    def test_can_generate_from_databases(self):
+        url, _ = make_url("dummy")
+        self.assertEqual(url.database, "dummy.sqlite3")
+        self.assertEqual(url.drivername, "sqlite+sqlite3")
+        self.assertEqual(url.host, None)
+        self.assertEqual(url.password, None)
+        self.assertEqual(url.port, None)
+        self.assertEqual(url.query, {})
+        self.assertEqual(url.username, None)
