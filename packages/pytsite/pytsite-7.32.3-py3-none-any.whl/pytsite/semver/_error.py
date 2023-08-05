701214e821e04b97fd3d2b3bfd4031750b0aa2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:37 +0000

    Add package pytsite/7.32.3/pytsite-7.32.3-py3-none-any.whl

---
+++ packages/pytsite/pytsite-7.32.3-py3-none-any.whl/pytsite/semver/_error.py
@@ -0,0 +1,22 @@
+"""PytSite Semantic Versioning Errors
+"""
+
+__author__ = 'Oleksandr Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class InvalidRequirementString(Exception):
+    pass
+
+
+class InvalidVersionString(Exception):
+    pass
+
+
+class InvalidCondition(Exception):
+    pass
+
+
+class InvalidComparisonOperator(Exception):
+    pass
