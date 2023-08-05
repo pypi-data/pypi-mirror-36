e654d8b1373d1f06773df85a61f76832a433ab04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package mlflow/0.6.0/mlflow-0.6.0.tar.gz

---
+++ packages/mlflow/mlflow-0.6.0.tar.gz/mlflow-0.6.0/mlflow/entities/experiment.py
@@ -0,0 +1,46 @@
+from mlflow.entities._mlflow_object import _MLflowObject
+from mlflow.protos.service_pb2 import Experiment as ProtoExperiment
+
+
+class Experiment(_MLflowObject):
+    """
+    Experiment object.
+    """
+    DEFAULT_EXPERIMENT_ID = 0
+
+    def __init__(self, experiment_id, name, artifact_location):
+        super(Experiment, self).__init__()
+        self._experiment_id = experiment_id
+        self._name = name
+        self._artifact_location = artifact_location
+
+    @property
+    def experiment_id(self):
+        """Integer ID of the experiment."""
+        return self._experiment_id
+
+    @property
+    def name(self):
+        """String name of the experiment."""
+        return self._name
+
+    @property
+    def artifact_location(self):
+        """String corresponding to the root artifact URI for the experiment."""
+        return self._artifact_location
+
+    @classmethod
+    def from_proto(cls, proto):
+        return cls(proto.experiment_id, proto.name, proto.artifact_location)
+
+    def to_proto(self):
+        proto = ProtoExperiment()
+        proto.experiment_id = self.experiment_id
+        proto.name = self.name
+        proto.artifact_location = self.artifact_location
+        return proto
+
+    @classmethod
+    def _properties(cls):
+        # TODO: Hard coding this list of props for now. There has to be a clearer way...
+        return ["experiment_id", "name", "artifact_location"]
---
+++ packages/mlflow/mlflow-0.6.0.tar.gz/mlflow-0.6.0/mlflow/store/artifact_repo.py
@@ -0,0 +1,91 @@
+from abc import abstractmethod, ABCMeta
+
+from mlflow.store.rest_store import RestStore
+from mlflow.exceptions import MlflowException
+
+
+class ArtifactRepository:
+    """
+    Defines how to upload (log) and download potentially large artifacts from different
+    storage backends.
+    """
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, artifact_uri):
+        self.artifact_uri = artifact_uri
+
+    @abstractmethod
+    def log_artifact(self, local_file, artifact_path=None):
+        """
+        Logs a local file as an artifact, optionally taking an ``artifact_path`` to place it in
+        within the run's artifacts. Run artifacts can be organized into directories, so you can
+        place the artifact in a directory this way.
+        :param local_file: Path to artifact to log
+        :param artifact_path: Directory within the run's artifact directory in which to log the
+                              artifact
+        """
+        pass
+
+    @abstractmethod
+    def log_artifacts(self, local_dir, artifact_path=None):
+        """
+        Logs the files in the specified local directory as artifacts, optionally taking
+        an ``artifact_path`` to place them in within the run's artifacts.
+        :param local_dir: Directory of local artifacts to log
+        :param artifact_path: Directory within the run's artifact directory in which to log the
+                              artifacts
+        """
+        pass
+
+    @abstractmethod
+    def list_artifacts(self, path):
+        """
+        Return all the artifacts for this run_uuid directly under path.
+        :param path: Relative source path that contain desired artifacts
+        :return: List of artifacts as FileInfo listed directly under path.
+        """
+        pass
+
+    @abstractmethod
+    def download_artifacts(self, artifact_path):
+        """
+        Download an artifact file or directory to a local directory if applicable, and return a
+        local path for it.
+        The caller is responsible for managing the lifecycle of the downloaded artifacts.
+        :param path: Relative source path to the desired artifact
+        :return: Full path desired artifact.
+        """
+        # TODO: Probably need to add a more efficient method to stream just a single artifact
+        # without downloading it, or to get a pre-signed URL for cloud storage.
+        pass
+
+    @staticmethod
+    def from_artifact_uri(artifact_uri, store):
+        """
+        Given an artifact URI for an Experiment Run (e.g., /local/file/path or s3://my/bucket),
+        returns an ArtifactReposistory instance capable of logging and downloading artifacts
+        on behalf of this URI.
+        :param store: An instance of AbstractStore which the artifacts are registered in.
+        """
+        if artifact_uri.startswith("s3:/"):
+            # Import these locally to avoid creating a circular import loop
+            from mlflow.store.s3_artifact_repo import S3ArtifactRepository
+            return S3ArtifactRepository(artifact_uri)
+        elif artifact_uri.startswith("gs:/"):
+            from mlflow.store.gcs_artifact_repo import GCSArtifactRepository
+            return GCSArtifactRepository(artifact_uri)
+        elif artifact_uri.startswith("wasbs:/"):
+            from mlflow.store.azure_blob_artifact_repo import AzureBlobArtifactRepository
+            return AzureBlobArtifactRepository(artifact_uri)
+        elif artifact_uri.startswith("sftp:/"):
+            from mlflow.store.sftp_artifact_repo import SFTPArtifactRepository
+            return SFTPArtifactRepository(artifact_uri)
+        elif artifact_uri.startswith("dbfs:/"):
+            from mlflow.store.dbfs_artifact_repo import DbfsArtifactRepository
+            if not isinstance(store, RestStore):
+                raise MlflowException('`store` must be an instance of RestStore.')
+            return DbfsArtifactRepository(artifact_uri, store.get_host_creds)
+        else:
+            from mlflow.store.local_artifact_repo import LocalArtifactRepository
+            return LocalArtifactRepository(artifact_uri)
