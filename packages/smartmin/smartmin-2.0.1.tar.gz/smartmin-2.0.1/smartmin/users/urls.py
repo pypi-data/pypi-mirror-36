20d96c6191cc5748b7c956874702d48416b20434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:03 +0000

    Add package smartmin/2.0.1/smartmin-2.0.1.tar.gz

---
+++ packages/smartmin/smartmin-2.0.1.tar.gz/smartmin-2.0.1/smartmin/users/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls import url
+from django.contrib.auth.views import logout
+from .views import login, UserCRUDL
+
+logout_url = getattr(settings, 'LOGOUT_REDIRECT_URL', None)
+
+urlpatterns = [
+    url(r'^login/$', login, dict(template_name='smartmin/users/login.html'), name="users.user_login"),
+    url(r'^logout/$', logout, dict(redirect_field_name='go', next_page=logout_url), name="users.user_logout"),
+]
+
+urlpatterns += UserCRUDL().as_urlpatterns()
