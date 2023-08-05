0ca15d56c5464dc1e5b9040cdbd1c0375da7fe18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:56 +0000

    Add package prynter/0.3/prynter-0.3.0-py3.6.egg

---
+++ packages/prynter/prynter-0.3.0-py3.6.egg/prynter/__main__.py
@@ -0,0 +1,60 @@
+import sys
+from . import run, __version__
+
+HELP = """
+usage: prynt [options] command
+    -h  --help         print this usage and exit
+    -v  --version      print version and exit
+    -s  --silent       do not print output
+"""
+
+OPTIONS_DICT = {
+    '--silent': 'silent',
+    's': 'silent',
+
+    'h': 'help',
+    '--help': 'help',
+
+    '--version': 'version',
+    'v': 'version'
+}
+
+def get_options(args):
+    opts = {}
+
+    for a in args:
+        if a not in OPTIONS_DICT: 
+            continue
+
+        if a.startswith('--'):
+            opts[OPTIONS_DICT[a]] = True
+        else:
+            # a ~ '-xyz'
+            for opt in a[1:]:
+                opts[OPTIONS_DICT[opt]] = True
+    
+    return opts
+
+def main():
+    if len(sys.argv) < 2:
+        print(HELP)
+        return
+
+    options = get_options(sys.argv[1:])
+
+    if 'help' in options:
+        print(HELP)
+        return
+    if 'version' in options:
+        print(__version__)
+        return
+
+    parameters = sys.argv[-1]
+    # Separate command in structions
+    instructions = parameters.split(';')
+
+    # verbose by default
+    run(*instructions, **options)
+
+if __name__ == '__main__':
+    main()
