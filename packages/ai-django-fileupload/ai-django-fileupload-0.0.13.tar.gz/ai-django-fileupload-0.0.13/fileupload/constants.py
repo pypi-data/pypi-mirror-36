6d9919286b9cdaa8c2369dc0d57779c9b449bf3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:48 +0000

    Add package ai-django-fileupload/0.0.13/ai-django-fileupload-0.0.13.tar.gz

---
+++ packages/ai-django-fileupload/ai-django-fileupload-0.0.13.tar.gz/ai-django-fileupload-0.0.13/fileupload/constants.py
@@ -0,0 +1,8 @@
+from django.contrib.staticfiles.templatetags.staticfiles import static
+
+
+UPLOADER_DEFAULT_THUMBNAIL = static('img/default-thumbnail.png')
+
+UPLOADER_LOGIN_REQUIRED = False
+
+UPLOADER_UPLOAD_DIRECTORY = 'media/attachments/'
---
+++ packages/ai-django-fileupload/ai-django-fileupload-0.0.13.tar.gz/ai-django-fileupload-0.0.13/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="ai-django-fileupload",
+    version="0.0.13",
+    author=u'Ambient Innovation GmbH',
+    author_email='info@ambient-innovation.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://github.com/ambient-innovation/ai-django-fileupload",
+    download_url="https://github.com/ambient-innovation/ai-django-fileupload",
+    license="The MIT License (MIT)",
+    description="Integrate jQuery fileupload",
+    long_description="A minimal django package with a working jquery file upload and list of files",
+    zip_safe=False,
+    install_requires=['pillow'],
+)
