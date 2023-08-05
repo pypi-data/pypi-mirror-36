ebe55ef077f457866c2374184fec5b5ea2709bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:51 +0000

    Add package pinkboto/0.0.6/pinkboto-0.0.6.tar.gz

---
+++ packages/pinkboto/pinkboto-0.0.6.tar.gz/pinkboto-0.0.6/pinkboto/__init__.py
@@ -0,0 +1,7 @@
+"""
+A Colorful AWS SDK wrapper for Python
+github.com/Hotmart-Org/pinkboto
+"""
+
+from .aws import aws
+from .utils import to_csv
---
+++ packages/pinkboto/pinkboto-0.0.6.tar.gz/pinkboto-0.0.6/test/test.py
@@ -0,0 +1,27 @@
+import pinkboto
+
+aws = pinkboto.aws(profile='production', region='us-east-1', cache=3600)
+
+# rds = aws.find({'resource': 'aws_db_instance'}, ['Endpoint.Address', 'AvailabilityZone'])
+# pinkboto.to_csv(rds, 'result.csv')
+
+
+
+selector = {'resource': 'aws_db_instance'}
+projection = [
+    'DBInstanceIdentifier',  # nome do banco
+    'Endpoint.Address',  # endpoint
+    'PubliclyAccessible',  # ip publico
+    'MultiAZ',  # multi AZ
+    'AvailabilityZone', 'SecondaryAvailabilityZone',  # qual zona
+    'DBSubnetGroup.DBSubnetGroupName',  # qual vpcs
+    'VpcSecurityGroups.VpcSecurityGroupId',  # security group
+    'DBInstanceClass',  # tipo da instancia
+    'Iops',  # iops reservado
+    'AllocatedStorage'  # tamanho do disco
+]
+
+rds = aws.find(selector, projection)
+
+pinkboto.to_csv(rds, 'rds.csv')
+a=1
