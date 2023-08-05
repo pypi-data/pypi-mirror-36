92d3006ad31cde44a81185a7acf63ef463bb6b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:39 +0000

    Add package specutils/0.4.dev1198/specutils-0.4.dev1198.tar.gz

---
+++ packages/specutils/specutils-0.4.dev1198.tar.gz/specutils-0.4.dev1198/docs/index.rst
@@ -0,0 +1,50 @@
+.. image:: img/logo.png
+
+The specutils package provides a basic interface for the loading, manipulation,
+and low-level analysis of spectroscopic data. The intention is for the
+generic data containers and accompanying modules to provide a basis for the
+astronomical community upon which more robust tooling can be built.
+
+The basic data container of specutils is the :class:`~specutils.Spectrum1D`
+class. It contains logic to handle multi-dimensional flux data, spectral axis
+in various forms (wavelenth, frequency, energy, velocity, etc.), convenient and
+unobtrusive wcs support, and uncertainty handling. This core container also
+maintaines a few other features including unit support, custom data loading,
+arithmetic operation support, and multi-dimensional flux data support.
+
+.. note:: support for collections of spectra with distinct spectral axes
+          information is slotted for a future release.
+
+
+Using specutils
+---------------
+
+.. toctree::
+   :maxdepth: 2
+
+   installation
+   getting_started
+   custom_loading
+   spectrum_collection
+   basic_analysis
+   arithmetic
+   smoothing
+   fitting
+   spectral_regions
+
+.. toctree::
+    :maxdepth: 1
+
+    high-level_API.rst
+
+
+Get Involved
+------------
+
+Please see :doc:`contributing` for information on bug reporting and
+contributing to the specutils project.
+
+.. toctree::
+   :maxdepth: 2
+
+   contributing
---
+++ packages/specutils/specutils-0.4.dev1198.tar.gz/specutils-0.4.dev1198/specutils/tests/test_continuum.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+import astropy.units as u
+
+from specutils.spectra.spectrum1d import Spectrum1D
+from specutils.fitting.continuum import fit_generic_continuum
+
+
+def single_peak_continuum():
+    np.random.seed(0)
+    x = np.linspace(0., 10., 200)
+    y_single = 3 * np.exp(-0.5 * (x - 6.3)**2 / 0.1**2)
+    y_single += np.random.normal(0., 0.2, x.shape)
+
+    y_continuum = 3.2 * np.exp(-0.5 * (x - 5.6)**2 / 4.8**2)
+    y_single += y_continuum
+    return x, y_single
+
+
+def test_continuum_fit():
+    """
+    This test fits the the first simulated spectrum from the fixture.  The
+    initial guesses are manually set here with bounds that essentially make
+    sense as the functionality of the test is to make sure the fit works and
+    we get a reasonable answer out **given** good initial guesses.
+    """
+
+    x_single_continuum, y_single_continuum = single_peak_continuum()
+    s_single_continuum = Spectrum1D(flux=y_single_continuum*u.Jy, spectral_axis=x_single_continuum*u.um)
+    g1_fit = fit_generic_continuum(s_single_continuum)
+
+    y_continuum_fitted = g1_fit(x_single_continuum)
+
+    y_continuum_fitted_expected = np.array([1.71364056, 1.87755574, 2.05310622, 2.23545755, 2.41977527,
+                                            2.60122493, 2.77497207, 2.93618225, 3.080021, 3.20165388,
+                                            3.29624643, 3.3589642, 3.38497273, 3.36943758, 3.30752428,
+                                            3.19439839, 3.02522545, 2.79517101, 2.49940062, 2.13307982])
+
+    assert np.allclose(y_continuum_fitted[::10], y_continuum_fitted_expected, atol=1e-5)
