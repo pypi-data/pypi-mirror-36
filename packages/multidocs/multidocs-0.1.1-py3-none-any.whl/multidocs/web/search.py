3cbbe056125dc1646b190c03b7bcd9577102491a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:29 +0000

    Add package multidocs/0.1.1/multidocs-0.1.1-py3-none-any.whl

---
+++ packages/multidocs/multidocs-0.1.1-py3-none-any.whl/multidocs/web/search.py
@@ -0,0 +1,19 @@
+import flask
+from flask.blueprints import Blueprint
+
+from multidocs.content import get_sidebar_html
+from .auth import requires_login
+
+
+blueprint = Blueprint("search", __name__)
+
+
+@blueprint.route("/search")
+@requires_login
+def search():
+    query = flask.request.args.get("for")
+    results = flask.current_app.search.search(query, num_results=15)
+    sidebar_html = get_sidebar_html()
+    return flask.render_template(
+        "search.html.j2", query=query, results=results, sidebar_html=sidebar_html
+    )
---
+++ packages/multidocs/multidocs-0.1.1-py3-none-any.whl/multidocs/web/static.py
@@ -0,0 +1,28 @@
+import os.path
+
+import flask
+from flask.blueprints import Blueprint
+
+from multidocs.globals import settings
+from .auth import requires_login
+
+
+blueprint = Blueprint("static", __name__)
+
+
+@blueprint.route("/<path:path>")
+@requires_login
+def serve_html(path):
+    full_path = os.path.join(settings.target_dir, path)
+    if os.path.isdir(full_path):
+        path += "/index.html"
+
+    if not os.path.exists(path) and not path.endswith(".html"):
+        path = path + ".html"
+
+    return flask.send_from_directory(settings.target_dir, path, cache_timeout=-1)
+
+
+@blueprint.route("/")
+def index():
+    return serve_html("index.html")
