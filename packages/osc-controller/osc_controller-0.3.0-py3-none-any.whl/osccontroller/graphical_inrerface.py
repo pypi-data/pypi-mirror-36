2a7711361e3ff67eb9645dd6e63c15bad439dfdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:01 +0000

    Add package osc-controller/0.3.0/osc_controller-0.3.0-py3-none-any.whl

---
+++ packages/osc-controller/osc_controller-0.3.0-py3-none-any.whl/osccontroller/graphical_inrerface.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+from osccontroller.option_containers import ButtonOptions
+
+
+class GraphicalInterface:
+
+    def __init__(self, width: int, height: int, client, title: str = "OSCController"):
+        self.tk_root = tk.Tk()
+        self.tk_root.title(title)
+        self.tk_root.geometry('%dx%d' % (width, height))
+        self.elements = []
+        self.client = client
+
+    def loop(self):
+        self.tk_root.mainloop()
+
+    def add_button(self, x: int, y: int, text: str, options: ButtonOptions = ButtonOptions(),
+                   osc_content=None, command=None):
+        options_dict = {arg: val for arg, val in options._asdict().items() if val is not None}
+        if osc_content is not None:
+            self.elements.append(tk.Button(self.tk_root,
+                                           text=text,
+                                           command=lambda content=osc_content: self.client.send(content),
+                                           cnf=options_dict))
+        elif command is not None:
+            self.elements.append(tk.Button(self.tk_root, text=text, command=command, cnf=options_dict))
+        else:
+            raise Exception()
+        self.elements[-1].place(x=x, y=y)
