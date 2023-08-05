da7be93dd15593aff9df4a6d8006094da0be4cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:36 +0000

    Add package GaiaLab/0.1.0/GaiaLab-0.1.0.tar.gz

---
+++ packages/GaiaLab/GaiaLab-0.1.0.tar.gz/GaiaLab-0.1.0/GaiaLab/hits/filters/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from . import kalman
+from . import lowpass
+"""
+Noise removal functions for cleaning the datasets used.
+"""
+__author__ = "Toby James and Alex Bombrun"
+__version__ = "0.1"
---
+++ packages/GaiaLab/GaiaLab-0.1.0.tar.gz/GaiaLab-0.1.0/GaiaLab/hits/response/__init__.py
@@ -0,0 +1,14 @@
+from . import anomaly
+from . import characteristics
+"""
+Funtions to analyze AOCS response to hits
+
+Packages:
+
+    anomaly:
+        Isolate anomalies. Generate splines for the hits.
+
+    characteristics:
+        Count the turning points in isolated anomalies.
+        Measure the response time of the AOCS to hits.
+"""
