d53f99d8c79897e29ab73981fa85492189f72627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:42 +0000

    Add package msoffcrypto-tool/4.6.2/msoffcrypto-tool-4.6.2.tar.gz

---
+++ packages/msoffcrypto-tool/msoffcrypto-tool-4.6.2.tar.gz/msoffcrypto-tool-4.6.2/msoffcrypto/__init__.py
@@ -0,0 +1,35 @@
+import olefile
+
+
+def OfficeFile(file):
+    '''Return an office file object based on the format of given file.
+
+    Args:
+        file (:obj:`_io.BufferedReader`): Input file.
+
+    Returns:
+        BaseOfficeFile object.
+
+    Examples:
+        >>> f = open("tests/inputs/example_password.docx", "rb")
+        >>> officefile = OfficeFile(f)
+        >>> officefile.keyTypes
+        ('password', 'private_key', 'secret_key')
+    '''
+    ole = olefile.OleFileIO(file)
+
+    # MS-DOC: The WordDocument stream MUST be present in the file.
+    # https://msdn.microsoft.com/en-us/library/dd926131(v=office.12).aspx
+    if ole.exists('wordDocument'):
+        from .format.doc97 import Doc97File
+        return Doc97File(file)
+    # MS-XLS: A file MUST contain exactly one Workbook Stream, ...
+    # https://msdn.microsoft.com/en-us/library/dd911009(v=office.12).aspx
+    elif ole.exists('Workbook'):
+        from .format.xls97 import Xls97File
+        return Xls97File(file)
+    elif ole.exists('EncryptionInfo'):
+        from .format.ooxml import OOXMLFile
+        return OOXMLFile(file)
+    else:
+        raise AssertionError("Unrecognized file format")
---
+++ packages/msoffcrypto-tool/msoffcrypto-tool-4.6.2.tar.gz/msoffcrypto-tool-4.6.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='msoffcrypto-tool',
+    version='4.6.2',
+    description='A Python tool and library for decrypting MS Office files with passwords or other keys',
+    long_description=open("README.md", "r").read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nolze/msoffcrypto-tool',
+    author='nolze',
+    author_email='nolze@archlinux.us',
+    license='MIT',
+    keywords='',
+    packages=[
+        "msoffcrypto",
+        "msoffcrypto.format",
+        "msoffcrypto.method",
+    ],
+    install_requires=[
+        'olefile >= 0.45',
+        'cryptography >= 2.3',
+    ],
+    test_require=[
+        'nose >= 1.3.7',
+        'coverage >= 4.5.1', 
+    ],
+    classifiers=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'msoffcrypto-tool = msoffcrypto.__main__:main',
+        ],
+    },
+)
