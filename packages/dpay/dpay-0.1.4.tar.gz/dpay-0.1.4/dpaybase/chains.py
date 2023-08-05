5d6312314cd6a3857f7767592ddfb4bf8f5139ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package dpay/0.1.4/dpay-0.1.4.tar.gz

---
+++ packages/dpay/dpay-0.1.4.tar.gz/dpay-0.1.4/dpaybase/chains.py
@@ -0,0 +1,23 @@
+default_prefix = "DWB"
+
+known_chains = {
+    "DPAY": {
+        "chain_id": "38f14b346eb697ba04ae0f5adcfaa0a437ed3711197704aa256a14cb9b4a8f26",
+        "prefix": "DWB",
+        "dpay_symbol": "BEX",
+        "bbd_symbol": "BBD",
+        "vests_symbol": "VESTS",
+    },
+    "TEST": {
+        "chain_id":
+            "9afbce9f2416520733bacb370315d32b6b2c43d6097576df1c1222859d91eecc",
+        "prefix":
+            "TST",
+        "dpay_symbol":
+            "TESTS",
+        "bbd_symbol":
+            "TBD",
+        "vests_symbol":
+            "VESTS",
+    },
+}
