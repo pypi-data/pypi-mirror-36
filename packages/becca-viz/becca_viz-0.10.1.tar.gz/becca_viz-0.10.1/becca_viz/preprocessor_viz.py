fe1a036eaadd3ad1a63f2ae2f6ee643aa2158706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:33 +0000

    Add package becca-viz/0.10.1/becca_viz-0.10.1.tar.gz

---
+++ packages/becca-viz/becca_viz-0.10.1.tar.gz/becca_viz-0.10.1/becca_viz/preprocessor_viz.py
@@ -0,0 +1,189 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import becca_viz.viz_tools as vt
+
+
+def render(preprocessor, bbox, radius=0):
+    """
+    Make a picture of the discretization that happens in the Preprocessor.
+
+    Parameters
+    ----------
+    bbox: list of floats
+        Of the form [x_bottom, x_top, y_left, y_right]
+    preprocessor: Preprocessor
+
+    Returns
+    -------
+    input_x_viz: array of floats
+        The absolute x positions of the input nodes.
+    input_viz_map: 2D array of ints
+        A map from the order the inputs occur to the visualization order.
+    """
+    xmin, xmax, ymin, ymax = bbox
+    frame_width = xmax - xmin
+    frame_height = ymax - ymin
+
+    n_inputs = preprocessor.n_inputs
+    n_disc = len(preprocessor.discretizers)
+    # The number of inputs plus the number of discretizers.
+    n_input_d = n_inputs + n_disc
+    positions = np.zeros(n_input_d)
+
+    x_spacing = (frame_width - 2 * radius) / float(len(positions) + 1)
+    node_x = xmin + radius + x_spacing * np.cumsum(np.ones(len(positions)))
+
+    # Get the x positions of all the nodes.
+    # This needs to be in the same order as activities.
+    i_disc = 0
+    for i_disc, discretizer in enumerate(preprocessor.discretizers):
+        positions[i_disc] = discretizer.position
+        num_tree_list = discretizer.numeric_cats.get_list()
+        str_tree_list = discretizer.string_cats.get_list()
+        for node in num_tree_list:
+            positions[node.i_input + n_disc] = node.position
+        for node in str_tree_list:
+            positions[node.i_input + n_disc] = node.position
+
+    position_order = np.argsort(positions)
+    i_input_d_by_position = np.arange(n_input_d)[position_order]
+    x_by_i_input_d = node_x[np.argsort(i_input_d_by_position)]
+    discretizer_x = x_by_i_input_d[:n_disc]
+    input_x = x_by_i_input_d[n_disc:]
+    input_viz_map = np.zeros((n_inputs, n_inputs), dtype=np.int)
+    input_viz_map[np.argsort(input_x), np.arange(n_inputs)] = 1
+    input_x_viz = np.matmul(input_x, input_viz_map)
+
+    # Create labels
+    xlabel = "sensors"
+    height = ymax - ymin
+    text_sep = height * vt.text_shift
+    plt.text(
+        xmin + radius,
+        ymin - text_sep,
+        xlabel,
+        fontsize=4,
+        color=vt.copper,
+        verticalalignment="top",
+        horizontalalignment="right",
+        family="sans-serif",
+    )
+
+    xlabel = "inputs"
+    text_sep = height * vt.text_shift
+    plt.text(
+        xmin + radius,
+        ymin + height + text_sep,
+        xlabel,
+        fontsize=4,
+        color=vt.copper,
+        verticalalignment="bottom",
+        horizontalalignment="right",
+        family="sans-serif",
+    )
+
+    # Find the spacing between labeled sensors
+    n_target = 4
+    d_label = int(2 ** np.maximum(0, int(np.log2(n_disc / n_target))))
+
+    for i_discretizer, discretizer in enumerate(preprocessor.discretizers):
+        # Build trees.
+        n_depth = 1. + np.maximum(
+            discretizer.numeric_cats.depth,
+            discretizer.string_cats.depth)
+        branch_length = frame_height / n_depth
+        sensor_x = discretizer_x[i_discretizer]
+
+        # Apply sensor labels
+        if i_discretizer % d_label == 0:
+            plt.text(
+                sensor_x,
+                ymin - text_sep,
+                str(i_discretizer),
+                fontsize=4,
+                color=vt.copper,
+                verticalalignment="top",
+                horizontalalignment="center",
+                family="sans-serif",
+            )
+
+        def plot_tree(tree):
+            root_x = input_x[tree.root.i_input]
+            activity = preprocessor.input_activities[tree.root.i_input]
+            plot_branch(sensor_x, root_x, ymin, activity, branch_length)
+            nodes = tree.get_list()
+            for node in nodes:
+                if node.lo_child is not None:
+                    y_start = ymin + (node.depth + 1.) * branch_length
+                    x_start = input_x[node.i_input]
+                    x_end = input_x[node.lo_child.i_input]
+                    lo_activity = preprocessor.input_activities[
+                        node.lo_child.i_input]
+                    plot_branch(
+                        x_start,
+                        x_end,
+                        y_start,
+                        lo_activity,
+                        branch_length,
+                        is_leaf=node.lo_child.leaf,
+                        max_y=ymax,
+                    )
+                    vt.plot_point_activity(
+                        x_end,
+                        y_start + branch_length,
+                        lo_activity,
+                        x_spacing,
+                    )
+                    x_end = input_x[node.hi_child.i_input]
+                    hi_activity = preprocessor.input_activities[
+                        node.hi_child.i_input]
+                    plot_branch(
+                        x_start,
+                        x_end,
+                        y_start,
+                        hi_activity,
+                        branch_length,
+                        is_leaf=node.hi_child.leaf,
+                        max_y=ymax,
+                    )
+                    vt.plot_point_activity(
+                        x_end,
+                        y_start + branch_length,
+                        hi_activity,
+                        x_spacing,
+                    )
+
+        # Build string branch.
+        plot_tree(discretizer.string_cats)
+        plot_tree(discretizer.numeric_cats)
+
+    return input_x_viz, input_viz_map
+
+
+def plot_branch(
+    x_start, x_end,
+    y_start,
+    activity,
+    branch_length,
+    is_leaf=False,
+    max_y=0.,
+):
+    """
+    Draw the branches of the discretized sensor trees.
+
+    @param y_start, x_start, x_end: floats
+        The start and end coordinates of the branch.
+    @param activity: float
+        The activity level of the branch, between 0 and 1.
+    @param branch_length: float
+        The y distance between generations in the tree.
+    @param is_leaf: boolean
+        Is this branch for a leaf node?
+    @param max_y: float
+        In the case of a leaf node, what is the x extent of the window.
+    """
+    y_end = y_start + branch_length
+    vt.plot_curve_activity(x_start, x_end,  y_start, y_end, activity)
+    if is_leaf:
+        vt.plot_line_activity([x_end, x_end], [y_end, max_y], activity)
---
+++ packages/becca-viz/becca_viz-0.10.1.tar.gz/becca_viz-0.10.1/becca_viz/viz.py
@@ -0,0 +1,238 @@
+"""
+Show the world and what's going on inside the brain.
+"""
+import os
+
+import numpy as np
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+
+import becca_viz.viz_tools as vt
+import becca_viz.preprocessor_viz as preprocessor_viz
+import becca_viz.postprocessor_viz as postprocessor_viz
+import becca_viz.input_filter_viz as input_filter_viz
+import becca_viz.ziptie_viz as ziptie_viz
+import becca_viz.featurizer_viz as featurizer_viz
+import becca_viz.feature_filter_viz as feature_filter_viz
+import becca_viz.model_viz as model_viz
+import becca_viz.actor_viz as actor_viz
+
+
+lwf = .3  # Linewidth of the frames
+
+wd_ = 18  # Total width of the image
+ht_ = 9  # Total heigh of the image
+brd = .5  # Border thickness around frames
+
+halfwidth_weight = 5
+centerwidth_weight = 1
+total_weight = 2 * halfwidth_weight + centerwidth_weight
+
+no_borders = wd_ - brd * 3  # Width available after borders are removed
+wdh = no_borders * halfwidth_weight / total_weight  # Width of wide columns
+wdc = no_borders * centerwidth_weight / total_weight  # Center column width
+
+htf = ht_ - brd * 2  # Full height, minus borders
+n_zipties = 2  # Assumed number of zipties
+n_rows = 1 + 2 * n_zipties  # Total number of frames stacked vertically
+no_borders_ht = ht_ - brd * (n_zipties + 2)  # Total non-border height
+htr = no_borders_ht / n_rows  # height of each row
+wdt = (wdh - brd) / 1.5  # Third-width
+wdq = (wdh - brd) / 2  # Quarter-width
+wds = (wdh - brd) / 3  # Sixth-width
+
+rad = htr / 6  # The radius of the rounded corners on frames
+
+# bboxes are in the format [x_min, x_max, y_min, y_max]
+preprocessor_bbox = [brd, brd + 2 * wds, brd, brd + htr]
+postprocessor_bbox = [2 * brd + 2 * wds, brd + wdh, brd, brd + htr]
+filt_0_bbox = [brd, brd + wdh, 2 * brd + htr, 2 * brd + 2 * htr]
+ziptie_0_bbox = [brd, brd + wdh, 2 * brd + 2 * htr, 2 * brd + 3 * htr]
+filt_1_bbox = [brd, brd + wdh, 3 * brd + 3 * htr, 3 * brd + 4 * htr]
+ziptie_1_bbox = [brd, brd + wdh, 3 * brd + 4 * htr, 3 * brd + 5 * htr]
+feature_filt_bbox = [2 * brd + wdh, 2 * brd + wdh + wdc, brd, brd + htf]
+model_bbox = [
+    2 * brd + wdh + wdc, 2 * brd + wdh + wdc + wdt, brd, brd + htf]
+actor_bbox = [
+    2 * brd + wdh + wdc + wdt, 2 * brd + 2 * wdh + wdc, brd, brd + htf]
+
+
+def visualize(brain):
+    """
+    Render the sensor information making its way up through the brain.
+
+    Parameters
+    ----------
+    brain: Brain
+    """
+    create_background()
+    x_inputs, preprocessor_viz_map = preprocessor_viz.render(
+        brain.preprocessor, preprocessor_bbox, radius=rad)
+    x_commands, postprocessor_viz_map = postprocessor_viz.render(
+        brain.postprocessor, postprocessor_bbox, radius=rad)
+    n_pre = preprocessor_viz_map.shape[0]
+    n_post = postprocessor_viz_map.shape[0]
+
+    pool_0_viz_map = np.block([
+        [np.zeros((n_post, n_pre), dtype=np.int), postprocessor_viz_map],
+        [preprocessor_viz_map, np.zeros((n_pre, n_post), dtype=np.int)]])
+    # TODO: handle multiple zipties
+    x_pool_0 = np.concatenate((x_inputs, x_commands))
+    x_cables_0, cables_0_viz_map = input_filter_viz.render(
+        brain.featurizer.filter,
+        filt_0_bbox,
+        x_pool_0,
+        pool_0_viz_map,
+        preprocessor_bbox[3],  # max y value of the Preprocessor
+        radius=rad)
+    x_pool_1, pool_1_viz_map = ziptie_viz.render(
+        brain.featurizer.ziptie,
+        ziptie_0_bbox,
+        x_cables_0,
+        cables_0_viz_map,
+        filt_0_bbox[3],  # max y value of the InputFilter
+        radius=rad)
+    pool_viz_map = [pool_0_viz_map, pool_1_viz_map]
+    y_pool_feature, feature_pool_viz_map = featurizer_viz.render(
+        brain.featurizer,
+        feature_filt_bbox,
+        pool_viz_map,
+        radius=rad,
+    )
+    y_feature, feature_viz_map = feature_filter_viz.render(
+        brain.model.filter,
+        feature_filt_bbox,
+        y_pool_feature,
+        feature_pool_viz_map,
+        radius=rad,
+        n_inputs=n_pre,
+        n_commands=n_post,
+        n_bundles_all=[brain.featurizer.ziptie.n_bundles],
+    )
+    model_viz.render(
+        brain.model,
+        model_bbox,
+        feature_viz_map,
+        radius=rad)
+    actor_viz.render(
+        brain.model,
+        brain.actor,
+        actor_bbox,
+        feature_viz_map,
+        radius=rad)
+
+    finalize(brain, dpi=300)
+
+
+def create_background(edgecolor=vt.oxide, facecolor=vt.dark_grey):
+    """
+    Set up the backdrop for the visualization.
+    """
+    # plt.style.use('dark_background')
+    fig = plt.figure(num=84782, figsize=(wd_, ht_))
+    fig.clf()
+    # plt.style.use('dark_background')
+    ax = plt.gca()
+    ax.add_patch(patches.Rectangle(
+        (0, 0),
+        wd_,
+        ht_,
+        facecolor=facecolor,
+        edgecolor='none',
+        zorder=-16.,
+    ))
+
+    # Preprocessor frame
+    vt.draw_frame(
+        bbox=preprocessor_bbox,
+        radius=rad,
+        facecolor=facecolor,
+        edgecolor=edgecolor,
+    )
+    # Postprocessor frame
+    vt.draw_frame(
+        bbox=postprocessor_bbox,
+        radius=rad,
+        facecolor=facecolor,
+        edgecolor=edgecolor,
+    )
+    # Cable filter 0 frame
+    vt.draw_frame(
+        bbox=filt_0_bbox,
+        radius=rad,
+        facecolor=facecolor,
+        edgecolor=edgecolor,
+    )
+    # Ziptie 0 frame
+    vt.draw_frame(
+        bbox=ziptie_0_bbox,
+        radius=rad,
+        facecolor=facecolor,
+        edgecolor=edgecolor,
+    )
+    # Cable filter 1 frame
+    vt.draw_frame(
+        bbox=filt_1_bbox,
+        radius=rad,
+        facecolor=facecolor,
+        edgecolor=edgecolor,
+    )
+    # Ziptie 1 frame
+    vt.draw_frame(
+        bbox=ziptie_1_bbox,
+        radius=rad,
+        facecolor=facecolor,
+        edgecolor=edgecolor,
+    )
+    # feature filter frame
+    vt.draw_frame(
+        bbox=feature_filt_bbox,
+        radius=rad,
+        facecolor=facecolor,
+        edgecolor=edgecolor,
+    )
+    # model frame
+    vt.draw_frame(
+        bbox=model_bbox,
+        radius=rad,
+        facecolor=facecolor,
+        edgecolor=edgecolor,
+    )
+    # actor frame
+    vt.draw_frame(
+        bbox=actor_bbox,
+        radius=rad,
+        facecolor=facecolor,
+        edgecolor=edgecolor,
+    )
+
+    return
+
+
+def finalize(brain, dpi=300, tag='activity', verbose=True):
+    """
+    Complete any final formatting and save a copy of the figure.
+
+    Parameters
+    ----------
+    brain: Brain
+    dpi: int
+        The dots per inch for the saved figure.
+    phase: int
+        During each time step the brain does a lot and it's tough to get
+        it all into one image. To handle this, it is broken up into phases.
+        Phase 1 is sensing (an upward pass through the architecture) and
+        Phase 2 is acting ( a downward pass through the architecture).
+
+    """
+    plt.axis('off')
+    plt.axis([0, wd_, 0, ht_])
+
+    filename = 'becca_{name}_{dpi:04d}_{age:08d}_{tag}.png'.format(
+        name=brain.name, age=brain.timestep, dpi=dpi, tag=tag)
+    pathname = os.path.join(brain.log_dir, filename)
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    plt.savefig(pathname, dpi=dpi, bbox_inches="tight", pad_inches=0)
+    if verbose:
+        print('Saved', pathname)
