6a128883a5df2b2c26777eb95d060228a547ddbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package manoc/1.2/manoc-1.2.tar.gz

---
+++ packages/manoc/manoc-1.2.tar.gz/manoc-1.2/manoc/commands/cmd_package-get.py
@@ -0,0 +1,38 @@
+import json
+import sys
+
+import click
+import jmespath
+import requests
+
+from manoc.cli import pass_context
+from manoc.utils.logging import log_request
+
+
+@click.command('package-get')
+@click.option('--query', help='JMESPath expression to filter result.')
+@click.argument('id')
+@pass_context
+def cli(ctx, id, query):
+    """Command on get package"""
+    url = "http://api-gateway-mano." + ctx.base_url + "/vnf-manager/" + ctx.tenant + "/vnfpkgm/v1/vnf_packages/" + id
+
+    headers = {
+        'Accept': "application/json",
+        'Authorization': "Bearer " + ctx.access_token
+    }
+
+    response = requests.request("GET", url, headers=headers)
+    log_request(ctx, response)
+
+    if response.status_code != 200:
+        click.echo("No resources found")
+        sys.exit(1)
+
+    if query is None:
+        click.echo(json.dumps(response.json(), indent=4))
+    else:
+        expression = jmespath.compile(query)
+        click.echo(expression.search(response.json()))
+
+
---
+++ packages/manoc/manoc-1.2.tar.gz/manoc-1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='manoc',
+    version='1.2',
+    packages=['manoc', 'manoc.commands', 'manoc.utils'],
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'configparser',
+        'requests',
+        'prettytable',
+        'jmespath'
+    ],
+    entry_points='''
+        [console_scripts]
+        manoc=manoc.cli:cli
+    ''',
+)
