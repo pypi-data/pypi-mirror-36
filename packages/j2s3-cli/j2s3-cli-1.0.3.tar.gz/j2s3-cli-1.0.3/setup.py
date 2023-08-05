3fa9ca815b62d6d9d500f3aef33bfa3ffc98f38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:47 +0000

    Add package j2s3-cli/1.0.3/j2s3-cli-1.0.3.tar.gz

---
+++ packages/j2s3-cli/j2s3-cli-1.0.3.tar.gz/j2s3-cli-1.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='j2s3-cli',
+    version='1.0.3',
+    url='https://github.com/jackmahoney/j2s3-cli',
+    license='Apache 2.0',
+    install_requires=[
+        'j2s3==1.0.3',
+        'click==6.7'
+    ],
+    py_modules=['app'],
+    entry_points='''
+      [console_scripts]
+      j2s3=app:cli
+    ''',
+    description='A cli for publishing Java maven projects to an S3 maven repository',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='jackmahoney',
+    author_email='jackmahoney212@gmail.com',
+)
+
