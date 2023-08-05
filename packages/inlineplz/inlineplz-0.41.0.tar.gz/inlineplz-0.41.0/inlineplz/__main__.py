ef93d8fb85c3ea8daada2e90605e995a13c8c40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:05 +0000

    Add package inlineplz/0.41.0/inlineplz-0.41.0.tar.gz

---
+++ packages/inlineplz/inlineplz-0.41.0.tar.gz/inlineplz-0.41.0/inlineplz/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from inlineplz.main import main
+
+
+if __name__ == "__main__":
+    exit(main())
