26737044fa1323f223a1b608c68b57ccb63b970f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:01 +0000

    Add package huobitrade/0.4.5/huobitrade-0.4.5.tar.gz

---
+++ packages/huobitrade/huobitrade-0.4.5.tar.gz/huobitrade-0.4.5/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/29 0029 14:10
+# @Author  : Hadrianl 
+# @File    : setup.py
+# @Contact   : 137150224@qq.com
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as rm:
+    long_description = rm.read()
+
+requires = ['websocket-client>=0.53',
+            'requests',
+            'pymongo',
+            'pyzmq',
+            'pandas',
+            'requests-futures',
+            'ecdsa']
+
+hb_packages = ['huobitrade', 'huobitrade/extra']
+
+setup(name='huobitrade',
+      version='0.4.5',
+      description='huobi_api for python',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Hadrianl',
+      autor_email='137150224@qq.com',
+      url='https://github.com/hadrianl/huobi',
+      packages=hb_packages,
+      classifiers=("Programming Language :: Python :: 3.6",
+                   "License :: OSI Approved :: MIT License"),
+      install_requires=requires)
