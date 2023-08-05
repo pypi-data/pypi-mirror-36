6f2581e245b8d856eac422f3ab36cf51ad912285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:55 +0000

    Add package hogwatch/0.1.9/hogwatch-0.1.9.tar.gz

---
+++ packages/hogwatch/hogwatch-0.1.9.tar.gz/hogwatch-0.1.9/setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    # Application name:
+    name="hogwatch",
+
+    # Version number (initial):
+    version="0.1.9",
+
+    # Application author details:
+    author="Akshay Kumar",
+    author_email="akshay.kmr4321@gmail.com",
+
+    # Packages
+    packages=find_packages(),
+    scripts=["bin/hogwatch"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="https://github.com/akshayKMR/hogwatch",
+
+    #
+    license="MIT",
+    description="find out whats hogging your internet",
+
+    long_description="""A bandwidth monitor that shows
+    per process network transfer""",
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "bottle",
+        'pywebview',
+        'gevent-websocket',
+        'netifaces'
+    ],
+)
