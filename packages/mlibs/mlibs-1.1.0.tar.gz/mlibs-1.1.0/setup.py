5d5ce5a2e27acf4d7f9cd139002c81f8098e5003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:05 +0000

    Add package mlibs/1.1.0/mlibs-1.1.0.tar.gz

---
+++ packages/mlibs/mlibs-1.1.0.tar.gz/mlibs-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name		=	'mlibs', 
+		version		=	'1.1.0',
+		py_modules	=	['kl_print_list'],
+                author          =       'szu_lee',
+                author_email    =       '664304797@qq.com',
+                description     =       'Make the world a better place',
+
+	 )
