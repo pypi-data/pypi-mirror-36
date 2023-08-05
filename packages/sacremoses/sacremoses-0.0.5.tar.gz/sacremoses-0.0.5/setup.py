e58eacf1d18e1cd74b206d20ea5c65cae9de34b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:43 +0000

    Add package sacremoses/0.0.5/sacremoses-0.0.5.tar.gz

---
+++ packages/sacremoses/sacremoses-0.0.5.tar.gz/sacremoses-0.0.5/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name = 'sacremoses',
+  packages = ['sacremoses'],
+  version = '0.0.5',
+  description = 'SacreMoses',
+  long_description = 'LGPL MosesTokenizer in Python',
+  author = '',
+  license = '',
+  package_data={'sacremoses': ['data/perluniprops/*.txt', 'data/nonbreaking_prefixes/nonbreaking_prefix.*']},
+  url = 'https://github.com/alvations/sacremoses',
+  keywords = [],
+  classifiers = [],
+  install_requires = ['six']
+)
