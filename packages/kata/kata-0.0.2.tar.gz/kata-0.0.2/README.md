a8b2076c32c122b471785eec1dd00d59c7b42200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:31 +0000

    Add package kata/0.0.2/kata-0.0.2.tar.gz

---
+++ packages/kata/kata-0.0.2.tar.gz/kata-0.0.2/README.md
@@ -0,0 +1,12 @@
+# Kata
+
+Kata: A single command to get a ready to use TDD setup for your Kata, in your language of choice
+
+
+
+
+
+---
+## Author Information
+Follow me on Twitter: [@ThisIsFlorianK](https://twitter.com/ThisIsFlorianK)  
+Find out more about my work: [Florian Kempenich — Personal Website](https://floriankempenich.com)
---
+++ packages/kata/kata-0.0.2.tar.gz/kata-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='kata',
+    version='0.0.2',
+    description='Kata made easy: A TDD setup in the language of your choice in a single command',
+    long_description=readme(),
+    keywords='test tdd kata clean-code home-automation softwarecrafter',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Testing'
+    ],
+    url='https://floriankempenich.github.io/kata',
+    author='Florian Kempenich',
+    author_email='Flori@nKempenich.com',
+    packages=['kata'],
+    license='MIT',
+    scripts=['bin/kata'],
+    install_requires=[
+        'click'
+    ],
+    include_package_data=True
+)
