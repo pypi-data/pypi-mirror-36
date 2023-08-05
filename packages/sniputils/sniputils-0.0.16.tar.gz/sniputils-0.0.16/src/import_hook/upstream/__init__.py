67077105b2942a421b798949e6cdbd5c80e9b438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:21 +0000

    Add package sniputils/0.0.16/sniputils-0.0.16.tar.gz

---
+++ packages/sniputils/sniputils-0.0.16.tar.gz/sniputils-0.0.16/src/import_hook/upstream/__init__.py
@@ -0,0 +1,47 @@
+"""
+inject monkey patch
+
+    append upstream folder path in env PATH while folder is module
+        to resolve relative import as absolute grammar
+        [support multiple to exec patch more times]
+    Usage:
+    as files like this:
+    /top
+        __init__.py
+        /foo
+            __init__.py
+            foo.py
+        /bar
+            __init__.py
+            bar.py
+
+    in bar.py code as:
+        # bar.py
+        import sniputils.upstream
+        import foo.foo.xxx      # <- this is relative import but use absolute grammar
+
+    it will append ['/top'] to sys.path
+    NOTE: re-import this module will also reload and append to sys.path,
+    it's means
+        # in /test/foo/foo.py
+        import sniputils.upstream
+        # in /retest/bar/bar.py
+        import sniputils.upstream
+        # in /top.py
+        import test.foo.foo
+        import retest.bar.bar <- will append ['/test', '/retest'] to sys.path
+"""
+
+from inspect import getsourcefile
+
+from .track_inject import path_inject
+from ..import_track import which_import_me
+from ..reimportable import set_reimport
+
+set_reimport(__name__)
+
+back_import = which_import_me()
+
+if back_import:
+    back_module = getsourcefile(back_import)
+    path_inject(back_module)
