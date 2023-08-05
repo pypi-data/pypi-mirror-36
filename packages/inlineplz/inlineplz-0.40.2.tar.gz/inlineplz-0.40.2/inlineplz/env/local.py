4594981fb69304412d239bb264a9b5f3d1b1847f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:57 +0000

    Add package inlineplz/0.40.2/inlineplz-0.40.2.tar.gz

---
+++ packages/inlineplz/inlineplz-0.40.2.tar.gz/inlineplz-0.40.2/inlineplz/env/local.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import os
+
+from inlineplz.env.base import EnvBase
+import inlineplz.util.git as git
+
+
+class Local(EnvBase):
+    def __init__(self):
+        if os.path.exists(".git"):
+            self.interface = "github"
+            self.commit = git.current_sha()
+            self.branch = git.current_branch()
+            self.url = git.url()
