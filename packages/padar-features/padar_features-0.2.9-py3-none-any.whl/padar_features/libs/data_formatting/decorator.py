d6513a0a434d15d7958d0a18d09976673dedc64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:34 +0000

    Add package padar-features/0.2.9/padar_features-0.2.9-py3-none-any.whl

---
+++ packages/padar-features/padar_features-0.2.9-py3-none-any.whl/padar_features/libs/data_formatting/decorator.py
@@ -0,0 +1,12 @@
+"""
+
+Decorators to extend feature computation function (based on 2d numpy array) to
+ support different types of input, such as pandas dataframe, csv file, 1d numpy
+ array, and to extend feature computation as a standalone script to compute
+windowed values directly
+
+Author: Qu Tang
+
+Date: 07/10/2018
+
+"""
