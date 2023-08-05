b69f8686b591795ac5407ec3bf7d5a3439afa83b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:39 +0000

    Add package idataapi-transform/1.4.4/idataapi-transform-1.4.4.tar.gz

---
+++ packages/idataapi-transform/idataapi-transform-1.4.4.tar.gz/idataapi-transform-1.4.4/idataapi_transform/DataProcess/Config/ConnectorConfig.py
@@ -0,0 +1,45 @@
+import aiohttp
+import asyncio
+import inspect
+from .MainConfig import main_config
+
+main_config = main_config()
+default_concurrency_limit = main_config["main"].getint("concurrency")
+
+
+class _SessionManger(object):
+    def __init__(self, concurrency_limit=default_concurrency_limit, loop=None):
+        self.session = self._generate_session(concurrency_limit=concurrency_limit, loop=loop)
+
+    @staticmethod
+    def _generate_connector(limit=default_concurrency_limit, loop=None):
+        """
+        https://github.com/KeepSafe/aiohttp/issues/883
+        if connector is passed to session, it is not available anymore
+        """
+        if not loop:
+            loop = asyncio.get_event_loop()
+        return aiohttp.TCPConnector(limit=limit, loop=loop)
+
+    @staticmethod
+    def _generate_session(concurrency_limit=default_concurrency_limit, loop=None):
+        if not loop:
+            loop = asyncio.get_event_loop()
+        return aiohttp.ClientSession(connector=_SessionManger._generate_connector(limit=concurrency_limit, loop=loop),
+                                     loop=loop)
+
+    def get_session(self):
+        return self.session
+
+    def __del__(self):
+        try:
+            if inspect.iscoroutinefunction(self.session.close):
+                loop = asyncio.get_event_loop()
+                loop.run_until_complete(self.session.close())
+            else:
+                self.session.close()
+        except Exception as e:
+            pass
+
+
+session_manger = _SessionManger()
---
+++ packages/idataapi-transform/idataapi-transform-1.4.4.tar.gz/idataapi-transform-1.4.4/idataapi_transform/DataProcess/Config/DefaultValue.py
@@ -0,0 +1,42 @@
+import os
+import hashlib
+from .MainConfig import main_config
+
+base_config = main_config()
+
+
+class DefaultVal(object):
+    per_limit = base_config["main"].getint("per_limit")
+    max_limit = base_config["main"].get("max_limit")
+    if max_limit != "None":
+        max_limit = int(max_limit)
+    else:
+        max_limit = None
+
+    max_retry = base_config["main"].getint("max_retry")
+    random_min_sleep = base_config["main"].getint("random_min_sleep")
+    random_max_sleep = base_config["main"].getint("random_max_sleep")
+
+    default_file_mode_r = "r"
+    default_file_mode_w = "w"
+    default_encoding = "utf8"
+    new_line = "\n"
+    join_val = " "
+    title = "example"
+    qsn = None
+
+    query_body = None
+    dest_without_path = "result"
+    dest = os.getcwd() + "/" + dest_without_path
+    interval = 5
+    concurrency = 50
+    default_key_type = "LIST"
+    report_interval = 10
+
+    @staticmethod
+    def default_id_hash_func(item):
+        if "appCode" in item and item["appCode"] and "id" in item and item["id"]:
+            value = (item["appCode"] + "_" + item["id"]).encode("utf8")
+        else:
+            value = str(item).encode("utf8")
+        return hashlib.md5(value).hexdigest()
