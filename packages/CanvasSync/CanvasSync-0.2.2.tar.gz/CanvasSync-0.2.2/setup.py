f0b0d0755dfaa1b8b1def48e14738615fd741c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:20 +0000

    Add package CanvasSync/0.2.2/CanvasSync-0.2.2.tar.gz

---
+++ packages/CanvasSync/CanvasSync-0.2.2.tar.gz/CanvasSync-0.2.2/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import re
+
+
+VERSION_FILE = "./CanvasSync/_version.py"
+
+
+def get_version():
+    """ Load the CanvasSync package version string """
+
+    version_string = open(VERSION_FILE, "r").read()
+    regex = r"__version__[ ]?=[ ]?['\"]([^'\"]*)['\"]"
+
+    match = re.search(regex, version_string)
+
+    if match:
+        return match.group(1)
+    else:
+        raise RuntimeError("Unable to load version string in %s" % VERSION_FILE)
+
+
+setup(name='CanvasSync',
+      version=get_version(),
+      description='Synchronizes modules, assignments and files from a Canvas server to a local folder',
+      long_description=open("README.txt").read(),
+      author='Mathias Perslev',
+      author_email='mathias@perslev.com',
+      url='https://github.com/perslev/CanvasSync',
+      license="LICENSE.txt",
+      packages=["CanvasSync", "CanvasSync/CanvasEntities", "CanvasSync/Settings", "CanvasSync/Statics"],
+      scripts=['bin/canvas.py'],
+      install_requires=["requests", "pycryptodome", "py-bcrypt", "six"],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Environment :: Console',
+                   'Operating System :: MacOS :: MacOS X',
+                   'Operating System :: POSIX',
+                   'Programming Language :: Python',
+                   'License :: OSI Approved :: MIT License']
+     )
