f7c4fa0da6d812a65e7183801786c21a20bb9c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package face-pose/0.0.3/face_pose-0.0.3.tar.gz

---
+++ packages/face-pose/face_pose-0.0.3.tar.gz/face_pose-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name="face_pose",
+    version="0.0.3",
+    description="get the facepose",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    license = "MIT",
+    author="zhaomingming",
+    author_email="13271929138@163.com",
+    url="http://www.zhaomingming.cn",
+    py_modules=['face_pose'],
+    platforms = 'any'
+)
