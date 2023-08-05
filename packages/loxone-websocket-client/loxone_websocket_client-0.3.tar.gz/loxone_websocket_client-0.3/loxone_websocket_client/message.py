a84af907fc5453da5814532ccc585c98995ced38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:57 +0000

    Add package loxone-websocket-client/0.3/loxone_websocket_client-0.3.tar.gz

---
+++ packages/loxone-websocket-client/loxone_websocket_client-0.3.tar.gz/loxone_websocket_client-0.3/loxone_websocket_client/message.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from re import search
+from json import loads
+
+
+class Message(object):
+
+    def __init__(self, payload):
+        self._raw_data = loads(payload.decode('utf8'))
+
+    @property
+    def data(self):
+        return self._raw_data.get('LL')
+
+    @property
+    def code(self):
+        if self.data.get('Code', None) is not None:
+            return int(self.data.get('Code', None))
+        if self.data.get('code', None) is not None:
+            return int(self.data.get('code', None))
+
+    @property
+    def control(self):
+        return self.data.get('control', None)
+
+    @control.setter
+    def control(self, value):
+        self._raw_data['LL']['control'] = value
+
+    @property
+    def value(self):
+        return self.data.get('value', None)
+
+    @property
+    def control_type(self):
+        if self.control == 'Auth':
+            return 'auth'
+        elif search(r'j?dev\/sys\/enc\/', self.control) is not None:
+            return 'enc'
+        elif search(r'j?dev\/sys\/getkey2\/', self.control) is not None:
+            return 'getkey2'
+        elif search(r'j?dev\/sys\/getkey', self.control) is not None:
+            return 'getkey'
+        elif search(r'j?dev\/sys\/gettoken\/', self.control) is not None:
+            return 'gettoken'
+        elif search(r'j?dev\/sys\/keyexchange\/', self.control) is not None:
+            return 'keyexchange'
+        elif search(r'j?dev\/sys\/refreshtoken\/', self.control) is not None:
+            return 'refreshtoken'
+        else:
+            return 'unknown'
