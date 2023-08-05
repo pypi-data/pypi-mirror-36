80252dc7b0ef59e767525f47a10a407501dd88fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package stpmex/0.0.2/stpmex-0.0.2.tar.gz

---
+++ packages/stpmex/stpmex-0.0.2.tar.gz/stpmex-0.0.2/stpmex/ordenes.py
@@ -0,0 +1,68 @@
+import random
+import time
+
+from .base import ACTUALIZA_CLIENT, Resource
+from .types import AccountType, Prioridad
+
+
+ORDEN_FIELDNAMES = """
+    institucionContraparte
+    empresa
+    fechaOperacion
+    folioOrigen
+    claveRastreo
+    institucionOperante
+    monto
+    tipoPago
+    tipoCuentaOrdenante
+    nombreOrdenante
+    cuentaOrdenante
+    rfcCurpOrdenante
+    tipoCuentaBeneficiario
+    nombreBeneficiario
+    cuentaBeneficiario
+    rfcCurpBeneficiario
+    emailBeneficiario
+    tipoCuentaBeneficiario2
+    nombreBeneficiario2
+    cuentaBeneficiario2
+    rfcCurpBeneficiario2
+    conceptoPago
+    conceptoPago2
+    claveCatUsuario1
+    claveCatUsuario2
+    clavePago
+    referenciaCobranza
+    referenciaNumerica
+    tipoOperacion
+    topologia
+    usuario
+    medioEntrega
+    prioridad
+    iva
+    """.split()
+
+ORDEN_DEFAULTS = dict(
+    rfcCurpBeneficiario='ND',
+    tipoPago=1,
+    tipoCuentaBeneficiario=AccountType.CLABE.value,
+    topologia='T',
+    medioEntrega=3,
+    prioridad=Prioridad.alta.value,
+    claveRastreo=lambda: f'CR{int(time.time())}',
+    referenciaNumerica=lambda: random.randint(10 ** 6, 10 ** 7)
+)
+
+
+class Orden(Resource):
+
+    __fieldnames__ = ORDEN_FIELDNAMES
+    __type__ = ACTUALIZA_CLIENT.get_type('ns0:ordenPagoWS')
+    _id = None
+    _defaults = ORDEN_DEFAULTS
+
+    def registra(self):
+        self.firma = self._compute_signature()
+        resp = ACTUALIZA_CLIENT.service.registraOrden(self.__object__)
+        self._id = resp.id
+        return resp
