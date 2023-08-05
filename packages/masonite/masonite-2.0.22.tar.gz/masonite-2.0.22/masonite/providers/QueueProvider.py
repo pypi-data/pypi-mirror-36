33fb30f8f81834455505f42021f0494a2aaef944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:57 +0000

    Add package masonite/2.0.22/masonite-2.0.22.tar.gz

---
+++ packages/masonite/masonite-2.0.22.tar.gz/masonite-2.0.22/masonite/providers/QueueProvider.py
@@ -0,0 +1,22 @@
+""" A RedirectionProvider Service Provider """
+
+from config import queue
+from masonite.drivers import QueueAsyncDriver
+from masonite.managers import QueueManager
+from masonite.provider import ServiceProvider
+
+
+class QueueProvider(ServiceProvider):
+
+    wsgi = False
+
+    def register(self):
+        self.app.bind('QueueAsyncDriver', QueueAsyncDriver)
+        self.app.bind('QueueManager', QueueManager)
+        self.app.bind('QueueConfig', queue)
+
+    def boot(self, QueueConfig, QueueManager):
+        self.app.bind(
+            'Queue',
+            QueueManager(self.app).driver(QueueConfig.DRIVER)
+        )
