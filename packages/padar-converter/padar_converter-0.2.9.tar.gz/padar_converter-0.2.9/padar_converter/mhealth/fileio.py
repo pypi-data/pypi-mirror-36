f868459468b6ff7f02628f437b018419b6add621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:20 +0000

    Add package padar-converter/0.2.9/padar_converter-0.2.9.tar.gz

---
+++ packages/padar-converter/padar_converter-0.2.9.tar.gz/padar_converter-0.2.9/padar_converter/mhealth/fileio.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+
+def load_sensor(filepath):
+    if filepath is None:
+        return None
+    result = pd.read_csv(filepath, parse_dates=[0], infer_datetime_format=True)
+    result = result.rename(columns = {result.columns[0]: 'HEADER_TIME_STAMP'})
+    result.set_index(result.columns[0], inplace=True)
+    return result
+
+
+def load_annotation(filepath):
+    if filepath is None:
+        return None
+    result = pd.read_csv(filepath, parse_dates=[0, 1, 2],
+                         infer_datetime_format=True)
+    result.set_index(result.columns[[0, 1, 2]], inplace=True)
+    return result
+
+def load_offset_mapping(filepath):
+    if filepath is None:
+        return None
+    result = pd.read_csv(filepath)
+    return result
+
+def load_orientation_corrections(filepath):
+    if filepath is None:
+        return None
+    result = pd.read_csv(filepath)
+    return result
