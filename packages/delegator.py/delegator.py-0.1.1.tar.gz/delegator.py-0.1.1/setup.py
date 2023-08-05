3c4c549ead1e34c5d282843323de0f3a944c1694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:24 +0000

    Add package delegator.py/0.1.1/delegator.py-0.1.1.tar.gz

---
+++ packages/delegator.py/delegator.py-0.1.1.tar.gz/delegator.py-0.1.1/setup.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import codecs
+
+from shutil import rmtree
+from setuptools import setup, Command
+
+__version__ = '0.1.1'
+
+try:
+    # Python 3
+    from os import dirname
+except ImportError:
+    # Python 2
+    from os.path import dirname
+
+here = os.path.abspath(dirname(__file__))
+
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
+    sys.exit()
+
+required = [
+    'pexpect>=4.1.0'
+]
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('\'{0}\' setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(__version__))
+        os.system('git push --tags')
+
+        sys.exit()
+
+setup(
+    name='delegator.py',
+    version=__version__,
+    description='Subprocesses for Humans 2.0.',
+    long_description=long_description,
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/delegator',
+    py_modules=['delegator'],
+    install_requires=required,
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    cmdclass={
+        'upload': UploadCommand,
+    }
+    )
