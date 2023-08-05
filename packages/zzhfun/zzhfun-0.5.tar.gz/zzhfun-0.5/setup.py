4a2137d6d4da0b0f6c877a887e01870cc1487113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:09 +0000

    Add package zzhfun/0.5/zzhfun-0.5.tar.gz

---
+++ packages/zzhfun/zzhfun-0.5.tar.gz/zzhfun-0.5/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(name='zzhfun',
+      version='0.5',
+      description='zzh model function and data function',
+      url='https://github.com/FlashSnail/zzhfun',
+      author='Zzh',
+      author_email='zzh_0729@foxmail.com',
+      packages=setuptools.find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ])
