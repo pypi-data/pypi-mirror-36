488ef32c16f1ed63cd60b5e05e217cbdb39b9b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:05 +0000

    Add package petal-plaid-python/1.0.0/petal-plaid-python-1.0.0.tar.gz

---
+++ packages/petal-plaid-python/petal-plaid-python-1.0.0.tar.gz/petal-plaid-python-1.0.0/plaid/api/auth.py
@@ -0,0 +1,30 @@
+from plaid.api.api import API
+
+
+class Auth(API):
+    '''Auth endpoints.'''
+
+    def get(self,
+            access_token,
+            _options={},
+            account_ids=None,
+            **petal_kwargs):
+        '''
+        Retrieve account and routing numbers for checking and savings accounts.
+        (`HTTP docs <https://plaid.com/docs/api/#auth>`__)
+
+        :param  str     access_token:
+        :param  [str]   account_ids:    A list of account_ids to retrieve for
+                                        the item. Optional.
+        :param dict petal_kwargs: Dict of 'proxies', 'verify', and 'petal_headers' (used in /get/auth to attach VGS
+        proxy, verify cert, and log header to the request).
+        '''
+        options = {}
+        options.update(_options)
+        if account_ids is not None:
+            options['account_ids'] = account_ids
+
+        return self.client.post('/auth/get', {
+            'access_token': access_token,
+            'options': options,
+        }, **petal_kwargs)
