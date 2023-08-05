dc7cc65c1019400368e897e449983fc90de467b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:01 +0000

    Add package vanilla-option-pricing/0.0.6/vanilla_option_pricing-0.0.6.tar.gz

---
+++ packages/vanilla-option-pricing/vanilla_option_pricing-0.0.6.tar.gz/vanilla_option_pricing-0.0.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+from docs.conf import version
+
+setup(
+    name='vanilla_option_pricing',
+    version=version,
+    packages=find_packages(),
+    url='https://vanilla-option-pricing.readthedocs.io/en/latest/',
+    license='MIT',
+    author='Emanuele Fabbiani',
+    author_email='emanuele.fabbiani@xtreamers.io',
+    description='A very simple library to calibrate mean-reverting stochastic models and compute their variance. '
+                'Includes vanilla option pricing functionalities.',
+    install_requires=['pandas', 'py_vollib', 'scipy', 'numpy', 'py_lets_be_rational'],
+    test_suite='nose.collector',
+    tests_require=['nose']
+)
