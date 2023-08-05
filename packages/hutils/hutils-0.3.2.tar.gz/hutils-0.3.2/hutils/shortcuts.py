0836f2132d742ad15dd985c383d62e63b02ec831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:34 +0000

    Add package hutils/0.3.2/hutils-0.3.2.tar.gz

---
+++ packages/hutils/hutils-0.3.2.tar.gz/hutils-0.3.2/hutils/shortcuts.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+#
+# this module provides various one liners
+from __future__ import absolute_import, unicode_literals
+
+import datetime
+from typing import Iterable, Tuple, Type
+
+
+def datetime_combine(
+        start_date: datetime.date, end_date: datetime.date = None) -> Tuple[datetime.datetime, datetime.datetime]:
+    """ 获取一段日期的起止时间。get start/end datetime from date.
+
+    Examples::
+
+        start, end = datetime_combine(datetime.date.today())
+    """
+    end_date = end_date or start_date
+    return (
+        datetime.datetime.combine(start_date, datetime.time.min),
+        datetime.datetime.combine(end_date, datetime.time.max),
+    )
+
+
+def datetime_to_str(value, fmt='%Y-%m-%d %H:%M:%S'):
+    """ 时间类型转换为字符串。datetime to string.
+
+    :type value: datetime.datetime
+    :type fmt: str
+    :rtype: str
+    """
+    return value.strftime(fmt)
+
+
+def date_to_str(value, fmt='%Y-%m-%d'):
+    """ 日期类型转换为字符串。date to string.
+
+    :type value: datetime.date
+    :type fmt: str
+    :rtype: str
+    """
+    return value.strftime(fmt)
+
+
+def get_uid(instance):
+    """ 获取实例的 uid (hex). get hex uid from instance.
+
+    Examples::
+
+        data = {
+            'uid': instance.uid.hex if instance else None,
+            'related_uid': instance.related.uid.hex if instance.related else None,
+        }
+
+        data = {
+            'uid': get_uid(instance),
+            'related_uid': get_uid(instance.related),
+        }
+
+    :rtype: str | None
+    """
+    return instance.uid.hex if instance else None
+
+
+def list_first(instances: Iterable, default=None):
+    """ 获取列表的第一个元素，假如没有第一个元素则返回默认值。get first value of a list or default value.
+
+    Examples::
+
+        list_first(instances)
+        # None
+    """
+    return list_get(instances, 0, default=default)
+
+
+def list_get(instances: Iterable, index: int, default=None):
+    """ 根据索引号获取列表值或者默认值。get default value on index out of range for list.
+
+    Examples::
+
+        list_get([0, 1, 2], 3, 4)
+        # 4
+    """
+    try:
+        return list(instances)[index]
+    except IndexError:
+        return default
+
+
+def mock_lambda(return_value=None, raises: Type[Exception] = None, **kwargs):
+    """ 伪造返回数据的快捷函数。convenient method to mock return value.
+
+    Examples::
+
+        mock.patch('hutils.merge_dicts', mock_lambda(a=1, b=2))
+        mock.patch('hutils.merge_dicts', mock_lambda(raises=ValueError))
+    """
+
+    def func(*_, **__):
+        if raises is not None:
+            raise raises
+        if return_value is not None:
+            if isinstance(return_value, dict):
+                return return_value.copy()
+            return return_value
+        return kwargs.copy()
+
+    return func
+
+
+def str_to_datetime(value, fmt='%Y-%m-%d %H:%M:%S'):
+    """ 时间类型转换为字符串。datetime to string.
+
+    :type value: str
+    :type fmt: str
+    :rtype: datetime.datetime
+    """
+    return datetime.datetime.strptime(value, fmt)
+
+
+def str_to_date(value, fmt='%Y-%m-%d'):
+    """ 日期类型转换为字符串。date to string.
+
+    :type value: str
+    :type fmt: str
+    :rtype: datetime.date
+    """
+    return datetime.datetime.strptime(value, fmt).date()
