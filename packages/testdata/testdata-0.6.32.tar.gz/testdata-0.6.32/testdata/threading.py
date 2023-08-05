a18fb89f4a7a5bd4ed9a096dd920e6b8a1391d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:39 +0000

    Add package testdata/0.6.32/testdata-0.6.32.tar.gz

---
+++ packages/testdata/testdata-0.6.32.tar.gz/testdata-0.6.32/testdata/threading.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, division, print_function, absolute_import
+import threading
+import sys
+import logging
+
+from .compat import queue, _thread, reraise
+
+
+logger = logging.getLogger(__name__)
+
+Queue = queue.Queue
+Empty = queue.Empty
+
+exc_queue = Queue()
+
+
+class Thread(threading.Thread):
+    """
+    Drop in replacement for the standard Thread that raises any error
+
+    by default, the threading library suppresses exceptions, this makes using a thread
+    for testing kind of annoying since AssertionErrors don't get picked up correctly, but
+    this thread will throw the error if .join() is used, and raise a KeyboardInterrupt if
+    .join() isn't used (stopping on the error is better than nothing)
+    """
+    @property
+    def exception(self):
+        """this will hold the exception if KeyboardInterrupt is raised and .join()
+        isn't used"""
+        global exc_queue
+        thread_e_info = None
+        try:
+            thread_e_info = exc_queue.get(False)
+            exc_queue.task_done()
+
+        except Empty:
+            pass
+
+        return thread_e_info[0] if thread_e_info else None
+
+    def run(self, *args, **kwargs):
+        try:
+            super(Thread, self).run(*args, **kwargs)
+
+        except Exception as e:
+            # http://stackoverflow.com/a/1854263/5006
+            global exc_queue
+            exc_info = sys.exc_info()
+            exc_queue.put((e, exc_info))
+            logger.exception(e)
+            _thread.interrupt_main()
+
+    def join(self, *args, **kwargs):
+        try:
+            super(Thread, self).join(*args, **kwargs)
+
+        except KeyboardInterrupt as e:
+            pass
+
+        finally:
+            global exc_queue
+            thread_e_info = None
+            try:
+                thread_e_info = exc_queue.get(False)
+                exc_queue.task_done()
+
+            except Empty:
+                pass
+
+            finally:
+                if thread_e_info:
+                    thread_e, thread_exc_info = thread_e_info
+                    reraise(*thread_exc_info)
+
