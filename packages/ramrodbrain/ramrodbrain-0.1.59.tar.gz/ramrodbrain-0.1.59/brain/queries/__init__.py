0004939a0459ae5f957e320596f2a6cae3248f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package ramrodbrain/0.1.59/ramrodbrain-0.1.59.tar.gz

---
+++ packages/ramrodbrain/ramrodbrain-0.1.59.tar.gz/ramrodbrain-0.1.59/brain/queries/__init__.py
@@ -0,0 +1,11 @@
+"""
+Query module is getting big
+"""
+
+# import here for backward compatible
+from ..static import RBT, RBJ, RBO, RBF, RPX, RPC, RPP
+
+
+# backward compatible api
+from .reads import *
+from .writes import *
