8cbfedbf0963c651572c590dcbd73ed5f7d1818a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:52 +0000

    Add package package_a8b11/0.2/package_a8b11-0.2.tar.gz

---
+++ packages/package_a8b11/package_a8b11-0.2.tar.gz/package_a8b11-0.2/package_a8b11/text.py
@@ -0,0 +1,4 @@
+from markdown import markdown
+
+def joke():
+    return markdown("which is the **biggest** rope? ... Europe!")
---
+++ packages/package_a8b11/package_a8b11-0.2.tar.gz/package_a8b11-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='package_a8b11', 
+      version='0.2', 
+      description='The funniest joke in the world', 
+      author='David Ivan', 
+      author_email='avandavad@gmail.com', 
+      license='MIT', 
+      packages=['package_a8b11'], 
+      install_requires=[
+          'markdown',
+      ],
+      zip_safe=False)
