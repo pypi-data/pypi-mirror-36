a98c7917409597e70350e95e8ef68267cbf9bbed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:23 +0000

    Add package combine/0.0.11/combine-0.0.11.tar.gz

---
+++ packages/combine/combine-0.0.11.tar.gz/combine-0.0.11/combine/config.py
@@ -0,0 +1,35 @@
+import os
+import datetime
+
+import yaml
+
+
+class Config:
+    def __init__(self, path):
+        self.path = path
+        self.data = {}
+
+        if os.path.exists(self.path):
+            self.data = yaml.safe_load(open(self.path, 'r'))
+            if self.data is None:
+                # allow an empty file to start with
+                self.data = {}
+
+    @property
+    def variables(self):
+        variables = self.default_variables
+        user_variables = self.data.get('variables', {})
+        variables.update(user_variables)
+        return variables
+
+    @property
+    def default_variables(self):
+        return {
+            'now': datetime.datetime.now  # as a function
+        }
+
+    def get_commands(self, command_type):
+        cmds = self.data.get(command_type, [])
+        if not isinstance(cmds, list):
+            cmds = [cmds]
+        return cmds
