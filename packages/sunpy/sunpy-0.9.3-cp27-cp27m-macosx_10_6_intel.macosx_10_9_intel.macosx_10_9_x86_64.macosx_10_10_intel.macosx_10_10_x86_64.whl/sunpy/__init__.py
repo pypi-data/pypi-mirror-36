96676a45dee0a401709e8e3b0d9a3e82a64f7473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:20 +0000

    Add package sunpy/0.9.3/sunpy-0.9.3-cp27-cp27m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/sunpy/sunpy-0.9.3-cp27-cp27m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl/sunpy/__init__.py
@@ -0,0 +1,29 @@
+"""
+SunPy
+=====
+
+An open-source Python library for Solar Physics data analysis.
+
+Web Links
+---------
+Homepage: http://sunpy.org
+Documentation: http://docs.sunpy.org/en/stable/
+"""
+from __future__ import absolute_import
+
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ''
+
+try:
+    from .version import githash as __githash__
+except ImportError:
+    __githash__ = ''
+
+from sunpy.util.config import load_config, print_config
+from sunpy.util import system_info
+from sunpy.tests import main as self_test
+
+# Load user configuration
+config = load_config()
