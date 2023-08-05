4ea5047768114e39ca0e620eca805532b8965638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:06 +0000

    Add package gilesbot/0.1/gilesbot-0.1.tar.gz

---
+++ packages/gilesbot/gilesbot-0.1.tar.gz/gilesbot-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gilesbot',
+      version='0.1',
+      description='A github bot for posting statues of CircleCI builds.',
+      url='http://github.com/Cadair/giles',
+      author='Stuart Mumford',
+      author_email='stuart@cadair.com',
+      license='MIT',
+      zip_safe=False)
