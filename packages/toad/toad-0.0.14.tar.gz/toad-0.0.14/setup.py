10873ea9b6a0e7016ce5448d55ea0292d8a2dcbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:11 +0000

    Add package toad/0.0.14/toad-0.0.14.tar.gz

---
+++ packages/toad/toad-0.0.14.tar.gz/toad-0.0.14/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages, Extension
+
+
+setup(
+    name = 'toad',
+    version = '0.0.14',
+    description = 'python utils for detect data',
+    long_description = open('README.rst').read(),
+    # long_description_content_type = 'text/markdown',
+    url = 'https://github.com/Secbone/toad',
+    author = 'ESC Team',
+    author_email = 'secbone@gmail.com',
+    packages = find_packages(exclude = ['tests']),
+    python_requires = '>=3.5',
+    setup_requires = [
+        'setuptools',
+    ],
+    install_requires = [
+        'numpy >= 1.15',
+        'pandas',
+        'scipy',
+        'sklearn',
+    ],
+    license = 'MIT',
+    classifiers = [
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
