ce19b021e03dfbe4cd830135c426cac8bc2dea50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:07 +0000

    Add package nester-mht/1.1.0/nester-mht-1.1.0.tar.gz

---
+++ packages/nester-mht/nester-mht-1.1.0.tar.gz/nester-mht-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester-mht',
+    version='1.1.0',
+    py_modules=['nester-mht'],
+    author='Hitomeng',
+    author_email='hitomeng@qq.com',
+    url='http://www.headerfirstlabs.com',
+    description='A simple printer for list',
+)
