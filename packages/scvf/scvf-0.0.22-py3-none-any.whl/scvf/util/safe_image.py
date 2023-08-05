38f50881bcab7bab30672f0f5ae5066aed96218a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:38 +0000

    Add package scvf/0.0.22/scvf-0.0.22-py3-none-any.whl

---
+++ packages/scvf/scvf-0.0.22-py3-none-any.whl/scvf/util/safe_image.py
@@ -0,0 +1,18 @@
+from threading import Lock
+
+
+class SafeImage:
+    def __init__(self):
+        self.lock = Lock()
+        self.image_container = []
+
+    def set(self, img):
+        with self.lock:
+            if len(self.image_container) != 0:
+                self.image_container.pop()
+            self.image_container.append(img)
+
+    def get(self):
+        with self.lock:
+            if len(self.image_container) != 0:
+                return self.image_container.pop()
