f3d89ae87ff3cfe784cf04ff5639da7b8e5898df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:54 +0000

    Add package samppy/1.0.5/samppy-1.0.5.tar.gz

---
+++ packages/samppy/samppy-1.0.5.tar.gz/samppy-1.0.5/samppy/__init__.py
@@ -0,0 +1,8 @@
+"""This package includes modules for standard Hamiltonian MCMC sampling
+and for some analyses of the posterior distribution
+defined by an array of (possibly Hamiltonian-generated) samples.
+"""
+__name__ = 'samppy'
+__version__ = '1.0.5'
+__all__ = ['hamiltonian_sampler', 'credibility', 'sample_entropy']
+
