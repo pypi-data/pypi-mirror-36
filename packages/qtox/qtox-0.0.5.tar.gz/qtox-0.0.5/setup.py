3593d2475526a23b2bdc23ae6273055b26a3619c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:15 +0000

    Add package qtox/0.0.5/qtox-0.0.5.tar.gz

---
+++ packages/qtox/qtox-0.0.5.tar.gz/qtox-0.0.5/setup.py
@@ -0,0 +1,24 @@
+import pathlib
+import setuptools
+
+
+simple_description = "Lets you re-run Tox commands faster and in parallel."
+try:
+    with open(pathlib.Path(__file__).parent / "README.rst") as file:
+        long_description = file.read()
+except BaseException:
+    long_description = simple_description
+
+
+setuptools.setup(
+    name="qtox",
+    version="0.0.5",
+    author="Tim Simpson",
+    description="Lets you re-run Tox commands faster and in parallel.",
+    long_description=long_description,
+    packages=setuptools.find_packages(exclude=["tests"]),
+    include_package_data=True,
+    install_requires=["typing"],
+    license="MIT",
+    entry_points={"console_scripts": ["qtox = qtox.main:main"]},
+)
