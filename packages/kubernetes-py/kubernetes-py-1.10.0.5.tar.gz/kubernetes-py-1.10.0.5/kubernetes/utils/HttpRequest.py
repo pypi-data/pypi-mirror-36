a70e8914e8e93126e6f2ad273e941347dd36c366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:10 +0000

    Add package kubernetes-py/1.10.0.5/kubernetes-py-1.10.0.5.tar.gz

---
+++ packages/kubernetes-py/kubernetes-py-1.10.0.5.tar.gz/kubernetes-py-1.10.0.5/kubernetes/utils/HttpRequest.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.md', which is part of this source code package.
+#
+import base64
+import json
+import os
+import tempfile
+import requests
+from kubernetes.utils.ConvertData import convert
+from six.moves.urllib.parse import urlencode
+
+
+class HttpRequest:
+    def __init__(self, method='GET', host='localhost:80', url='/', data=None, auth=None,
+                 cert=None, ca_cert=None, ca_cert_data=None, token=None):
+
+        self.http_method = method
+        self.http_host = host
+        self.url = url
+        self.data = data
+        self.auth = auth
+        self.cert = cert
+        self.ca_cert = ca_cert
+        self.ca_cert_data = ca_cert_data
+        self.token = token
+
+    def send(self):
+        state = dict(success=False, reason=None, status=None, data=None)
+        http_headers = dict()
+        http_headers['Accept'] = 'application/json'
+
+        if self.http_method in ['PUT', 'POST', 'PATCH']:
+            http_headers['Content-type'] = 'application/json'
+
+        if self.token is not None:
+            http_headers['Authorization'] = 'Bearer {token}'.format(token=self.token)
+
+        if self.data is not None and self.http_method in ['GET']:
+            url = "{0}?{1}".format(self.url, urlencode(self.data))
+            self.url = url
+
+        self.url = self.http_host + self.url
+
+        temp = None
+        verify = False
+        if self.ca_cert is not None:
+            verify = self.ca_cert
+        if self.ca_cert_data is not None:
+            temp = tempfile.NamedTemporaryFile(delete=False)
+            data = base64.b64decode(self.ca_cert_data)
+            temp.write(data)
+            temp.close()
+            verify = temp.name
+
+        try:
+
+            response = requests.request(
+                method=self.http_method,
+                url=self.url,
+                auth=self.auth,
+                cert=self.cert,
+                headers=http_headers,
+                data="" if self.data is None else json.dumps(self.data),
+                verify=verify
+            )
+
+        except Exception as err:
+            raise err
+
+        finally:
+            if temp is not None:
+                os.unlink(temp.name)
+
+        state['status'] = response.status_code
+        state['reason'] = response.reason
+        resp_data = response.content.decode()
+
+        if len(resp_data) > 0:
+            try:
+                state['data'] = convert(data=json.loads(resp_data))
+            except ValueError:
+                state['data'] = resp_data.strip()
+
+        if 200 <= state['status'] <= 299:
+            state['success'] = True
+
+        return state
---
+++ packages/kubernetes-py/kubernetes-py-1.10.0.5.tar.gz/kubernetes-py-1.10.0.5/setup.py
@@ -0,0 +1,55 @@
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# See https://github.com/pybuilder/pybuilder/issues/56
+del os.link
+
+# install_reqs = parse_requirements(os.path.abspath(__file__).replace('setup.py', 'requirements.txt'),
+#                                   session=PipSession())
+# reqs = [str(ir.req) for ir in install_reqs]
+
+
+def version():
+    with open(os.path.abspath(__file__).replace('setup.py', 'version.meta'), 'r') as v:
+        return v.read().replace('\n', '')
+
+
+setup(
+    name='kubernetes-py',
+    version=version(),
+    description='A python module for Kubernetes.',
+    author='mnubo inc.',
+    author_email='it-admin@mnubo.com',
+    url='https://github.com/mnubo/kubernetes-py',
+    download_url='https://github.com/mnubo/kubernetes-py/tarball/' + version(),
+    keywords=['kubernetes', 'k8s'],
+    packages=[
+        'kubernetes',
+        'kubernetes.models',
+        'kubernetes.models.unversioned',
+        'kubernetes.models.v1',
+        'kubernetes.models.v1alpha1',
+        'kubernetes.models.v1beta1',
+        'kubernetes.models.v2alpha1',
+        'kubernetes.utils'
+    ],
+    install_requires=[
+        'six>=1.10.0',
+        'PyYAML>=3.13',
+        'requests>=2.10.0',
+        'uuid>=1.30',
+        'python-dateutil>=2.6.0'
+    ],
+    scripts=[],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'nose-cover3'
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
