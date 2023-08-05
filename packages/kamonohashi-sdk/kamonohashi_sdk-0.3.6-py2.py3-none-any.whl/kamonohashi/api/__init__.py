7b244abc49db249bcc5d2e0cf6d77be328081edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:08 +0000

    Add package kamonohashi-sdk/0.3.6/kamonohashi_sdk-0.3.6-py2.py3-none-any.whl

---
+++ packages/kamonohashi-sdk/kamonohashi_sdk-0.3.6-py2.py3-none-any.whl/kamonohashi/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from kamonohashi.api.account_api import AccountApi
+from kamonohashi.api.data_api import DataApi
+from kamonohashi.api.data_set_api import DataSetApi
+from kamonohashi.api.job_api import JobApi
+from kamonohashi.api.preprocessing_api import PreprocessingApi
+from kamonohashi.api.storage_api import StorageApi
