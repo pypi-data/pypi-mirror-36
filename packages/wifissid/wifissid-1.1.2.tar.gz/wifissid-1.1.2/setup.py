d8b7c9cdf83dec6ebea8e66b4da0dda7d9078442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:37 +0000

    Add package wifissid/1.1.2/wifissid-1.1.2.tar.gz

---
+++ packages/wifissid/wifissid-1.1.2.tar.gz/wifissid-1.1.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import setuptools
+
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="wifissid",
+    version="1.1.2",
+    author="luxun",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/wifissid/",
+    packages=setuptools.find_packages(),
+    py_modules=['wifissid'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
