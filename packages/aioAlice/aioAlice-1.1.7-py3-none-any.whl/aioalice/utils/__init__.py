87bb95719cae892c65492859173a463521946ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package aioAlice/1.1.7/aioAlice-1.1.7-py3-none-any.whl

---
+++ packages/aioAlice/aioAlice-1.1.7-py3-none-any.whl/aioalice/utils/__init__.py
@@ -0,0 +1,17 @@
+from . import exceptions
+from .json import json
+from .payload import generate_json_payload
+
+
+def ensure_cls(klass):
+    def converter(val):
+        if val is None:
+            return
+        if isinstance(val, dict):
+            return klass(**val)
+        if isinstance(val, list):
+            return [converter(v) for v in val]
+        if not isinstance(val, klass):
+            return klass(val)
+        return val
+    return converter
