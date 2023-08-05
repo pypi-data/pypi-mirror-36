501ff59703aab0c01c442734e941a2af7b84ca78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:13 +0000

    Add package signal-processing/0.0.1/signal_processing-0.0.1.tar.gz

---
+++ packages/signal-processing/signal_processing-0.0.1.tar.gz/signal_processing-0.0.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 13 17:42:03 2018
+
+@author: osama
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="signal_processing",
+    version="0.0.1",
+    author="Osama Dar",
+    author_email="osamadar1996@gmail.com",
+    description="This repository provides some helper functions for signal processing functions related to time series (in Python).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/daroodar/TimeSeriesSignalProcessing",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/signal-processing/signal_processing-0.0.1.tar.gz/signal_processing-0.0.1/signal_proceessing/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 13 17:40:31 2018
+
+@author: osama
+"""
+
+name = "signal_processing"
+
