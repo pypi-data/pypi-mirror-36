8b932bbee7ccea595d3836d8d3d0fb0be00cb31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:32 +0000

    Add package speckle/0.1.3/speckle-0.1.3.tar.gz

---
+++ packages/speckle/speckle-0.1.3.tar.gz/speckle-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="speckle", 
+	description="Python client for Speckle framework.",
+	url="https://github.com/speckleworks/SpeckleBlender",
+	author="Tom Svilans",
+	author_email="tom.svilans@gmail.com",
+	license="MIT",
+	version="0.1.3",
+	packages=find_packages(),
+	)
