dd1bb930bfb683308a0d3945ed952ad71a004e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:41 +0000

    Add package EasyBlogger/3.1.3/EasyBlogger-3.1.3.tar.gz

---
+++ packages/EasyBlogger/EasyBlogger-3.1.3.tar.gz/EasyBlogger-3.1.3/setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name="EasyBlogger",
+      version="3.1.3",
+      author="Raghu Rajagopalan",
+      author_email="raghu.nospam@gmail.com",
+      description=("A (very) easy CLI interface to Blogger blogs"),
+      license="BSD",
+      keywords="blogger, cli, markdown, asciidoc",
+      url="https://github.com/raghur/easyblogger",
+      packages=['blogger'],
+      install_requires=[
+          'pypandoc',
+          'google-api-python-client',
+          'oauth2client',
+          'python-gflags',
+          'httplib2',
+          'toml',
+          'gevent',
+          'coloredlogs',
+          'pyyaml',
+          'chardet'
+      ],
+      entry_points={
+          'console_scripts': [
+              'easyblogger = blogger.main:main'
+          ]
+      },
+      long_description=readme(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Topic :: Utilities",
+          "Topic :: Internet :: WWW/HTTP :: Site Management",
+          "License :: OSI Approved :: BSD License",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6"
+      ],
+      )
