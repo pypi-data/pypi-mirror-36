c3819048ea57617f0c19fa76f1136c803f0a6d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:31 +0000

    Add package python-opendata-transport/0.1.4/python_opendata_transport-0.1.4.tar.gz

---
+++ packages/python-opendata-transport/python_opendata_transport-0.1.4.tar.gz/python_opendata_transport-0.1.4/README.rst
@@ -0,0 +1,39 @@
+python-opendata-transport
+=========================
+
+Python API for interacting with `transport.opendata.ch <http://transport.opendata.ch/>`_.
+
+Th module which is the base for the integration into `Home Assistant <https://home-assistant.io>`_
+is simply retrieving the details about a given connection between to stations.
+
+This module is not official, developed, supported or endorsed by opendata.ch.
+
+Installation
+------------
+
+The module is available from the `Python Package Index <https://pypi.python.org/pypi>`_.
+
+.. code:: bash
+
+    $ pip3 install python_opendata_transport
+
+Usage
+-----
+
+The file ``example.py`` contains an example about how to use this module.
+
+Development
+-----------
+
+For development is recommended to use a ``venv``.
+
+.. code:: bash
+
+    $ python3.6 -m venv .
+    $ source bin/activate
+    $ python3 setup.py develop
+
+License
+-------
+
+``python-opendata-transport`` is licensed under MIT, for more details check LICENSE.
---
+++ packages/python-opendata-transport/python_opendata_transport-0.1.4.tar.gz/python_opendata_transport-0.1.4/opendata_transport/__init__.py
@@ -0,0 +1,67 @@
+"""
+Copyright (c) 2015-2018 Fabian Affolter <fabian@affolter-engineering.ch>
+
+Licensed under MIT. All rights reserved.
+"""
+import asyncio
+import logging
+
+import aiohttp
+import async_timeout
+
+from . import exceptions
+
+_LOGGER = logging.getLogger(__name__)
+_RESOURCE = 'http://transport.opendata.ch/v1/'
+
+
+class OpendataTransport(object):
+    """A class for handling connections from Opendata Transport."""
+
+    def __init__(self, start, destination, loop, session):
+        """Initialize the connection."""
+        self._loop = loop
+        self._session = session
+        self.start = start
+        self.destination = destination
+        self.from_name = self.from_id = self.to_name = self.to_id = None
+        self.connections = dict()
+
+    async def async_get_data(self):
+        url = '{resource}connections?from={start}&to={dest}'.format(
+            resource=_RESOURCE, start=self.start, dest=self.destination)
+
+        try:
+            with async_timeout.timeout(5, loop=self._loop):
+                response = await self._session.get(url, raise_for_status=True)
+
+            _LOGGER.debug(
+                "Response from transport.opendata.ch: %s", response.status)
+            data = await response.json()
+            _LOGGER.debug(data)
+        except (asyncio.TimeoutError):
+            _LOGGER.error("Can not load data from transport.opendata.ch")
+            raise exceptions.OpendataTransportConnectionError()
+        except (aiohttp.ClientError) as aiohttpClientError:
+            _LOGGER.error("Response from transport.opendata.ch: %s",
+                          aiohttpClientError)
+            raise exceptions.OpendataTransportConnectionError()
+
+        try:
+            self.from_id = data['from']['id']
+            self.from_name = data['from']['name']
+            self.to_id = data['to']['id']
+            self.to_name = data['to']['name']
+            index = 0
+            for conn in data['connections'][1:4]:
+                self.connections[index] = dict()
+                self.connections[index]['departure'] = \
+                    conn['from']['departure']
+                self.connections[index]['duration'] = conn['duration']
+                self.connections[index]['transfers'] = conn['transfers']
+                self.connections[index]['number'] = \
+                    conn['sections'][0]['journey']['name']
+                self.connections[index]['platform'] = conn['from']['platform']
+                index = index + 1
+        except (TypeError, IndexError):
+            raise exceptions.OpendataTransportError()
