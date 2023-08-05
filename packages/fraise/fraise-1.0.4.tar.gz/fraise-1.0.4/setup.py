3c019a66ae0713e0469c4ad6d8e70a13ef88a525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:24 +0000

    Add package fraise/1.0.4/fraise-1.0.4.tar.gz

---
+++ packages/fraise/fraise-1.0.4.tar.gz/fraise-1.0.4/setup.py
@@ -0,0 +1,13 @@
+# import shutil
+
+from setuptools import setup
+
+setup(name='fraise',
+      version='1.0.4',
+      description='Generate memorable pass phrases',
+      url='http://github.com/daveygit2050/fraise',
+      author='Dave Randall',
+      author_email='dave@goldsquare.co.uk',
+      license='Apache v2',
+      packages=['fraise'],
+      zip_safe=False)
