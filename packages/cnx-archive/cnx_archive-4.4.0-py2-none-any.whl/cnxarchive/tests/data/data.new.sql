03be4c454a19f5de96415e64f7d1ec879975a7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:04 +0000

    Add package cnx-archive/4.4.0/cnx_archive-4.4.0-py2-none-any.whl

---
+++ packages/cnx-archive/cnx_archive-4.4.0-py2-none-any.whl/cnxarchive/tests/data/data.new.sql
@@ -0,0 +1,24 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.13
+-- Dumped by pg_dump version 9.5.13
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: modules; Type: TABLE DATA; Schema: public; Owner: cnxarchive
+--
+
+SET SESSION AUTHORIZATION DEFAULT;
+
+ALTER TABLE public.modules DISABLE TRIGGER ALL;
+
