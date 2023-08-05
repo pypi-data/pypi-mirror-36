72e4f4ca45b3cffedf8816d81b2dea2b60be41b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:03 +0000

    Add package zutil/0.1.4/zutil-0.1.4.tar.gz

---
+++ packages/zutil/zutil-0.1.4.tar.gz/zutil-0.1.4/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+import setuptools
+
+setup(name="zutil",
+      version='0.1.4',
+      description="Utilities used for generating zCFD control dictionaries",
+      author="Zenotech",
+      author_email="support@zenotech.com",
+      url="https://zcfd.zenotech.com/",
+      packages=["zutil", "zutil.post", "zutil.analysis", "zutil.plot"],
+      install_requires=[
+          'mpi4py',
+          'ipython<6.0',
+          'Fabric',
+          'ipywidgets',
+          'matplotlib',
+          'numpy',
+          'pandas',
+          'PyYAML'
+        ],
+      )
