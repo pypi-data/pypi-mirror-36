0d2e3641968842736bfaa06a76ec8dc81a07e8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:35 +0000

    Add package inlineplz/0.40.5/inlineplz-0.40.5.tar.gz

---
+++ packages/inlineplz/inlineplz-0.40.5.tar.gz/inlineplz-0.40.5/inlineplz/linters/detectsecrets.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import traceback
+import sys
+import dirtyjson as json
+
+from inlineplz.parsers.base import ParserBase
+from inlineplz.decorators import linter
+
+
+@linter(
+    name="detect-secrets",
+    install=[[sys.executable, "-m", "pip", "install", "-U", "detect-secrets"]],
+    help_cmd=["detect-secrets", "-h"],
+    run=["detect-secrets", "scan", "--all-files"],
+    rundefault=["detect-secrets", "scan", "--all-files"],
+    dotfiles=[],
+    language="all",
+    autorun=True,
+    run_per_file=False,
+)
+class DetectSecretsParser(ParserBase):
+    """Parse json detect-secrets output."""
+
+    def parse(self, lint_data):
+        messages = set()
+        try:
+            for path, msgs in json.loads(lint_data).get("results").items():
+                for msgdata in msgs:
+                    try:
+                        line = msgdata["line_number"]
+                        msgbody = msgdata["type"]
+                        messages.add((path, line, msgbody))
+                    except (ValueError, KeyError):
+                        print("Invalid message: {0}".format(msgdata))
+        except ValueError:
+            print(traceback.format_exc())
+            print(lint_data)
+        return messages
---
+++ packages/inlineplz/inlineplz-0.40.5.tar.gz/inlineplz-0.40.5/inlineplz/linters/proselint.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import sys
+
+from inlineplz.parsers.base import ParserBase
+from inlineplz.decorators import linter
+
+
+@linter(
+    name="proselint",
+    install=[[sys.executable, "-m", "pip", "install", "-U", "proselint"]],
+    help_cmd=["proselint", "-h"],
+    run=["proselint"],
+    rundefault=["proselint"],
+    dotfiles=[],
+    language="text",
+    autorun=True,
+    run_per_file=True,
+    concurrency=1,
+)
+class ProselintParser(ParserBase):
+    """Parse proselint output."""
+
+    def parse(self, lint_data):
+        messages = set()
+        for file_path, output in lint_data:
+            try:
+                if file_path.strip() and output.strip():
+                    for line in output.split("\n"):
+                        parts = line.split(":")
+                        line = int(parts[1].strip())
+                        msgbody = ":".join(parts[2:]).strip()
+                        messages.add((file_path, line, msgbody))
+            except (ValueError, IndexError):
+                print("Invalid message: {0}".format(output))
+        return messages
