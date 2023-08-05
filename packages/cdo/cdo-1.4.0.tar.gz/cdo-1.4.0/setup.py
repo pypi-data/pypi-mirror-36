120bb35ba0dcb12cc8a6530c49e9fa35572eee35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:56 +0000

    Add package cdo/1.4.0/cdo-1.4.0.tar.gz

---
+++ packages/cdo/cdo-1.4.0.tar.gz/cdo-1.4.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup (name   = 'cdo',
+  version     = '1.4.0',
+  author      = "Ralf Mueller",
+  author_email= "stark.dreamdetective@gmail.com",
+  license     = "GPLv2",
+  description = """python bindings to CDO""",
+  py_modules  = ["cdo"],
+  url         = "https://code.mpimet.mpg.de/projects/cdo/wiki/Cdo%7Brbpy%7D",
+  classifiers = [
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+    ],
+  install_requires=['six'],
+  )
