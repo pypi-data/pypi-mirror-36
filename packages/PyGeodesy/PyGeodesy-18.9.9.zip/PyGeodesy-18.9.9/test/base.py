b94b53db4b04f2ef20a68e53c809a847b4e129ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:56 +0000

    Add package PyGeodesy/18.9.9/PyGeodesy-18.9.9.zip

---
+++ packages/PyGeodesy/PyGeodesy-18.9.9.zip/PyGeodesy-18.9.9/test/base.py
@@ -0,0 +1,397 @@
+
+# -*- coding: utf-8 -*-
+
+# Base class and functions for PyGeodesy tests.
+
+# After (C) Chris Veness 2011-2015 published under the same MIT Licence,
+# see <http://www.movable-type.co.uk/scripts/latlong-vectors.html>
+# and <http://www.movable-type.co.uk/scripts/latlong.html>.
+
+from inspect import isclass, isfunction, ismethod, ismodule
+from os.path import abspath, basename, dirname
+from platform import architecture, java_ver, mac_ver, win32_ver, uname
+import sys
+from time import time
+try:
+    import geographiclib
+except ImportError:
+    geographiclib = None
+try:
+    import numpy
+except ImportError:
+    numpy = None
+
+PyGeodesy_dir = dirname(dirname(abspath(__file__)))
+# extend sys.path to include the ../.. directory
+if PyGeodesy_dir not in sys.path:  # Python 3+ ModuleNotFoundError
+    sys.path.insert(0, PyGeodesy_dir)
+
+from pygeodesy import version as PyGeodesy_version, \
+                      iterNumpy2over, normDMS  # PYCHOK expected
+
+__all__ = ('isIntelPython', 'isiOS', 'isNix', 'isPyPy',  # constants
+           'isPython2', 'isPython3', 'isWindows',
+           'PyGeodesy_dir', 'Python_O',
+           'TestsBase',
+           'ios_ver', 'runner', 'secs2str',
+           'tilde', 'type2str', 'versions')
+__version__ = '18.09.08'
+
+try:
+    _int = int, long
+    _str = basestring
+except NameError:  # Python 3+
+    _int = int
+    _str = str
+
+_os_bitstr = architecture()[0]
+_pseudo_home_dir = dirname(PyGeodesy_dir or '~') or '~'
+
+Python_O = sys.executable  # python or Pythonista path
+
+isIntelPython = 'intelpython' in Python_O
+# isiOS is used by some tests known to fail on iOS only
+isiOS         = sys.platform == 'ios'  # public
+isNix         = uname()[0] in ('Linux', 'linux')
+isPyPy        = 'PyPy ' in sys.version  # public
+isPython2     = sys.version_info[0] == 2
+isPython3     = sys.version_info[0] == 3
+isWindows     = sys.platform.startswith('win')
+
+try:
+    # use distro only for Linux, not macOS, etc.
+    if not isNix:
+        raise ImportError
+
+    import distro  # <http://pypi.org/project/distro/>
+
+    def _2str(ustr):
+        s = u = str(ustr).strip()
+        for c in u:
+            if not (c.isalnum() or c in '._-'):
+                s = s.replace(c, ' ')
+        return '_'.join(s.strip().split())
+
+    _Nix = _2str(distro.id()).capitalize()  # .name()?
+
+    def nix_ver():  # *nix release
+        return _2str(distro.version()), _os_bitstr
+
+except ImportError:
+    _Nix = ''  # not linux?
+
+    def nix_ver():  # PYCHOK expected
+        return _Nix, _os_bitstr
+
+
+class TestsBase(object):
+    '''Tests based on @examples from the original JavaScript code
+       and examples in <http://www.edwilliams.org/avform.htm> or
+       elsewhere as indicated.
+    '''
+    _file     = ''
+    _name     = ''
+    _iterisk  = ''
+    _prefix   = '    '
+    _time     = 0
+    _verbose  = True  # print all tests, otherwise failures only
+    _versions = ''  # cached versions() string
+
+    failed  = 0
+    known   = 0
+    skipped = 0
+    total   = 0
+
+    def __init__(self, testfile, version, module=None, verbose=True):
+        if not self._versions:  # get versions once
+            TestsBase._versions = versions()
+        self._file = testfile
+        self._name = basename(testfile)
+        self.title(self._name, version, module=module)
+        self._time = time()
+        self._verbose = verbose
+
+    def errors(self):
+        '''Return the number of tests failures,
+           excluding the KNOWN failures.
+        '''
+        return self.failed - self.known  # new failures
+
+    def exit(self, errors=0):
+        '''Exit with the number of test failures as exist status.
+        '''
+        sys.exit(min(errors + self.errors(), 99))
+
+    def iterNumpy2over(self, n=None):
+        '''Set the I{iterNumpy2} threshold.
+
+           @keyword n: Optional, new threshold value (integer).
+
+           @return: Previous threshold (integer).
+        '''
+        p = iterNumpy2over(n)
+        self.test('iterNumpy2over', iterNumpy2over(), n)
+        return p
+
+    def printf(self, fmt, *args, **kwds):  # nl=0, nt=0
+        '''Print a formatted line to sys.stdout.
+        '''
+        nl = '\n' * kwds.get('nl', 0)
+        nt = '\n' * kwds.get('nt', 0)
+        print(''.join((nl, self._prefix, (fmt % args), nt)))
+
+    def results(self, passed='passed', nl=1):
+        '''Summarize the test results.
+        '''
+        s = time() - self._time
+        r = passed
+        t = self.total
+        n = self.failed
+        if n:
+            k = self.known or ''
+            if k:
+                k = ', ALL KNOWN' if k == n else (
+                    ', incl. %s KNOWN' % (k,))
+            r = '(%.1f%%) FAILED%s' % (100.0 * n / t, k)
+            n = '%s of %d' % (n, t)
+        elif t:
+            n = 'all %d' % (t,)
+        else:
+            n = 'all'
+        k = self.skipped or ''
+        if k:
+            k = ', %d skipped' % (k,)
+        r = '%s%s (%s) %s' % (r, k, self._versions, secs2str(s))
+        self.printf('%s %s tests %s', n, self._name, r, nl=nl)
+
+    def skip(self, text=''):
+        '''Skip this test, leave a message.
+        '''
+        self.skipped += 1
+        if text and self._verbose:
+            self.printf('test skipped (%d): %s', self.skipped, text)
+
+    def subtitle(self, module, testing='ing', **kwds):
+        '''Print the subtitle of a test suite.
+        '''
+        t = (basename(module.__name__), module.__version__) + \
+             tuple('%s=%s' % t for t in sorted(kwds.items()))
+        self.printf('test%s(%s)', testing, ', '.join(t), nl=1)
+
+    def test(self, name, value, expect, fmt='%s', known=False, nt=0):
+        '''Compare a test value with the expected one.
+        '''
+        if self._iterisk:
+            name += self._iterisk
+
+        if not isinstance(expect, _str):
+            expect = fmt % (expect,)  # expect as str
+
+        f, v = '', fmt % (value,)  # value as str
+        if v != expect and v != normDMS(expect):
+            self.failed += 1  # failures
+            if known:  # failed before
+                self.known += 1
+                f = ', KNOWN'
+            f = '  FAILED%s, expected %s' % (f, expect)
+
+        self.total += 1  # tests
+        if f or self._verbose:
+            self.printf('test %d %s: %s%s', self.total, name, v, f, nt=nt)
+
+    def test__(self, fmt, *args, **kwds):
+        '''Print subtotal test line.
+        '''
+        t = '-' * len(str(self.total))
+        self.printf('test %s %s', t, (fmt % args), **kwds)
+
+    def testiter(self):
+        '''Test with/-out I{iterNumpy2} threshold.
+        '''
+        yield iterNumpy2over(100000)
+        self._iterisk = '*'
+        yield iterNumpy2over(1)
+        self._iterisk = ''
+
+    def title(self, test, version, module=None):
+        '''Print the title of the test suite.
+        '''
+        if module:
+            m = ' (module %s %s)' % (basename(module.__name__),
+                                     module.__version__)
+        else:
+            m = ''
+        self.printf('testing %s %s%s', test, version, m, nl=1)
+
+
+if isiOS:  # MCCABE 13
+
+    def ios_ver(**unused):
+        '''Get the iOS version information.
+        '''
+        t = mac_ver()
+        # append 'machine' iPad, iPhone to 'release'
+        r = t[0] + ' ' + t[2].split(',')[0]
+        return (r,) + t[1:]
+
+    try:  # prefer StringIO over io
+        from StringIO import StringIO
+    except ImportError:  # Python 3+
+        from io import StringIO
+    from runpy import run_path
+    from traceback import format_exception
+
+    def runner(test):
+        '''Invoke one test module and return
+           the exit status and console output.
+        '''
+        # Mimick partial behavior of function runner
+        # further below because subprocess.Popen is
+        # not available on iOS/Pythonista/Python.
+        # One issue however, the test script is
+        # imported and run in the same process.
+
+        x = None  # no exit, no exception
+
+        sys3 = sys.argv, sys.stdout, sys.stderr
+        sys.stdout = sys.stderr = std = StringIO()
+        try:
+            sys.argv = [test]
+            run_path(test, run_name='__main__')
+        except:  # PYCHOK have to on Pythonista
+            x = sys.exc_info()
+            if x[0] is SystemExit:
+                x = x[1].code  # exit status
+            else:  # append traceback
+                x = [t for t in format_exception(*x)
+                             if 'runpy.py", line' not in t]
+                print(''.join(map(tilde, x)).rstrip())
+                x = 1  # count as a failure
+        sys.argv, sys.stdout, sys.stderr = sys3
+
+        r = std.getvalue()
+        if isinstance(r, bytes):  # Python 3+
+            r = r.decode('utf-8')
+
+        std.close()
+        std = None  # del std
+
+        if x is None:  # no exit status or exception:
+            # count failed tests excluding KNOWN ones
+            x = r.count('FAILED, expected')
+        return x, r
+
+    Python_O = basename(Python_O)
+
+else:  # non-iOS
+
+    def ios_ver(**unused):  # PYCHOK expected
+        '''Get the iOS version information.
+        '''
+        return ('', ('', '', ''), '')
+
+    from subprocess import PIPE, STDOUT, Popen
+
+    if not __debug__:
+        Python_O += ' -O'  # optimized
+
+    def runner(test):  # PYCHOK expected
+        '''Invoke one test module and return
+           the exit status and console output.
+        '''
+        c = Python_O.split() + [test]
+        p = Popen(c, creationflags=0,
+                     executable   =sys.executable,
+                   # shell        =True,
+                     stdin        =None,
+                     stdout       =PIPE,  # XXX
+                     stderr       =STDOUT)  # XXX
+
+        r = p.communicate()[0]
+        if isinstance(r, bytes):  # Python 3+
+            r = r.decode('utf-8')
+
+        # the exit status reflects the number of
+        # test failures in the tested module
+        return p.returncode, r
+
+
+def secs2str(secs):
+    '''Convert a time value to string.
+    '''
+    unit = ['sec', 'ms', 'us', 'ps']
+    while secs < 1 and len(unit) > 1:
+        secs *= 1000.0
+        unit.pop(0)
+    return '%.3f %s' % (secs, unit[0])
+
+
+def tilde(path):
+    '''Return a shortened path, especially Pythonista.
+    '''
+    return path.replace(_pseudo_home_dir, '~')
+
+
+def type2str(obj, attr):
+    '''Return the type name of an object attribute.
+    '''
+    t = getattr(obj, attr, None)
+    if isclass(t):
+        t = '() class'
+    elif isinstance(t, float):
+        t = ' float'
+    elif ismethod(t):
+        t = '() method'
+    elif isfunction(t):
+        if isclass(obj):
+            t = '() method'
+        else:
+            t = '() function'
+    elif isinstance(t, _int):
+        t = ' int'
+    elif ismodule(t):
+        t = ' module'
+    elif type(t) is property:
+        t = ' property'
+    elif isinstance(t, _str):
+        t = ' str'
+    else:
+        t = str(type(t)).replace("'", '')
+        if t.startswith('<') and t.endswith('>'):
+            t = ' ' + t[1:-1]
+        else:
+            t = ' attribute'
+    return t
+
+
+def versions():
+    '''Get pygeodesy, Python versions, size, OS name and release.
+    '''
+    t = 'Intel-' if isIntelPython else 'PyPy-' if isPyPy else ''
+    vs = 'PyGeodesy', PyGeodesy_version, (t +
+         'Python'), sys.version.split()[0], _os_bitstr
+    if geographiclib:
+        vs += 'geographiclib', geographiclib.__version__
+    if numpy:
+        vs += 'numpy', numpy.__version__
+
+    # - mac_ver() returns ('10.12.5', ..., 'x86_64') on
+    #   macOS and ('10.3.3', ..., 'iPad4,2') on iOS
+    # - win32_ver is ('XP', ..., 'SP3', ...) on Windows XP SP3
+    # - platform() returns 'Darwin-16.6.0-x86_64-i386-64bit'
+    #   on macOS and 'Darwin-16.6.0-iPad4,2-64bit' on iOS
+    # - sys.platform is 'darwin' on macOS, 'ios' on iOS,
+    #   'win32' on Windows and 'cygwin' on Windows/Gygwin
+    # - distro.id() and .name() return 'Darwin' on macOS
+    for t, r in (('iOS',     ios_ver),
+                 ('macOS',   mac_ver),
+                 ('Windows', win32_ver),
+                 (_Nix,      nix_ver),
+                 ('Java',    java_ver),
+                 ('uname',   uname)):
+        r = r()[0]
+        if r:
+            vs += t, r
+            break
+
+    return ' '.join(vs)
---
+++ packages/PyGeodesy/PyGeodesy-18.9.9.zip/PyGeodesy-18.9.9/test/run.py
@@ -0,0 +1,140 @@
+
+# -*- coding: utf-8 -*-
+
+# Script to run some or all PyGeodesy tests with Python 2 or 3.
+
+# Tested with 64-bit Python 2.6.9, 2.7,13, 3.5.3, 3.6.4, 3.6.5 and
+# 3.7.0 on macOS 10.12 Sierra and 10.13 High Sierra and with
+# Pythonista 3.1 and 3.2 on iOS 10.3, 11.0, 11.1, 11.3 and 11.4.
+
+from glob import glob
+from os import environ, linesep as NL
+from os.path import abspath, dirname, join
+from time import time
+import sys
+
+_test_dir = dirname(abspath(__file__))
+# extend sys.path to include the .. directory
+if _test_dir not in sys.path:
+    sys.path.insert(0, _test_dir)
+
+from base import isiOS, PyGeodesy_dir, Python_O, \
+          runner, secs2str, tilde, versions  # PYCHOK expected
+
+__all__ = ()
+__version__ = '18.08.21'
+
+# command line options
+_failedonly = False
+_raiser     = False
+_results    = False
+_verbose    = False
+
+if __name__ == '__main__':  # MCCABE 28
+
+    def _write(text):
+        _results.write(text.encode('utf-8'))
+
+    argv0, args = tilde(sys.argv[0]), sys.argv[1:]
+
+    if isiOS and not args:
+        # allow this script to be used
+        # with options inside Pythonista
+        try:
+            _input = raw_input  # old name
+        except NameError:  # Python 3+
+            _input = input
+        args = _input('enter %s args: ' % (argv0,)).split()
+
+    while args and args[0].startswith('-'):
+        arg = args.pop(0)
+        if '-help'.startswith(arg):
+            print('usage: %s [-failedonly] [-raiser] [-results] [-verbose] [test/test...py ...]' % (argv0,))
+            sys.exit(0)
+        elif '-failedonly'.startswith(arg):
+            _failedonly = True
+        elif '-raiser'.startswith(arg):
+            _raiser = True  # break on error
+        elif '-results'.startswith(arg):
+            _results = True
+        elif '-verbose'.startswith(arg):
+            _verbose = True
+        else:
+            print('%s invalid option: %s' % (argv0, arg))
+            sys.exit(1)
+
+    # replace home dir with ~
+    Python_O = Python_O.replace(environ.get('HOME', '~'), '~')
+
+    # shorten Python path [-OO]
+    if len(Python_O) > 32:
+        Python_O = Python_O[:16] + '...' + Python_O[-16:]
+
+    # PyGeodesy and Python versions, size, OS name and release
+    v = versions()
+
+#   import pygeodesy
+#   v = ' '.join((v, tilde(pygeodesy.__file__)))
+
+    if _results:  # save all test results
+        t = '-'.join(['testresults'] + v.split()) + '.txt'
+        t = join(PyGeodesy_dir, 'testresults', t)
+        _results = open(t, 'wb')  # note, 'b' not 't'!
+        _write('%s typical test results (%s)%s' % (argv0, v, NL))
+
+    if not args:  # no tests specified, get all test*.py
+        # scripts in the same directory as this one
+        args = sorted(glob(join(_test_dir, 'test[A-Z]*.py')))
+
+    T, X, s = 0, 0, time()
+    for arg in args:
+
+        t = 'running %s %s' % (Python_O, tilde(arg))
+        print(t)
+
+        x, r = runner(arg)
+        X += x  # failures, excluding KNOWN ones
+
+        if _results:
+            _write(NL + t + NL)
+            _write(r)
+
+        # if not X:  # number of tests
+        T += r.count(NL + '    test ')
+
+        if 'Traceback' in r:
+            print(r + NL)
+            if not x:  # count as failure
+                X += 1
+            if _raiser:
+                break
+
+        elif _failedonly:
+            for t in r.split(NL):
+                # print failures, KNOWN ones and totals
+                if 'FAILED,' in t or 'passed' in t:
+                    print(t.rstrip())
+            print('')
+
+        elif _verbose:
+            print(r + NL)
+
+        elif x:
+            for t in r.split(NL):
+                # print failures, without KNOWN ones
+                if 'FAILED,' in t and 'KNOWN' not in t:
+                    print(t.rstrip())
+
+    if X:
+        x = '%d FAILED' % (X,)
+    elif T > 0:
+        x = 'all %s tests OK' % (T,)
+    else:
+        x = 'all OK'
+
+    s = secs2str(time() - s)
+    t = '%s %s %s (%s) %s' % (argv0, Python_O, x, v, s)
+    print(t)
+    if _results:
+        _write(NL + t + NL)
+        _results.close()
