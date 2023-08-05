8655b57506644c2e15d6ebc0d47faa91868e39ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:01 +0000

    Add package rustcsv/0.1.0a9/rustcsv-0.1.0a9-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/rustcsv/rustcsv-0.1.0a9-cp37-cp37m-manylinux1_x86_64.whl/rustcsv/__init__.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+
+from typing import Union, BinaryIO
+
+from .rustcsv import CSVReader, CSVWriter, __build__
+
+try:
+    from ._version import version
+except ImportError:
+    version = "UNKNOWN"
+
+__all__ = ["CSVReader", "CSVWriter", "__build__", "version"]
+
+
+# Can't subclass for better docstrings: https://github.com/PyO3/pyo3/issues/220
+# class CSVReader(_RustCSVReader):
+#     def __new__(
+#         cls,
+#         source: Union[BinaryIO, str],
+#         delimiter: bytes = b",",
+#         terminator: bytes = b"\n",
+#     ):
+#         """
+#
+#         Parameters
+#         ----------
+#         source
+#             :any:`binary file` or string to read CSV from.
+#         delimiter
+#             Byte to use as CSV field delimiter
+#         terminator
+#             Byte to use as CSV record terminator
+#
+#         Returns
+#         -------
+#         CSVReader
+#         """
+#         self = super(CSVReader, cls).__new__(
+#             cls, path_or_fd=source, delimiter=delimiter, terminator=terminator
+#         )
+#
+#         return self
