58b91b648f351f44502d1bd3eac0201dce5f0cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:45 +0000

    Add package NoHotEncoder/0.2/NoHotEncoder-0.2.tar.gz

---
+++ packages/NoHotEncoder/NoHotEncoder-0.2.tar.gz/NoHotEncoder-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='NoHotEncoder',
+    author='Karishnu Poddar',
+    author_email='karishnu@gmail.com',
+    packages=['nohotencoder'],
+    install_requires=['pandas'],
+    version='0.2',
+    license='MIT',
+    description='Converts One Hot Encoding to Integer encoding',
+    long_description=open('README.rst').read()
+)
