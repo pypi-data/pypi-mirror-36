652a2d5e2f161540723f43db9a82d1a821f59fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:04 +0000

    Add package zensols.grsync/0.0.7/zensols.grsync-0.0.7-py3.6.egg

---
+++ packages/zensols.grsync/zensols.grsync-0.0.7-py3.6.egg/zensols/grsync/cli.py
@@ -0,0 +1,51 @@
+from zensols.actioncli import OneConfPerActionOptionsCliEnv
+from zensols.grsync import (
+    AppConfig,
+    DistManager,
+)
+
+
+class ConfAppCommandLine(OneConfPerActionOptionsCliEnv):
+    """Command line entry point.
+
+    """
+    def __init__(self):
+        dist_dir_op = ['-d', '--distdir', False,
+                       {'dest': 'dist_dir',
+                        'metavar': 'DIRECTORY',
+                        'help': 'the location of build out distribution'}]
+        target_dir_op = ['-t', '--targetdir', False,
+                         {'dest': 'target_dir', 'metavar': 'DIRECTORY',
+                          'help': 'the location of build out target dir'}]
+        wheel_dir_op = [None, '--wheeldep', False,
+                        {'dest': 'wheel_dependency',
+                         'default': 'zensols.grsync',
+                         'metavar': 'STRING',
+                         'help': 'used to create the wheel dep files'}]
+        cnf = {'executors':
+               [{'name': 'distribution',
+                 'executor': lambda params: DistManager(**params),
+                 'actions': [{'name': 'info',
+                              'meth': 'discover_info',
+                              'doc': 'pretty print discovery information',
+                              'opts': []},
+                             {'name': 'freeze',
+                              'doc': 'create a distribution',
+                              'opts': [dist_dir_op, wheel_dir_op]},
+                             {'name': 'thaw',
+                              'doc': 'build out a distribution',
+                              'opts': [dist_dir_op, target_dir_op]}]}],
+               'config_option': {'name': 'config',
+                                 'expect': False,
+                                 'opt': ['-c', '--config', False,
+                                         {'dest': 'config', 'metavar': 'FILE',
+                                          'help': 'configuration file'}]},
+               'whine': 1}
+        super(ConfAppCommandLine, self).__init__(
+            cnf, config_env_name='grsyncrc', config_type=AppConfig,
+            pkg_dist='zensols.grsync')
+
+
+def main():
+    cl = ConfAppCommandLine()
+    cl.invoke()
