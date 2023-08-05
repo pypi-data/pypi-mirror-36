7015c07cc0743e0c1c2647526e3b369d427e2a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:39 +0000

    Add package DriverPower/1.0.1/DriverPower-1.0.1-py3-none-any.whl

---
+++ packages/DriverPower/DriverPower-1.0.1-py3-none-any.whl/driverpower/dataIO.py
@@ -0,0 +1,190 @@
+""" Data input and output module for DriverPower.
+
+Input file types: X (tsv), y (tsv), functional scores (tsv), models (pkl)
+
+"""
+import logging
+import pickle
+import os
+import pkg_resources
+import pandas as pd
+import numpy as np
+import warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    import xgboost as xgb
+
+
+logger = logging.getLogger('IO')
+
+
+def read_feature(path, use_features=None):
+    """Read X (features) table in TSV format (or compressed).
+
+    X must contain a column named 'binID' (key) and other columns will be treated as features.
+
+    Args:
+        path (str): Path to the file.
+        use_features (list): List of features to load.
+
+    Returns:
+        pd.df: A panda DF indexed by binID.
+
+    """
+    if path.lower().endswith(('.h5', '.hdf5')):
+        # HDF5
+        if use_features is not None:
+            X = pd.read_hdf(path, 'X')
+            X = X.loc[:, use_features]
+        else:
+            X = pd.read_hdf(path, 'X')
+    elif path.lower().endswith(('.buffer')):
+        # XGBoost binary
+        X = xgb.DMatrix(path)
+    else:
+        # TSV or compressed TSV
+        if use_features is not None:
+            X = pd.read_csv(path, sep='\t', header=0, index_col='binID',
+                            usecols=['binID'] + use_features)
+        else:
+            X = pd.read_csv(path, sep='\t', header=0, index_col='binID')
+    if type(X) is pd.DataFrame:
+        # sanity check
+        assert len(X.index.values) == len(X.index.unique()), "binID in feature table is not unique."
+        na_count = X.isnull().sum()
+        if na_count.sum() > 0:
+            na_names = na_count.index.values[np.where(na_count>0)]
+            logger.warning('NA values found in features [{}]'.format(', '.join(na_names)))
+            logger.warning('Fill NA with 0')
+            X.fillna(0, inplace=True)
+        logger.info('Successfully load {} features for {} bins'.format(X.shape[1], X.shape[0]))
+        return X
+    elif type(X) is xgb.DMatrix:
+        # xgb.DMatrix
+        logger.info('Successfully load {} features for {} bins'.format(X.num_col(), X.num_row()))
+        return X
+
+
+def read_response(path):
+    """Read y (response) table in TSV format.
+    
+    y should have exactly four columns: binID, length, nMut, nSample, N
+    
+    Args:
+        path (str): Path to the file.
+    
+    Returns:
+        pd.df: A panda DF indexed by binID.
+        
+    """
+    y = pd.read_csv(path, sep='\t', header=0, index_col='binID',
+                    usecols=['binID', 'length', 'nMut', 'nSample', 'N'])
+    # sanity check
+    assert len(y.index.values) == len(y.index.unique()), "binID in response table is not unique."
+    return y
+
+
+def read_fs(path, fs_cut):
+    """Read functional scores table in TSV format.
+    
+    func_score must contain a column named 'binID' (key) and other columns will be treated as scores (one per column).
+    
+    Args:
+        path (str): Path to the file.
+        fs_cut (dict): Keys are functional score names.
+    
+    Returns:
+        pd.df: A panda DF indexed by binID
+         
+    """
+    fs = pd.read_csv(path, sep='\t', header=0, index_col='binID',
+                     usecols=['binID'] + list(fs_cut.keys()))
+    # sanity check
+    assert len(fs.index.values) == len(fs.index.unique()), "binID in functional score table is not unique."
+    return fs
+
+
+def read_fi(path, cutoff=0.5):
+    """Read feature importance table in TSV format.
+
+    Feature importance table must contain two columns: name and importance
+
+    Args:
+        path (str): path to the file.
+        cutoff (float): cutoff of feature selection.
+
+    Returns:
+        list: useful features. Return None if path is None.
+
+    """
+    if path is not None:
+        fi = pd.read_csv(path, sep='\t', header=0, index_col='name',
+                         usecols=['name', 'importance'])
+        assert len(fi.index.values) == len(fi.index.unique()), \
+            "Feature name in feature importance table is not unique."
+        keep = (fi.importance >= cutoff).values
+        return fi.index.values[keep].tolist()
+    else:
+        return None
+
+
+def save_fi(fi_scores, feature_names, project_name, out_dir):
+    """ Save feature importance results to disk
+    
+    Args:
+        fi_scores (np.array): array of feature importance scores.
+        feature_names (np.array): array of feature names.
+        project_name (str): name of the project, prefix of the output file.
+        out_dir (str): output directory. 
+
+    Returns:
+        pd.DF: feature importance table
+        
+    """
+    res = pd.DataFrame({'name':feature_names, 'importance':fi_scores}, columns=['name', 'importance'])
+    path = os.path.join(out_dir, project_name+'.feature_importance.tsv')
+    res.to_csv(path, sep='\t', index=False)
+    return res
+
+
+def read_param(path=None):
+    """ Load the parameter for xgboost.
+
+    Args:
+        path: path to the pkl file. None for default.
+
+    Returns:
+        dict: parameter dict.
+    """
+    # use default if path is None
+    path = path if path is not None else pkg_resources.resource_filename(__name__, 'xgb_param.pkl')
+    with open(path, 'rb') as f:
+        param = pickle.load(f)
+    return param
+
+
+def save_prediction(ypred, y, project_name, out_dir, model_name):
+    y['pred'] = ypred
+    path = os.path.join(out_dir, '{}.{}.model.train.pred.tsv'.format(project_name, model_name))
+    y.to_csv(path, sep='\t')
+    return
+
+
+def read_model(path):
+    with open(path, 'rb') as f:
+        model = pickle.load(f)
+    return model
+
+
+def save_model(model, project_name, out_dir, model_name):
+    path = os.path.join(out_dir, '{}.{}.model.pkl'.format(project_name, model_name))
+    with open(path, 'wb') as f:
+        pickle.dump(model, f)
+
+
+def save_result(y, project_name, out_dir):
+    # sort by last but 2 column
+    y = y.sort_values(y.columns[-2], ascending=True)
+    path = os.path.join(out_dir, '{}.result.tsv'.format(project_name))
+    y.to_csv(path, sep='\t')
---
+++ packages/DriverPower/DriverPower-1.0.1-py3-none-any.whl/driverpower/interface.py
@@ -0,0 +1,151 @@
+""" Command-line interface
+
+Sub-commands:
+    1. model - train the BMR model.
+    2. infer - test for driver elements.
+"""
+
+
+import logging
+import argparse
+import os
+import sys
+from driverpower import __version__
+from driverpower.model import run_bmr
+from driverpower.infer import make_inference
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                    format='%(asctime)s | %(levelname)s: %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S')
+# create logger
+logger = logging.getLogger('DP')
+
+
+def get_args():
+    class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+                          argparse.MetavarTypeHelpFormatter,
+                          argparse.RawDescriptionHelpFormatter):
+        pass
+    parser = argparse.ArgumentParser(prog='driverpower',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description='DriverPower v{}: Combined burden and functional impact '
+                                                 'tests for coding and non-coding cancer driver discovery.\n\n'
+                                                 'See documentation and examples at '
+                                                 'http://driverpower.readthedocs.io/en/latest/'.format(__version__))
+    # global argument
+    parser.add_argument('-v', '--version', dest='version', action="store_true",
+                        help='Print the version of DriverPower')
+    subparsers = parser.add_subparsers(title='The DriverPower sub-commands include',
+                                       dest='subcommand')
+    #
+    # Background model
+    #
+    parser_bmr = subparsers.add_parser('model',
+                                       help='Build the background mutation model',
+                                       formatter_class=CustomFormatter,
+                                       description='DriverPower v{}: Combined burden and functional impact '
+                                                   'tests for coding and non-coding cancer driver discovery.\n\n'
+                                                   'See documentation and examples at '
+                                                   'http://driverpower.readthedocs.io/en/latest/'.format(__version__))
+    # Input data
+    dat_bmr = parser_bmr.add_argument_group(title="input data")
+    dat_bmr.add_argument('--feature', dest='X_path', required=True, type=str,
+                         help='Path to the training feature table')
+    dat_bmr.add_argument('--response', dest='y_path', required=True, type=str,
+                         help='Path to the training response table')
+    dat_bmr.add_argument('--featImp', dest='fi_path', required=False, type=str,
+                         help='Path to the feature importance table [optional]', default=None)
+    # Parameters
+    par_bmr = parser_bmr.add_argument_group(title="parameters")
+    par_bmr.add_argument('--method', dest='model_name', required=True, type=str,
+                         help='Algorithms to use', choices=['Binomial', 'NegativeBinomial', 'GBM'])
+    par_bmr.add_argument('--featImpCut', dest='fi_cut', required=False, type=float,
+                         help='Cutoff of feature importance score [optional]', default=0.5)
+    par_bmr.add_argument('--gbmParam', dest='param_path', required=False, type=str,
+                         help='Path to the parameter pickle [optional]', default=None)
+    par_bmr.add_argument('--gbmFold', dest='kfold', required=False, type=int,
+                         help='Train gbm with k-fold, k>=2 [optional]', default=3)
+    par_bmr.add_argument('--predict', dest='pred', required=False, action="store_true",
+                         help='Output the prediction for training set [optional]')
+    par_bmr.add_argument('--name', dest='project_name', required=False, type=str,
+                         help='Identifier for output files [optional]', default='DriverPower')
+    par_bmr.add_argument('--modelDir', dest='out_dir', type=str,
+                         help='Directory of output model files [optional]', default='./output/')
+    #
+    # Inference
+    #
+    parser_infer = subparsers.add_parser('infer',
+                                         help='Infer driver elements',
+                                         formatter_class=CustomFormatter,
+                                         description = 'DriverPower v{}: Combined burden and functional impact '
+                                                       'tests for coding and non-coding cancer driver discovery.\n\n'
+                                                       'See documentation and examples at '
+                                                       'http://driverpower.readthedocs.io/en/latest/'.format(__version__))
+    # Input data
+    dat_infer = parser_infer.add_argument_group(title="input data")
+    dat_infer.add_argument('--feature', dest='X_path', required=True, type=str,
+                           help='Path to the test feature table')
+    dat_infer.add_argument('--response', dest='y_path', required=True, type=str,
+                           help='Path to the test response table')
+    dat_infer.add_argument('--model', dest='model_path', required=True, type=str,
+                           help='Path to the model pickle')
+    dat_infer.add_argument('--funcScore', dest='fs_path', required=False, type=str,
+                           help='Path to the functional score table [optional]', default=None)
+    # Parameters
+    par_infer = parser_infer.add_argument_group(title="parameters")
+    par_infer.add_argument('--method', dest='test_method', required=False, type=str,
+                           help='Test method to use [optional]', choices=['auto', 'binomial', 'negative_binomial'], default='auto')
+    par_infer.add_argument('--scale', dest='scale', required=False, type=float,
+                           help='Scaling factor for theta in negative binomial distribution [optional]', default=1)
+    par_infer.add_argument('--funcScoreCut', dest='fs_cut', required=False, type=str,
+                           help='Score name:cutoff pairs for all scores e.g.,'
+                                '"CADD:0.01,DANN:0.03,EIGEN:0.03" [optional]',
+                           default=None)
+    par_infer.add_argument('--geoMean', dest='use_gmean', required=False, type=bool,
+                           help='Use geometric mean in test [optional]', default=True)
+    par_infer.add_argument('--name', dest='project_name', required=False, type=str,
+                           help='Identifier for output files [optional]', default=None)
+    par_infer.add_argument('--outDir', dest='out_dir', type=str,
+                           help='Directory of output files [optional]', default='./output/')
+    args = parser.parse_args()
+    ###
+    # Check and modify args
+    ###
+    if len(sys.argv) == 1:
+        # print help when no argument
+        parser.print_help()
+        sys.exit(1)
+    if hasattr(args, 'out_dir'):
+        args.out_dir = os.path.abspath(args.out_dir)
+    return args
+
+
+def main():
+    args = get_args()
+    logger.info('Welcome to DriverPower v{}'.format(__version__))
+    if args.subcommand == 'model':
+        run_bmr(model_name=args.model_name,
+                X_path=args.X_path,
+                y_path=args.y_path,
+                fi_cut=args.fi_cut,
+                fi_path=args.fi_path,
+                kfold=args.kfold,
+                save_pred=args.pred,
+                param_path=args.param_path,
+                project_name=args.project_name,
+                out_dir=args.out_dir)
+    elif args.subcommand == 'infer':
+        make_inference(model_path=args.model_path,
+                       X_path=args.X_path,
+                       y_path=args.y_path,
+                       fs_path=args.fs_path,
+                       fs_cut=args.fs_cut,
+                       test_method=args.test_method,
+                       scale=args.scale,
+                       use_gmean=args.use_gmean,
+                       project_name=args.project_name,
+                       out_dir=args.out_dir)
+
+
+if __name__ == '__main__':
+    main()
