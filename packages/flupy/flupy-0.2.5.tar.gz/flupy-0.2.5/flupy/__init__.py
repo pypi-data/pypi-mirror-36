4e22887b502e6b3d5f222b27a8a8f722ac791aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:05 +0000

    Add package flupy/0.2.5/flupy-0.2.5.tar.gz

---
+++ packages/flupy/flupy-0.2.5.tar.gz/flupy-0.2.5/flupy/__init__.py
@@ -0,0 +1,4 @@
+__project__ = 'flupy'
+__version__ = '0.2.5'
+
+from flupy.fluent import flu, map_item, map_attr, as_flu, with_iter
---
+++ packages/flupy/flupy-0.2.5.tar.gz/flupy-0.2.5/flupy/cli/cli.py
@@ -0,0 +1,42 @@
+import argparse
+import importlib
+import sys
+from signal import SIG_DFL, SIGPIPE, signal
+
+from flupy import flu, with_iter
+from flupy.cli.lazy_import import csv, json, math, os, random, re, statistics, collections, itertools
+from flupy.cli.utils import walk_files, walk_dirs
+
+
+def read_file(filename):
+    with open(filename, 'r') as f:
+        yield from f
+
+parser = argparse.ArgumentParser(description='flupy: a fluent interface for python')
+parser.add_argument('command', help='flupy command to execute on input')
+parser.add_argument('-f', '--file', help='path to input file')
+
+
+def main():
+    args = parser.parse_args()
+
+    _command = args.command
+    _file = args.file
+
+    if _file:
+        _ = flu(read_file(_file)).map(str.rstrip)
+    else:
+        # Do not raise exception for Broken Pipe
+        signal(SIGPIPE, SIG_DFL)
+        _ = flu(sys.stdin).map(str.rstrip)
+
+    pipeline = eval(_command)
+
+    if hasattr(pipeline, '__iter__') and not isinstance(pipeline, (str, bytes)):
+        for r in pipeline:
+            sys.stdout.write(str(r) + '\n')
+
+    elif pipeline is None:
+        pass
+    else:
+        sys.stdout.write(str(pipeline) + '\n')
