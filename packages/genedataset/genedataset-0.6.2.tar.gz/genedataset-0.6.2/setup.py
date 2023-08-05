d4b8099db685cc21310b64b478aedc427ce96644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:55 +0000

    Add package genedataset/0.6.2/genedataset-0.6.2.tar.gz

---
+++ packages/genedataset/genedataset-0.6.2.tar.gz/genedataset-0.6.2/setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+requires = [
+    'pandas',
+    ]
+
+setup(name='genedataset',
+      version='0.6.2',
+      description='Store and access gene expression datasets and gene definitions.',
+      long_description=README,
+      classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        ],
+      author='Jarny Choi',
+      author_email='jarny.choi@gmail.com',
+      url='https://github.com/jarny/genedataset',
+      keywords='gene microarray rna-seq',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      )
