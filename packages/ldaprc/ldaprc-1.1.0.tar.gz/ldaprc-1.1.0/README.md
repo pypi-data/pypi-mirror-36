2e3ac43d6c93660096ef283134d82b5b4baa90a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:08 +0000

    Add package ldaprc/1.1.0/ldaprc-1.1.0.tar.gz

---
+++ packages/ldaprc/ldaprc-1.1.0.tar.gz/ldaprc-1.1.0/README.md
@@ -0,0 +1,128 @@
+# OpenLDAP Settings Library #
+
+This small library allows you to use your system’s existing OpenLDAP-style configuration settings with Python libraries like [ldap3][1] and [Python-LDAP][2].
+
+```python
+>>> from ldaprc import LDAPRC
+>>> conf = LDAPRC()
+>>> conf.uri
+ldaps://ldap.example.com
+```
+
+## Why? ##
+
+If all of your Python tools use this library to obtain LDAP settings, you will…
+
+  * Avoid maintaining identical settings in multiple places
+  * Always know that your Python tools are acting on the same data as `ldapsearch`, `ldapmodify`, etc.
+
+## Settings Discovery ##
+
+We follow the same rules for discovering settings as the OpenLDAP library by checking in this order (from least to most specific):
+
+  * System-wide configuration files (such as `/etc/openldap/ldap.conf`)
+  * The current user’s configuration file (`~/.ldaprc`)
+  * An `ldaprc` file in the current working directory or a parent
+  * Files referenced by the `LDAPCONF` and `LDAPRC` environment variables
+  * Environment variables starting with “LDAP” (like `LDAPURI`)
+
+More specific settings replace any that were previously discovered. All of the above steps are skipped if the `LDAPNOINIT` variable is set.
+
+You can circumvent the discovery process by explicitly providing a configuration file, causing all other files and environment variables to be ignored.
+
+```python
+>>> from ldaprc import LDAPRC
+>>> conf = LDAPRC('/path/to/file')
+```
+
+This can be useful for testing, or for applications that should not inherit anything from the rest of the system.
+
+## Accessing Settings ##
+
+All discovered settings are made available as case-insensitive properties of your `LDAPRC` object.
+
+```python
+>>> conf.base
+dc=example,dc=com
+>>> conf.BASE
+dc=example,dc=com
+```
+
+## Multiple URIs ##
+
+OpenLDAP allows the URI to contain multiple values separated by whitespace.
+
+```
+URI ldap01.example.com ldap02.example.com
+```
+
+The ldap3 library won’t accept this as a valid URI, so to be safe, the `uri` attribute of an LDAPRC object will always be a string representing the first URI listed.
+
+```python
+>>> conf.uri
+ldap01.example.com
+```
+
+Use the `uri_list` attribute to get the list of all URIs discovered.
+
+```python
+>>> conf.uri_list
+['ldap01.example.com', 'ldap02.example.com']
+```
+
+## Troubleshooting ##
+
+Since there are so many possible sources for these settings, you can find out where each came from (when you see unexpected/incorrect values, etc).
+
+```python
+>>> conf.binddn
+uid=wrong_guy,ou=people,dc=example,dc=com
+>>> conf.explain('binddn')
+BINDDN: Using value from LDAPBINDDN environment variable
+```
+
+To see everything, just call `explain()` with no arguments.
+
+```python
+>>> conf.explain()
+BASE: Using value from /etc/openldap/ldap.conf
+BINDDN: Using value from LDAPBINDDN environment variable
+SASL_MECH: Using value from /home/you/.ldaprc
+TLS_CACERTDIR: Using value from /home/you/.ldaprc
+TLS_REQCERT: Using value from /etc/openldap/ldap.conf
+URI: Using value from /etc/openldap/ldap.conf
+```
+
+## Examples ##
+
+Look up a user’s e-mail address (using the ldap3 library).
+
+```python
+from ldap3 import Connection, Server
+from ldaprc import LDAPRC
+
+
+conf = LDAPRC()
+server = Server(conf.uri)
+with Connection(server) as conn:
+    conn.search(
+        conf.base,
+        '(&(objectClass=posixAccount)(uid=rob))',
+        attributes=['mail'],
+    )
+    if conn.entries:
+        print(conn.entries[0].mail.value)
+```
+
+## Installation ##
+
+```console
+$ pip install ldaprc
+```
+
+## Requirements ##
+
+Python 2 or 3
+
+[1]: https://github.com/cannatag/ldap3
+[2]: https://www.python-ldap.org/
---
+++ packages/ldaprc/ldaprc-1.1.0.tar.gz/ldaprc-1.1.0/ldaprc.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+ldaprc.py
+
+Created by Rob McBroom
+"""
+import os
+import re
+
+from collections import namedtuple
+
+
+LDAPSetting = namedtuple(
+    'LDAPSetting',
+    ('raw_name', 'value', 'source'),
+)
+
+
+class LDAPRC(object):
+    """Parse various 'ldaprc' files and return the settings."""
+    def __init__(self, ldaprc=None):
+        self.ldaprc = ldaprc
+        conf_file_env = ('LDAPCONF', 'LDAPRC')
+        if ldaprc is None and 'LDAPNOINIT' not in os.environ:
+            # no ldaprc given, fall back on defaults
+            # settings in later files will take precedence over earlier files
+            ldaprc_files = [
+                '/etc/openldap/ldap.conf',            # Red Hat, macOS
+                '/etc/ldap/ldap.conf',                # Debian and friends
+                '/usr/local/etc/openldap/ldap.conf',  # FreeBSD
+                os.getenv('HOME', '') + '/.ldaprc',   # User
+            ]
+            # look for ldaprc files in this, or a parent directory
+            path = os.getcwd()
+            while path:
+                if 'ldaprc' in os.listdir(path):
+                    ldaprc_path = path + os.path.sep + 'ldaprc'
+                    if os.path.isfile(ldaprc_path):
+                        ldaprc_files.append(ldaprc_path)
+                keep_going = (path != os.path.dirname(path))
+                path = os.path.dirname(path) if keep_going else None
+            # look for a file referenced by environment variables
+            for conf_var in conf_file_env:
+                if conf_var in os.environ:
+                    ldaprc_files.append(os.environ[conf_var])
+        else:
+            # use only the user-specified ldaprc if present
+            ldaprc_files = (ldaprc,)
+
+        self._settings = {}
+        setting_pattern = re.compile(r'^([a-z]\w+)\s+(.*)', flags=re.I)
+        # check each available file for settings
+        # for settings found in multiple files, later files take precedence
+        for rc_file in ldaprc_files:
+            if os.path.exists(rc_file):
+                for rc_line in open(rc_file):
+                    rc_line = rc_line.strip()
+                    m = setting_pattern.match(rc_line)
+                    if m:
+                        # this looks like a valid setting
+                        setting_key = m.group(1)
+                        setting_value = m.group(2)
+                        self._settings[setting_key.lower()] = LDAPSetting(
+                            setting_key, setting_value, rc_file,
+                        )
+        # check environment variables, which trump everything
+        # unless the user provided a conf file
+        if self.ldaprc is not None or 'LDAPNOINIT' in os.environ:
+            return
+        ldap_env_vars = [v for v in os.environ if v.startswith('LDAP')]
+        for rc_var in ldap_env_vars:
+            if rc_var in conf_file_env:
+                # these don't represent individual settings
+                continue
+            setting_key = rc_var[4:]
+            setting_value = os.environ[rc_var]
+            self._settings[setting_key.lower()] = LDAPSetting(
+                setting_key, setting_value,
+                '{} environment variable'.format(rc_var),
+            )
+        # handle multiple URIs
+        raw_uri = self._settings.get('uri')
+        if raw_uri:
+            uri_list = raw_uri.value.split()
+            self._settings['uri_list'] = LDAPSetting(
+                'URI List', uri_list, raw_uri.source
+            )
+            if len(uri_list) > 1:
+                uri = uri_list[0]
+                self._settings['uri'] = LDAPSetting(
+                    raw_uri.raw_name, uri, raw_uri.source
+                )
+
+    def __getattr__(self, setting):
+        if setting.lower() in self._settings:
+            return self._settings[setting.lower()].value
+        raise AttributeError("no value defined for '{}'".format(setting))
+
+    def __repr__(self):
+        if self.ldaprc is None:
+            return 'LDAPRC()'
+        return "LDAPRC('{}')".format(self.ldaprc)
+
+    def explain(self, setting=None):
+        """show where each setting came from"""
+        if setting is not None and getattr(self, setting):
+            explain_settings = (self._settings[setting.lower()],)
+        else:
+            explain_settings = sorted(
+                self._settings.values(),
+                key=lambda s: s.raw_name.lower(),
+            )
+        for data in explain_settings:
+            print('{}: Using value from {}'.format(data.raw_name, data.source))
