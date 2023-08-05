ef3f2e5dd01a5d3358c72258ea8d36f948365037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:05 +0000

    Add package readthecode/0.1.4/readthecode-0.1.4.tar.gz

---
+++ packages/readthecode/readthecode-0.1.4.tar.gz/readthecode-0.1.4/main.py
@@ -0,0 +1,42 @@
+import importlib
+
+# import os
+import sys
+import webbrowser
+
+# TODO allow user set binary to open
+# TODO display options and allow user use
+# we want to open in a web browser, maybe as option
+browsers = [
+    "firefox",
+    "mozilla",
+    "google-chrome",
+    "chrome",
+    "chromium",
+    "chromium-browser",
+    # sorry IE
+]
+
+
+for w in webbrowser._tryorder:
+    if w not in browsers[:]:
+        browsers.append(w)
+
+webbrowser._tryorder = browsers
+print(webbrowser._tryorder)
+
+
+def cli():
+    libname = sys.argv[1]
+    lib = importlib.import_module(libname)
+
+    try:
+        path = lib.__path__[0]
+    except AttributeError:
+        path = lib.__file__
+
+    # TODO make this an option
+    # if os.path.isfile(path):
+    #     path = os.path.dirname(path)
+    print("Opening {} ".format(path))
+    webbrowser.open_new_tab("file://{}".format(path))
