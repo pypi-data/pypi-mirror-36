d9ac606f82e96fea7f937016a4aa8219894cd303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:18 +0000

    Add package gabapi/0.0.2/gabapi-0.0.2.tar.gz

---
+++ packages/gabapi/gabapi-0.0.2.tar.gz/gabapi-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gabapi',
+      version='0.0.2',
+      description='Provides access to Gab REST API',
+      url='http://github.com/undeadmockingbird/AltTech',
+      author='Undead Mockingbird',
+      author_email='undeadmockingbird@protonmail.com',
+      license='MIT',
+      packages=['gabapi'],
+      zip_safe=False)
