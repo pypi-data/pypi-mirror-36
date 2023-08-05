674e4d3bec235421c955f0e821b0937bee5b01cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:30 +0000

    Add package guniflask/0.2.3/guniflask-0.2.3.tar.gz

---
+++ packages/guniflask/guniflask-0.2.3.tar.gz/guniflask-0.2.3/guniflask/utils/model.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+import sqlalchemy
+
+
+def model_to_dict(model):
+    col_attrs = sqlalchemy.inspect(model).mapper.column_attrs
+    return {c.key: getattr(model, c.key) for c in col_attrs}
+
+
+def dict_to_model(dict_obj, model_cls=None):
+    if model_cls is None:
+        return dict(dict_obj)
+    col_attrs = sqlalchemy.inspect(model_cls).mapper.column_attrs
+    kwargs = {c.key: dict_obj[c.key] for c in col_attrs if c.key in dict_obj}
+    model = model_cls(**kwargs)
+    return model
+
+
+def update_model_by_dict(model, dict_obj):
+    col_attrs = sqlalchemy.inspect(model).mapper.column_attrs
+    for c in col_attrs:
+        if c.key in dict_obj:
+            setattr(model, c.key, dict_obj[c.key])
