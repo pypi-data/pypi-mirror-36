a793eff3efd722d45a990a87a487e1e8f1ed233e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:15 +0000

    Add package GISIC/0.2/GISIC-0.2.tar.gz

---
+++ packages/GISIC/GISIC-0.2.tar.gz/GISIC-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name="GISIC",
+      version='0.2',
+      description="Gaussian Inflection Spline Interpolation Continuum",
+      author='Devin D. Whitten',
+      author_email='dwhitten@nd.edu',
+      license="ND",
+      packages=['GISIC'],
+      zip_safe=False
+)
