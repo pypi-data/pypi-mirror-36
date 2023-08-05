f28b8a0a794f5d27e6a896f7cf50e97c1e18a96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:14 +0000

    Add package officegenerator/0.13.0/officegenerator-0.13.0.tar.gz

---
+++ packages/officegenerator/officegenerator-0.13.0.tar.gz/officegenerator-0.13.0/officegenerator/__init__.py
@@ -0,0 +1,8 @@
+## @package officegenerator
+## @brief Generate office files with predefined styles
+from officegenerator.libodfgenerator import Color, ColumnWidthODS, ODS_Read, ODS_Write,  ODT,  OdfCell,  OdfSheet
+from officegenerator.libxlsxgenerator import OpenPyXL,  ColumnWidthXLSX
+from officegenerator.commons import *
+from officegenerator.tests import *
+
+
