5b6d012559c17292ad853b1f7a4d31488bcb313f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:17 +0000

    Add package pylas/0.3.3/pylas-0.3.3.tar.gz

---
+++ packages/pylas/pylas-0.3.3.tar.gz/pylas-0.3.3/pylas/compression.py
@@ -0,0 +1,153 @@
+""" The functions related to the LAZ format (compressed LAS)
+Lazperf is made optional by catching the ModuleNotFoundError, and raising an exception
+when compression/decompression is actually needed
+
+There are also functions to use Laszip (meant to be used as a fallback)
+"""
+import os
+import subprocess
+
+import numpy as np
+
+from .errors import LazPerfNotFound, PylasError
+
+HAS_LAZPERF = False
+
+try:
+    import lazperf
+
+    HAS_LAZPERF = True
+    # we should capture ModuleNotFoundError but it's a python3.6 exception type
+    # and ReadTheDocs uses 3.5
+except:
+    HAS_LAZPERF = False
+
+
+def raise_if_no_lazperf():
+    if not HAS_LAZPERF:
+        raise LazPerfNotFound("Cannot manipulate laz data")
+    elif lazperf.__version__ < "1.3.0":
+        raise LazPerfNotFound(
+            "Version >= 1.3.0 required, you have {}".format(lazperf.__version__)
+        )
+
+
+def is_point_format_compressed(point_format_id):
+    compression_bit_7 = (point_format_id & 0x80) >> 7
+    compression_bit_6 = (point_format_id & 0x40) >> 6
+    if not compression_bit_6 and compression_bit_7:
+        return True
+    return False
+
+
+def compressed_id_to_uncompressed(point_format_id):
+    return point_format_id & 0x3f
+
+
+def uncompressed_id_to_compressed(point_format_id):
+    return (2 ** 7) | point_format_id
+
+
+def decompress_buffer(compressed_buffer, points_dtype, point_count, laszip_vlr):
+    raise_if_no_lazperf()
+
+    point_compressed = np.frombuffer(compressed_buffer, dtype=np.uint8)
+
+    vlr_data = np.frombuffer(laszip_vlr.record_data, dtype=np.uint8)
+    decompressor = lazperf.VLRDecompressor(
+        point_compressed, points_dtype.itemsize, vlr_data
+    )
+
+    point_uncompressed = decompressor.decompress_points(point_count)
+
+    point_uncompressed = np.frombuffer(
+        point_uncompressed, dtype=points_dtype, count=point_count
+    )
+
+    return point_uncompressed
+
+
+def create_laz_vlr(points_record):
+    raise_if_no_lazperf()
+    record_schema = lazperf.RecordSchema()
+
+    if points_record.point_format.id >= 6:
+        raise PylasError("Can't compress points with format id >= 6")
+    record_schema.add_point()
+
+    if "gps_time" in points_record.dimensions_names:
+        record_schema.add_gps_time()
+
+    if "red" in points_record.dimensions_names:
+        record_schema.add_rgb()
+
+    num_extra_bytes = points_record.point_format.num_extra_bytes
+    if num_extra_bytes > 0:
+        record_schema.add_extra_bytes(num_extra_bytes)
+    elif num_extra_bytes < 0:
+        raise PylasError(
+            "Incoherent number of extra bytes ({})".format(num_extra_bytes)
+        )
+
+    return lazperf.LazVLR(record_schema)
+
+
+def compress_buffer(uncompressed_buffer, record_schema, offset):
+    raise_if_no_lazperf()
+
+    compressor = lazperf.VLRCompressor(record_schema, offset)
+    uncompressed_buffer = np.frombuffer(uncompressed_buffer, dtype=np.uint8)
+    compressed = compressor.compress(uncompressed_buffer)
+
+    return compressed
+
+
+def find_laszip_executable():
+    laszip_names = ("laszip", "laszip.exe", "laszip-cli", "laszip-cli.exe")
+
+    for binary in laszip_names:
+        in_path = (
+            os.path.isfile(os.path.join(x, binary))
+            for x in os.environ["PATH"].split(os.pathsep)
+        )
+        if any(in_path):
+            return binary
+
+    else:
+        return None
+
+
+def _pass_through_laszip(stream, action="decompress"):
+    laszip_binary = find_laszip_executable()
+    if laszip_binary is None:
+        raise FileNotFoundError("Could not find laszip executable")
+
+    if action == "decompress":
+        out_t = "-olas"
+    elif action == "compress":
+        out_t = "-olaz"
+    else:
+        raise ValueError("Invalid Action")
+
+    prc = subprocess.Popen(
+        [laszip_binary, "-stdin", out_t, "-stdout"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    data, stderr = prc.communicate(stream.read())
+    if prc.returncode != 0:
+        raise RuntimeError(
+            "Laszip failed to {} with error code {}\n\t{}".format(
+                action, prc.returncode, "\n\t".join(stderr.decode().splitlines())
+            )
+        )
+    return data
+
+
+def laszip_compress(stream):
+    return _pass_through_laszip(stream, action="compress")
+
+
+def laszip_decompress(stream):
+    return _pass_through_laszip(stream, action="decompress")
---
+++ packages/pylas/pylas-0.3.3.tar.gz/pylas-0.3.3/pylas/lasdatas/las14.py
@@ -0,0 +1,57 @@
+from .base import LasBase
+from .. import evlrs
+from .. import extradims
+from ..utils import ctypes_max_limit
+from ..vlrs.known import ExtraBytesVlr, ExtraBytesStruct
+
+
+class LasData(LasBase):
+    def __init__(self, *, header=None, vlrs=None, points=None, evlrs=None):
+        super().__init__(header=header, vlrs=vlrs, points=points)
+        self.evlrs = [] if evlrs is None else evlrs
+
+    def update_header(self):
+        super().update_header()
+        if len(self.vlrs.get("WktCoordinateSystemVlr")) == 1:
+            self.header.global_encoding.wkt = 1
+
+    def add_extra_dim(self, name, type, description=""):
+        name = name.replace(" ", "_")
+        type_id = extradims.get_id_for_extra_dim_type(type)
+        extra_byte = ExtraBytesStruct(
+            data_type=type_id, name=name.encode(), description=description.encode()
+        )
+
+        try:
+            extra_bytes_vlr = self.vlrs.get("ExtraBytesVlr")[0]
+        except IndexError:
+            extra_bytes_vlr = ExtraBytesVlr()
+            self.vlrs.append(extra_bytes_vlr)
+        finally:
+            extra_bytes_vlr.extra_bytes_structs.append(extra_byte)
+            self.points_data.add_extra_dims([(name, type)])
+
+    def write_to(self, out_stream, do_compress=False):
+        if do_compress and self.points_data.point_format >= 6:
+            raise NotImplementedError(
+                "LazPerf cannot compress 1.4 files with point format >= 6"
+            )
+
+        start = out_stream.tell()
+        self.header.start_of_waveform_data_packet_record = 0
+
+        if len(self.points_data) > ctypes_max_limit(
+            self.header.__class__.legacy_point_count.size
+        ):
+            self.header.legacy_point_count = 0
+        else:
+            self.header.legacy_point_count = len(self.points_data)
+        super().write_to(out_stream, do_compress=do_compress)
+
+        raw_evlrs = evlrs.RawEVLRList.from_list(self.evlrs)
+        if len(self.evlrs) > 0:
+            self.header.start_of_first_evlr = out_stream.tell()
+            self.header.number_of_evlr = len(raw_evlrs)
+        raw_evlrs.write_to(out_stream)
+        out_stream.seek(start)
+        self.header.write_to(out_stream)
