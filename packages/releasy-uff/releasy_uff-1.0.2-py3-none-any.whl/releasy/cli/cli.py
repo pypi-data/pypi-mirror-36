2f256ba775655239fbfd411bbf79de15a380c9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:31 +0000

    Add package releasy-uff/1.0.2/releasy_uff-1.0.2-py3-none-any.whl

---
+++ packages/releasy-uff/releasy_uff-1.0.2-py3-none-any.whl/releasy/cli/cli.py
@@ -0,0 +1,15 @@
+import argparse
+
+class Cli(object):
+    '''Command Line Interface superclass'''
+    def __init__(self, parser):
+        self.parser = parser
+
+    def parse(self, argv):
+        self.args = self.parser.parse_args(argv)
+
+    def run(self):
+        pass
+
+    def getArgs(self):
+        return self.args
