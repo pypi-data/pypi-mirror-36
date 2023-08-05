e4c30538ff71ddef3c1f89f600f14985f517215e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package openedx2zim/0.5.0/openedx2zim-0.5.0.tar.gz

---
+++ packages/openedx2zim/openedx2zim-0.5.0.tar.gz/openedx2zim-0.5.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
+#from pip.req import parse_requirements
+
+setup(
+    name='openedx2zim',
+    version='0.5.0',
+    description="Make zimfile from open edx MOOCs",
+    long_description=open('README.md').read(),
+    author='dattaz',
+    author_email='taz@dattaz.fr',
+    url='http://github.com/kiwix/openedx',
+    keywords="kiwix zim openedx edx offline",
+    license="GPL",
+    #packages=find_packages('.'),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    package_dir={'openedxtozim': 'openedxtozim'},
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'docopt',
+        'bs4',
+        'lxml',
+        'webvtt-py',
+        'youtube-dl',
+        'python-slugify',
+        'jinja2',
+        'mistune',
+        'requests',
+        'iso-639'
+        ],
+    platforms='Linux',
+    scripts=['openedx2zim'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4'
+    ],
+)
