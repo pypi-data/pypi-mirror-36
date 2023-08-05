0734db2871048e2f018910f4bf2ac79835ff95d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:55 +0000

    Add package crython/0.0.9/crython-0.0.9.tar.gz

---
+++ packages/crython/crython-0.0.9.tar.gz/crython-0.0.9/crython/__init__.py
@@ -0,0 +1,18 @@
+"""
+    crython
+    ~~~~~~~
+
+    Lightweight task scheduler using cron expressions.
+
+    :copyright: (c) 2013 Andrew Hawker.
+    :license: MIT, see LICENSE for more details.
+"""
+
+from .job import job
+from .tab import start, stop
+
+
+__all__ = ['job', 'start', 'stop']
+
+
+__version__ = '0.0.9'
