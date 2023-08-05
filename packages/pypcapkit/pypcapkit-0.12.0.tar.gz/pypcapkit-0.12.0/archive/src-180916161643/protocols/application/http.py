995cd069de55cba667dd28028012e6c12bc81b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:14 +0000

    Add package pypcapkit/0.12.0/pypcapkit-0.12.0.tar.gz

---
+++ packages/pypcapkit/pypcapkit-0.12.0.tar.gz/pypcapkit-0.12.0/archive/src-180916161643/protocols/application/http.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""hypertext transfer protocol
+
+`pcapkit.protocols.application.http` contains `HTTP`
+only, which is a base class for Hypertext Transfer
+Protocol (HTTP) protocol family, eg. HTTP/1.*, HTTP/2.
+
+"""
+import chardet
+
+from pcapkit.corekit.infoclass import Info
+from pcapkit.corekit.protochain import ProtoChain
+from pcapkit.protocols.null import NoPayload
+from pcapkit.protocols.application.application import Application
+from pcapkit.utilities.exceptions import UnsupportedCall, ProtocolError
+
+
+__all__ = ['HTTP']
+
+
+class HTTP(Application):
+    """This class implements all protocols in HTTP family.
+
+    - Hypertext Transfer Protocol (HTTP/1.1) [RFC 7230]
+    - Hypertext Transfer Protocol version 2 (HTTP/2) [RFC 7540]
+
+    Properties:
+        * name -- str, name of corresponding procotol
+        * info -- Info, info dict of current instance
+        * alias -- str, acronym of corresponding procotol
+        * layer -- str, `Application`
+        * protocol -- str, name of next layer protocol
+        * protochain -- ProtoChain, protocol chain of current instance
+
+    Methods:
+        * read_http -- read Hypertext Transfer Protocol (HTTP)
+
+    Attributes:
+        * _file -- BytesIO, bytes to be extracted
+        * _info -- Info, info dict of current instance
+        * _protos -- ProtoChain, protocol chain of current instance
+
+    Utilities:
+        * _read_protos -- read next layer protocol type
+        * _read_fileng -- read file buffer
+        * _read_unpack -- read bytes and unpack to integers
+        * _read_binary -- read bytes and convert into binaries
+        * _read_packet -- read raw packet data
+
+    """
+    ##########################################################################
+    # Properties.
+    ##########################################################################
+
+    @property
+    def name(self):
+        """Name of current protocol."""
+        return 'Hypertext Transfer Protocol'
+
+    @property
+    def length(self):
+        """Deprecated."""
+        raise UnsupportedCall(f"'{self.__class__.__name__}' object has no attribute 'length'")
+
+    ##########################################################################
+    # Data models.
+    ##########################################################################
+
+    def __init__(self, _file, length=None, **kwargs):
+        self._file = _file
+        self._info = Info(self.read_http(length))
+
+        self._next = NoPayload()
+        self._protos = ProtoChain(self.__class__, self.alias)
+
+    @classmethod
+    def __index__(cls):
+        return ('HTTPv1', 'HTTPv2')
