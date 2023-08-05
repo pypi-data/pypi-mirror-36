01ae00741928b9b006924b052c2bb7d893f62210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:26 +0000

    Add package thoth-adviser/0.1.0/thoth-adviser-0.1.0.tar.gz

---
+++ packages/thoth-adviser/thoth-adviser-0.1.0.tar.gz/thoth-adviser-0.1.0/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+
+def get_install_requires():
+    with open('requirements.txt', 'r') as requirements_file:
+        # TODO: respect hashes in requirements.txt file
+        res = requirements_file.readlines()
+        return [req.split(' ', maxsplit=1)[0] for req in res if req]
+
+
+def get_version():
+    with open(os.path.join('thoth', 'adviser', '__init__.py')) as f:
+        content = f.readlines()
+
+    for line in content:
+        if line.startswith('__version__ ='):
+            # dirty, remove trailing and leading chars
+            return line.split(' = ')[1][1:-2]
+    raise ValueError("No version identifier found")
+
+
+setup(
+    name='thoth-adviser',
+    version=get_version(),
+    description='Package and package stack adviser for the Thoth project',
+    long_description='Package and package stack adviser for the Thoth project',
+    author='Fridolin Pokorny',
+    author_email='fridolin@redhat.com',
+    license='GPLv3+',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['thoth-adviser=thoth.adviser.cli:cli']
+    },
+    zip_safe=False,
+    install_requires=get_install_requires()
+)
