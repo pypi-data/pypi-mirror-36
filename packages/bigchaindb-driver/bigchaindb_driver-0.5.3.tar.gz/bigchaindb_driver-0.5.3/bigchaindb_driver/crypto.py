34c87023bbbb41d40d23447c94276974c7e3fb8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package bigchaindb-driver/0.5.3/bigchaindb_driver-0.5.3.tar.gz

---
+++ packages/bigchaindb-driver/bigchaindb_driver-0.5.3.tar.gz/bigchaindb_driver-0.5.3/bigchaindb_driver/crypto.py
@@ -0,0 +1,24 @@
+# Copyright BigchainDB GmbH and BigchainDB contributors
+# SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
+# Code is Apache-2.0 and docs are CC-BY-4.0
+
+from collections import namedtuple
+
+from cryptoconditions import crypto
+
+
+CryptoKeypair = namedtuple('CryptoKeypair', ('private_key', 'public_key'))
+
+
+def generate_keypair():
+    """Generates a cryptographic key pair.
+
+    Returns:
+        :class:`~bigchaindb_driver.crypto.CryptoKeypair`: A
+        :obj:`collections.namedtuple` with named fields
+        :attr:`~bigchaindb_driver.crypto.CryptoKeypair.private_key` and
+        :attr:`~bigchaindb_driver.crypto.CryptoKeypair.public_key`.
+
+    """
+    return CryptoKeypair(
+        *(k.decode() for k in crypto.ed25519_generate_key_pair()))
