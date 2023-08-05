7f6142a9cc7e8a8721e4679c554c50bbb26a3b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:13 +0000

    Add package cvpysdk/11.13/cvpysdk-11.13.tar.gz

---
+++ packages/cvpysdk/cvpysdk-11.13.tar.gz/cvpysdk-11.13/cvpysdk/subclients/cloudapps/google_subclient.py
@@ -0,0 +1,248 @@
+# -*- coding: utf-8 -*-
+
+# --------------------------------------------------------------------------
+# Copyright Commvault Systems, Inc.
+# See LICENSE.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+"""File for operating on a GMail/GDrive Subclient.
+
+GoogleSubclient is the only class defined in this file.
+
+GoogleSubclient:    Derived class from CloudAppsSubclient Base class, representing a
+GMail/GDrive/OneDrive subclient, and to perform operations on that subclient
+
+GoogleSubclient:
+
+    _get_subclient_properties()         --  gets the properties of Google Subclient
+
+    _get_subclient_properties_json()    --  gets the properties JSON of Google Subclient
+
+    content()                           --  update the content of the subclient
+
+    restore_out_of_place()              --  runs out-of-place restore for the subclient
+
+    discover()                          --  runs user discovery on subclient
+
+"""
+
+from __future__ import unicode_literals
+
+from ...exception import SDKException
+
+from ..casubclient import CloudAppsSubclient
+
+
+class GoogleSubclient(CloudAppsSubclient):
+    """Derived class from CloudAppsSubclient Base class, representing a GMail/GDrive/OneDrive subclient,
+        and to perform operations on that subclient."""
+
+    def _get_subclient_properties(self):
+        """Gets the subclient  related properties of File System subclient.."""
+        super(GoogleSubclient, self)._get_subclient_properties()
+        if 'content' in self._subclient_properties:
+            self._content = self._subclient_properties['content']
+
+    def _get_subclient_properties_json(self):
+        """get the all subclient related properties of this subclient.
+
+           Returns:
+                dict - all subclient properties put inside a dict
+
+        """
+        subclient_json = {
+            "subClientProperties":
+                {
+                    "impersonateUser": self._impersonateUser,
+                    "proxyClient": self._proxyClient,
+                    "subClientEntity": self._subClientEntity,
+                    "content": self._content,
+                    "commonProperties": self._commonProperties,
+                    "contentOperationType": 1
+                }
+        }
+        return subclient_json
+
+    @property
+    def content(self):
+        """Gets the appropriate content from the Subclient relevant to the user.
+
+            Returns:
+                list - list of content associated with the subclient
+        """
+        content = []
+
+        for account in self._content:
+            temp_account = account["cloudconnectorContent"]["includeAccounts"]
+
+            content_dict = {
+                'SMTPAddress': temp_account["contentName"],
+                'display_name': temp_account["contentValue"]
+            }
+
+            content.append(content_dict)
+
+        return content
+
+    @content.setter
+    def content(self, subclient_content):
+        """Creates the list of content JSON to pass to the API to add/update content of a
+            Cloud Apps Subclient.
+
+            Args:
+                subclient_content (list)  --  list of the content to add to the subclient
+
+            Returns:
+                list - list of the appropriate JSON for an agent to send to the POST Subclient API
+        """
+        content = []
+
+        try:
+            for account in subclient_content:
+                temp_content_dict = {
+                    "cloudconnectorContent": {
+                        "includeAccounts": {
+                            "contentValue": account['display_name'],
+                            "contentType": 134,
+                            "contentName": account['SMTPAddress']
+                        }
+                    }
+                }
+
+                content.append(temp_content_dict)
+        except KeyError as err:
+            raise SDKException('Subclient', '102', '{} not given in content'.format(err))
+
+        self._set_subclient_properties("_content", content)
+
+    def restore_out_of_place(
+            self,
+            client,
+            destination_path,
+            paths,
+            overwrite=True,
+            restore_data_and_acl=True,
+            copy_precedence=None,
+            from_time=None,
+            to_time=None,
+            to_disk=False):
+        """Restores the files/folders specified in the input paths list to the input client,
+            at the specified destionation location.
+
+            Args:
+                client                (str/object) --  either the name of the client or
+                                                           the instance of the Client
+
+                destination_path      (str)        --  full path of the restore location on client
+
+                paths                 (list)       --  list of full paths of
+                                                           files/folders to restore
+
+                overwrite             (bool)       --  unconditional overwrite files during restore
+                    default: True
+
+                restore_data_and_acl  (bool)       --  restore data and ACL files
+                    default: True
+
+                copy_precedence         (int)   --  copy precedence value of storage policy copy
+                    default: None
+
+                from_time           (str)       --  time to retore the contents after
+                        format: YYYY-MM-DD HH:MM:SS
+
+                    default: None
+
+                to_time           (str)         --  time to retore the contents before
+                        format: YYYY-MM-DD HH:MM:SS
+
+                    default: None
+
+                to_disk             (bool)       --  If True, restore to disk will be performed
+
+            Returns:
+                object - instance of the Job class for this restore job
+
+            Raises:
+                SDKException:
+                    if client is not a string or Client instance
+
+                    if destination_path is not a string
+
+                    if paths is not a list
+
+                    if failed to initialize job
+
+                    if response is empty
+
+                    if response is not success
+
+        """
+        self._instance_object._restore_association = self._subClientEntity
+
+        return self._instance_object.restore_out_of_place(
+            client=client,
+            destination_path=destination_path,
+            paths=paths,
+            overwrite=overwrite,
+            restore_data_and_acl=restore_data_and_acl,
+            copy_precedence=copy_precedence,
+            from_time=from_time,
+            to_time=to_time,
+            to_disk=to_disk
+        )
+
+    def discover(self, discover_type='USERS'):
+        """This method discovers the users/groups on Google GSuite Account/OneDrive
+
+                Args:
+
+                    discover_type (str)  --  Type of discovery
+
+                        Valid Values are
+
+                        -   USERS
+                        -   GROUPS
+
+                        Default: USERS
+
+                Returns:
+
+                    List (list)  --  List of users on GSuite account
+
+                Raises:
+                    SDKException:
+                        if response is empty
+
+                        if response is not success
+
+
+        """
+
+        if discover_type.upper() == 'USERS':
+            disc_type = 10
+        elif discover_type.upper() == 'GROUPS':
+            disc_type = 5
+        _get_users = self._services['GET_CLOUDAPPS_USERS'] % (self._instance_object.instance_id,
+                                                              self._client_object.client_id,
+                                                              disc_type)
+
+        flag, response = self._cvpysdk_object.make_request('GET', _get_users)
+
+        if flag:
+            if response.json() and "scDiscoveryContent" in response.json():
+                self._discover_properties = response.json()[
+                    "scDiscoveryContent"][0]
+
+                if "contentInfo" in self._discover_properties:
+                    self._contentInfo = self._discover_properties["contentInfo"]
+
+                user_list = []
+
+                for user in self._contentInfo:
+                    user_list.append(user['contentName'])
+                return user_list
+            else:
+                raise SDKException('Response', '102')
+        else:
+            raise SDKException('Response', '101', self._update_response_(response.text))
