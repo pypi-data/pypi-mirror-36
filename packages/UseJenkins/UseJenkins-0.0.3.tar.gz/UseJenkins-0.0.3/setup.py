aad9473e8a95b30e5715b42ae74eca9edec39e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:18 +0000

    Add package UseJenkins/0.0.3/UseJenkins-0.0.3.tar.gz

---
+++ packages/UseJenkins/UseJenkins-0.0.3.tar.gz/UseJenkins-0.0.3/setup.py
@@ -0,0 +1,23 @@
+#!/local/bin/env python3
+# -*- coding:utf8 -*-
+# Fangbinbin
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="UseJenkins",
+    version="0.0.3",
+    author="Fangbinbin",
+    author_email="fangbinbin30@163.com",
+    description="How to Use Jenkins！",
+    url="https://github.com/fangbinbin30",
+    py_modules = ['UseJenkins'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
