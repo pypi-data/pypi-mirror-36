74e5eaefb2cecf3820f63cbd7a104de8dde02469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:59 +0000

    Add package spotty/1.1.0/spotty-1.1.0.tar.gz

---
+++ packages/spotty/spotty-1.1.0.tar.gz/spotty-1.1.0/spotty/test.py
@@ -0,0 +1,14 @@
+import hashlib
+
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+res = md5('D:\\Downloads\\setup.txt')
+
+print(res)
