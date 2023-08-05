ab23ba711be8b4f99c8b8b2d1390c21adc5eaac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:05 +0000

    Add package higlass_jupyter/0.1.0a0/higlass_jupyter-0.1.0a0.tar.gz

---
+++ packages/higlass_jupyter/higlass_jupyter-0.1.0a0.tar.gz/higlass_jupyter-0.1.0a0/higlass_jupyter/__init__.py
@@ -0,0 +1,35 @@
+from ._version import version_info, __version__
+import ipywidgets as widgets
+from traitlets import Unicode
+from traitlets import default
+from traitlets import List
+
+
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'static',
+        'dest': 'jupyter_higlass',
+        'require': 'jupyter_higlass/extension'
+    }]
+
+
+class HiGlassDisplay(widgets.DOMWidget):
+    _view_name = Unicode('HiGlassDisplayView').tag(sync=True)
+    _model_name = Unicode('HiGlassDisplayModel').tag(sync=True)
+    _view_module = Unicode('jupyter_higlass').tag(sync=True)
+    _model_module = Unicode('jupyter_higlass').tag(sync=True)
+
+    _model_data = List([]).tag(sync=True)
+    viewconf = Unicode('{}').tag(sync=True)
+
+    def __init__(self, **kwargs):
+        # self.viewconf = viewconf
+        super(HiGlassDisplay, self).__init__(**kwargs)
+
+    # @default('layout')
+    # def _default_layout(self):
+    #     return widgets.Layout(height='600px', align_self='stretch')
+
+    # def set_data(self, js_data):
+    #     self._model_data = js_data
