7ed1b86599d5407dd15cafe36c9e7acfa00950dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:09 +0000

    Add package values/0.0.1/values-0.0.1-py2.py3-none-any.whl

---
+++ packages/values/values-0.0.1-py2.py3-none-any.whl/values/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from public import public
+import this_is
+
+@public
+def get(input):
+    if not this_is.iterable(input) or this_is.string(input):
+        return [input]
+    return list(input)
+
