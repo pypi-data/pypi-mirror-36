be3e256c70e2c55f5d70e720c5983d6d9d1e9849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:13 +0000

    Add package stylo/0.4.0/stylo-0.4.0.tar.gz

---
+++ packages/stylo/stylo-0.4.0.tar.gz/stylo-0.4.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+from stylo import __version__
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='stylo',
+      version=__version__,
+      description='Using a blend of Python and Maths for the '
+      'creation of images',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Multimedia :: Graphics'
+      ],
+      author='Alex, Carney',
+      author_email='alcarneyme@gmail.com',
+      license='MIT',
+      packages=['stylo'],
+      install_requires=[
+          'Pillow',
+          'hypothesis',
+          'matplotlib',
+          'numpy',
+          'pytest'
+      ],
+      setup_requires=['pytest-runner'],
+      test_suite='tests',
+      python_requires='>=3.0',
+      include_package_data=True,
+      zip_safe=False)
