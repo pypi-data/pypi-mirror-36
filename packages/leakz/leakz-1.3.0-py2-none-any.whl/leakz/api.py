597301b12cb61f5883e3b76294a255ec88fdddc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:59 +0000

    Add package leakz/1.3.0/leakz-1.3.0-py2-none-any.whl

---
+++ packages/leakz/leakz-1.3.0-py2-none-any.whl/leakz/api.py
@@ -0,0 +1,45 @@
+import json
+
+import requests
+
+
+class LeakzException(Exception):
+    pass
+
+class LeakzNotLeaked(LeakzException):
+    pass
+
+
+class LeakzRequestException(LeakzException):
+    pass
+
+
+class LeakzJSONDecodeException(LeakzException):
+    pass
+
+
+def api_request(path: str, base_url: str = "https://lea.kz") -> dict:
+    try:
+        response = requests.get(base_url + path)
+        if response.status_code == 404:
+            raise LeakzNotLeaked
+        return response.json()
+    except requests.exceptions.RequestException:
+        raise LeakzRequestException
+    except json.decoder.JSONDecodeError as error:
+        raise LeakzJSONDecodeException
+
+
+def leaked_mail(email_address: str) -> list:
+    response = api_request("/api/mail/" + email_address).get("leaked")
+    if response:
+        return response.split(', ')
+    return list()
+
+
+def password_from_hash(hash: str) -> str:
+    return api_request("/api/hash/" + hash).get("password")
+
+
+def hashes_from_password(password) -> dict:
+    return api_request("/api/password/" + password)
