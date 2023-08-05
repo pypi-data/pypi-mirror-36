a91889779b09c2a38c62372b42ee8c8c13e8b16d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package lycee/2.6.1/lycee-2.6.1.tar.gz

---
+++ packages/lycee/lycee-2.6.1.tar.gz/lycee-2.6.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lycee",
+    version="2.6.1",
+    author="Vincent MAILLE",
+    author_email="vincent.maille@ac-amiens.fr",
+    description="Un ensemble de fonctions pour utiliser facilement Python au lycée.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://edupython.tuxfamily.org/",
+    packages=setuptools.find_packages(),
+	install_requires = ['numpy', 'scipy', 'matplotlib'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: CeCILL-B Free Software License Agreement (CECILL-B)",
+        "Operating System :: OS Independent",
+    ),
+)
