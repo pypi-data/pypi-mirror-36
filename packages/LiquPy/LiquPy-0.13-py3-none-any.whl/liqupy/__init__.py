f5516eac0957f72d6dcf15bd6cd7a47df4517094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:29 +0000

    Add package LiquPy/0.13/LiquPy-0.13-py3-none-any.whl

---
+++ packages/LiquPy/LiquPy-0.13-py3-none-any.whl/liqupy/__init__.py
@@ -0,0 +1,19 @@
+# **************************************************************************************
+# LiquPy: Open-source Python Library for Soil Liquefaction and Lateral Spread Analysis
+# https://github.com/LiquPy/LiquPy
+# **************************************************************************************
+
+__all__ = ['verify_ls_model',
+           'calc_ls_bartlett',
+           'calc_ls_bardet',
+           'calc_ls_javadi2006',
+           'calc_ls_javadi_moderate2006',
+           'calc_ls_rezania',
+           'calc_ls_baziar2013',
+           'calc_ls_goh',
+           'verify_ls_model']
+
+__version__ = '0.12'
+
+name = 'LiquPy'
+
