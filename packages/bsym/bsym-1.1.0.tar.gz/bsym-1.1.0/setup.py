bfd331bec998c8e64172f621336ade2523c5d355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:49 +0000

    Add package bsym/1.1.0/bsym-1.1.0.tar.gz

---
+++ packages/bsym/bsym-1.1.0.tar.gz/bsym-1.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from bsym import __version__ as VERSION
+
+readme = 'README.md'
+long_description = open( readme ).read()
+
+config = {
+    'description': 'A Basic Symmetry Module',
+    'long_description': long_description,
+    'long_description_content_type': 'text/markdown',
+    'author': 'Benjamin J. Morgan',
+    'author_email': 'b.j.morgan@bath.ac.uk',
+    'url': 'https://github.com/bjmorgan/bsym',
+    'download_url': "https://github.com/bjmorgan/bsym/archive/%s.tar.gz" % (VERSION),
+    'author_email': 'b.j.morgan@bath.ac.uk',
+    'version': VERSION,
+    'install_requires': [ 'numpy', 
+			  'pymatgen>=v2017.10.16', 
+                          'coverage',
+                          'codeclimate-test-reporter' ],
+    'python_requires': '>=3.5',
+    'license': 'MIT',
+    'packages': [ 'bsym', 'bsym.interface' ],
+    'scripts': [],
+    'name': 'bsym'
+}
+
+setup(**config)
