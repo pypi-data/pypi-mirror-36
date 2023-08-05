9a050990b9c8889e35d22632299fc52a9559eeb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:07 +0000

    Add package wty-listdir/1.0/wty_listdir-1.0.tar.gz

---
+++ packages/wty-listdir/wty_listdir-1.0.tar.gz/wty_listdir-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'wty_listdir',
+    version = '1.0',
+    py_modules = ['wty_listdir'],
+    author = 'wangty',
+    author_email = '1002814312@qq.com',
+    url = ' ',
+    description = 'test'
+    )
