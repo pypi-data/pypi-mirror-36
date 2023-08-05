69e3270999461c99185627d0aa9377210c6e10b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:06 +0000

    Add package typepy/0.2.2/typepy-0.2.2-py2.py3-none-any.whl

---
+++ packages/typepy/typepy-0.2.2-py2.py3-none-any.whl/typepy/converter/_integer.py
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+from decimal import Decimal, InvalidOperation
+
+from ..error import TypeConversionError
+from ._interface import AbstractValueConverter
+
+
+class IntegerConverter(AbstractValueConverter):
+    def force_convert(self):
+        try:
+            return int(Decimal(self._value))
+        except (TypeError, OverflowError, ValueError, InvalidOperation):
+            raise TypeConversionError(
+                "failed to force_convert to int: type={}".format(type(self._value))
+            )
