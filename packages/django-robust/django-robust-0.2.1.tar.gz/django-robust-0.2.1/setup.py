91f1b9ff53cd8c72c71b428c9ad0bc30b721830a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:43 +0000

    Add package django-robust/0.2.1/django-robust-0.2.1.tar.gz

---
+++ packages/django-robust/django-robust-0.2.1.tar.gz/django-robust-0.2.1/setup.py
@@ -0,0 +1,27 @@
+import sys
+
+from setuptools import find_packages, setup
+
+if sys.version_info < (3, 6):
+    raise ImportError('django-robust 0.2.x only supports python3.6 and newer')
+
+setup(
+    name='django-robust',
+    version='0.2.1',
+    description='robust background queue for django',
+    author='Victor Kotseruba',
+    author_email='barbuzaster@gmail.com',
+    url='https://github.com/barbuza/django-robust',
+    include_package_data=True,
+    packages=find_packages(exclude=['django_robust', 'dummy']),
+    python_requires='>=3.6',
+    install_requires=[
+        'django >= 1.11',
+        'psycopg2 >= 2.5',
+        'django-redis >= 4.8.0',
+        'redis >= 2.10',
+        'django-object-actions',
+        'schedule',
+        'pygments'
+    ]
+)
