0d6d656f91a41486232adb8e678cc51fd244b2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:49 +0000

    Add package pipm/10.4.4/pipm-10.4.4.tar.gz

---
+++ packages/pipm/pipm-10.4.4.tar.gz/pipm-10.4.4/pipm/__init__.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from pip import _internal as p
+
+# patch for program name
+from pipm.commands import InstallCommandPlus, UninstallCommandPlus, UpdateCommand, FreezeCommandPlus
+
+
+def get_prog():
+    return 'pipm'
+
+
+p.get_prog = get_prog
+
+p.commands_dict[InstallCommandPlus.name] = InstallCommandPlus
+p.commands_dict['i'] = InstallCommandPlus
+
+p.commands_dict[UninstallCommandPlus.name] = UninstallCommandPlus
+p.commands_dict['rm'] = UninstallCommandPlus
+
+p.commands_dict[FreezeCommandPlus.name] = FreezeCommandPlus
+p.commands_dict['save'] = FreezeCommandPlus
+
+p.commands_dict['update'] = UpdateCommand
+p.commands_dict['upgrade'] = UpdateCommand
+p.commands_dict['u'] = UpdateCommand
+
+
+# endpatch
+
+def main():
+    p.main()
+
+
+if __name__ == '__main__':
+    main()
