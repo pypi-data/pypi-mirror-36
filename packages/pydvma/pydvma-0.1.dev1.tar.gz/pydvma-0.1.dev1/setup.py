106c1f3b6bca40f64f1fc4d4625cd21676dfb95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:11 +0000

    Add package pydvma/0.1.dev1/pydvma-0.1.dev1.tar.gz

---
+++ packages/pydvma/pydvma-0.1.dev1.tar.gz/pydvma-0.1.dev1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  6 15:06:51 2018
+
+@author: tb267
+"""
+from setuptools import setup, find_packages
+
+requires = ['distutils', 'numpy', 'scipy', 'time', 'datetime', 'pyqtgraph', 'copy', 'tkinter', 'pyaudio', 'pandas', 'matplotlib']
+
+setup(
+    name='pydvma',
+    version='0.1.dev1',
+    install_requires=requires,
+    packages=['pydvma','labs'],
+    license='BSD 3-Clause License',
+    long_description=open('README.md').read(),
+)
