ae0e6251a67bda53824550a6986df6ac9edab408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:20 +0000

    Add package kotti-group-manager/0.1.0.dev0/kotti_group_manager-0.1.0.dev0-py2-none-any.whl

---
+++ packages/kotti-group-manager/kotti_group_manager-0.1.0.dev0-py2-none-any.whl/kotti_group_manager/populator.py
@@ -0,0 +1,9 @@
+"""
+Populate contains two functions that are called on application startup
+(if you haven't modified kotti.populators).
+"""
+from kotti_controlpanel.util import add_settings
+from kotti_group_manager.controlpanel import GroupRulesControlPanel
+
+def populate():
+    add_settings(GroupRulesControlPanel)
