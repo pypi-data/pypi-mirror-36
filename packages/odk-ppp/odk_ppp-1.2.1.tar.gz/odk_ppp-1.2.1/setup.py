17bf2b4bff016ceec76c9bfc55821d84f27b63ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:55 +0000

    Add package odk-ppp/1.2.1/odk_ppp-1.2.1.tar.gz

---
+++ packages/odk-ppp/odk_ppp-1.2.1.tar.gz/odk_ppp-1.2.1/setup.py
@@ -0,0 +1,37 @@
+"""Setup for Pypi"""
+from setuptools import setup, find_packages
+
+
+packages = find_packages(exclude=['test'])
+
+setup(
+    name='odk_ppp',  # ppp was already taken
+    version='1.2.1',
+    author='Joe Flack',
+    author_email='jflack@jhu.edu',
+    url='http://www.pma2020.org',
+    packages=packages,
+    package_dir={
+        'ppp': 'ppp'
+    },
+    package_data={
+        'ppp': [
+            'templates/*.html',
+            'templates/content/*.html',
+            'templates/content/group/*.html',
+            'templates/content/prompt/*.html',
+            'templates/content/prompt/inputs/*.html',
+            'templates/content/repeat/*.html',
+            'templates/content/table/*.html'
+        ]
+    },
+    license='LICENSE.txt',
+    description='Converts XlsForm Excel files (ODK, SurveyCTO, etc) into '
+                'readable formats.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'xlrd>=0.9.3',
+        'Jinja2>=2.9.6'
+        'pmix==0.2.2'
+    ]
+)
