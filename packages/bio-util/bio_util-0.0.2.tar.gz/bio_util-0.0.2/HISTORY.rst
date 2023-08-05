aa5d13a15e6341689bb2ff7670abafb3495078c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:11 +0000

    Add package bio-util/0.0.2/bio_util-0.0.2.tar.gz

---
+++ packages/bio-util/bio_util-0.0.2.tar.gz/bio_util-0.0.2/HISTORY.rst
@@ -0,0 +1,14 @@
+=======
+History
+=======
+
+0.0.1 (2018-09-13)
+------------------
+
+* First release on PyPI.
+
+0.0.2 (2018-09-14)
+------------------
+
+Added load_references and run_cmd methods
+
---
+++ packages/bio-util/bio_util-0.0.2.tar.gz/bio_util-0.0.2/bio_util/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for bio_util."""
+
+__author__ = """Vinicio Reynoso"""
+__email__ = 'vr1087@gmail.com'
+__version__ = '0.0.2'
+
+from .bio_util import load_references
+from .shell import run_cmd
