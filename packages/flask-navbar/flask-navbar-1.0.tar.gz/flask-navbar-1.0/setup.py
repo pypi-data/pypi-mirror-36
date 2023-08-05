ad5103daed1edd0cb502d062d1503c3f7e254954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package flask-navbar/1.0/flask-navbar-1.0.tar.gz

---
+++ packages/flask-navbar/flask-navbar-1.0.tar.gz/flask-navbar-1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='flask-navbar',
+      version='1.0',
+      description='Easily create navigation for Flask applications. An extended copy of Flask_nav 0.6',
+      long_description=read('README.rst'),
+      author='zcyuefan',
+      author_email='zcyuefan@126.com',
+      url='http://github.com/zcyuefan/flask-navbar',
+      license='MIT',
+      packages=find_packages(exclude=['tests', 'example']),
+      install_requires=['flask', 'visitor', 'dominate'],
+      classifiers=[
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ])
