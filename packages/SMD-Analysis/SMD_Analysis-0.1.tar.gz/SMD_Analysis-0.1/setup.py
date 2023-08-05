1deadd9742ce702bf8d706180da5dda8b3a86f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:55 +0000

    Add package SMD-Analysis/0.1/SMD_Analysis-0.1.tar.gz

---
+++ packages/SMD-Analysis/SMD_Analysis-0.1.tar.gz/SMD_Analysis-0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+setuptools.setup(
+    name="SMD_Analysis",
+    version="0.1",
+    author="rehab shahzadi",
+    author_email="rehab.shahzadi@kics.edu.pk",
+    description="An API for social media data analysis",
+    packages=setuptools.find_packages(),
+
+    classifiers = (
+                  "Programming Language :: Python :: 3",
+                  "Operating System :: OS Independent",
+              ),
+     install_requires=[
+        'pandas==0.23.4',
+        'nltk==3.3',
+         'scikit-learn==0.19.2',
+         'scipy==1.1.0',
+
+    ],
+)
