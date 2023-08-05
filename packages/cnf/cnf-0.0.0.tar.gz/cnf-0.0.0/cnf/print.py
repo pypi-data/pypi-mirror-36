e0b957e3c701e020947dfe42326d23bf818b657a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package cnf/0.0.0/cnf-0.0.0.tar.gz

---
+++ packages/cnf/cnf-0.0.0.tar.gz/cnf-0.0.0/cnf/print.py
@@ -0,0 +1,25 @@
+from .functions import group
+
+def pretty_print(filter_list):
+    cnf = group(filter_list)
+    s = ''
+    for i, and_group in enumerate(cnf):
+        if i > 0:
+            s += '\nand '
+        for j, or_filt in enumerate(and_group):
+            if j > 0:
+                s += '\n\tor '
+            s += or_filt['name']
+    return s
+
+
+
+def filename(filter_list):
+    cnf = group(filter_list)
+    s = ''
+    for i, and_group in enumerate(cnf):
+        if i > 0:
+            s+='_and'
+        for j, or_filt in enumerate(and_group):
+            s += '_' + or_filt['name']
+    return s
