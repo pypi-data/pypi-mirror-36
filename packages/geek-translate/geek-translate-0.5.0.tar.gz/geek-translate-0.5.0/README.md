8ce67a5a04a8e3ab606eecd82ce6df8b06cfef26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:23 +0000

    Add package geek-translate/0.5.0/geek-translate-0.5.0.tar.gz

---
+++ packages/geek-translate/geek-translate-0.5.0.tar.gz/geek-translate-0.5.0/README.md
@@ -0,0 +1,16 @@
+﻿## **搜狗翻译API**
+
+
+### **基本使用**
+1. 调用搜狗翻译API，需要传3个参数text,target,from_target
+2. text为要翻译的文本（必传，type可以是str或者list）
+3. target为要翻译成的语言（可不传，默认为'en'，英文）
+4. from_target为传入的text的语言（可不传，默认为'zh-CHS'，中文）
+
+### **相关说明**
+#### **接口地址**
+http://snapshot.sogoucdn.com/engtranslate
+#### **请求方式**
+只支持POST请求
+#### **请求数据和返回数据格式**
+JSON对象
---
+++ packages/geek-translate/geek-translate-0.5.0.tar.gz/geek-translate-0.5.0/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup
+
+setup(
+    name='geek-translate',
+    version='0.5.0',
+    description='sogou',
+    install_requires=[
+        "requests >= 2.13.0"
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Intended Audience :: Developers'
+    ],
+    author='chen',
+    url='https://github.com/mydu27/deemo',
+    author_email='chenjiageng@geekpark.net',
+    packages=['geek_translate'],
+    include_package_data=False,
+    zip_safe=True
+)
