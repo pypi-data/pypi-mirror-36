207e33b9a8b3511b8902a9ca347d449a0647eb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:33 +0000

    Add package becca-toolbox/0.10.1/becca_toolbox-0.10.1.tar.gz

---
+++ packages/becca-toolbox/becca_toolbox-0.10.1.tar.gz/becca_toolbox-0.10.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='becca_toolbox',
+    version='0.10.1',
+    description='Some tools for working with Becca',
+    url='http://github.com/brohrer/becca_toolbox',
+    download_url='https://github.com/brohrer/becca_toolbox/archive/master.zip',
+    author='Brandon Rohrer',
+    author_email='brohrer@gmail.com',
+    license='MIT',
+    packages=['becca_toolbox'],
+    include_package_data=True,
+    zip_safe=False)
