c17bb91c9a6543483490b52a3c50d0adefbc0f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:55 +0000

    Add package prince-text-detection/0.0.2/prince_text_detection-0.0.2.tar.gz

---
+++ packages/prince-text-detection/prince_text_detection-0.0.2.tar.gz/prince_text_detection-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="prince_text_detection",
+    version="0.0.2",
+    author="Prince Bhatia",
+    author_email="princebhatia09@example.com",
+    description="A Python library for detecting text on images",
+    keywords=['text_detector'],
+    url="https://github.com/TarzanWonder",
+    packages=['prince_text_detection'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
