f3839e01a90aa788e1d4839b9be663975280d0b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:36 +0000

    Add package tap.py/2.5/tap.py-2.5-py2.py3-none-any.whl

---
+++ packages/tap.py/tap.py-2.5-py2.py3-none-any.whl/tap/tests/testcase.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2018, Matt Layman and contributors
+
+import unittest
+
+from tap.tests.factory import Factory
+
+
+class TestCase(unittest.TestCase):
+    def __init__(self, methodName="runTest"):
+        super(TestCase, self).__init__(methodName)
+        self.factory = Factory()
