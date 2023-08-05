498b236a8cd235c9418238e94892b788c638d171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:04 +0000

    Add package unified-range/0.0.2/unified_range-0.0.2.tar.gz

---
+++ packages/unified-range/unified_range-0.0.2.tar.gz/unified_range-0.0.2/setup.py
@@ -0,0 +1,19 @@
+# from distutils.core import setup
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='unified_range',
+    version='0.0.2',
+    packages=['unified_range', ],
+    license='MIT License',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
