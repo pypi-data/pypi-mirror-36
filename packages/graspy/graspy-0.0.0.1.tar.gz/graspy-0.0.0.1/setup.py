97ae79836d323df125284773da7f99a7190e4b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:30 +0000

    Add package graspy/0.0.0.1/graspy-0.0.0.1.tar.gz

---
+++ packages/graspy/graspy-0.0.0.1.tar.gz/graspy-0.0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='graspy',
+    version='0.0.0.1',
+    description='Placeholder for graspy',
+    author='Jaewon Chung',
+    author_email='jaewonc78@gmail.com',
+    packages=['graspy'])
