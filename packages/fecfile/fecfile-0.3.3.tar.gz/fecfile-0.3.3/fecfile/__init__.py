c351079cbc3a30a2eb72bdfbdfd939d08372ff87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:17 +0000

    Add package fecfile/0.3.3/fecfile-0.3.3.tar.gz

---
+++ packages/fecfile/fecfile-0.3.3.tar.gz/fecfile-0.3.3/fecfile/__init__.py
@@ -0,0 +1,38 @@
+from . import fecparser
+import requests
+
+
+FecParserMissingMappingError = fecparser.FecParserMissingMappingError
+
+
+def loads(input):
+    return fecparser.loads(input)
+
+
+def parse_line(line, version, line_num=None):
+    return fecparser.parse_line(line, version, line_num)
+
+
+def from_http(file_number):
+    url = 'http://docquery.fec.gov/dcdev/posted/{n}.fec'.format(n=file_number)
+    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    if r.status_code == 404:
+        url = 'http://docquery.fec.gov/paper/posted/{n}.fec'.format(
+            n=file_number
+        )
+        r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    if r.status_code == 404:
+        return None
+    return fecparser.loads(r.text)
+
+
+def from_file(file_path):
+    parsed = {}
+    with open(file_path) as file:
+        unparsed = file.read()
+        parsed = fecparser.loads(unparsed)
+    return parsed
+
+
+def print_example(parsed):
+    fecparser.print_example(parsed)
