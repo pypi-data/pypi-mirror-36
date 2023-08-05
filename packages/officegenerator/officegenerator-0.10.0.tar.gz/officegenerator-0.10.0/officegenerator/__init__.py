fdd8ee597e7a0567a26213398db29c4eb33b48c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:42 +0000

    Add package officegenerator/0.10.0/officegenerator-0.10.0.tar.gz

---
+++ packages/officegenerator/officegenerator-0.10.0.tar.gz/officegenerator-0.10.0/officegenerator/__init__.py
@@ -0,0 +1,6 @@
+## @package officegenerator
+## @brief Generate office files with predefined styles
+from officegenerator.libodfgenerator import Color, ColumnWidthODS, ODS_Read, ODS_Write,  ODT,  OdfCell,  OdfSheet
+from officegenerator.libxlsxgenerator import OpenPyXL,  ColumnWidthXLSX
+from officegenerator.commons import *
+
---
+++ packages/officegenerator/officegenerator-0.10.0.tar.gz/officegenerator-0.10.0/setup.py
@@ -0,0 +1,94 @@
+
+from setuptools import setup, Command
+
+import os
+import platform
+import site
+from officegenerator.commons import __version__
+
+class Doc(Command):
+    description = "Update translations"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        #es
+        os.system("xgettext -L Python --no-wrap --no-location --from-code='UTF-8' -o locale/officegenerator.pot *.py officegenerator/*.py")
+        os.system("msgmerge -N --no-wrap -U locale/es.po locale/officegenerator.pot")
+        os.system("msgfmt -cv -o officegenerator/locale/es/LC_MESSAGES/officegenerator.mo locale/es.po")
+
+
+class Doxygen(Command):
+    description = "Create/update doxygen documentation in doc/html"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        print("Creating Doxygen Documentation")
+        os.system("rm -Rf build")
+        os.chdir("doc")
+        os.system("rm -Rf doc/html")
+        os.system("doxygen Doxyfile")
+        os.system("rsync -avzP -e 'ssh -l turulomio' html/ frs.sourceforge.net:/home/users/t/tu/turulomio/userweb/htdocs/doxygen/officegenerator/ --delete-after")
+        os.chdir("..")
+
+class Uninstall(Command):
+    description = "Uninstall installed files with install"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        if platform.system()=="Linux":
+            os.system("rm -Rf {}/officegenerator*".format(site.getsitepackages()[0]))
+            os.system("rm /usr/bin/officegenerator*")
+        else:
+            print ("Uninstall only works in Linux")
+
+########################################################################
+
+with open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+setup(name='officegenerator',
+     version=__version__,
+     description='Python module to read and write LibreOffice and MS Office files',
+     long_description=long_description,
+     long_description_content_type='text/markdown',
+     classifiers=['Development Status :: 4 - Beta',
+                  'Intended Audience :: Developers',
+                  'Topic :: Software Development :: Build Tools',
+                  'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                  'Programming Language :: Python :: 3',
+                 ], 
+     keywords='office generator',
+     url='https://officegenerator.sourceforge.io/',
+     author='Turulomio',
+     author_email='turulomio@yahoo.es',
+     license='GPL-3',
+     packages=['officegenerator'],
+     install_requires=['odfpy','openpyxl'],
+     entry_points = {'console_scripts': ['officegenerator_demo=officegenerator.demo:main',
+                                         'officegenerator_odf2xml=officegenerator.odf2xml:main',
+                                        ],
+                    },
+     cmdclass={'doxygen': Doxygen,
+               'uninstall':Uninstall, 
+               'doc': Doc,
+              },
+     zip_safe=False,
+     include_package_data=True
+)
