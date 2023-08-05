39a4159d7a09a6426a0ae93339419686ce69406d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:13 +0000

    Add package nonrepeat/0.1.2/nonrepeat-0.1.2.tar.gz

---
+++ packages/nonrepeat/nonrepeat-0.1.2.tar.gz/nonrepeat-0.1.2/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+modules = \
+['nonrepeat']
+setup_kwargs = {
+    'name': 'nonrepeat',
+    'version': '0.1.2',
+    'description': 'Generate non-repeat filenames, ids and strings',
+    'long_description': None,
+    'author': 'Pacharapol Withayasakpunt',
+    'author_email': 'patarapolw@gmail.com',
+    'url': None,
+    'py_modules': modules,
+}
+
+
+setup(**setup_kwargs)
