f107b5060a48e4186e30d380935b5331460bd43b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:55 +0000

    Add package infobot/0.0.4/infobot-0.0.4.tar.gz

---
+++ packages/infobot/infobot-0.0.4.tar.gz/infobot-0.0.4/infobot/konstants.py
@@ -0,0 +1,19 @@
+# k_configfile = "configFileName"
+
+packageName = "infobot"
+socialModule = "social."
+storageModule = "storage."
+
+dataDirectoryKey = "directory"
+counterKey = "counterfile"
+startKey = "start"
+lastKey = "last"
+previousKey = "previous"
+footerKey = "footer"
+headerKey = "header"
+
+mastodonKey = "mastodon"
+fakeKey = "fakeKey"
+
+useridKey = "userid"
+passwdKey = "password"
