004f897b55016b1ec54f6747ecfccd399643d73e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:08 +0000

    Add package pyblockMesh/0.0.3/pyblockMesh-0.0.3.tar.gz

---
+++ packages/pyblockMesh/pyblockMesh-0.0.3.tar.gz/pyblockMesh-0.0.3/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+
+setuptools.setup(name='pyblockMesh',
+                 version='0.0.3',
+                 description='Python wrapper around blockMeshDict creation',
+                 long_description=open('README.org').read().strip(),
+                 author='Vishakh Kumar',
+                 author_email='grokkingStuff@gmail.com',
+                 url='https://github.com/grokkingStuff/pyblockmesh',
+                 py_modules=['pyblockmesh.vertex','pyblockmesh.edge','pyblockmesh.face','pyblockmesh.hexahedron'],
+                 packages=['pyblockmesh'],
+                 install_requires=[],
+                 license='MIT License',
+                 zip_safe=False,
+                 python_requires='>=3',
+                 keywords='blockmesh blockmeshdict openfoam pyfoam',
+                 classifiers=[
+                             # How mature is this project? Common values are
+                             #   3 - Alpha
+                             #   4 - Beta
+                             #   5 - Production/Stable
+                             'Development Status :: 3 - Alpha',
+
+                             # Indicate who your project is intended for
+                             'Intended Audience :: Developers',
+
+                             # Pick your license as you wish (should match "license" above)
+                             'License :: OSI Approved :: MIT License',
+
+                             # Specify the Python versions you support here. In particular, ensure
+                             # that you indicate whether you support Python 2, Python 3 or both.
+                             'Programming Language :: Python :: 3',
+                             'Programming Language :: Python :: 3.2',
+                             'Programming Language :: Python :: 3.2',
+                             'Programming Language :: Python :: 3.3',
+                             'Programming Language :: Python :: 3.4',
+                             ])
