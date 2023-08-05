6a73745be78be4974fc03877cb3df9a0ca98a37c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:15 +0000

    Add package ppipa/0.0.8/ppipa-0.0.8-py2.py3-none-any.whl

---
+++ packages/ppipa/ppipa-0.0.8-py2.py3-none-any.whl/ppipa/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""PP's FreeIPA Module
+
+ppipa allows easy interaction with FreeIPA servers.
+
+Author: Peter Pakos <peter.pakos@wandisco.com>
+
+Copyright (C) 2018 WANdisco
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from .__version__ import __version__
+from .freeipaserver import FreeIPAServer
+from .freeipauser import FreeIPAUser
