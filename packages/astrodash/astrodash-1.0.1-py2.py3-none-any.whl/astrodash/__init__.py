069a11c78b9e47ac76a5a3d3c8098590608d0dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:12 +0000

    Add package astrodash/1.0.1/astrodash-1.0.1-py2.py3-none-any.whl

---
+++ packages/astrodash/astrodash-1.0.1-py2.py3-none-any.whl/astrodash/__init__.py
@@ -0,0 +1,35 @@
+import sys
+from astrodash.download_data_files import download_all_files
+
+download_all_files('v05')
+
+try:
+    from PyQt5 import QtGui
+    from astrodash.gui_main import MainApp
+except ImportError:
+    print("Warning: You will need to install 'PyQt5' if you want to use the graphical interface. " \
+          "Using the automatic library will continue to work.")
+
+from astrodash.classify import Classify
+
+
+def main():
+    classification = Classify(filenames=['test_spectrum_file.dat',
+                                         'test_spectrum_file.dat'],
+                              redshifts=[0.34, 0.13])
+    print(classification.list_best_matches())
+    # classification.plot_with_gui(indexToPlot=1)
+
+
+def run_gui():
+    app = QtGui.QApplication(sys.argv)
+    form = MainApp()
+    form.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
+    # run_gui()
+
+
