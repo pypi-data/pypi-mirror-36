39ee0160ea7ed9c9d5000ecce9948b804f197755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:53 +0000

    Add package apys/2.0.0/apys-2.0.0-py3-none-any.whl

---
+++ packages/apys/apys-2.0.0-py3-none-any.whl/apys/commandline.py
@@ -0,0 +1,38 @@
+import argparse
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='apys - a restful api framework.')
+    parser.add_argument('-s', '--start', '--serve', 
+                        action='store_true', default=False, 
+                        help='starts server')
+    parser.add_argument('-c', '--config',
+                        action='store', default=False, 
+                        help='select config file')
+    parser.add_argument('-i', '--init',
+                        action='store_true', default=False, 
+                        help='initialize project folder')
+    parser.add_argument('-b', '--build', 
+                        action='store_true', default=False, 
+                        help='build project [not yet implemented]')
+    parser.add_argument('-t', '--test',
+                        action='store_true', default=False, 
+                        help='run automated tests [not yet implemented]')
+    parser.add_argument('-v', '--version',
+                        action='store_true', default=False, 
+                        help='show lib version')
+    
+    args = parser.parse_args()
+
+    if args.start:
+        from apys import server
+        server.start(args.config)
+    elif args.init:
+        from apys import project
+        project.init()
+    elif args.version:
+        import apys
+        print(apys.__version__)
+    else:
+        parser.print_help()
---
+++ packages/apys/apys-2.0.0-py3-none-any.whl/apys/server.py
@@ -0,0 +1,36 @@
+import sys
+
+from aiohttp import web
+
+from apys import apiobject, routes, config
+
+
+def start(config_file=None):
+    """
+    Starts server
+    """
+
+    # Preparing environment
+    app = web.Application()
+    global_api = apiobject.mount(config_file)
+    
+    # Setting log files
+    if global_api.config['log']['file']:
+        f_out = open(global_api.config['log']['file']['debug'], 'a')
+        sys.stdout = f_out
+
+        f_err = open(global_api.config['log']['file']['error'], 'a')
+        sys.stderr = f_err
+
+    # Preparing routes
+    routes.prepare(app, global_api, cors_url=global_api.config['server']['cors'])
+
+    # Start logging
+    start_str = 'Using [' + global_api.bcolors.HEADER + '{}' + global_api.bcolors.ENDC + '] configuration\n'
+    if config_file:
+        global_api.debug(start_str.format(config_file))
+    else:
+        global_api.debug(start_str.format(config.default()))
+
+    # Running server
+    web.run_app(app, port=global_api.config['server']['port'])
