49e7533e9ee9a12dbae68703ee4591dbbd969cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:36 +0000

    Add package chaospy/2.3.5/chaospy-2.3.5.tar.gz

---
+++ packages/chaospy/chaospy-2.3.5.tar.gz/chaospy-2.3.5/src/chaospy/__init__.py
@@ -0,0 +1,26 @@
+"""
+Uncertainty Quantification Toolbox
+==================================
+
+This module contains tools for performing uncertainty quantification of models.
+"""
+from chaospy.version import __version__
+import chaospy.bertran
+import chaospy.chol
+import chaospy.descriptives
+import chaospy.distributions
+import chaospy.orthogonal
+import chaospy.poly
+import chaospy.spectral
+import chaospy.quad
+import chaospy.saltelli
+import chaospy.regression
+
+from chaospy.distributions import *
+from chaospy.orthogonal import *
+from chaospy.poly import *
+from chaospy.spectral import *
+from chaospy.quad import *
+from chaospy.saltelli import *
+from chaospy.descriptives import *
+from chaospy.regression import *
