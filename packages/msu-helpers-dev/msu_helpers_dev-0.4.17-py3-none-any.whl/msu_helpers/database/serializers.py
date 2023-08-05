e85f3e2babb49228e4c5457b2744e64b2d3260bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package msu-helpers-dev/0.4.17/msu_helpers_dev-0.4.17-py3-none-any.whl

---
+++ packages/msu-helpers-dev/msu_helpers_dev-0.4.17-py3-none-any.whl/msu_helpers/database/serializers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from rest_framework import serializers
+from .models import *
+
+__all__ = ['StudyGroupSerializer', 'UserSerializer']
+
+
+class StudyGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudyGroup
+        fields = ['pk', 'code']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['pk', 'first_name', 'last_name', 'study_group', 'birthday', 'about', 'profile_pic', 'email', 'lang',
+                  'activated', 'is_staff', ]
+        depth = 1
