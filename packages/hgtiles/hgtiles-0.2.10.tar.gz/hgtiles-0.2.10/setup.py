cfb822ae5d53b095ccd0f19e774b773ff48d32f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:06 +0000

    Add package hgtiles/0.2.10/hgtiles-0.2.10.tar.gz

---
+++ packages/hgtiles/hgtiles-0.2.10.tar.gz/hgtiles-0.2.10/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='hgtiles',
+    version='0.2.10',
+    description='Tile loading for higlass-server',
+    author='Peter Kerpedjiev',
+    author_email='pkerpedjiev@gmail.com',
+    install_requires=[
+        'pandas>=0.19',
+    ],
+    url='',
+    packages=['hgtiles'],
+)
