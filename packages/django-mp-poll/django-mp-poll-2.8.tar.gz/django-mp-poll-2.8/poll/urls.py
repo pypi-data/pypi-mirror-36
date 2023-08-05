972cb93f4a090305612b567d9d132e9f49b1fd63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:52 +0000

    Add package django-mp-poll/2.8/django-mp-poll-2.8.tar.gz

---
+++ packages/django-mp-poll/django-mp-poll-2.8.tar.gz/django-mp-poll-2.8/poll/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+
+from poll import views
+
+
+urlpatterns = [
+
+    path('latest/', views.get_latest_poll, name='latest'),
+
+    path('vote/', views.VoteView.as_view(), name='vote')
+
+]
