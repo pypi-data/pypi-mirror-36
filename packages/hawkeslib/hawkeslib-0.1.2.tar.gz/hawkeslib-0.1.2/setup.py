65b2dcb1443e5df37de81061ece19a472ce0f546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:54 +0000

    Add package hawkeslib/0.1.2/hawkeslib-0.1.2.tar.gz

---
+++ packages/hawkeslib/hawkeslib-0.1.2.tar.gz/hawkeslib-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+import numpy
+from distutils.extension import Extension
+
+ext_mods = cythonize([Extension("hawkeslib.model.c.c_uv_exp", ["hawkeslib/model/c/c_uv_exp.pyx"], include_dirs=[numpy.get_include()],
+                                       libraries=["m"],
+                                       language="c++",
+                                       extra_compile_args=["-O3", "-ffast-math", "-march=native", "-fopenmp"],
+                                       extra_link_args=['-fopenmp']),
+                            Extension("hawkeslib.model.c.c_mv_exp", ["hawkeslib/model/c/c_mv_exp.pyx"], include_dirs=[numpy.get_include()],
+                                       libraries=["m"],
+                                       language="c++",
+                                       extra_compile_args=["-O3", "-march=native", "-fopenmp"],
+                                       extra_link_args=['-fopenmp']),
+                             Extension("hawkeslib.model.c.c_uv_bayes", ["hawkeslib/model/c/c_uv_bayes.pyx"], include_dirs=[numpy.get_include()],
+                                       libraries=["m"],
+                                       extra_compile_args=["-O3", "-ffast-math", "-march=native"])
+                             ])
+
+setup(name="hawkeslib",
+      version="0.1.2",
+      description="parameter estimation for simple Hawkes (self-exciting) processes",
+      author="Caner Turkmen",
+      author_email="caner.turkmen@boun.edu.tr",
+      url="http://hawkeslib.rtfd.io",
+      ext_modules=ext_mods,
+      packages=["hawkeslib", "hawkeslib.model", "hawkeslib.model.c"]
+)
+
