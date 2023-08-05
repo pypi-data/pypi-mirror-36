af86ebd2075e059bd972cadfdb38fe220330baf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:48 +0000

    Add package atc-beta-helper/0.1.1/atc-beta-helper-0.1.1.tar.gz

---
+++ packages/atc-beta-helper/atc-beta-helper-0.1.1.tar.gz/atc-beta-helper-0.1.1/autocnn_helper/__init__.py
@@ -0,0 +1,12 @@
+from autocnn_helper.config import (
+    get_cluster_def,
+    get_parameter,
+    get_exp_info,
+    get_outputs_path,
+    get_data_path,
+    get_tf_config,
+    get_log_level,
+    get_api,
+    send_metrics
+)
+# from autocnn_helper.deploy import Deploy as deploy
