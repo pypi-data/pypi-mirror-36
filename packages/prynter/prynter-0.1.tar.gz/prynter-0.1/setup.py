7f8f441ab6b9c3ad20f2916c9db2dfcfe3cdaddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:13 +0000

    Add package prynter/0.1/prynter-0.1.tar.gz

---
+++ packages/prynter/prynter-0.1.tar.gz/prynter-0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="prynter",
+    version="0.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Iván Canales",
+    author_email="kanales.contact@gmail.com",
+    description="Small utility to execute short python commands",
+    url="https://github.com/kanales/prynter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
