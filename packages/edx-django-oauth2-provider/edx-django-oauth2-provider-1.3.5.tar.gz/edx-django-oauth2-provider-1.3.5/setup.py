10f1cd5971a01583051f5c93441b8b80b0e44bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package edx-django-oauth2-provider/1.3.5/edx-django-oauth2-provider-1.3.5.tar.gz

---
+++ packages/edx-django-oauth2-provider/edx-django-oauth2-provider-1.3.5.tar.gz/edx-django-oauth2-provider-1.3.5/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+import provider
+
+setup(
+    name='edx-django-oauth2-provider',
+    version=provider.__version__,
+    description='edX fork of django-oauth2-provider',
+    long_description=open('README.rst').read(),
+    author='edX',
+    author_email='oscm@edx.org',
+    url='https://github.com/edx/django-oauth2-provider',
+    packages=find_packages(exclude=('tests*',)),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=['shortuuid>=0.4.3,<1.0.0', 'Django>=1.11,<2.1'],
+    include_package_data=True,
+    zip_safe=False,
+)
