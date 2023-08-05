62a1d47487541f6bbfd418b6d90f9073e2fb389c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:26 +0000

    Add package content-io/1.2.5/content-io-1.2.5.tar.gz

---
+++ packages/content-io/content-io-1.2.5.tar.gz/content-io-1.2.5/cio/utils/uri.py
@@ -0,0 +1,82 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
+from ..conf import settings
+import six
+
+
+class URI(six.text_type):
+
+    @staticmethod
+    def __new__(cls, uri=None, scheme=None, namespace=None, path=None, ext=None, version=None):
+        if isinstance(uri, URI):
+            return uri
+        elif uri is not None:
+            return URI._parse(uri)
+        else:
+            return URI._render(scheme, namespace, path, ext, version)
+
+    @classmethod
+    def _parse(cls, uri):
+        base, _, version = uri.partition(settings.URI_VERSION_SEPARATOR)
+        scheme, _, path = base.rpartition(settings.URI_SCHEME_SEPARATOR)
+        namespace, _, path = path.rpartition(settings.URI_NAMESPACE_SEPARATOR)
+        _path, _, ext = path.rpartition(settings.URI_EXT_SEPARATOR)
+        if '/' in ext:
+            ext = ''
+        else:
+            path = _path
+
+        if not path and ext:
+            path, ext = ext, ''
+
+        return cls._render(
+            scheme or settings.URI_DEFAULT_SCHEME,
+            namespace or None,
+            path,
+            ext or None,
+            version or None
+        )
+
+    @classmethod
+    def _render(cls, scheme, namespace, path, ext, version):
+        def parts_gen():
+            if scheme:
+                yield scheme
+                yield settings.URI_SCHEME_SEPARATOR
+            if namespace:
+                yield namespace
+                yield settings.URI_NAMESPACE_SEPARATOR
+            if path:
+                yield path
+                if ext:
+                    yield settings.URI_EXT_SEPARATOR
+                    yield ext
+                if version:
+                    yield settings.URI_VERSION_SEPARATOR
+                    yield version
+
+        uri = six.text_type.__new__(cls, ''.join(parts_gen()))
+        uri.scheme = scheme
+        uri.namespace = namespace
+        uri.path = path
+        uri.ext = ext
+        uri.version = version
+        return uri
+
+    def is_absolute(self):
+        """
+        Validates that uri contains all parts except version
+        """
+        return self.namespace and self.ext and self.scheme and self.path
+
+    def has_parts(self, *parts):
+        return not any(getattr(self, part, None) is None for part in parts)
+
+    def clone(self, **parts):
+        part = lambda part: parts.get(part, getattr(self, part))
+        args = (part(p) for p in ('scheme', 'namespace', 'path', 'ext', 'version'))
+        return URI._render(*args)
+
+    class Invalid(Exception):
+        pass
---
+++ packages/content-io/content-io-1.2.5.tar.gz/content-io-1.2.5/tests/test_utils.py
@@ -0,0 +1,79 @@
+# coding=utf-8
+from cio import lazy_shortcut
+from cio.conf import settings
+from cio.utils.formatters import ContentFormatter
+from cio.utils.uri import URI
+from cio.utils.imports import import_class
+from tests import BaseTest
+
+
+class UtilsTest(BaseTest):
+
+    def test_uri(self):
+        self.assertEqual(URI('i18n://sv@page/title.txt'), 'i18n://sv@page/title.txt')
+
+        uri = URI(scheme='i18n', namespace='sv-se', path='page/title', ext='txt')
+        self.assertEqual(uri, 'i18n://sv-se@page/title.txt')
+
+        uri = URI('page/title')
+        self.assertFalse(uri.is_absolute())
+        self.assertEqual(uri.scheme, 'i18n')
+        self.assertIsNone(uri.namespace)
+        self.assertEqual(uri.path, 'page/title')
+        self.assertIsNone(uri.ext)
+
+        uri = uri.clone(namespace='sv-se')
+        self.assertFalse(uri.is_absolute())
+        self.assertEqual(uri, 'i18n://sv-se@page/title')
+
+        uri = uri.clone(ext='txt')
+        self.assertEqual(uri, 'i18n://sv-se@page/title.txt')
+        self.assertTrue(uri.is_absolute())
+
+        uri = uri.clone(path='images/me.jpg/title', version='draft')
+        self.assertEqual(uri, 'i18n://sv-se@images/me.jpg/title.txt#draft')
+        self.assertEqual(uri.version, 'draft')
+        self.assertEqual(uri.path, 'images/me.jpg/title')
+        self.assertEqual(uri.ext, 'txt')
+
+        uri = uri.clone(ext=None)
+        self.assertEqual(uri, 'i18n://sv-se@images/me.jpg/title#draft')
+        self.assertIsNone(uri.ext)
+
+        with settings(URI_DEFAULT_SCHEME='l10n'):
+            uri = URI('page/title')
+            self.assertEqual(uri, 'l10n://page/title')
+            self.assertEqual(uri.scheme, 'l10n')
+        uri = uri.clone(scheme=None)
+        self.assertEqual(uri, 'page/title')
+        self.assertEqual(uri.scheme, None)
+
+        uri = URI('i18n://sv@page/title.txt#draft')
+        self.assertEqual(uri, 'i18n://sv@page/title.txt#draft')
+        self.assertEqual(uri.version, 'draft')
+
+    def test_formatter(self):
+        tests = [
+            (u"These are no variables: {} {0} {x} {x:f} {x!s} {x!r:.2f} { y } {{ y }}", {}, None),
+            (u"This is no variable {\n\t'foo!': 'bar', ham: 'spam'\n}", {}, None),
+            (u"These are variabls {v}, {n!s}, {n:.2f}, {n!s:>5}", dict(v=u'VALUE', n=1),
+             u"These are variabls VALUE, 1, 1.00,     1"),
+            (u"This is {mixed} with variables {}, {x}", dict(x=u'X'),
+             u"This is {mixed} with variables {}, X")
+        ]
+
+        formatter = ContentFormatter()
+
+        for template, context, value in tests:
+            self.assertEqual(formatter.format(template, **context), value or template)
+
+    def test_import_class(self):
+        CF = import_class('cio.utils.formatters', 'ContentFormatter')
+        self.assertEqual(CF, ContentFormatter)
+
+        with self.assertRaises(ImportError):
+            import_class('cio.utils.formatters', 'FooBar')
+
+    def test_lazy_shortcut(self):
+        uri_module = lazy_shortcut('cio.utils', 'uri')
+        self.assertEqual(uri_module.URI, URI)
