dfbd1c52c4c3841bd50ae678cef0e9e971717dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:32 +0000

    Add package flask-wdb-hook/0.2.1/flask-wdb-hook-0.2.1.tar.gz

---
+++ packages/flask-wdb-hook/flask-wdb-hook-0.2.1.tar.gz/flask-wdb-hook-0.2.1/setup.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from distutils.sysconfig import get_python_lib
+
+from setuptools import setup
+
+site_packages_path = get_python_lib().replace(sys.prefix + os.path.sep, '')
+
+setup(
+    name="flask-wdb-hook",
+    version='0.2.1',
+    author="Florian Mounier @ kozea",
+    author_email="florian.mounier@kozea.fr",
+    url="http://github.com/Kozea/wdb",
+    license='GPLv3',
+    packages=[],
+    install_requires=['wdb >= 3.2.5'],
+    data_files=[(site_packages_path, ['flask-wdb.pth'])],
+    description="Hook to replace flask werkzeug debugger with wdb."
+)
