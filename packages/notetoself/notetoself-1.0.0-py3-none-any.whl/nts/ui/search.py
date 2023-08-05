a54c1b29323ad2d99fb4df6fa833c78700fc226c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:51 +0000

    Add package notetoself/1.0.0/notetoself-1.0.0-py3-none-any.whl

---
+++ packages/notetoself/notetoself-1.0.0-py3-none-any.whl/nts/ui/search.py
@@ -0,0 +1,14 @@
+from colorama import Fore
+
+import nts.api as nts
+
+
+def search_notes(query: str) -> int:
+    notes = nts.load_notes()
+
+    found = nts.find_notes(notes, query)
+    for name in found.keys():
+        print(f"{Fore.BLUE}* {Fore.RESET}{name}")
+
+    if not found:
+        print(f"{Fore.RED}No notes found.")
