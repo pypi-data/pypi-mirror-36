5066989cca2a7148e862328c0df5c1459e390916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:30 +0000

    Add package sknester/1.0.0/sknester-1.0.0.tar.gz

---
+++ packages/sknester/sknester-1.0.0.tar.gz/sknester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'sknester',
+    version = '1.0.0',
+    py_modules = ['sknester'],
+    author = 'sujan kumar',
+    author_email = 'dhodiumsujankumar@gmail.com',
+    url = 'http://www.dsujankumar.com/',
+    description = 'A simple printer to print items of deeply nested lists, with one item per line',
+    )
