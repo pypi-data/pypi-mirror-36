24b3e01a70b5619d8a5ea19a96b300ff87b10f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:30 +0000

    Add package duckietown-challenges/3.0.9/duckietown-challenges-3.0.9.tar.gz

---
+++ packages/duckietown-challenges/duckietown-challenges-3.0.9.tar.gz/duckietown-challenges-3.0.9/src/duckietown_challenges/utils.py
@@ -0,0 +1,150 @@
+import math
+import os
+
+import decorator
+import yaml
+
+from . import dclogger, InvalidConfiguration
+
+
+def write_data_to_file(data, filename):
+    """
+        Writes the data to the given filename.
+        If the data did not change, the file is not touched.
+
+    """
+    if not isinstance(data, str):
+        msg = 'Expected "data" to be a string, not %s.' % type(data).__name__
+        raise ValueError(msg)
+    if len(filename) > 256:
+        msg = 'Invalid argument filename: too long. Did you confuse it with data?'
+        raise ValueError(msg)
+
+    filename = expand_all(filename)
+    d8n_make_sure_dir_exists(filename)
+
+    if os.path.exists(filename):
+        current = open(filename).read()
+        if current == data:
+            if not 'assets/' in filename:
+                dclogger.debug('already up to date %s' % (filename))
+            return
+
+    tmp = filename + '.tmp'
+    with open(tmp, 'w') as f:
+        f.write(data)
+    os.rename(tmp, filename)
+    dclogger.debug('Written to: %s' % (filename))
+
+
+def expand_all(filename):
+    """
+        Expands ~ and ${ENV} in the string.
+
+        Raises DTConfigException if some environment variables
+        are not expanded.
+
+    """
+    fn = filename
+    fn = os.path.expanduser(fn)
+    fn = os.path.expandvars(fn)
+    if '$' in fn:
+        msg = 'Could not expand all variables in path %r.' % fn
+        raise ValueError(msg)
+    return fn
+
+
+def d8n_make_sure_dir_exists(filename):
+    """
+        Makes sure that the path to file exists, by creating directories.
+
+    """
+    dirname = os.path.dirname(filename)
+
+    # dir == '' for current dir
+    if dirname != '' and not os.path.exists(dirname):
+        d8n_mkdirs_thread_safe(dirname)
+
+
+def d8n_mkdirs_thread_safe(dst):
+    """
+        Make directories leading to 'dst' if they don't exist yet.
+
+        This version is thread safe.
+
+    """
+    if dst == '' or os.path.exists(dst):
+        return
+    head, _ = os.path.split(dst)
+    if os.sep == ':' and not ':' in head:
+        head += ':'
+    d8n_mkdirs_thread_safe(head)
+    try:
+        mode = 511  # 0777 in octal
+        os.mkdir(dst, mode)
+    except OSError as err:
+        if err.errno != 17:  # file exists
+            raise
+
+
+from contracts import indent, raise_wrapped
+
+
+@decorator.decorator
+def wrap_config_reader(f, x, *args, **kwargs):
+    """ Decorator for a function that takes a dict """
+
+    # def f2(x, *args, **kwargs):
+    try:
+        return f(x, *args, **kwargs)
+    except InvalidConfiguration as e:
+        msg = 'Could not interpret the configuration data using %s()' % f.__name__
+        msg += '\n\n' + indent(safe_yaml_dump(x), '  ')
+        raise_wrapped(InvalidConfiguration, e, msg, compact=True)
+    except Exception as e:
+        msg = 'Could not interpret the configuration data using %s()' % f.__name__
+        msg += '\n\n' + indent(safe_yaml_dump(x), '  ')
+        raise_wrapped(InvalidConfiguration, e, msg, compact=False)
+    # return f2
+
+
+def safe_yaml_dump(x):
+    s = yaml.safe_dump(x, encoding='utf-8', indent=4, allow_unicode=True)
+    return s
+
+
+def friendly_size(b):
+    if b == 0:
+        return 'empty'
+
+    if b < 1024:
+        return '%d  B' % b
+
+    if b < 1024 * 1024:
+        kbs = math.ceil(b / 1024.0)
+        return '%d KB' % kbs
+
+    if b < 1024 * 1024 * 1024:
+        mbs = math.ceil(b / (1024.0 * 1024.0))
+        return '%d MB' % mbs
+
+    gbs = b / (1024.0 * 1024.0 * 1024)
+    return '$.2f GB' % gbs
+
+def friendly_size2(b):
+    if b == 0:
+        return 'empty'
+
+    if b < 1024:
+        return '%d  B' % b
+
+    if b < 1024 * 1024:
+        kbs = b / 1024.0
+        return '%.2f KB' % kbs
+
+    if b < 1024 * 1024 * 1024:
+        mbs = b / (1024.0 * 1024.0)
+        return '%.2f MB' % mbs
+
+    gbs = b / (1024.0 * 1024.0 * 1024)
+    return '%.2f GB' % gbs
