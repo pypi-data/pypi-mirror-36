d214d7c1a97309758666eba52574ac26849c94a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:14 +0000

    Add package aiojira/0.1.5/aiojira-0.1.5-py3-none-any.whl

---
+++ packages/aiojira/aiojira-0.1.5-py3-none-any.whl/aiojira/__init__.pyi
@@ -0,0 +1,29 @@
+from typing import Dict, Tuple, Any, List
+
+
+class Resource(object):
+    displayName: Any
+    key: Any
+    name: Any
+    filename: Any
+    value: Any
+    scope: Any
+    votes: Any
+    id: Any
+    mimeType: Any
+    closed: Any
+
+    ...
+
+class Project(Resource): ...
+
+class JIRA(object):
+    @staticmethod
+    async def create(server: str = None, options: Dict = None, basic_auth: Tuple[str, str] = None,
+                     oauth: Dict[str, str] = None, jwt: Dict = None, kerberos: bool = False,
+                     kerberos_options: Dict[str, str] = None, validate: bool = False, get_server_info: bool = True,
+                     async_: bool = False, async_workers: int = 5, logging: bool = True, max_retries: int = 3,
+                     proxies: Any = None, timeout: int = None, auth: str = None):
+        ...
+
+    async def projects(self) -> List[Project]: ...
