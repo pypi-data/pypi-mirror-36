fecd56dcf41286458db78ae034286b4cc7e92760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:02 +0000

    Add package dci-utils/0.0.28/dci_utils-0.0.28.tar.gz

---
+++ packages/dci-utils/dci_utils-0.0.28.tar.gz/dci_utils-0.0.28/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dci_utils",
+    version="0.0.28",
+    author="Andrew Johnson",
+    author_email="andrejohnson@expedia.com",
+    description="A set of utilities for DCI jobs",
+    url="https://ewegithub.sb.karmalab.net/EWE/dci-data-dci-utils",
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
