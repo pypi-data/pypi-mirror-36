19404eaeb166371ce4403a94ef1a26208e4830d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:14 +0000

    Add package hiku/0.5.0rc3/hiku-0.5.0rc3.tar.gz

---
+++ packages/hiku/hiku-0.5.0rc3.tar.gz/hiku-0.5.0rc3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hiku',
+    version='0.5.0rc3',
+    description='Library to implement Graph APIs',
+    author='Vladimir Magamedov',
+    author_email='vladimir@magamedov.com',
+    url='https://github.com/vmagamedov/hiku',
+    packages=find_packages(exclude=['test*']),
+    include_package_data=True,
+    license='BSD',
+    install_requires=[],
+)
