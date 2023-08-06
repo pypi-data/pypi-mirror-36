e1a3b805cf192aeaa5d21d432f3e0a32f9445540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:50 +0000

    Add package ufobit/0.8/ufobit-0.8.tar.gz

---
+++ packages/ufobit/ufobit-0.8.tar.gz/ufobit-0.8/ufobit/network/rates.py
@@ -0,0 +1,193 @@
+from collections import OrderedDict
+from decimal import ROUND_DOWN
+from functools import wraps
+from time import time
+
+import requests
+
+from .utils import Decimal
+
+DEFAULT_CACHE_TIME = 60
+
+# Constant for use in deriving exchange
+# rates when given in terms of 1 BTC.
+ONE = Decimal(1)
+
+SATOSHI = 1
+BTC = 10 ** 8
+
+SUPPORTED_CURRENCIES = OrderedDict([
+    ('ufoshi', 'Ufoshi'),
+    ('ufo', 'UFO'),
+    ('satoshi', 'Satoshi'),
+    ('btc', 'Bitcoin'),
+    ('usd', 'United States Dollar'),
+    ('rub', 'Russian Ruble'),
+])
+
+# https://en.wikipedia.org/wiki/ISO_4217
+CURRENCY_PRECISION = {
+    'ufoshi': 0,
+    'ufo': 8,
+    'satoshi': 0,
+    'btc': 8,
+    'usd': 2,
+    'rub': 2,
+}
+
+
+def set_rate_cache_time(seconds):
+    global DEFAULT_CACHE_TIME
+    DEFAULT_CACHE_TIME = seconds
+
+
+def ufoshi_to_ufoshi():
+    return SATOSHI
+
+
+def ufo_to_ufoshi():
+    return BTC
+
+
+class RatesAPI:
+    """Each method converts exactly 1 unit of the currency to the equivalent
+    number of ufoshi.
+    """
+
+    MAIN_ENDPOINT = 'https://api.coinmarketcap.com/v1/ticker/uniform-fiscal-object/'
+
+    @classmethod
+    def currency_to_ufoshi(cls, currency):
+        r = requests.get(cls.MAIN_ENDPOINT, params={'convert': currency})
+        rate = r.json()[0]['price_' + currency.lower()]
+        return int(ONE / Decimal(rate) * BTC)
+
+    @classmethod
+    def usd_to_ufoshi(cls):
+        return cls.currency_to_ufoshi('usd')
+
+    @classmethod
+    def rub_to_ufoshi(cls):
+        return cls.currency_to_ufoshi('rub')
+
+    @classmethod
+    def btc_to_ufoshi(cls):
+        return cls.currency_to_ufoshi('btc')
+
+    @classmethod
+    def satoshi_to_ufoshi(cls):
+        return cls.currency_to_ufoshi('btc') / Decimal(BTC)
+
+
+EXCHANGE_RATES = {
+    'ufoshi': ufoshi_to_ufoshi,
+    'ufo': ufo_to_ufoshi,
+    'satoshi': RatesAPI.satoshi_to_ufoshi,
+    'btc': RatesAPI.btc_to_ufoshi,
+    'usd': RatesAPI.usd_to_ufoshi,
+    'rub': RatesAPI.rub_to_ufoshi,
+}
+
+
+def currency_to_ufoshi(amount, currency):
+    """Converts a given amount of currency to the equivalent number of
+    ufoshi. The amount can be either an int, float, or string as long as
+    it is a valid input to :py:class:`decimal.Decimal`.
+
+    :param amount: The quantity of currency.
+    :param currency: One of the :ref:`supported currencies`.
+    :type currency: ``str``
+    :rtype: ``int``
+    """
+    ufoshis = EXCHANGE_RATES[currency]()
+    return int(ufoshis * Decimal(amount))
+
+
+class CachedRate:
+    __slots__ = ('satoshis', 'last_update')
+
+    def __init__(self, satoshis, last_update):
+        self.satoshis = satoshis
+        self.last_update = last_update
+
+
+def currency_to_ufoshi_local_cache(f):
+    start_time = time()
+
+    cached_rates = dict([
+        (currency, CachedRate(None, start_time)) for currency in EXCHANGE_RATES.keys()
+    ])
+
+    @wraps(f)
+    def wrapper(amount, currency):
+        now = time()
+
+        cached_rate = cached_rates[currency]
+
+        if not cached_rate.satoshis or now - cached_rate.last_update > DEFAULT_CACHE_TIME:
+            cached_rate.satoshis = EXCHANGE_RATES[currency]()
+            cached_rate.last_update = now
+
+        return int(cached_rate.satoshis * Decimal(amount))
+
+    return wrapper
+
+
+@currency_to_ufoshi_local_cache
+def currency_to_ufoshi_local_cached():
+    pass  # pragma: no cover
+
+
+def currency_to_ufoshi_cached(amount, currency):
+    """Converts a given amount of currency to the equivalent number of
+    satoshi. The amount can be either an int, float, or string as long as
+    it is a valid input to :py:class:`decimal.Decimal`. Results are cached
+    using a decorator for 60 seconds by default. See :ref:`cache times`.
+
+    :param amount: The quantity of currency.
+    :param currency: One of the :ref:`supported currencies`.
+    :type currency: ``str``
+    :rtype: ``int``
+    """
+    return currency_to_ufoshi_local_cached(amount, currency)
+
+
+def ufoshi_to_currency(num, currency):
+    """Converts a given number of ufoshi to another currency as a formatted
+    string rounded down to the proper number of decimal places.
+
+    :param num: The number of ufoshi.
+    :type num: ``int``
+    :param currency: One of the :ref:`supported currencies`.
+    :type currency: ``str``
+    :rtype: ``str``
+    """
+    return '{:f}'.format(
+        Decimal(
+            num / Decimal(EXCHANGE_RATES[currency]())
+        ).quantize(
+            Decimal('0.' + '0' * CURRENCY_PRECISION[currency]),
+            rounding=ROUND_DOWN
+        ).normalize()
+    )
+
+
+def ufoshi_to_currency_cached(num, currency):
+    """Converts a given number of ufoshi to another currency as a formatted
+    string rounded down to the proper number of decimal places. Results are
+    cached using a decorator for 60 seconds by default. See :ref:`cache times`.
+
+    :param num: The number of ufoshi.
+    :type num: ``int``
+    :param currency: One of the :ref:`supported currencies`.
+    :type currency: ``str``
+    :rtype: ``str``
+    """
+    return '{:f}'.format(
+        Decimal(
+            num / Decimal(currency_to_ufoshi_cached(1, currency))
+        ).quantize(
+            Decimal('0.' + '0' * CURRENCY_PRECISION[currency]),
+            rounding=ROUND_DOWN
+        ).normalize()
+    )
---
+++ packages/ufobit/ufobit-0.8.tar.gz/ufobit-0.8/ufobit/network/services.py
@@ -0,0 +1,180 @@
+import re
+from functools import wraps
+
+import requests
+
+from . import currency_to_ufoshi
+from .meta import Unspent
+
+DEFAULT_TIMEOUT = 10
+
+
+class NoAPIKey(Exception):
+    pass
+
+
+def requires_key(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        if not config['api_key']:
+            raise NoAPIKey('Set ufobit.config["api_key"] to your CryptoID API key.')
+        return f(*args, **kwargs)
+    return decorator
+
+
+def set_service_timeout(seconds):
+    global DEFAULT_TIMEOUT
+    DEFAULT_TIMEOUT = seconds
+
+
+class UFO:
+    MAIN_ENDPOINT = 'https://explorer.ufobject.com/api'
+
+    @classmethod
+    def broadcast_tx(cls, tx_hex):
+        r = requests.post(
+            cls.MAIN_ENDPOINT + '/tx/send',
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            data={'rawtx': tx_hex}
+        )
+        r.raise_for_status()
+        if r.text == '0':
+            return False
+        return r.json()['txid']
+
+    @classmethod
+    def get_tx(cls, txid):
+        r = requests.get(cls.MAIN_ENDPOINT + f'/tx/{txid}')
+        r.raise_for_status()
+        if r.text == '0':
+            return False
+        return r.json()
+
+    @classmethod
+    def get_unspent(cls, address):
+        r = requests.get(cls.MAIN_ENDPOINT + f'/addr/{address}/utxo')
+        r.raise_for_status()
+        return [
+                   Unspent(int(tx['satoshis']),
+                           tx['confirmations'],
+                           tx['scriptPubKey'],
+                           tx['txid'],
+                           tx['vout'],
+                           True if tx['address'][0] == 'U' else False)  # sic! typo in api itself
+                   for tx in r.json()
+               ][::-1]
+
+    @classmethod
+    def get_balance(cls, address):
+        r = requests.get(cls.MAIN_ENDPOINT + f'/addr/{address}/balance')
+        r.raise_for_status()
+        print(r.json())
+        return r.json()
+
+    @classmethod
+    def get_transactions(cls, address):
+        r = requests.get(cls.MAIN_ENDPOINT + f'addr/{address}')
+        r.raise_for_status()
+        return [tx['txid'] for tx in r.json()['txs']]
+
+
+class NetworkAPI:
+    IGNORED_ERRORS = (ConnectionError,
+                      requests.exceptions.ConnectionError,
+                      requests.exceptions.Timeout,
+                      requests.exceptions.ReadTimeout)
+
+    GET_BALANCE_MAIN = [UFO.get_balance]
+    GET_TRANSACTIONS_MAIN = [UFO.get_transactions]
+    GET_UNSPENT_MAIN = [UFO.get_unspent]
+    BROADCAST_TX_MAIN = [UFO.broadcast_tx]
+    GET_TX_MAIN = [UFO.get_tx]
+
+    @classmethod
+    def get_tx(cls, txid):
+        for api_call in cls.GET_TX_MAIN:
+            try:
+                return api_call(txid)
+            except cls.IGNORED_ERRORS:
+                pass
+
+        raise ConnectionError('All APIs are unreachable.')
+
+    @classmethod
+    def get_balance(cls, address):
+        """Gets the balance of an address in satoshi.
+
+        :param address: The address in question.
+        :type address: ``str``
+        :raises ConnectionError: If all API services fail.
+        :rtype: ``int``
+        """
+
+        for api_call in cls.GET_BALANCE_MAIN:
+            try:
+                return api_call(address)
+            except cls.IGNORED_ERRORS:
+                pass
+
+        raise ConnectionError('All APIs are unreachable.')
+
+    @classmethod
+    def get_transactions(cls, address):
+        """Gets the ID of all transactions related to an address.
+
+        :param address: The address in question.
+        :type address: ``str``
+        :raises ConnectionError: If all API services fail.
+        :rtype: ``list`` of ``str``
+        """
+
+        for api_call in cls.GET_TRANSACTIONS_MAIN:
+            try:
+                return api_call(address)
+            except cls.IGNORED_ERRORS:
+                pass
+
+        raise ConnectionError('All APIs are unreachable.')
+
+    @classmethod
+    def get_unspent(cls, address):
+        """Gets all unspent transaction outputs belonging to an address.
+
+        :param address: The address in question.
+        :type address: ``str``
+        :raises ConnectionError: If all API services fail.
+        :rtype: ``list`` of :class:`~bit.network.meta.Unspent`
+        """
+
+        for api_call in cls.GET_UNSPENT_MAIN:
+            try:
+                return api_call(address)
+            except cls.IGNORED_ERRORS:
+                pass
+
+        raise ConnectionError('All APIs are unreachable.')
+
+    @classmethod
+    def broadcast_tx(cls, tx_hex):  # pragma: no cover
+        """Broadcasts a transaction to the blockchain.
+
+        :param tx_hex: A signed transaction in hex form.
+        :type tx_hex: ``str``
+        :raises ConnectionError: If all API services fail.
+        """
+        success = None
+
+        for api_call in cls.BROADCAST_TX_MAIN:
+            try:
+                c = api_call(tx_hex)
+                if not c:
+                    continue
+                return c
+            except cls.IGNORED_ERRORS:
+                pass
+
+        if success is False:
+            raise ConnectionError('Transaction broadcast failed, or '
+                                  'Unspents were already used.')
+
+        raise ConnectionError('All APIs are unreachable.')
