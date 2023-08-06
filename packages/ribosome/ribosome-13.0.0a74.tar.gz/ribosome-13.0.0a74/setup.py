82acc31a1563e3fcbc9e3fbeb92cac5de178db7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:03 +0000

    Add package ribosome/13.0.0a74/ribosome-13.0.0a74.tar.gz

---
+++ packages/ribosome/ribosome-13.0.0a74.tar.gz/ribosome-13.0.0a74/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+version_parts = (13, 0, 0, 'a', 74)
+version = '.'.join(map(str, version_parts))
+
+setup(
+    name='ribosome',
+    description='neovim plugin framework',
+    version=version,
+    author='Torsten Schmits',
+    author_email='torstenschmits@gmail.com',
+    license='MIT',
+    url='https://github.com/tek/ribosome',
+    packages=find_packages(exclude=['unit', 'unit.*', 'integration', 'integration.*', 'test', 'test.*']),
+    install_requires=[
+        'amino~=13.0.1a5',
+        'pyuv~=1.4.0',
+        'msgpack-python~=0.5.6'
+    ],
+    tests_require=[
+        'chiasma~=0.1.0.a28',
+        'kallikrein~=0.22.0a15',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ribosome_start_plugin = ribosome.cli:start_plugin',
+        ],
+    },
+)
