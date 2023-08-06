47e7727d9a156f74a09d4820b1e15bb178cc7519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:31 +0000

    Add package r6sapi/0.8.0/r6sapi-0.8.0.tar.gz

---
+++ packages/r6sapi/r6sapi-0.8.0.tar.gz/r6sapi-0.8.0/r6sapi/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'r6sapi'
+__author__ = 'billyoyo'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016-2018 billyoyo'
+__version__ = '0.8.0'
+
+from .r6sapi import *
+
