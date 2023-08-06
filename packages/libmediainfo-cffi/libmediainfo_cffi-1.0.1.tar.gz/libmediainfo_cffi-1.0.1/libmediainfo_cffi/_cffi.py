df9910c3bce8933dc541f73e8786d81dbe3e0d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:20 +0000

    Add package libmediainfo-cffi/1.0.1/libmediainfo_cffi-1.0.1.tar.gz

---
+++ packages/libmediainfo-cffi/libmediainfo_cffi-1.0.1.tar.gz/libmediainfo_cffi-1.0.1/libmediainfo_cffi/_cffi.py
@@ -0,0 +1,20 @@
+from cffi import FFI
+
+ffibuilder = FFI()
+
+with open('libmediainfo_cffi/mediainfo_wrapper.h', 'r') as fh:
+    wrapper_header = fh.read()
+
+with open("libmediainfo_cffi/mediainfo_wrapper.cpp", "r") as fh:
+    wrapper = fh.read()
+
+ffibuilder.cdef(wrapper_header)
+
+ffibuilder.set_source("_mediainfo_cffi",
+                      wrapper,
+                      libraries=['mediainfo'],
+                      source_extension='.cpp',
+                      extra_compile_args=['-D_UNICODE', '-I/usr/include/MediaInfo'])
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=True)
