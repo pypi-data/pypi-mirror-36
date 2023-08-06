d2f80806be3e584f929cb74a698bc6bc0f09dce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:38 +0000

    Add package corelated-logs/0.0.6/corelated_logs-0.0.6-py3-none-any.whl

---
+++ packages/corelated-logs/corelated_logs-0.0.6-py3-none-any.whl/corelated_logs/ext/requests/patch.py
@@ -0,0 +1,28 @@
+import logging
+
+import structlog
+import wrapt
+import uuid
+
+logger = logging.getLogger()
+
+
+def patch():
+    wrapt.wrap_function_wrapper("requests", "Session.prepare_request", _inject_header)
+
+
+def _inject_header(wrapped, instance, args, kwargs):
+    request = args[0]
+    headers = getattr(request, "headers", {})
+    inject_request_id_header(headers)
+    setattr(request, "headers", headers)
+    return wrapped(*args, **kwargs)
+
+
+def inject_request_id_header(headers):
+    try:
+        headers["X_CO_REQUEST_ID"] = str(
+            structlog.get_config()["context_class"]._tl.dict_["request_id"]
+        )
+    except Exception as e:
+        headers["X_CO_REQUEST_ID"] = str(uuid.uuid())
