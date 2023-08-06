44a312387025dd8f32743b673b832750d2063d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:26 +0000

    Add package zb/0.0.5/zb-0.0.5.tar.gz

---
+++ packages/zb/zb-0.0.5.tar.gz/zb-0.0.5/zb/base.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from collections import OrderedDict
+
+
+class AttrDict(dict):
+    """Dict that can get attribute by dot"""
+
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
+class OrderedAttrDict(OrderedDict):
+    """OrderedDict that can get attribute by dot"""
+
+    def __init__(self, *args, **kwargs):
+        super(OrderedAttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
+class ZbList(list):
+    """List that can get elements by discrete indices"""
+
+    def __init__(self, *args, **kwargs):
+        super(ZbList, self).__init__(*args, **kwargs)
+
+    def discrete_index(self, indices):
+        """get elements by discrete indices
+
+        :param indices: list
+            discrete indices
+        :return: elements
+        """
+        elements = []
+        for i in indices:
+            elements.append(self[i])
+        return elements
