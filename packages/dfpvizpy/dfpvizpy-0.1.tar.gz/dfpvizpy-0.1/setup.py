6fda3182c56b331346f7772a5baa6c151ff21556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:54 +0000

    Add package dfpvizpy/0.1/dfpvizpy-0.1.tar.gz

---
+++ packages/dfpvizpy/dfpvizpy-0.1.tar.gz/dfpvizpy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dfpvizpy',
+      version='0.1',
+      description='Data for Progress Viz Styling in Python',
+      #url='http://github.com/storborg/funniest',
+      author='Data For Progress',
+      license='MIT',
+      packages=['dfpvizpy'],
+      include_package_data=True,
+      zip_safe=False)
