aec1177eea87c88eedb88cb51aed51c5b40c5c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:34 +0000

    Add package torskel/0.1.80/torskel-0.1.80.tar.gz

---
+++ packages/torskel/torskel-0.1.80.tar.gz/torskel-0.1.80/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+VERSION = '0.1.80'
+
+setup(
+    name='torskel',
+    version=VERSION,
+    packages=find_packages(),
+    url='https://github.com/frostspb/torskel',
+    license='MIT',
+    author='Frostspb',
+    description='Python package with skeleton of the base Tornado application',
+    long_description="""Contains basic functions for logging, asynchronous redis,
+    support for reactJS and asynchronous http request Only Python 3.7+""",
+    keywords=["tornado"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.7",
+    ],
+    install_requires=[
+        'xmltodict>=0.11.0',
+
+
+    ],
+)
