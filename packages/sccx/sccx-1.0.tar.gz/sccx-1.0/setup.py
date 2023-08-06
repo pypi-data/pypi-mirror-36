082c5ce9ecc784685b74102f78f2ca39bf5d870b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:22 +0000

    Add package sccx/1.0/sccx-1.0.tar.gz

---
+++ packages/sccx/sccx-1.0.tar.gz/sccx-1.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sccx",
+    version="1.0",
+    author="Ziyadsk",
+    author_email="Ziyadkader@outlook.com",
+    description="A Great cheat cheet and a quick reference command line tool",
+    url="https://github.com/ziyadsk/scc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
