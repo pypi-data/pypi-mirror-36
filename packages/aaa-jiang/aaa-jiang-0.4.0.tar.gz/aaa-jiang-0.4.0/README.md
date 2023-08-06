fa3a709e3942647c67bb2238c492f1fc6e5d3490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:32 +0000

    Add package aaa-jiang/0.4.0/aaa-jiang-0.4.0.tar.gz

---
+++ packages/aaa-jiang/aaa-jiang-0.4.0.tar.gz/aaa-jiang-0.4.0/README.md
@@ -0,0 +1,4 @@
+# aaa
+<a href="https://teamcity.tek-pi.com/viewType.html?buildTypeId=Aaa_Build&guest=1">
+<img src="https://teamcity.tek-pi.com/app/rest/builds/buildType:(id:Aaa_Build)/statusIcon"/>
+</a>
---
+++ packages/aaa-jiang/aaa-jiang-0.4.0.tar.gz/aaa-jiang-0.4.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(name='aaa-jiang',
+      version='0.4.0',
+      description='Scalable Reconstruction Framework.',
+      url='https://github.com/threebegetsallthings/aaa.git',
+      author='hongjiang',
+      author_email='hushangjituan@outlook.com',
+      license='Apache',
+      packages=find_packages('aa'),
+      package_dir={'': 'aa'},
+      install_requires=['dxl-learn>0.0.13',
+                        'dxl-core',
+                        'tensorflow',
+                        'doufo'],
+      entry_points="""
+            [console_scripts]
+            srf=srf.api.cli.main:srf
+      """,
+      zip_safe=False)
