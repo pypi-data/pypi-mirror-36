7316f1a276b6b4a2eac03508630a7169e3f79ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:29 +0000

    Add package DataSuper/0.9.1/DataSuper-0.9.1.tar.gz

---
+++ packages/DataSuper/DataSuper-0.9.1.tar.gz/DataSuper-0.9.1/datasuper/__init__.py
@@ -0,0 +1,8 @@
+"""DataSuper - Organization for scientific projects."""
+
+__version__ = '0.1.0'
+__author__ = 'David C. Danko <dcdanko@gmail.com>'
+
+from .repo import *
+from datasuper import database
+from .factories import *
---
+++ packages/DataSuper/DataSuper-0.9.1.tar.gz/DataSuper-0.9.1/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+setuptools.setup(
+    name="DataSuper",
+    version="0.9.1",
+    url="https://github.com/dcdanko/DataSuper",
+
+    author="David C. Danko",
+    author_email="dcdanko@gmail.com",
+
+    description="Organization for scientific projects.",
+    long_description=open('README.rst').read(),
+
+    packages=['datasuper'],
+    package_dir={'datasuper': 'datasuper'},
+
+    install_requires=[
+        'click==6.7',
+        'py-archy==1.0.1',
+        'PyYAML==3.12',
+        'tinydb==3.5.0',
+        'ujson==1.35',
+        'yaml_backed_structs==0.9.0',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'datasuper=datasuper.cli:main'
+        ]
+    },
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
