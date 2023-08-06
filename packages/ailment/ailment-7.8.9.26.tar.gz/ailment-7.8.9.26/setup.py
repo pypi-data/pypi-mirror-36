b903949a03c4cfeda1bf4f838ebbfb6a4b801fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:15 +0000

    Add package ailment/7.8.9.26/ailment-7.8.9.26.tar.gz

---
+++ packages/ailment/ailment-7.8.9.26.tar.gz/ailment-7.8.9.26/setup.py
@@ -0,0 +1,19 @@
+
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+    packages = find_packages()
+except ImportError:
+    from distutils.core import setup
+    import os
+    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
+
+setup(
+    name='ailment',
+    version='7.8.9.26',
+    python_requires='<3.0',
+    packages=packages,
+    install_requires=[],
+    description='The angr intermediate language.',
+    url='https://github.com/angr/ailment',
+)
