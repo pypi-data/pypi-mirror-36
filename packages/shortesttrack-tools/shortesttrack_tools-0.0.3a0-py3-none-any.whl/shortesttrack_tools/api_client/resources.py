ce69a4e87356bbb541ee5500c6b09bb42521e924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:56 +0000

    Add package shortesttrack-tools/0.0.3a0/shortesttrack_tools-0.0.3a0-py3-none-any.whl

---
+++ packages/shortesttrack-tools/shortesttrack_tools-0.0.3a0-py3-none-any.whl/shortesttrack_tools/api_client/resources.py
@@ -0,0 +1,11 @@
+from urlobject import URLObject
+import os
+
+
+ROOT = URLObject(os.environ.get('HOST', 'https://shortesttrack.com'))
+
+UAA_SERVICE_ENDPOINT = ROOT.add_path('api/uaa')
+OAUTH_SERVICE_ENDPOINT = ROOT.add_path('oauth')
+JWTKEY_ENDPOINT = ROOT.add_path('oauth_jwtkey')
+METADATA_SERVICE_ENDPOINT = ROOT.add_path('api/metadata')
+EXECUTION_SCHEDULING_SERVICE_ENDPOINT = ROOT.add_path('api/exec-scheduling')
