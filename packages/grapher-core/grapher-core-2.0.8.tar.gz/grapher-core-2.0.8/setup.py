bd0ce745c01dbf17ea3409da7a63025c9bb93594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:49 +0000

    Add package grapher-core/2.0.8/grapher-core-2.0.8.tar.gz

---
+++ packages/grapher-core/grapher-core-2.0.8.tar.gz/grapher-core-2.0.8/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name='grapher-core',
+    version='2.0.8',
+    license='Apache Software License',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Systems Administration',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords=['data', 'visualization', 'monitoring', 'graphs'],
+    python_requires='~=3.7',
+    description='Grapher core server and libraries',
+    author='Volodymyr Paslavskyy',
+    author_email='qfoxic@gmail.com',
+    packages=['grapher.core'],
+    install_requires=['websockets==6.0'],
+    url='https://gitlab.com/grapher/grapher-core/',
+    download_url='https://gitlab.com/grapher/grapher-core/-/archive/2.0.8/grapher-core-2.0.8.tar.gz'
+)
