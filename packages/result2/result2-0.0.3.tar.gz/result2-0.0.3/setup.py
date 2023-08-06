42997da4b2a8fee1a2dd0e91b95e623a25ae27cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:22 +0000

    Add package result2/0.0.3/result2-0.0.3.tar.gz

---
+++ packages/result2/result2-0.0.3.tar.gz/result2-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(name='result2',
+            version='0.0.3',
+            description='A Swift-Style result type for Python',
+            author='Alexander Li',
+            author_email='superpowerlee@gmail.com',
+            url='https://github.com/ipconfiger/result2',
+            download_url='https://github.com/ipconfiger/result2/tarball/0.0.1',
+            packages=['result2'],
+            zip_safe=True,
+            include_package_data=True,
+            license='MIT',
+            keywords='swift result enum',
+            long_description="Swift style Result for Python",
+            classifiers=[
+                          'Development Status :: 4 - Beta',
+                          'License :: OSI Approved :: MIT License',
+                          'Programming Language :: Python :: 2',
+                          'Programming Language :: Python :: 3',
+                      ],
+      )
