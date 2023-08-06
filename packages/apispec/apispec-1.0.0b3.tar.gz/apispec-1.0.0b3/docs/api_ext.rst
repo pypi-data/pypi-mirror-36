4be6aa82df9813a47556ab6712249bdd9f4b2576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:56 +0000

    Add package apispec/1.0.0b3/apispec-1.0.0b3.tar.gz

---
+++ packages/apispec/apispec-1.0.0b3.tar.gz/apispec-1.0.0b3/docs/api_ext.rst
@@ -0,0 +1,32 @@
+Built-in Extensions
+===================
+
+apispec.ext.marshmallow
+-----------------------
+
+.. automodule:: apispec.ext.marshmallow
+    :members:
+
+apispec.ext.marshmallow.openapi
++++++++++++++++++++++++++++++++
+
+.. automodule:: apispec.ext.marshmallow.openapi
+    :members:
+
+apispec.ext.flask
+-----------------
+
+.. automodule:: apispec.ext.flask
+    :members:
+
+apispec.ext.tornado
+-------------------
+
+.. automodule:: apispec.ext.tornado
+    :members:
+
+apispec.ext.bottle
+-------------------
+
+.. automodule:: apispec.ext.bottle
+    :members:
---
+++ packages/apispec/apispec-1.0.0b3.tar.gz/apispec-1.0.0b3/tests/conftest.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from collections import namedtuple
+
+import pytest
+
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+
+
+@pytest.fixture(params=('2.0', '3.0.0'))
+def spec_fixture(request):
+    ma_plugin = MarshmallowPlugin()
+    spec = APISpec(
+        title='Validation',
+        version='0.1',
+        openapi_version=request.param,
+        plugins=(ma_plugin, ),
+    )
+    return namedtuple(
+        'Spec', ('spec', 'marshmallow_plugin', 'openapi'),
+    )(
+        spec, ma_plugin, ma_plugin.openapi,
+    )
+
+
+@pytest.fixture
+def spec(request, spec_fixture):
+    return spec_fixture.spec
