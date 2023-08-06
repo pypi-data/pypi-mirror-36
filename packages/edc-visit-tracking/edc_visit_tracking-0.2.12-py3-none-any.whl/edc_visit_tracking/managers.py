fd8af5ed708fea974a613c16952a75fd53e11101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:13 +0000

    Add package edc-visit-tracking/0.2.12/edc_visit_tracking-0.2.12-py3-none-any.whl

---
+++ packages/edc-visit-tracking/edc_visit_tracking-0.2.12-py3-none-any.whl/edc_visit_tracking/managers.py
@@ -0,0 +1,74 @@
+from django.db import models
+
+
+class CrfModelManager(models.Manager):
+    """A manager class for Crf models, models that have an FK to
+    the visit model.
+    """
+
+    use_in_migrations = True
+
+    def get_by_natural_key(self, subject_identifier, visit_schedule_name,
+                           schedule_name, visit_code, visit_code_sequence):
+        instance = self.model.visit_model_cls().objects.get_by_natural_key(
+            subject_identifier, visit_schedule_name, schedule_name,
+            visit_code, visit_code_sequence)
+        return self.get(**{self.model.visit_model_attr(): instance})
+
+    def get_for_visit(self, visit, **kwargs):
+        """Returns an instance for the given visit.
+        """
+        options = {self.model.visit_model_attr(): visit}
+        options.update(**kwargs)
+        return self.get(**options)
+
+    def filter_for_visit(self, visit, **kwargs):
+        """Returns an instance for the given visit.
+        """
+        options = {self.model.visit_model_attr(): visit}
+        options.update(**kwargs)
+        return self.filter(**options)
+
+    def get_for_subject_identifier(self, subject_identifier):
+        """Returns a queryset for the given subject_identifier.
+        """
+        options = {'{}__subject_identifier'.format(
+            self.model.visit_model_attr()): subject_identifier}
+        return self.filter(**options)
+
+
+class VisitModelManager(models.Manager):
+    """A manager class for visit models.
+    """
+
+    def get_by_natural_key(self, subject_identifier, visit_schedule_name,
+                           schedule_name, visit_code, visit_code_sequence):
+        return self.get(
+            subject_identifier=subject_identifier,
+            visit_schedule_name=visit_schedule_name,
+            schedule_name=schedule_name,
+            visit_code=visit_code,
+            visit_code_sequence=visit_code_sequence)
+
+    def last_visit(self, subject_identifier=None, visit_schedule_names=None,
+                   schedule_names=None):
+        """Returns the last visit for a subject.
+
+        By specify visit_schedule_name and/or schedule_name,
+        the last visit becomes the last visit for within the visit schedule
+        or schedule.
+
+        Note: schedule names are in
+        <visit_schedule_name>.<schedule_name> dot format
+        """
+        options = {}
+        if schedule_names:
+            schedule_names = [name.split('.')[-1] for name in schedule_names]
+            if not visit_schedule_names and '.' in schedule_names[0]:
+                visit_schedule_names = list(
+                    set([name.split('.')[0] for name in schedule_names]))
+        if subject_identifier:
+            options.update(dict(subject_identifier=subject_identifier))
+        if visit_schedule_names:
+            options.update(dict(visit_schedule_name__in=visit_schedule_names))
+        return self.filter(**options).order_by('report_datetime').last()
---
+++ packages/edc-visit-tracking/edc_visit_tracking-0.2.12-py3-none-any.whl/edc_visit_tracking/model_mixins/visit_model_mixin/visit_model_mixin.py
@@ -0,0 +1,142 @@
+import copy
+
+from django.core.exceptions import ImproperlyConfigured
+from django.db import models
+from django.db.models.deletion import PROTECT
+from edc_appointment.constants import IN_PROGRESS_APPT, COMPLETE_APPT
+from edc_appointment.models import Appointment
+from edc_base.model_managers.historical_records import HistoricalRecords
+from edc_constants.constants import YES, NO
+from edc_identifier.model_mixins import NonUniqueSubjectIdentifierFieldMixin
+from edc_visit_schedule.model_mixins import VisitScheduleModelMixin
+
+from ...choices import VISIT_REASON
+from ...constants import FOLLOW_UP_REASONS, REQUIRED_REASONS
+from ...constants import NO_FOLLOW_UP_REASONS, MISSED_VISIT
+from ...managers import VisitModelManager
+from ..previous_visit_model_mixin import PreviousVisitModelMixin
+from .visit_model_fields_mixin import VisitModelFieldsMixin
+
+
+class VisitModelMixin(
+        VisitModelFieldsMixin, VisitScheduleModelMixin,
+        NonUniqueSubjectIdentifierFieldMixin,
+        PreviousVisitModelMixin, models.Model):
+
+    """
+    For example:
+
+        class SubjectVisit(VisitModelMixin, CreatesMetadataModelMixin,
+                           RequiresConsentModelMixin, BaseUuidModel):
+
+            class Meta(VisitModelMixin.Meta):
+                app_label = 'my_app'
+    """
+    appointment = models.OneToOneField(Appointment, on_delete=PROTECT)
+
+    objects = VisitModelManager()
+
+    history = HistoricalRecords()
+
+    def __str__(self):
+        return f'{self.subject_identifier} {self.visit_code}.{self.visit_code_sequence}'
+
+    def save(self, *args, **kwargs):
+        self.subject_identifier = self.appointment.subject_identifier
+        self.visit_schedule_name = self.appointment.visit_schedule_name
+        self.schedule_name = self.appointment.schedule_name
+        self.visit_code = self.appointment.visit_code
+        self.visit_code_sequence = self.appointment.visit_code_sequence
+        self.require_crfs = NO if self.reason == MISSED_VISIT else YES
+        super().save(*args, **kwargs)
+
+    def natural_key(self):
+        return (self.subject_identifier,
+                self.visit_schedule_name,
+                self.schedule_name,
+                self.visit_code,
+                self.visit_code_sequence)
+    natural_key.dependencies = ['edc_appointment.appointment']
+
+    @property
+    def appointment_zero(self):
+        appointment_zero = None
+        try:
+            if self.appointment.visit_code_sequence == 0:
+                appointment_zero = self.appointment
+        except AttributeError:
+            pass
+        if not appointment_zero:
+            try:
+                appointment_zero = self.appointment.__class__.objects.get(
+                    subject_identifier=self.appointment.subject_identifier,
+                    visit_code_sequence=0)
+            except self.appointment.__class__.DoesNotExist:
+                pass
+        return appointment_zero
+
+    def get_visit_reason_no_follow_up_choices(self):
+        """Returns the visit reasons that do not imply any
+        data collection; that is, the subject is not available.
+        """
+        dct = {}
+        for item in NO_FOLLOW_UP_REASONS:
+            dct.update({item: item})
+        return dct
+
+    def get_visit_reason_follow_up_choices(self):
+        """Returns visit reasons that imply data is being collected;
+        that is, subject is present.
+        """
+        dct = {}
+        for item in FOLLOW_UP_REASONS:
+            dct.update({item: item})
+        return dct
+
+    def get_visit_reason_choices(self):
+        """Returns a tuple of the reasons choices for the reason field.
+        """
+        return VISIT_REASON
+
+    def _check_visit_reason_keys(self):
+        user_keys = (
+            [k for k in self.get_visit_reason_no_follow_up_choices().iterkeys()]
+            + [k for k in self.get_visit_reason_follow_up_choices().iterkeys()])
+        default_keys = copy.deepcopy(REQUIRED_REASONS)
+        if list(set(default_keys) - set(user_keys)):
+            missing_keys = list(set(default_keys) - set(user_keys))
+            if missing_keys:
+                raise ImproperlyConfigured(
+                    'User\'s visit reasons tuple must contain all keys '
+                    'for no follow-up {1} and all for follow-up {2}. '
+                    'Missing {3}. Override methods \'get_visit_reason_'
+                    'no_follow_up_choices\' and \'get_visit_reason_follow_'
+                    'up_choices\' on the visit model if you are not using '
+                    'the default keys of {4}. Got {0}'.format(
+                        user_keys,
+                        NO_FOLLOW_UP_REASONS,
+                        FOLLOW_UP_REASONS,
+                        missing_keys,
+                        REQUIRED_REASONS))
+
+    def post_save_check_appointment_in_progress(self):
+        if self.reason in self.get_visit_reason_no_follow_up_choices():
+            if self.appointment.appt_status != COMPLETE_APPT:
+                self.appointment.appt_status = COMPLETE_APPT
+                self.appointment.save()
+        else:
+            if self.appointment.appt_status != IN_PROGRESS_APPT:
+                self.appointment.appt_status = IN_PROGRESS_APPT
+                self.appointment.save()
+
+    class Meta:
+        abstract = True
+        unique_together = (
+            ('subject_identifier', 'visit_schedule_name',
+             'schedule_name', 'visit_code', 'visit_code_sequence'),
+            ('subject_identifier', 'visit_schedule_name',
+             'schedule_name', 'report_datetime'),
+        )
+        ordering = (('subject_identifier', 'visit_schedule_name',
+                     'schedule_name', 'visit_code', 'visit_code_sequence',
+                     'report_datetime', ))
