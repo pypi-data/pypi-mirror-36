9a520a0532453d6591d3ca9f590ee6affafc98fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:33 +0000

    Add package wagtail-tag-manager/0.0.8/wagtail_tag_manager-0.0.8-py2.py3-none-any.whl

---
+++ packages/wagtail-tag-manager/wagtail_tag_manager-0.0.8-py2.py3-none-any.whl/wagtail_tag_manager/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from wagtail_tag_manager.views import ManageView
+from wagtail_tag_manager.endpoints import lazy_endpoint
+
+app_name = "wtm"
+
+urlpatterns = [
+    path("manage/", ManageView.as_view(), name="manage"),
+    path("lazy/", lazy_endpoint, name="lazy"),
+]
