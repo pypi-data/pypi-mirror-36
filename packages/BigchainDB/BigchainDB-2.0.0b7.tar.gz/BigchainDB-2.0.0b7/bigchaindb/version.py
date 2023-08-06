2486522eff523523a63daebd5f33a96871a864d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:03 +0000

    Add package BigchainDB/2.0.0b7/BigchainDB-2.0.0b7.tar.gz

---
+++ packages/BigchainDB/BigchainDB-2.0.0b7.tar.gz/BigchainDB-2.0.0b7/bigchaindb/version.py
@@ -0,0 +1,8 @@
+# Copyright BigchainDB GmbH and BigchainDB contributors
+# SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
+# Code is Apache-2.0 and docs are CC-BY-4.0
+
+__version__ = '2.0.0b7'
+__short_version__ = '2.0b7'
+# supported Tendermint version
+__tm_supported_versions__ = ["0.22.8"]
