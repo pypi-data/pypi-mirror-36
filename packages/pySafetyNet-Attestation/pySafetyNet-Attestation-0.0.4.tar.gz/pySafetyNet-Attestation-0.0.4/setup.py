34ddc4147cf83e68b7d108c77a19297c62cab081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:05 +0000

    Add package pySafetyNet-Attestation/0.0.4/pySafetyNet-Attestation-0.0.4.tar.gz

---
+++ packages/pySafetyNet-Attestation/pySafetyNet-Attestation-0.0.4.tar.gz/pySafetyNet-Attestation-0.0.4/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pySafetyNet-Attestation",
+    version="0.0.4",
+    author="Daniel Roos",
+    author_email="daniel@roos.io",
+    description="Verify JWS-attestations from Google's SafetyNet.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RoosDaniel/SafetyNet-Attestation",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pyOpenSSL",
+        "certvalidator",
+        "jwcrypto",
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
