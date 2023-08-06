bcdeb9fd05f48f8067e66dc332e8d82f7625aa64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:13 +0000

    Add package fab-addon-geoalchemy/0.1.4/fab_addon_geoalchemy-0.1.4.tar.gz

---
+++ packages/fab-addon-geoalchemy/fab_addon_geoalchemy-0.1.4.tar.gz/fab_addon_geoalchemy-0.1.4/fab_addon_geoalchemy/version.py
@@ -0,0 +1,10 @@
+VERSION_MAJOR = 0
+VERSION_MINOR = 1
+VERSION_BUILD = 4
+VERSION_INFO = (VERSION_MAJOR, VERSION_MINOR, VERSION_BUILD)
+VERSION_STRING = "%d.%d.%d" % VERSION_INFO
+AUTHOR_NAME = "Dolf Andringa"
+DESCRIPTION = "Add support for GeoAlchemy2 Geometry fields to Flask Appbuilder"
+AUTHOR_EMAIL = "dolfandringa@gmail.com"
+
+__version__ = VERSION_INFO
