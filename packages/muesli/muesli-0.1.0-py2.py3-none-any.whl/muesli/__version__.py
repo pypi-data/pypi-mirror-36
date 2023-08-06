0948225ef46f442628da366503ef1d19c1010039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:31 +0000

    Add package muesli/0.1.0/muesli-0.1.0-py2.py3-none-any.whl

---
+++ packages/muesli/muesli-0.1.0-py2.py3-none-any.whl/muesli/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+VERSION = (0, 1, 0)
+
+__title__ = 'muesli'
+__description__ = 'Dummy Data for Humans.'
+__url__ = 'https://www.github.com/vinayak-mehta/muesli'
+__version__ = '.'.join(map(str, VERSION))
+__author__ = 'Vinayak Mehta'
+__author_email__ = 'vmehta94@gmail.com'
+__license__ = 'MIT License'
