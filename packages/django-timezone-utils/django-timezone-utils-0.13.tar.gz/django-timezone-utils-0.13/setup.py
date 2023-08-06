78fa537afd625ddb9cbe37eb1893ba7a1491a844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:20 +0000

    Add package django-timezone-utils/0.13/django-timezone-utils-0.13.tar.gz

---
+++ packages/django-timezone-utils/django-timezone-utils-0.13.tar.gz/django-timezone-utils-0.13/setup.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+from setuptools import setup
+import os
+
+version = __import__('timezone_utils').VERSION
+
+
+setup(
+    name='django-timezone-utils',
+    version=version,
+    description='Time Zone Utilities for Django Models',
+    long_description=open(
+        os.path.join(
+            os.path.dirname(__file__),
+            "README.rst"
+        )
+    ).read(),
+    author="Michael Barr",
+    author_email="micbarr+developer@gmail.com",
+    license="MIT",
+    packages=['timezone_utils'],
+    install_requires=[
+        'pytz',
+        'django>=1.11'
+    ],
+    zip_safe=False,
+    platforms='any',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries',
+    ],
+    url='http://github.com/michaeljohnbarr/django-timezone-utils/',
+)
