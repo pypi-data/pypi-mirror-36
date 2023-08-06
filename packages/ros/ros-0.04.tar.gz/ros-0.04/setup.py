6f8aa6489b9a8d37d721f6dfa19777a13f15c2dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:37 +0000

    Add package ros/0.04/ros-0.04.tar.gz

---
+++ packages/ros/ros-0.04.tar.gz/ros-0.04/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+#from pip.req import parse_requirements
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+install_reqs = parse_requirements("ros/requirements.txt") #, session="i")
+requirements = [str(r) for r in install_reqs]
+setup(
+    name = 'ros',
+    packages = [ 'ros' ], # this must be the same as the name above
+    package_data={ 'ros' : [
+        'requirements.txt',
+        '*.ros',
+        '*.yaml',
+        'bin/*',
+        'ros/*'
+    ]},
+    version = '0.04',
+    description = 'Ros Knowledge Network',
+    author = 'Steve Cox',
+    author_email = 'scox@renci.org',
+    install_requires = requirements,
+    include_package_data=True,
+    url = 'http://github.com/NCATS-Tangerine/ros.git',
+    download_url = 'http://github.com/NCATS-Tangerine/ros/archive/0.1.tar.gz',
+    keywords = [ 'knowledge', 'network', 'graph', 'biomedical' ],
+    classifiers = [ ],
+)
