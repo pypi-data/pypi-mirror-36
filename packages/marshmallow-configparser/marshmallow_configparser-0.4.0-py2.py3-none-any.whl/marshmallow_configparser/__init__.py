b0c4410203e39266b805b4dcd61454c3e8b25b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:13 +0000

    Add package marshmallow-configparser/0.4.0/marshmallow_configparser-0.4.0-py2.py3-none-any.whl

---
+++ packages/marshmallow-configparser/marshmallow_configparser-0.4.0-py2.py3-none-any.whl/marshmallow_configparser/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.4.0'
+
+import logging
+
+from .compatibility import is_marshmallow3
+from .convenience_fields import ConfigBoolean, ConfigInteger, ConfigString
+from .fields import (UUID, Boolean, Date, DateTime, Decimal, Dict, Email,
+                     Float, FormattedString, Function, Integer, List,
+                     LocalDateTime, Method, Number, String, Time, TimeDelta,
+                     Url)
+from .schema import ConfigParserSchema
+from .validators import IsNotBlank, IsNotNone
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
