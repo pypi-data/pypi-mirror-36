5f5c1a14b7dff5de12fe256b2974582db89fa2e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:52 +0000

    Add package pysabnzbd/1.1.0/pysabnzbd-1.1.0.tar.gz

---
+++ packages/pysabnzbd/pysabnzbd-1.1.0.tar.gz/pysabnzbd-1.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='pysabnzbd',
+    packages=['pysabnzbd'],
+    version='1.1.0',
+    description='Python wrapper for SABnzbd API',
+    author='Jerad Meisner',
+    author_email='jerad.meisner@gmail.com',
+    url='https://github.com/jeradM/pysabnzbd',
+    download_url='https://github.com/jeradM/pysabnzbd/archive/1.1.0.tar.gz',
+    keywords=['SABnzbd'],
+    install_requires=[
+        'aiohttp',
+        'async-timeout'
+    ],
+    classifiers=[]
+)
