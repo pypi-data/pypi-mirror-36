ccf0d8c20d5897c5c7c38df0307ad0be7aae02e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:49 +0000

    Add package parameterized_model/0.1.1/parameterized_model-0.1.1.tar.gz

---
+++ packages/parameterized_model/parameterized_model-0.1.1.tar.gz/parameterized_model-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='parameterized_model',
+    version='0.1.1',
+    packages=['parameterized_model',],
+    license='Apache License Version 2.0',
+    long_description='Parameterized any given model weights with a single vector.',
+    url='https://github.com/young-geng/parameterized_model',
+    author='Young',
+    author_email='young.gengxy@gmail.com',
+)
