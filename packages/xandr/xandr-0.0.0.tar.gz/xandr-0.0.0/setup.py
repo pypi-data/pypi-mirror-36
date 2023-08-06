2c626e9d95b1603f92e9cef02cf48ac3c2a3c2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:59 +0000

    Add package xandr/0.0.0/xandr-0.0.0.tar.gz

---
+++ packages/xandr/xandr-0.0.0.tar.gz/xandr-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='xandr',
+      version='0.0.0',
+      description='Dummy package for xandr package name usage',
+      url='',
+      author='mtaifi',
+      license='GPL-3',
+      packages=['xandr'],
+      zip_safe=False)
