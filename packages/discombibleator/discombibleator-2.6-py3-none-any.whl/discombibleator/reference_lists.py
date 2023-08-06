d5b7826b6de2e5b26216a0ab20a31722b9d1b50c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:51 +0000

    Add package discombibleator/2.6/discombibleator-2.6-py3-none-any.whl

---
+++ packages/discombibleator/discombibleator-2.6-py3-none-any.whl/discombibleator/reference_lists.py
@@ -0,0 +1,9 @@
+class Reference_Lists:
+
+  """Class of reference lists to be used in other functions.
+
+  """
+  def __init__(self):
+      self.multiword_signifiers = ["hour", "watch", "journey", "walk", "cubit", "cubits"]
+      self.ordinal_times = ["second", "third", "fourth", "sixth", "seventh", "ninth", "tenth", "eleventh"]
+      self.punctuation = [".", ";", ",", "!", "?", "(", ")", ":"]
