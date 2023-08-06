22707e480c10dc1715c30702bc18a099e9f767f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:19 +0000

    Add package mp/0.1.3/mp-0.1.3.tar.gz

---
+++ packages/mp/mp-0.1.3.tar.gz/mp-0.1.3/mp/engine/python/builtins.py
@@ -0,0 +1,39 @@
+from mp.core import error
+from mp.engine.python import attribute as attr
+from mp.engine.python.attribute import np as _np
+
+_print = print
+_max = max
+_min = min
+
+_const = lambda toward, value: attr.AttrConst(toward, value)
+_float_to_int = lambda args: [int(arg) for arg in args]
+
+
+def array(toward, args):
+    args = _float_to_int(args.get_values())
+    value = _np.zeros(shape=args)
+    return _const(toward, value)
+
+
+def print(toward, args):
+    values = args.get_values()
+    for arg, value in zip(args.list, values):
+        output = '%s = %s' % (arg.name, value)
+        _print(output)
+    toward.is_data = False
+    return None
+
+
+def max(toward, args):
+    args = args.get_values()
+    if len(args) < 2:
+        raise error.TooMuchOrLessArguments(toward.sub, 2, len(args), +1)
+    return _const(toward, _max(args))
+
+
+def min(toward, args):
+    args = args.get_values()
+    if len(args) < 2:
+        raise error.TooMuchOrLessArguments(toward.sub, 2, len(args), +1)
+    return _const(toward, _min(args))
