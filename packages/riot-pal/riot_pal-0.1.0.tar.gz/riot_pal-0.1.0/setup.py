1d676cd13b86827f2eb42a97604f93eb44718bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:27 +0000

    Add package riot-pal/0.1.0/riot_pal-0.1.0.tar.gz

---
+++ packages/riot-pal/riot_pal-0.1.0.tar.gz/riot_pal-0.1.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+    Setup file for riot_pal.
+"""
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+
+setup(
+    name="riot_pal",
+    version="0.1.0",
+    author="RIOT OS",
+    author_email="devel@riot-os.org",
+    license="MIT",
+    description="A protocol abstraction layer for RIOT and low level devices",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RIOT-OS",
+    packages=find_packages(),
+    platforms='any',
+    python_requires='>=3.4.*',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers"
+    ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "pytest-regtest", "pprint"],
+    install_requires=['pyserial'],
+)
