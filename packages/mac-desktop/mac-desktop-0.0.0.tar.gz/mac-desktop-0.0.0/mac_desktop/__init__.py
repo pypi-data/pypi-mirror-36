c9ca7001fbcfc8c07a50d823f8a34c3dda20fedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:47 +0000

    Add package mac-desktop/0.0.0/mac-desktop-0.0.0.tar.gz

---
+++ packages/mac-desktop/mac-desktop-0.0.0.tar.gz/mac-desktop-0.0.0/mac_desktop/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import applescript
+from public import public
+
+
+def _frontmost():
+    return applescript.tell.app("System Events", "name of (first process whose frontmost is true)").out
+
+
+@public
+def show():
+    hide()
+    applescript.tell.app("System Events", "key code 103")
+
+
+@public
+def hide():
+    frontmost = _frontmost()
+    applescript.tell.app("Dock", "activate")
+    applescript.tell.app(frontmost, "activate")
+
