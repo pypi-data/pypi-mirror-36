9b2bc33feb31ef466155beaad5160b4da426cf17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:30 +0000

    Add package dyz/0.11.12/dyz-0.11.12.tar.gz

---
+++ packages/dyz/dyz-0.11.12.tar.gz/dyz-0.11.12/setup.py
@@ -0,0 +1,46 @@
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+version = '0.11.12'
+
+setup(
+    name='dyz',
+    version=version,
+    description="dyz",
+    long_description=README,
+    classifiers=[
+    ],
+    keywords='dyz',
+    author='me',
+    author_email='me@example.org',
+    url='https://example.org',
+    license='LGPL v3',
+    py_modules=['dyz'],
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'odoorpc',
+        'prettytable',
+        'ConfigParser',
+        'XlsxWriter',
+        'lxml',
+        'pyyaml',
+        'validictory',
+        'python-dateutil',
+        'dyools',
+        'psycopg2',
+        'faker',
+        'wrapt',
+        'future',
+        'six',
+    ],
+    entry_points='''
+        [console_scripts]
+        oo=dyz:main
+        otm=dyz:main
+    ''',
+)
