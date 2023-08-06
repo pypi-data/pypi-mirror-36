3eea462b4a2e71df888e707eaa700f34f361d7d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:23 +0000

    Add package chaton/0.1.1/chaton-0.1.1.tar.gz

---
+++ packages/chaton/chaton-0.1.1.tar.gz/chaton-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='chaton',
+      version='0.1.1',
+      description='A small chatbot for social robots',
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+      ],
+      keywords='chatbot robot',
+      url='https://gitlab.com/etri_hmi/chaton',
+      author='Minsu Jang',
+      author_email='minsu@etri.re.kr',
+      license='GPL 3.0',
+      packages=['chaton'],
+      install_requires=[
+          'lark-parser',
+      ],
+      zip_safe=False)
