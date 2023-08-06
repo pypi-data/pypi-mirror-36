8e8ea27e20ac54381d44c740f5c194462cb80f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:00 +0000

    Add package actingweb/2.4.2/actingweb-2.4.2-py2.py3-none-any.whl

---
+++ packages/actingweb/actingweb-2.4.2-py2.py3-none-any.whl/actingweb/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "actor",
+    "oauth",
+    "auth",
+    "aw_proxy",
+    "peertrustee",
+    "property",
+    "subscription",
+    "trust",
+    "config",
+    "on_aw",
+    "aw_web_request"
+]
