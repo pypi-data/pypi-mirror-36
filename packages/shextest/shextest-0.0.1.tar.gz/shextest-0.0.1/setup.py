92be38a3f06b8507ee917c0b41f1d9c73ab2e5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:06 +0000

    Add package shextest/0.0.1/shextest-0.0.1.tar.gz

---
+++ packages/shextest/shextest-0.0.1.tar.gz/shextest-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='shextest',
+    version="0.0.1",
+    url="https://github.com/shexSpec/shexTest",
+    author='Eric Prud\'hommeaux',
+    author_email='eric+pypi@mayo.edu',
+    description='ShEx tests',
+    long_description='A pip-installable version of the ShEx test suite',
+    install_requires=[],
+    tests_require = [],
+    scripts=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Code Generators',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7']
+)
