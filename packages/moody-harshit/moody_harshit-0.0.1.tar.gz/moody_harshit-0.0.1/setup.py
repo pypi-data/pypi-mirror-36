19b45a8f91cd21be366ab01256a96523d5b5d01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:15 +0000

    Add package moody-harshit/0.0.1/moody_harshit-0.0.1.tar.gz

---
+++ packages/moody-harshit/moody_harshit-0.0.1.tar.gz/moody_harshit-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='moody_harshit',
+    version='0.0.1',
+    description='a pip-installable package example',
+    packages=['moody_harshit'],
+    author='Harshit',
+    author_email='harshitagarwal37@gmail.com',
+    keywords=['example'],
+    url='https://github.com/bgse/hellostackoverflow'
+)
