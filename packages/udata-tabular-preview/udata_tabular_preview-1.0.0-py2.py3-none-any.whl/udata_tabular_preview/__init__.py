a63b8d9c2c4893f9cc8daa01e7a5bee0ba9e5b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:49 +0000

    Add package udata-tabular-preview/1.0.0/udata_tabular_preview-1.0.0-py2.py3-none-any.whl

---
+++ packages/udata-tabular-preview/udata_tabular_preview-1.0.0-py2.py3-none-any.whl/udata_tabular_preview/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+Tabular Preview
+
+A plugin to preview tabular files (CSV, Excel, ...)
+'''
+from __future__ import unicode_literals
+
+__version__ = '1.0.0'
+__description__ = 'A plugin to preview tabular files (CSV, Excel, ...) using csvapi'
