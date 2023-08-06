84bb2e799c0f2bb4c330dc26dbbec6a91ed2eacc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:21 +0000

    Add package bio2bel-kegg/0.1.2/bio2bel_kegg-0.1.2.tar.gz

---
+++ packages/bio2bel-kegg/bio2bel_kegg-0.1.2.tar.gz/bio2bel_kegg-0.1.2/src/bio2bel_kegg/parsers/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""This module contains multiple parsers for the KEGG public data sources."""
+
+from . import entities, pathways, description
+from .description import *
+from .entities import *
+from .pathways import *
+
+__all__ = (
+        description.__all__ +
+        entities.__all__ +
+        pathways.__all__
+)
