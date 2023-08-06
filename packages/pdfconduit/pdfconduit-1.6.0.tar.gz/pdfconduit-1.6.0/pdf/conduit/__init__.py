ded421bf3ce1ba97627582884a7c060717868b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:03 +0000

    Add package pdfconduit/1.6.0/pdfconduit-1.6.0.tar.gz

---
+++ packages/pdfconduit/pdfconduit-1.6.0.tar.gz/pdfconduit-1.6.0/pdf/conduit/__init__.py
@@ -0,0 +1,6 @@
+from pdf.conduit.encrypt import Encrypt
+from pdf.conduit.merge import Merge
+from pdf.conduit.watermark import WatermarkAdd, Watermark, Label
+
+
+__all__ = ["Encrypt", "Merge", "Watermark", "Label", "WatermarkAdd"]
