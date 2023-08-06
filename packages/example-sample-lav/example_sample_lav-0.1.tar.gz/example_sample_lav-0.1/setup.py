1f2a33ec1a0bf1ad64ab9a1ba9ec8464c07b99d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:04 +0000

    Add package example-sample-lav/0.1/example_sample_lav-0.1.tar.gz

---
+++ packages/example-sample-lav/example_sample_lav-0.1.tar.gz/example_sample_lav-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='example_sample_lav',
+      version='0.1',
+      description='1st prog',
+      url='https://www.python.org/lavanya123/example_sample_lav/',
+      author='lavanya123',
+      author_email='lavanyapavuluri@123.com',
+      license='MIT',
+      packages=['example_sample_lav'],
+      zip_safe=False)
