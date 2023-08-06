236ea62530c5863315bb446897b0f052aae7eb25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:53 +0000

    Add package aepp-sdk/0.22.0.1rc1/aepp_sdk-0.22.0.1rc1-py3-none-any.whl

---
+++ packages/aepp-sdk/aepp_sdk-0.22.0.1rc1-py3-none-any.whl/aeternity/tests/__init__.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from aeternity.config import Config
+from aeternity.signing import KeyPair
+
+logging.getLogger("requests").setLevel(logging.DEBUG)
+logging.getLogger("urllib3").setLevel(logging.DEBUG)
+
+
+PUBLIC_KEY = os.environ.get('WALLET_PUB')
+PRIVATE_KEY = os.environ.get('WALLET_PRIV')
+NODE_URL = os.environ.get('TEST_URL')
+NODE_URL_INTERNAL = os.environ.get('TEST_INTERNAL_URL')
+EPOCH_VERSION = '0.22.0'
+# set the key folder as environment variables
+KEYPAIR = KeyPair.from_public_private_key_strings(PUBLIC_KEY, PRIVATE_KEY)
+
+
+Config.set_defaults(Config(
+    external_url=NODE_URL,
+    internal_url=NODE_URL_INTERNAL
+))
