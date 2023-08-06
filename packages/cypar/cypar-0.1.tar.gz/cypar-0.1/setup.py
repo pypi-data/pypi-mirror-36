0b79ff40a53f9a1b795eae273a56d677f4c93ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:09 +0000

    Add package cypar/0.1/cypar-0.1.tar.gz

---
+++ packages/cypar/cypar-0.1.tar.gz/cypar-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cypar',
+      version='0.1',
+      description='Package handling cy-type parameter files',
+      url='http://gitlab.com/cycyustc/cypar',
+      author='Yang Chen',
+      author_email='cycyustc@gmail.com',
+      license='CYCY',
+      packages=['cypar'],
+      zip_safe=False)
