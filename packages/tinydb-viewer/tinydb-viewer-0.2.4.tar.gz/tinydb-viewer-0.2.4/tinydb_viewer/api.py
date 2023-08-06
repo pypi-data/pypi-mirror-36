a21e62ca4fa5e274f14dbb8188a2ce6c48df6087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:39 +0000

    Add package tinydb-viewer/0.2.4/tinydb-viewer-0.2.4.tar.gz

---
+++ packages/tinydb-viewer/tinydb-viewer-0.2.4.tar.gz/tinydb-viewer-0.2.4/tinydb_viewer/api.py
@@ -0,0 +1,43 @@
+from flask import request, jsonify, Response
+
+from . import app
+from .config import config
+
+
+@app.route('/api/create', methods=['POST'])
+def create_table():
+    r = request.get_json()
+    if r['fileId'] == config['get_file_id']():
+        config.update({
+            'table': config['tinydb'].table(r['tableName']),
+            'handsontable': r['handsontable']
+        })
+
+        return Response(status=201)
+    else:
+        return Response(status=304)
+
+
+@app.route('/api/edit', methods=['POST'])
+def edit_record():
+    record = request.get_json()
+    record_id = config['table'].update({
+        record['fieldName']: record['data']
+    }, config['query'].id == record['id'])
+    record = config['table'].get(record_id)
+
+    return jsonify({
+        'id': record_id,
+        'record': record
+    }), 201
+
+
+@app.route('/api/delete/<int:record_id>', methods=['DELETE'])
+def delete_record(record_id):
+    record = config['table'].get(record_id)
+    config['table'].remove(doc_ids=[record_id])
+
+    return jsonify({
+        'id': record_id,
+        'record': record
+    }), 303
