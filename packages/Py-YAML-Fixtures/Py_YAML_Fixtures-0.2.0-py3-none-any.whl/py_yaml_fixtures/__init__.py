09dffd432f44d7e8aac41fb7e7104011d1f418ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:09 +0000

    Add package Py-YAML-Fixtures/0.2.0/Py_YAML_Fixtures-0.2.0-py3-none-any.whl

---
+++ packages/Py-YAML-Fixtures/Py_YAML_Fixtures-0.2.0-py3-none-any.whl/py_yaml_fixtures/__init__.py
@@ -0,0 +1,14 @@
+"""
+    py_yaml_fixtures
+    ~~~~~~~~~~~~~~~~
+
+    A simple library to load database fixtures from Jinja-templated YAML files
+
+    :copyright: Copyright © 2018 Brian Cappello
+    :license: MIT, see LICENSE for more details
+"""
+
+__version__ = '0.2.0'
+
+
+from .fixtures_loader import FixturesLoader
