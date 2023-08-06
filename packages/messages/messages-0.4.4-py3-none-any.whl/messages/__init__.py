f90f6a7aa52e6f60a796a588e6b720ea2e98f93d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:29 +0000

    Add package messages/0.4.4/messages-0.4.4-py3-none-any.whl

---
+++ packages/messages/messages-0.4.4-py3-none-any.whl/messages/__init__.py
@@ -0,0 +1,20 @@
+"""
+messages - A package designed to make sending various types of messages easy.
+"""
+
+import logging
+
+from .api import send
+
+from .email_ import Email
+from .slack import SlackWebhook
+from .slack import SlackPost
+from .telegram import TelegramBot
+from .text import Twilio
+
+
+__version__ = '0.4.4'
+
+
+# Setup logger
+logging.getLogger(__name__).addHandler(logging.NullHandler())
