9e36259008e7c15b39a241bca6d85e3ea39239d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:33 +0000

    Add package django-agnocomplete/0.13.0/django-agnocomplete-0.13.0.tar.gz

---
+++ packages/django-agnocomplete/django-agnocomplete-0.13.0.tar.gz/django-agnocomplete-0.13.0/agnocomplete/exceptions.py
@@ -0,0 +1,32 @@
+"""
+Agnocomplete exception classes
+"""
+from requests.exceptions import HTTPError
+
+
+class UnregisteredAgnocompleteException(Exception):
+    """
+    Occurs when trying to instanciate an unregistered Agnocompletion class
+    """
+    pass
+
+
+class AuthenticationRequiredAgnocompleteException(Exception):
+    """
+    Occurs when trying to instanciate an unregistered Agnocompletion class
+    """
+    pass
+
+
+class ImproperlyConfiguredView(Exception):
+    """
+    Occurs if you want to misuse an AgnocompleteGenericView
+    """
+    pass
+
+
+class HTTPError(HTTPError):
+    """
+    Occurs when the 3rd party API returns an error code
+    """
+    pass
