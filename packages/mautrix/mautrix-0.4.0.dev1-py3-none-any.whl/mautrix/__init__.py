e3dc93356294de12bf6738630a872e557047936d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:15 +0000

    Add package mautrix/0.4.0.dev1/mautrix-0.4.0.dev1-py3-none-any.whl

---
+++ packages/mautrix/mautrix-0.4.0.dev1-py3-none-any.whl/mautrix/__init__.py
@@ -0,0 +1,4 @@
+from .api import HTTPAPI, MatrixError, MatrixRequestError, MatrixResponseError
+from .client import ClientAPI
+from .appservice import AppService, AppServiceAPI, IntentAPI, IntentError
+from .__meta__ import __version__, __author__
