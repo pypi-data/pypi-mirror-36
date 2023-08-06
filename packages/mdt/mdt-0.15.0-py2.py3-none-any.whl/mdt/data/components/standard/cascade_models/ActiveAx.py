613ef55764529b4e9e3735bc3b06eb8485631088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:25 +0000

    Add package mdt/0.15.0/mdt-0.15.0-py2.py3-none-any.whl

---
+++ packages/mdt/mdt-0.15.0-py2.py3-none-any.whl/mdt/data/components/standard/cascade_models/ActiveAx.py
@@ -0,0 +1,39 @@
+from mdt import CascadeTemplate
+
+__author__ = 'Robbert Harms'
+__date__ = '2017-07-19'
+__maintainer__ = 'Robbert Harms'
+__email__ = 'robbert.harms@maastrichtuniversity.nl'
+__licence__ = 'LGPL v3'
+
+
+class ActiveAx(CascadeTemplate):
+
+    models = ('BallStick_r1 (Cascade)',
+              'ActiveAx')
+    inits = {'ActiveAx': {'CylinderGPD.theta': 'Stick0.theta',
+                          'CylinderGPD.phi': 'Stick0.phi'}}
+
+
+class ActiveAx_ExVivo(CascadeTemplate):
+
+    models = ('BallStick_r1 (Cascade)',
+              'ActiveAx_ExVivo')
+    inits = {'ActiveAx_ExVivo': {'CylinderGPD.theta': 'Stick0.theta',
+                                 'CylinderGPD.phi': 'Stick0.phi'}}
+
+
+class ActiveAx_Fixed(CascadeTemplate):
+
+    models = ('BallStick_r1 (Cascade)',
+              'ActiveAx')
+    fixes = {'ActiveAx': {'CylinderGPD.theta': 'Stick0.theta',
+                          'CylinderGPD.phi': 'Stick0.phi'}}
+
+
+class ActiveAx_ExVivo_Fixed(CascadeTemplate):
+
+    models = ('BallStick_r1 (Cascade)',
+              'ActiveAx_ExVivo')
+    fixes = {'ActiveAx_ExVivo': {'CylinderGPD.theta': 'Stick0.theta',
+                                 'CylinderGPD.phi': 'Stick0.phi'}}
