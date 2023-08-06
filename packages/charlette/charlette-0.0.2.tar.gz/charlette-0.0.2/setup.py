e115b72a5016296e09ebc3731f00f65bfa98637b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:54 +0000

    Add package charlette/0.0.2/charlette-0.0.2.tar.gz

---
+++ packages/charlette/charlette-0.0.2.tar.gz/charlette-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+from charlette.__version__ import __version__
+
+setup(
+    name="charlette",
+    version=__version__,
+    packages=find_packages(),
+    install_requires=["Django", "channels", "starlette", "innate==0.1.1"],
+    license="MIT License",
+    url="https://github.com/erm/charlette",
+    description="Django app that uses Starlette components with Channels.",
+    entry_points={"console_scripts": ["charlette = charlette.__main__:innate.cli"]},
+    author="Jordan Eremieff",
+    author_email="jordan@eremieff.com",
+)
