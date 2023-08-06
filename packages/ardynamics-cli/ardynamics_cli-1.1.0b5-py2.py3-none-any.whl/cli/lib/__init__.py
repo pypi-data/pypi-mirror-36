54c63ca2a2ba6d3d68ddb7f72567107fad2bec72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:46 +0000

    Add package ardynamics-cli/1.1.0b5/ardynamics_cli-1.1.0b5-py2.py3-none-any.whl

---
+++ packages/ardynamics-cli/ardynamics_cli-1.1.0b5-py2.py3-none-any.whl/cli/lib/__init__.py
@@ -0,0 +1,7 @@
+from .domain import Domain
+from .config import Config
+from .mariadb import MariaDB
+from .httpd import Httpd
+from .php import Php
+from .user import User
+from .error import *
---
+++ packages/ardynamics-cli/ardynamics_cli-1.1.0b5-py2.py3-none-any.whl/cli/lib/mariadb.py
@@ -0,0 +1,36 @@
+import os
+import mysql.connector as mariadb
+
+
+class MariaDB:
+    _config = None
+    _connection = None
+
+    def __init__(self, config):
+        """
+        :type config: Config
+        """
+        self._config = config
+        db_hostname = 'localhost'
+        db_username = 'root'
+        db_password = config.get_secret('db_root_password', True)
+        self._connection = mariadb.connect(host=db_hostname, user=db_username, passwd=db_password)
+
+    def create_database(self, username, password):
+        print('    Creating database ' + username + '...')
+        statements = [
+            str.join("", ['CREATE DATABASE IF NOT EXISTS ', username]),
+            str.join("", ['CREATE USER IF NOT EXISTS \'', username, '\'@\'localhost\' IDENTIFIED BY \'', password, '\'']),
+            str.join("", ['GRANT ALL PRIVILEGES ON ', username, '.* TO \'', username, '\'@\'localhost\''])
+        ]
+        cursor = self._connection.cursor()
+        for query in statements:
+            cursor.execute(query)
+
+    def import_database(self, database_name, backup_file):
+        print('    Importing SQL dump file... ' + backup_file)
+        if not os.path.isfile(backup_file):
+            from cli.lib import InputError
+            raise InputError('Unable to find dump file: ' + backup_file)
+        db_password = self._config.get_secret('db_root_password', True)
+        os.system('mysql -u root -p' + db_password + ' ' + database_name + ' < ' + backup_file)
