c63b7d461920a6e95a1b94891b1717b214deefd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:42 +0000

    Add package pwbs/0.4.0.dev0/pwbs-0.4.0.dev0.tar.gz

---
+++ packages/pwbs/pwbs-0.4.0.dev0.tar.gz/pwbs-0.4.0.dev0/pwbs/__main__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""PAiP Web Build System
+
+NAME - PAiP Web Build System
+AUTHOR - Patryk Adamczyk <patrykadamczyk@paipweb.com>
+LICENSE - MIT
+
+"""
+# Imports
+
+from . import main
+
+# Underscore Variables
+
+"""Author of the module"""
+__author__ = 'Patryk Adamczyk'
+"""Module License"""
+__license__ = 'MIT'
+"""Documentation format"""
+__docformat__ = 'restructuredtext en'
+
+# Main Script
+main()
