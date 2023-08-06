32419dc7425b3ceed7c063bfcc5623895764f2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:26 +0000

    Add package ciecam02/1.0.4/ciecam02-1.0.4.tar.gz

---
+++ packages/ciecam02/ciecam02-1.0.4.tar.gz/ciecam02-1.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ciecam02",
+    version="1.0.4",
+    author="Dannyvi",
+    author_email="dannyvis@icloud.com",
+    description="An easy fast transformer between rgb and ciecam02 color space.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dannyvi/ciecam02",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy >= 1.7.1",
+    ],
+)
+
