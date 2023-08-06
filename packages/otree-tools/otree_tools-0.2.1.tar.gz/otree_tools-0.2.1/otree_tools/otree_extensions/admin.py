f1e8fdeef17c91c66546322d63f91152f42d3560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:01 +0000

    Add package otree-tools/0.2.1/otree_tools-0.2.1.tar.gz

---
+++ packages/otree-tools/otree_tools-0.2.1.tar.gz/otree_tools-0.2.1/otree_tools/otree_extensions/admin.py
@@ -0,0 +1,6 @@
+# TODO: incorporate ~admin to include mturk, paypal and json export
+
+
+from otree_tools.views import EnterExitEventList, FocusEventList, AllSessionsList
+
+data_export_views = [AllSessionsList, EnterExitEventList, FocusEventList, ]
