081fb77b6d10ef4c02fa556bb6568e78a04fb7c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:46 +0000

    Add package corelated-logs/0.0.7/corelated_logs-0.0.7.tar.gz

---
+++ packages/corelated-logs/corelated_logs-0.0.7.tar.gz/corelated_logs-0.0.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="corelated_logs",
+    version="0.0.7",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["structlog==18.2.0", "wrapt==1.10.11", "pre-commit-hooks==1.4.0"],
+    url="",
+    license="BSD",
+    author="sohit kumar",
+    author_email="sumitk002@gmail.com",
+    test_suite="tests",
+    description="A python library to generate corelation id and bind it to headers in outgoing request",
+)
