653e209683fe45eed9d12623d7589f082d0de111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:32 +0000

    Add package lib-bgp-data/0.1.41/lib_bgp_data-0.1.41.tar.gz

---
+++ packages/lib-bgp-data/lib_bgp_data-0.1.41.tar.gz/lib_bgp_data-0.1.41/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lib_bgp_data',
+    packages=find_packages(),
+    version='0.1.41',
+    author='Justin Furuness',
+    author_email='jfuruness@gmail.com',
+    url='https://github.com/jfuruness/lib_bgp_data.git',
+    download_url='https://github.com/jfuruness/lib_bgp_data.git',
+    keywords=['Furuness', 'furuness', 'pypi', 'package'],  # arbitrary keywords
+    install_requires=[
+        'pybgpstream',
+        'setuptools',
+        'psycopg2',
+        'requests',
+        'bs4'
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'],
+    entry_points={
+        'console_scripts': [
+            'parse_bgpstream.com = lib_bgpstream_parser.__main__:main'
+        ]},
+)
+
