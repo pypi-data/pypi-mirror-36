63c142f3aae0d514ae3022853b8a845292d16036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:17 +0000

    Add package mdx_attr_cols/0.2/mdx-attr-cols-0.2.tar.gz

---
+++ packages/mdx_attr_cols/mdx-attr-cols-0.2.tar.gz/mdx-attr-cols-0.2/mdx_attr_cols.py
@@ -0,0 +1,88 @@
+""" Bootstrap 3 grid extension fo Python Markdown
+"""
+
+from markdown.util import etree
+from markdown import Extension
+from markdown.treeprocessors import Treeprocessor
+
+
+class AttrColTreeProcessor(Treeprocessor):
+    """ The attr_cols element tree processor. """
+
+    CONF_DEFAULTS = {
+        'columns': 12,
+        'attr': 'cols',
+        'tags': ['section'],
+    }
+
+    def __init__(self, md, conf):
+        super(AttrColTreeProcessor, self).__init__(md)
+        conf_vars = self.CONF_DEFAULTS.copy()
+        conf_vars.update(conf or {})
+        self.columns = int(conf_vars.get('columns'))
+        self.attr = str(conf_vars.get('attr'))
+        self.tags = set(conf_vars.get('tags'))
+
+    def create_rows(self, node):
+        row, row_cols = None, 0
+        for i, child in enumerate(list(node)):
+            if child.tag.lower() not in self.tags:
+                continue
+
+            if self.attr not in child.attrib:
+                if row is None:
+                    # if there currently isn't a row, search sub-sections
+                    self.create_rows(child)
+                else:
+                    row, row_cols = None, 0
+                continue
+            cols = int(child.attrib[self.attr])
+            del child.attrib[self.attr]
+
+            if row is None:
+                row = etree.Element("div", {"class": "row"})
+                node.insert(i, row)
+
+            col = etree.SubElement(row, "div", {
+                "class": "col-md-%d" % (cols,),
+            })
+            row_cols += cols
+
+            col.append(child)
+            node.remove(child)
+
+            if row_cols >= self.columns:
+                row, row_cols = None, 0
+
+    def run(self, root):
+        self.create_rows(root)
+        return root
+
+
+class AttrColExtension(Extension):
+    """ The attr_cols markdown extension. """
+
+    REQUIRED_EXTENSIONS = ('attr_list', 'mdx_outline')
+    REQUIRED_EXTENSION_INTERNAL_NAMES = ('attr_list', 'outline')
+
+    def __init__(self, configs):
+        self.conf = configs
+
+    def extendMarkdown(self, md, md_globals=None):
+        """Initializes markdown extension components."""
+        if any(
+                x not in md.treeprocessors
+                for x in self.REQUIRED_EXTENSION_INTERNAL_NAMES):
+            raise RuntimeError(
+                "The attr_cols markdown extension depends the following"
+                " extensions which must preceded it in the extension"
+                " list: %s" % ", ".join(self.REQUIRED_EXTENSIONS))
+        processor = AttrColTreeProcessor(md, self.conf)
+        md.treeprocessors.register(
+            processor, 'attr_cols',
+            5)  # 5 is the lowest priority
+
+
+def makeExtension(**kwargs):
+    """ Initialize the attr_cols extension. """
+    return AttrColExtension(configs=kwargs)
---
+++ packages/mdx_attr_cols/mdx-attr-cols-0.2.tar.gz/mdx-attr-cols-0.2/tests.py
@@ -0,0 +1,118 @@
+from unittest import TestCase
+
+import xmltodict
+
+from markdown import Markdown
+from markdown.util import etree
+
+from mdx_attr_cols import AttrColTreeProcessor, AttrColExtension, makeExtension
+
+
+class XmlTestCaseMixin(object):
+    def mk_doc(self, s):
+        return etree.fromstring(
+            "<div>" + s.strip() + "</div>")
+
+    def assert_xml_equal(self, a, b):
+        self.assertEqual(
+            xmltodict.parse(etree.tostring(a)),
+            xmltodict.parse(etree.tostring(b)))
+
+
+class TestAttrColTreeProcessor(XmlTestCaseMixin, TestCase):
+    def mk_processor(self, **conf):
+        md = Markdown()
+        return AttrColTreeProcessor(md, conf)
+
+    def test_config_none(self):
+        md = Markdown
+        p = AttrColTreeProcessor(md, None)
+        self.assertEqual(p.columns, 12)
+        self.assertEqual(p.attr, 'cols')
+        self.assertEqual(p.tags, set(['section']))
+
+    def test_config_defaults(self):
+        p = self.mk_processor()
+        self.assertEqual(p.columns, 12)
+        self.assertEqual(p.attr, 'cols')
+        self.assertEqual(p.tags, set(['section']))
+
+    def test_config_overrides(self):
+        p = self.mk_processor(
+            columns=16,
+            attr='columns',
+            tags=['section', 'div'],
+        )
+        self.assertEqual(p.columns, 16)
+        self.assertEqual(p.attr, 'columns')
+        self.assertEqual(p.tags, set(['section', 'div']))
+
+    def test_simple_rows(self):
+        root = self.mk_doc("""
+            <section cols='4'>Foo</section>
+            <section cols='6'>Bar</section>
+            <section cols='2'>Beep</section>
+        """)
+        p = self.mk_processor()
+        new_root = p.run(root)
+        self.assert_xml_equal(new_root, self.mk_doc("""
+            <div class="row"><div class="col-md-4"><section>Foo</section>
+            </div><div class="col-md-6"><section>Bar</section>
+            </div><div class="col-md-2"><section>Beep</section>
+            </div></div>
+        """))
+
+
+class TestAttrColExtension(TestCase):
+    def mk_markdown(self, extensions=None):
+        if extensions is None:
+            extensions = ['attr_list', 'mdx_outline']
+        md = Markdown(extensions=extensions)
+        return md
+
+    def assert_registered(self, md):
+        processor = md.treeprocessors['attr_cols']
+        self.assertTrue(isinstance(processor, AttrColTreeProcessor))
+
+    def assert_not_registered(self, md):
+        self.assertFalse('attr_cols' in md.treeprocessors)
+
+    def text_create(self):
+        ext = AttrColExtension({'a': 'b'})
+        self.assertEqual(ext.conf, {'a': 'b'})
+
+    def test_extend_markdown(self):
+        md = self.mk_markdown()
+        ext = AttrColExtension({})
+        ext.extendMarkdown(md)
+        self.assert_registered(md)
+
+    def test_missing_attr_list(self):
+        md = self.mk_markdown(['mdx_outline'])
+        ext = AttrColExtension({})
+        self.assertRaisesRegexp(
+            RuntimeError,
+            "The attr_cols markdown extension depends the following"
+            " extensions which must preceded it in the extension list:"
+            " attr_list, mdx_outline",
+            ext.extendMarkdown, md)
+        self.assert_not_registered(md)
+
+    def test_missing_outline(self):
+        md = self.mk_markdown([])
+        ext = AttrColExtension({})
+        self.assertRaisesRegexp(
+            RuntimeError,
+            "The attr_cols markdown extension depends the following"
+            " extensions which must preceded it in the extension list:"
+            " attr_list, mdx_outline",
+            ext.extendMarkdown, md)
+        self.assert_not_registered(md)
+
+
+class TestExtensionRegistration(TestCase):
+    def test_make_extension(self):
+        configs = {'a': 'b'}
+        ext = makeExtension(**configs)
+        self.assertTrue(isinstance(ext, AttrColExtension))
+        self.assertEqual(ext.conf, configs)
