9a5bf71ee61d40f49f5da14c20f1364c204eca69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:05 +0000

    Add package xdepy/0.0.1/xdepy-0.0.1.tar.gz

---
+++ packages/xdepy/xdepy-0.0.1.tar.gz/xdepy-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='xdepy',
+      version='0.0.1',
+      description='a python module for quant trading by Xde',
+      url='https://github.com/xdesoft/xdepy',
+      author='xdexde',
+      author_email='zxdxinde@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
