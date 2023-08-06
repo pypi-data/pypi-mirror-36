df08853ff33ffd8b1a582dd4b7e03bb35d50e573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:19 +0000

    Add package opensubmit-web/0.7.9/opensubmit_web-0.7.9-py3-none-any.whl

---
+++ packages/opensubmit-web/opensubmit_web-0.7.9-py3-none-any.whl/opensubmit/models/__init__.py
@@ -0,0 +1,11 @@
+from .course import Course
+from .grading import Grading
+from .gradingscheme import GradingScheme
+from .testmachine import TestMachine
+from .submission import Submission
+from .submissionfile import SubmissionFile
+from .submissiontestresult import SubmissionTestResult
+from .assignment import Assignment
+from .userprofile import UserProfile
+from .studyprogram import StudyProgram
+
