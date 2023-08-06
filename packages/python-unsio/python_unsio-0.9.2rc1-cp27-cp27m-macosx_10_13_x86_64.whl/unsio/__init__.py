da4f77020135b775c7b2316b127945078505ab14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:04 +0000

    Add package python-unsio/0.9.2rc1/python_unsio-0.9.2rc1-cp27-cp27m-macosx_10_13_x86_64.whl

---
+++ packages/python-unsio/python_unsio-0.9.2rc1-cp27-cp27m-macosx_10_13_x86_64.whl/unsio/__init__.py
@@ -0,0 +1,14 @@
+
+from __future__ import print_function
+
+try:
+	from unsio.py_unsio import *
+except:
+	import sys
+	print("Unable to import [unsio.py_unsio]....",file=sys.stderr)
+
+from .version import version as __version__
+
+#from . import test, csnapshot
+
+#from .csnapshot import *
