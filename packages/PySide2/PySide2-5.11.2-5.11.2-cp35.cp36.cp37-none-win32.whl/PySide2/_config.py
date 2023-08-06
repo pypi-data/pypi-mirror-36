b3e747b70c5bc334b6cfa17683da3f80deabf663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:59 +0000

    Add package PySide2/5.11.2/PySide2-5.11.2-5.11.2-cp35.cp36.cp37-none-win32.whl

---
+++ packages/PySide2/PySide2-5.11.2-5.11.2-cp35.cp36.cp37-none-win32.whl/PySide2/_config.py
@@ -0,0 +1,17 @@
+built_modules = list(name for name in
+    "Core;Gui;Widgets;PrintSupport;Sql;Network;Test;Concurrent;WinExtras;Xml;XmlPatterns;Help;Multimedia;MultimediaWidgets;OpenGL;Positioning;Location;Qml;Quick;QuickWidgets;Scxml;Sensors;TextToSpeech;Charts;Svg;DataVisualization;UiTools;AxContainer;WebChannel;WebSockets;3DCore;3DRender;3DInput;3DLogic;3DAnimation;3DExtras"
+    .split(";"))
+
+shiboken_library_soversion = str(5.11)
+pyside_library_soversion = str(5.11)
+
+version = "5.11.2"
+version_info = (5, 11, 2, "", "")
+
+__build_date__ = '2018-09-19T06:45:19+00:00'
+
+
+
+
+# Timestamp used for snapshot build, which is part of snapshot package version.
+__setup_py_package_timestamp__ = ''
