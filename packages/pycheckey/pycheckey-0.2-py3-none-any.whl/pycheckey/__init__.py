d145c27d11398e8c7dd8bc16d96fe5e2df33869b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:59 +0000

    Add package pycheckey/0.2/pycheckey-0.2-py3-none-any.whl

---
+++ packages/pycheckey/pycheckey-0.2-py3-none-any.whl/pycheckey/__init__.py
@@ -0,0 +1,8 @@
+__name__ = 'pycheckey'
+__version__ = '0.2'
+__author__ = 'Daniel Starner'
+__email__ = 'starner.daniel5@gmail.com'
+__description__ = 'Ensure nested dictionary structure.'
+__repository__ = 'https://github.com/dstarner/pycheckey'
+
+from .pycheckey import KeyEnsurer
---
+++ packages/pycheckey/pycheckey-0.2-py3-none-any.whl/pycheckey/pycheckey.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+import typing
+
+
+@dataclass
+class KeyEnsurer:
+    """Dataclass that allows to deeply check that keys exist.
+    """
+
+    data: typing.Union[typing.Dict, 'collections.OrderedDict[str, typing.Any]']
+    required_keys: typing.List[str]
+
+    missing = []
+
+    def key_exists(self, data, key):
+        rest = ''
+        if '.' in key:
+            key, rest = key.split('.', 1)
+        if key not in data:
+            return False
+        if rest:
+            self.key_exists(data[key], rest)
+        return True
+
+    def validate(self):
+        self.missing = []
+        for key in self.required_keys:
+            if not self.key_exists(self.data, key):
+                self.missing.append(key)
