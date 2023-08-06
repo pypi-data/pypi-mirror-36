4fe5aa4c2237dcdddc439d47bd242c9cc463e31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:51 +0000

    Add package discombibleator/2.6/discombibleator-2.6.tar.gz

---
+++ packages/discombibleator/discombibleator-2.6.tar.gz/discombibleator-2.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='discombibleator',
+      version="2.6",
+      packages = ['discombibleator'],
+      include_package_data=True,
+      author = "Brian Friederich",
+      author_email = "briantfriederich@gmail.com",
+      zip_safe=False)
