d2b744f8d43153bd4a543fa9fc32295d6a1ce5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:34 +0000

    Add package amatino/0.0.7/amatino-0.0.7.tar.gz

---
+++ packages/amatino/amatino-0.0.7.tar.gz/amatino-0.0.7/amatino/tests/test_sequence.py
@@ -0,0 +1,23 @@
+"""
+Amatino API Python Bindings
+Test Sequence Module
+Author: hugh@amatino.io
+
+Provides a constant manifest of tests to execute
+"""
+import amatino.tests.primary as primary
+import amatino.tests.ancillary as ancillary
+import amatino.tests.derived as derived
+from amatino.tests.ancillary.custom_unit import CustomUnitTest
+
+SEQUENCE = [
+    ancillary.SessionTest,
+    ancillary.GlobalUnitTest,
+    CustomUnitTest,
+    ancillary.UserTest,
+    primary.EntityTest,
+    primary.AccountTest,
+    primary.TransactionTest,
+    derived.LedgerTest,
+    derived.RecursiveLedgerTest
+]
