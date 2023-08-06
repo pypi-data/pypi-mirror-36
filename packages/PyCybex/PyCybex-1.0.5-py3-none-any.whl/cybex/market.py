9c11a9f2e88bc823bb19a5073a3b786371474c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:01 +0000

    Add package PyCybex/1.0.5/PyCybex-1.0.5-py3-none-any.whl

---
+++ packages/PyCybex/PyCybex-1.0.5-py3-none-any.whl/cybex/market.py
@@ -0,0 +1,58 @@
+from bitshares.market import Market as BitsharesMarket
+from bitshares.price import Price as BitsharesPrice
+from bitshares.amount import Amount as BitsharesAmount
+from bitshares.utils import formatTime
+import pdb
+
+class Kline(dict):
+    def __init__(self, d, instance):
+        self['base'] = instance.rpc.get_asset(d['key']['base'])
+        self['quote'] = instance.rpc.get_asset(d['key']['quote'])
+        for i in ['open', 'close', 'high', 'low']:
+            for j in ['Base', 'Quote']:
+                self['{}{}Volume'.format(i, j)] = BitsharesAmount(
+                    amount = d['{}_{}'.format(i, j.lower())],
+                    asset = self[j.lower()],
+                    bitshares_instance = instance
+                )
+            self['{}Price'.format(i)] = BitsharesPrice(
+                base = self['{}BaseVolume'.format(i)],
+                quote = self['{}QuoteVolume'.format(i)],
+                bitshares_instance = instance)
+
+class Market(BitsharesMarket):
+    def __init__(
+        self,
+        *args,
+        base = None,
+        quote = None,
+        cybex_instance = None,
+        **kwargs
+    ):
+        super(Market, self).__init__(args = args, base = base, quote = quote, bitshares_instance = cybex_instance)
+
+    def get_market_history(
+        self,
+        bucket_seconds,
+        start,
+        end
+    ):
+        return [Kline(d, self.bitshares) 
+                for d in self.bitshares.rpc.get_market_history(
+                self['base']['id'],
+                self['quote']['id'],
+                bucket_seconds,
+                formatTime(start),
+                formatTime(end),
+                api = 'history'
+            )]
+
+    def get_fill_order_history(
+        self,
+        limit = 100):
+        return self.bitshares.rpc.get_fill_order_history(
+                self['base']['id'],
+                self['quote']['id'],
+                limit,
+                api = 'history'
+            )
