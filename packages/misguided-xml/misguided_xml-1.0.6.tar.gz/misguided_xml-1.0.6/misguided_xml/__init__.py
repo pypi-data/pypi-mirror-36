9e81863b016b16552d687a84faf51321ae592e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:03 +0000

    Add package misguided-xml/1.0.6/misguided_xml-1.0.6.tar.gz

---
+++ packages/misguided-xml/misguided_xml-1.0.6.tar.gz/misguided_xml-1.0.6/misguided_xml/__init__.py
@@ -0,0 +1,21 @@
+from xml import (XMLComment,
+                 XMLCDATA,
+                 XML,
+                 XMLSubsetString,
+
+                 ELEMENT,
+                 ATTRIBUTE,
+                 VALUE,
+
+                 NoXMLDeclarationDetected,
+                 InvalidXMLDeclaration,
+                 InvalidXMLElement,
+                 XMLSyntaxError,
+                 XMLElementNotFound,
+                 XMLTooManyElementsFound,
+                 XMLEndTagNotFound,
+                 BadSearchCriteria,
+                 UndefinedNamespace,
+                 XMLDeclaration,
+
+                 )
