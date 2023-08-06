fb21f02be4cf67819abb3b712685a1094ffc773c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:52 +0000

    Add package SystemCmd/1.3.8/SystemCmd-1.3.8.tar.gz

---
+++ packages/SystemCmd/SystemCmd-1.3.8.tar.gz/SystemCmd-1.3.8/src/system_cmd/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '1.3.8' 
+
+
+import logging
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+from .meat import *
+from .interface import *
+from .structures import *
+
+
