6c8f72494fdbdc75bd2d3696a62d2746cb35693d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:10 +0000

    Add package cy-trans/1.0.4/cy_trans-1.0.4.tar.gz

---
+++ packages/cy-trans/cy_trans-1.0.4.tar.gz/cy_trans-1.0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cy_trans',
+  packages = ['cy_trans'],
+  version = '1.0.4',
+  description = 'translate picture',
+  long_description='translate picture en to zh, ja to zh',
+  author = 'cycyc',
+  author_email = '285066083@qq.com',
+  keywords = ['pip3', 'trans', 'python3','python','translate'],
+  license='MIT',
+  install_requires = ['requests']
+)
