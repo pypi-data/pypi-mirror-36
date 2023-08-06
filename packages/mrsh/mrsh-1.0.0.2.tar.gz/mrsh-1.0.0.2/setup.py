be79a492246ad3ba9dc2f838f2f685781731cbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:08 +0000

    Add package mrsh/1.0.0.2/mrsh-1.0.0.2.tar.gz

---
+++ packages/mrsh/mrsh-1.0.0.2.tar.gz/mrsh-1.0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = "mrsh",
+    version = "1.0.0.2",
+    author = "Gioacchino Bombaci",
+    author_email = "gioakbombaci@gmail.com",
+    description = ("Core code for mrsh infrastructure"),
+    license = "GPL",
+    keywords = "mrsh's core",
+    url = "http://packages.python.org/mrsh",
+    packages=['mrsh','mrsh.rabbit', 'mrsh.mongo','mrsh.mongo.teacher', 'mrsh.mongo.factorizer', 'mrsh.mongo.common', 'mrsh.mongo.controller', 'mrsh.mongo.reccomender', 'mrsh.mongo.web_app', 'mrsh.mongo.news_consumer', 'mrsh.mongo.chat', 'mrsh.mongo.social_worker','mrsh.logger', 'mrsh.util','mrsh.social', 'mrsh.mongo.affinity'],
+    install_requires=[
+          'pika','pymongo','fluent-logger','nltk','stop-words','PyYAML'
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
