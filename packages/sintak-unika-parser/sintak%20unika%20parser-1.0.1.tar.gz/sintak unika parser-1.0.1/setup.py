95c3a7ddda20baf311850ab31da8b2f594081544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:47 +0000

    Add package sintak-unika-parser/1.0.1/sintak%20unika%20parser-1.0.1.tar.gz

---
+++ packages/sintak-unika-parser/sintak%20unika%20parser-1.0.1.tar.gz/sintak unika parser-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from lib.utils.core import settings
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sintak unika parser",
+    version=settings.VERSION,
+    author="lintangtimur",
+    author_email="lintangtimur915@gmail.com",
+    description="Packages for parser and scrap information from sintak unika",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lintangtimur/SintakParser",
+    packages=['sintak-unika-parser'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
