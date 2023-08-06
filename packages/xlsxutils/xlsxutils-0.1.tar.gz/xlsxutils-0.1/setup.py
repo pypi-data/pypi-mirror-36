3cd9cd798bfada2977b97209a13b8ac89e472682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:16 +0000

    Add package xlsxutils/0.1/xlsxutils-0.1.tar.gz

---
+++ packages/xlsxutils/xlsxutils-0.1.tar.gz/xlsxutils-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xlsxutils',
+    version='0.1',
+    packages=['venv.Lib.site-packages.xlsxwriter'],
+    url='https://github.com/goldinger/xlsxutils.git',
+    license='Apache 2.0',
+    author='asghir121317',
+    author_email='amine@sghir.me',
+    description='Excel Generator utils'
+)
