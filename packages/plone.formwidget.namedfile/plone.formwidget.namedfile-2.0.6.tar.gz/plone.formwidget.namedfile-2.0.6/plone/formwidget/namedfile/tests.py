0e7007da247e8da1826c8f3558560e54584179b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:07 +0000

    Add package plone.formwidget.namedfile/2.0.6/plone.formwidget.namedfile-2.0.6.tar.gz

---
+++ packages/plone.formwidget.namedfile/plone.formwidget.namedfile-2.0.6.tar.gz/plone.formwidget.namedfile-2.0.6/plone/formwidget/namedfile/tests.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from plone.formwidget.namedfile.testing import INTEGRATION_TESTING
+from plone.testing import layered
+
+import doctest
+import unittest
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(
+        layered(doctest.DocFileSuite(
+            'widget.rst',
+            optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS,
+            encoding='utf-8'),
+            layer=INTEGRATION_TESTING),
+    )
+    return suite
---
+++ packages/plone.formwidget.namedfile/plone.formwidget.namedfile-2.0.6.tar.gz/plone.formwidget.namedfile-2.0.6/setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '2.0.6'
+
+setup(
+    name='plone.formwidget.namedfile',
+    version=version,
+    description="Image widget for z3c.form and Plone",
+    long_description=open("README.rst").read() + "\n" +
+    open("CHANGES.rst").read(),
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.1",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='plone image widget z3c.form',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='https://pypi.python.org/pypi/plone.formwidget.namedfile',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.formwidget'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.namedfile',
+        'plone.z3cform >= 0.7.4',
+        'six',
+        'z3c.form',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
