decad1eaeda199266214002d0678665752f87228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:50 +0000

    Add package selfcmp/0.0.1.1/selfcmp-0.0.1.1.tar.gz

---
+++ packages/selfcmp/selfcmp-0.0.1.1.tar.gz/selfcmp-0.0.1.1/selfcmp/spw.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+
+import sys, getopt
+import os
+
+def help_out():
+	print("help: \'python -m selfcmp.spw -s <dir_name>\'")
+	
+dir_name = ''
+	
+def check_file(argv):
+	try:
+		opts, args = getopt.getopt(argv,"-h-s:",["--help"])
+	except getopt.GetoptError:
+		help_out()
+		sys.exit(2)
+	
+	for opt,val in opts:
+		if opt in ('-h','--help'):
+			help_out()
+			sys.exit()
+		elif opt == '-s':
+			dir_name = val
+			os.makedirs(dir_name)
+	if not os.path.exists('conj.yaml') :
+		file_out = open('conj.yaml','w')
+	file_out.write(dir_name+'\n')
+	file_out.write('Problem: 0')
+	
+
+if __name__ == "__main__" :
+	check_file(sys.argv[1:])
