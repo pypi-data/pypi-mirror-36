8a08292828dacb65639287753d1d569b8773bede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:38 +0000

    Add package depynd/0.6.0/depynd-0.6.0.tar.gz

---
+++ packages/depynd/depynd-0.6.0.tar.gz/depynd-0.6.0/depynd/feature_selection/_mifs.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+from depynd.information import conditional_mutual_information
+
+
+def _mifs(X, y, lamb, k, **kwargs):
+    """Select effective features in ``X`` on predicting ``y`` using mutual-information-based feature selection
+    [brown2012conditional]_.
+
+    Parameters
+    ----------
+    X : array-like, shape (n_samples, n_features)
+        Observations of feature variables.
+    y : array-like, shape (n_samples)
+        Observations of the target variable.
+    lamb: float or None
+        Threshold for independence tests. Ignored if `k` is specified.
+    k : int or None
+        Number of selected features.
+    kwargs : dict
+        Optional parameters for MI estimation.
+
+    Returns
+    -------
+    indices : list
+        Indices of the selected features.
+
+    References
+    ----------
+    .. [brown2012conditional] Brown, Gavin, et al. "Conditional likelihood maximisation: a unifying framework for
+        information theoretic feature selection." Journal of machine learning research 13.Jan (2012): 27-66.
+    """
+    n, d = X.shape
+    selected = []
+    if k is not None:
+        selected = _grow(selected, X, y, -np.inf, k, **kwargs)
+    else:
+        selected = _grow(selected, X, y, lamb, d, **kwargs)
+        selected = _shrink(selected, X, y, lamb, 0, **kwargs)
+    return selected
+
+
+def _grow(selected, X, y, lamb, k, **kwargs):
+    n, d = X.shape
+    while True:
+        max_cmi = -np.inf
+        not_selected = set(range(d)) - set(selected)
+        z = X[:, selected]
+        for i in not_selected:
+            x = X[:, i]
+            cmi = conditional_mutual_information(x, y, z, **kwargs)
+            if max_cmi < cmi:
+                max_cmi = cmi
+                max_idx = i
+        if max_cmi <= lamb or len(selected) == k:
+            return selected
+        selected.append(max_idx)
+
+
+def _shrink(selected, X, y, lamb, k, **kwargs):
+    while True:
+        min_cmi = np.inf
+        for i in selected:
+            x = X[:, i]
+            z = X[:, list(set(selected) - {i})]
+            cmi = conditional_mutual_information(x, y, z, **kwargs)
+            if min_cmi > cmi:
+                min_cmi = cmi
+                min_idx = i
+        if min_cmi >= lamb or len(selected) == k:
+            return selected
+        selected.remove(min_idx)
---
+++ packages/depynd/depynd-0.6.0.tar.gz/depynd-0.6.0/depynd/information/_dr.py
@@ -0,0 +1,62 @@
+import numpy as np
+from scipy.stats import multivariate_normal
+from scipy.optimize import minimize
+from sklearn.cluster import KMeans
+
+
+def _mi_dr(X, Y, sigma, n_bases, maxiter):
+    """Estimate mutual information between X and Y using density ratio estimation.
+
+    Parameters
+    ----------
+    X : array-like, shape (n_samples, n_features_x) or (n_samples)
+        Observations of a variable.
+    Y : array-like, shape (n_samples, n_features_y) or (n_samples)
+        Observations of the other variable.
+    sigma : float
+        Kernel width for density ratio estimator.
+    n_bases : int
+        Number of bases used in density ratio estimation.
+    maxiter : int
+        Maximum number of iteration in density ratio estimation.
+
+    Returns
+    -------
+    mi : float
+        Estimated mutual information between ``X`` and ``Y``.
+    """
+    n, d_x = X.shape
+    _, d_y = Y.shape
+    b = min(n_bases, n)
+    XY = np.hstack([X, Y])
+    UV = KMeans(b).fit(XY).cluster_centers_
+    U, V = np.split(UV, [d_x], axis=1)
+    phi_x = np.array([_normal(X, u, sigma) for u in U])
+    phi_y = np.array([_normal(Y, v, sigma) for v in V])
+    phi = phi_x * phi_y
+    h_x = np.sum(phi_x, axis=1)
+    h_y = np.sum(phi_y, axis=1)
+    h_xy = np.sum(phi, axis=1)
+    h = (h_x * h_y - h_xy) / (n ** 2 - n)
+
+    def fun(alpha):
+        return -np.sum(np.log(alpha.dot(phi)))
+
+    def jac(alpha):
+        return -phi.dot(1 / alpha.dot(phi))
+
+    x0 = np.random.uniform(0, 1, b)
+    bounds = [(0, None)] * b
+    constraints = [{'type': 'eq', 'fun': lambda alpha: alpha.dot(h) - 1}]
+    result = minimize(fun=fun, jac=jac, x0=x0, bounds=bounds, constraints=constraints, options={'maxiter': maxiter})
+
+    if not result.success:
+        return np.nan
+
+    mi = np.mean(np.log(result.x.dot(phi)))
+    return mi
+
+
+def _normal(X, mean, sigma):
+    cov = sigma ** 2 * np.eye(len(mean))
+    return multivariate_normal.pdf(X, mean=mean, cov=cov)
