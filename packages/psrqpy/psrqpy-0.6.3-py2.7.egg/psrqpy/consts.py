da2e3b56358e0592e1ee3bb6b78f3882b03cd7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:32 +0000

    Add package psrqpy/0.6.3/psrqpy-0.6.3-py2.7.egg

---
+++ packages/psrqpy/psrqpy-0.6.3-py2.7.egg/psrqpy/consts.py
@@ -0,0 +1,17 @@
+
+def myfunc(x=1):
+    """
+    Function to do a thing!
+
+    Args:
+        x (float): a number
+
+    Returns:
+        float: another number
+    """
+
+    return 2*x
+
+#: some constant
+MYCONST = 1 
+
