1dca614b76dfe4bb6a6faf73dfead0d1611dd4e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:35 +0000

    Add package drf-microservice/0.5.2/drf-microservice-0.5.2.tar.gz

---
+++ packages/drf-microservice/drf-microservice-0.5.2.tar.gz/drf-microservice-0.5.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = f.read().strip()
+
+setup(
+    name='drf-microservice',
+    version='0.5.2',
+    author='Alain IVARS',
+    author_email='alainivars@gmail.com',
+    url='http://github.com/alainivars/drf-microservice',
+    description='''
+    Create a REST API endpoints with Authentication and Registration
+    ''',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='''
+    django rest auth registration rest-framework django-registration api
+    ''',
+    zip_safe=False,
+    test_suite='setuptest.setuptest.SetupTestSuite',
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
