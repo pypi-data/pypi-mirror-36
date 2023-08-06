267518ac2c456c2ec10ba876cf510abe955d22e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:54 +0000

    Add package pymath2/1.1.0/pymath2-1.1.0-py2.py3-none-any.whl

---
+++ packages/pymath2/pymath2-1.1.0-py2.py3-none-any.whl/pymath/__main__.py
@@ -0,0 +1,6 @@
+from itertools import (combinations, combinations_with_replacement,
+                       permutations, product)
+from math import *
+from statistics import *
+
+from pymath import *
