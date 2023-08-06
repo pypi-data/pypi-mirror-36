5febb52f52a512866934a7ba113e1055fa74fb2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:21 +0000

    Add package pymetaterp/1.0/pymetaterp-1.0.tar.gz

---
+++ packages/pymetaterp/pymetaterp-1.0.tar.gz/pymetaterp-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='pymetaterp',
+      version='1.0',
+      description='A python parser that builds python ASTs in 502 lines of python without using modules',
+      long_description=open("readme.rst").read(),
+      url='https://github.com/asrp/pymetaterp',
+      author='asrp',
+      author_email='asrp@email.com',
+      packages=['pymetaterp'],
+      keywords='parser peg python minimal')
