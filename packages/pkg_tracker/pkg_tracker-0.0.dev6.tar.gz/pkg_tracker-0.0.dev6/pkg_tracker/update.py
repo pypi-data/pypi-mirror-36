b5e28ad411f1b20387d49c3473c6759fd514d76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:29 +0000

    Add package pkg_tracker/0.0.dev6/pkg_tracker-0.0.dev6.tar.gz

---
+++ packages/pkg_tracker/pkg_tracker-0.0.dev6.tar.gz/pkg_tracker-0.0.dev6/pkg_tracker/update.py
@@ -0,0 +1,8 @@
+from .requirements_updater import RequirementsUpdater
+
+
+def main(work_branches=['master'], base='master'):
+    RequirementsUpdater(work_branches, base).create_pull()
+
+if __name__ == "__main__":
+    main()
