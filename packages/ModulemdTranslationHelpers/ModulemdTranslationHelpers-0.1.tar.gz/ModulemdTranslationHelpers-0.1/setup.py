76348fe44b942b8d07dec28b6ea969b734758d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:10 +0000

    Add package ModulemdTranslationHelpers/0.1/ModulemdTranslationHelpers-0.1.tar.gz

---
+++ packages/ModulemdTranslationHelpers/ModulemdTranslationHelpers-0.1.tar.gz/ModulemdTranslationHelpers-0.1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# This file is part of ModulemdTranslationHelpers
+# Copyright (C) 2018 Stephen Gallagher
+#
+# Fedora-License-Identifier: MIT
+# SPDX-2.0-License-Identifier: MIT
+# SPDX-3.0-License-Identifier: MIT
+#
+# This program is free software.
+# For more information on the license, see COPYING.
+# For more information on free software, see
+# <https://www.gnu.org/philosophy/free-sw.en.html>.
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ModulemdTranslationHelpers',
+    version='0.1',
+    packages=['ModulemdTranslationHelpers'],
+    url='https://github.com/sgallagher/ModulemdTranslationHelpers',
+    license='MIT',
+    author='Stephen Gallagher',
+    author_email='sgallagh@redhat.com',
+    description='Tools for working with translations of modulemd',
+    long_description=long_description,
+    # ModulemdTranslationHelpers also requires koji and libmodulemd which
+    # are not available on PyPI and must be installed separately. On Fedora,
+    #  this is done with `dnf install koji libmodulemd`
+    install_requires=[
+        'click',
+        'requests',
+        'babel',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ModulemdTranslationHelpers=ModulemdTranslationHelpers.cli:cli'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    include_package_data=True,
+)
