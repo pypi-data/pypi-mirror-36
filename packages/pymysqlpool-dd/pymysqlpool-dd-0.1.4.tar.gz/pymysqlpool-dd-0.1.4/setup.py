91131be4dd9f0fe58549c2c37c5e49206d5e80c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:39 +0000

    Add package pymysqlpool-dd/0.1.4/pymysqlpool-dd-0.1.4.tar.gz

---
+++ packages/pymysqlpool-dd/pymysqlpool-dd-0.1.4.tar.gz/pymysqlpool-dd-0.1.4/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+setup(
+    name='pymysqlpool-dd',
+    version='0.1.4',
+    packages=find_packages(),
+    url='https://github.com/waipbmtd/pymysqlpool',
+    license='MIT',
+    author='Devin',
+    author_email='waipbmtd@gmail.com',
+    requires=['pymysql'],
+    description='MySQL connection pool utility.',
+)
