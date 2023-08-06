79bc40ce16b8c5eef1fcc6cae351500eca23ead5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:43 +0000

    Add package hor2vec/2.1.1/hor2vec-2.1.1-py2.py3-none-any.whl

---
+++ packages/hor2vec/hor2vec-2.1.1-py2.py3-none-any.whl/hor2vec/__init__.py
@@ -0,0 +1,11 @@
+#  _               ____
+# | |__   ___  _ _|___ \__   _____  ___
+# | '_ \ / _ \| '__|__) \ \ / / _ \/ __|
+# | | | | (_) | |  / __/ \ V /  __/ (__
+# |_| |_|\___/|_| |_____| \_/ \___|\___|
+#
+
+from hor2vec.__main__ import main
+
+__version__ = '2.1.1'
+__doc__ = 'Change your horizontal pure text to vertical with some options.'
