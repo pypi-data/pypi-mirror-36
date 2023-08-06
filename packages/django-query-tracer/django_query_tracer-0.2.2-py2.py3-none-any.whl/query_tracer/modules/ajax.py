d15c7e61f685d1ceeb56f2de2c31748c1468bc8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:29 +0000

    Add package django-query-tracer/0.2.2/django_query_tracer-0.2.2-py2.py3-none-any.whl

---
+++ packages/django-query-tracer/django_query_tracer-0.2.2-py2.py3-none-any.whl/query_tracer/modules/ajax.py
@@ -0,0 +1,36 @@
+import json
+
+from query_tracer.modules import QueryTracerModule
+from query_tracer import settings
+
+
+class AjaxDumpModule(QueryTracerModule):
+    """
+    Dumps the content of all AJAX responses.
+    """
+
+    logger_name = 'ajax'
+
+    def process_request(self, request):
+        if request.is_ajax():
+            if not getattr(request, 'REQUEST', None):
+                request.REQUEST = request.GET if request.method=='GET' else request.POST
+            data = dict(request.REQUEST.items())
+            if settings.QUERYTRACER_AJAX_PRETTY_PRINT:
+                try:
+                    data = json.dumps(data, indent=4)
+                except:
+                    pass
+            self.logger.info(data)
+
+    def process_response(self, request, response):
+        if request.is_ajax():
+            # Let's do a quick test to see what kind of response we have
+            if len(response.content) < settings.QUERYTRACER_AJAX_CONTENT_LENGTH:
+                content = response.content
+                if settings.QUERYTRACER_AJAX_PRETTY_PRINT:
+                    try:
+                        content = json.dumps(json.loads(content), indent=4)
+                    except:
+                        pass
+                self.logger.info(content)
