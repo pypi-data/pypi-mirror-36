20503b13c16a2694eed616db1f1e9f3f42c63b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:31 +0000

    Add package pyeosio/0.1.0/pyeosio-0.1.0.tar.gz

---
+++ packages/pyeosio/pyeosio-0.1.0.tar.gz/pyeosio-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pyeosio',
+      version='0.1.0',
+      description='EOSIO node connector for Python',
+      url='https://github.com/psy2848048/pyeosio',
+      author='Junhang (Bryan) RHEE',
+      author_email='psy2848048@gmail.com',
+      license='MIT',
+      packages=['pyeosio'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
