c4cc703df8acc8ce36dd00713f5667c06f6bbc0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:55 +0000

    Add package onegov.file/0.9.0/onegov.file-0.9.0-py3-none-any.whl

---
+++ packages/onegov.file/onegov.file-0.9.0-py3-none-any.whl/onegov/file/__init__.py
@@ -0,0 +1,12 @@
+from onegov.file.collection import FileCollection, FileSetCollection
+from onegov.file.integration import DepotApp
+from onegov.file.models import File, FileSet, AssociatedFiles
+
+__all__ = [
+    'AssociatedFiles',
+    'DepotApp',
+    'File',
+    'FileCollection',
+    'FileSet',
+    'FileSetCollection'
+]
