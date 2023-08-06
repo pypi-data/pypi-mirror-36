6412394df0af371133f492b7f268e80f65db0573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:31 +0000

    Add package id4me-rp-client/0.0.1/id4me_rp_client-0.0.1-py2.py3-none-any.whl

---
+++ packages/id4me-rp-client/id4me_rp_client-0.0.1-py2.py3-none-any.whl/id4me_rp_client/__init__.py
@@ -0,0 +1,3 @@
+from .id4me_rp_client import ID4meClient, ID4meContext, ID4meClaimsRequest, ID4meClaimRequestProperties
+from .id4me_constants import *
+from .id4me_exceptions import *
---
+++ packages/id4me-rp-client/id4me_rp_client-0.0.1-py2.py3-none-any.whl/id4me_rp_client/id4me_exceptions.py
@@ -0,0 +1,18 @@
+
+class ID4meException(Exception):
+    pass
+
+class ID4meDNSResolverException(ID4meException):
+    pass
+
+class ID4meDNSSECException(ID4meException):
+    pass
+
+class ID4meAuthorityConfigurationException(ID4meException):
+    pass
+
+class ID4meRelyingPartyRegistrationException(ID4meException):
+    pass
+
+class ID4meTokenRequestException(ID4meException):
+    pass
