53b0cb6eae2edc184331d27b996405b36a1c7c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:54 +0000

    Add package stat-distributions/0.1/stat_distributions-0.1.tar.gz

---
+++ packages/stat-distributions/stat_distributions-0.1.tar.gz/stat_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stat_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['stat_distributions'],
+      author = "Nelson Tsaku",
+      
+      zip_safe=False)
