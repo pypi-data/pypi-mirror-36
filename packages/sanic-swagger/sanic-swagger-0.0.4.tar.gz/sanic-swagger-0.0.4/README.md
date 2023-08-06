bdd31193eaa1cb48186e75445a072be16bf22336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:53 +0000

    Add package sanic-swagger/0.0.4/sanic-swagger-0.0.4.tar.gz

---
+++ packages/sanic-swagger/sanic-swagger-0.0.4.tar.gz/sanic-swagger-0.0.4/README.md
@@ -0,0 +1,24 @@
+# sanic-swagger
+
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/8e7b064677ab4b6cbc2508b626bcba0a)](https://app.codacy.com/app/abatilo/sanic-swagger?utm_source=github.com&utm_medium=referral&utm_content=abatilo/sanic-swagger&utm_campaign=Badge_Grade_Settings)
+[![CircleCI](https://circleci.com/gh/abatilo/sanic-swagger.svg?style=svg)](https://circleci.com/gh/abatilo/sanic-swagger)
+[![codecov](https://codecov.io/gh/abatilo/sanic-swagger/branch/master/graph/badge.svg)](https://codecov.io/gh/abatilo/sanic-swagger)
+[![PyPI status](https://img.shields.io/pypi/status/sanic-swagger.svg)](https://pypi.python.org/pypi/sanic-swagger/)
+[![PyPI version](https://badge.fury.io/py/sanic-swagger.svg)](https://badge.fury.io/py/sanic-swagger)
+[![PyPI pyversions](https://img.shields.io/pypi/pyversions/sanic-swagger.svg)](https://pypi.python.org/pypi/sanic-swagger/)
+[![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2Fabatilo%2Fsanic-swagger.svg?type=shield)](https://app.fossa.io/projects/git%2Bgithub.com%2Fabatilo%2Fsanic-swagger?ref=badge_shield)
+[![MIT license](http://img.shields.io/badge/license-MIT-brightgreen.svg)](http://opensource.org/licenses/MIT)
+
+Annotate your [Sanic](https://github.com/channelcat/sanic) endpoints, and
+automatically generate a
+[Swagger](https://swagger.io/)/[OpenAPI](https://swagger.io/resources/open-api/)
+compatible JSON spec file.
+
+This project is a fork of both
+[sanic-openapi](https://github.com/channelcat/sanic-openapi) and
+[sanic-attrs](https://github.com/vltr/sanic-attrs).
+
+As such, you can write all of your models as
+[attrs](https://github.com/python-attrs/attrs) which gives you the handy
+ability to use [cattrs](https://github.com/Tinche/cattrs) for dealing with your
+serialization and deserialization of your models to and from JSON.
---
+++ packages/sanic-swagger/sanic-swagger-0.0.4.tar.gz/sanic-swagger-0.0.4/setup.py
@@ -0,0 +1,70 @@
+import os
+import sys
+
+from setuptools import setup
+from setuptools.command.install import install
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.4'
+about = {'version': VERSION}
+
+with open(
+    os.path.join(here, 'sanic_swagger', '__init__.py'), 'r', encoding='utf-8'
+) as f:
+    for line in f:
+        if line.startswith('__version__'):
+            about['version'] = line.strip().split('=')[1].strip(' \'"')
+
+with open(os.path.join(here, 'README.md'), 'r', encoding='utf-8') as f:
+    README = f.read()
+
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = ('Git tag: {0} does not match '
+                    'the version of this app: {1}').format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+
+setup(
+    name='sanic-swagger',
+    version=about['version'],
+    url='http://github.com/abatilo/sanic-swagger/',
+    license='MIT',
+    author='Aaron Batilo',
+    author_email='aaronbatilo@gmail.com',
+    maintainer='Aaron Batilo',
+    maintainer_email='aaronbatilo@gmail.com',
+    description='OpenAPI / Swagger support for Sanic using attrs',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    packages=['sanic_swagger'],
+    package_data={'sanic_swagger': ['ui/*']},
+    platforms='any',
+    install_requires=[
+        'sanic>=0.7.0',
+        'attrs>=18.0.0',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='sanic swagger openapi attrs cattrs',
+    python_requires='==3.6.*',
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
+)
