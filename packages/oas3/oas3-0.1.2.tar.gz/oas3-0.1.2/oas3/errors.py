6ce676801562d2c4119d75a2c38a1fd3ccb8f707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:05 +0000

    Add package oas3/0.1.2/oas3-0.1.2.tar.gz

---
+++ packages/oas3/oas3-0.1.2.tar.gz/oas3-0.1.2/oas3/errors.py
@@ -0,0 +1,16 @@
+"""
+oas3.errors
+~~~~~~~~~~~
+Defines custom pacakage specific exceptions.
+"""
+
+class ValidationError(Exception):
+    pass
+
+
+class LoadingError(Exception):
+    pass
+
+
+class DumpingError(Exception):
+    pass
