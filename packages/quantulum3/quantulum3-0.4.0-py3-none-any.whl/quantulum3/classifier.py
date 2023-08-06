43de5244f5ef1d8050473ad30bdfca861d7c33bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:07 +0000

    Add package quantulum3/0.4.0/quantulum3-0.4.0-py3-none-any.whl

---
+++ packages/quantulum3/quantulum3-0.4.0-py3-none-any.whl/quantulum3/classifier.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+"""
+:mod:`Quantulum` classifier functions.
+"""
+
+# Standard library
+import os
+import json
+import pickle
+import logging
+import re
+import string
+import pkg_resources
+
+# Semi-dependencies
+try:
+    from sklearn.linear_model import SGDClassifier
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    USE_CLF = True
+except ImportError:
+    SGDClassifier, TfidfVectorizer = None, None
+    USE_CLF = False
+
+try:
+    import wikipedia
+except ImportError:
+    wikipedia = None
+
+try:
+    from stemming.porter2 import stem
+except ImportError:
+    stem = None
+
+# Quantulum
+from . import load
+
+
+################################################################################
+def download_wiki(store=True):  # pragma: no cover
+    """
+    Download WikiPedia pages of ambiguous units.
+    @:param store (bool) store wikipedia data in wiki.json file
+    """
+    if not wikipedia:
+        print("Cannot download wikipedia pages. Install package wikipedia first.")
+        return
+
+    ambiguous = [i for i in list(load.UNITS.items()) if len(i[1]) > 1]
+    ambiguous += [i for i in list(load.DERIVED_ENT.items()) if len(i[1]) > 1]
+    pages = set([(j.name, j.uri) for i in ambiguous for j in i[1]])
+
+    print()
+    objs = []
+    for num, page in enumerate(pages):
+
+        obj = {'url': page[1]}
+        obj['_id'] = obj['url'].replace('https://en.wikipedia.org/wiki/', '')
+        obj['clean'] = obj['_id'].replace('_', ' ')
+
+        print('---> Downloading %s (%d of %d)' % (obj['clean'], num + 1,
+                                                  len(pages)))
+
+        obj['text'] = wikipedia.page(obj['clean']).content
+        obj['unit'] = page[0]
+        objs.append(obj)
+
+    path = os.path.join(load.TOPDIR, 'wiki.json')
+    os.remove(path)
+    if store:
+        with open(path, 'w') as wiki_file:
+            json.dump(objs, wiki_file, indent=4, sort_keys=True)
+
+    print('\n---> All done.\n')
+    return objs
+
+
+################################################################################
+def clean_text(text):
+    """
+    Clean text for TFIDF
+    """
+    if not stem:
+        raise ImportError("Module stemming is not installed.")
+
+    my_regex = re.compile(r'[%s]' % re.escape(string.punctuation))
+    new_text = my_regex.sub(' ', text)
+
+    new_text = [
+        stem(i) for i in new_text.lower().split()
+        if not re.findall(r'[0-9]', i)
+    ]
+
+    new_text = ' '.join(new_text)
+
+    return new_text
+
+
+################################################################################
+def train_classifier(download=True,
+                     parameters=None,
+                     ngram_range=(1, 1),
+                     store=True):
+    """
+    Train the intent classifier
+    TODO auto invoke if sklearn version is new or first install or sth
+    @:param store (bool) store classifier in clf.pickle
+    """
+    path = os.path.join(load.TOPDIR, 'train.json')
+    with open(path, 'r', encoding='utf-8') as train_file:
+        training_set = json.load(train_file)
+
+    wiki_set = download_wiki(store) if download else None
+    if not wiki_set:
+        path = os.path.join(load.TOPDIR, 'wiki.json')
+        with open(path, 'r', encoding='utf-8') as wiki_file:
+            wiki_set = json.load(wiki_file)
+
+    target_names = list(set([i['unit'] for i in training_set + wiki_set]))
+    train_data, train_target = [], []
+    for example in training_set + wiki_set:
+        train_data.append(clean_text(example['text']))
+        train_target.append(target_names.index(example['unit']))
+
+    tfidf_model = TfidfVectorizer(
+        sublinear_tf=True, ngram_range=ngram_range, stop_words='english')
+
+    matrix = tfidf_model.fit_transform(train_data)
+
+    if parameters is None:
+        parameters = {
+            'loss': 'log',
+            'penalty': 'l2',
+            'max_iter': 50,
+            'alpha': 0.00001,
+            'fit_intercept': True
+        }
+
+    clf = SGDClassifier(**parameters).fit(matrix, train_target)
+    obj = {
+        'scikit-learn_version':
+        pkg_resources.get_distribution('scikit-learn').version,
+        'tfidf_model':
+        tfidf_model,
+        'clf':
+        clf,
+        'target_names':
+        target_names
+    }
+    if store:  # pragma: no cover
+        path = os.path.join(load.TOPDIR, 'clf.pickle')
+        with open(path, 'wb') as file:
+            pickle.dump(obj, file)
+    return obj
+
+
+################################################################################
+def load_classifier():
+    """
+    Load the intent classifier
+    """
+
+    path = os.path.join(load.TOPDIR, 'clf.pickle')
+    with open(path, 'rb') as file:
+        obj = pickle.load(file, encoding='latin1')
+
+    cur_scipy_version = pkg_resources.get_distribution('scikit-learn').version
+    if cur_scipy_version != obj.get('scikit-learn_version'):  # pragma: no cover
+        logging.warning(
+            "The classifier was built using a different scikit-learn version (={}, !={}). The disambiguation tool could behave unexpectedly. Consider running classifier.train_classfier()"
+            .format(obj.get('scikit-learn_version'), cur_scipy_version))
+
+    return obj['tfidf_model'], obj['clf'], obj['target_names']
+
+
+if USE_CLF:
+    TFIDF_MODEL, CLF, TARGET_NAMES = load_classifier()
+else:
+    TFIDF_MODEL, CLF, TARGET_NAMES = None, None, None
+
+
+################################################################################
+def disambiguate_entity(key, text):
+    """
+    Resolve ambiguity between entities with same dimensionality.
+    """
+
+    new_ent = load.DERIVED_ENT[key][0]
+
+    if len(load.DERIVED_ENT[key]) > 1:
+        transformed = TFIDF_MODEL.transform([text])
+        scores = CLF.predict_proba(transformed).tolist()[0]
+        scores = zip(scores, TARGET_NAMES)
+
+        # Filter for possible names
+        names = [i.name for i in load.DERIVED_ENT[key]]
+        scores = [i for i in scores if i[1] in names]
+
+        # Sort by rank
+        scores = sorted(scores, key=lambda x: x[0], reverse=True)
+        try:
+            new_ent = load.ENTITIES[scores[0][1]]
+        except IndexError:
+            logging.debug('\tAmbiguity not resolved for "%s"', str(key))
+
+    return new_ent
+
+
+################################################################################
+def disambiguate_unit(unit, text):
+    """
+    Resolve ambiguity between units with same names, symbols or abbreviations.
+    """
+
+    new_unit = load.UNIT_SYMBOLS.get(unit) or load.UNITS.get(unit)
+    if not new_unit:
+        new_unit = load.LOWER_UNITS.get(
+            unit.lower()) or load.UNIT_SYMBOLS_LOWER.get(unit.lower())
+        if not new_unit:
+            raise KeyError('Could not find unit "%s" from "%s"' % (unit, text))
+
+    if len(new_unit) > 1:
+        transformed = TFIDF_MODEL.transform([clean_text(text)])
+        scores = CLF.predict_proba(transformed).tolist()[0]
+        scores = zip(scores, TARGET_NAMES)
+
+        # Filter for possible names
+        names = [i.name for i in new_unit]
+        scores = [i for i in scores if i[1] in names]
+
+        # Sort by rank
+        scores = sorted(scores, key=lambda x: x[0], reverse=True)
+        try:
+            final = load.UNITS[scores[0][1]][0]
+            logging.debug('\tAmbiguity resolved for "%s" (%s)', unit, scores)
+        except IndexError:
+            logging.debug('\tAmbiguity not resolved for "%s"', unit)
+            final = new_unit[0]
+    else:
+        final = new_unit[0]
+
+    return final
---
+++ packages/quantulum3/quantulum3-0.4.0-py3-none-any.whl/quantulum3/load.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+:mod:`Quantulum` unit and entity loading functions.
+"""
+
+from builtins import open
+
+# Standard library
+import os
+import json
+from collections import defaultdict
+import re
+
+# Dependencies
+import inflect
+
+# Quantulum
+from . import classes as c
+
+TOPDIR = os.path.dirname(__file__) or "."
+
+PLURALS = inflect.engine()
+
+METRIC_PREFIXES = {
+    'Y': 'yotta',
+    'Z': 'zetta',
+    'E': 'exa',
+    'P': 'peta',
+    'T': 'tera',
+    'G': 'giga',
+    'M': 'mega',
+    'k': 'kilo',
+    'h': 'hecto',
+    'da': 'deca',
+    'd': 'deci',
+    'c': 'centi',
+    'm': 'mill',
+    'µ': 'micro',
+    'n': 'nano',
+    'p': 'pico',
+    'f': 'femto',
+    'a': 'atto',
+    'z': 'zepto',
+    'y': 'yocto',
+    'Ki': 'kibi',
+    'Mi': 'mebi',
+    'Gi': 'gibi',
+    'Ti': 'tebi',
+    'Pi': 'pebi',
+    'Ei': 'exbi',
+    'Zi': 'zebi',
+    'Yi': 'yobi'
+}
+
+
+def get_string_json(raw_json_text):
+    text = raw_json_text
+    text = bytes(text, 'utf-8').decode('ascii', 'ignore')
+    text = re.sub(r'[^\x00-\x7f]', r'', text)
+    return text
+
+
+################################################################################
+def get_key_from_dimensions(derived):
+    """
+    Translate dimensionality into key for DERIVED_UNI and DERIVED_ENT dicts.
+    """
+
+    return tuple((i['base'], i['power']) for i in derived)
+
+
+################################################################################
+def get_dimension_permutations(entities, derived):
+    """
+    Get all possible dimensional definitions for an entity.
+    """
+
+    new_derived = defaultdict(int)
+    for item in derived:
+        new = entities[item['base']].dimensions
+        if new:
+            for new_item in new:
+                new_derived[new_item['base']] += (
+                    new_item['power'] * item['power'])
+        else:
+            new_derived[item['base']] += item['power']
+
+    final = [[{
+        'base': i[0],
+        'power': i[1]
+    } for i in list(new_derived.items())], derived]
+    final = [sorted(i, key=lambda x: x['base']) for i in final]
+
+    candidates = []
+    for item in final:
+        if item not in candidates:
+            candidates.append(item)
+
+    return candidates
+
+
+################################################################################
+def load_entities():
+    """
+    Load entities from JSON file.
+    """
+
+    path = os.path.join(TOPDIR, 'entities.json')
+    string_json = ''.join(open(path, encoding='utf-8').readlines())
+    string_json = get_string_json(string_json)
+    entities = json.loads(string_json)
+    names = [i['name'] for i in entities]
+
+    try:
+        assert len(set(names)) == len(entities)
+    except AssertionError:  # pragma: no cover
+        raise Exception('Entities with same name: %s' %
+                        [i for i in names if names.count(i) > 1])
+
+    entities = dict(
+        (k['name'],
+         c.Entity(name=k['name'], dimensions=k['dimensions'], uri=k['URI']))
+        for k in entities)
+
+    derived_ent = defaultdict(list)
+    for ent in entities:
+        if not entities[ent].dimensions:
+            continue
+        perms = get_dimension_permutations(entities, entities[ent].dimensions)
+        for perm in perms:
+            key = get_key_from_dimensions(perm)
+            derived_ent[key].append(entities[ent])
+
+    return entities, derived_ent
+
+
+ENTITIES, DERIVED_ENT = load_entities()
+
+
+################################################################################
+def get_derived_units(names):
+    """
+    Create dictionary of unit dimensions.
+    """
+
+    derived_uni = {}
+
+    for name in names:
+        key = get_key_from_dimensions(names[name].dimensions)
+        derived_uni[key] = names[name]
+        plain_derived = [{'base': name, 'power': 1}]
+        key = get_key_from_dimensions(plain_derived)
+        derived_uni[key] = names[name]
+        if not names[name].dimensions:
+            names[name].dimensions = plain_derived
+        names[name].dimensions = [{
+            'base': names[i['base']].name,
+            'power': i['power']
+        } for i in names[name].dimensions]
+
+    return derived_uni
+
+
+################################################################################
+def load_units():
+    """
+    Load units from JSON file.
+    """
+
+    names = {}
+    unit_symbols, unit_symbols_lower, = defaultdict(list), defaultdict(list)
+    surfaces, lowers, symbols = defaultdict(list), defaultdict(
+        list), defaultdict(list)
+
+    path = os.path.join(TOPDIR, 'units.json')
+    string_json = ''.join(open(path, encoding='utf-8').readlines())
+    for unit in json.loads(string_json):
+        load_unit(unit, names, unit_symbols, unit_symbols_lower, surfaces,
+                  lowers, symbols)
+
+    derived_uni = get_derived_units(names)
+
+    return names, unit_symbols, unit_symbols_lower, surfaces, lowers, symbols, derived_uni
+
+
+def load_unit(unit, names, unit_symbols, unit_symbols_lower, surfaces, lowers,
+              symbols):
+    try:
+        assert unit['name'] not in names
+    except AssertionError:  # pragma: no cover
+        msg = 'Two units with same name in units.json: %s' % unit['name']
+        raise Exception(msg)
+
+    obj = c.Unit(
+        name=unit['name'],
+        surfaces=unit['surfaces'],
+        entity=ENTITIES[unit['entity']],
+        uri=unit['URI'],
+        symbols=unit['symbols'],
+        dimensions=unit['dimensions'],
+        currency_code=unit.get('currency_code'))
+
+    names[unit['name']] = obj
+
+    for symbol in unit['symbols']:
+        unit_symbols[symbol].append(obj)
+        unit_symbols_lower[symbol.lower()].append(obj)
+        if unit['entity'] == 'currency':
+            symbols[symbol].append(obj)
+
+    for surface in unit['surfaces']:
+        surfaces[surface].append(obj)
+        lowers[surface.lower()].append(obj)
+        split = surface.split()
+        index = None
+        if ' per ' in surface:
+            index = split.index('per') - 1
+        elif 'degree ' in surface:
+            index = split.index('degree')
+        if index is not None:
+            plural = ' '.join([
+                i if num != index else PLURALS.plural(split[index])
+                for num, i in enumerate(split)
+            ])
+        else:
+            plural = PLURALS.plural(surface)
+        if plural != surface:
+            surfaces[plural].append(obj)
+            lowers[plural.lower()].append(obj)
+
+    # If SI-prefixes are given, add them
+    if unit.get('prefixes'):
+        for prefix in unit['prefixes']:
+            try:
+                assert prefix in METRIC_PREFIXES
+            except AssertionError:  # pragma: no cover
+                raise Exception(
+                    "Given prefix '{}' for unit '{}' not supported".format(
+                        prefix, unit['name']))
+            try:
+                assert len(unit['dimensions']) <= 1
+            except AssertionError:  # pragma: no cover
+                raise Exception(
+                    "Prefixing not supported for multiple dimensions in {}".
+                    format(unit['name']))
+
+            uri = unit['URI']
+            slash_position = uri.rfind('/') + 1
+            uri = uri[:slash_position] + METRIC_PREFIXES[prefix].capitalize(
+            ) + uri[slash_position:].lower()
+
+            prefixed_unit = {
+                'name':
+                METRIC_PREFIXES[prefix] + unit['name'],
+                'surfaces':
+                [METRIC_PREFIXES[prefix] + i for i in unit['surfaces']],
+                'entity':
+                unit['entity'],
+                'URI':
+                uri,
+                'dimensions': [],
+                'symbols': [prefix + i for i in unit['symbols']]
+            }
+            load_unit(prefixed_unit, names, unit_symbols, unit_symbols_lower,
+                      surfaces, lowers, symbols)
+
+
+NAMES, UNIT_SYMBOLS, UNIT_SYMBOLS_LOWER, UNITS, LOWER_UNITS, PREFIX_SYMBOLS, \
+    DERIVED_UNI = load_units()
+ALL_UNIT_SYMBOLS = {}
+ALL_UNIT_SYMBOLS.update(UNIT_SYMBOLS)
+ALL_UNIT_SYMBOLS.update(UNIT_SYMBOLS_LOWER)
+ALL_UNITS = {}
+ALL_UNITS.update(UNITS)
+ALL_UNITS.update(LOWER_UNITS)
+
+################################################################################
+
+
+def build_common_words():
+    # Read raw 4 letter file
+    path = os.path.join(TOPDIR, 'common-words.txt')
+    words = defaultdict(list)  # Collect words based on length
+    with open(path, 'r', encoding='utf-8') as file:
+        for line in file:
+            if line.startswith('#'):
+                continue
+            line = line.rstrip()
+            if line not in ALL_UNITS and line not in UNIT_SYMBOLS:
+                words[len(line)].append(line)
+                words[len(line)].append(PLURALS.plural(line))
+    return words
+
+
+################################################################################
+
+
+def load_common_words():
+    path = os.path.join(TOPDIR, 'common-words.json')
+    dumped = {}
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            dumped = json.load(file)
+    except OSError:  # pragma: no cover
+        pass
+
+    words = defaultdict(list)  # Collect words based on length
+    for length, word_list in dumped.items():
+        words[int(length)] = word_list
+    return words
+
+
+COMMON_WORDS = load_common_words()
