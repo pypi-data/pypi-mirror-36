55f148ef109355e21492b525782a016cf91e73ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:19 +0000

    Add package ravenpackapi/1.0.23/ravenpackapi-1.0.23.tar.gz

---
+++ packages/ravenpackapi/ravenpackapi-1.0.23.tar.gz/ravenpackapi-1.0.23/ravenpackapi/exceptions.py
@@ -0,0 +1,32 @@
+from functools import wraps
+
+
+class APIException(Exception):
+    def __init__(self, *args, **kwargs):
+        response = kwargs.pop('response', None)
+        super(APIException, self).__init__(*args)
+        self.response = response
+
+
+class MissingAPIException(Exception):
+    def __str__(self):
+        return "Define the api parameter with your own APIKEY " \
+               "to access the API methods from a model"
+
+
+class DataFileTimeout(Exception):
+    pass
+
+
+def api_method(func):
+    @wraps(func)
+    def decorated_func(instance, *args, **kwargs):
+        if not getattr(instance, 'api'):
+            raise MissingAPIException()
+        return func(instance, *args, **kwargs)
+
+    return decorated_func
+
+
+class ValidationError(Exception):
+    pass
