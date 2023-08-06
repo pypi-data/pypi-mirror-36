7e84d87b43ad42e2f8936ff342fe254929f4b222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:16 +0000

    Add package dline/2.3.0/dline-2.3.0.tar.gz

---
+++ packages/dline/dline-2.3.0.tar.gz/dline-2.3.0/dline/client/guildlog.py
@@ -0,0 +1,46 @@
+
+class PrivateGuild:
+    def __init__(self):
+        self.name = "Private Messages"
+        self.roles = None
+        self.emojis = None
+        self.channels = None
+        self.me = None
+        self.members = []
+
+    def set_channels(self, channels):
+        self.channels = channels
+        if not channels:
+            return
+        for channel in channels:
+            for member in channel.members:
+                if member not in self.members:
+                    self.members.append(member)
+        self.me = channels[0].me
+
+# Simple wrapper class to hold a list of ChannelLogs
+class GuildLog():
+    def __init__(self, guild, channel_log_list):
+        self._guild = guild
+        self._channel_logs = list(channel_log_list)
+
+    @property
+    def guild(self):
+        return self._guild
+
+    @property
+    def name(self):
+        return self._guild.name
+
+    @property
+    def logs(self):
+        return self._channel_logs
+
+    def clear_logs(self):
+        for channel_log in self._channel_logs:
+            del channel_log[:]
+
+    # takes list of ChannelLog
+    def add_logs(self, log_list):
+        for logs in log_list:
+            self._channel_logs.append(logs)
