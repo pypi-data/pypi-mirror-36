288b210dca4f9a0c9d95f765f3c24b8ad56d147e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:30 +0000

    Add package py2jnb/0.0.3/py2jnb-0.0.3-py2.py3-none-any.whl

---
+++ packages/py2jnb/py2jnb-0.0.3-py2.py3-none-any.whl/py2jnb/tools.py
@@ -0,0 +1,13 @@
+"""
+Provide simplified entry points for common task
+"""
+from .converter import convert
+from .reader import read
+
+
+def python_to_notebook(input_filename, output_filename):
+    """
+    Convert the given python source file into a properly formatted notebook.
+    """
+    cvt = read(input_filename)
+    convert(cvt, output_filename)
