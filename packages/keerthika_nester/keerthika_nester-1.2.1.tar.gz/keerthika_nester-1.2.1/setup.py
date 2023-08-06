8dd2618df67a020550e757c6214780e018d58d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:35 +0000

    Add package keerthika_nester/1.2.1/keerthika_nester-1.2.1.tar.gz

---
+++ packages/keerthika_nester/keerthika_nester-1.2.1.tar.gz/keerthika_nester-1.2.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = "keerthika_nester",
+	version = "1.2.1",
+	py_module = ['keerthika_nester'],
+	author = "keerthika",
+	author_email = "keerthikamari@yahoo.com",
+	description = "A simple printer of nested lists"
+	)
+
