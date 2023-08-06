c44de67f7411eaaa7d6dee1145ad08f243ec7845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:26 +0000

    Add package running/0.1.1/running-0.1.1.tar.gz

---
+++ packages/running/running-0.1.1.tar.gz/running-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='running',
+    version='0.1.1',
+    description='User-friendly running pace calculator',
+    url='https://github.com/jonathanlofgren/running',
+    author='Jonathan Löfgren',
+    author_email='lofgren021@gmail.com',
+    py_modules=['running'],
+    install_requires=[
+        'Click',
+    ],
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License'
+    ],
+    entry_points='''
+        [console_scripts]
+        running=running:running
+    ''',
+    project_urls={
+        'Source': 'https://github.com/jonathanlofgren/running'
+    }
+)
