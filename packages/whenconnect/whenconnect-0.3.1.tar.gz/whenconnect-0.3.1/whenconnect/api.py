5118303ec71e4de741be5181c72bccbd408a9b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:25 +0000

    Add package whenconnect/0.3.1/whenconnect-0.3.1.tar.gz

---
+++ packages/whenconnect/whenconnect-0.3.1.tar.gz/whenconnect-0.3.1/whenconnect/api.py
@@ -0,0 +1,32 @@
+"""
+- 外部调用API，进而启动core
+    - 确认core状态
+    - 传入参数，启动core
+    - 返回
+- core将初始化pipe初始化用于数据传递，并在子线程中启动scanner用于监听设备，并将三者互相绑定
+"""
+
+from whenconnect.manager import TaskManager
+from whenconnect.core import start_detect
+
+
+def _get_task_type(device):
+    if isinstance(device, str):
+        return 'any'
+    if isinstance(device, list):
+        return 'specific'
+
+
+def when_connect(device, do):
+    return TaskManager.register_task(_get_task_type(device), event_type='connect', device_list=device, todo=do)
+
+
+def when_disconnect(device, do):
+    return TaskManager.register_task(_get_task_type(device), event_type='disconnect', device_list=device, todo=do)
+
+
+__all__ = [
+    'when_connect',
+    'when_disconnect',
+    'start_detect'
+]
