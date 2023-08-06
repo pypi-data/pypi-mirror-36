0a0745e40c501966167307a10ee10f82bf5bdba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:28 +0000

    Add package mtwaffle/0.5/mtwaffle-0.5.tar.gz

---
+++ packages/mtwaffle/mtwaffle-0.5.tar.gz/mtwaffle-0.5/mtwaffle/__init__.py
@@ -0,0 +1,7 @@
+from mtwaffle.mt import *
+from mtwaffle.graphs import *
+
+from mtwaffle.edi import read_edi, write_edi
+from mtwaffle.utils import read_json, write_json, AttrDict
+
+__version__ = '0.5'
