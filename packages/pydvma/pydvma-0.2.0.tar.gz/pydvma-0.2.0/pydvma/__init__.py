6f9e677a3e50e63a850692985c1ac577324a6be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:58 +0000

    Add package pydvma/0.2.0/pydvma-0.2.0.tar.gz

---
+++ packages/pydvma/pydvma-0.2.0.tar.gz/pydvma-0.2.0/pydvma/__init__.py
@@ -0,0 +1,8 @@
+from .settings import MySettings
+from .file import load_data, save_data
+from .oscilloscope import Oscilloscope, Recorder
+from .logdata import DataSet, TimeData, FreqData, TfData, SonoData, MetaData
+from .plotting import PlotData
+from .analysis import convert_to_frequency
+
+
