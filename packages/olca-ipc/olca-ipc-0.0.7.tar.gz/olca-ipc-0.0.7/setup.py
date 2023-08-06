bd379d960a69fd4af85d07768a74021675a978ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:07 +0000

    Add package olca-ipc/0.0.7/olca-ipc-0.0.7.tar.gz

---
+++ packages/olca-ipc/olca-ipc-0.0.7.tar.gz/olca-ipc-0.0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='olca-ipc',
+    version='0.0.7',
+    description='A Python package for calling openLCA functions from Python.',
+    long_description=long_description,
+    url='https://github.com/GreenDelta/olca-ipc.py',
+    packages=['olca'],
+    install_requires=['requests'],
+    keywords=['openLCA', 'life cycle assessment', 'LCA'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Utilities",
+    ]
+)
