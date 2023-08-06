cda9d97c47dfca01d430fe593d568d7b082699de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:46 +0000

    Add package x86cpu/0.4/x86cpu-0.4-cp37-none-win_amd64.whl

---
+++ packages/x86cpu/x86cpu-0.4-cp37-none-win_amd64.whl/x86cpu/__init__.py
@@ -0,0 +1,11 @@
+""" x86cpu namespace
+"""
+
+from .cpuinfo import info, cpuid, has_cpuid
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+# Override version
+__version__ = '0.4'
