eb4aad7de5524dfd8c75cba24979b883fe5fbf49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:50 +0000

    Add package py2jnb/0.0.5/py2jnb-0.0.5-py2.py3-none-any.whl

---
+++ packages/py2jnb/py2jnb-0.0.5-py2.py3-none-any.whl/tests/test_conversion.py
@@ -0,0 +1,42 @@
+import os
+from py2jnb.tools import python_to_notebook
+
+def test_convert():
+    print("\nSTART TEST")
+    SAMPLE_DIR = os.path.join(os.getcwd(),'samples')
+
+    INPUT_FNAME = 'hello.py'
+    BASENAME, _ = os.path.splitext(INPUT_FNAME)
+    INPUT_PY = os.path.join(SAMPLE_DIR,INPUT_FNAME)
+    print("Input file:",INPUT_PY)
+    assert os.path.exists(INPUT_PY)
+
+    OUTPUT_IPYNB = os.path.join(SAMPLE_DIR,BASENAME+".ipynb")
+    print("Output file: ", OUTPUT_IPYNB)
+    if os.path.exists(OUTPUT_IPYNB):
+        os.remove(OUTPUT_IPYNB)
+
+    python_to_notebook(INPUT_PY,OUTPUT_IPYNB)
+    assert os.path.exists(OUTPUT_IPYNB)
+
+    # os.remove(OUTPUT_IPYNB)
+
+
+def test_cells_convert():
+    SAMPLE_DIR = os.path.join(os.getcwd(), 'samples')
+
+    INPUT_FNAME = 'hello_cells.py'
+    BASENAME, _ = os.path.splitext(INPUT_FNAME)
+    INPUT_PY = os.path.join(SAMPLE_DIR, INPUT_FNAME)
+    print("Input file:", INPUT_PY)
+    assert os.path.exists(INPUT_PY)
+
+    OUTPUT_IPYNB = os.path.join(SAMPLE_DIR, BASENAME + ".ipynb")
+    print("Output file: ", OUTPUT_IPYNB)
+    if os.path.exists(OUTPUT_IPYNB):
+        os.remove(OUTPUT_IPYNB)
+
+    python_to_notebook(INPUT_PY,OUTPUT_IPYNB)
+    assert os.path.exists(OUTPUT_IPYNB)
+
+    # os.remove(OUTPUT_IPYNB)
