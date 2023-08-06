3a7658cb61d5356221755f8f36c9ca6623cb1f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:56 +0000

    Add package Minetorch/0.1.7/Minetorch-0.1.7.tar.gz

---
+++ packages/Minetorch/Minetorch-0.1.7.tar.gz/Minetorch-0.1.7/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='Minetorch',
+    description='A tools collection for pytorch users',
+    version='0.1.7',
+    packages=['minetorch'],
+    url="https://github.com/louis-she/minetorch",
+    author='louis',
+    author_email='chenglu.she@gmail.com',
+    keywords='pytorch minecraft',
+    install_requires=['tensorboardX', 'torch']
+
+)
+
