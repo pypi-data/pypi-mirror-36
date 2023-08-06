31daa3c317e47c3061bd37d3f1550c35d973ce20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:22 +0000

    Add package hawkrest/1.0.1/hawkrest-1.0.1.tar.gz

---
+++ packages/hawkrest/hawkrest-1.0.1.tar.gz/hawkrest-1.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(name='hawkrest',
+      version='1.0.1',
+      description="Hawk HTTP Authorization for Django Rest Framework",
+      long_description='',
+      author='Kumar McMillan',
+      author_email='kumar.mcmillan@gmail.com',
+      license='MPL 2.0 (Mozilla Public License)',
+      url='https://github.com/kumar303/hawkrest',
+      include_package_data=True,
+      classifiers=[
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Internet :: WWW/HTTP',
+      ],
+      packages=find_packages(exclude=['tests']),
+      install_requires=['djangorestframework', 'mohawk>=0.3.4'])
