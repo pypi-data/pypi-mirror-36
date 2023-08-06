b9034775491fd0c1744c419c159e1b818f7f2b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:24 +0000

    Add package pyzyre/0.0a27/pyzyre-0.0a27-py27-none-any.whl

---
+++ packages/pyzyre/pyzyre-0.0a27-py27-none-any.whl/pyzyre/client/__init__.py
@@ -0,0 +1,227 @@
+import logging
+import os
+import os.path
+from time import sleep
+import names
+from pprint import pprint
+import re
+
+import zmq
+from czmq import Zactor, zactor_fn, create_string_buffer, lib
+
+from ._task import task as client_task
+from ..utils import resolve_endpoint
+from pyzyre.constants import GOSSIP_PORT, SERVICE_PORT, ZYRE_GROUP, GOSSIP_CONNECT, ENDPOINT, CURVE_ALLOW_ANY, \
+    NODE_NAME, DefaultHandler
+
+ZAUTH_TRACE = os.getenv('ZAUTH_TRACE', False)
+
+logger = logging.getLogger(__name__)
+
+
+class Client(object):
+
+    def __init__(self, handler=DefaultHandler(), **kwargs):
+
+        # disable CZMQ from capturing SIGINT
+        os.environ['ZSYS_SIGHANDLER'] = 'false'
+
+        self.handler = handler
+
+        self.group = kwargs.get('group', ZYRE_GROUP)
+        self._groups = self.group.split(',')
+        self.group = '|'.join(self.group.split(','))
+        self.interface = kwargs.get('interface') or '*'
+
+        self.parent_loop = kwargs.get('loop')
+        self.gossip_bind = kwargs.get('gossip_bind')
+        self.beacon = kwargs.get('beacon')
+        self.gossip_connect = kwargs.get('gossip_connect', GOSSIP_CONNECT)
+        self.endpoint = kwargs.get('endpoint', ENDPOINT)
+        self.cert = kwargs.get('cert')
+        self.gossip_publickey = kwargs.get('gossip_publickey')
+        self.first_node = None
+        self.zauth = kwargs.get('zauth_curve_allow')
+        self.advertised_endpoint = kwargs.get('advertised_endpoint')
+
+        self.name = kwargs.get('name', NODE_NAME)
+        if not self.name:
+            self.name = '{}_{}'.format(names.get_first_name().lower(), names.get_last_name().lower())
+
+        self.actor = None
+        self.task = zactor_fn(client_task)
+
+        if self.gossip_bind:
+            self.beacon = False
+            self.gossip_connect = False
+
+        elif self.gossip_connect:
+            self.beacon = False
+
+        else:
+            self.endpoint = False
+            self.beacon = True
+
+        if isinstance(self.zauth, str):
+            self.zauth = self._zauth(self.zauth)
+
+        self._init_zyre()
+
+    def _zauth(self, allow=CURVE_ALLOW_ANY):
+        logger.debug("spinning up zauth..")
+
+        zauth = Zactor(zactor_fn(lib.zauth), None)
+
+        if ZAUTH_TRACE:
+            logger.debug('turning on auth verbose')
+            lib.zstr_sendx(zauth, "VERBOSE".encode('utf-8'), None)
+            zauth.sock().wait()
+
+        if allow:
+            logger.debug("configuring Zauth...")
+            lib.zstr_sendx(zauth, "CURVE".encode('utf-8'), allow, None)
+            zauth.sock().wait()
+
+        logger.debug('Zauth complete')
+
+        return zauth
+
+    def _init_gossip_connect(self):
+        try:
+            logger.debug('resolving gossip-connect: {}'.format(self.gossip_connect))
+            self.gossip_connect = resolve_endpoint(self.gossip_connect, GOSSIP_PORT)
+
+        except RuntimeError as e:
+            logger.error(e)
+            logger.debug('falling back to beacon mode..')
+            self.beacon = 1
+            self.gossip_connect = None
+
+        self.endpoint = resolve_endpoint(self.endpoint, SERVICE_PORT)
+
+    def _init_gossip_bind(self):
+        self.gossip_bind = resolve_endpoint(self.gossip_bind, GOSSIP_PORT)
+        self.endpoint = resolve_endpoint(self.endpoint, SERVICE_PORT)
+
+    def _init_zyre(self):
+        # setup czmq/zyre
+        # disable CZMQ from capturing SIGINT
+        os.environ['ZSYS_SIGHANDLER'] = 'false'
+
+        if self.gossip_bind:
+            self._init_gossip_bind()
+
+        if self.gossip_connect:
+            self._init_gossip_connect()
+
+        if self.beacon:
+            os.environ["ZSYS_INTERFACE"] = self.interface
+
+        actor_args = [
+            'group=%s' % self.group,
+            'name=%s' % self.name,
+        ]
+
+        if self.gossip_bind:
+            actor_args.append('gossip_bind=%s' % self.gossip_bind)
+
+        elif self.gossip_connect:
+            actor_args.append('gossip_connect=%s' % self.gossip_connect)
+
+        else:
+            actor_args.append('beacon=1')
+
+        if self.endpoint:
+            actor_args.append('endpoint=%s' % self.endpoint)
+
+        if self.advertised_endpoint:
+            actor_args.append('advertised_endpoint=%s' % self.advertised_endpoint)
+
+        if self.cert:
+            actor_args.append('publickey=%s' % self.cert.public_txt().decode('utf-8'))
+            actor_args.append('secretkey=%s' % self.cert.secret_txt().decode('utf-8'))
+
+        if self.gossip_publickey:
+            actor_args.append('gossip_publickey=%s' % self.gossip_publickey.decode('utf-8'))
+
+        actor_args = ','.join(actor_args)
+        self.actor_args = create_string_buffer(actor_args.encode('utf-8'))
+
+        self.actor = None
+        self._actor = None
+
+    def start_zyre(self):
+        self._actor = Zactor(self.task, self.actor_args)
+        self.actor = zmq.Socket(shadow=self._actor.resolve(self._actor).value)
+
+    def stop_zyre(self):
+        self.actor.send_multipart(['$$STOP'.encode('utf-8')])
+        m = self.actor.recv_multipart()
+        sleep(0.01)
+        del self._actor
+
+        if self.zauth:
+            del self.zauth  # destroy old actor
+            self.zauth = None  # re-establish attrib
+
+    def join(self, group):
+        logger.debug('sending join')
+        self.actor.send_multipart(['JOIN', group])
+
+    def shout(self, group, message):
+        if isinstance(message, str):
+            message = message.encode('utf-8')
+
+        self.actor.send_multipart(['SHOUT'.encode('utf-8'), group.encode('utf-8'), message])
+
+    def whisper(self, message, address):
+        logger.debug('sending whisper to %s' % address)
+        if isinstance(message, str):
+            message = message.decode('utf-8')
+        self.actor.send_multipart(['WHISPER'.encode('utf-8'), address, message])
+        logger.debug('message sent via whisper')
+
+    def leave(self, group):
+        logger.debug('sending LEAVE for %s' % group)
+        self.actor.send_multipart(['LEAVE', group])
+
+    def handle_message(self, s, e):
+        m = s.recv_multipart()
+
+        m_type = m.pop(0)
+
+        if m_type == 'SHOUT':
+            group, peer, address, message = m
+            self.handler.on_shout(self, group, peer, address, message)
+
+        elif m_type == 'ENTER':
+            # set teh first node name in case we need it later (gossip)
+            if not self.first_node and self.gossip_connect:
+                self.first_node = m[1]
+
+            self.handler.on_enter(self, peer=m)
+
+        elif m_type == 'WHISPER':
+            peer, message = m
+            self.handler.on_whisper(self, peer, message)
+
+        elif m_type == 'EXIT':
+            peer, peers_remining = m
+            self.handler.on_exit(self, peer)
+
+        elif m_type == 'JOIN':
+            peer, group = m
+            self.handler.on_join(self, peer, group)
+
+        elif m_type == 'LEAVE':
+            peer, group = m
+            self.handler.on_leave(self, peer, group)
+
+        elif m_type == 'EVASIVE':
+            peer = m
+            self.handler.on_evasive(self, peer)
+
+        else:
+            logger.warn("unhandled m_type {} rest of message is {}".format(m_type, m))
+
+
---
+++ packages/pyzyre/pyzyre-0.0a27-py27-none-any.whl/pyzyre/client/_task.py
@@ -0,0 +1,216 @@
+import logging
+import os
+import uuid
+
+import zmq
+from czmq import Zsock, string_at, Zmsg, Zcert
+from pyzyre.constants import ZYRE_GROUP
+from zyre import Zyre, ZyreEvent
+
+logger = logging.getLogger(__name__)
+
+EVASIVE_TIMEOUT = os.environ.get('ZYRE_EVASIVE_TIMEOUT', 5000)  # zyre defaults
+EXPIRED_TIMEOUT = os.environ.get('ZYRE_EXPIRED_TIMEOUT', 30000)
+TRACE_EVASIVE = os.getenv('ZYRE_EVASITVE_TRACE')
+NODE_NAME = os.getenv('ZYRE_NODE_NAME')
+TRACE = os.getenv('ZYRE_TRACE')
+
+
+def task(pipe, arg):
+    args = string_at(arg).decode('utf-8')
+    args = dict(item.split("=", 1) for item in args.split(","))
+
+    name = args.get('name')
+    if not name:
+        raise RuntimeError('missing node name')
+
+    group = args.get('group', ZYRE_GROUP)
+
+    logger.debug('setting up node: %s' % name)
+    n = Zyre(name.encode('utf-8'))
+
+    logger.debug('setting evasive timeout: {}'.format(EVASIVE_TIMEOUT))
+    n.set_evasive_timeout(int(EVASIVE_TIMEOUT))
+
+    logger.debug('setting experation timer: {}'.format(EXPIRED_TIMEOUT))
+    n.set_expired_timeout(int(EXPIRED_TIMEOUT))
+
+    if TRACE:
+        logger.info('setting verbose...')
+        logger.setLevel(logging.DEBUG)
+        n.set_verbose()
+
+    if args.get('publickey'):
+        cert = Zcert.new_from_txt(args['publickey'].encode('utf-8'), args['secretkey'].encode('utf-8'))
+        n.set_zcert(cert)
+
+    if args.get('advertised_endpoint'):
+        logger.debug('setting advertised_endpoint: %s' % args['advertised_endpoint'])
+        n.set_advertised_endpoint(args['advertised_endpoint'].encode('utf-8'))
+
+    if args.get('endpoint'):
+        logger.debug('setting endpoint: {}'.format(args['endpoint']))
+        if n.set_endpoint(args['endpoint'].encode('utf-8')) == -1:
+            logger.error('unable to bind endpoint: %s')
+            logger.warn('endpoint will be auto generated using tcp://*:*')
+
+    if not args.get('beacon'):
+        logger.debug('setting up gossip')
+        if args.get('gossip_bind'):
+            logger.debug('binding gossip: {}'.format(args['gossip_bind']))
+            n.gossip_bind(args['gossip_bind'].encode('utf-8'))
+        else:
+            logger.debug('connecting to gossip group: {}'.format(args['gossip_connect']))
+            if args.get('gossip_publickey'):
+                n.gossip_connect_curve(args['gossip_publickey'].encode('utf-8'), args['gossip_connect'].encode('utf-8'))
+            else:
+                n.gossip_connect(args['gossip_connect'].encode('utf-8'))
+
+    poller = zmq.Poller()
+
+    pipe_zsock_s = Zsock(pipe, False)
+    assert pipe_zsock_s
+
+    pipe_s = zmq.Socket(shadow=pipe_zsock_s.resolve(pipe_zsock_s).value)
+    assert pipe_s
+
+    poller.register(pipe_s, zmq.POLLIN)
+
+    # getting the zyre [czmq] socket
+    # https://marc.info/?l=zeromq-dev&m=144719752222833&w=2
+    s = n.socket()
+    assert s
+
+    # re-casting as native pyzmq socket
+    ss = zmq.Socket(shadow=s.resolve(s).value)
+    assert ss
+
+    # registering
+    poller.register(ss, zmq.POLLIN)
+
+    logger.debug('staring node...')
+    n.start()
+
+    group = group.split('|')
+    for g in group:
+        logger.debug('joining: %s' % g)
+        n.join(g.encode('utf-8'))
+
+    pipe_zsock_s.signal(0)  # OK
+
+    peers = {}
+    peer_first = None
+    terminated = False
+    # TODO- catch SIGINT
+    while not terminated:
+        items = dict(poller.poll())
+
+        try:
+            if pipe_s in items and items[pipe_s] == zmq.POLLIN:
+                message = Zmsg.recv(pipe)
+
+                if not message:
+                    break  # SIGINT
+
+                msg_type = message.popstr().decode('utf-8').upper()
+
+                # message to quit
+                if msg_type == "$$STOP":
+                    for g in group:
+                        n.leave(g.encode('utf-8'))
+                    terminated = True
+
+                elif msg_type == '$$ID':
+                    pipe_s.send_string(n.uuid().decode('utf-8'))
+
+                elif msg_type == 'WHISPER':
+                    address = message.popstr().decode('utf-8')
+                    msg = message.popstr().decode('utf-8')
+                    m = Zmsg()
+                    m.addstr(msg)
+                    address = peers[str(address)]
+                    address = uuid.UUID(address).hex.upper()
+                    n.whisper(address, m)
+
+                elif msg_type == 'JOIN':
+                    group = message.popstr().decode('utf-8')
+                    logger.debug('joining %s' % group)
+                    n.join(group)
+
+                elif msg_type == 'SHOUT':
+                    g = message.popstr()
+                    msg = message.popstr()
+                    logger.debug('shouting[%s]: %s' % (g.decode('utf-8'), msg))
+                    n.shouts(g,  msg)
+
+                elif msg_type == 'LEAVE':
+                    g = message.popstr()
+                    logger.debug('leaving: %s' % g)
+                    n.leave(g.encode('utf-8'))
+
+                else:
+                    logger.warn('unknown message type: {}'.format(msg_type))
+
+            elif ss in items and items[ss] == zmq.POLLIN:
+                e = ZyreEvent(n)
+
+                msg_type = e.type().decode('utf-8').upper()
+                #logger.debug('found ZyreEvent: %s' % msg_type)
+                #logger.debug(e.get_msg().popstr())
+
+                if msg_type == "ENTER":
+                    logger.debug('ENTER {} - {}'.format(e.peer_name(), e.peer_uuid()))
+                    peers[e.peer_name()] = e.peer_uuid()
+                    if not peer_first:
+                        peer_first = e.peer_name() # this should be the gossip node
+
+                    pipe_s.send_multipart(['ENTER'.encode('utf-8'), e.peer_uuid(), e.peer_name()])
+                    #headers = e.headers() # zlist
+
+                elif msg_type == 'LEAVE':
+                    logger.debug('LEAVE [{}] [{}]'.format(e.group(), e.peer_name()))
+                    pipe_s.send_multipart(['LEAVE'.encode('utf-8'), e.peer_name(), e.group()])
+
+                elif msg_type == 'JOIN':
+                    logger.debug('JOIN [{}] [{}]'.format(e.group(), e.peer_name()))
+                    pipe_s.send_multipart(['JOIN'.encode('utf-8'), e.peer_name(), e.group()])
+
+                elif msg_type == "SHOUT":
+                    m = e.get_msg().popstr()
+                    logger.debug('SHOUT [{}] [{}]: {} - {}'.format(e.group(), e.peer_name(), e.peer_uuid(), m))
+                    pipe_s.send_multipart(['SHOUT'.encode('utf-8'), e.group(), e.peer_name(), e.peer_uuid(), m])
+
+                elif msg_type == "WHISPER":
+                    m = e.get_msg().popstr()
+                    logger.debug('WHISPER [{}]: {}'.format(e.peer_name(), m))
+                    pipe_s.send_multipart(['WHISPER'.encode('utf-8'), e.peer_name(), e.peer_uuid(), m])
+
+                elif msg_type == 'EXIT':
+                    logger.debug('EXIT [{}] [{}]'.format(e.group(), e.peer_name()))
+                    if e.peer_name() in peers:
+                        del peers[e.peer_name()]
+                        if args.get('gossip_connect') and (len(peers) == 0 or e.peer_name() == peer_first):
+                            logger.debug('lost connection to gossip node, reconnecting...')
+
+                            if args.get('gossip_publickey'):
+                                n.gossip_connect_curve(args['gossip_publickey'], args['gossip_connect'])
+                            else:
+                                n.gossip_connect(args['gossip_connect'])
+                    pipe_s.send_multipart(['EXIT'.encode('utf-8'), e.peer_name(), str(len(peers)).encode('utf-8')])
+
+                elif msg_type == 'EVASIVE':
+                    if TRACE or TRACE_EVASIVE:
+                        logger.debug('EVASIVE {}'.format(e.peer_name()))
+
+                    pipe_s.send_multipart(['EVASIVE'.encode('utf-8'), e.peer_name()])
+
+                else:
+                    logger.warn('unknown message type: {}'.format(msg_type))
+
+        except Exception as e:
+            logger.exception("Unhandled exception in main io loop")
+
+    logger.debug('shutting down node')
+    n.stop()
+
+    logger.debug('done')
