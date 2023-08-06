ace785b46d20c420cf1e60859b5f821cbd151dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:22 +0000

    Add package tanktools/1.0.2/tanktools-1.0.2-py2-none-any.whl

---
+++ packages/tanktools/tanktools-1.0.2-py2-none-any.whl/tanktools/__init__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2018 Alexander A. Grechin
+#
+# Licensed under the BSD 3-Clause license.
+# See LICENSE file in the project root for full license information.
+#
+"""Global constants"""
+
+__author__ = 'Alexander Grechin'
+__author_email__ = 'infinum@mail.ru'
+__license__ = 'BSD'
+__version__ = '1.0.2'
