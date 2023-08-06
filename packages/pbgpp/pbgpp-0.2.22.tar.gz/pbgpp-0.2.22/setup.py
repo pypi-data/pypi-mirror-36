0de615a519e3383459a565dcebb4cb3c7c2744be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:41 +0000

    Add package pbgpp/0.2.22/pbgpp-0.2.22.tar.gz

---
+++ packages/pbgpp/pbgpp-0.2.22.tar.gz/pbgpp-0.2.22/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+packages = ['pbgpp.' + pkg for pkg in find_packages('pbgpp')]
+packages.append('pbgpp')
+
+setup(
+    name='pbgpp',
+    version='0.2.22',
+    description='PCAP BGP Parser',
+    author='DE-CIX Management GmbH',
+    author_email='rnd@de-cix.net',
+    url='https://github.com/de-cix/pbgp-parser',
+    keywords=['bgp', 'parsing', 'pcap'],
+    license='Apache License 2.0',
+
+    zip_safe=False,
+    packages=packages,
+    install_requires=['pcapy', 'kafka-python'],
+    entry_points={
+        'console_scripts': [
+            'pbgpp = pbgpp.Application.CLI:main'
+        ]
+    }
+)
