b303ad1a68091678c46c4127b81b87e89266fd8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:15 +0000

    Add package MailboxValidator/1.1.0/MailboxValidator-1.1.0-py3-none-any.whl

---
+++ packages/MailboxValidator/MailboxValidator-1.1.0-py3-none-any.whl/MailboxValidator/SingleValidation.py
@@ -0,0 +1,44 @@
+import http.client
+import urllib
+import hashlib
+import json
+
+class SingleValidation:
+    def __init__(self, apikey):
+        self.apikey = apikey
+
+    def ValidateEmail(self, email):
+        p = { 'key': self.apikey, 'format': 'json', 'email': email }
+
+        try:
+            conn = http.client.HTTPConnection("api.mailboxvalidator.com")
+            conn.request("GET", "/v1/validation/single?" + urllib.parse.urlencode(p))
+            res = conn.getresponse()
+            # print res.read()
+            return json.loads(res.read())
+        except:
+            return None
+
+    def DisposableEmail(self, email):
+        p = { 'key': self.apikey, 'format': 'json', 'email': email }
+
+        try:
+            conn = http.client.HTTPConnection("api.mailboxvalidator.com")
+            conn.request("GET", "/v1/email/disposable?" + urllib.parse.urlencode(p))
+            res = conn.getresponse()
+            # print res.read()
+            return json.loads(res.read())
+        except:
+            return None
+
+    def FreeEmail(self, email):
+        p = { 'key': self.apikey, 'format': 'json', 'email': email }
+
+        try:
+            conn = http.client.HTTPConnection("api.mailboxvalidator.com")
+            conn.request("GET", "/v1/email/free?" + urllib.parse.urlencode(p))
+            res = conn.getresponse()
+            # print res.read()
+            return json.loads(res.read())
+        except:
+            return None
