b9c67da460264036e821c3e0a04d3b7136db287c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:37 +0000

    Add package pypi-install/0.0.5/pypi_install-0.0.5.tar.gz

---
+++ packages/pypi-install/pypi_install-0.0.5.tar.gz/pypi_install-0.0.5/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+setuptools.setup(
+	name="pypi_install",
+	version="0.0.5",
+	author="Osama Arshad Dar",
+	author_email="14beeodar@seecs.edu.pk",
+	description="Uploading code to PyPi made easy",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/daroodar/Pypi-Install",
+	packages=setuptools.find_packages(),
+	classifiers=[
+	"Programming Language :: Python :: 2",
+	"License :: OSI Approved :: MIT License",
+	"Operating System :: OS Independent",
+	],
+)
