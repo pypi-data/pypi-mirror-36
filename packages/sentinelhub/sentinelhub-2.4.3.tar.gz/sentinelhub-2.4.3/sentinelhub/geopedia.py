591cbb446e86c7cd51dc5f68e7b7a87e13af583d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:05 +0000

    Add package sentinelhub/2.4.3/sentinelhub-2.4.3.tar.gz

---
+++ packages/sentinelhub/sentinelhub-2.4.3.tar.gz/sentinelhub-2.4.3/sentinelhub/geopedia.py
@@ -0,0 +1,48 @@
+"""
+Module for working with Geopedia OGC services
+"""
+
+import logging
+
+from .ogc import OgcImageService
+from .config import SHConfig
+
+LOGGER = logging.getLogger(__name__)
+
+
+class GeopediaService:
+    """ The class for Geopedia OGC services
+
+    :param base_url: Base url of Geopedia's OGC services. If ``None``, the url specified in the configuration
+                    file is taken.
+    :type base_url: str or None
+    """
+    def __init__(self, base_url=None):
+        self.base_url = SHConfig().gpd_base_url if base_url is None else base_url
+
+
+class GeopediaImageService(GeopediaService, OgcImageService):
+    """Geopedia OGC services class for providing image data. Most of the methods are inherited from
+    `sentinelhub.ogc.OgcImageService` class.
+
+    :param base_url: Base url of Geopedia's OGC services. If ``None``, the url specified in the configuration
+                    file is taken.
+    :type base_url: str or None
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def get_dates(self, request):
+        """ Geopedia does not support date queries
+
+        :param request: OGC-type request
+        :type request: WmsRequest or WcsRequest
+        :return: Undefined date
+        :rtype: [None]
+        """
+        return [None]
+
+    def get_wfs_iterator(self):
+        """ This method is inherited from OgcImageService but is not implemented.
+        """
+        raise NotImplementedError
