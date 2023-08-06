b9108618787aa621160ec0b74b26858523d873b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:17 +0000

    Add package dotfiler/0.0.2/dotfiler-0.0.2.tar.gz

---
+++ packages/dotfiler/dotfiler-0.0.2.tar.gz/dotfiler-0.0.2/dot/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from .api import (  # noqa: F401
+    link_topic,
+    list_topics,
+)
+from .config import (  # noqa: F401
+    DotfileConfig,
+    get_config,
+    parse_topic,
+)
+from .topic import (  # noqa:F401
+    Topic,
+)
+from .cli import (  # noqa:F401
+    cli,
+)
