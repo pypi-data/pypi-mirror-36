8cb41d6acdebe32f126a2acd88e206edcb760f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:55 +0000

    Add package dcbase/1.5.3/dcbase-1.5.3.tar.gz

---
+++ packages/dcbase/dcbase-1.5.3.tar.gz/dcbase-1.5.3/base/builder/project_additional/api/blog/tags.py
@@ -0,0 +1,32 @@
+# coding= utf-8
+
+from base.application.components import Base
+from base.application.components import api
+from base.application.components import authenticated
+
+from src.models.knowledgebase import PostCategory, Tag
+
+
+@authenticated()
+@api(
+    URI='/wiki/tags',
+    SPECIFICATION_PATH='Blog'
+)
+class GetAllTags(Base):
+
+    def get(self):
+        import base.common.orm
+        return self.ok({'tags': Tag.all(base.common.orm.orm.session())})
+
+
+@authenticated()
+@api(
+    URI='/wiki/categories',
+    SPECIFICATION_PATH='Blog'
+)
+class GetAllCategories(Base):
+
+    def get(self):
+        import base.common.orm
+        return self.ok({'categories': PostCategory.all(base.common.orm.orm.session())})
+
