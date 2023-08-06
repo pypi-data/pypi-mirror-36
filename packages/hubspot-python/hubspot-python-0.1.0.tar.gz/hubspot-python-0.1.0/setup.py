7a15b61d67a04b8da6c9757cbdfe71e4c79b86d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:21 +0000

    Add package hubspot-python/0.1.0/hubspot-python-0.1.0.tar.gz

---
+++ packages/hubspot-python/hubspot-python-0.1.0.tar.gz/hubspot-python-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='hubspot-python',
+      version='0.1.0',
+      description='HubSpot API written in python',
+      author='Lelia Rubiano',
+      author_email='lrubiano5@gmail.com',
+      url='https://github.com/GearPlug/hubspot-python.git',
+      packages=['hubspot'],
+      install_requires=[
+          'requests',
+      ],
+      keywords=['hubspot', 'crm'],
+      zip_safe=False,
+      license='GPL',
+     )
