45c1e1fef5ee12161d04b93be5ab8afd9ea02bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:02 +0000

    Add package pyessh/0.1.3/pyessh-0.1.3.tar.gz

---
+++ packages/pyessh/pyessh-0.1.3.tar.gz/pyessh-0.1.3/README.md
@@ -0,0 +1,42 @@
+# Pyessh
+A tool for easy to use SSH execute commands at multi-server.
+
+
+![gif](https://github.com/Leviathan1995/pyessh/blob/master/doc/pyessh.gif)
+
+
+## Usage
+
+#### Installation
+
+```
+# pip3 install paramiko
+# pip3 install prompt_toolkit
+# pip3 install pyessh
+```
+
+#### Editing the Configuration File
+
+```
+# vim /etc/local/pyessh.conf
+```
+
+add remote server IP addresses at pyessh.conf
+
+```
+127.0.0.1
+...
+```
+
+#### Get Started
+
+```
+# pyessh
+```
+and type your command
+
+
+
+## License
+
+MIT
---
+++ packages/pyessh/pyessh-0.1.3.tar.gz/pyessh-0.1.3/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+try:
+    # Use setuptools if available, for install_requires (among other things).
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup (
+    name='pyessh',
+    packages=['pyessh'],
+    version='0.1.3',
+    description='A tool for easy to execute commands at multiserver',
+    long_description=open('README.md').read(),
+    author='leviathan1995',
+    author_email='leviathan0992@gmail.com',
+    url='https://github.com/Leviathan1995/pyessh',
+    license='MIT',
+    install_requires=[
+        'paramiko',
+        'plumbum',
+        'prompt_toolkit',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyessh = pyessh.pyessh:Pyessh.run'
+        ]
+    },
+    include_package_data=True,
+    data_files=[('/usr/local/', ['pyessh.conf'])]
+)
