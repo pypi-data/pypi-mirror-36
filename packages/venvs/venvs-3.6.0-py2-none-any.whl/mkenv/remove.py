0815247d497888178e36c89f60010c63611f3e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:01 +0000

    Add package venvs/3.6.0/venvs-3.6.0-py2-none-any.whl

---
+++ packages/venvs/venvs-3.6.0-py2-none-any.whl/mkenv/remove.py
@@ -0,0 +1,28 @@
+from filesystems.exceptions import FileNotFound
+import click
+
+from mkenv.common import _FILESYSTEM, _ROOT, _EX_NOINPUT
+
+
+def run(locator, filesystem, names, force):
+    for name in names:
+        virtualenv = locator.for_name(name=name)
+        try:
+            virtualenv.remove_from(filesystem=filesystem)
+        except FileNotFound:
+            if not force:
+                return _EX_NOINPUT
+
+
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@_FILESYSTEM
+@_ROOT
+@click.option(
+    "-f", "--force",
+    flag_value=True,
+    help="Ignore errors if the virtualenv does not exist.",
+)
+@click.argument("names", nargs=-1)
+@click.pass_context
+def main(context, **kwargs):
+    context.exit(run(**kwargs) or 0)
