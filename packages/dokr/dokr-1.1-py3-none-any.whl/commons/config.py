5e28d77bd57a05a1bdd3b7f3933eccd85edf4658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:17 +0000

    Add package dokr/1.1/dokr-1.1-py3-none-any.whl

---
+++ packages/dokr/dokr-1.1-py3-none-any.whl/commons/config.py
@@ -0,0 +1,47 @@
+import os 
+import json 
+import sys
+import logging
+import pkg_resources  # part of setuptools
+   
+pth = os.getcwd() + "/commons/config.json";
+
+    
+def read_config():
+    logging.info("pth" + pth)
+    with open(pth) as json_file:  
+        data = json.load(json_file)
+    return data
+
+
+def write_config(data):
+    with open(pth, 'w') as outfile: 
+        json.dump(data, outfile, indent=4)
+
+
+def enable_debugging():
+    data = read_config()
+    data['debug'] = True
+    write_config(data)
+
+
+def set_env(key , value):
+    data = read_config()
+    data[key] = value
+    write_config(data)
+
+
+def get_env(key):
+    return read_config()[key]
+
+
+def log_config():
+   # level_input = sys.argv[2];
+    level_input = 'DEBUG';
+    if level_input == 'DEBUG':
+        print(level_input)
+        logging.basicConfig(level=logging.DEBUG)
+        
+def find_version():
+    version = pkg_resources.require("dokr")[0].version  
+    print (version)      
