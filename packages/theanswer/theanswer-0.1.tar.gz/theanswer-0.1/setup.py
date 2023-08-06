662458c8176a0d1b9b5e6a019e6d82648482b111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:17 +0000

    Add package theanswer/0.1/theanswer-0.1.tar.gz

---
+++ packages/theanswer/theanswer-0.1.tar.gz/theanswer-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='theanswer',
+      version='0.1',
+      description='answer to the ultimate question',
+      url='https://github.com/zabop/PyPIattempt',
+      author='Pal Szabo',
+      author_email='szabopal96@gmail.com',
+      license='MIT',
+      packages=['theanswer'],
+      zip_safe=False)
