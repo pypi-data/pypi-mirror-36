aec21ca49442cea26b13d0cb3d33d135b94b3d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:35 +0000

    Add package GXBubble/0.1.9/GXBubble-0.1.9.tar.gz

---
+++ packages/GXBubble/GXBubble-0.1.9.tar.gz/GXBubble-0.1.9/gxbubble/unity.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of ProjectBubble.
+#
+# (c) Giant - MouGuangyi <mouguangyi@ztgame.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+
+import os
+from gxbubble import shell
+
+
+class Unity:
+    __path = ''
+
+    def __init__(self, unity_path):
+        self.__path = os.environ.get(unity_path, '')
+
+    def valid(self):
+        return os.path.exists(self.__path)
+
+    def run(self, **options):
+        if not self.valid():
+            raise FileNotFoundError('CANNOT find Unity!')
+
+        # logpath = options.get('logFile', None)
+        # if logpath is not None:
+        #     print('tail -f %s' % logpath)
+        #     os.system('tail -f %s' % logpath)
+
+        parameters = ' '
+        for k, v in options.items():
+            if v in (True, False):
+                parameters += (' -' + k) if v else ''
+            else:
+                parameters += (' -' + k + ' ' + v)
+
+        return shell(self.__path + parameters)
+
