50e54a71660b42857d6af7d2f5430f1b8474ef3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:05 +0000

    Add package dirbpy/1.2.3/dirbpy-1.2.3.tar.gz

---
+++ packages/dirbpy/dirbpy-1.2.3.tar.gz/dirbpy-1.2.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='dirbpy',
+    version='1.2.3',
+    author='Marc-Olivier Bouchard',
+    author_email='mo.bouchard1997@gmail.com',
+    url='https://github.com/marcolivierbouch/dirbpy',
+    description='This is the new version of dirb in python.',
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    package_dir={"": "src"},
+    platforms=["unix", "linux", "osx"],
+    install_requires=[
+        "argparse",
+        "requests",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    py_modules=["dirbpy"],
+    zip_safe=False,
+)
+
