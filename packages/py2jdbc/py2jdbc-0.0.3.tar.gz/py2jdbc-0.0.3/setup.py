e36d1d683b8dc9be6f3981b92ac30a59a0b94671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:00 +0000

    Add package py2jdbc/0.0.3/py2jdbc-0.0.3.tar.gz

---
+++ packages/py2jdbc/py2jdbc-0.0.3.tar.gz/py2jdbc-0.0.3/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#  -*- coding: utf8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+from setuptools import setup
+
+setup(
+    name='py2jdbc',
+    version='0.0.3',
+    license='BSD 2-Clause License',
+    description="Python DBI interface to JDBC databases",
+    long_description="""
+``py2jdbc`` is an open source Python module that accesses JDBC-compliant databases.
+It implements the [DB API 2.0](https://www.python.orgfdev/peps/pep-0249)
+specification.
+
+- Documentation: http://py2jdbc.readthedocs.org/
+- Source: https://github.com/swstephe/py2jdbc
+- Download: https://pypi.python.org/pypi/py2jdbc
+    """,
+    author='Scott Stephens',
+    author_email='scott@ariftek.com',
+    url='https://github.com/swstephe/py2jdbc',
+    packages=['py2jdbc'],
+    package_dir={'': '.'},
+    py_modules=['py2jdbc'],
+    include_package_data=True,
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Database',
+    ],
+    keywords='Database, JDBC, JNI, PyPy'
+)
