04890eb9a1d351b79173a7e49c33c2ce438154d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:04 +0000

    Add package id4me-rp-client/0.0.2/id4me-rp-client-0.0.2.tar.gz

---
+++ packages/id4me-rp-client/id4me-rp-client-0.0.2.tar.gz/id4me-rp-client-0.0.2/id4me_rp_client/id4me_constants.py
@@ -0,0 +1,64 @@
+from enum import Enum
+
+
+class OIDCScope(Enum):
+    openid = 'openid'
+    profile = 'profile'
+    email = 'email'
+    address = 'address'
+    phone = 'phone'
+    offline_access = 'offline_access'
+
+    def __str__(self):
+        return self.value
+
+
+class OIDCApplicationType(Enum):
+    web = 'web'
+    native = 'native'
+
+    def __str__(self):
+        return self.value
+
+
+class OIDCPrompt(Enum):
+    none = 'none'
+    login = 'login'
+    consent = 'consent'
+    login_and_consent = 'login consent'
+    selectaccount = 'select_account'
+    selectaccount_and_login = 'select_account login'
+    selectaccount_and_consent = 'select_account consent'
+    selectaccount_and_login_and_consent = 'select_account login consent'
+
+    def __str__(self):
+        return self.value
+
+
+class OIDCClaim(Enum):
+    sub = 'sub'
+    name = 'name'
+    given_name = 'given_name'
+    family_name = 'family_name'
+    middle_name = 'middle_name'
+    nickname = 'nickname'
+    preferred_username = 'preferred_username'
+    profile = 'profile'
+    picture = 'picture'
+    website = 'website'
+    email = 'email'
+    email_verified = 'email_verified'
+    gender = 'gender'
+    birthdate = 'birthdate'
+    zoneinfo = 'zoneinfo'
+    locale = 'locale'
+    phone_number = 'phone_number'
+    phone_number_verified = 'phone_number_verified'
+    address = 'address'
+    updated_at = 'updated_at'
+
+    def __str__(self):
+        return self.value
+
+class ID4meClaim(Enum):
+    pass
---
+++ packages/id4me-rp-client/id4me-rp-client-0.0.2.tar.gz/id4me-rp-client-0.0.2/id4me_rp_client/id4me_exceptions.py
@@ -0,0 +1,21 @@
+
+class ID4meException(Exception):
+    pass
+
+class ID4meDNSResolverException(ID4meException):
+    pass
+
+class ID4meDNSSECException(ID4meException):
+    pass
+
+class ID4meAuthorityConfigurationException(ID4meException):
+    pass
+
+class ID4meRelyingPartyRegistrationException(ID4meException):
+    pass
+
+class ID4meTokenRequestException(ID4meException):
+    pass
+
+class ID4meUserInfoRequestException(ID4meException):
+    pass
