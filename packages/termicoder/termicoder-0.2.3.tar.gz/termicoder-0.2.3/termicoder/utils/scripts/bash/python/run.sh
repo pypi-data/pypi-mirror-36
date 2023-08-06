0f1ba24ea9f19c609a81ee85e78efd904cb9934f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:15 +0000

    Add package termicoder/0.2.3/termicoder-0.2.3.tar.gz

---
+++ packages/termicoder/termicoder-0.2.3.tar.gz/termicoder-0.2.3/termicoder/utils/scripts/bash/python/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# this script runs PRG with reading
+# USAGE . run.sh <python file name>
+
+code=$1
+
+(python3 $code)
---
+++ packages/termicoder/termicoder-0.2.3.tar.gz/termicoder-0.2.3/termicoder/utils/templates/python/template.py
@@ -0,0 +1,10 @@
+# Author:      YourName
+# Institution: YourInstitute
+t = int(input().strip())
+
+for i in range(t):
+    # do something
+    pass
+
+# <> with <3 using Termicoder:
+# https://termicoder.github.io
