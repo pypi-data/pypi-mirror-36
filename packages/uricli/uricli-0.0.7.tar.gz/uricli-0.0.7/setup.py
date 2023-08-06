9e0dd89301a3880cda1f84b1ef28423ec9564d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:45 +0000

    Add package uricli/0.0.7/uricli-0.0.7.tar.gz

---
+++ packages/uricli/uricli-0.0.7.tar.gz/uricli-0.0.7/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+
+"""setup.py: setuptools control."""
+
+import re
+from setuptools import setup
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('uricli/uricli.py').read(),
+    re.M
+).group(1)
+
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+setup(
+    name="uricli",
+    packages=["uricli"],
+    entry_points={
+        "console_scripts": ['uri = uricli.uricli:main']
+    },
+    version=version,
+    description="Utility CLI for submitting solutions to URI Online Judge.",
+    long_description=long_descr,
+    author="Rafael Telles",
+    author_email="rafael@telles.pw",
+    install_requires=[
+        "docopt==0.6.2",
+        "PyInquirer==1.0.2",
+        "requests==2.19.1",
+        "mechanicalSoup==0.11.0",
+    ],
+    url="https://github.com/rafael-telles/uri-cli",
+)
