69fb8f6a298e6d6cba554478b5c0c38d60f45846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:32 +0000

    Add package swagger-marshmallow-codegen/0.4.1/swagger_marshmallow_codegen-0.4.1-py2.py3-none-any.whl

---
+++ packages/swagger-marshmallow-codegen/swagger_marshmallow_codegen-0.4.1-py2.py3-none-any.whl/swagger_marshmallow_codegen/cmd.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+import sys
+import logging
+import argparse
+from magicalimport import import_symbol
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--driver", default="swagger_marshmallow_codegen.driver:Driver")
+    parser.add_argument("--logging", default="INFO", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"])
+    parser.add_argument("--full", default=False, action="store_true")
+    parser.add_argument("file", default=None)
+    args = parser.parse_args()
+
+    driver_cls = args.driver
+    if ":" not in driver_cls:
+        driver_cls = "swagger_marshmallow_codegen.driver:{}".format(driver_cls)
+
+    if args.full:
+        options = {"targets": {"schema": True, "input": True, "output": True}}
+    else:
+        options = {"targets": {"schema": True}}
+
+    driver = import_symbol(driver_cls)(options)
+
+    # todo: option
+    logging.basicConfig(
+        format="%(levelname)5s:%(name)36s:%(message)s",
+        level=logging._nameToLevel[args.logging]
+    )
+
+    if args.file is None:
+        driver.run(sys.stdin, sys.stdout)
+    else:
+        with open(args.file) as rf:
+            driver.run(rf, sys.stdout)
