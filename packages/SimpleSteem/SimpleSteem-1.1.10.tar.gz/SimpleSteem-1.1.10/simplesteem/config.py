9b555a78b4eb1c497de08ff1b781e09d9bd6f2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:36 +0000

    Add package SimpleSteem/1.1.10/SimpleSteem-1.1.10.tar.gz

---
+++ packages/SimpleSteem/SimpleSteem-1.1.10.tar.gz/SimpleSteem-1.1.10/simplesteem/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+screenmode = "verbose"
+permissions = "login,offline,vote"
+logpath = ""
+client_id = ""
+callback_url = ""
+mainaccount = "artturtle"
+keys = ['5KN2rkFoR1s6hNdA5VbQqh5rd3Atvnf46apRhhnCTup25quQusv','5JSckknbPMJ1if6yY2f1j8ei5ssbdnXecm29qQDxL5XZbxWtJgA']
+nodes = ["https://steemd.minnowsupportproject.org","https://gtg.steem.house:8090","https://steemd.pevo.science","https://rpc.steemliberator.com"]
+client_secret = ""
