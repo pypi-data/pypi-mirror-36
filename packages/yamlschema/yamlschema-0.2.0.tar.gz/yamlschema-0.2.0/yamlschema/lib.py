a6290b58f1bebe63f53fbdbd4619dae07a37d305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:59 +0000

    Add package yamlschema/0.2.0/yamlschema-0.2.0.tar.gz

---
+++ packages/yamlschema/yamlschema-0.2.0.tar.gz/yamlschema-0.2.0/yamlschema/lib.py
@@ -0,0 +1,31 @@
+"""
+Validate a YAML file
+"""
+from __future__ import print_function
+
+import sys
+
+import yaml
+
+import click
+
+from jsonschema import validate, FormatChecker
+
+
+@click.command()
+@click.argument('yamlfile', type=click.File())
+@click.argument('yamlschema', type=click.File())
+def validateYAMLCLI(yamlfile, yamlschema):
+    print("Validating {}\n".format(yamlfile.name), file=sys.stderr)
+    validateYAML(yamlfile, yamlschema)
+    print("{} is valid\n".format(yamlfile.name), file=sys.stderr)
+
+
+def validateYAML(yamlFile, yamlSchema, loader=yaml.Loader):
+    """
+    Validate yaml file based off of a schema
+    """
+    contents = yaml.load(yamlFile, Loader=loader)
+    schema = yaml.load(yamlSchema, Loader=loader)
+    validate(contents, schema, format_checker=FormatChecker())
+    return True
