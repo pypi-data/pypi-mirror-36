a779928a496ac6ee3a1fffed473e05145ffd67c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:44 +0000

    Add package envinfo/1.5.2/envinfo-1.5.2.tar.gz

---
+++ packages/envinfo/envinfo-1.5.2.tar.gz/envinfo-1.5.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='envinfo',
+    version='1.5.2',
+    install_requires=[
+        'synfo'
+    ],
+    url='https://github.com/mrstephenneal/synfo',
+    license='GPL-3.0',
+    author='Stephen Neal',
+    author_email='stephen@stephenneal.net',
+    description='Legacy wrapper module for synfo.'
+)
