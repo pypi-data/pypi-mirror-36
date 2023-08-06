016e59c31bd15426780b0ccdfd9e48a7aca96a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:10 +0000

    Add package GBprobability/0.1/GBprobability-0.1.tar.gz

---
+++ packages/GBprobability/GBprobability-0.1.tar.gz/GBprobability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='GBprobability',
+      version='0.1',
+      author = 'Tagne Nguifo Eric Hermann',
+      description='Gaussian distributions',
+      packages=['GBprobability'],
+      zip_safe=False)
