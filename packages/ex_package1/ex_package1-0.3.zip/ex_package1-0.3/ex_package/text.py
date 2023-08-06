07ddba31fc880385dde77a85fbc752477ce23fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:18 +0000

    Add package ex_package1/0.3/ex_package1-0.3.zip

---
+++ packages/ex_package1/ex_package1-0.3.zip/ex_package1-0.3/ex_package/text.py
@@ -0,0 +1,11 @@
+'''
+package: ex_package
+version: 0.1
+
+'''
+from __init__ import __version__
+print __version__
+
+def joke():
+    print (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
---
+++ packages/ex_package1/ex_package1-0.3.zip/ex_package1-0.3/setup.py
@@ -0,0 +1,14 @@
+#from setuptools import setup
+from distutils.core import setup
+setup(name='ex_package1',
+      version='0.3',
+      description='1st prog',
+	  url='https://www.python.org/lavanya123/ex_package',
+      author='lavanya123',
+      author_email='lavanyapavuluri@123.com',
+      license='MIT',
+	  packages=['ex_package'],
+      zip_safe=False)
+     
+
+
