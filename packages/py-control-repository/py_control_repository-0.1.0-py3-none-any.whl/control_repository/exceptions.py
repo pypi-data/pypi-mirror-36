6b33f7610298340c1619bd7d2b00453ab4ecbbfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:47 +0000

    Add package py-control-repository/0.1.0/py_control_repository-0.1.0-py3-none-any.whl

---
+++ packages/py-control-repository/py_control_repository-0.1.0-py3-none-any.whl/control_repository/exceptions.py
@@ -0,0 +1,45 @@
+class ControlRepositoryException(Exception):
+    """
+    Main module exception.
+    Also raised when ControlRepository cannot be initialized
+    """
+    pass
+
+
+class EnvironmentNotFoundException(ControlRepositoryException):
+    """
+    Puppet environment not found in the control repository
+    """
+    pass
+
+
+class PuppetfileNotFoundException(ControlRepositoryException):
+    """
+    No Puppetfile in the current environment
+    """
+    pass
+
+
+class PuppetfileUpdateException(ControlRepositoryException):
+    """
+    Puppetfile fails to write content to github
+    """
+    pass
+
+
+class ModuleParserException(ControlRepositoryException):
+    """
+    No valid module can be parsed
+    """
+    pass
+
+
+class ModuleAlreadyPresentException(ControlRepositoryException):
+    """
+    When a puppet module is already present in some puppetfile
+    """
+    pass
+
+
+class ModuleBadGitReferenceTypeExcption(Exception):
+    pass
