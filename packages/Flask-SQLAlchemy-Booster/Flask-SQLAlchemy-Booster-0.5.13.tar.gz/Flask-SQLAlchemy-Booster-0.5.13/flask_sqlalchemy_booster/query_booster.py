c5731ff82ede0d4860ddbfa725c32f16323b46d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:18 +0000

    Add package Flask-SQLAlchemy-Booster/0.5.13/Flask-SQLAlchemy-Booster-0.5.13.tar.gz

---
+++ packages/Flask-SQLAlchemy-Booster/Flask-SQLAlchemy-Booster-0.5.13.tar.gz/Flask-SQLAlchemy-Booster-0.5.13/flask_sqlalchemy_booster/query_booster.py
@@ -0,0 +1,57 @@
+from flask_sqlalchemy import BaseQuery
+
+
+class QueryBooster(BaseQuery):
+
+    cls = None
+
+    def desc(self, attr='id'):
+        return self.order_by(getattr(self.model_class, attr).desc())
+
+    def asc(self, attr='id'):
+        return self.order_by(getattr(self.model_class, attr))
+
+    def last(self, *criterion, **kwargs):
+        return self.filter_by(**kwargs).filter(*criterion).desc().first()
+
+    def get_all(self, keyvals, key='id'):
+        if len(keyvals) == 0:
+            return []
+        original_keyvals = keyvals
+        keyvals_set = list(set(keyvals))
+        resultset = self.filter(getattr(self.model_class, key).in_(keyvals_set))
+        key_result_mapping = {getattr(result, key): result for result in resultset.all()}
+        return [key_result_mapping.get(kv) for kv in original_keyvals]
+
+    # def get(self, keyval, key='id'):
+    #     if keyval is None:
+    #         return None
+    #     if key not in self.model_class.__table__.columns:
+    #         raise Exception("Not a valid key")
+    #     if self.model_class.__table__.columns[key].primary_key:
+    #         try:
+    #             self._get_existing_condition()
+    #             return self.get(keyval)
+    #         except:
+    #             return self.filter(getattr(self.model_class, key) == keyval).first()
+    #     else:
+    #         return self.filter(getattr(self.model_class, key) == keyval).first()
+
+    def is_joined_with(self, model_class):
+        return model_class in [entity.class_ for entity in self._join_entities]
+
+
+    def buckets(self, bucket_size=None, offset_to_start_from=0, dont_apply_offsets_on_buckets=False):
+        if dont_apply_offsets_on_buckets:
+            items_count = self.count()
+            while items_count > 0:
+                items = self.limit(bucket_size).offset(offset_to_start_from).all()
+                yield items
+                items_count = self.count()
+        else:
+            items_count = self.offset(offset_to_start_from).count()
+            no_of_buckets = items_count / bucket_size  + 1
+            for bucket in range(no_of_buckets):
+                items = self.limit(bucket_size).offset(offset_to_start_from + bucket*bucket_size).all()
+                yield items
+
