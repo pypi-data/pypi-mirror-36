e464b7bc0d8f9b92c9017af8673ade2ef1093c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:11 +0000

    Add package PyDaron/0.0.1/PyDaron-0.0.1-py3.6.egg

---
+++ packages/PyDaron/PyDaron-0.0.1-py3.6.egg/PyDaron/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+use crypt() function with string and key
+Exemple:
+
+crypted,key = crypt('THIS IS STRING','THIS IS KEY')
+
+
+use decrypt() function with crypted string and key
+
+Ex
+
+decrypted = decrypt('CRYPTED','KEY')
+
+
+"""
+__version__ = "0.0.1"
+
+from PyDaron.crypto import *
+from PyDaron.decrypto import *
+
+
+
+name = "PyDaron"
