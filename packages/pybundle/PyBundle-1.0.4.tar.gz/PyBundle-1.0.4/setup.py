df48ae7c429f16fe3088585627dcc921441a3b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:08 +0000

    Add package pybundle/1.0.4/PyBundle-1.0.4.tar.gz

---
+++ packages/pybundle/PyBundle-1.0.4.tar.gz/PyBundle-1.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyBundle',
+    version='1.0.4',
+    packages=find_packages(),
+    url='https://github.com/mrstephenneal/PyBundle',
+    license='MIT',
+    author='Stephen Neal',
+    author_email='stephen@stephenneal.net',
+    description='Wrapper functions for adjusting paths in executable builds'
+)
