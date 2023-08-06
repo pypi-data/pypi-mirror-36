7ae9331787b0f598d5a10de3abe4fb1b9511ea21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:51 +0000

    Add package mxmul-pkg/0.1.0/mxmul_pkg-0.1.0.tar.gz

---
+++ packages/mxmul-pkg/mxmul_pkg-0.1.0.tar.gz/mxmul_pkg-0.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mxmul_pkg",
+    version="0.1.0",
+    author="Tian Song",
+    author_email="songtian@bit.edu.cn",
+    description="An exmaple for teaching how to publish a Python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
