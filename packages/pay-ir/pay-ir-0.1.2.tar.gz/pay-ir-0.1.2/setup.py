9452d7ffade69c6982cf740f7d3dd73ca00d9a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:48 +0000

    Add package pay-ir/0.1.2/pay-ir-0.1.2.tar.gz

---
+++ packages/pay-ir/pay-ir-0.1.2.tar.gz/pay-ir-0.1.2/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='pay-ir',
+    version='0.1.2',
+    packages=find_packages(),
+    test_suite='pay_ir.tests',
+    include_package_data=True,
+    author='Moeen Zamani',
+    author_email='moeen.zamani@gmail.com',
+    license='MIT',
+    description='Python interface for pay.ir payment gateway.',
+    url='https://github.com/moeenz/pay-ir',
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python'
+    ],
+)
