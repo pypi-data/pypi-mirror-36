963d497cfe838256bb447ac38706b03bdbcbe462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:17 +0000

    Add package nino-violino/0.0.10/nino_violino-0.0.10-py3-none-any.whl

---
+++ packages/nino-violino/nino_violino-0.0.10-py3-none-any.whl/block_generator/block_organizer.py
@@ -0,0 +1,61 @@
+import random, copy
+
+def get_ending_of_block(timing_data):
+    block_end = timing_data['starting_beats'][-1] + timing_data['number_of_bars'] * timing_data['bar_length']
+    return block_end
+
+def check_block_repeated(block, blocks, no_blocks):
+    blocks_to_check = blocks[-no_blocks:]
+    block_repeated = all([block['block_data']['id'] == check_block['block_data']['id'] for check_block in blocks])
+    return block_repeated
+
+def get_blocks_ids(blocks):
+    ids = list(set([b['block_data']['id'] for b in blocks]))
+    return ids
+
+def shuffle_blocks(blocks):
+    shuffled_blocks = []
+    while any([b['block_data']['block_occurences'] for b in blocks]):
+        new_block = random.choice(blocks)
+
+        if len(shuffled_blocks) > 1 and len(blocks) > 1:
+            block_has_many_repeats = check_block_repeated(new_block, shuffled_blocks, 2) 
+            block_ids = get_blocks_ids(blocks)
+            if block_has_many_repeats and len(block_ids) > 1:
+                continue
+        if new_block['block_data']['block_occurences'] == 0: 
+            continue
+
+        print('Putting block ', new_block['block_data']['id'], ' on position ', len(shuffled_blocks), ' which has starting beats ', new_block['structure_data']['timing_data']['starting_beats'])
+        shuffled_blocks.append(copy.deepcopy(new_block))
+        new_block['block_data']['block_occurences'] -= 1
+    return shuffled_blocks
+
+def get_shuffled_blocks(blocks):
+    new_blocks = copy.deepcopy(blocks)
+    shuffled_blocks = shuffle_blocks(new_blocks)
+
+    for b in range(1, len(shuffled_blocks)): 
+        block_t_data = shuffled_blocks[b]['structure_data']['timing_data']
+        last_block_t_data = shuffled_blocks[b-1]['structure_data']['timing_data']
+        new_starting_beats = [get_ending_of_block(last_block_t_data) + starting_beat for starting_beat in block_t_data['starting_beats']]
+
+        shuffled_blocks[b]['structure_data']['timing_data']['starting_beats'] = new_starting_beats
+
+    return shuffled_blocks
+
+
+#This is kind of a hacky solution, so I should probably think of a better way to do it. 
+#Basically, I copy each block based on its number of occurences (block['block_data']['block_occurences']), which makes it easier to calculate the starting beats. 
+#Then I find all corresponding original blocks based on the block_id, and add up the starting beats. 
+def organize_blocks(blocks):
+    shuffled_blocks = get_shuffled_blocks(blocks)
+    for block in blocks: 
+        block['structure_data']['timing_data']['starting_beats'] = []
+
+    for block in blocks: 
+        for s_block in shuffled_blocks: 
+            if block['block_data']['id'] == s_block['block_data']['id']: 
+                block['structure_data']['timing_data']['starting_beats'] += s_block['structure_data']['timing_data']['starting_beats']
+
+    return blocks
---
+++ packages/nino-violino/nino_violino-0.0.10-py3-none-any.whl/block_generator/violino_conf.py
@@ -0,0 +1,33 @@
+import os
+conf_path = '/'.join(os.path.realpath(__file__).split('/')[:-1])
+
+configuration = {
+#    'soundfont' : conf_path + '/misc/SGM-V2.01.sf2',
+    'soundfont' : conf_path + '/misc/32MbGMStereo.sf2',
+
+    'song_path' : conf_path + '/misc/generated_songs/',
+
+    'bpm_change_chance' : 0.15, 
+    'bpm_change_limit' : 3,
+    'bpm_range' : list(range(150, 500, 15)), 
+
+
+    'no_bars_initial' : 5,
+    'occurence_multiplier' : 40,
+    'bar_len_initial' : 20,
+    'bar_len_change_chance' : 0.15,
+
+    'number_of_blocks_range' : [3,6], 
+    'number_of_repeats_range' : list(range(1, 5)),
+    'max_note_len_range' : list(range(2, 5)),
+
+    'base_chords_choices' : ['major', 'minor'],
+    'lower_chord_diff_limit' : 1, 
+    'upper_chord_diff_limit' : 2,
+    'chord_types' : ['major', 'minor'], 
+    'instrument_pool_range' : list(range(2, 9)),
+
+    'base_volume' : 10,
+#    'instrument_pool' : ['piano'],
+
+}
