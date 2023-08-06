856c9efda259a0c6990894fc7a3d430daa079a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:42 +0000

    Add package ase-decaf/0.1.0/ase-decaf-0.1.0.tar.gz

---
+++ packages/ase-decaf/ase-decaf-0.1.0.tar.gz/ase-decaf-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name="ase-decaf",
+    version='0.1.0',
+    packages=['decaf'],
+    license='GNU General Public License v3.0',
+    long_description=open('README.txt').read(),
+    author='Sencer Selcuk',
+    author_email='sselcuk@bnl.gov'
+)
