297f3aa07724b0b670911659a7565417455d7a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:35 +0000

    Add package hsettings/0.1.1/hsettings-0.1.1.tar.gz

---
+++ packages/hsettings/hsettings-0.1.1.tar.gz/hsettings-0.1.1/README.rst
@@ -0,0 +1,19 @@
+Hybrid Settings
+---------------
+
+Description
+===========
+
+This package can load settings from multiple sources and hybrid them into one dict like object.
+
+Installation
+============
+
+For Python 3.5+
+
+```
+pip install hsettings
+```
+
+Usage
+=====
---
+++ packages/hsettings/hsettings-0.1.1.tar.gz/hsettings-0.1.1/setup.py
@@ -0,0 +1,40 @@
+import sys
+import os
+from setuptools import setup, find_packages
+
+sys.path.insert(0, os.path.abspath('lib'))
+import hsettings
+
+
+static_setup_params = dict(
+    name=hsettings.__prog__,
+    version=hsettings.__version__,
+    description=hsettings.__descr__,
+    long_description=hsettings.__doc__,
+    author=hsettings.__author__,
+    author_email=hsettings.__author_email__,
+    url='https://github.com/wwtg99/hsettings',
+    license='MIT',
+    keywords='settings',
+    python_requires='>=3.5',
+    package_dir={'': 'lib'},
+    packages=find_packages('lib'),
+    install_requires=[
+        'pyyaml>=3.13'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        ],
+    # Installing as zip files would break due to references to __file__
+    zip_safe=False
+)
+
+
+def main():
+    """Invoke installation process using setuptools."""
+    setup(**static_setup_params)
+
+
+if __name__ == '__main__':
+    main()
