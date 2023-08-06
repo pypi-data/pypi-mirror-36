ec0e765b5d6c44ba161f7685b246625e2541b396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:15 +0000

    Add package angr-management/7.8.9.26/angr-management-7.8.9.26.tar.gz

---
+++ packages/angr-management/angr-management-7.8.9.26.tar.gz/angr-management-7.8.9.26/angrmanagement/__main__.py
@@ -0,0 +1,71 @@
+import sys
+import os
+import ctypes
+# make pyqode happy
+os.environ['QT_API'] = 'pyside'
+import thread
+
+
+def check_dependencies():
+
+    try:
+        import PySide
+    except ImportError:
+        sys.stderr.write("Cannot find the PySide package. You may install it via pip:\n" +
+                         "    pip install pyside\n")
+        return False
+
+    try:
+        import pyqode.core
+        import pyqode.python
+    except ImportError:
+        sys.stderr.write("Cannot find the pyqode package. You may install it via pip:\n" +
+                         "    pip install pyqode.core pyqode.python\n")
+        return False
+
+    try:
+        import qtconsole
+    except ImportError:
+        sys.stderr.write("Cannot find the qtconsole package. You may install it via pip:\n" +
+                         "    pip install qtconsole\n")
+        return False
+
+    return True
+
+
+def set_app_user_model_id():
+    # Explicitly call SetCurrentProcessExplicitAppUserModelID() so the taskbar icon is displayed correctly.
+
+    if sys.platform == 'win32':
+        winver = sys.getwindowsversion()
+        if winver.major >= 5:
+            myappid = u'angr-management'
+            ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
+
+def main():
+
+    if not check_dependencies():
+        sys.exit(1)
+
+    set_app_user_model_id()
+
+    from PySide.QtGui import QApplication
+
+    from .logic import GlobalInfo
+    from .ui.css import CSS
+    from .ui.main_window import MainWindow
+
+    app = QApplication(sys.argv)
+
+    GlobalInfo.gui_thread = thread.get_ident()
+
+    # apply the CSS
+    app.setStyleSheet(CSS.global_css())
+
+    MainWindow(file_to_open=sys.argv[1] if len(sys.argv) > 1 else None)
+
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
---
+++ packages/angr-management/angr-management-7.8.9.26.tar.gz/angr-management-7.8.9.26/setup.py
@@ -0,0 +1,30 @@
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+    packages = find_packages()
+except ImportError:
+    from distutils.core import setup
+    import os
+    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
+
+setup(
+    name='angr-management',
+    version='7.8.9.26',
+    python_requires='<3.0',
+    description='GUI for angr',
+    url='https://github.com/angr/angr-management',
+    packages=packages,
+    package_data={
+    },
+    install_requires=[
+        'angr==7.8.9.26',
+        'pygments==2.2.0',
+        'websocket-client==0.53.0',
+        'qtconsole==4.4.1',
+        'ipython==5.8.0',
+        'pyzmq==17.1.2',
+        'PySide==1.2.4',
+        'pyqode.core==2.11.0',
+        'pyqode.python==2.10.0',
+    ]
+)
