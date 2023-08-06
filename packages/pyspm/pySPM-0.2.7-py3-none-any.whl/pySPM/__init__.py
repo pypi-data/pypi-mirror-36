2380b9af33ea05604ffceae3e714c6815b3b307a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:25 +0000

    Add package pyspm/0.2.7/pySPM-0.2.7-py3-none-any.whl

---
+++ packages/pyspm/pySPM-0.2.7-py3-none-any.whl/pySPM/__init__.py
@@ -0,0 +1,23 @@
+# -- coding: utf-8 --
+
+# Copyright 2018 Olivier Scholder <o.scholder@gmail.com>
+
+from __future__ import absolute_import
+
+#from . import ToF
+from .SPM import *
+from . import align, utils
+from .nanoscan import Nanoscan
+from .Bruker import Bruker
+from .collection import Collection
+from .ITM import ITM
+from .ITS import ITS
+from .ITA import ITA, ITA_collection
+from .SXM import SXM
+from .utils import constants as const
+
+__all__ = ["ITA","ITS", "ITM", "PCA", "Block", "SPM", "Bruker", "nanoscan", "utils", "SXM"]
+__version__ = '0.2.7'
+__author__ = 'Olivier Scholder'
+__copyright__ = "Copyright 2018, O. Scholder, Zürich, Switzerland"
+__email__ = "o.scholder@gmail.com"
