c574bed1ac54cf010988ca2215d7cce12e017ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:48 +0000

    Add package visdom-observer/0.2.1/visdom_observer-0.2.1.tar.gz

---
+++ packages/visdom-observer/visdom_observer-0.2.1.tar.gz/visdom_observer-0.2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import *
+
+long_description = """
+A sacred `RunObserver` which uses `visdom` to plot logged metrics and image artifacts. Supports creation
+of dynamically updating plots with different sets of metrics easily.
+"""
+
+setup(name='visdom_observer',
+		version='0.2.1',
+		description='A sacred `RunObserver` which uses visdom to plot metrics and artifacts',
+		long_description=long_description,
+		author='Rajat Vadiraj Dwaraknath',
+		url='https://github.com/rajatvd/VisdomObserver',
+		install_requires=['sacred', 'visdom', 'imageio'],
+		author_email='rajatvd@gmail.com',
+		license='MIT',
+		packages=['visdom_observer'],
+		zip_safe=False)
