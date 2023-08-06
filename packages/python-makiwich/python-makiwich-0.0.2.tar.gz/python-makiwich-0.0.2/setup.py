99e1bc680ba028d217294789d824f6d3948107bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:57 +0000

    Add package python-makiwich/0.0.2/python-makiwich-0.0.2.tar.gz

---
+++ packages/python-makiwich/python-makiwich-0.0.2.tar.gz/python-makiwich-0.0.2/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from setuptools import setup
+
+from maki import VERSION
+
+package_name = "python-makiwich"
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist")
+    os.system("twine upload -r pypi dist/%s-%s.tar.gz" % (package_name, VERSION))
+    sys.exit()
+
+if sys.argv[-1] == "tag":
+    os.system("git tag -a v{} -m 'tagging v{}'".format(VERSION, VERSION))
+    os.system("git push && git push --tags")
+    sys.exit()
+
+
+setup(
+    name=package_name,
+    version=VERSION,
+    description="Create markers with maki icons for leaflet",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zostera/python-makiwich",
+    author="Jan Pieter Waagmeester",
+    author_email="jieter@zostera.nl",
+    license="ISC",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="Maki icon markers",
+    packages=["maki"],
+    install_requires=["xmltodict"],
+    package_data={"maki": ["maki/img/*.svg", "maki/img/icons/*.svg"]},
+)
