faf91a880de4caec00e8807fff379110f3c8aea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:41 +0000

    Add package langsci/0.0.7/langsci-0.0.7.tar.gz

---
+++ packages/langsci/langsci-0.0.7.tar.gz/langsci-0.0.7/langsci/txt2bib.py
@@ -0,0 +1,13 @@
+import sys  
+try:
+    from langscibibtex import Record
+except ImportError:
+    from langsci.langscibibtex import Record 
+
+filename = sys.argv[1]
+lines = open(filename).readlines()
+for l in lines: 
+    if l.strip=='':
+        continue
+    r = Record(l) 
+    print(r.bibstring) 
