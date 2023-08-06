0bd067f13c77f4c01283b215e9f17b72b3c16dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:16 +0000

    Add package njnuko/3.0.0.dev0/njnuko-3.0.0.dev0.tar.gz

---
+++ packages/njnuko/njnuko-3.0.0.dev0.tar.gz/njnuko-3.0.0.dev0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '3.0.0'
+
+with open("README.md","r") as fh:
+      long_description=fh.read()
+
+
+
+setup(name='njnuko',
+      version="3.0.0",
+      description="file sorting",
+      long_description="""\
+file sorting project""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='njnuko',
+      author='njnuko',
+      author_email='njnuko@163.com',
+      url='https://github.com/njnuko/njnuko',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
