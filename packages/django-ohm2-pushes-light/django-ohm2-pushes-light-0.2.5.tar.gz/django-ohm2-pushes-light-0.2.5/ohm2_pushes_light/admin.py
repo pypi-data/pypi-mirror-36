4968f9b2beb1f50f10fb59a611c18d70b2f8c76a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:06 +0000

    Add package django-ohm2-pushes-light/0.2.5/django-ohm2-pushes-light-0.2.5.tar.gz

---
+++ packages/django-ohm2-pushes-light/django-ohm2-pushes-light-0.2.5.tar.gz/django-ohm2-pushes-light-0.2.5/ohm2_pushes_light/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models as ohm2_pushes_light_models
+
+"""
+class ModelAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(ohm2_pushes_light_models.Model, ModelAdmin)
+"""
---
+++ packages/django-ohm2-pushes-light/django-ohm2-pushes-light-0.2.5.tar.gz/django-ohm2-pushes-light-0.2.5/ohm2_pushes_light/gateways/onesignal/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models as onesignal_models
+
+"""
+class ModelAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(onesignal_models.Model, ModelAdmin)
+"""
