44bc87d6ea242878c072d565e5d088ea71229675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:10 +0000

    Add package collabi/0.0.11/collabi-0.0.11-py2-none-any.whl

---
+++ packages/collabi/collabi-0.0.11-py2-none-any.whl/collabi/__init__.py
@@ -0,0 +1,8 @@
+"""UnityCloud Collaboration Command Line Interface
+"""
+
+__author__ = 'Unity'
+__version__ = '0.0.11'
+__license__ = 'BSD'
+
+__all__ = ['CloudConfig', 'Rest', 'Core', 'Auth', 'Collab']
