bed5fecbd740387231b439c73f5d6489eff069d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:57 +0000

    Add package invain/0.4.0/invain-0.4.0.tar.gz

---
+++ packages/invain/invain-0.4.0.tar.gz/invain-0.4.0/README.rst
@@ -0,0 +1,315 @@
+=============
+InVaIN
+=============
+
+|  Python module to get stock data from YAHOO! Finance 
+
+Brief Overview
+--------------
+Real time stock data works either through the **Simple API** or the **Advanced API**
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+|  **Simple API** is only for one ticker. Once created it will get any stock data for that ticker. It returns just the value so no need for parsing data. Simple API allow for ticker to be changed.
+|  **Advanced API** hold a list of tickers that can be added to and modified. Advanced API can use Simple object methods, but also have the ability to add several data fields to gather large data sets quickly. Returns values in a dict object so parsing data is neccesary. 
+
+Historical data works through the **History API**
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+|  **History API** is only for historical data and will **NOT** provide real time data. History API holds a list of tickers that can be added to, a time period to get data for, and the frequency (*daily, weekly, or monthly*) and number of frequency between (*any number >= 1*) of data to return all of which can be modified or retrieved.
+|  When created it will automatically pull data for the tickers passed, using the default values for time period(*30 days from current day to current day*), frequency(*daily*), and number of frequency between (*1*), or the values supplied by you.
+
+Install
+-------
+
+From PyPI with pip:
+
+.. code:: bash
+
+    $ pip install invain
+
+Code Examples
+-------------
+
+Example: Simple API -- Apple Inc. (``AAPL``)
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+.. code:: python
+
+     import invain
+
+    #Create Simple API object
+    api = invain.Simple('HD') 
+    #Get market price
+    print(api.get_price())
+    ###################
+    '173.07''
+
+Example: Advanced API Method 1 -- Apple Inc. (``AAPL``), Alphabet Inc. (``GOOG``), Microsoft Inc. (``MSFT``) 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+.. code:: python 
+
+    import invain
+
+    #Create Advanced API object with all the tickers provided
+    api = invain.Advanced('AAPL','GOOG', 'MSFT')
+
+    #Use Simple API method to get price
+    prices = api.get_price()
+
+    #Returns dict object with stock as the index
+    for stock in prices:
+       print(stock, prices[stock])
+    ###################
+    'AAPL 173.07'
+    'GOOG 1047.41'
+    'MSFT 84.88'
+
+Example: Advanced API Method 2 -- Apple Inc. (``AAPL``), Alphabet Inc. (``GOOG``), Microsoft Inc. (``MSFT``) 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+.. code:: python 
+
+    import invain
+
+    #Create Advanced API object with all the tickers provided
+    api = invain.Advanced('AAPL','GOOG', 'MSFT')
+
+    #Add price field to data 
+    api.add_price()
+
+    #Fetch custom data set
+    prices = api.get_customData()
+
+    #Returns dict object with stock as the index
+    for stock in prices:
+       print(stock, prices[stock])
+    ###################
+    'AAPL {'price': 173.07}'
+    'GOOG {'price': 1047.41}'
+    'MSFT {'price': 84.88}'
+
+Example: History API Method 1 -- Apple Inc. (``AAPL``), Alphabet Inc. (``GOOG``), Microsoft Inc. (``MSFT``) 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+.. code:: python
+
+    import invain
+
+    #Create History API object with all tickers provided. 
+    #This will pull stock data for the past 30 days with an interval of 1 day.
+    #Note that when creating a History object, historical data is automatically pulled. 
+    #If tickers, start or end dates, or frequency are changed, you will need to run api.update_dataset()
+    api = invain.History('AAPL','GOOG', 'MSFT')
+
+    #Get custom data set
+    historical_data = api.get_dataset()
+
+    #Returns dict object with stock as the index
+    for stock in historical_data:
+       print(stock, historical_data[stock])
+    ###################
+    AAPL{
+      'Adj Close': [...],
+      'Close': [...],
+      'Date': [...],
+      'High': [...],
+      'Low': [...],
+      'Open': [...],
+      'Volume': [...]
+    }
+    GOOG{
+      'Adj Close': [...],
+      'Close': [...],
+      'Date': [...],
+      'High': [...],
+      'Low': [...],
+      'Open': [...],
+      'Volume': [...]
+    }
+    MSFT{
+      'Adj Close': [...],
+      'Close': [...],
+      'Date': [...],
+      'High': [...],
+      'Low': [...],
+      'Open': [...],
+      'Volume': [...]
+    }
+
+Available methods
+-----------------
+
+History API
+^^^^^^^^^^^^
+
+- ``add_ticker(ticker)``
+- ``add_tickers(tickers)`` -- takes argument as array (['ticker1', 'ticker2', ...]) or individual parameters (ticker1, ticker2, ...)
+- ``remove_ticker(tickers)``
+- ``get_tickers()``
+- ``get_period()``
+- ``get_startDate()``
+- ``get_endDate()``
+- ``change_period(periods)``-- takes argument as array (['period1', 'period2']) or individual parameters ('period1', 'period2')
+- ``change_startTime(start)``
+- ``change_endTime(end)``
+- ``get_dataset()``
+- ``update_dataset()``
+- ``change_frequency(frequency, num_between=1)`` --  frequency can be daily, weekly, or monthly. num_between is the step between dates (Not required. Default is 1)
+- ``get_date()`` -- if ticker(s) provided only returns data for those ticker(s)
+- ``get_open()`` -- if ticker(s) provided only returns data for those ticker(s)
+- ``get_high()`` -- if ticker(s) provided only returns data for those ticker(s)
+- ``get_low()`` -- if ticker(s) provided only returns data for those ticker(s)
+- ``get_close()`` -- if ticker(s) provided only returns data for those ticker(s)
+- ``get_adj_close()`` -- if ticker(s) provided only returns data for those ticker(s)
+- ``get_volume()`` -- if ticker(s) provided only returns data for those ticker(s)
+- ``get_combo(ticker="ticker",field="high")`` -- Either ticker or field MUST be given (both is valid). If no ticker is provided returns data for all tickers. If no field is provided returns data for all fields.
+
+Simple API
+^^^^^^^^^^
+
+- ``change_ticker(ticker)``
+- ``get_ticker()``
+- ``get_price()``
+- ``get_volume()``
+- ``get_ask()``
+- ``get_askSize()``
+- ``get_averageDailyVolume3Month()``
+- ``get_averageDailyVolume10Day()``
+- ``get_bid()``
+- ``get_bidSize()``
+- ``get_bookValue()``
+- ``get_currency()``
+- ``get_earningsTimestamp()``
+- ``get_earningsTimestampEnd()``
+- ``get_earningsTimestampStart()``
+- ``get_epsForward()``
+- ``get_epsTrailingTwelveMonths()``
+- ``get_fiftyDayAverage()``
+- ``get_fiftyDayAverageChange()``
+- ``get_fiftyDayAverageChangePercent()``
+- ``get_fiftyTwoWeekHigh()``
+- ``get_fiftyTwoWeekHighChange()``
+- ``get_fiftyTwoWeekHighChangePercent()``
+- ``get_fiftyTwoWeekLow()``
+- ``get_fiftyTwoWeekLowChange()``
+- ``get_fiftyTwoWeekLowChangePercent()``
+- ``get_financialCurrency()``
+- ``get_forwardPE()``
+- ``get_fullExchangeName()``
+- ``get_gmtOffSetMilliseconds()``
+- ``get_longName()``
+- ``get_marketCap()``
+- ``get_marketChange()``
+- ``get_marketChangePercent()``
+- ``get_marketDayHigh()``
+- ``get_marketDayLow()``
+- ``get_marketOpen()``
+- ``get_marketPreviousClose()``
+- ``get_marketTime()``
+- ``get_postMarketChange()``
+- ``get_postMarketChangePercent()``
+- ``get_postMarketPrice()``
+- ``get_postMarketTime()``
+- ``get_priceHint()``
+- ``get_priceToBook()``
+- ``get_sharesOutstanding()``
+- ``get_shortName()``
+- ``get_symbol()`` -- Returns ticker for stock data being fetched
+- ``get_tradeable()``
+- ``get_trailingPE()``
+- ``get_twoHundredDayAverage()``
+- ``get_twoHundredDayAverageChange()``
+- ``get_twoHundredDayAverageChangePercent()``
+
+Advanced API
+^^^^^^^^^^^^
+
+- ``get_customData()``
+- ``add_ticker(ticker)``
+- ``add_tickers(tickers)`` -- takes list of tickers as argument (or add_tickers(ticker1,ticker2,...))
+- ``get_tickers(tickers)``
+- ``remove_tickers(tickers)``
+- ``add_price()``
+- ``add_volume()``
+- ``add_ask()``
+- ``add_askSize()``
+- ``add_averageDailyVolume3Month()``
+- ``add_averageDailyVolume10Day()``
+- ``add_bid()``
+- ``add_bidSize()``
+- ``add_bookValue()``
+- ``add_currency()``
+- ``add_earningsTimestamp()``
+- ``add_earningsTimestampEnd()``
+- ``add_earningsTimestampStart()``
+- ``add_epsForward()``
+- ``add_epsTrailingTwelveMonths()``
+- ``add_fiftyDayAverage()``
+- ``add_fiftyDayAverageChange()``
+- ``add_fiftyDayAverageChangePercent()``
+- ``add_fiftyTwoWeekHigh()``
+- ``add_fiftyTwoWeekHighChange()``
+- ``add_fiftyTwoWeekHighChangePercent()``
+- ``add_fiftyTwoWeekLow()``
+- ``add_fiftyTwoWeekLowChange()``
+- ``add_fiftyTwoWeekLowChangePercent()``
+- ``add_financialCurrency()``
+- ``add_forwardPE()``
+- ``add_fullExchangeName()``
+- ``add_gmtOffSetMilliseconds()``
+- ``add_longName()``
+- ``add_marketCap()``
+- ``add_marketChange()``
+- ``add_marketChangePercent()``
+- ``add_marketDayHigh()``
+- ``add_marketDayLow()``
+- ``add_marketOpen()``
+- ``add_marketPreviousClose()``
+- ``add_marketTime()``
+- ``add_postMarketChange()``
+- ``add_postMarketChangePercent()``
+- ``add_postMarketPrice()``
+- ``add_postMarketTime()``
+- ``add_priceHint()``
+- ``add_priceToBook()``
+- ``add_sharesOutstanding()``
+- ``add_shortName()``
+- ``add_symbol()``
+- ``add_tradeable()``
+- ``add_trailingPE()``
+- ``add_twoHundredDayAverage()``
+- ``add_twoHundredDayAverageChange()``
+- ``add_twoHundredDayAverageChangePercent()``
+- ``Remove for Above Functions``
+
+TODO/Future Updates
+-------------------
+- ``Historical Data - In Progress (install from clone to use pre-alpha version)``
+- ``Better Error Handling - After Historical Data is in package release``
+- ``Add Documentation - Will attempt to do this periodically untill full docs are complete. Any assistance on this would be great :)``
+
+Requirements
+------------
+requests
+
+Feedback, Issues, and Features:
+-------------------------------
+Feedback
+^^^^^^^^
+|  I'd love to get some feedback from users. I want to know how you are using InVaIN so I can focus on updating it in ways that improve your experience. If you'd like to do so you can email me at *invainapi@gmail.com* **(NOTE: Email abuse will result in your email address being BLOCKED AND/OR REMOVAL OF YOUR ACCESS to InVaIN's data retrieval services)**
+|  In that vein, I just wanted to outline some guidelines for submitting issues on github.
+
+Bugs
+^^^^
+If you experience any bugs when running the package please submit an issue with a description of the issue. Bugs will take priority over all other issues. 
+
+Bad Data
+^^^^^^^^
+If you experience any problems with returned data please create an issue and include:
+- ``Code related to creating, modifiying, and accessing the InVaIN API object``
+- ``Data Returned``
+- ``Day and Approximate Time of Access``
+
+This will allow me to better identify what is causing the issue. If you'd rather not post this information on github, please email it to the email listed in the feedback section.
+
+New Features
+^^^^^^^^^^^^
+Please don't hesitate to ask for new features you'd like to see or make suggestions for improvements. You can open an issue here on github and I'll take a look at it as soon as I can.
---
+++ packages/invain/invain-0.4.0.tar.gz/invain-0.4.0/invain/History.py
@@ -0,0 +1,228 @@
+import requests
+import datetime
+import time
+import sys
+from .Utils import *
+
+class History():
+    
+    def __init__(self, ticker, *extras, **kwargs):
+        if kwargs:
+            #Check if start, end, or frequency are provided, and set accordingly
+            start = kwargs['start'] if 'start' in kwargs else None
+            end = kwargs['end'] if 'end' in kwargs else None
+            frequency = kwargs['frequency'] if 'frequency' in kwargs else 'daily'
+            num_between = kwargs['num_between'] if 'num_between' in kwargs else 1
+        else:
+            start = None
+            end = None
+            frequency = 'daily'
+            num_between = 1
+
+        now = int(time.mktime(datetime.datetime.now().timetuple()))
+
+        #Check if start exists - if not default start time is 30 days from now, if start exists set start to strat.
+        period1 = now - 30*24*60*60 if start is None else int(time.mktime(start.timetuple()))
+        period2 =  now if end is None else int(time.mktime(end.timetuple()))
+
+        if period1 > period2:
+            print("Start is greater than end, cannot initialize History() object \nQuitting")
+            sys.exit(1)
+        
+        #Check if ticker var is a list or string then set self.ticker to a list of ticker(s)
+        self.ticker = [symbol.upper() for symbol in ticker] if is_sequence(ticker) else [ticker.upper()]
+
+        #Add extra tickers if object created with parameters in the form of ('TIC','KER','XTRA')
+        if extras: self.add_tickers([symbol.upper() for symbol in extras])
+        
+        #Set period to a list of unix timestamps, with the first item being the later time.
+        self.period = [period1, period2]
+
+        self.interval =  ""
+        self.change_frequency(frequency,num_between)
+        
+        #Grab Historical Data
+        self.hist_data = hist(self.ticker, self.period, self.interval)
+
+    #Add Tickers to End of List, Extend Allows for ticker to be a List                
+    def add_ticker(self, ticker):
+        self.ticker.extend([symbol.upper() for symbol in ticker]) if is_sequence(ticker) else self.ticker.extend([ticker.upper()])   
+                
+    #If Someone is Sketch About Singular Form
+    def add_tickers(self, tickers, *extras):
+        self.add_ticker(tickers)
+        self.add_ticker(extras)
+
+    def remove_ticker(self, ticker):
+        try:
+            if ticker in self.ticker: self.ticker.remove(ticker.upper())
+            else: raise ValueError('InVaIN.Advanced() Failed to remove ticker. Ticker not found in list')
+        except ValueError as err:
+            print(err.args,"\n Non-Fatal Error")
+            
+    def get_tickers(self):
+        return self.ticker
+
+    #Returns list of datetimes instead of unix timestamps
+    def get_period(self):
+        return [datetime.datetime.fromtimestamp(timestamp) for timestamp in self.period]
+    
+    def get_startDate(self):
+        return datetime.datetime.fromtimestamp(self.period[0])
+
+    def get_endDate(self):
+        return datetime.datetime.fromtimestamp(self.period[1])
+
+    #Parameter set to *periods to allow user to provide period in an array (['period1', 'period2']) or individual parameters ('period1', 'period2')
+    def change_period(self, *periods):
+        #if more than two or no parameters provided there is a problem
+        if len(periods) > 2 or not periods :
+            print("Error History.change_period(): change_period requires either 1 list of 2 datetime objects, or 2 datetime objects as parameters. Period not changed")
+            return
+        
+        #if not is_sequence(periods[0]):
+        if len(periods) == 2:
+            for period in periods:
+                #If either item is not a datetime object, there is a problem
+                if not isinstance(period, datetime.datetime):
+                    print("Error History.change_period(): change_period requires either 1 list of 2 datetime objects, or 2 datetime objects as parameters. Period not changed")
+                    return
+                
+            start = int(time.mktime(periods[0].timetuple()))
+            end = int(time.mktime(periods[1].timetuple()))
+            
+            #Start time cannot be greater than end time
+            if start > end:
+                print("Error History.change_period(): Start time cannot be newer than end time. Please make sure you are providing the parameters in the correct order: (start, end) \n Period not changed")
+                return
+            
+            self.period[0] = start
+            self.period[1] = end
+            return
+        #If two lists are provided, there is a problem        
+        #if is_sequence(periods[1]):
+        #    print("Error History.change_period(): change_period requires either 1 list of 2 datetime objects, or 2 datetime objects as parameters. Period not changed")
+         #   return
+        
+        #our last option is that the period is a list of datetimes    
+        for period in periods[0]:
+            #If either item is not a datetime object, there is a problem
+            if not isinstance(period, datetime.datetime):
+                print("Error History.change_period(): change_period requires either 1 list of 2 datetime objects, or 2 datetime objects as parameters. Period not changed")
+                return
+        start = int(time.mktime(periods[0][0].timetuple()))
+        end = int(time.mktime(periods[0][1].timetuple()))
+        if start > end:
+            print("Error History.change_period(): Start time cannot be newer than end time. Please make sure datetimes in list are in the correct order: [start, end] \n Period not changed")
+            return
+        
+        self.period[0] = start
+        self.period[1] = end
+
+    def change_startTime(self, start):
+        if not isinstance(start, datetime.datetime):
+            print("Error History.change_startTime(): change_startTime requires a single datetime object as parameters. Start not changed")
+            return
+        start = int(time.mktime(start.timetuple()))
+        self.period[0] = start
+        
+    def change_endTime(self, end):
+        if not isinstance(end, datetime.datetime):
+            print("Error History.change_endTime(): change_endTime requires a single datetime object as parameters. End not changed")
+            return
+        end = int(time.mktime(end.timetuple()))
+        self.period[1] = end
+    
+    def get_dataset(self):
+        return self.hist_data
+
+    def update_dataset(self):
+        if self.period[0]>self.period[1]:
+            print("Error History.update_dataset(): Start time cannot be newer than end time. Please update period \n Data not updated")
+            return
+        
+        self.hist_data = hist(self.ticker, self.period, self.interval)
+        return self.hist_data
+
+    def change_frequency(self, frequency, num_between=1):
+        compare = frequency.strip().lower()
+        if isinstance(num_between, int):
+            n = str(num_between)
+        else:
+            print("Error Changing Frequency: num_between is not of type int")
+            return False
+        
+        self.interval = n +"d" if compare == 'daily' else n + "wk" if compare == 'weekly' else n + "mo" if compare == 'monthly' else print("Error Changing Frequency: Not a Valid Frequency"); return False
+        return True
+
+    def get_date(self, ticker=None):
+        #Check if ticker is provided; if ticker is provided and is a list of tickers, return data for those tickers, otherwise return data for that ticker, or if no ticker is provided return data for all tickers 
+        return {key : self.hist_data[key]['Date'] for key in self.hist_data} if ticker is None else {key : self.hist_data[key]['Date'] for key in ticker} if is_sequence(ticker) else {ticker : self.hist_data[ticker]['Date']}
+
+    def get_open(self, ticker=None):
+        #Check if ticker is provided; if ticker is provided and is a list of tickers, return data for those tickers, otherwise return data for that ticker, or if no ticker is provided return data for all tickers 
+        return {key : self.hist_data[key]['Open'] for key in self.hist_data} if ticker is None else {key : self.hist_data[key]['Open'] for key in ticker} if is_sequence(ticker) else {ticker : self.hist_data[ticker]['Open']}
+
+    def get_high(self, ticker=None):
+        #Check if ticker is provided; if ticker is provided and is a list of tickers, return data for those tickers, otherwise return data for that ticker, or if no ticker is provided return data for all tickers 
+        return {key : self.hist_data[key]['High'] for key in self.hist_data} if ticker is None else {key : self.hist_data[key]['High'] for key in ticker} if is_sequence(ticker) else {ticker : self.hist_data[ticker]['High']}
+
+    def get_low(self, ticker=None):
+        #Check if ticker is provided; if ticker is provided and is a list of tickers, return data for those tickers, otherwise return data for that ticker, or if no ticker is provided return data for all tickers 
+        return {key : self.hist_data[key]['Low'] for key in self.hist_data} if ticker is None else {key : self.hist_data[key]['Low'] for key in ticker} if is_sequence(ticker) else {ticker : self.hist_data[ticker]['Low']}
+
+    def get_close(self, ticker=None):
+        #Check if ticker is provided; if ticker is provided and is a list of tickers, return data for those tickers, otherwise return data for that ticker, or if no ticker is provided return data for all tickers 
+        return {key : self.hist_data[key]['Close'] for key in self.hist_data} if ticker is None else {key : self.hist_data[key]['Close'] for key in ticker} if is_sequence(ticker) else {ticker : self.hist_data[ticker]['Close']}
+
+    def get_adj_close(self, ticker=None):
+        #Check if ticker is provided; if ticker is provided and is a list of tickers, return data for those tickers, otherwise return data for that ticker, or if no ticker is provided return data for all tickers 
+        return {key : self.hist_data[key]['Adj Close'] for key in self.hist_data} if ticker is None else {key : self.hist_data[key]['Adj Close'] for key in ticker} if is_sequence(ticker) else {ticker : self.hist_data[ticker]['Adj Close']}
+
+    def get_volume(self, ticker=None):
+        #Check if ticker is provided; if ticker is provided and is a list of tickers, return data for those tickers, otherwise return data for that ticker, or if no ticker is provided return data for all tickers 
+        return {key : self.hist_data[key]['Volume'] for key in self.hist_data} if ticker is None else {key : self.hist_data[key]['Volume'] for key in ticker} if is_sequence(ticker) else {ticker : self.hist_data[ticker]['Volume']}
+
+    def get_combo(self,**kwargs):
+        #If user does not provide either ticker or field, then we return an error
+        if not kwargs : return "Error History.get_combo(): No ticker or data field provided. Cannot combine"
+
+        ticker = kwargs['tickers'] if 'tickers' in kwargs else None
+        field = kwargs['fields'] if 'fields' in kwargs else None
+        #Check if ticker is provided; if ticker is provided and is a list of tickers, return data for those tickers, otherwise return data for that ticker, or if no ticker is provided return data for all tickers 
+        tickers = [key for key in self.hist_data] if ticker is None else [key.upper() for key in ticker] if is_sequence(ticker) else [ticker.upper()]
+        #Check if field is provided; if field is provided and is a list of field, return data for those field, otherwise return data for that field, or if no field is provided return data for all fields 
+        fields = [header for header in self.hist_data[self.ticker[0]]] if field is None else [header.title() for header in field] if is_sequence(field) else [field.title()]
+
+        data = {key : {header : self.hist_data[key][header] for header in fields} for key in tickers}
+        return data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
