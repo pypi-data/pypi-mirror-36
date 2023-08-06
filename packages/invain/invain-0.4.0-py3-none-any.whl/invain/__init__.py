db9faab1dda3fc2ef113e16e5da7f89ae692f433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:57 +0000

    Add package invain/0.4.0/invain-0.4.0-py3-none-any.whl

---
+++ packages/invain/invain-0.4.0-py3-none-any.whl/invain/__init__.py
@@ -0,0 +1,8 @@
+# deprecated to keep older scripts who import this from breaking
+from .Simple import Simple
+from .Advanced import Advanced
+from .History import History
+
+
+__all__ = ['Simple', 'Advanced', 'History', 'Utils', '__version__']
+
