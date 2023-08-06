7dc0cd1a21d6b383142f97f240b2d11b25c8e670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:02 +0000

    Add package gym-vec-env/0.0.2/gym-vec-env-0.0.2.tar.gz

---
+++ packages/gym-vec-env/gym-vec-env-0.0.2.tar.gz/gym-vec-env-0.0.2/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+  name="gym-vec-env",
+  version="0.0.2",
+  author="Tom Brown",
+  author_email="nottombrown@gmail.com",
+  url="https://github.com/nottombrown",
+  packages=setuptools.find_packages(),
+  install_requires=[
+    'gym'
+  ],
+)
