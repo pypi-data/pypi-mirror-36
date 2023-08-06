ebf1e31e2c4fd2077d9c1b6b377f1da1a62df74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:09 +0000

    Add package numpy-posit/1.15.2.0.0.1.dev1/numpy_posit-1.15.2.0.0.1.dev1-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/numpy-posit/numpy_posit-1.15.2.0.0.1.dev1-cp37-cp37m-manylinux1_x86_64.whl/numpy/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+#
+# To compare versions robustly, use `numpy.lib.NumpyVersion`
+short_version = '1.15.2.0.0.1.dev1'
+version = '1.15.2.0.0.1.dev1'
+full_version = '1.15.2.0.0.1.dev1'
+git_revision = '09675389e9de7003a74c332b84e6a94146bf5ddf'
+release = True
+
+if not release:
+    version = full_version
