289627a094a0f0577001482499aa636accbf41a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:42 +0000

    Add package dsopz/2.0.beta1/dsopz-2.0.beta1.tar.gz

---
+++ packages/dsopz/dsopz-2.0.beta1.tar.gz/dsopz-2.0.beta1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+version = '2.0.beta1'
+
+setup(
+  name = 'dsopz',
+  packages = ['dsopz','dsopz.cmd'],
+  version = version,
+  description = 'Google Datastore Operations',
+  entry_points = {
+      "console_scripts": [
+          "dsopz = dsopz"
+      ]
+  },
+  author = 'Paulo Henrique Murer',
+  author_email = 'paulo.murer@gmail.com',
+  url = 'https://github.com/murer/dsopz',
+  download_url = 'https://github.com/murer/dsopz/tarball/dsopz-%s' % (version),
+  keywords = ['gae', 'datastore', 'appengine'],
+  classifiers = [],
+)
