06c320666ec9dc83f06fa798f931dc732e187791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:03 +0000

    Add package HinetPy/0.4.7/HinetPy-0.4.7-py3-none-any.whl

---
+++ packages/HinetPy/HinetPy-0.4.7-py3-none-any.whl/HinetPy/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+HinetPy
+=======
+
+HinetPy is a Hi-net client, written in Python, for seismologists, to request
+and process seismic waveform data from Hi-net website.
+
+Basis usage:
+
+>>> from HinetPy import Client, win32
+>>> from datetime import datetime
+>>> client = Client("username", "password")
+>>> starttime = datetime(2010, 1, 1, 0, 0)
+>>> data, ctable = client.get_waveform('0101', starttime, 20)
+>>> win32.extract_sac(data, ctable)
+>>> win32.extract_pz(ctable)
+"""
+
+__title__ = 'HinetPy'
+__version__ = '0.4.7'
+__author__ = 'Dongdong Tian'
+__license__ = 'MIT'
+
+from .client import Client
+from .header import NETWORK
+
+__all__ = ["Client", "NETWORK", "win32"]
