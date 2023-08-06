0cc90d5d23f36847dd422ae52efadce906f176a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:18 +0000

    Add package wr-profiles/4.1.0/wr-profiles-4.1.0.tar.gz

---
+++ packages/wr-profiles/wr-profiles-4.1.0.tar.gz/wr-profiles-4.1.0/wr_profiles/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "4.1.0"
+
+from .envvar_profile import EnvvarProfile, EnvvarProfileProperty, envvar_profile, envvar_profile_cls
+
+__all__ = [
+    "EnvvarProfile",
+    "EnvvarProfileProperty",
+    "envvar_profile",
+    "envvar_profile_cls",
+]
