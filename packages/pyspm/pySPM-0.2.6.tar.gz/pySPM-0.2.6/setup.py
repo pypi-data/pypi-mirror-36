3fa04d260338114a437d3d2ce67166f7f4a560ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:28 +0000

    Add package pyspm/0.2.6/pySPM-0.2.6.tar.gz

---
+++ packages/pyspm/pySPM-0.2.6.tar.gz/pySPM-0.2.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pySPM",
+    version='0.2.6',
+    description="library to handle SPM and ToF-SIMS data",
+    long_description="library to handle SPM and ToF-SIMS data",
+    url="https://github.com/scholi/pySPM",
+    author = "Olivier Scholder",
+    author_email = "o.scholder@gmail.com",
+    license="Apache 2.0",
+    keywords='tof sims iontof spm sfm sxm afm kpfm pca imaging ita itm bruker nanonis',
+    packages=find_packages(exclude=['contrib','docs','tests']),
+    package_data={'pySPM':['data/elements.db','data/test.sxm']},
+    include_package_data=True,
+    install_requires=['numpy','scipy','pandas','scikit-image','scikit-learn','matplotlib'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
