20e5dda660f7266d16e7fcf698c49a19a206953c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:17 +0000

    Add package balebot/1.2.7/balebot-1.2.7.tar.gz

---
+++ packages/balebot/balebot-1.2.7.tar.gz/balebot-1.2.7/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'CHANGELOG.md')) as f:
+    long_description = f.read()
+setuptools.setup(name='balebot',
+                 version='1.2.7',
+                 description='python framework for Bale messenger Bot API',
+                 author='bale',
+                 author_email='balebot@elenoon.ir',
+                 license='GNU',
+                 long_description=long_description,
+                 long_description_content_type='text/markdown',
+                 url='https://github.com/balemessenger/bale-bot-python',
+                 install_requires=[
+                     'aiohttp==2.3.7',
+                     'asyncio==3.4.3',
+                     'graypy==0.2.14',
+                     'backoff==1.6.0'
+                 ],
+                 packages=setuptools.find_packages())
