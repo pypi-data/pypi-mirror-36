edf758b721de759b417a71a8ff39b838e31e5c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:00 +0000

    Add package ybc-coordinate/1.0.2/ybc_coordinate-1.0.2.tar.gz

---
+++ packages/ybc-coordinate/ybc_coordinate-1.0.2.tar.gz/ybc_coordinate-1.0.2/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ybc_coordinate',
+      version='1.0.2',
+      description='City coordinate.',
+      long_description='City coordinate.',
+      author='hurs',
+      author_email='hurs@fenbi.com',
+      keywords=['python', 'city', 'coordinate'],
+      url='http://pip.zhenguanyu.com/',
+      packages=['ybc_coordinate'],
+      package_data={'ybc_coordinate': ['*.py']},
+      license='MIT',
+     )
