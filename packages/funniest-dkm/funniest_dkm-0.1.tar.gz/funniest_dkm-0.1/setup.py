95f480c9aeed834b2720e0761733dddf0be5c37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:43 +0000

    Add package funniest-dkm/0.1/funniest_dkm-0.1.tar.gz

---
+++ packages/funniest-dkm/funniest_dkm-0.1.tar.gz/funniest_dkm-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest_dkm',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/pascalmin/funniest',
+      author='Flying Circus',
+      author_email='pascalmin@gmail.com',
+      license='MIT',
+      packages=['funniest_dkm'],
+      zip_safe=False)
