70f5fc8de811120cfb96f0d2e5f1ee38fc9cdd9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:46 +0000

    Add package liangzhi/0.1/liangzhi-0.1-py3-none-any.whl

---
+++ packages/liangzhi/liangzhi-0.1-py3-none-any.whl/liangzhi/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+def joke():
+    return (u'How do you tell HTML from HTML5?'
+            u'Try it out in Internet Explorer.'
+            u'Does it work?'
+            u'No?'
+            u'It\'s HTML5.')
