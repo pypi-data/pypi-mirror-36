4bfdd93460469f9bfec8aaea65fd65d00bea0091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:20 +0000

    Add package infobot/0.1.1/infobot-0.1.1-py2.py3-none-any.whl

---
+++ packages/infobot/infobot-0.1.1-py2.py3-none-any.whl/infobot/social/fake.py
@@ -0,0 +1,45 @@
+import infobot.konstants as K
+from infobot.social.template import SocialPlugin
+
+
+class FakeSocialPluginConf():
+    def __init__(self, filedata):
+        "Handles the configuation data for social plugin"
+        self.userid = filedata[K.useridKey]
+        self.password = filedata[K.passwdKey]
+
+
+class FakeSocialPlugin(SocialPlugin):
+    def __init__(self, config, socialplugindetails, storageadmin):
+        """
+        This is a reference class for a social plugin
+        It does not post to any social network
+        It simply outputs to the stdout as if it is posting
+        """
+        super().__init__(config, K.fakeKey)
+        self._details = FakeSocialPluginConf(socialplugindetails)
+        self._userid = self._details.userid
+        self._password = self._details.password
+        self.storageAdmin = storageadmin
+
+    def login(self):
+        print("Logging into fake with {}/{}".
+              format(self._userid, self._password))
+        return True
+
+    def logout(self):
+        print("Logging out from fake.")
+        return True
+
+    def post(self, topic, num, data):
+        hdr = self.storageAdmin.get_header(self.socialName, topic, num)
+        ftr = self.storageAdmin.get_footer(self.socialName, topic, num)
+        postdata = "{}\n{}\n{}".format(hdr, data, ftr)
+        print(postdata)
+        return postdata
+
+    def list_followers(self):
+        return["a", "b", "c"]
+
+    def follow(self, other):
+        print("following @"+other)
