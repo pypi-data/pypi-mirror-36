3f5b8027add6d95ab56f462722a8016e89ca83aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:36 +0000

    Add package icalevents/0.1.11/icalevents-0.1.11.tar.gz

---
+++ packages/icalevents/icalevents-0.1.11.tar.gz/icalevents-0.1.11/test/test_icalevents.py
@@ -0,0 +1,113 @@
+import unittest
+from icalevents import icalevents
+from datetime import date, timedelta
+from time import sleep
+
+
+class ICalEventsTests(unittest.TestCase):
+    def test_events_url(self):
+        url = "https://raw.githubusercontent.com/irgangla/icalevents/master/test/test_data/basic.ics"
+        start = date(2017, 5, 18)
+        end = date(2017, 5, 19)
+
+        evs = icalevents.events(url=url, file=None, start=start, end=end)
+
+        self.assertEqual(len(evs), 2, "two events are found")
+
+    def test_events_start(self):
+        ical = "test/test_data/basic.ics"
+        start = date(2017, 5, 16)
+
+        evs = icalevents.events(url=None, file=ical, start=start)
+
+        self.assertEqual(len(evs), 3, "three events are found")
+
+    def test_events(self):
+        ical = "test/test_data/basic.ics"
+        start = date(2017, 5, 18)
+        end = date(2017, 5, 19)
+
+        evs = icalevents.events(url=None, file=ical, start=start, end=end)
+
+        self.assertEqual(len(evs), 2, "two events are found")
+        
+    def test_events_recurring(self):
+        ical = "test/test_data/recurring.ics"
+        start = date(2018, 10, 15)
+        end = date(2018, 11, 15)
+        
+        evs = icalevents.events(file=ical, start=start, end=end)
+        
+        e1 = evs[1]
+        self.assertEqual(e1.start.hour, 10, "check time with DST")
+        self.assertEqual(e1.start.tzinfo.utcoffset(e1.start), timedelta(seconds=7200), "check UTC offset with DST")
+        
+        e2 = evs[2]
+        self.assertEqual(e2.start.hour, 10, "check time without DST")
+        self.assertEqual(e2.start.tzinfo.utcoffset(e2.start), timedelta(seconds=3600), "check UTC offset without DST")
+        
+        self.assertEqual(e2.start.day, 5, "Check observance of exdate.")
+
+    def test_event_attributes(self):
+        ical = "test/test_data/basic.ics"
+        start = date(2017, 7, 12)
+        end = date(2017, 7, 13)
+
+        ev = icalevents.events(url=None, file=ical, start=start, end=end)[0]
+
+        self.assertEqual(ev.summary, "graue Restmülltonne")
+        self.assertEqual(ev.description, "graue Restmülltonne nicht vergessen!")
+        self.assertTrue(ev.all_day)
+
+    def test_events_async_url(self):
+        url = "https://raw.githubusercontent.com/irgangla/icalevents/master/test/test_data/basic.ics"
+        start = date(2017, 5, 18)
+        end = date(2017, 5, 19)
+        key = "basic"
+
+        icalevents.events_async(key, url=url, file=None, start=start, end=end)
+
+        sleep(4)
+
+        self.assertTrue(icalevents.all_done(key), "request is finished")
+        self.assertEqual(len(icalevents.latest_events(key)), 2, "two events are found")
+
+    def test_events_async(self):
+        ical = "test/test_data/basic.ics"
+        start = date(2017, 5, 18)
+        end = date(2017, 5, 19)
+        key = "basic"
+
+        icalevents.events_async(key, url=None, file=ical, start=start, end=end)
+
+        sleep(4)
+
+        self.assertTrue(icalevents.all_done(key), "request is finished")
+        self.assertEqual(len(icalevents.latest_events(key)), 2, "two events are found")
+
+    def test_request_data(self):
+        ical = "test/test_data/basic.ics"
+        start = date(2017, 5, 18)
+        end = date(2017, 5, 19)
+        key = "basic"
+
+        icalevents.request_data(key, url=None, file=ical, string_content=None, start=start, end=end, fix_apple=False)
+
+        self.assertTrue(icalevents.all_done(key), "request is finished")
+        self.assertEqual(len(icalevents.latest_events(key)), 2, "two events are found")
+
+    def test_string_data(self):
+        ical = "test/test_data/basic.ics"
+
+        with open(ical, mode='rb') as f:
+            string_content = f.read()
+
+        start = date(2017, 5, 18)
+        end = date(2017, 5, 19)
+        key = "basic"
+
+        icalevents.request_data(key, url=None, file=None, string_content=string_content, start=start, end=end,
+                                fix_apple=False)
+
+        self.assertTrue(icalevents.all_done(key), "request is finished")
+        self.assertEqual(len(icalevents.latest_events(key)), 2, "two events are found")
