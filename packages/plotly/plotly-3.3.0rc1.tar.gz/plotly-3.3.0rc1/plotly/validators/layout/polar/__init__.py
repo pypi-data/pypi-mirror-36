752d3f68a1b269999ab4a4d77d01a65fc8dd8c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:25 +0000

    Add package plotly/3.3.0rc1/plotly-3.3.0rc1.tar.gz

---
+++ packages/plotly/plotly-3.3.0rc1.tar.gz/plotly-3.3.0rc1/plotly/validators/layout/polar/__init__.py
@@ -0,0 +1,7 @@
+from ._sector import SectorValidator
+from ._radialaxis import RadialAxisValidator
+from ._hole import HoleValidator
+from ._gridshape import GridshapeValidator
+from ._domain import DomainValidator
+from ._bgcolor import BgcolorValidator
+from ._angularaxis import AngularAxisValidator
