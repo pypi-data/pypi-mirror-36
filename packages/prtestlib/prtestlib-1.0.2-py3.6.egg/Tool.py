04c545099b05956d94e35e797283cf8b8c6854b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:49 +0000

    Add package prtestlib/1.0.2/prtestlib-1.0.2-py3.6.egg

---
+++ packages/prtestlib/prtestlib-1.0.2-py3.6.egg/Tool.py
@@ -0,0 +1,14 @@
+# from sztestlib import szlibone
+#
+# szlibone.run()
+
+
+# # 直接导入一个包，需要在__init__里面做些手脚，在__init__里面 from . import szlibone(包内引用：相对引用)
+# import sztestlib
+#
+# # 注意：这里要加 sztestlib
+# sztestlib.szlibone.run()
+
+
+def tt():
+    print("tttt")
