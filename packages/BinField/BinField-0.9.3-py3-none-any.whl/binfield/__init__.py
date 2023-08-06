aa9e7c8422f2a4391faa2e714ab8f13a09fc7ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:39 +0000

    Add package BinField/0.9.3/BinField-0.9.3-py3-none-any.whl

---
+++ packages/BinField/BinField-0.9.3-py3-none-any.whl/binfield/__init__.py
@@ -0,0 +1,25 @@
+#    Copyright 2016 - 2017 Alexey Stepanov aka penguinolog
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Implement BinField in python."""
+
+from __future__ import absolute_import
+
+from .binfield import BinField
+
+__version__ = "0.9.3"
+__author__ = "Alexey Stepanov"
+__author_email__ = "penguinolog@gmail.com"
+__url__ = "https://github.com/penguinolog/binfield"
+__description__ = "Python BinField implementation for binary data manipulation"
+__license__ = "Apache License, Version 2.0"
