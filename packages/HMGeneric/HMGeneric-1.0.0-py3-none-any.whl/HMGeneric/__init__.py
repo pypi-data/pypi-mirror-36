2c4c530ae9f0a1573859e5e9a5996367d94d96e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:20 +0000

    Add package HMGeneric/1.0.0/HMGeneric-1.0.0-py3-none-any.whl

---
+++ packages/HMGeneric/HMGeneric-1.0.0-py3-none-any.whl/HMGeneric/__init__.py
@@ -0,0 +1,7 @@
+from HMGenericPackage.array import *
+from HMGenericPackage.ethernet import *
+from HMGenericPackage.menu import *
+from HMGenericPackage.parse import *
+from HMGenericPackage.service import *
+from HMGenericPackage.terminal import *
+from HMGenericPackage.ethernet import *
