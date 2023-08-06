a1175a2d413c93e899029f6f0be72eb4a8b50b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:45 +0000

    Add package decrunch/0.3.1/decrunch-0.3.1.tar.gz

---
+++ packages/decrunch/decrunch-0.3.1.tar.gz/decrunch-0.3.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, Extension
+
+try:
+	from Cython.Build import cythonize
+except ImportError:
+	cythonize = None
+
+
+extensions = [
+	Extension(
+		"decrunch",
+		[
+			"crn_decomp.cpp",
+			"decrunch." + ("pyx" if cythonize else "cpp"),
+		],
+		language="c++", include_dirs=["crunch"],
+	)
+]
+if cythonize:
+	extensions = cythonize(extensions)
+
+
+setup(ext_modules=extensions)
