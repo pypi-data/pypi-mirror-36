622a5e60f23da589a9347bb91887711b9d0e9dcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:31 +0000

    Add package agda-pkg/0.1.20/agda-pkg-0.1.20.tar.gz

---
+++ packages/agda-pkg/agda-pkg-0.1.20.tar.gz/agda-pkg-0.1.20/src/commands/freeze.py
@@ -0,0 +1,40 @@
+'''
+  apkg
+  ~~~~
+
+  The Agda Package Manager.
+
+'''
+
+# ----------------------------------------------------------------------------
+
+import click
+import logging
+import click_log
+
+from pony.orm             import db_session, select
+
+from ..service.database   import db
+from ..service.database   import ( Library , LibraryVersion )
+
+
+# ----------------------------------------------------------------------------
+
+# -- Logger def.
+logger = logging.getLogger(__name__)
+click_log.basic_config(logger)
+
+# -- Command def.
+@click.group()
+def freeze(): pass
+
+@freeze.command()
+@click_log.simple_verbosity_option(logger)
+@db_session
+def freeze():
+  """List of installed packages."""
+
+  for library in select(l for l in Library if l.installed):
+    installedVersion = library.getInstalledVersion()    
+    if installedVersion is not None:
+      click.echo(installedVersion.freezeName)
