31d0cc69e8b6a620b7d746eafe0d26098ef4fb7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:38 +0000

    Add package tympeg/0.2.9/tympeg-0.2.9.tar.gz

---
+++ packages/tympeg/tympeg-0.2.9.tar.gz/tympeg-0.2.9/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+setup(
+    name="tympeg",
+    version="0.2.9",
+    packages=find_packages(),
+    scripts=[],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=['docutils>=0.3'],
+
+    # package_data={
+    #     # If any package contains *.txt or *.rst files, include them:
+    #     '': ['*.txt', '*.rst'],
+    #     # And include any *.msg files found in the 'hello' package, too:
+    #     'hello': ['*.msg'],
+    # },
+
+    # metadata for upload to PyPI
+    author="TaiSheng Yeager",
+    author_email="taishengyeager@gmail.com",
+    description="Yet another scripting solution for ffmpeg operations: Converting, concatanating, saving HLS streams, meta-data analysis",
+    license="MIT",
+    keywords="ffmpeg video audio subtitle HLS stream",
+    url="https://github.com/taishengy/tympeg",   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+)
