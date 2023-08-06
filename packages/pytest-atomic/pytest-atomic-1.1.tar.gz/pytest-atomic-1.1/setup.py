b311faad921a2ff9acc1e9e40ef83aa53130a58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:54 +0000

    Add package pytest-atomic/1.1/pytest-atomic-1.1.tar.gz

---
+++ packages/pytest-atomic/pytest-atomic-1.1.tar.gz/pytest-atomic-1.1/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-atomic',
+    version='1.1',
+    author='megachweng',
+    author_email='megachweng@gmail.com',
+    maintainer='megachweng',
+    maintainer_email='megachweng@gmail.com',
+    license='MIT',
+    url='https://github.com/megachweng/pytest-atomic',
+    description='atomic pytest suit or even electronic',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    py_modules=['pytest_atomic'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    install_requires=['pytest>=3.5.0'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+    entry_points={
+        'pytest11': [
+            'atomic = pytest_atomic',
+        ],
+    },
+)
