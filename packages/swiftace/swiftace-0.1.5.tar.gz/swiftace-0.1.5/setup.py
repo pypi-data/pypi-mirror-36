e895adb8659f79ede512cf365f0630a3501c87e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:27 +0000

    Add package swiftace/0.1.5/swiftace-0.1.5.tar.gz

---
+++ packages/swiftace/swiftace-0.1.5.tar.gz/swiftace-0.1.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="swiftace",
+    version="0.1.5",
+    author="Aakash N S",
+    author_email="opensource@swiftace.ai",
+    entry_points={
+        'console_scripts': ['swiftace=swiftace.cli:main'],
+    },
+    description="SwiftAce Python SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://swiftace.ai/",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
