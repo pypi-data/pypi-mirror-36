6bd8714ac38a89ee6780401a2c261f88a0d188ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:32 +0000

    Add package arcticrepository/0.4/arcticrepository-0.4.tar.gz

---
+++ packages/arcticrepository/arcticrepository-0.4.tar.gz/arcticrepository-0.4/arcticrepository/__init__.py
@@ -0,0 +1,22 @@
+from arctic import Arctic
+import pandas as pd
+import requests
+
+
+class ArcticRepository:
+
+    def __init__(self, config):
+        self.config = config
+        self.store = Arctic(config['arctic']['host'])
+        self.store.initialize_library(config["arctic"]["collection"])
+        self.library = self.store[config["arctic"]["collection"]]
+
+    def insert(self, id, data, metadata):
+        dataFrame = pd.DataFrame.from_dict(data)
+        self.library.write(id, dataFrame, metadata=metadata)
+        r = requests.put(self.config["elastic"]["url"] + id + '?pretty',
+                         json=metadata)
+        r.raise_for_status()
+
+    def read(self, name):
+        return self.library.read(name)
