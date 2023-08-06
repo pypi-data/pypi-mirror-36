584baa0982a49cde868c1f1821931d18ad377861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:26 +0000

    Add package chariot/0.4.3/chariot-0.4.3.tar.gz

---
+++ packages/chariot/chariot-0.4.3.tar.gz/chariot-0.4.3/chariot/feeder.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+from joblib import Parallel, delayed
+from chariot.base_processor import BaseProcessor
+
+
+def _apply(target, process, data, inverse=False):
+    input_data = data[target]
+    if isinstance(input_data, pd.Series):
+        input_data = input_data.values
+
+    if not inverse:
+        transformed = process.transform(input_data)
+    else:
+        if hasattr(process, "inverse_transform"):
+            transformed = process.inverse_transform(input_data)
+    return (target, transformed)
+
+
+class Feeder(BaseProcessor):
+
+    def __init__(self, spec):
+        super().__init__(spec)
+        self._resource = None
+
+    def set_resource(self, data):
+        self._resource = data
+        return self
+
+    def _get_keys(self, data, ignore):
+        if isinstance(data, pd.DataFrame):
+            keys = [c for c in data.columns if c not in ignore]
+        else:
+            keys = [k for k in data if k not in ignore]
+        return keys
+
+    def transform(self, data=None, ignore=(), n_jobs=-1, inverse=False):
+        _data = data if data else self._resource
+        tasks = []
+        for k in self.spec:
+            if k not in ignore:
+                tasks.append((k, self.spec[k]))
+
+        target_transformed = Parallel(n_jobs=n_jobs)(
+            delayed(_apply)(i, p, _data, inverse) for i, p in tasks)
+        target_transformed = dict(target_transformed)
+
+        applied = {}
+        for key in self._get_keys(_data, ignore):
+            if key in target_transformed:
+                applied[key] = target_transformed[key]
+            else:
+                applied[key] = _data[key]
+
+        return applied
+
+    def make_generator(self, data, batch_size, epoch=-1, n_jobs=-1,
+                       ignore=()):
+        _data = data if data else self._resource
+        data_length = len(_data)
+        steps_per_epoch = data_length // batch_size
+        keys = self._get_keys(_data, ignore)
+
+        def generator():
+            indices = np.arange(data_length)
+            count = 0
+            _epoch = 0
+            while True:
+                if count > 0 and count % steps_per_epoch == 0:
+                    _epoch += 1
+                    count = 0
+                    if epoch > 0 and _epoch >= epoch:
+                        break
+
+                selected = np.random.choice(indices, size=batch_size,
+                                            replace=False)
+
+                if isinstance(_data, pd.DataFrame):
+                    batch = _data[keys].iloc[selected, :]
+                else:
+                    batch = {}
+                    for key in keys:
+                        if isinstance(_data[key], pd.Series):
+                            batch[key] = _data[key].iloc[selected]
+                        else:
+                            batch[key] = _data[key][selected]
+
+                count += 1
+                yield self.transform(batch, n_jobs=n_jobs)
+
+        return steps_per_epoch, generator
+
+    def iterate(self, data=None, batch_size=32, epoch=-1, n_jobs=-1,
+                ignore=()):
+        _, generator = self.make_generator(data, batch_size, epoch, n_jobs,
+                                           ignore)
+        for b in generator():
+            yield b
+
+    def inverse_transform(self, batch, n_jobs=-1, ignore=()):
+        return self.transform(batch, n_jobs=n_jobs, ignore=ignore, inverse=True)
---
+++ packages/chariot/chariot-0.4.3.tar.gz/chariot-0.4.3/chariot/transformer/tokenizer/token.py
@@ -0,0 +1,61 @@
+class Token():
+
+    def __init__(self, token, token_type="spaCy"):
+        self._token = token
+        self.token_type = token_type
+        self.__surface = ""
+
+    def set_surface(self, surface):
+        self.__surface = surface
+
+    @property
+    def is_spacy(self):
+        return self.token_type == "spaCy"
+
+    @property
+    def is_ja(self):
+        return self.token_type == "ja"
+
+    @property
+    def surface(self):
+        if self.__surface:
+            return self.__surface
+        elif self.token_type == "spaCy":
+            return self._token.text
+        elif self.token_type == "ja":
+            return self._token.surface
+        else:
+            return self._token
+
+    @property
+    def base_form(self):
+        if self.token_type == "spaCy":
+            return self._token.lemma_
+        elif self.token_type == "ja":
+            return self._token.base_form
+        else:
+            return "-"
+
+    @property
+    def pos(self):
+        if self.token_type == "spaCy":
+            return self._token.pos_
+        elif self.token_type == "ja":
+            return self._token.part_of_speech[0]
+        else:
+            return "-"
+
+    @property
+    def tag(self):
+        if self.token_type == "spaCy":
+            return self._token.tag_
+        elif self.token_type == "ja":
+            return self._token.part_of_speech[1]
+        else:
+            return "-"
+
+    def __repr__(self):
+        return "<{}:{}>".format(self.surface, self.pos)
+
+    def __reduce_ex__(self, proto):
+        return type(self), (self._token, self.token_type)
