ab40e0ca2c30eab52330137f4c76fa17ec95de5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:35 +0000

    Add package emannotationschemas/0.2.14/emannotationschemas-0.2.14.tar.gz

---
+++ packages/emannotationschemas/emannotationschemas-0.2.14.tar.gz/emannotationschemas-0.2.14/emannotationschemas/__init__.py
@@ -0,0 +1,36 @@
+from emannotationschemas.synapse import SynapseSchema
+from emannotationschemas.errors import UnknownAnnotationTypeException
+from emannotationschemas.flatten import create_flattened_schema
+from emannotationschemas.presynaptic_bouton_type import PresynapticBoutonType
+from emannotationschemas.postsynaptic_compartment import PostsynapticCompartment
+from emannotationschemas.cell_type_local import CellTypeLocal
+__version__ = '0.2.14'
+
+type_mapping = {
+    'synapse': SynapseSchema,
+    'presynaptic_bouton_type': PresynapticBoutonType,
+    'postsynaptic_compartment': PostsynapticCompartment,
+    'cell_type_ai_manual': CellTypeLocal,
+    'synapse_ai_manual': SynapseSchema,
+}
+
+def get_types():
+    return [k for k in type_mapping.keys()]
+
+
+def get_schema(type):
+    try:
+        return type_mapping[type]
+    except KeyError:
+        msg = 'type {} is not a known annotation type'.format(type)
+        raise UnknownAnnotationTypeException(msg)
+
+
+def get_flat_schema(type):
+    try:
+        Schema = type_mapping[type]
+        FlatSchema = create_flattened_schema(Schema)
+        return FlatSchema
+    except KeyError:
+        msg = 'type {} is not a known annotation type'.format(type)
+        raise UnknownAnnotationTypeException(msg)
