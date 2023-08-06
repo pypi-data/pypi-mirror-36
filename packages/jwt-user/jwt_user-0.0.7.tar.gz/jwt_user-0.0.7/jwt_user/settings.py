830bb0a7f3bd34b812e7e503fdccd9a767b57b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:39 +0000

    Add package jwt-user/0.0.7/jwt_user-0.0.7.tar.gz

---
+++ packages/jwt-user/jwt_user-0.0.7.tar.gz/jwt_user-0.0.7/jwt_user/settings.py
@@ -0,0 +1,22 @@
+import os
+import uuid
+from datetime import timedelta
+
+DEFAULTS = {
+	'JWT_PRIVATE_KEY': os.environ.get('JWT_PRIVATE_KEY', uuid.uuid4().hex),
+	'JWT_SECRET_KEY': os.environ.get('JWT_SECRET_KEY', uuid.uuid4().hex),
+	'JWT_ALGORITHM': 'HS256',
+	'JWT_VERIFY': True,
+	'JWT_VERIFY_EXPIRATION': True,
+	'JWT_LEEWAY': 0,
+	'JWT_EXPIRATION_DELTA': timedelta(seconds=3600),
+	'JWT_AUDIENCE': None,
+	'JWT_ISSUER': None,
+	'JWT_ALLOW_REFRESH': False,
+	'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=7),
+	'JWT_AUTH_HEADER_PREFIX': 'JWT',
+	'JWT_AUTH_HEADER': 'Authorization'
+}
+
+VALID_USER_FIELDS = {'username', 'user_id'}
+EXCLUDE_USER_FIELDS = set()
