3636b3a08d599c1ad5ca49db97cf7d287337bee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:49 +0000

    Add package tkmvc/0.1.23/tkmvc-0.1.23.tar.gz

---
+++ packages/tkmvc/tkmvc-0.1.23.tar.gz/tkmvc-0.1.23/tkmvc/models/bank_model.py
@@ -0,0 +1,30 @@
+########################################################################################################################
+#    File: models.py
+#  Author: Dan Huckson, https://github.com/unodan
+#    Date: 2018-09-20
+########################################################################################################################
+
+
+class Transaction:
+    def __init__(self):
+        self.value = 0
+        self.callbacks = {}
+
+    def add_callback(self, func):
+        self.callbacks[func] = None
+
+    def _callbacks(self):
+        for func in self.callbacks:
+            func(self.value)
+
+    def set(self, value):
+        self.value = value
+        self._callbacks()
+
+    def get(self):
+        return self.value
+
+
+class Interest:
+    def __init__(self):
+        self.transaction = Transaction()
