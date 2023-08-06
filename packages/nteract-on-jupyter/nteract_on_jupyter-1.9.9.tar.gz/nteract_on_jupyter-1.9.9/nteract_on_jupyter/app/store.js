c2cef577938e61b0d604293424d7a7c8ff052928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:27 +0000

    Add package nteract-on-jupyter/1.9.9/nteract_on_jupyter-1.9.9.tar.gz

---
+++ packages/nteract-on-jupyter/nteract_on_jupyter-1.9.9.tar.gz/nteract_on_jupyter-1.9.9/nteract_on_jupyter/app/store.js
@@ -0,0 +1,29 @@
+/* @flow strict */
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+import { createEpicMiddleware, combineEpics } from "redux-observable";
+
+import type { AppState } from "@nteract/core";
+
+import { List as ImmutableList, Map as ImmutableMap } from "immutable";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+import { reducers, epics as coreEpics } from "@nteract/core";
+
+const rootReducer = combineReducers({
+  app: reducers.app,
+  comms: reducers.comms,
+  config: reducers.config,
+  core: reducers.core
+});
+
+export default function configureStore(initialState: AppState) {
+  const rootEpic = combineEpics(...coreEpics.allEpics);
+  const middlewares = [createEpicMiddleware(rootEpic)];
+
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(...middlewares))
+  );
+}
