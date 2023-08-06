2cb79d36aaa44b1a3e0aa1d41943950f338108d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:17 +0000

    Add package slack-entities/0.0.11.1/slack_entities-0.0.11.1.tar.gz

---
+++ packages/slack-entities/slack_entities-0.0.11.1.tar.gz/slack_entities-0.0.11.1/slack_entities/entities/outcoming_message.py
@@ -0,0 +1,18 @@
+from slack_entities.client.client import get_client
+from .channel import Channel
+
+
+class OutcomingMessage:
+    def __init__(self, channel: Channel, attachments: list=None, text: str='', token: str=None):
+        self.channel = channel
+        self.token = token
+        self.text = text
+        self.attachments = attachments if attachments else []
+
+    def send(self):
+        return get_client(token=self.token).api_call(
+            'chat.postMessage',
+            channel=self.channel.id,
+            text=self.text,
+            attachments=self.attachments
+        )
