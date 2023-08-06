480a5ff3ccf08e6ffe42ee5c8cf94e6c39962d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:30 +0000

    Add package checkQC/2.0.0rc1/checkQC-2.0.0rc1-py3-none-any.whl

---
+++ packages/checkQC/checkQC-2.0.0rc1-py3-none-any.whl/checkQC/exceptions.py
@@ -0,0 +1,53 @@
+
+
+"""
+Collecting handled Exceptions used by CheckQC.
+"""
+
+
+class CheckQCException(Exception):
+    pass
+
+
+class ConfigurationError(CheckQCException):
+    pass
+
+
+class InstrumentTypeUnknown(ConfigurationError):
+    pass
+
+
+class RunModeUnknown(ConfigurationError):
+    pass
+
+
+class ReagentVersionUnknown(ConfigurationError):
+    pass
+
+
+class RunInfoXMLNotFound(CheckQCException):
+    pass
+
+
+class RunParametersNotFound(CheckQCException):
+    pass
+
+
+class StatsJsonNotFound(CheckQCException):
+    pass
+
+
+class QCHandlerNotFound(CheckQCException):
+    pass
+
+
+class ConfigurationError(CheckQCException):
+    pass
+
+
+class ConfigEntryMissing(ConfigurationError):
+    pass
+
+
+class RunfolderNotFoundError(CheckQCException):
+    pass
