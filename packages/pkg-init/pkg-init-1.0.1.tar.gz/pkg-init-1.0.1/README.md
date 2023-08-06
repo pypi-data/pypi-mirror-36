e45757d4d48c70dfba836bb0f8f0fd5b23dc8997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:54 +0000

    Add package pkg-init/1.0.1/pkg-init-1.0.1.tar.gz

---
+++ packages/pkg-init/pkg-init-1.0.1.tar.gz/pkg-init-1.0.1/README.md
@@ -0,0 +1,25 @@
+# Initialize on a new package
+
+## Usage
+
+```sh
+pip install pkg-init
+cd /move/to/your/project/directory
+package-init package-name
+```
+
+or
+
+```sh
+git clone https://github.com/bluelief/pkg-init.git
+cd pkg-init
+python setup.py install
+cd /move/to/your/project/directory
+package-init package-name
+```
+
+
+## License
+
+Copyright (c) 2018 bluelief.  
+Licensed under the MIT License.  
---
+++ packages/pkg-init/pkg-init-1.0.1.tar.gz/pkg-init-1.0.1/setup.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2018 bluelief.
+# This source code is licensed under the MIT license.
+
+from setuptools import setup, find_packages
+
+
+entry_points = {
+    'console_scripts': [
+        'package-init = pkg_init.core:main'
+    ]
+}
+
+
+version = __import__('pkg_init').get_version()
+
+
+setup(
+    name="pkg-init",
+    version=version,
+    url='https://github.com/bluelief/pkg-init',
+    author="bluelief",
+    description="Initialize on a new package.",
+    license="MIT",
+    keywords="python, project",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points=entry_points,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Japanese'
+    ],
+    project_urls={
+        'Github': 'https://github.com/bluelief/pkg-init',
+    },
+)
