c5daf383ea8d788abaed305467263e7a084bc498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:52 +0000

    Add package glimix-core/2.0.2/glimix-core-2.0.2.tar.gz

---
+++ packages/glimix-core/glimix-core-2.0.2.tar.gz/glimix-core-2.0.2/glimix_core/link/test/test_link.py
@@ -0,0 +1,17 @@
+from glimix_core.link import LogitLink, LogLink, ProbitLink
+from numpy.testing import assert_allclose
+
+
+def test_probit_link():
+    link = ProbitLink()
+    assert_allclose(link.value(link.inv(3.2)), 3.2)
+
+
+def test_logit_link():
+    link = LogitLink()
+    assert_allclose(link.value(link.inv(3.2)), 3.2)
+
+
+def test_loglink_link():
+    link = LogLink()
+    assert_allclose(link.value(link.inv(3.2)), 3.2)
