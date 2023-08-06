7dbbfc36b1175b0465f42b5c56f3916d17ff0cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:07 +0000

    Add package l10n-ar-api/1.1.7/l10n_ar_api-1.1.7.tar.gz

---
+++ packages/l10n-ar-api/l10n_ar_api-1.1.7.tar.gz/l10n_ar_api-1.1.7/l10n_ar_api/afip_webservices/config.py
@@ -0,0 +1,13 @@
+authorization_urls = dict(
+    homologation = 'https://wsaahomo.afip.gov.ar/ws/services/LoginCms?wsdl',
+    production = 'https://wsaa.afip.gov.ar/ws/services/LoginCms?wsdl', 
+)
+
+service_urls = dict(
+    wsfev1_homologation = 'https://wswhomo.afip.gov.ar/wsfev1/service.asmx?wsdl',
+    wsfev1_production = 'https://servicios1.afip.gov.ar/wsfev1/service.asmx?wsdl', 
+    wsfexv1_homologation = 'https://wswhomo.afip.gov.ar/wsfexv1/service.asmx?wsdl',
+    wsfexv1_production = 'https://servicios1.afip.gov.ar/wsfexv1/service.asmx?wsdl',
+    ws_sr_padron_a4_homologation = 'https://awshomo.afip.gov.ar/sr-padron/webservices/personaServiceA4?WSDL',
+    ws_sr_padron_a4_production = 'https://aws.afip.gov.ar/sr-padron/webservices/personaServiceA4?WSDL',
+)
