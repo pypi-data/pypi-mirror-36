4dfbca963368ea1a039c4d88fa00bef9da417fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:19 +0000

    Add package hscraper/0.51/hscraper-0.51.tar.gz

---
+++ packages/hscraper/hscraper-0.51.tar.gz/hscraper-0.51/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(name='hscraper',
+    version='0.51',
+    description='A tool to rip galleries from ehentai, r34.xxx, danbooru.donmai and hitomi.la',
+    url='https://github.com/XrossFox/hscraper',
+    author='XrossFox',
+    author_email='warhead_1090@hotmail.com',
+    license='MIT',
+    packages=find_packages(exclude=["tests"],),
+    zip_safe=False,
+    install_requires=[
+        'beautifulsoup4>=4.6.0',
+        'urllib3>=1.22',
+        'click>=6.7',
+    ],
+    entry_points={
+        'console_scripts': [
+            'hscraper = hscraper.h_core:clickerino'
+        ]
+    },
+    data_files = [("",["LICENSE"])],
+)
