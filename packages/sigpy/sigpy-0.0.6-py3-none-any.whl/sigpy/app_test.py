568fe8cf2adae88b401133987a342693fcef21b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:54 +0000

    Add package sigpy/0.0.6/sigpy-0.0.6-py3-none-any.whl

---
+++ packages/sigpy/sigpy-0.0.6-py3-none-any.whl/sigpy/app_test.py
@@ -0,0 +1,196 @@
+import unittest
+import numpy as np
+import numpy.testing as npt
+from sigpy import app, linop, util, prox
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+class TestApp(unittest.TestCase):
+
+    def test_MaxEig(self):
+        n = 5
+        mat = util.randn([n, n])
+        A = linop.MatMul([n, 1], mat)
+        s = np.linalg.svd(mat, compute_uv=False)
+
+        npt.assert_allclose(app.MaxEig(A.H * A, max_iter=100).run(), s[0]**2, atol=1e-2)
+
+    def test_LinearLeastSquares(self):
+        n = 5
+        mat = np.eye(n) + 0.1 * util.randn([n, n])
+        A = linop.MatMul([n, 1], mat)
+        x = util.randn([n, 1])
+        y = A(x)
+        x_lstsq = np.linalg.lstsq(mat, y, rcond=-1)[0]
+
+        x_rec = app.LinearLeastSquares(A, y).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+
+        x_rec = app.LinearLeastSquares(
+            A, y, alg_name='GradientMethod', max_iter=1000).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+
+        x_rec = app.LinearLeastSquares(A, y, max_iter=1000,
+                                       alg_name='PrimalDualHybridGradient').run()
+        npt.assert_allclose(x_rec, x_lstsq)
+        
+    def test_l2reg_LinearLeastSquares(self):
+        n = 5
+        mat = np.eye(n) + 0.1 * util.randn([n, n])
+        A = linop.MatMul([n, 1], mat)
+        x = util.randn([n, 1])
+        y = A(x)
+        lamda = 0.1
+        x_lstsq = np.linalg.solve(np.matmul(mat.conjugate().T, mat) + lamda * np.eye(n),
+                                  np.matmul(mat.conjugate().T, y))
+
+        x_rec = app.LinearLeastSquares(A, y, lamda=lamda).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+
+        x_rec = app.LinearLeastSquares(
+            A, y, alg_name='GradientMethod', max_iter=1000, lamda=lamda).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+
+        x_rec = app.LinearLeastSquares(A, y, max_iter=1000, lamda=lamda,
+                                       alg_name='PrimalDualHybridGradient').run()
+        npt.assert_allclose(x_rec, x_lstsq)
+        
+    def test_l2reg_bias_LinearLeastSquares(self):
+        n = 5
+        mat = np.eye(n) + 0.1 * util.randn([n, n])
+        A = linop.MatMul([n, 1], mat)
+        x = util.randn([n, 1])
+        y = A(x)
+        z = util.randn([n, 1])
+        lamda = 0.1
+        mu = 0.01
+        x_lstsq = np.linalg.solve(np.matmul(mat.conjugate().T, mat) + (lamda + mu) * np.eye(n),
+                                  np.matmul(mat.conjugate().T, y) + mu * z)
+
+        x_rec = app.LinearLeastSquares(A, y, lamda=lamda, mu=mu, z=z).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+
+        x_rec = app.LinearLeastSquares(
+            A, y, alg_name='GradientMethod', max_iter=1000, lamda=lamda, mu=mu, z=z).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+
+        x_rec = app.LinearLeastSquares(A, y, max_iter=1000, lamda=lamda, mu=mu, z=z,
+                                       alg_name='PrimalDualHybridGradient').run()
+        npt.assert_allclose(x_rec, x_lstsq)
+        
+    def test_proxg_LinearLeastSquares(self):
+        n = 5
+        mat = np.eye(n) + 0.1 * util.randn([n, n])
+        A = linop.MatMul([n, 1], mat)
+        x = util.randn([n, 1])
+        y = A(x)
+        lamda = 0.1
+        x_lstsq = np.linalg.solve(np.matmul(mat.conjugate().T, mat) + lamda * np.eye(n),
+                                  np.matmul(mat.conjugate().T, y))
+
+        proxg = prox.L2Reg([n, 1], lamda)
+        x_rec = app.LinearLeastSquares(
+            A, y, alg_name='GradientMethod', max_iter=1000, proxg=proxg).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+
+        x_rec = app.LinearLeastSquares(A, y, max_iter=1000, proxg=proxg,
+                                       alg_name='PrimalDualHybridGradient').run()
+        npt.assert_allclose(x_rec, x_lstsq)
+        
+    def test_l2reg_proxg_LinearLeastSquares(self):
+        n = 5
+        mat = np.eye(n) + 0.1 * util.randn([n, n])
+        A = linop.MatMul([n, 1], mat)
+        x = util.randn([n, 1])
+        y = A(x)
+        lamda = 0.1
+        x_lstsq = np.linalg.solve(np.matmul(mat.conjugate().T, mat) + 2 * lamda * np.eye(n),
+                                  np.matmul(mat.conjugate().T, y))
+
+        proxg = prox.L2Reg([n, 1], lamda)
+        
+        x_rec = app.LinearLeastSquares(
+            A, y, alg_name='GradientMethod', max_iter=1000, lamda=lamda, proxg=proxg).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+
+        x_rec = app.LinearLeastSquares(
+            A, y, alg_name='PrimalDualHybridGradient', max_iter=1000,
+            lamda=lamda, proxg=proxg).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+        
+    def test_l2reg_bias_proxg_LinearLeastSquares(self):
+        n = 5
+        mat = np.eye(n) + 0.1 * util.randn([n, n])
+        A = linop.MatMul([n, 1], mat)
+        x = util.randn([n, 1])
+        y = A(x)
+        z = util.randn([n, 1])
+        lamda = 0.1
+        mu = 0.01
+        x_lstsq = np.linalg.solve(np.matmul(mat.conjugate().T, mat) + (2 * lamda + mu) * np.eye(n),
+                                  np.matmul(mat.conjugate().T, y) + mu * z)
+
+        proxg = prox.L2Reg([n, 1], lamda)
+        x_rec = app.LinearLeastSquares(
+            A, y, alg_name='GradientMethod', max_iter=1000, lamda=lamda, mu=mu, z=z,
+            proxg=proxg).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+        
+        x_rec = app.LinearLeastSquares(
+            A, y, alg_name='PrimalDualHybridGradient', max_iter=1000,
+            lamda=lamda, mu=mu, z=z,
+            proxg=proxg).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+
+    def test_L2ConstrainedMinimization(self):
+        n = 5
+        mat = np.eye(n) + 0.1 * util.randn([n, n])
+        A = linop.MatMul([n, 1], mat)
+        x = util.randn([n, 1])
+        y = A(x)
+
+        eps = 0
+
+        def proxg(lamda, x):
+            return x / (1 + lamda)
+
+        x_rec = app.L2ConstrainedMinimization(A, y, proxg, eps).run()
+        npt.assert_allclose(x_rec, x)
+
+    def test_precond_LinearLeastSquares(self):
+        n = 5
+        mat = np.eye(n) + 0.1 * util.randn([n, n])
+        A = linop.MatMul([n, 1], mat)
+        x = util.randn([n, 1])
+        y = A(x)
+        x_lstsq = np.linalg.lstsq(mat, y, rcond=-1)[0]
+        p = 1 / (np.sum(abs(mat)**2, axis=0).reshape([n, 1]))
+
+        P = linop.Multiply([n, 1], p)
+        x_rec = app.LinearLeastSquares(A, y).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+
+        alpha = p / app.MaxEig(P * A.H * A).run()
+        x_rec = app.LinearLeastSquares(A, y, alg_name='GradientMethod',
+                                       max_iter=1000, alpha=alpha).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+
+        tau = p
+        x_rec = app.LinearLeastSquares(A, y, alg_name='PrimalDualHybridGradient',
+                                       max_iter=1000, tau=tau).run()
+        npt.assert_allclose(x_rec, x_lstsq)
+
+    def test_dual_precond_LinearLeastSquares(self):
+        n = 5
+        mat = np.eye(n) + 0.1 * util.randn([n, n])
+        A = linop.MatMul([n, 1], mat)
+        x = util.randn([n, 1])
+        y = A(x)
+        x_lstsq = np.linalg.lstsq(mat, y, rcond=-1)[0]
+
+        d = 1 / np.sum(abs(mat)**2, axis=1, keepdims=True).reshape([n, 1])        
+        x_rec = app.LinearLeastSquares(A, y, alg_name='PrimalDualHybridGradient',
+                                       max_iter=1000, sigma=d).run()
+        npt.assert_allclose(x_rec, x_lstsq)
---
+++ packages/sigpy/sigpy-0.0.6-py3-none-any.whl/sigpy/mri/precond.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+"""MRI preconditioners.
+"""
+import sigpy as sp
+
+
+__all__ = ['kspace_precond', 'circulant_precond']
+
+
+def kspace_precond(mps, weights=None, coord=None, lamda=0, device=sp.cpu_device):
+    """Compute a diagonal preconditioner in k-space.
+
+    Considers the optimization problem:
+
+    .. math::
+        \min_P \| P A A^H - I \|_2^2
+
+    where A is the Sense operator.
+
+    Args:
+        mps (array): sensitivity maps of shape [num_coils] + image shape.
+        weights (array): k-space weights.
+        coord (array): k-space coordinates of shape [...] + [ndim].
+        lamda (float): regularization.
+
+    Returns:
+        array: k-space preconditioner of same shape as k-space.
+    """
+    dtype = mps.dtype
+
+    if weights is not None:
+        weights = sp.to_device(weights, device)
+
+    device = sp.Device(device)
+    xp = device.xp
+
+    mps_shape = list(mps.shape)
+    img_shape = list(mps_shape[1:])
+    img2_shape = [i * 2 for i in img_shape]
+    ndim = len(img_shape)
+    num_coils = mps.shape[0]
+
+    scale = sp.prod(img2_shape)**1.5 / sp.prod(img_shape)
+    with device:
+        if coord is None:
+            idx = [slice(None, None, 2)] * ndim
+
+            ones = xp.zeros(img2_shape, dtype=dtype)
+            if weights is None:
+                ones[idx] = 1
+            else:
+                ones[idx] = weights**0.5
+
+            psf = sp.ifft(ones)
+        else:
+            coord2 = coord * 2
+            ones = xp.ones(coord.shape[:-1], dtype=dtype)
+            if weights is not None:
+                ones *= weights**0.5
+
+            psf = sp.nufft_adjoint(ones, coord2, img2_shape)
+
+        mps = sp.to_device(mps, device)
+        mps_ij = mps * xp.conj(mps.reshape([num_coils, 1] + img_shape))
+        xcorr_fourier = xp.sum(xp.abs(sp.fft(mps_ij, [num_coils, num_coils] + img2_shape, axes=range(-ndim, 0)))**2, axis=0)
+        xcorr = sp.ifft(xcorr_fourier, axes=range(-ndim, 0))
+        xcorr *= psf
+        
+        if coord is None:
+            p_inv = sp.fft(xcorr, axes=range(-ndim, 0))[[slice(None)] + idx]
+        else:
+            p_inv = sp.nufft(xcorr, coord2)
+
+        if weights is not None:
+            p_inv *= weights**0.5
+
+        mps_norm2 = sp.norm2(mps, axes=range(-ndim, 0)).reshape([num_coils] + [1] * (p_inv.ndim - 1))
+        p_inv *= scale / mps_norm2
+        p_inv += lamda
+        p_inv /= 1 + lamda
+        p_inv[p_inv == 0] = 1
+        p = 1 / p_inv
+
+        return p.astype(dtype)
+
+
+def circulant_precond(mps, weights=None, coord=None, lamda=0, device=sp.cpu_device):
+    """Compute circulant preconditioner.
+
+    Considers the optimization problem:
+
+    .. math::
+        \min_P \| A^H A - F P F^H  \|_2^2
+
+    where A is the Sense operator, and F is a unitary Fourier transform operator.
+
+    Args:
+        mps (array): sensitivity maps of shape [num_coils] + image shape.
+        weights (array): k-space weights.
+        coord (array): k-space coordinates of shape [...] + [ndim].
+        lamda (float): regularization.
+
+    Returns:
+        array: circulant preconditioner of image shape.
+
+    """
+    if coord is not None:
+        coord = sp.to_device(coord, device)
+
+    if weights is not None:
+        weights = sp.to_device(weights, device)
+
+    dtype = mps.dtype
+    device = sp.Device(device)
+    xp = device.xp
+
+    mps_shape = list(mps.shape)
+    img_shape = mps_shape[1:]
+    img2_shape = [i * 2 for i in img_shape]
+    ndim = len(img_shape)
+    num_coils = mps.shape[0]
+
+    scale = sp.prod(img2_shape)**1.5 / sp.prod(img_shape)**2
+    with device:
+        idx = [slice(None, None, 2)] * ndim
+        if coord is None:
+            ones = xp.zeros(img2_shape, dtype=dtype)
+            if weights is None:
+                ones[idx] = 1
+            else:
+                ones[idx] = weights**0.5
+
+            psf = sp.ifft(ones)
+        else:
+            coord2 = coord * 2
+            ones = xp.ones(coord.shape[:-1], dtype=dtype)
+            if weights is not None:
+                ones *= weights**0.5
+
+            psf = sp.nufft_adjoint(ones, coord2, img2_shape)
+
+        mps = sp.to_device(mps, device)
+        xcorr_fourier = xp.abs(sp.fft(xp.conj(mps), [num_coils] + img2_shape, axes=range(-ndim, 0)))**2
+        xcorr = sp.ifft(xcorr_fourier, axes=range(-ndim, 0))
+        xcorr *= psf
+        p_inv = sp.fft(xcorr, axes=range(-ndim, 0))
+        p_inv = p_inv[[slice(None)] + idx]
+        p_inv *= scale
+        if weights is not None:
+            p_inv *= weights**0.5
+
+        p_inv = xp.sum(p_inv, axis=0)
+        p_inv += lamda
+        p_inv[p_inv == 0] = 1
+        p = 1 / p_inv
+
+        return p.astype(dtype)
