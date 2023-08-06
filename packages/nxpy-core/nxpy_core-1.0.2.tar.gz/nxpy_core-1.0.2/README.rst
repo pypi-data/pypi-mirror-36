d02d3ddfa6ac4df454083220a52c20d894302795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:43 +0000

    Add package nxpy-core/1.0.2/nxpy_core-1.0.2.tar.gz

---
+++ packages/nxpy-core/nxpy_core-1.0.2.tar.gz/nxpy_core-1.0.2/README.rst
@@ -0,0 +1,12 @@
+Nxpy Core
+=========
+
+You can install the library with pip::
+
+    pip install nxpy-core
+
+*Nxpy Core* contains generic implementation components on which other libraries depend. It is not
+meant for standalone use. 
+
+The library's documentation is available on
+`ReadTheDocs <https://nxpy.readthedocs.io/en/latest/core.html>`_.
---
+++ packages/nxpy-core/nxpy_core-1.0.2.tar.gz/nxpy_core-1.0.2/nxpy/core/error/error.py
@@ -0,0 +1,16 @@
+# nxpy_core -------------------------------------------------------------------
+
+# Copyright Nicola Musatti 2008 - 2018
+# Use, modification, and distribution are subject to the Boost Software
+# License, Version 1.0. (See accompanying file LICENSE.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
+# See https://github.com/nmusatti/nxpy/tree/master/libs/core. -----------------
+
+r"""
+Exception classes.
+
+"""
+
+class ArgumentError(Exception):
+    r"""Raised when a function receives an invalid argument"""
