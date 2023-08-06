3962661c5e143804a927828bfd857284fb9d34ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:49 +0000

    Add package beryllium/0.0.2/beryllium-0.0.2.tar.gz

---
+++ packages/beryllium/beryllium-0.0.2.tar.gz/beryllium-0.0.2/beryllium/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+from spider.driver.base.driver import *
+from spider.driver.base.exception_bak import *
+from spider.driver.base.field import *
+from spider.driver.base.listcssselector import *
+from spider.driver.base.logger import *
+from spider.driver.base.mongodb import *
+from spider.driver.base.mysql import *
+from spider.driver.base.page import *
+from spider.driver.base.tabsetup import *
