5cb00b6f6d585e9dc36a27c957fde63286dd7a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:50 +0000

    Add package getmac/0.6.0/getmac-0.6.0.tar.gz

---
+++ packages/getmac/getmac-0.6.0.tar.gz/getmac-0.6.0/getmac/__init__.py
@@ -0,0 +1,2 @@
+from .getmac import get_mac_address, __version__
+__all__ = ['get_mac_address']
---
+++ packages/getmac/getmac-0.6.0.tar.gz/getmac-0.6.0/setup.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from io import open  # Python 2 compatibility
+
+from setuptools import setup, find_packages
+
+from getmac.getmac import __version__
+
+
+with open('README.md', encoding='utf-8') as f:  # Loads in the README for PyPI
+    long_description = f.read()
+
+
+setup(
+    name='getmac',
+    version=__version__,
+    author='Christopher Goes',
+    author_email='ghostofgoes@gmail.com',
+    description='Get MAC addresses of remote hosts and local interfaces',
+    long_description=long_description,  # This is what you see on PyPI page
+    # PEP 566, PyPI Warehouse, setuptools>=38.6.0 make markdown possible
+    long_description_content_type='text/markdown',
+    url='https://github.com/GhostofGoes/getmac',
+    download_url='https://pypi.org/project/getmac/',
+    project_urls={
+        'Documentation': 'https://getmac.readthedocs.io/en/latest/',
+        'Discord Server': 'https://discord.gg/python',
+        'Docker': 'https://hub.docker.com/r/ghostofgoes/getmac/',
+    },
+    license='MIT',
+    data_files=[('share/man/man1', ['docs/man/getmac.1'])],
+    packages=find_packages(exclude=['tests.py']),
+    zip_safe=True,
+    entry_points={  # These enable commandline usage of the tool
+        'console_scripts': [
+            'getmac = getmac.__main__:main',
+        ],
+    },
+    tests_require=[
+        'setuptools>=38.6.0',
+        'tox>=3',
+        'flake8',
+        'codespell',
+        'mock;python_version<"3"',
+    ],
+    install_requires=['argparse'] if sys.version_info[:2] < (2, 7) else [],
+    platforms=['any'],
+    keywords='getmac get-mac macaddress mac-address mac address media access '
+             'control ethernet networking network networks layer2 layer-2 osi '
+             'ieee 802 mac-48 hardware-address tool script python python3',
+    classifiers=[  # Used by PyPI to classify the project and make it searchable
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: OS Independent',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: IronPython',
+        'Programming Language :: Python :: Implementation :: Jython',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Systems Administration',
+        'Topic :: System :: Networking',
+        'Topic :: Utilities',
+    ]
+)
