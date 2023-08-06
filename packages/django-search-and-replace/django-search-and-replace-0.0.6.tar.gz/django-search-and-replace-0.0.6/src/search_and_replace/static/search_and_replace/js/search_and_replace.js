24ccdd52d33c7c49949d8f3bce683c12d6e0381d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:17 +0000

    Add package django-search-and-replace/0.0.6/django-search-and-replace-0.0.6.tar.gz

---
+++ packages/django-search-and-replace/django-search-and-replace-0.0.6.tar.gz/django-search-and-replace-0.0.6/src/search_and_replace/static/search_and_replace/js/search_and_replace.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', function () {
+    var search_and_replace = document.getElementById("search-and-replace");
+
+    var search = new Mark(document.querySelectorAll("td.search"));
+    search.mark(search_and_replace.dataset.search, {
+        separateWordSearch: false,
+        className: "mark-search",
+        caseSensitive: true,
+        diacritics: false
+    });
+
+    var replace = new Mark(document.querySelectorAll("td.replace"));
+    replace.mark(search_and_replace.dataset.replace, {
+        separateWordSearch: false,
+        className: "mark-replace",
+        caseSensitive: true,
+        diacritics: false
+    });
+}, false);
