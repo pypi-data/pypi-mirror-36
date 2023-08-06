85e3cf6c772db4ab3d8ae69bdfb6fa5a7b3f975a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:58 +0000

    Add package howard/1.0/howard-1.0.tar.gz

---
+++ packages/howard/howard-1.0.tar.gz/howard-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='howard',
+    version='1.0',
+    packages=[''],
+    package_dir={'': 'howard'},
+    url='https://github.com/nhumrich/howard',
+    license='MIT',
+    author='nhumrich',
+    author_email='',
+    description='Convert dictionaries to dataclasses and back'
+)
