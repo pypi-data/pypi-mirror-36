8635d627c53780f81aa86818afc28a675ff90dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:08 +0000

    Add package nino-violino/0.0.5/nino_violino-0.0.5.tar.gz

---
+++ packages/nino-violino/nino_violino-0.0.5.tar.gz/nino_violino-0.0.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+  name='nino_violino',
+  packages=['block_generator', 'block_generator.block_parser', 'block_generator.song_namer'],
+  version='0.0.5',
+  description='Yet another music generator. ',
+  keywords=['nino_violino'],
+  author='Nikola Dokoski',
+  install_requires=[
+    'midiutil',
+    'haikunator',
+  ],
+)
