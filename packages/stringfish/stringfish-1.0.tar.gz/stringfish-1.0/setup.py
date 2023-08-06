c4f31634e3e60ddc75fc8fd72e2ee0b6122f3fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:12 +0000

    Add package stringfish/1.0/stringfish-1.0.tar.gz

---
+++ packages/stringfish/stringfish-1.0.tar.gz/stringfish-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='stringfish',
+	version='1.0',
+	description='Ultra-thin wrapper around subprocess for simple default use with string commands',
+	url='http://github.com/gregorpatof/stringfish',
+	author='gregorpatof',
+	author_email='gr3gorpatof@gmail.com',
+	packages=['stringfish'],
+	classifiers=(
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	),
+	zip_safe=False)
