fb9cd70d669f5ebf7487373cc0a3c0b44505b72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:37 +0000

    Add package ftw.solr/2.2.0/ftw.solr-2.2.0.tar.gz

---
+++ packages/ftw.solr/ftw.solr-2.2.0.tar.gz/ftw.solr-2.2.0/ftw/solr/testing.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+
+import ftw.solr
+
+
+class FtwSolrLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load any other ZCML that is required for your tests.
+        # The z3c.autoinclude feature is disabled in the Plone fixture base
+        # layer.
+        # import plone.app.dexterity
+        # self.loadZCML(package=plone.app.dexterity)
+        self.loadZCML(package=ftw.solr)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'ftw.solr:default')
+
+
+FTW_SOLR_FIXTURE = FtwSolrLayer()
+
+
+FTW_SOLR_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(FTW_SOLR_FIXTURE,),
+    name='FtwSolrLayer:IntegrationTesting'
+)
+
+
+FTW_SOLR_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(FTW_SOLR_FIXTURE,),
+    name='FtwSolrLayer:FunctionalTesting'
+)
