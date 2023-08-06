1c91a9f5f901ce1baae7e4c7ce7d20e3c038ddff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:04 +0000

    Add package turing-api/1.0.1/turing_api-1.0.1.tar.gz

---
+++ packages/turing-api/turing_api-1.0.1.tar.gz/turing_api-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="turing_api",
+    version="1.0.1",
+    author="Turing Analytics",
+    author_email="aditya@turingiq.com",
+    description="Turing visual search and visually similar recommendations API library for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/turingiq/turing-python",
+    packages=setuptools.find_packages(),
+    install_requires=['requests==2.*'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+)
