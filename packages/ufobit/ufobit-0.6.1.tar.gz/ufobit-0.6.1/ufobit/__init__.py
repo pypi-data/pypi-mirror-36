b3922dd84144c8a6ca854f4619103b828f667c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:48 +0000

    Add package ufobit/0.6.1/ufobit-0.6.1.tar.gz

---
+++ packages/ufobit/ufobit-0.6.1.tar.gz/ufobit-0.6.1/ufobit/__init__.py
@@ -0,0 +1,8 @@
+from .format import verify_sig
+from .network.fees import set_fee_cache_time
+from .network.rates import SUPPORTED_CURRENCIES, set_rate_cache_time
+from .network.services import set_service_timeout
+from .wallet import Key, PrivateKey, wif_to_key
+from .network.services import config
+
+__version__ = '0.6.1'
