43f64e0581f1281fcf22749e455639e8bcd8bba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:29 +0000

    Add package discrete-signals/0.4.0/discrete-signals-0.4.0.tar.gz

---
+++ packages/discrete-signals/discrete-signals-0.4.0.tar.gz/discrete-signals-0.4.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+DESC = 'A domain specific language for modeling and manipulating discrete time signals.'  # noqa
+
+setup(
+    name='discrete-signals',
+    version='0.4.0',
+    description=DESC,
+    url='http://github.com/mvcisback/DiscreteSignals',
+    author='Marcell Vazquez-Chanlatte',
+    author_email='marcell.vc@eecs.berkeley.edu',
+    license='MIT',
+    install_requires=[
+        'attrs',
+        'funcy',
+        'sortedcontainers',
+    ],
+    packages=find_packages(),
+)
