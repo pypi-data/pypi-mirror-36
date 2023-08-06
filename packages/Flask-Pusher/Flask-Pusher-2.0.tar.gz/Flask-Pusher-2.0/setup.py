7bb0ef08980d2cce8231dd3c24db0b5fe8573d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:38 +0000

    Add package Flask-Pusher/2.0/Flask-Pusher-2.0.tar.gz

---
+++ packages/Flask-Pusher/Flask-Pusher-2.0.tar.gz/Flask-Pusher-2.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='Flask-Pusher',
+    version='2.0',
+    url='https://www.github.com/iurisilvio/Flask-Pusher',
+    license='MIT',
+    author='Iuri de Silvio',
+    author_email='iurisilvio@gmail.com',
+    description='Flask extension for Pusher',
+    py_modules=['flask_pusher'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'pusher<3',
+        'Flask-Jsonpify',  # for jsonp auth support
+    ],
+    test_suite="tests",
+    tests_require=[
+        'mock',
+        'unittest2',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
