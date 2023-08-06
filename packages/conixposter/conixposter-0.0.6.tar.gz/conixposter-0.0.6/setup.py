58673ba8927bda906587770f2507830bfd165776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:25 +0000

    Add package conixposter/0.0.6/conixposter-0.0.6.tar.gz

---
+++ packages/conixposter/conixposter-0.0.6.tar.gz/conixposter-0.0.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+setup(
+    name='conixposter',  # Required
+    version='0.0.6',  # Required
+    description='Interface to a conix administrative domain',  # Optional
+    url='https://github.com/conix-center/smart-cities-demo/tree/master/client-library/python/conixposter',  # Optional
+    author='Joshua Adkins',  # Optional
+    author_email='adkins@berkeley.edu',  # Optional
+    keywords='sample setuptools development access control post sensor data',  # Optional
+    packages=find_packages(exclude=['examples', 'docs', 'tests']),  # Required
+    install_requires=['pint',
+                      'aenum',
+                      'wavemqtt'],  # Optional
+    
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    #entry_points={  # Optional
+    #    'console_scripts': [
+    #        'conixpost=conixposter::main',
+    #    ],
+    #},
+)
