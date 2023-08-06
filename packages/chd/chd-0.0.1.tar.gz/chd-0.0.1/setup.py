7fe89f19d746f1f444102eea937f9201bdc4bc19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:23 +0000

    Add package chd/0.0.1/chd-0.0.1.tar.gz

---
+++ packages/chd/chd-0.0.1.tar.gz/chd-0.0.1/setup.py
@@ -0,0 +1,50 @@
+import codecs
+
+from setuptools import setup, find_packages
+
+import chd
+
+
+install_requires = [
+    'setuptools>=40.4.1',
+    'cryptography>=2.3.1',
+    'scrapy>=1.5.1'
+]
+
+
+def long_description():
+    with codecs.open('README.rst', encoding='utf8') as f:
+        return f.read()
+
+
+setup(
+    name='chd',
+    version=chd.__version__,
+    description=chd.__doc__.strip(),
+    download_url='https://github.com/mikhailsidorov/chd',
+    author=chd.__author__,
+    author_email='mikhail.a.sidorov@yandex.com',
+    license=chd.__license__,
+    url='https://github.com/mikhailsidorov/chd',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'chd = chd:main',
+        ],
+    },
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Environment :: Console',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Education',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Terminals',
+        'Topic :: Utilities'
+    ],
+)
