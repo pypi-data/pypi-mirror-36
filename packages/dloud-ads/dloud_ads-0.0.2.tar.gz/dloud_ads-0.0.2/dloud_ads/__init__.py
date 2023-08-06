f61025964a0b279fd964b73f8d657d356b9838f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:34 +0000

    Add package dloud-ads/0.0.2/dloud_ads-0.0.2.tar.gz

---
+++ packages/dloud-ads/dloud_ads-0.0.2.tar.gz/dloud_ads-0.0.2/dloud_ads/__init__.py
@@ -0,0 +1,5 @@
+"""dloud_ads - Abstract Data Structures commonly used in CS scenarios. Implemented by Data Loud Labs!"""
+
+__version__ = '0.0.1'
+__author__ = 'Pedro Sousa <pjgs.sousa@gmail.com>'
+__all__ = []
