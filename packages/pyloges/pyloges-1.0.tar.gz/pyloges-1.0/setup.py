4101bc25c6fcdb2a50bc8acc7457da3820a637f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:55 +0000

    Add package pyloges/1.0/pyloges-1.0.tar.gz

---
+++ packages/pyloges/pyloges-1.0.tar.gz/pyloges-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyloges',
+    version='1.0',
+    packages=['pyloges'],
+    url='https://github.com/Nekit10/pyloges/',
+    license='GNU GPL v3',
+    author='Nikta Serba (Koni Dev Team)',
+    author_email='nikitaserba@icloud.com',
+    description='Simple and powerful logging library'
+)
