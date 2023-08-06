08076b6d8b2353e086553a1ac46054df5e9ff004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:58 +0000

    Add package MicroTokenizer/0.15.2/MicroTokenizer-0.15.2.tar.gz

---
+++ packages/MicroTokenizer/MicroTokenizer-0.15.2.tar.gz/MicroTokenizer-0.15.2/MicroTokenizer/cli_command/train.py
@@ -0,0 +1,13 @@
+import plac
+
+from MicroTokenizer.train.train import train as train_func
+
+
+# TODO: plac based CLI may have bug, need be checked
+@plac.annotations(
+    output_dir=("output directory to store model in", "positional", None, str),
+    train_data=("location of training data", "positional",
+                None, str, None, "n")
+)
+def train(output_dir, *train_data):
+    train_func(train_data, output_dir)
