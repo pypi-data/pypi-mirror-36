1467f23e97796ba7a5b28aad7f0250bb044b3e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:00 +0000

    Add package tkmvc/0.1.14/tkmvc-0.1.14-py2.py3-none-any.whl

---
+++ packages/tkmvc/tkmvc-0.1.14-py2.py3-none-any.whl/tkmvc/views/bank.py
@@ -0,0 +1,22 @@
+########################################################################################################################
+#    File: bank.py
+#  Author: Dan Huckson, https://github.com/unodan
+#    Date: 2018-09-20
+########################################################################################################################
+
+from tkinter import Toplevel, Label, Entry
+
+
+class Bank(Toplevel):  # View
+    def __init__(self, master):
+        Toplevel.__init__(self, master)
+        self.protocol('WM_DELETE_WINDOW', self.master.destroy)
+
+        Label(self, text='Account Balance').pack(side='left')
+        self.balance = Entry(self, width=8)
+        self.balance.pack(side='left')
+
+    def set_balance(self, amount):
+        self.balance.delete(0, 'end')
+        self.balance.insert('end', str(amount))
+
