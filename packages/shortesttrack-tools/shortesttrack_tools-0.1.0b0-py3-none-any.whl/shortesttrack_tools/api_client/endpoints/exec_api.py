ff44388afc5c0bd7327410ae0e219330cedb73ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:17 +0000

    Add package shortesttrack-tools/0.1.0b0/shortesttrack_tools-0.1.0b0-py3-none-any.whl

---
+++ packages/shortesttrack-tools/shortesttrack_tools-0.1.0b0-py3-none-any.whl/shortesttrack_tools/api_client/endpoints/exec_api.py
@@ -0,0 +1,6 @@
+from shortesttrack_tools.api_client.endpoints.base_endpoint import BaseEndpoint
+
+
+class ExecAPIEndpoint(BaseEndpoint):
+    def __init__(self, api_client, *args, **kwargs):
+        super().__init__(api_client, base=api_client.endpoint_urls.EXEC_API_SERVICE_ENDPOINT, *args, **kwargs)
