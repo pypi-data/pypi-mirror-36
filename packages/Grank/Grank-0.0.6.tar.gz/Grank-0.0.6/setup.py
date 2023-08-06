9496856c0a099a3143f0d7f962a8c4d755d4ed3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:04 +0000

    Add package Grank/0.0.6/Grank-0.0.6.tar.gz

---
+++ packages/Grank/Grank-0.0.6.tar.gz/Grank-0.0.6/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+setuptools.setup(
+    name="Grank",
+    version="0.0.6",
+    py_modules=['grank'],
+    packages=setuptools.find_packages(),
+    install_requires= [
+        'click',
+        'requests',
+        'pandas',
+        'numpy',
+        'matplotlib'
+    ],
+    entry_points="""
+        [console_scripts]
+        grank=grank.core:main
+    """,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
