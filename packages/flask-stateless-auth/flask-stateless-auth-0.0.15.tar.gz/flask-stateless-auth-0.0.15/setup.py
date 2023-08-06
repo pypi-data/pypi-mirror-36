d2121b80c1a8034f44086cd25d68d0b5450b6405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:39 +0000

    Add package flask-stateless-auth/0.0.15/flask-stateless-auth-0.0.15.tar.gz

---
+++ packages/flask-stateless-auth/flask-stateless-auth-0.0.15.tar.gz/flask-stateless-auth-0.0.15/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+install_requires = [
+    'flask'
+]
+
+tests_require = [
+    'flask_sqlalchemy',
+    'pytest',
+    'tox'
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flask-stateless-auth",
+    version="0.0.15",
+    author="Omar Ryhan",
+    author_email="omarryhan@gmail.com",
+    license="MIT",
+    description="Flask stateless authentication with secrets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=install_requires,
+    tests_require=tests_require,
+    url="https://github.com/omarryhan/flask-stateless-auth",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
