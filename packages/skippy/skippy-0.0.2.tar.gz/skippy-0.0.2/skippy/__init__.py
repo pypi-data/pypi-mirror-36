cd51b1d3f67a819f55ed427ceb43d537067dc44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:58 +0000

    Add package skippy/0.0.2/skippy-0.0.2.tar.gz

---
+++ packages/skippy/skippy-0.0.2.tar.gz/skippy-0.0.2/skippy/__init__.py
@@ -0,0 +1,7 @@
+from skippy.get_data import get_data
+from skippy.split_data import split_data
+from skippy.get_model import get_model
+from skippy.pickle_model import pickle_model
+from skippy.plot import plot_cm
+from skippy.classification import classification
+
