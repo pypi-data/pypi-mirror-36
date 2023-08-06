7ffeee14ed71ce48d4c8020782267f5b82254456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:36 +0000

    Add package area4/1.0.6/area4-1.0.6-py3-none-any.whl

---
+++ packages/area4/area4-1.0.6-py3-none-any.whl/area4/__init__.py
@@ -0,0 +1,51 @@
+# ~ Area4 Package by RDIL ~ 
+
+# Package info variables:
+name = "area4" 
+author = "https://github.com/RDIL"
+author_email = "contactspaceboom@gmail.com"
+description = "Dividers in Python, the easy way! Multiple different divider looks."
+
+# Divider variables:
+divider1 = str("------------------------")
+divider2 = str("________________________")
+divider3 = str("........................")
+divider4 = str("⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛")
+divider5 = str("⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️")
+divider6 = str("⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️")
+divider7 = str("========================")
+divider8 = str("########################")
+custom_div = str("")
+
+# Functions:  
+def div1():
+    print(divider1)
+
+def div2():
+    print(divider2)
+
+def div3():
+    print(divider3)
+
+def div4():
+    print(divider4)
+
+def div5():
+    print(divider5)
+
+def div6():
+    print(divider6)
+
+def div7():
+    print(divider7)
+
+def div8():
+    print(divider8)
+
+def customdiv():
+    print(custom_div)
+
+def area4info():
+    print("Name:", name)
+    print("Author:", author, "\nAuthor Email:", author_email)
+    print("Description:", description)
