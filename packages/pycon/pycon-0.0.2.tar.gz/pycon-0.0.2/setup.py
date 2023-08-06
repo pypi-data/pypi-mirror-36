3ab0ac053133a0a913c7b72e7b1993b740c03f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:48 +0000

    Add package pycon/0.0.2/pycon-0.0.2.tar.gz

---
+++ packages/pycon/pycon-0.0.2.tar.gz/pycon-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = 'pycon',
+    packages = ['pycon'],
+    version = '0.0.2',
+    description = 'pycon is a Python dictionary that provides attribute-style access',
+    author='Qiyang Li',
+    author_email='colin.qiyang.li@gmail.com',
+    url='https://github.com/ColinQiyangLi/pycon',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS'
+    ]
+)
