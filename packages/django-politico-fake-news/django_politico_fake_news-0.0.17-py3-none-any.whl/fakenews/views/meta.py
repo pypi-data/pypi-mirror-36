4893f3e9c1605bcb60bf32b53a23780f55223776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:32 +0000

    Add package django-politico-fake-news/0.0.17/django_politico_fake_news-0.0.17-py3-none-any.whl

---
+++ packages/django-politico-fake-news/django_politico_fake_news-0.0.17-py3-none-any.whl/fakenews/views/meta.py
@@ -0,0 +1,42 @@
+meta = {
+    "id": "",
+    "publishPath": "interactives/2018/is-this-true/",
+    "stagingUrl": "https://s3.amazonaws.com/staging.interactives.politico.com/interactives/2018/is-this-true/",
+    "url": "https://www.politico.com/interactives/2018/is-this-true/",
+    "timestamp": "2018-06-18T06:00-0400",
+    "dateline": "06/18/18 06:00 AM EDT",
+    "header": {
+        "headline": "Is This True?",
+        "subhed": "A Fake News Database",
+        "byline": "",
+        "byline_link": "",
+    },
+    "share": {
+        "fbook": {
+            "card_title": "Is This True?: A Fake News Database",
+            "card_description": "",
+            "author": "politico",
+        },
+        "twitter": {
+            "card_title": "Is This True?: A Fake News Database",
+            "share_tweet": "",
+            "card_description": "",
+            "author": "@politico",
+        },
+        "image": {
+            "url": "",
+            "alt": "<Text>",
+            "type": "image/jpeg",
+            "width": "600",
+            "height": "300",
+        },
+        "keywords": "POLITICO, News, Washington D.C.",
+    },
+    "telium": {
+        "free_paid_content": "free",
+        "site_section": "",
+        "ad_unit_section": "",
+        "content_author": "",
+        "page_name": "Is This True?: A Fake News Database",
+    },
+}
