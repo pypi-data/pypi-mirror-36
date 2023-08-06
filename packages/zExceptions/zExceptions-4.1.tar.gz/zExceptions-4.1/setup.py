977fa001223b25c5f5d4898f5a8acd9f6719e784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:51 +0000

    Add package zExceptions/4.1/zExceptions-4.1.tar.gz

---
+++ packages/zExceptions/zExceptions-4.1.tar.gz/zExceptions-4.1/setup.py
@@ -0,0 +1,57 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='zExceptions',
+    version='4.1',
+    url='https://github.com/zopefoundation/zExceptions',
+    license='ZPL 2.1',
+    description="zExceptions contains common exceptions used in Zope.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=(open('README.rst').read() + '\n' +
+                      open('CHANGES.rst').read()),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.publisher',
+        'zope.security',
+    ],
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Framework :: Zope :: 2",
+        "Framework :: Zope :: 4",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
