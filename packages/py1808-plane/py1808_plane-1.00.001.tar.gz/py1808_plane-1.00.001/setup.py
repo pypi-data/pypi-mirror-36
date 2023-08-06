aab0c5f8256019d0e64685bc4cc26babfbab4c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:10 +0000

    Add package py1808-plane/1.00.001/py1808_plane-1.00.001.tar.gz

---
+++ packages/py1808-plane/py1808_plane-1.00.001.tar.gz/py1808_plane-1.00.001/setup.py
@@ -0,0 +1,17 @@
+'''
+AUTHOR: YiFoErLiu
+VERSION: V1.0.000
+DESC: 打包当前PYTHON程序包的模块
+'''
+from distutils.core import setup
+
+
+# 具体打包信息
+setup(
+    name="py1808_plane",  # 发布的包文件名称
+    version="1.00.001",  # 发布的包的版本序号
+    description="注释说明~当前程序包的使用说明",  # 发布包的描述信息
+    author="YiFoErLiu",  # 发布包的作者信息
+    author_email="lkk199404@163.com",  # 作者联系邮箱信息
+    py_modules=["__init__","project"]  # 发布的包中的模块文件 列表
+)
