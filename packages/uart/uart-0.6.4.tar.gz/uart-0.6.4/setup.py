df3f834070faea51a73417151060fce47944cce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:41 +0000

    Add package uart/0.6.4/uart-0.6.4.tar.gz

---
+++ packages/uart/uart-0.6.4.tar.gz/uart-0.6.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import re
+import os
+
+description = 'Utility for simply creating and modifying VHDL bus slave modules'
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = 'Utility for simply creating and modifying VHDL bus slave modules'
+
+setup(
+    name='uart',
+    version='0.6.4',
+    packages=['uart'],
+    license='MIT',
+    install_requires=[
+        'curses-menu==0.5.0',
+        'docopt==0.6.2',
+        'prettytable==0.7.2',
+        'pylatexenc==1.2',
+        'pypandoc==1.4',
+      ],
+    description=description,
+    long_description=long_description,
+    entry_points={
+        'console_scripts': ['uart = uart.__main__:main'],
+    },
+    author='Ola Groettvik',
+    author_email='olagrottvik@gmail.com',
+    url='http://github.com/olagrottvik/uart',
+    keywords=['vhdl', 'bus', 'axi'],
+)
