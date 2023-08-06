6475953c877bf2930270a00b677ebe2b923ffd80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:48 +0000

    Add package edx-enterprise-data/0.2.9/edx_enterprise_data-0.2.9-py3-none-any.whl

---
+++ packages/edx-enterprise-data/edx_enterprise_data-0.2.9-py3-none-any.whl/enterprise_data/api/v0/serializers.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Serializers for enterprise api version 0 endpoint.
+"""
+from __future__ import absolute_import, unicode_literals
+
+from rest_framework import serializers
+
+from enterprise_data.models import EnterpriseEnrollment, EnterpriseUser
+
+
+class EnterpriseEnrollmentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for EnterpriseEnrollment model.
+    """
+    course_api_url = serializers.SerializerMethodField()
+
+    def get_course_api_url(self, obj):
+        """Constructs course api url"""
+        return '/enterprise/v1/enterprise-catalogs/{enterprise_id}/courses/{course_id}'.format(
+            enterprise_id=obj.enterprise_id, course_id=obj.course_id
+        )
+
+    class Meta:
+        model = EnterpriseEnrollment
+        exclude = ('created', )
+
+
+class EnterpriseUserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for EnterpriseUser model.
+    """
+
+    class Meta:
+        model = EnterpriseUser
+        exclude = ('created', )
+
+    def to_representation(self, instance):
+        representation = super(EnterpriseUserSerializer, self).to_representation(instance)
+        request = self.context.get('request')
+        extra_fields = request.query_params.get('extra_fields')
+        if extra_fields is not None:
+            if 'enrollment_count' in extra_fields:
+                representation['enrollment_count'] = instance.enrollments.count()
+        return representation
+
+
+class LearnerCompletedCoursesSerializer(serializers.Serializer):    # pylint: disable=abstract-method
+    """
+    Serializer for learner's completed courses.
+    """
+    user_email = serializers.EmailField()
+    completed_courses = serializers.IntegerField()
