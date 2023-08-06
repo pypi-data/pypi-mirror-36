73938e371969c2a372fb1412f0987fe763e06571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:49 +0000

    Add package kylinpy/1.6.0/kylinpy-1.6.0.tar.gz

---
+++ packages/kylinpy/kylinpy-1.6.0.tar.gz/kylinpy-1.6.0/kylinpy/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .kylinpy import Kylinpy
+
+__version__ = '1.6.0'
+
+__all__ = [
+    'Kylinpy',
+]
