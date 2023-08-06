efc921d2902167f9cbe9be9f75cf101b3272e3c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:42 +0000

    Add package python-awips/18.1.2/python-awips-18.1.2.tar.gz

---
+++ packages/python-awips/python-awips-18.1.2.tar.gz/python-awips-18.1.2/dynamicserialize/dstypes/com/raytheon/uf/common/dataaccess/response/AbstractResponseData.py
@@ -0,0 +1,40 @@
+##
+##
+
+# File auto-generated against equivalent DynamicSerialize Java class
+
+import abc
+from six import with_metaclass
+
+class AbstractResponseData(with_metaclass(abc.ABCMeta, object)):
+    @abc.abstractmethod
+    def __init__(self):
+        self.time = None
+        self.level = None
+        self.locationName = None
+        self.attributes = None
+
+    def getTime(self):
+        return self.time
+
+    def setTime(self, time):
+        self.time = time
+
+    def getLevel(self):
+        return self.level
+
+    def setLevel(self, level):
+        self.level = level
+
+    def getLocationName(self):
+        return self.locationName
+
+    def setLocationName(self, locationName):
+        self.locationName = locationName
+
+    def getAttributes(self):
+        return self.attributes
+
+    def setAttributes(self, attributes):
+        self.attributes = attributes
+
