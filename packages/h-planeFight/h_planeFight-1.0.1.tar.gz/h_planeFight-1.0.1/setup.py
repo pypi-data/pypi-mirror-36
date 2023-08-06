7d0823de4b9573a7e8076e6839a7d0c5fc253daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:55 +0000

    Add package h-planeFight/1.0.1/h_planeFight-1.0.1.tar.gz

---
+++ packages/h-planeFight/h_planeFight-1.0.1.tar.gz/h_planeFight-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="h_planeFight",
+    version="1.0.1",
+    description="飞机大战测试",
+    author='hyx',
+    author_email="675868749@qq.com",
+    py_modules=["planeFight","setup"],
+)
