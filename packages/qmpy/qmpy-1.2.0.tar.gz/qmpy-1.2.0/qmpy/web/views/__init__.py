f8299d803732ec3a6ce6d8eb9e9612904b67fedd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:00 +0000

    Add package qmpy/1.2.0/qmpy-1.2.0.tar.gz

---
+++ packages/qmpy/qmpy-1.2.0.tar.gz/qmpy-1.2.0/qmpy/web/views/__init__.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from materials import *
+from analysis import *
+from data import *
+from download import *
+from home import *
+from computing import *
+
+from documentation import *
+
+from qmpy.models import *
---
+++ packages/qmpy/qmpy-1.2.0.tar.gz/qmpy-1.2.0/qmpy/web/views/materials/__init__.py
@@ -0,0 +1,6 @@
+from entry import *
+from structure import *
+from composition import *
+from discovery import *
+from chem_pots import *
+from deposit import *
