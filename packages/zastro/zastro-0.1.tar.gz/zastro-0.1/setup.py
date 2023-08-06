50a3c9abcdc9a5f8d9589cb3b1f22c0a6180781c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:09 +0000

    Add package zastro/0.1/zastro-0.1.tar.gz

---
+++ packages/zastro/zastro-0.1.tar.gz/zastro-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='zastro',
+      version='0.1',
+      description='the collection of codes I use frequently for my work ',
+      url='http://github.com/zabop/zastro',
+      author='Pal Szabo',
+      author_email='szabopal96@gmail.com',
+      license='MIT',
+      packages=['zastro'],
+      zip_safe=False)
+
