c82e2a9cd7ef389148915cf9d4e14a23fec5dd3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:39 +0000

    Add package Daudmo-mandelbrot/0.1/Daudmo-mandelbrot-0.1.tar.gz

---
+++ packages/Daudmo-mandelbrot/Daudmo-mandelbrot-0.1.tar.gz/Daudmo-mandelbrot-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Daudmo-mandelbrot',
+      version='0.1',
+      description='Package with different mandelbrot implementations',
+      url='http://github.com//UiO-INF3331/INF3331-daudmo',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      include_package_data=True,
+      zip_safe=False)
