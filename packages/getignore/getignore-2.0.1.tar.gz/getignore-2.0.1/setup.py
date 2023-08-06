130c7de0e8d1ff1b2cef4d14c2a68a33f9a06042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:44 +0000

    Add package getignore/2.0.1/getignore-2.0.1.tar.gz

---
+++ packages/getignore/getignore-2.0.1.tar.gz/getignore-2.0.1/setup.py
@@ -0,0 +1,27 @@
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+
+setup(
+    name="getignore",
+    version="2.0.1",
+    description="Command line utility to download common .gitignore files",
+    long_description=readme(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="github gitignore download",
+    url="https://github.com/Jackevansevo/getignore",
+    author="Jack Evans",
+    author_email="jack@evans.gb.net",
+    license="MIT",
+    entry_points={"console_scripts": ["getignore=getignore:main"]},
+    py_modules=["getignore"],
+)
