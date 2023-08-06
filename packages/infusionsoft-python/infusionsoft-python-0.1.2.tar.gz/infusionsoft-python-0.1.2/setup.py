46940669548ffe733413aa449971e5d5a503e027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:41 +0000

    Add package infusionsoft-python/0.1.2/infusionsoft-python-0.1.2.tar.gz

---
+++ packages/infusionsoft-python/infusionsoft-python-0.1.2.tar.gz/infusionsoft-python-0.1.2/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='infusionsoft-python',
+      version='0.1.2',
+      description='API wrapper for Infusionsoft written in Python',
+      long_description=read('README.md'),
+      url='https://github.com/GearPlug/infusionsoft-python',
+      author='Yordy Gelvez',
+      author_email='yordy.gelvez@gmail.com',
+      license='GPL',
+      packages=['infusionsoft'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
