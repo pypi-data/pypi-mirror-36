58537afd1e5e27a7594310a7789a7d06c4d3935f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:57 +0000

    Add package ACEBinf/1.0.1/ACEBinf-1.0.1.tar.gz

---
+++ packages/ACEBinf/ACEBinf-1.0.1.tar.gz/ACEBinf-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+#from Cython.Build import cythonize
+
+setup(
+    name='ACEBinf',
+    version='1.0.1',
+    author="ACEnglish",
+    author_email="acenglish@gmail.com",
+    url="https://github.com/ACEnglish/acebinf",
+    packages=['acebinf',],
+    license='Unlicense',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "pyvcf>=0.6.8"
+    ],
+)
+#setup(
+    #ext_modules = cythonize("fast_vcf.pyx")
+#)
