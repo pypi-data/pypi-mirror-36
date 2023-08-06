6a1acf9317a84441abb75c30e45ca2bd26f84e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:02 +0000

    Add package nn_calculator/0.2/nn_calculator-0.2.tar.gz

---
+++ packages/nn_calculator/nn_calculator-0.2.tar.gz/nn_calculator-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nn_calculator',
+    packages=['calculator'],
+    description='Simple application to teach CI/CD',
+    version='0.2',
+    url='https://gitlab.com/MvdLinden/nn-cicd.git',
+    author='Marco van der Linden',
+    author_email='mvanderlinden@xebia.com',
+    keywords=['cicd', 'example']
+    )
