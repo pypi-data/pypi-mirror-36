e5a785655df48d437c56709c5d83f6a069caa34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:03 +0000

    Add package feiyan/1.0.7/feiyan-1.0.7.tar.gz

---
+++ packages/feiyan/feiyan-1.0.7.tar.gz/feiyan-1.0.7/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+
+from setuptools import setup, find_packages
+
+with open("feiyan/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="feiyan",
+    version="1.0.7",
+    author="jerrygaoyang",
+    author_email="917616767@qq.com",
+    description="iot feiyan package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jerrygaoyang/feiyan",
+    packages=find_packages(),
+    install_requires=["requests"]
+)
