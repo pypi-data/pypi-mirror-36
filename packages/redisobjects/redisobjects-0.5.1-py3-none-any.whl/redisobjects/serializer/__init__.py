21ce2d1760ac4bee1fd2ff0ed08f807b932da02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:18 +0000

    Add package redisobjects/0.5.1/redisobjects-0.5.1-py3-none-any.whl

---
+++ packages/redisobjects/redisobjects-0.5.1-py3-none-any.whl/redisobjects/serializer/__init__.py
@@ -0,0 +1,11 @@
+from .identity_serializer import IdentitySerializer
+from .json_serializer import JsonSerializer
+from .generic_serializer import GenericSerializer
+from .tuple_serializer import TupleSerializer
+
+__all__ = [
+    'IdentitySerializer',
+    'JsonSerializer',
+    'GenericSerializer',
+    'TupleSerializer',
+]
