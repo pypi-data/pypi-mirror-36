4866a8d3c704f124134ef755eb6dd9ecacb949f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:25 +0000

    Add package beem/0.19.56/beem-0.19.56.tar.gz

---
+++ packages/beem/beem-0.19.56.tar.gz/beem-0.19.56/beem/constants.py
@@ -0,0 +1,15 @@
+# This Python file uses the following encoding: utf-8
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+STEEM_100_PERCENT = 10000
+STEEM_1_PERCENT = 100
+STEEM_REVERSE_AUCTION_WINDOW_SECONDS_HF20 = 900
+STEEM_REVERSE_AUCTION_WINDOW_SECONDS_HF6 = 1800
+STEEM_VOTE_REGENERATION_SECONDS = 432000
+STEEM_VOTING_MANA_REGENERATION_SECONDS = 432000
+STEEM_VOTE_DUST_THRESHOLD = 50000000
+STEEM_ROOT_POST_PARENT = ''
