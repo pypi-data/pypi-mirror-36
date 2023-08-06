a40dc2cd129d7feb1aa5c029160a2f2c3ff396f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:20 +0000

    Add package TwitterImgUtil/0.1/TwitterImgUtil-0.1.tar.gz

---
+++ packages/TwitterImgUtil/TwitterImgUtil-0.1.tar.gz/TwitterImgUtil-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='TwitterImgUtil',
+      version='0.1',
+      description='include twitter images grabbing, adding labels function',
+      url='https://github.com/BrefCool/TwitterImgUtil',
+      author='bref',
+      author_email='syx525@bu.edu',
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
+      package=['TwitterImgUtil'],
+      zip_safe=False)
