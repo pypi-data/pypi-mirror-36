d594aed1c9d4d4efc3c42693bbb98a5e307acddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:42 +0000

    Add package python-pypeline/0.1.0/python-pypeline-0.1.0.tar.gz

---
+++ packages/python-pypeline/python-pypeline-0.1.0.tar.gz/python-pypeline-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "python-pypeline",
+    url = "https://github.com/akiljames83/Pypeline",
+    version = "0.1.0",
+    install_requires = ['docutils>=0.3','numpy>=0.1'],
+
+    # metadata for upload to PyPI
+    author = "Akil Hamilton",
+    author_email = "akil.james83@gmail.com",
+    description = "This is a package which will allow for easy implementation of basic Data Structures.",
+    license = "PSF",
+    keywords = "data structures implementation"
+)
