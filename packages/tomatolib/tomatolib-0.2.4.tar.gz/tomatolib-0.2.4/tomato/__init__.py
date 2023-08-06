c450fcbabc40effe06221e17a1bda0272a35409b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:29 +0000

    Add package tomatolib/0.2.4/tomatolib-0.2.4.tar.gz

---
+++ packages/tomatolib/tomatolib-0.2.4.tar.gz/tomatolib-0.2.4/tomato/__init__.py
@@ -0,0 +1,10 @@
+import json
+import ujson
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def patch_json():
+    json.__name__ = 'ujson'
+    json.dumps = ujson.dumps
+    json.loads = ujson.loads
---
+++ packages/tomatolib/tomatolib-0.2.4.tar.gz/tomatolib-0.2.4/tomato/store/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+from .mongo_client import MongoClient
+from .mysql_client import MySQLClient
+from .redis_client import RedisClient
