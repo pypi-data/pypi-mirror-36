b299d2631818826b991d4859b7f843e02ca9f6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:43 +0000

    Add package ipinfo/1.0.1/ipinfo-1.0.1-py3-none-any.whl

---
+++ packages/ipinfo/ipinfo-1.0.1-py3-none-any.whl/ipinfo/cache/interface.py
@@ -0,0 +1,17 @@
+import abc
+
+
+class CacheInterface(metaclass=abc.ABCMeta):
+    """Interface for using custom cache."""
+
+    @abc.abstractmethod
+    def __contains__(self, key):
+        pass
+
+    @abc.abstractmethod
+    def __setitem__(self, key, value):
+        pass
+
+    @abc.abstractmethod
+    def __getitem__(self, key):
+        pass
