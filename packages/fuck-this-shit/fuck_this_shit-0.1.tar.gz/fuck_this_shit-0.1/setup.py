1f584670d997a77afff3c86282366a46a35f5da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:20 +0000

    Add package fuck-this-shit/0.1/fuck_this_shit-0.1.tar.gz

---
+++ packages/fuck-this-shit/fuck_this_shit-0.1.tar.gz/fuck_this_shit-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='fuck_this_shit',
+      version='0.1',
+      description='The fuck_this_shit joke in the world',
+      url='http://github.com/storborg/fuck_this_shit',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['fuck_this_shit'],
+      zip_safe=False)
