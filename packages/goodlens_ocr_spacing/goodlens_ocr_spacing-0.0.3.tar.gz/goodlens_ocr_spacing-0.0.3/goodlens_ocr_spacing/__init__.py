a723dd95d0e1c9aaab19b4f70c8ddc87f4e7a284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:22 +0000

    Add package goodlens_ocr_spacing/0.0.3/goodlens_ocr_spacing-0.0.3.tar.gz

---
+++ packages/goodlens_ocr_spacing/goodlens_ocr_spacing-0.0.3.tar.gz/goodlens_ocr_spacing-0.0.3/goodlens_ocr_spacing/__init__.py
@@ -0,0 +1,9 @@
+from pykospacing import spacing
+
+def word_spacing(text):
+
+    result = spacing(text)
+    print(result)
+    return result
+
+
---
+++ packages/goodlens_ocr_spacing/goodlens_ocr_spacing-0.0.3.tar.gz/goodlens_ocr_spacing-0.0.3/sample/word_spacing.py
@@ -0,0 +1,3 @@
+from goodlens_ocr_spacing import word_spacing
+
+word_spacing("제품 수령 후 바로 냉장모관 해주시고 개봉한 제품은 변질의 우려가 있으가능한 빨리 섭취")
