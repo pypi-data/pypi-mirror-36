b7609d4a583867f4e276008ae02c48f034e63a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:56 +0000

    Add package rivalcfg/3.1.0/rivalcfg-3.1.0.tar.gz

---
+++ packages/rivalcfg/rivalcfg-3.1.0.tar.gz/rivalcfg-3.1.0/rivalcfg/profiles/__init__.py
@@ -0,0 +1,25 @@
+from .rival import rival
+from .rival100 import rival100
+from .rival110 import rival110
+from .rival300 import rival300
+from .rival310 import rival310
+from .rival300csgofadeedition import rival300csgofadeedition
+from .rival300csgohyperbeastedition import rival300csgohyperbeastedition
+from .rival300hpomen import rival300hpomen
+from .rival500 import rival500
+from .rival600 import rival600
+from .hotssenseiraw import hotssenseiraw
+
+mice_profiles = [
+    rival,
+    rival100,
+    rival110,
+    rival300,
+    rival300csgofadeedition,
+    rival300csgohyperbeastedition,
+    rival300hpomen,
+    rival310,
+    rival500,
+    rival600,
+    hotssenseiraw,
+]
