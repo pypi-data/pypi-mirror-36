94f36101183c13d52ebbf81b2abbd2c8c6e5aa5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:35 +0000

    Add package BaseMountRetrieve/0.3.6/BaseMountRetrieve-0.3.6.tar.gz

---
+++ packages/BaseMountRetrieve/BaseMountRetrieve-0.3.6.tar.gz/BaseMountRetrieve-0.3.6/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from BaseMountRetrieve.basemountretrieve import __version__, __author__, __email__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BaseMountRetrieve",
+    install_requires=['click', 'pandas', 'numpy'],
+    setup_requires=['click', 'pandas', 'numpy'],
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    description="Internal BFSSI package for retrieving files from BaseMount",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bfssi-forest-dussault/BaseMountRetrieve",
+    packages=setuptools.find_packages(),
+    scripts=['BaseMountRetrieve/basemountretrieve.py']
+)
