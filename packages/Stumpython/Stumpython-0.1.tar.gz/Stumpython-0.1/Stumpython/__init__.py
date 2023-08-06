99d0f6de35ce1a8ae2b62c6ce3475741bd89cc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:27 +0000

    Add package Stumpython/0.1/Stumpython-0.1.tar.gz

---
+++ packages/Stumpython/Stumpython-0.1.tar.gz/Stumpython-0.1/Stumpython/__init__.py
@@ -0,0 +1,2 @@
+from .bubble_sort import bubble_sort
+from .quick_sort import quick_sort
---
+++ packages/Stumpython/Stumpython-0.1.tar.gz/Stumpython-0.1/Stumpython/app.py
@@ -0,0 +1,22 @@
+from Stumpython import bubble_sort, insertion_sort, quick_sort, selection_sort
+
+
+if __name__ == "__main__":
+    alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    bubble_sort(alist)
+    print(alist)
+
+if __name__ == "__main__":
+    alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    selection_sort(alist)
+    print(alist)
+
+if __name__ == "__main__":
+    alist = [54,26,93,17,77,31,44,55,20]
+    insertion_sort(alist)
+    print(alist)
+
+if __name__ == "__main__":
+    alist = [54,26,93,17,77,31,44,55,20]
+    quick_sort(alist)
+    print(alist)
