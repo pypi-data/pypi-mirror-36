1ac849e2f04aa266c44dd0521dbf97b71f99f4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:32 +0000

    Add package chinesevocablist/0.3.0/chinesevocablist-0.3.0.tar.gz

---
+++ packages/chinesevocablist/chinesevocablist-0.3.0.tar.gz/chinesevocablist-0.3.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='chinesevocablist',
+      version='0.3.0',
+      description='Programmatic interface to the Chinese Vocab List',
+      url='http://github.com/kerrickstaley/Chinese-Vocab-List',
+      author='Kerrick Staley',
+      author_email='k@kerrickstaley.com',
+      license='MIT',
+      packages=['chinesevocablist'],
+      zip_safe=False,
+      install_requires=[
+        'pyyaml>=3.12',
+      ])
