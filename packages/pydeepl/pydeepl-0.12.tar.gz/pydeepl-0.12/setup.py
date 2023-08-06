55b1b9032cc163f6d61deea77ea8cded8ac92a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:21 +0000

    Add package pydeepl/0.12/pydeepl-0.12.tar.gz

---
+++ packages/pydeepl/pydeepl-0.12.tar.gz/pydeepl-0.12/setup.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+from distutils.core import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='pydeepl',
+    packages=['pydeepl'],
+    version='0.12',
+    description='A python API wrapper for DeepL translating service.',
+    long_description=readme(),
+    author='Emilio Carrión Peñalba, frinkelpi',
+    author_email='emiliok1997@gmail.com',
+    url='https://github.com/EmilioK97/pydeepl',
+    keywords=['deepl', 'API', 'translate', 'translation'],
+    classifiers=[
+      'Development Status :: 5 - Production/Stable',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+      'requests>=2.18.4',
+    ],
+)
