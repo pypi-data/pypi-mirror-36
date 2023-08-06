5426046f044d45e2c4f0a74f29089fadf3efaec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:29 +0000

    Add package masonite-dropbox-driver/0.0.4/masonite-dropbox-driver-0.0.4.tar.gz

---
+++ packages/masonite-dropbox-driver/masonite-dropbox-driver-0.0.4.tar.gz/masonite-dropbox-driver-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="masonite-dropbox-driver",
+    packages=[
+        'masonite.contrib.dropbox',
+        'masonite.contrib.dropbox.drivers',
+        'masonite.contrib.dropbox.providers',
+    ],
+    version='0.0.4',
+    install_requires=[
+        'dropbox',
+    ],
+    classifiers=[],
+    author='Bjorn Theart',
+    author_email='bjorntheart@gmail.com',
+    url='https://github.com/bjorntheart',
+    description='Dropbox upload driver for the Masonite framework',
+    keywords=['python web framework', 'python3', 'masonite', 'dropbox'],
+    include_package_data=True,
+)
