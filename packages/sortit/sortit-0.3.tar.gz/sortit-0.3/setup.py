ce1f9cfb194a849cb1c292486918cb55e3dc1b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:21 +0000

    Add package sortit/0.3/sortit-0.3.tar.gz

---
+++ packages/sortit/sortit-0.3.tar.gz/sortit-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='sortit',
+      version='0.3',
+      description='some sorting functions',
+      long_description='You\'ll probably want to talk about these over lunch, just make sure you get back to work on time.',
+      url='http://github.com/storborg/funniest',
+      author='Sortmaster X',
+      author_email='sortmasterx@sortit.com',
+      license='MIT',
+      packages=['sortit'],
+      zip_safe=False)
---
+++ packages/sortit/sortit-0.3.tar.gz/sortit-0.3/sortit/sorters_test.py
@@ -0,0 +1,3 @@
+import sortit
+
+print(sortit.bubble_sort([2, 3, 4]))
