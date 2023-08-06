a9f4e24c2d09ad3509dace11a51de6d0ae2ea8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:22 +0000

    Add package htchirp/1.0/htchirp-1.0.tar.gz

---
+++ packages/htchirp/htchirp-1.0.tar.gz/htchirp-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='htchirp',
+    version='1.0',
+    description='Pure Python Chirp client for HTCondor',
+    keywords='htcondor chirp',
+    url='https://github.com/htcondor/htchirp',
+    author='Jason Patton',
+    author_email='jpatton@cs.wisc.edu',
+    license='ASL 2.0',
+    packages=['htchirp'],
+    zip_safe=False
+    )
