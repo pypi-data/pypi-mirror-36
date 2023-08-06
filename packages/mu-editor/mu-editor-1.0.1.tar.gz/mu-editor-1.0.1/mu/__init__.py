a438952d7afee8b181737d2543a6e009a38e411f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:02 +0000

    Add package mu-editor/1.0.1/mu-editor-1.0.1.tar.gz

---
+++ packages/mu-editor/mu-editor-1.0.1.tar.gz/mu-editor-1.0.1/mu/__init__.py
@@ -0,0 +1,20 @@
+import gettext
+import locale
+import os
+
+# Configure locale and language
+# Define where the translation assets are to be found.
+localedir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'locale'))
+try:
+    # Use the operating system's locale.
+    language_code, encoding = locale.getdefaultlocale()
+    if not language_code:
+        raise ValueError()
+except (TypeError, ValueError):
+    language_code = 'en'
+# DEBUG/TRANSLATE: override the language code here (e.g. to Chinese).
+# language_code = 'zh'
+gettext.translation('mu', localedir=localedir,
+                    languages=[language_code], fallback=True).install()
+
+__version__ = '1.0.1'
