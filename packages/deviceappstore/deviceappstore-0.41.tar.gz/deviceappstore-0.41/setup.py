f06a02bf1d9a2a74ef8c1af6bae2c5092990eb2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:45 +0000

    Add package deviceappstore/0.41/deviceappstore-0.41.tar.gz

---
+++ packages/deviceappstore/deviceappstore-0.41.tar.gz/deviceappstore-0.41/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='deviceappstore',           # This is the name of your PyPI-package.
+    version='0.41',                   # Update the version number for new releases
+    scripts=['deviceappstore.py'],   # The name of your scipt, and also the command you'll be using for calling it
+    install_requires=[
+        'esptool',
+        'pyserial'
+    ],
+)
