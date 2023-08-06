f2fd441dfae3b5dde7d0e53da56eb27f75c3944a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:05 +0000

    Add package djangokarat/0.8.2/djangokarat-0.8.2.tar.gz

---
+++ packages/djangokarat/djangokarat-0.8.2.tar.gz/djangokarat-0.8.2/djangokarat/utils.py
@@ -0,0 +1,14 @@
+def has_karat_fields(instance):
+    if not instance._meta.karat_table and not instance._meta.karat_fields:
+        return False
+    return True
+
+
+def convert__all__to_fields(instance):
+    # Checks meta karat_fields for __all__ to add to fields
+    if (instance._meta.karat_table and not instance._meta.karat_fields) or instance._meta.karat_fields == '__all__':
+        instance._meta.karat_fields = [f.name for f in instance._meta.get_fields()]
+
+
+def get_karat_id_field(instance):
+    pass
