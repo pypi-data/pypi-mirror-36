5b253d46b3535df75d66147fc2ceca1959dc0e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:22 +0000

    Add package single-beat/0.3.0/single-beat-0.3.0.tar.gz

---
+++ packages/single-beat/single-beat-0.3.0.tar.gz/single-beat-0.3.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='single-beat',
+    version='0.3.0',
+    long_description=__doc__,
+    description='ensures only one instance of your process across your servers',
+    url='https://github.com/ybrs/single-beat',
+    packages=['singlebeat'],
+    zip_safe=True,
+    install_requires=[
+        'tornado>=4.2.1',
+        'redis >= 2.9.1'
+    ],
+    test_require=[
+        'psutil>=5.2.2'
+    ],
+    entry_points={
+        'console_scripts': [
+            'single-beat = singlebeat.beat:run_process',
+        ],
+    }
+)
