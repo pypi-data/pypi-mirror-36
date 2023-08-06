9f79daebd1ded2a2b8a658a1045ad100a1646c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:54 +0000

    Add package feiyan/1.0.3/feiyan-1.0.3.tar.gz

---
+++ packages/feiyan/feiyan-1.0.3.tar.gz/feiyan-1.0.3/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="feiyan",
+    version="1.0.3",
+    author="jerrygaoyang",
+    author_email="917616767@qq.com",
+    description="iot feiyan package",
+    long_description='iot feiyan package',
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    install_requires=["requests"]
+)
