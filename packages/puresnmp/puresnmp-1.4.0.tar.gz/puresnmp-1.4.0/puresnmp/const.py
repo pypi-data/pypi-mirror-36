aec61d828aa9ab810fc70aa80a57f609c3c17ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:24 +0000

    Add package puresnmp/1.4.0/puresnmp-1.4.0.tar.gz

---
+++ packages/puresnmp/puresnmp-1.4.0.tar.gz/puresnmp-1.4.0/puresnmp/const.py
@@ -0,0 +1,25 @@
+"""
+This file contains various values used to avoid magic numbers and strings in
+the application.
+"""
+# pylint: disable=too-few-public-methods
+
+
+class Version:
+    """
+    The SNMP Version identifier. This is used in the SNMP :term:`PDU`.
+    """
+
+    V2C = 0x01
+    V1 = 0x00
+
+
+class Length:
+    """
+    A simple "namespace" to avoid magic values for indefinite lengths.
+    """
+
+    INDEFINITE = "indefinite"
+
+
+MAX_VARBINDS = 2147483647  # Defined in RFC 3416
