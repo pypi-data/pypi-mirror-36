b8808c4f2dce24a7a9356a5e0e2af000d1b219b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:18 +0000

    Add package bee-django-mission/0.0.25/bee-django-mission-0.0.25.tar.gz

---
+++ packages/bee-django-mission/bee-django-mission-0.0.25.tar.gz/bee-django-mission-0.0.25/bee_django_mission/admin.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import ContentType, Line, Stage, MissionType,Mission
+# Register your models here.
+admin.site.register(ContentType)
+admin.site.register(Line)
+admin.site.register(Stage)
+admin.site.register(MissionType)
+admin.site.register(Mission)
