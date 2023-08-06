0593d2a88a9c1070e30b6f1f10f9da0f1937d51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:16 +0000

    Add package struct-lmm/0.0.23/struct-lmm-0.0.23.tar.gz

---
+++ packages/struct-lmm/struct-lmm-0.0.23.tar.gz/struct-lmm-0.0.23/struct_lmm/__init__.py
@@ -0,0 +1,18 @@
+r"""
+*********
+StructLMM
+*********
+
+Write me
+
+"""
+
+from __future__ import absolute_import as _
+
+from . import interpretation, lmm, runner, utils
+from .lmm import LMM, LMMCore, StructLMM
+from .runner import run_lmm, run_lmm_int, run_struct_lmm
+
+__version__ = '0.0.23'
+
+__all__ = ['lmm', 'runner', 'interpretation', 'utils', '__version__']
---
+++ packages/struct-lmm/struct-lmm-0.0.23.tar.gz/struct-lmm-0.0.23/struct_lmm/lmm/__init__.py
@@ -0,0 +1,11 @@
+r"""
+- :class:`.StructLMM`
+- :class:`.LMMCore`
+- :class:`.LMM`
+"""
+
+from .lmm import LMM
+from .lmm_core import LMMCore
+from .structlmm import StructLMM
+
+__all__ = ['StructLMM', 'LMMCore', 'LMM']
