8917af3b9d385a7e3097fa3324150bc93bfe63c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:10 +0000

    Add package django-smart-lists/1.0.21/django-smart-lists-1.0.21.tar.gz

---
+++ packages/django-smart-lists/django-smart-lists-1.0.21.tar.gz/django-smart-lists-1.0.21/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-smart-lists',
+    version='1.0.21',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Easy lists for django views',
+    long_description=README,
+    url='https://github.com/plecto/django-smart-lists',
+    author='Kristian Oellegaard',
+    author_email='kristian@kristian.io',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
