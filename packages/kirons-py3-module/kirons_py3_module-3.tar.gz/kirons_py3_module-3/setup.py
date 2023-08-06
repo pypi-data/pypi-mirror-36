bd832b050c45c43f10729ab2c2b546b9d70cbcf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:02 +0000

    Add package kirons-py3-module/3/kirons_py3_module-3.tar.gz

---
+++ packages/kirons-py3-module/kirons_py3_module-3.tar.gz/kirons_py3_module-3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="kirons_py3_module",
+    version="3",
+    author="KironDevCoder",
+    author_email="mojanghouse@gmail.com",
+    description="A simple module made by KironDevCoder for hard-to-program programs",
+    long_description="A simple module made by KironDevCoder for hard-to-program programs",
+    long_description_content_type="text/markdown",
+    #url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
