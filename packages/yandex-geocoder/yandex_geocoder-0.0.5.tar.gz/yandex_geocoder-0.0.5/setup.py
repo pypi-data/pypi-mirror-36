9e364418b394906d254c267bbe9e5f7aab659979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:37 +0000

    Add package yandex-geocoder/0.0.5/yandex_geocoder-0.0.5.tar.gz

---
+++ packages/yandex-geocoder/yandex_geocoder-0.0.5.tar.gz/yandex_geocoder-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    author='Nikita Sivakov',
+    author_email='sivakov512@gmail.com',
+    description=(
+        'Simple library for getting address coordinates via Yandex geocoder'),
+    install_requires=['requests'],
+    keywords='yandex geocoder geo coordinates maps api',
+    license='MIT',
+    long_description_markdown_filename='README.md',
+    name='yandex_geocoder',
+    packages=['yandex_geocoder'],
+    python_requires='>=3.5',
+    setup_requires=['setuptools-markdown'],
+    url='https://github.com/sivakov512/yandex-geocoder',
+    version='0.0.5',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
