67eea3372b8efb819348c935c291fdec1f6e1b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:32 +0000

    Add package persistent-doc/0.5.1/persistent_doc-0.5.1.tar.gz

---
+++ packages/persistent-doc/persistent_doc-0.5.1.tar.gz/persistent_doc-0.5.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(name='persistent_doc',
+      version='0.5.1',
+      description='An XML-like document with spreadsheet formulas for values and underlying persistent data structures',
+      long_description=open("readme.md").read(),
+      long_description_content_type="text/markdown",
+      url='https://github.com/asrp/persistent_doc',
+      author='asrp',
+      author_email='asrp@email.com',
+      packages=find_packages(),
+      install_requires=['pyrsistent'],
+      keywords='persistent tree document formula')
