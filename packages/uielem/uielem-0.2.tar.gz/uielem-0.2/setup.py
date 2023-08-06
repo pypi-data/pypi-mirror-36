46dca7c35d2be48c449822d226a8b34042fad153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:30 +0000

    Add package uielem/0.2/uielem-0.2.tar.gz

---
+++ packages/uielem/uielem-0.2.tar.gz/uielem-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='uielem',
+      version='0.2',
+      description='Wrapper over Tkinter for more Pythonic UI building',
+      long_description=open("README.md").read(),
+      long_description_content_type="text/markdown",
+      url='https://github.com/asrp/uielem',
+      author='asrp',
+      author_email='asrp@email.com',
+      py_modules=find_packages(),
+      keywords='wrapper tkinter pythonic')
