303ecefa366fef4e9a35f13804972c3e96836687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:04 +0000

    Add package pickle-secure/0.1.3/pickle_secure-0.1.3.tar.gz

---
+++ packages/pickle-secure/pickle_secure-0.1.3.tar.gz/pickle_secure-0.1.3/src/pickle_secure/__init__.py
@@ -0,0 +1,27 @@
+from pickle_secure.pickle_secure import (
+    HIGHEST_PROTOCOL,
+    DEFAULT_PROTOCOL,
+    PickleError,
+    PicklingError,
+    UnpicklingError,
+    dumps,
+    dump,
+    loads,
+    load,
+    Pickler,
+    Unpickler,
+)
+
+__all__ = [
+    "HIGHEST_PROTOCOL",
+    "DEFAULT_PROTOCOL",
+    "PickleError",
+    "PicklingError",
+    "UnpicklingError",
+    "dumps",
+    "dump",
+    "loads",
+    "load",
+    "Pickler",
+    "Unpickler",
+]
