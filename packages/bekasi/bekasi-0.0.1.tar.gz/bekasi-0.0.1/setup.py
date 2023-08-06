6455b4791b11fb40b02283e967be440cf3ff007c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:14 +0000

    Add package bekasi/0.0.1/bekasi-0.0.1.tar.gz

---
+++ packages/bekasi/bekasi-0.0.1.tar.gz/bekasi-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+        name="bekasi",
+        version="0.0.1",
+        author="randidwiputra",
+        author_email="randi.dwi.putra@gmail.com",
+        description="A small example package from Bekasi Indonesia",
+        url="https://github.com/randidwiputra/pip-bekasi",
+        packages=setuptools.find_packages(),
+        classifiers=(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ),
+)
