ba5275b31edaf3f40272cc8408dbfcd282fd4f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:59 +0000

    Add package lixinger-openapi/0.2.5/lixinger_openapi-0.2.5-py3-none-any.whl

---
+++ packages/lixinger-openapi/lixinger_openapi-0.2.5-py3-none-any.whl/lixinger_openapi/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+理杏仁开放平台API，非官方
+'''
+import lixinger_openapi.json
+import lixinger_openapi.data
+from lixinger_openapi.token import load_token, get_token
+
+__version__ = '0.2.5'
+
+__author__ = 'sheki lyu'
+
+name = "lixinger_openapi"
