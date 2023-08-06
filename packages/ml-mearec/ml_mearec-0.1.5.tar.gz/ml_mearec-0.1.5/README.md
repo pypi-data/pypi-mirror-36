46b90fa564c47322c5e5ee6e41e2ac41f3638568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:48 +0000

    Add package ml-mearec/0.1.5/ml_mearec-0.1.5.tar.gz

---
+++ packages/ml-mearec/ml_mearec-0.1.5.tar.gz/ml_mearec-0.1.5/README.md
@@ -0,0 +1,21 @@
+# ml_spyking_circus
+
+MountainLab processor package that wraps [MEArec](https://github.com/alejoe91/MEArec).
+
+
+## Conda installation
+
+You need to have have previously installed [mountainlab using conda](https://github.com/flatironinstitute/mountainlab-js).
+
+```
+conda install -c flatiron ml_mearec
+```
+
+## Installation without conda
+
+It is also possible to install this software without using conda. See the [MountainLab documentation](https://github.com/flatironinstitute/mountainlab-js).
+
+## Singularity usage
+
+This repository corresponds to a Singularity container. Therefore it is possible to run these processors without any installation and/or from a shared jupyter notebook.
+
---
+++ packages/ml-mearec/ml_mearec-0.1.5.tar.gz/ml_mearec-0.1.5/setup.py
@@ -0,0 +1,72 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+pkgs = setuptools.find_packages()
+print('found these packages:', pkgs)
+
+pkg_name='ml_mearec'
+
+setuptools.setup(
+    name=pkg_name,
+    version="0.1.5",
+    author="Jeremy Magland",
+    author_email="",
+    description="MountainLab wrapper for MEArec",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/magland/ml_mearec",
+    packages=pkgs,
+    package_data={
+        # Include all processor files
+        '': ['*.mp']
+    },
+    install_requires=
+    [
+	'MEAutility',
+        'h5py',
+        'numpy',
+        'click',
+        'neo',
+        'elephant',
+        'matplotlib',
+        'scipy',
+        'quantities',
+        'pyyaml',
+        'numpy',
+        'mlprocessors',
+        'mountainlab_pytools'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+    conda={
+        "build_number":0,
+        "build_script":[
+            #"python -m pip install --no-deps --ignore-installed .",
+            "python -m pip install .",
+            "CMD=\"ln -sf $SP_DIR/"+pkg_name+" `CONDA_PREFIX=$PREFIX ml-config package_directory`/"+pkg_name+"\"",
+            "echo $CMD",
+            "$CMD"
+        ],
+        "test_commands":[
+            "ml-list-processors",
+            "ml-spec -p mearec.gen_spiketrains",
+            "ml-spec -p mearec.gen_recording"
+        ],
+        "test_imports":[
+        ],
+        "requirements":[
+            "python",
+            "pip",
+            "mountainlab",
+            "mlprocessors",
+            "mountainlab_pytools",
+            "numpy",
+            "h5py"
+        ]
+    }
+)
