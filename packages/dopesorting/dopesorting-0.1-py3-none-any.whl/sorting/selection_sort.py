3558e1b049703aa39c9fadf516e9975966aac714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:23 +0000

    Add package dopesorting/0.1/dopesorting-0.1-py3-none-any.whl

---
+++ packages/dopesorting/dopesorting-0.1-py3-none-any.whl/sorting/selection_sort.py
@@ -0,0 +1,11 @@
+def selection_sort(alist):
+    for fillslot in range(len(alist) - 1, 0, -1):
+        position_of_max = 0
+        for location in range(1, fillslot + 1):
+            if alist[location] > alist[position_of_max]:
+                position_of_max = location
+
+        temp = alist[fillslot]
+        alist[fillslot] = alist[position_of_max]
+        alist[position_of_max] = temp
+
