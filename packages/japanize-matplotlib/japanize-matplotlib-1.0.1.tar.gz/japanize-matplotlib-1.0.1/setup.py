235a8b9e7d9a0096a6240fe86f151a25ebb1a170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:44 +0000

    Add package japanize-matplotlib/1.0.1/japanize-matplotlib-1.0.1.tar.gz

---
+++ packages/japanize-matplotlib/japanize-matplotlib-1.0.1.tar.gz/japanize-matplotlib-1.0.1/setup.py
@@ -0,0 +1,16 @@
+# encoding: utf8
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='japanize-matplotlib',
+    version='1.0.1',
+    description='matplotlibのフォント設定を自動で日本語化する',
+    author='uehara1414',
+    author_email='akiya.noface@gmail.com',
+    url='https://github.com/uehara1414/japanize-matplotlib',
+    packages=find_packages(),
+    package_data={
+        'fonts': ['fonts/*'],
+    },
+)
