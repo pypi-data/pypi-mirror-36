3d29e146802a01610b834d8b5ed0e7c111c89fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:58 +0000

    Add package coingate-api/0.1.1/coingate_api-0.1.1.tar.gz

---
+++ packages/coingate-api/coingate_api-0.1.1.tar.gz/coingate_api-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+
+setup(
+    name='coingate_api',
+    version='0.1.1',
+    packages=['coingate_api',],
+    install_requires=['requests>=2.10',],
+    license='Apache License version 2',
+    description='SDK of CoinGate API for Python 3+',
+    long_description=open('README.md').read(),
+    keywords='coingate coingate-api payment merchant',
+    author='Alex Shinkevich aka alexshin',
+    author_email='alex.shinkevich@gmail.com',
+    url='https://github.com/Sociumnet/coingate-api-python',
+    download_url='https://github.com/Sociumnet/coingate-api-python/archive/v0.1.1.tar.gz',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
