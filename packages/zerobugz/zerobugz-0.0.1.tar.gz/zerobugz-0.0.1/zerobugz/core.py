7a9e5256a6a4a614701ecf155a1f9b3b03e89703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:26 +0000

    Add package zerobugz/0.0.1/zerobugz-0.0.1.tar.gz

---
+++ packages/zerobugz/zerobugz-0.0.1.tar.gz/zerobugz-0.0.1/zerobugz/core.py
@@ -0,0 +1,27 @@
+import os
+import json
+import requests
+
+serverBaseUrl = "https://0bugz.com/api"
+variationsAPIUrl = "{}/variations".format(serverBaseUrl)
+headers = {
+    'Content-type': "application/json"
+}
+
+# verify=False,
+def load_test_cases(schema, count):
+    variationsPayload = None
+    with open(schema) as json_data:
+        variationsPayload = json.load(json_data)
+
+    if variationsPayload:
+        variationsResponse = requests.post(variationsAPIUrl, headers=headers, data=json.dumps(variationsPayload))
+        assert(variationsResponse.status_code >= 200 and variationsResponse.status_code <= 300)
+        # print(variationsResponse.text)
+        results = json.loads(variationsResponse.text)['result']
+        assert(len(results) == count)
+        variations = []
+        for result in results:
+            variations.append((result, ))
+        return variations
+    return None
