2fcb9cdb8d44723bf80f95ca52fbc4f6116d94f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:43 +0000

    Add package mlhub-cli/0.3.7/mlhub_cli-0.3.7-py3-none-any.whl

---
+++ packages/mlhub-cli/mlhub_cli-0.3.7-py3-none-any.whl/mlhub/PredictionModel.py
@@ -0,0 +1,25 @@
+class PredictionModel:
+    """
+    This class must be named `PredictionModel`, take no constructor params
+    and must exist in a file called `PredictionModel.py`
+    """
+    def __init__(self):
+        """
+        Do any initialization of your model here (eg. load model params from a file)
+        """
+        pass
+
+    def predict(self, x, field_names):
+        """
+        Do the model inference here.
+
+        Parameters:
+        ===========
+            x - numpy.NDArray - input to the model.
+            field_names - a list of field names.
+
+        Returns:
+        ========
+            numpy.NDArray - the result of running the model on the input `x`.
+        """
+        return x
