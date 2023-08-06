0a273bc786906d49251350ce1f1b9627ac5383f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:58 +0000

    Add package treelogger/0.1/treelogger-0.1.tar.gz

---
+++ packages/treelogger/treelogger-0.1.tar.gz/treelogger-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='treelogger',
+      version='0.1',
+      description='Create tree structures for your logs',
+      url='',
+      author='Hong Yang',
+      author_email='hong.meng.yang@outlook.com',
+      license='MIT',
+      packages=['treelogger'],
+      zip_safe=False)
