b757783d59c7b47eaa4f5e762c094276418278c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:48 +0000

    Add package pypiscout/1.3/pypiscout-1.3.tar.gz

---
+++ packages/pypiscout/pypiscout-1.3.tar.gz/pypiscout-1.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("readme.md", "r") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="pypiscout",
+    version=1.3,
+    author="Marcel Schmalzl",
+    description="Prints formatted console output tagged as info, warning or error. Usage is similar to `print()`.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/holzkohlengrill/SCout",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
