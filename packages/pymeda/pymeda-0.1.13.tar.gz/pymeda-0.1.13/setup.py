7c9137d99c53cc3920e995d9aff038b3ab6d6c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:35 +0000

    Add package pymeda/0.1.13/pymeda-0.1.13.tar.gz

---
+++ packages/pymeda/pymeda-0.1.13.tar.gz/pymeda-0.1.13/setup.py
@@ -0,0 +1,65 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/tutorials/distributing-packages/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+VERSION = '0.1.13'
+
+# Install Cython
+try:
+    import Cython
+except ModuleNotFoundError:
+    import pip
+    # For installing Cython due to pip.main removal in Pip10
+    import subprocess
+    import sys
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'Cython'])
+
+    import Cython
+
+setup(
+    name='pymeda',
+    version=VERSION,
+    description='Matrix Exploratory Data Analysis',
+    url='https://github.com/neurodata-nomads/pymeda',
+    author='Jaewon Chung',
+    author_email='j1c@jhu.edu',
+    license='MIT',
+    keywords='data visualization analysis',
+    packages=['pymeda'],  # Required
+    setup_requires=['Cython', 'cython'],
+    install_requires=[
+        'Cython',
+        'pandas',
+        'numpy',
+        'scipy',
+        'plotly==2.2.2',
+        'matplotlib',
+        'colorlover',
+        'scikit-learn',
+        'knor==0.0.1',
+    ],
+    package_data={
+        'pymeda': ['*.html'],
+    },
+    include_package_data=True,
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ])
