38a9c0cae08d2297abfdfa90a5630af36044476c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:31 +0000

    Add package UWGeodynamics/0.9.2/UWGeodynamics-0.9.2-py27-none-any.whl

---
+++ packages/UWGeodynamics/UWGeodynamics-0.9.2-py27-none-any.whl/UWGeodynamics/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM UWGeodynamics SETUP.PY
+#
+underworld_version = 'NOT INSTALLED'
+short_version = '0.9.2'
+version = '0.9.2'
+full_version = '0.9.2-dev-10ac92e(master)'
+git_revision = '10ac92ee87a6139b5a1e4ed5f11be23651103025'
+release = False
+
+if not release:
+    version = full_version
