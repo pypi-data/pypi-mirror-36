a30b95650303f4e8f29d2e3e01376fe92c048255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:45 +0000

    Add package chi2comb/0.0.2/chi2comb-0.0.2-cp37-cp37m-macosx_10_6_intel.whl

---
+++ packages/chi2comb/chi2comb-0.0.2-cp37-cp37m-macosx_10_6_intel.whl/chi2comb/__init__.py
@@ -0,0 +1,28 @@
+"""
+Combination of chi-squared distributions
+========================================
+
+Functions
+---------
+chi2comb_cdf  Function distribution of combination of chi-squared distributions.
+
+Documentation can be found at <https://github.com/limix/chi2comb-py>.
+"""
+
+from __future__ import absolute_import
+
+try:
+    from ._ffi import ffi as _
+except Exception as e:
+    msg = "\nIt is likely caused by a broken installation of this package."
+    msg += "\nPlease, make sure you have a C compiler and try to uninstall"
+    msg += "\nand reinstall the package again."
+    e.msg = e.msg + msg
+    raise e
+
+from ._chi2comb import chi2comb_cdf, ChiSquared, Info
+from ._testit import test
+
+__version__ = "0.0.2"
+
+__all__ = ["__version__", "test", "chi2comb_cdf", "ChiSquared", "Info"]
