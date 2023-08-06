69d687af916d5984fb7ab59a69dbb44bc81fe320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:14 +0000

    Add package qbase/1.0.5/qbase-1.0.5.tar.gz

---
+++ packages/qbase/qbase-1.0.5.tar.gz/qbase-1.0.5/qbase/__init__.py
@@ -0,0 +1,14 @@
+
+__all__ = (
+    "__title__", "__uri__", "__version__", "__description__",
+    "__author__", "__email__", "__license__", "__copyright__",
+)
+
+__title__ = "qbase"
+__uri__ = "https://gitlab.com/jdjshx/quanti-frame.git"
+__version__ = "1.0.5"
+__description__ = "qq based programe for fun"
+__author__ = "lijialun"
+__email__ = "cslijialun@126.com"
+__license__ = "Apache License, Version 2.0"
+__copyright__ = "Copyright"
