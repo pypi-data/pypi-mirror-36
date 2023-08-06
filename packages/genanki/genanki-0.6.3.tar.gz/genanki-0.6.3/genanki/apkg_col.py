8591ab58f7c232d7e0f088209307c5b59a2c7f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:42 +0000

    Add package genanki/0.6.3/genanki-0.6.3.tar.gz

---
+++ packages/genanki/genanki-0.6.3.tar.gz/genanki-0.6.3/genanki/apkg_col.py
@@ -0,0 +1,135 @@
+APKG_COL = r'''
+INSERT INTO col VALUES(
+    null,
+    1411124400,
+    1425279151694,
+    1425279151690,
+    11,
+    0,
+    0,
+    0,
+    '{
+        "activeDecks": [
+            1
+        ],
+        "addToCur": true,
+        "collapseTime": 1200,
+        "curDeck": 1,
+        "curModel": "1425279151691",
+        "dueCounts": true,
+        "estTimes": true,
+        "newBury": true,
+        "newSpread": 0,
+        "nextPos": 1,
+        "sortBackwards": false,
+        "sortType": "noteFld",
+        "timeLim": 0
+    }',
+    ?3,
+    '{
+        "1": {
+            "collapsed": false,
+            "conf": 1,
+            "desc": "",
+            "dyn": 0,
+            "extendNew": 10,
+            "extendRev": 50,
+            "id": 1,
+            "lrnToday": [
+                0,
+                0
+            ],
+            "mod": 1425279151,
+            "name": "Default",
+            "newToday": [
+                0,
+                0
+            ],
+            "revToday": [
+                0,
+                0
+            ],
+            "timeToday": [
+                0,
+                0
+            ],
+            "usn": 0
+        },
+        "' || ?2 || '": {
+            "collapsed": false,
+            "conf": 1,
+            "desc": "",
+            "dyn": 0,
+            "extendNew": 0,
+            "extendRev": 50,
+            "id": ' || ?2 || ',
+            "lrnToday": [
+                163,
+                2
+            ],
+            "mod": 1425278051,
+            "name": "' || ?1 || '",
+            "newToday": [
+                163,
+                2
+            ],
+            "revToday": [
+                163,
+                0
+            ],
+            "timeToday": [
+                163,
+                23598
+            ],
+            "usn": -1
+        }
+    }',
+    '{
+        "1": {
+            "autoplay": true,
+            "id": 1,
+            "lapse": {
+                "delays": [
+                    10
+                ],
+                "leechAction": 0,
+                "leechFails": 8,
+                "minInt": 1,
+                "mult": 0
+            },
+            "maxTaken": 60,
+            "mod": 0,
+            "name": "Default",
+            "new": {
+                "bury": true,
+                "delays": [
+                    1,
+                    10
+                ],
+                "initialFactor": 2500,
+                "ints": [
+                    1,
+                    4,
+                    7
+                ],
+                "order": 1,
+                "perDay": 20,
+                "separate": true
+            },
+            "replayq": true,
+            "rev": {
+                "bury": true,
+                "ease4": 1.3,
+                "fuzz": 0.05,
+                "ivlFct": 1,
+                "maxIvl": 36500,
+                "minSpace": 1,
+                "perDay": 100
+            },
+            "timer": 0,
+            "usn": 0
+        }
+    }',
+    '{}'
+);
+'''
