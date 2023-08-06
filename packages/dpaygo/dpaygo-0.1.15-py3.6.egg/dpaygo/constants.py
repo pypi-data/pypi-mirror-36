173b7b35ae7da596ae6803bcd336efa5c8a8bbbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:21 +0000

    Add package dpaygo/0.1.15/dpaygo-0.1.15-py3.6.egg

---
+++ packages/dpaygo/dpaygo-0.1.15-py3.6.egg/dpaygo/constants.py
@@ -0,0 +1,15 @@
+# This Python file uses the following encoding: utf-8
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+DPAY_100_PERCENT = 10000
+DPAY_1_PERCENT = 100
+DPAY_REVERSE_AUCTION_WINDOW_SECONDS_HF20 = 900
+DPAY_REVERSE_AUCTION_WINDOW_SECONDS_HF6 = 1800
+DPAY_VOTE_REGENERATION_SECONDS = 432000
+DPAY_VOTING_MANA_REGENERATION_SECONDS = 432000
+DPAY_VOTE_DUST_THRESHOLD = 50000000
+DPAY_ROOT_POST_PARENT = ''
