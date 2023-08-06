e6522f22af78fc37aeed231b0785228de2c006e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:18 +0000

    Add package pika-client/0.7/pika_client-0.7.tar.gz

---
+++ packages/pika-client/pika_client-0.7.tar.gz/pika_client-0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='pika_client',
+      version='0.7',
+      description='Pika Client',
+      url='https://github.com/BenjiBackslash/pika_client',
+      author='Hanan Wiener',
+      author_email='hanan888@gmail.com',
+      license='MIT',
+      packages=find_packages(exclude=['contrib', 'docs', 'test*']),
+      install_requires=['pika==0.12.0'],
+      zip_safe=False)
