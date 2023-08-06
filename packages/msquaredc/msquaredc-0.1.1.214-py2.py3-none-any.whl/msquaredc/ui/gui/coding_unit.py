7b481105b2bbb4a9ab341a953e47f402baa25231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:17 +0000

    Add package msquaredc/0.1.1.214/msquaredc-0.1.1.214-py2.py3-none-any.whl

---
+++ packages/msquaredc/msquaredc-0.1.1.214-py2.py3-none-any.whl/msquaredc/ui/gui/coding_unit.py
@@ -0,0 +1,10 @@
+import tkinter
+
+
+class Paper(tkinter.Frame):  # pragma : no cover
+    def __init__(self, master, coding, data):
+        super(Paper, self).__init__(master)
+
+
+if __name__ == "__main__":
+    Paper("config.yml", None)
