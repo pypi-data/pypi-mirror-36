3bc78f15c5a28f1f95e7ffa98f79ad5ca3ba9c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:29 +0000

    Add package python-cheatsheet/0.3.0/python_cheatsheet-0.3.0-py2.py3-none-any.whl

---
+++ packages/python-cheatsheet/python_cheatsheet-0.3.0-py2.py3-none-any.whl/python_cheatsheet/a.py
@@ -0,0 +1,38 @@
+"""
+.. module:: a
+   :platform: Unix, Windows
+   :synopsis: A is for
+
+.. moduleauthor:: Dave Dawson <davewd@me.com>
+
+
+"""
+
+def public_fn_with_googley_docstring(name, state=None):
+    """This function does something.
+
+    Args:
+       name (str):  The name to use.
+
+    Kwargs:
+       state (bool): Current state to be in.
+
+    Returns:
+       int.  The return code::
+
+          0 -- Success!
+          1 -- No good.
+          2 -- Try again.
+
+    Raises:
+       AttributeError, KeyError
+
+    A really great idea.  A way you might use me is
+
+    >>> print public_fn_with_googley_docstring(name='foo', state=None)
+    0
+
+    BTW, this always returns 0.  **NEVER** use with :class:`MyPublicClass`.
+
+    """
+    return 0
---
+++ packages/python-cheatsheet/python_cheatsheet-0.3.0-py2.py3-none-any.whl/python_cheatsheet/s.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+
+
+#String
+import string
+string.ascii_lowercase[:26]
