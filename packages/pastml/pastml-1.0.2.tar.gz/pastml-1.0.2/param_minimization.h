3d41e642c6acfd97caff57a6b028c5e2bbe8c3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:15 +0000

    Add package pastml/1.0.2/pastml-1.0.2.tar.gz

---
+++ packages/pastml/pastml-1.0.2.tar.gz/pastml-1.0.2/param_minimization.h
@@ -0,0 +1,12 @@
+//
+// Created by azhukova on 2/20/18.
+//
+
+#ifndef PASTML_PARAM_MINIMIZATION_H
+#define PASTML_PARAM_MINIMIZATION_H
+
+#include <stdbool.h>
+
+double minimize_params(Tree* s_tree, size_t num_annotations, double *parameters, char **character, bool optimize_frequencies,
+                       double scale_low, double scale_up, double epsilon_low, double epsilon_up);
+#endif //PASTML_PARAM_MINIMIZATION_H
