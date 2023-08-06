e96913b82ec01f4912f1074158a414a64989ac84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:31 +0000

    Add package pycallnumber/0.1.3/pycallnumber-0.1.3-py2.py3-none-any.whl

---
+++ packages/pycallnumber/pycallnumber-0.1.3-py2.py3-none-any.whl/pycallnumber/__meta__.py
@@ -0,0 +1,13 @@
+"""Single source for all metadata about pycallnumber."""
+
+
+__version__ = '0.1.0'
+__name__ = 'pycallnumber'
+__url__ = 'https://github.com/unt-libraries/pycallnumber'
+__description__ = 'A Python library for parsing call numbers.'
+__long_description__ = 'Visit {} for the latest documentation.'.format(__url__)
+__license__ = 'BSD'
+__author__ = 'Jason Thomale'
+__author_email__ = 'jason.thomale@unt.edu'
+__maintainer__ = 'University of North Texas Libraries'
+__keywords__ = 'python, callnumber, callnumbers, call number, call numbers'
