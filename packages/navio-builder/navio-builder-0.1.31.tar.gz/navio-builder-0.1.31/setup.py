af85bc68e13d51576ff388fb2b9d92ff6b427e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:43 +0000

    Add package navio-builder/0.1.31/navio-builder-0.1.31.tar.gz

---
+++ packages/navio-builder/navio-builder-0.1.31.tar.gz/navio-builder-0.1.31/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import navio.meta_builder
+setup(
+    name="navio-builder",
+    version=navio.meta_builder.__version__,
+    author='Navio Online OpenSource projects',
+    author_email='oss@navio.online',
+    url=navio.meta_builder.__website__,
+    packages=["navio", "navio.builder"],
+    entry_points={'console_scripts': ['nb=navio.builder:main']},
+    install_requires=['sh'],
+    license="MIT License",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Build Tools'
+    ],
+    keywords=['devops', 'build tool'],
+    description="Lightweight Python Build Tool",
+    long_description=open("README.rst").read()+"\n"+open("CHANGES.rst").read()
+)
