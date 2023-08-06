19baa539d8cec12b19ff4024bd4e1cc0616d0115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:36 +0000

    Add package skyljk/1.3.0/skyljk-1.3.0.tar.gz

---
+++ packages/skyljk/skyljk-1.3.0.tar.gz/skyljk-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name = 'skyljk',
+		version = '1.3.0',
+		py_modules = ['skyljk'],
+		author = 'ljkpython',
+		author_email = 'ljk66688@126.com',
+		url = 'http://www.headfirstlabs.com',
+		description = 'A simple printer of nested lists'
+	)
