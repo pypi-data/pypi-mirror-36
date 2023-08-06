7dd311b19719a9c7c1a7a4ca18b71073bb0c41d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:35 +0000

    Add package pyspark-flame/0.2.6/pyspark-flame-0.2.6.tar.gz

---
+++ packages/pyspark-flame/pyspark-flame-0.2.6.tar.gz/pyspark-flame-0.2.6/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='pyspark-flame',
+    version='0.2.6',
+    description='A low-overhead sampling profiler for PySpark, that outputs Flame Graphs',
+    long_description=long_description,
+    author='James Pickering',
+    author_email='james_pic@hotmail.com',
+    license='MIT',
+    url='https://github.com/jamespic/pyspark-flame',
+    download_url='https://github.com/jamespic/pyspark-flame/archive/0.2.4.tar.gz',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    scripts=['FlameGraph/flamegraph.pl'],
+    install_requires=['pyspark'],
+    setup_requires=['pypandoc'],
+    test_suite='test'
+)
