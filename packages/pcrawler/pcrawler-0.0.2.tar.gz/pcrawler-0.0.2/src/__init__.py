b0b771e23903bd4ddee09ad719f302b317d3c0cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:34 +0000

    Add package pcrawler/0.0.2/pcrawler-0.0.2.tar.gz

---
+++ packages/pcrawler/pcrawler-0.0.2.tar.gz/pcrawler-0.0.2/src/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/1 21:07
+# @Author  : ganliang
+# @File    : __init__.py.py
+# @Desc    : pcrawler爬虫程序
+
+
+# __all__ = ["main", "config", "core", "downloader", "monitor", "processor", "scheduler", "storage", "util"]
+# __all__ = ["main"]
