d684e614b359c004f983d3723f257a7d64170c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:20 +0000

    Add package envinfo/1.3.4/envinfo-1.3.4.tar.gz

---
+++ packages/envinfo/envinfo-1.3.4.tar.gz/envinfo-1.3.4/envinfo/__init__.py
@@ -0,0 +1,7 @@
+from envinfo.system import EnvInfo
+from envinfo.package import Packages
+from envinfo.drives import Drives
+
+
+__all__ = ['EnvInfo', 'Packages', 'Drives']
+# TODO: Conisider refactoring envinfo ---> synfo?
