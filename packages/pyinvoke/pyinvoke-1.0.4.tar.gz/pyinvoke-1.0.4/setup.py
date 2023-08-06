8e44882750dacdbf8b2d1b37145c7564d9f1ecda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:59 +0000

    Add package pyinvoke/1.0.4/pyinvoke-1.0.4.tar.gz

---
+++ packages/pyinvoke/pyinvoke-1.0.4.tar.gz/pyinvoke-1.0.4/setup.py
@@ -0,0 +1,18 @@
+
+import io
+import setuptools
+
+with io.open('README.md', encoding='utf8') as fp:
+  readme = fp.read()
+
+setuptools.setup(
+  name = 'pyinvoke',
+  version = '1.0.4',
+  license = 'MIT',
+  long_description = readme,
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/NiklasRosenstein/pyinvoke',
+  author = 'Niklas Rosenstein',
+  author_email = 'rosensteinniklas@gmail.com',
+  py_modules = ['pyinvoke']
+)
