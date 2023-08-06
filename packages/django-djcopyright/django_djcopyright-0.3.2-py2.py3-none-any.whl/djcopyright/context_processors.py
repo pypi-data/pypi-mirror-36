648b3b0a6c20573ad0a222fceb65cfc1af6afac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:53 +0000

    Add package django-djcopyright/0.3.2/django_djcopyright-0.3.2-py2.py3-none-any.whl

---
+++ packages/django-djcopyright/django_djcopyright-0.3.2-py2.py3-none-any.whl/djcopyright/context_processors.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# django-djcopyright
+# djcopyright/context_processors.py
+
+
+from __future__ import unicode_literals
+
+from djcopyright.utils import get_copyright
+
+
+__all__ = [
+    "djcopyright_years",
+]
+
+
+def djcopyright_years(request):
+    """
+    Return formatted copyright years string named as "COPYRIGHT_YEARS" to context.
+    Args:
+        request: (django.http.request.HttpRequest) django request instance.
+    Returns:
+        dict: dict with "COPYRIGHT_YEARS" key with value of copyright.
+    """
+
+    return {
+        "COPYRIGHT_YEARS": get_copyright(),
+    }
