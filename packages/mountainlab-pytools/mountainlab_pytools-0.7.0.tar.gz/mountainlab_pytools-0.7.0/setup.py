2023cf2554c97fe80298c411b1eb64dc0b23633f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:32 +0000

    Add package mountainlab-pytools/0.7.0/mountainlab_pytools-0.7.0.tar.gz

---
+++ packages/mountainlab-pytools/mountainlab_pytools-0.7.0.tar.gz/mountainlab_pytools-0.7.0/setup.py
@@ -0,0 +1,56 @@
+import setuptools
+
+pkg_name="mountainlab_pytools"
+
+setuptools.setup(
+    name=pkg_name,
+    version="0.7.0",
+    author="Jeremy Magland",
+    author_email="jmagland@flatironinstitute.org",
+    description="Tools for using MountainLab with python",
+    url="https://github.com/magland/mountainlab_pytools",
+    packages=setuptools.find_packages(),
+    package_data={
+        '': ['mlproc/*.js']
+    },
+    install_requires=[
+        'requests',
+        'numpy',
+        'numpydoc',
+        'ipython',
+        'vdom',
+        'ipywidgets',
+        'jp_proxy_widget'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+    conda={
+        "build_number":0,
+        "build_script":[
+            "python -m pip install vdom jp_proxy_widget",
+            "python -m pip install --no-deps --ignore-installed .",
+            "echo $CMD",
+            "$CMD"
+        ],
+        "test_commands":[
+        ],
+        "test_imports":[
+            "mountainlab_pytools",
+            "mountainlab_pytools.mlproc",
+            "mountainlab_pytools.mdaio",
+            "mountainlab_pytools.processormanager"
+        ],
+        "requirements":[
+            "python",
+            "pip",
+            "requests",
+            "numpy",
+            "numpydoc",
+            "ipython",
+            "ipywidgets"
+        ]
+    }
+)
