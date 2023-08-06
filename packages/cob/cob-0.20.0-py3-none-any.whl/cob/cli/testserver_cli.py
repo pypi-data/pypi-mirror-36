3871c5b5798f608fe2f130b4632b121a09bf045c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:51 +0000

    Add package cob/0.20.0/cob-0.20.0-py3-none-any.whl

---
+++ packages/cob/cob-0.20.0-py3-none-any.whl/cob/cli/testserver_cli.py
@@ -0,0 +1,17 @@
+import click
+import logbook
+
+from ..app import build_app
+from ..bootstrapping import ensure_project_bootstrapped
+
+_logger = logbook.Logger(__name__)
+
+
+@click.command()
+@click.option('-p', '--port', type=int, default=5000)
+@click.option('--debug/--no-debug', is_flag=True, default=True)
+def testserver(port, debug):
+    ensure_project_bootstrapped()
+    flask_app = build_app(config_overrides={'TESTING': True, 'DEBUG': debug})
+    logbook.StderrHandler(level=logbook.DEBUG).push_application()
+    flask_app.run(port=port, debug=debug)
