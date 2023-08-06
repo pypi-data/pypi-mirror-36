a7595e707f76722c513b49023f4a656b1a0f76b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:11 +0000

    Add package Fast-data-vis/0.1.dev0/Fast-data-vis-0.1.dev0.tar.gz

---
+++ packages/Fast-data-vis/Fast-data-vis-0.1.dev0.tar.gz/Fast-data-vis-0.1.dev0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='Fast-data-vis',
+    version='0.1dev',
+    packages=['fast-data-vis',],
+    license=open('LICENSE').read(),
+    long_description=open('README.md').read(),
+    url='https://pypi.org/project/Fast-data-vis/'
+)
