84f9ffba8533fa03783ab21457d3057d6b499f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:29 +0000

    Add package nester-jhy/1.0.0/nester_jhy-1.0.0.tar.gz

---
+++ packages/nester-jhy/nester_jhy-1.0.0.tar.gz/nester_jhy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from  distutils.core import setup
+
+setup(
+      name = 'nester_jhy',
+	  version = '1.0.0',
+	  py_modules = ['nester_jhy'],
+	  author = 'jianghouyan',
+	  author_email = '719828169@qq.com',
+	  url = 'http://www.baidu.com',
+	  description = 'The first abs',
+	  )
