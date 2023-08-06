e09118b772bc0cd4360fe5cca7402aae541155ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:51 +0000

    Add package astrolabe-py/0.0.20/astrolabe_py-0.0.20.tar.gz

---
+++ packages/astrolabe-py/astrolabe_py-0.0.20.tar.gz/astrolabe_py-0.0.20/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#
+# Setup script.
+#   Written by: Tom Hicks. 6/22/2018.
+#   Last Modified: Fix several errors found while creating dist.
+#
+import os
+import re
+import sys
+from setuptools import setup, find_packages
+
+# Eval the VERSION number from the version file
+with open('astrolabe_py/version.py') as vfile:
+    exec(vfile.read())
+
+# Read in the long description from the README file
+with open("README.rst", "r", encoding="UTF-8") as rm:
+    long_description = rm.read()
+
+# Build the list of scripts to be installed.
+# script_dir = 'scripts'
+# scripts = []
+# for script in os.listdir(script_dir):
+#     scripts.append(os.path.join(script_dir,script))
+
+setup(
+    name="astrolabe_py",
+    version=VERSION,
+    author="Tom Hicks",
+    author_email="hickst@email.arizona.edu",
+    description="Tools for working with Astrolabe data.",
+    license='Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AstrolabeProject/astrolabe_py",
+    packages=find_packages(exclude=['test.*']),
+    package_data ={
+        'astrolabe_py': ['data/*']
+    },
+    install_requires=[
+        # 'six>=1.10.0',
+        # 'xmlrunner>=1.7.7',
+        # 'PrettyTable>=0.7',
+        # 'numpy>=1.6.0',
+        'astropy>=3.0.3',
+        'python-irodsclient>=0.0.8'
+    ],
+    python_requires='~=3.6',
+    # scripts=scripts,
+    scripts=[ "checker", "uploader" ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Archiving',
+    ],
+    keywords='Astrolabe datastore iRods metadata uploading'
+)
