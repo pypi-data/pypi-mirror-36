716ff3c1b8efdca498b0b490c693d9c9379d93e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:10 +0000

    Add package tsumiki/0.0.3/tsumiki-0.0.3-py3-none-any.whl

---
+++ packages/tsumiki/tsumiki-0.0.3-py3-none-any.whl/tsumiki/cmd.py
@@ -0,0 +1,31 @@
+import pathlib
+
+import click
+from tsumiki import Tsumiki
+
+
+def make_output_path(input_path, ext=".html"):
+    p = pathlib.Path(input_path)
+    filename = pathlib.PurePath(input_path).stem + ext
+    return p.resolve().parent / filename
+
+
+def convert_to_html(input_path):
+    with open(input_path, "r", encoding="utf8") as f:
+        source = f.read()
+        return Tsumiki(source).html
+
+
+@click.command()
+@click.argument("input_path", type=click.Path(exists=True))
+@click.argument("output_path", required=False)
+def tsumiki(input_path, output_path=None):
+    html = convert_to_html(input_path)
+
+    if output_path:
+        p = pathlib.Path(output_path)
+    else:
+        p = make_output_path(input_path)
+    
+    with p.open("w", encoding="utf8") as f:
+        f.write(html)
---
+++ packages/tsumiki/tsumiki-0.0.3-py3-none-any.whl/tsumiki/template/columns_style.tpl
@@ -0,0 +1,20 @@
+<style>
+  .tsumiki {
+      width: 100%;
+  }
+  .tsumiki .columns{{ column_count }} {
+    -webkit-columns: {{ column_count }};
+    -moz-columns: {{ column_count }};
+    -ms-columns: {{ column_count }};
+    columns: {{ column_count }};
+    display: inline-flex;
+    align-items: top;
+    width: 100%;
+  }
+
+{% for col in range(column_count) %}
+  .tsumiki #col{{ col }} {
+      width: calc(100% / {{ column_count }});
+  }
+{% endfor %}
+</style>
