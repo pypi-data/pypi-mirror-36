80edf87f34b2cc0cbf0099b5757892a886ac3678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:59 +0000

    Add package twine/1.12.0/twine-1.12.0.tar.gz

---
+++ packages/twine/twine-1.12.0.tar.gz/twine-1.12.0/setup.py
@@ -0,0 +1,93 @@
+# Copyright 2018 Donald Stufft and individual contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from setuptools import setup
+
+import sys
+
+import twine
+
+
+blake2_requires = []
+
+if sys.version_info[:2] < (3, 6):
+    blake2_requires += [
+        "pyblake2",
+    ]
+
+
+setup(
+    name=twine.__title__,
+    version=twine.__version__,
+
+    description=twine.__summary__,
+    long_description=open("README.rst").read(),
+    license=twine.__license__,
+    url=twine.__uri__,
+    project_urls={
+        'Packaging tutorial': 'https://packaging.python.org/tutorials/distributing-packages/',
+        'Twine documentation': 'https://twine.readthedocs.io/en/latest/',
+        'Twine source': 'https://github.com/pypa/twine/',
+    },
+
+    author=twine.__author__,
+    author_email=twine.__email__,
+
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+
+    packages=["twine", "twine.commands"],
+
+    entry_points={
+        "twine.registered_commands": [
+            "check = twine.commands.check:main",
+            "upload = twine.commands.upload:main",
+            "register = twine.commands.register:main",
+        ],
+        "console_scripts": [
+            "twine = twine.__main__:main",
+        ],
+    },
+
+    install_requires=[
+        "pkginfo >= 1.4.2",
+        "readme_renderer >= 21.0",
+        "requests >= 2.5.0, != 2.15, != 2.16",
+        "requests-toolbelt >= 0.8.0",
+        "setuptools >= 0.7.0",
+        "tqdm >= 4.14",
+    ],
+    extras_require={
+        'with-blake2': blake2_requires,
+        'keyring': [
+            'keyring',
+        ],
+    },
+)
---
+++ packages/twine/twine-1.12.0.tar.gz/twine-1.12.0/tests/test_repository.py
@@ -0,0 +1,142 @@
+# Copyright 2015 Ian Cordasco
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import requests
+
+from twine import repository
+from twine.utils import DEFAULT_REPOSITORY
+
+import pretend
+
+
+def test_gpg_signature_structure_is_preserved():
+    data = {
+        'gpg_signature': ('filename.asc', 'filecontent'),
+    }
+
+    tuples = repository.Repository._convert_data_to_list_of_tuples(data)
+    assert tuples == [('gpg_signature', ('filename.asc', 'filecontent'))]
+
+
+def test_content_structure_is_preserved():
+    data = {
+        'content': ('filename', 'filecontent'),
+    }
+
+    tuples = repository.Repository._convert_data_to_list_of_tuples(data)
+    assert tuples == [('content', ('filename', 'filecontent'))]
+
+
+def test_iterables_are_flattened():
+    data = {
+        'platform': ['UNKNOWN'],
+    }
+
+    tuples = repository.Repository._convert_data_to_list_of_tuples(data)
+    assert tuples == [('platform', 'UNKNOWN')]
+
+    data = {
+        'platform': ['UNKNOWN', 'ANOTHERPLATFORM'],
+    }
+
+    tuples = repository.Repository._convert_data_to_list_of_tuples(data)
+    assert tuples == [('platform', 'UNKNOWN'),
+                      ('platform', 'ANOTHERPLATFORM')]
+
+
+def test_set_client_certificate():
+    repo = repository.Repository(
+        repository_url=DEFAULT_REPOSITORY,
+        username='username',
+        password='password',
+    )
+
+    assert repo.session.cert is None
+
+    repo.set_client_certificate(('/path/to/cert', '/path/to/key'))
+    assert repo.session.cert == ('/path/to/cert', '/path/to/key')
+
+
+def test_set_certificate_authority():
+    repo = repository.Repository(
+        repository_url=DEFAULT_REPOSITORY,
+        username='username',
+        password='password',
+    )
+
+    assert repo.session.verify is True
+
+    repo.set_certificate_authority('/path/to/cert')
+    assert repo.session.verify == '/path/to/cert'
+
+
+def test_make_user_agent_string():
+    repo = repository.Repository(
+        repository_url=DEFAULT_REPOSITORY,
+        username='username',
+        password='password',
+    )
+
+    assert 'User-Agent' in repo.session.headers
+
+    user_agent = repo.session.headers['User-Agent']
+    assert 'twine/' in user_agent
+    assert 'requests/' in user_agent
+    assert 'requests-toolbelt/' in user_agent
+    assert 'pkginfo/' in user_agent
+    assert 'setuptools/' in user_agent
+
+
+def response_with(**kwattrs):
+    resp = requests.Response()
+    for attr, value in kwattrs.items():
+        if hasattr(resp, attr):
+            setattr(resp, attr, value)
+
+    return resp
+
+
+def test_package_is_uploaded_404s():
+    repo = repository.Repository(
+        repository_url=DEFAULT_REPOSITORY,
+        username='username',
+        password='password',
+    )
+    repo.session = pretend.stub(
+        get=lambda url, headers: response_with(status_code=404)
+    )
+    package = pretend.stub(
+        safe_name='fake',
+        metadata=pretend.stub(version='2.12.0'),
+    )
+
+    assert repo.package_is_uploaded(package) is False
+
+
+def test_package_is_uploaded_200s_with_no_releases():
+    repo = repository.Repository(
+        repository_url=DEFAULT_REPOSITORY,
+        username='username',
+        password='password',
+    )
+    repo.session = pretend.stub(
+        get=lambda url, headers: response_with(status_code=200,
+                                               _content=b'{"releases": {}}',
+                                               _content_consumed=True),
+    )
+    package = pretend.stub(
+        safe_name='fake',
+        metadata=pretend.stub(version='2.12.0'),
+    )
+
+    assert repo.package_is_uploaded(package) is False
