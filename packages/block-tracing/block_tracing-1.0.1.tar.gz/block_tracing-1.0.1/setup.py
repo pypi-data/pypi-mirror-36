9565f8987195e4e4eaf00971f85f32394462744c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:55 +0000

    Add package block-tracing/1.0.1/block_tracing-1.0.1.tar.gz

---
+++ packages/block-tracing/block_tracing-1.0.1.tar.gz/block_tracing-1.0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="block_tracing",
+    version='1.0.1',
+    author="Rian Hunter",
+    author_email="rian@alum.mit.edu",
+    description="Protect process memory",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/rianhunter/block_tracing',
+    license="MIT",
+    py_modules=["block_tracing"],
+)
