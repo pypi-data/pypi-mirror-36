be6ca42390648c1f049f94ebb2537ffbb6502278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:20 +0000

    Add package nonechucks/0.1/nonechucks-0.1.tar.gz

---
+++ packages/nonechucks/nonechucks-0.1.tar.gz/nonechucks-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(name='nonechucks',
+      version='0.1',
+      url='https://github.com/msamogh/nonechucks',
+      license='MIT',
+      author='Amogh Mannekote',
+      author_email='msamogh@gmail.com',
+      description="""NoneChucks is a library for PyTorch that allows you to drop samples from your data and ensures that your Datasets and Transforms do not fail because of a few samples that are not parseable.""",
+      packages=find_packages(),
+      long_description=open('README.md').read(),
+      zip_safe=False)
