6bf77ee886fae65a91893217e3e3a530368f2a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:09 +0000

    Add package json-placeholder-client/1.0.0/json_placeholder_client-1.0.0.tar.gz

---
+++ packages/json-placeholder-client/json_placeholder_client-1.0.0.tar.gz/json_placeholder_client-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='json_placeholder_client',
+    version='1.0.0',
+    description='Client for JSON-Placeholder',
+    install_requires=['requests'],
+    packages=['json_placeholder']
+)
