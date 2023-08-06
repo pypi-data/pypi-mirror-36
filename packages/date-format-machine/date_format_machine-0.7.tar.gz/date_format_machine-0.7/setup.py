04fac3679babc65d9f750600f1197cc2585f55ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:26 +0000

    Add package date-format-machine/0.7/date_format_machine-0.7.tar.gz

---
+++ packages/date-format-machine/date_format_machine-0.7.tar.gz/date_format_machine-0.7/setup.py
@@ -0,0 +1,30 @@
+from __future__ import with_statement
+from setuptools import setup
+
+setup(
+    name='date_format_machine',
+    version='0.7',
+    description='Tool for manipulating date strings',
+    url='https://github.com/ryantownshend/date_format_machine',
+    author='Ryan Townshend',
+    author_email='citizen.townshend@gmail.com',
+    install_requires=[
+        'click>=7.0',
+        'click-log>=0.3.2',
+        'pendulum>=2.0.3',
+        'tabulate>=0.8.2',
+        'PyYAML>=3.13',
+    ],
+    py_modules=['dfm'],
+    packages=['dfm'],
+    package_data={
+        'dfm':
+            ['dfm_formats.yaml']
+    },
+    entry_points={
+        'console_scripts': [
+            'dfm = dfm.dfm:cli',
+            'date_format_machine = dfm.dfm:cli',
+        ],
+    },
+)
