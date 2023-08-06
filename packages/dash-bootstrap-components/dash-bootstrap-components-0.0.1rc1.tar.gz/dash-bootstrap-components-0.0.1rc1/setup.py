b0a50fa58b922d0092e8a8a9a9b2934c8b77d595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:10 +0000

    Add package dash-bootstrap-components/0.0.1rc1/dash-bootstrap-components-0.0.1rc1.tar.gz

---
+++ packages/dash-bootstrap-components/dash-bootstrap-components-0.0.1rc1.tar.gz/dash-bootstrap-components-0.0.1rc1/setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import find_packages, setup
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+
+def _get_version():
+    """ Get version by parsing _version programmatically """
+    version_ns = {}
+    with open(os.path.join(HERE, "dash_bootstrap_components", "_version.py")) as f:
+        exec(f.read(), {}, version_ns)
+    version = version_ns["__version__"]
+    return version
+
+
+setup(
+    name="dash-bootstrap-components",
+    version=_get_version(),
+    description="""
+Bootstrap themed components for use in Plotly Dash
+    """,
+    author="ASI Data Science",
+    author_email="engineering@asidatascience.com",
+    url="https://www.asidatascience.com/",
+    packages=find_packages(),
+    install_requires=["dash"],
+)
