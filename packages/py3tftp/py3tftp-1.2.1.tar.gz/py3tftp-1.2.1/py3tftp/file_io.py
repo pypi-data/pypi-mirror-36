011ef99e5188b641916359ea96550ebff5da778e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:43 +0000

    Add package py3tftp/1.2.1/py3tftp-1.2.1.tar.gz

---
+++ packages/py3tftp/py3tftp-1.2.1.tar.gz/py3tftp-1.2.1/py3tftp/file_io.py
@@ -0,0 +1,94 @@
+import os
+import os.path as opath
+from .netascii import Netascii
+
+
+def sanitize_fname(fname):
+    """
+    Ensures that fname is a path under the current working directory.
+    """
+    root_dir = os.getcwd()
+    return opath.join(
+        bytes(root_dir, encoding='ascii'),
+        opath.normpath(
+            b'/' + fname).lstrip(b'/'))
+
+
+class FileReader(object):
+    """
+    A wrapper around a regular file that implements:
+    - read_chunk - for closing the file when bytes read is
+      less than chunk_size.
+    - finished - for easier notifications
+    interfaces.
+    When it goes out of scope, it ensures the file is closed.
+    """
+
+    def __init__(self, fname, chunk_size=0, mode=None):
+        self.fname = sanitize_fname(fname)
+        self.chunk_size = chunk_size
+        self._f = None
+        self._f = self._open_file()
+        self.finished = False
+
+        if mode == b'netascii':
+            self._f = Netascii(self._f)
+
+    def _open_file(self):
+        return open(self.fname, 'rb')
+
+    def file_size(self):
+        return os.stat(self.fname).st_size
+
+    def read_chunk(self, size=None):
+        size = size or self.chunk_size
+        if self.finished:
+            return b''
+
+        data = self._f.read(size)
+
+        if not data or (size > 0 and len(data) < size):
+            self._f.close()
+            self.finished = True
+
+        return data
+
+    def __del__(self):
+        if self._f and not self._f.closed:
+            self._f.close()
+
+
+class FileWriter(object):
+    """
+    Wrapper around a regular file that implements:
+    - write_chunk - for closing the file when bytes written
+      is less than chunk_size.
+    When it goes out of scope, it ensures the file is closed.
+    """
+    def __init__(self, fname, chunk_size, mode=None):
+        self.fname = sanitize_fname(fname)
+        self.chunk_size = chunk_size
+        self._f = None
+        self._f = self._open_file()
+
+        if mode == b'netascii':
+            self._f = Netascii(self._f)
+
+    def _open_file(self):
+        return open(self.fname, 'xb')
+
+    def _flush(self):
+        if self._f:
+            self._f.flush()
+
+    def write_chunk(self, data):
+        bytes_written = self._f.write(data)
+
+        if not data or len(data) < self.chunk_size:
+            self._f.close()
+
+        return bytes_written
+
+    def __del__(self):
+        if self._f and not self._f.closed:
+            self._f.close()
