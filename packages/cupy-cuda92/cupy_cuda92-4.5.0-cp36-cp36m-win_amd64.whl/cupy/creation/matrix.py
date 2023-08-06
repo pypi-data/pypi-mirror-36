fbe27482d8009c6b0ffcbbc26d2dc28a51ab0cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:19 +0000

    Add package cupy-cuda92/4.5.0/cupy_cuda92-4.5.0-cp36-cp36m-win_amd64.whl

---
+++ packages/cupy-cuda92/cupy_cuda92-4.5.0-cp36-cp36m-win_amd64.whl/cupy/creation/matrix.py
@@ -0,0 +1,78 @@
+import numpy
+
+import cupy
+
+
+def diag(v, k=0):
+    """Returns a diagonal or a diagonal array.
+
+    Args:
+        v (array-like): Array or array-like object.
+        k (int): Index of diagonals. Zero indicates the main diagonal, a
+            positive value an upper diagonal, and a negative value a lower
+            diagonal.
+
+    Returns:
+        cupy.ndarray: If ``v`` indicates a 1-D array, then it returns a 2-D
+        array with the specified diagonal filled by ``v``. If ``v`` indicates a
+        2-D array, then it returns the specified diagonal of ``v``. In latter
+        case, if ``v`` is a :class:`cupy.ndarray` object, then its view is
+        returned.
+
+    .. seealso:: :func:`numpy.diag`
+
+    """
+    if isinstance(v, cupy.ndarray):
+        ndim = v.ndim
+    else:
+        ndim = numpy.ndim(v)
+        if ndim == 1:
+            v = cupy.array(v)
+        if ndim == 2:
+            # to save bandwidth, don't copy non-diag elements to GPU
+            v = numpy.array(v)
+
+    if ndim == 1:
+        size = v.size + abs(k)
+        ret = cupy.zeros((size, size), dtype=v.dtype)
+        ret.diagonal(k)[:] = v
+        return ret
+    elif ndim == 2:
+        return cupy.array(v.diagonal(k))
+    else:
+        raise ValueError("Input must be 1- or 2-d.")
+
+
+def diagflat(v, k=0):
+    """Creates a diagonal array from the flattened input.
+
+    Args:
+        v (array-like): Array or array-like object.
+        k (int): Index of diagonals. See :func:`cupy.diag` for detail.
+
+    Returns:
+        cupy.ndarray: A 2-D diagonal array with the diagonal copied from ``v``.
+
+    """
+    if numpy.isscalar(v):
+        v = numpy.asarray(v)
+
+    return cupy.diag(v.ravel(), k)
+
+
+# TODO(okuta): Implement tri
+
+
+# TODO(okuta): Implement tril
+
+
+# TODO(okuta): Implement triu
+
+
+# TODO(okuta): Implement vander
+
+
+# TODO(okuta): Implement mat
+
+
+# TODO(okuta): Implement bmat
