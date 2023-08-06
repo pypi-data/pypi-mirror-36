fce9088905a828bcce9bc9b7d3623c2bb4e0987a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:59 +0000

    Add package OLCTools/0.4.9/OLCTools-0.4.9.tar.gz

---
+++ packages/OLCTools/OLCTools-0.4.9.tar.gz/OLCTools-0.4.9/accessoryFunctions/metadataprinter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import json
+import os
+__author__ = 'adamkoziol'
+
+
+class MetadataPrinter(object):
+
+    def printmetadata(self):
+        # Iterate through each sample in the analysis
+        for sample in self.metadata:
+            if type(sample.general.fastqfiles) is list:
+                # Set the name of the json file
+                jsonfile = os.path.join(sample.general.outputdirectory, '{}_metadata.json'.format(sample.name))
+                try:
+                    # Open the metadata file to write
+                    with open(jsonfile, 'w') as metadatafile:
+                        # Write the json dump of the object dump to the metadata file
+                        json.dump(sample.dump(), metadatafile, sort_keys=True, indent=4, separators=(',', ': '))
+                except IOError:
+                    # Print useful information in case of an error
+                    print(sample.name, sample.datastore)
+                    raise
+
+    def __init__(self, inputobject):
+        try:
+            self.metadata = inputobject.runmetadata.samples
+        except AttributeError:
+            try:
+                self.metadata = inputobject.metadata.samples
+            except AttributeError:
+                try:
+                    self.metadata = inputobject.metadata
+                except AttributeError:
+                    self.metadata = inputobject.runmetadata
