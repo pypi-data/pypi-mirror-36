eb31a35d3e5f654047f9588f5af342f683fdcfe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:47 +0000

    Add package TextToOwO/v1.1.2/TextToOwO-v1.1.2.tar.gz

---
+++ packages/TextToOwO/TextToOwO-v1.1.2.tar.gz/TextToOwO-v1.1.2/TextToOwO/owo.py
@@ -0,0 +1,33 @@
+import re
+import random
+
+def last_replace(s, old, new):
+    li = s.rsplit(old, 1)
+    return new.join(li)
+
+def text_to_owo(text):
+    """ Converts your text to OwO """
+    smileys = ['(・`ω´・)', ';;w;;', '^w^', '>w<', 'UwU']
+    vowels = ['a','e','i','o','u','A','E','I','O','U']
+    
+    text = text.replace('L', 'W').replace('l', 'w')
+    text = text.replace('R', 'W').replace('r', 'w')
+    
+    while '!!' in text:
+        text = text.replace('!!', '!')
+    while '??' in text:
+        text = text.replace('??', '?')
+        
+    text = last_replace(text, '!', ' {}'.format(random.choice(smileys)))
+    text = last_replace(text, '?', '? owo')
+    text = last_replace(text, '...', ' UwU')
+    text = last_replace(text, '.', '. {}'.format(random.choice(smileys)))
+    text = re.sub('\s\s+' , ' ', text)
+    
+    for v in vowels:
+        if 'n{}'.format(v) in text:
+            text = text.replace('n{}'.format(v), 'ny{}'.format(v))
+        if 'N{}'.format(v) in text:
+            text = text.replace('N{}'.format(v), 'N{}{}'.format('Y' if v.isupper() else 'y', v))
+            
+    return text
---
+++ packages/TextToOwO/TextToOwO-v1.1.2.tar.gz/TextToOwO-v1.1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'TextToOwO',
+    packages = ['TextToOwO'],
+    version = 'v1.1.2',
+    description = 'OwO',
+    author = 'Zenrac',
+    author_email = 'zenrac@outlook.fr',
+    url = 'https://github.com/Zenrac/OwO-Translator/',
+    download_url = 'https://github.com/Zenrac/TextToOwO/archive/v1.1.2.tar.gz',
+    keywords = ['OwO', 'Convewtew'],
+    classifiers = [],
+)
