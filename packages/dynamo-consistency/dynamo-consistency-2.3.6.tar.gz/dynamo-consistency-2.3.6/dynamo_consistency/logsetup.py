4c7586ad3cb6239313f6db4ccf7b3dffa1c44b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:20 +0000

    Add package dynamo-consistency/2.3.6/dynamo-consistency-2.3.6.tar.gz

---
+++ packages/dynamo-consistency/dynamo-consistency-2.3.6.tar.gz/dynamo-consistency-2.3.6/dynamo_consistency/logsetup.py
@@ -0,0 +1,51 @@
+"""
+The module that sets up logging for us
+"""
+
+import os
+import logging
+
+from .parser import OPTS as opts
+
+LOG_FORMAT = '%(asctime)s:%(levelname)s:%(name)s: %(message)s'
+
+
+if opts.DEBUG:
+    logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
+elif opts.INFO:
+    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+else:
+    logging.basicConfig(format=LOG_FORMAT)
+
+
+def change_logfile(*filenames):
+    """
+    Changes the output file of all of the loggers.
+    Creates any directories that are needed to hold the logs.
+
+    :param filenames: The files to write new logs to
+    """
+
+    new_hldrs = []
+
+    for name in filenames:
+        logdir = os.path.dirname(name)
+
+        if not os.path.exists(logdir):
+            os.makedirs(logdir)
+
+        fhdl = logging.FileHandler(name, 'a')
+        fhdl.setFormatter(logging.Formatter(LOG_FORMAT))
+
+        new_hldrs.append(fhdl)
+
+    for logger in logging.Logger.manager.loggerDict.values():
+        if not isinstance(logger, logging.Logger):
+            continue
+
+        hdlr_copy = list(logger.handlers)
+        for hdlr in hdlr_copy:
+            logger.removeHandler(hdlr)
+
+        for fhdl in new_hldrs:
+            logger.addHandler(fhdl)
