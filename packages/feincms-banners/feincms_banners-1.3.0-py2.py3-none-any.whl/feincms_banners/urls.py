594fdb9fd5b40a2d5db99e5a898ea1fe1ba345a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:41 +0000

    Add package feincms-banners/1.3.0/feincms_banners-1.3.0-py2.py3-none-any.whl

---
+++ packages/feincms-banners/feincms_banners-1.3.0-py2.py3-none-any.whl/feincms_banners/urls.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.conf.urls import url
+
+from feincms_banners import views
+
+
+urlpatterns = [
+    url(r"^b/c/(?P<code>[^/]+)/$", views.click, name="banner_click"),
+    url(r"^b/i/(?P<code>[^/]+)/$", views.impression, name="banner_impression"),
+]
