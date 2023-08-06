bd1c3bf23c4e9a855461134caeefac9e2f465f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:29 +0000

    Add package ex_package1/0.10/ex_package1-0.10.tar.gz

---
+++ packages/ex_package1/ex_package1-0.10.tar.gz/ex_package1-0.10/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+from distutils.core import setup
+setup(name='ex_package1',
+      version='0.10',
+      description='1st prog',
+	  url='https://www.python.org/lavanya123/ex_packagev0.10',
+      author='lavanya123',
+      author_email='lavanyapavuluri@123.com',
+      license='MIT',
+	  #packages=['ex_package'],
+	  packages=find_packages(),
+      zip_safe=False)
+     
+
+
