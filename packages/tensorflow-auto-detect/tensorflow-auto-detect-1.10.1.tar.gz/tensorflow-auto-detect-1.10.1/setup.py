25580c7a769eaf1797404b535fd186ebb86e82c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:29 +0000

    Add package tensorflow-auto-detect/1.10.1/tensorflow-auto-detect-1.10.1.tar.gz

---
+++ packages/tensorflow-auto-detect/tensorflow-auto-detect-1.10.1.tar.gz/tensorflow-auto-detect-1.10.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import logging
+import setuptools
+
+import packaging_utils
+from tfdetect import pkg
+
+log = logging.getLogger(__name__)
+
+_tf_pip_name = pkg.detect_tensorflow_package()
+log.warn('Installing tensorflow as detected: %r', _tf_pip_name)
+
+with open('README.rst', 'r') as fh:
+    readme = fh.read()
+
+_conf = dict(
+    name='tensorflow-auto-detect',
+    summary=
+    'Automatically install CPU or GPU tensorflow determined by looking for a CUDA installation.',
+    author='Trevor Joynson',
+    author_email='github@skywww.net',
+    long_description=readme,
+    license='GPL',
+    packages=setuptools.find_packages(),
+    version=pkg.get_version(),
+    install_requires=[_tf_pip_name],
+    keywords=['tensorflow', 'wtf'],
+    classifier=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
+
+_conf.update(packaging_utils.setup_requirements())
+
+if __name__ == '__main__':
+    setuptools.setup(**_conf)
