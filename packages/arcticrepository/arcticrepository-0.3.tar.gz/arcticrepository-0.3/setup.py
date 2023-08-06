8526b33af8575679572fa670e73d5839657721e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:11 +0000

    Add package arcticrepository/0.3/arcticrepository-0.3.tar.gz

---
+++ packages/arcticrepository/arcticrepository-0.3.tar.gz/arcticrepository-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='arcticrepository',
+      version='0.3',
+      description='',
+      url='',
+      author='Tim',
+      author_email='tswatson123@gmail.com',
+      license='MIT',
+      install_requires=[
+        "arctic==1.68.0",
+        "pandas== 0.23.4",
+        "requests"
+      ],
+      packages=['arcticrepository'])
