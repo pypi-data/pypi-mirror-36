f96476803bc44131bf9bc6a32af4fe86d8b6c05d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:29 +0000

    Add package mailcomposer/0.2.2/mailcomposer-0.2.2-py2.py3-none-any.whl

---
+++ packages/mailcomposer/mailcomposer-0.2.2-py2.py3-none-any.whl/mailcomposer/dummy.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""Dummy MailComposer interface."""
+
+# For Python 2 compatibility
+from __future__ import print_function
+
+import os
+import sys
+
+from .base import BaseMailComposer
+from .exceptions import MailComposerError
+
+
+class DummyMailComposer(BaseMailComposer):
+    """Dummy MailComposer interface.
+
+    This class is provided mainly for testing, and as a fallback in case
+    no other interfaces are available. Calling display() will print the
+    message to stdout.
+    """
+
+    __slots__ = []
+
+    def display(self, blocking=True):
+        """Print the message to stdout."""
+
+        print(str(self))
