37288defd2f87ea54997e74c8267a6c1c735a0b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:58 +0000

    Add package comparator/0.3.0/comparator-0.3.0.tar.gz

---
+++ packages/comparator/comparator-0.3.0.tar.gz/comparator-0.3.0/comparator/db/__init__.py
@@ -0,0 +1,5 @@
+from comparator.db.bigquery import BigQueryDb
+from comparator.db.postgres import PostgresDb
+from comparator.db.redshift import RedshiftDb
+
+__all__ = [BigQueryDb, PostgresDb, RedshiftDb]
