4f4897ac2101dfaa9ab7dd08457f3301820e4565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:29 +0000

    Add package django-simditor/0.0.15/django-simditor-0.0.15.tar.gz

---
+++ packages/django-simditor/django-simditor-0.0.15.tar.gz/django-simditor-0.0.15/setup.py
@@ -0,0 +1,40 @@
+"""setup.py"""
+# -*- coding: utf-8 -*-
+#/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+VERSION = '0.0.15'
+LONG_DESCRIPTION = open('intro.rst', 'r').read()
+
+INSTALL_REQUIRES = [
+    'Django',
+    'pillow'
+]
+
+setup(
+    name='django-simditor',
+    version=VERSION,
+    description='Django admin Simditor integration.',
+    long_description=LONG_DESCRIPTION,
+    author='Silence',
+    author_email='istommao@gmail.com',
+    url='https://github.com/istommao/django-simditor',
+    zip_safe=False,
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(exclude=[".DS_Store"]),
+    keywords='Django admin Simditor integration!',
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ]
+)
