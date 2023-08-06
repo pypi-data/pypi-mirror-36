5f61bc64c298d822866c8e0c411a01b52eb79199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:29 +0000

    Add package sorting-limo/0.1/sorting_limo-0.1.tar.gz

---
+++ packages/sorting-limo/sorting_limo-0.1.tar.gz/sorting_limo-0.1/README.md
@@ -0,0 +1,7 @@
+This Python package for sorting. Constaints 4 differents ways of sorting a list in python.
+    -Insertion sort
+    -Bubble sort
+    -Quick sort
+    -Selection sort
+
+Enjoy
---
+++ packages/sorting-limo/sorting_limo-0.1.tar.gz/sorting_limo-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sorting_limo',
+    version='0.1',
+    description='4 differents ways of sorting_limo lists',
+    author='Sebastià L.',
+    license='Sebastià L',
+    packages=['sorting_limo'],
+    zip_safe=False,
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
