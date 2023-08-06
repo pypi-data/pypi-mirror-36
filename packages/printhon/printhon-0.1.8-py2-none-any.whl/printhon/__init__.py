e3e3164a06eae1ca13abe9f1c6879dcdb01e351c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:16 +0000

    Add package printhon/0.1.8/printhon-0.1.8-py2-none-any.whl

---
+++ packages/printhon/printhon-0.1.8-py2-none-any.whl/printhon/__init__.py
@@ -0,0 +1,8 @@
+# Copyright Ryota Natsume 2018. MIT license, see LICENSE file.
+from .color import printb, printy, printg, printr, b, y, g, r
+from .namevalue import printnv, nv, line, p
+
+color = 'black'
+
+__version__ = '0.1.8'
+
---
+++ packages/printhon/printhon-0.1.8-py2-none-any.whl/printhon/color.py
@@ -0,0 +1,31 @@
+def printb(mes):
+    # blue
+    print("\033[0;34m" + str(mes) + "\033[0;39m")
+
+def printy(mes):
+    # yellow
+    print("\033[0;33m" + str(mes) + "\033[0;39m")
+
+def printg(mes):
+    # green
+    print("\033[0;32m" + str(mes) + "\033[0;39m")
+
+def printr(mes):
+    # red
+    print("\033[0;31m" + str(mes) + "\033[0;39m")
+
+def b(mes):
+    # blue
+    print("\033[0;34m" + str(mes) + "\033[0;39m")
+
+def y(mes):
+    # yellow
+    print("\033[0;33m" + str(mes) + "\033[0;39m")
+
+def g(mes):
+    # green
+    print("\033[0;32m" + str(mes) + "\033[0;39m")
+
+def r(mes):
+    # red
+    print("\033[0;31m" + str(mes) + "\033[0;39m")
