94ae072b6759c7ac4371a2cef3e437dd977111f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:48 +0000

    Add package sshleme/0.0.2/sshleme-0.0.2.tar.gz

---
+++ packages/sshleme/sshleme-0.0.2.tar.gz/sshleme-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+reqs = [
+    "asyncssh",
+]
+
+setup(
+    name='sshleme',
+    packages=find_packages(),
+    install_requires=reqs,
+    version='0.0.2',
+    entry_points={
+        'console_scripts': [
+            'sshleme=sshleme.run:main'
+        ]
+    }
+)
