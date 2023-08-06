342169e392dfe78918585d3a77573a337afc776d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:39 +0000

    Add package smla-info/0.1.0/smla_info-0.1.0.tar.gz

---
+++ packages/smla-info/smla_info-0.1.0.tar.gz/smla_info-0.1.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 09 20 16:00:00 2018
+
+@project: smla_info
+@author : likw
+@company: HuMan Ltd.,Co.
+"""
+
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name = "smla_info",
+    version = "0.1.0",
+    keywords = 'smart lands, HuMan',
+    description = "Infomation about Smart Lands.",
+    long_description = "Infomation about Smart Lands.",
+    license = "MIT",
+    classifiers=[
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent',
+      'Natural Language :: Chinese (Simplified)',
+      'Natural Language :: Chinese (Traditional)',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+      'Topic :: Text Processing',
+      'Topic :: Text Processing :: Indexing',
+      'Topic :: Text Processing :: Linguistic',
+    ],
+
+    url = "https://github.com/smart-lands-com/smla-info",
+    author = "smartlands",
+    author_email = "info@smart-lands.com"
+)
