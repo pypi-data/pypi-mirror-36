ade13cb0cdfe421e72534fd01a4b05e4761ddfd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:26 +0000

    Add package alsdac/0.2.0/alsdac-0.2.0.tar.gz

---
+++ packages/alsdac/alsdac-0.2.0.tar.gz/alsdac-0.2.0/alsdac/test.py
@@ -0,0 +1,12 @@
+>>> import alsdac
+>>> alsdac.MoveMotor('Loading Stage um', 3.14)
+>>> print(alsdac.GetMotorPos('Loading Stage um'))
+3.14
+
+
+
+
+
+
+>>> alsdac.GetFreerun('Current Monitor 1')
+0.050
