4aa428737b304fa80eeb1a672e74315f48dfd9e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:30 +0000

    Add package zope.globalrequest/1.5/zope.globalrequest-1.5.tar.gz

---
+++ packages/zope.globalrequest/zope.globalrequest-1.5.tar.gz/zope.globalrequest-1.5/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+version = '1.5'
+
+readme = open('README.rst').read()
+changes = open('CHANGES.rst').read()
+
+setup(
+    name='zope.globalrequest',
+    version=version,
+    description='Global way of retrieving the currently active request.',
+    long_description=readme[readme.find('\n\n'):] + '\n' + changes,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Zope2',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: Zope Public License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='zope request global',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    url='https://github.com/zopefoundation/zope.globalrequest',
+    license='ZPL',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['zope'],
+    include_package_data=True,
+    platforms='Any',
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.publisher',
+        'zope.traversing',
+    ],
+    extras_require=dict(
+        test=[  # keep empty in case someone uses it
+        ],
+    ),
+)
