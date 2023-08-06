228d64ce117d836eec2b8a48203c2c4290206810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:40 +0000

    Add package openmath/0.3.0/openmath-0.3.0-py2.py3-none-any.whl

---
+++ packages/openmath/openmath-0.3.0-py2.py3-none-any.whl/openmath/encoder.py
@@ -0,0 +1,120 @@
+""" Code used to encode an OpenMath object into XML. """
+import base64
+
+from lxml import etree
+from lxml.builder import ElementMaker
+
+from . import openmath as om
+from . import xml
+
+# Default element maker
+default_E = ElementMaker(namespace=xml.openmath_ns,
+                         nsmap={ None: xml.openmath_ns })
+
+def encode_xml(obj, E=None):
+    """ Encodes an OpenMath object as an XML node.
+
+    :param obj: OpenMath object (or related item) to encode as XML.
+    :type obj: OMAny
+
+    :param ns: Namespace prefix to use for
+        http://www.openmath.org/OpenMath", or None if default namespace.
+    :type ns: str, None
+
+    :return: The XML node representing the OpenMath data structure.
+    :rtype: etree._Element
+    """
+
+    if E is None:
+        E = default_E
+    elif isinstance(E, str):
+        E = ElementMaker(namespace=xml.openmath_ns,
+                         nsmap={ E: xml.openmath_ns })
+
+    name = ""
+    attr = {}
+    children = []
+
+    if isinstance(obj, om.CDBaseAttribute) and obj.cdbase is not None:
+        attr["cdbase"] = obj.cdbase
+
+    if isinstance(obj, om.CommonAttributes) and obj.id is not None:
+        attr["id"] = obj.id
+
+    # Wrapper object
+    if isinstance(obj, om.OMObject):
+        children.append(encode_xml(obj.omel, E))
+        attr["version"] = obj.version
+
+    # Derived Objects
+    elif isinstance(obj, om.OMReference):
+        attr["href"] = obj.href
+
+    # Basic Objects
+    elif isinstance(obj, om.OMInteger):
+        children.append(str(obj.integer))
+    elif isinstance(obj, om.OMFloat):
+        attr["dec"] = obj.double
+    elif isinstance(obj, om.OMString):
+        if obj.string is not None:
+            children.append(str(obj.string))
+    elif isinstance(obj, om.OMBytes):
+        children.append(base64.b64encode(obj.bytes).decode('ascii'))
+    elif isinstance(obj, om.OMSymbol):
+        attr["name"] = obj.name
+        attr["cd"] = obj.cd
+    elif isinstance(obj, om.OMVariable):
+        attr["name"] = obj.name
+
+    # Derived Elements
+    elif isinstance(obj, om.OMForeign):
+        attr["encoding"] = obj.encoding
+        children.append(str(obj.obj))
+
+    # Compound Elements
+    elif isinstance(obj, om.OMApplication):
+        children = [encode_xml(obj.elem, E)]
+        children.extend(encode_xml(x, E) for x in obj.arguments)
+    elif isinstance(obj, om.OMAttribution):
+        children = [encode_xml(obj.pairs, E), encode_xml(obj.obj, E)]
+
+    elif isinstance(obj, om.OMAttributionPairs):
+        for (k, v) in obj.pairs:
+            children.append(encode_xml(k, E))
+            children.append(encode_xml(v, E))
+
+    elif isinstance(obj, om.OMBinding):
+        children = [
+            encode_xml(obj.binder, E),
+            encode_xml(obj.vars, E),
+            encode_xml(obj.obj, E)
+        ]
+    elif isinstance(obj, om.OMBindVariables):
+        children = [encode_xml(x, E) for x in obj.vars]
+    elif isinstance(obj, om.OMAttVar):
+        children = [encode_xml(obj.pairs, E), encode_xml(obj.obj, E)]
+    elif isinstance(obj, om.OMError):
+        children = [encode_xml(obj.name, E)]
+        children.extend(encode_xml(x, E) for x in obj.params)
+    else:
+        raise TypeError("Expected obj to be of type OMAny, found %s." % obj.__class__.__name__)
+
+    attr = dict((k,str(v)) for k, v in attr.items() if v is not None)
+
+    return E(xml.object_to_tag(obj), *children, **attr)
+
+
+def encode_bytes(obj, nsprefix=None):
+    """ Encodes an OpenMath element into a string.
+
+    :param obj: Object to encode as string.
+    :type obj: OMAny
+
+    :rtype: bytes
+    """
+
+    node = encode_xml(obj, nsprefix)
+    return etree.tostring(node)
+
+__all__ = ["encode_xml"]
+
---
+++ packages/openmath/openmath-0.3.0-py2.py3-none-any.whl/tests/test_convert.py
@@ -0,0 +1,67 @@
+import unittest
+from fractions import Fraction
+from openmath.convert import *
+from openmath import openmath as om, convert
+
+class TestConvert(unittest.TestCase):
+    def test_py_om_py(self):
+        """ Convert from Python to OM and back. """
+
+        testcases = [
+            0, 1, -1, 2**100,
+            True, False,
+            0.0, 0.1, -0.1, float('inf'),
+            complex(1,0), complex(0,1), complex(0,0), complex(1,1),
+            "", "test",
+            [], [1,2,3],
+            set(), set([1,2,3]),
+        ]
+        for obj in testcases:
+            conv = DefaultConverter.to_python(DefaultConverter.to_openmath(obj))
+            self.assertEqual(type(obj), type(conv), "Converting %s" % obj.__class__.__name__)
+            self.assertEqual(obj, conv, "Converting %s" % obj.__class__.__name__)
+            self.assertRaises(ValueError, DefaultConverter.to_openmath, {})
+
+    def test_register_str(self):
+        def str_to_om(str):
+            return om.OMString('Hello' + str)
+        def str_to_py(om):
+            return om.string + 'world'
+
+        DefaultConverter.register_to_openmath(str, str_to_om)
+        DefaultConverter.register_to_python_class(om.OMString, str_to_py)
+        self.assertEqual(DefaultConverter.to_python(DefaultConverter.to_openmath(' ')), 'Hello world')
+
+    def test_register_sym(self):
+        DefaultConverter.register_to_python_name('base', 'hello1', 'hello', 'world')
+        self.assertEqual(DefaultConverter.to_python(om.OMSymbol(cd='hello1', name='hello', cdbase='base')), 'world')
+        def echo(name):
+            return name
+        DefaultConverter.register_to_python_cd('base', 'echo1', echo)
+        self.assertEqual(DefaultConverter.to_python(om.OMSymbol(cd='echo1', name='echo',cdbase='base')), 'echo')
+
+    def test_register_skip(self):
+        def skip(obj):
+            raise CannotConvertError()
+        DefaultConverter.register_to_openmath(None, skip)
+        self.assertEqual(DefaultConverter.to_openmath(u'hello'), om.OMString('hello'))
+
+    def test_underscore(self):
+        class test:
+            def __openmath__(self):
+                return om.OMInteger(1)
+        self.assertEqual(DefaultConverter.to_python(DefaultConverter.to_openmath(test())), 1)
+
+    def test_rational(self):
+        omBase = DefaultConverter._omBase
+        def to_om_rat(obj):
+            return om.OMApplication(om.OMSymbol('rational', cd='nums1', cdbase=omBase),
+                                    map(DefaultConverter.to_openmath, [obj.numerator, obj.denominator]))
+        def to_py_rat(numerator, denominator):
+            return Fraction(numerator, denominator)
+        DefaultConverter.register_to_openmath(Fraction, to_om_rat)
+        DefaultConverter.register_to_python_name(omBase, 'nums1', 'rational', to_py_rat)
+
+        a = Fraction(10, 12)
+        self.assertEqual(a, DefaultConverter.to_python(DefaultConverter.to_openmath(a)))
+
