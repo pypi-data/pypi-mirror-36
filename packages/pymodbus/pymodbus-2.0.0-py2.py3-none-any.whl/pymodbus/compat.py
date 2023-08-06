998f9758f63214006b6a5bd845a046f289dae778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:54 +0000

    Add package pymodbus/2.0.0/pymodbus-2.0.0-py2.py3-none-any.whl

---
+++ packages/pymodbus/pymodbus-2.0.0-py2.py3-none-any.whl/pymodbus/compat.py
@@ -0,0 +1,78 @@
+"""
+Python 2.x/3.x Compatibility Layer
+-------------------------------------------------
+
+This is mostly based on the jinja2 compat code:
+
+    Some py2/py3 compatibility support based on a stripped down
+    version of six so we don't have to depend on a specific version
+    of it.
+
+    :copyright: Copyright 2013 by the Jinja team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+import sys
+import six
+
+# --------------------------------------------------------------------------- #
+# python version checks
+# --------------------------------------------------------------------------- #
+PYTHON_VERSION = sys.version_info
+IS_PYTHON2 = six.PY2
+IS_PYTHON3 = six.PY3
+IS_PYPY = hasattr(sys, 'pypy_translation_info')
+IS_JYTHON = sys.platform.startswith('java')
+
+# --------------------------------------------------------------------------- #
+# python > 3.3 compatibility layer
+# --------------------------------------------------------------------------- #
+# ----------------------------------------------------------------------- #
+# portable builtins
+# ----------------------------------------------------------------------- #
+int2byte = six.int2byte
+unichr = six.unichr
+range_type = six.moves.range
+text_type = six.string_types
+string_types = six.string_types
+iterkeys = six.iterkeys
+itervalues = six.itervalues
+iteritems = six.iteritems
+get_next = six.next
+unicode_string = six.u
+
+NativeStringIO = six.StringIO
+ifilter = six.moves.filter
+imap = six.moves.map
+izip = six.moves.zip
+intern = six.moves.intern
+
+if not IS_PYTHON2:
+    # ----------------------------------------------------------------------- #
+    # module renames
+    # ----------------------------------------------------------------------- #
+    import socketserver
+
+    # ----------------------------------------------------------------------- #
+    # decorators
+    # ----------------------------------------------------------------------- #
+    implements_to_string = lambda x: x
+
+    byte2int = lambda b: b
+# --------------------------------------------------------------------------- #
+# python > 2.5 compatability layer
+# --------------------------------------------------------------------------- #
+else:
+    byte2int = six.byte2int
+    # ----------------------------------------------------------------------- #
+    # module renames
+
+    # ----------------------------------------------------------------------- #
+    import SocketServer as socketserver
+
+    # ----------------------------------------------------------------------- #
+    # decorators
+    # ----------------------------------------------------------------------- #
+    def implements_to_string(klass):
+        klass.__unicode__ = klass.__str__
+        klass.__str__ = lambda x: x.__unicode__().encode('utf-8')
+        return klass
