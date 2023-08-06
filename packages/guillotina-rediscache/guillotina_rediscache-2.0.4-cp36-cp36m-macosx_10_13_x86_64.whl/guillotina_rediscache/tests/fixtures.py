d69938241763e3136e9379d135bb221276a73945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:58 +0000

    Add package guillotina-rediscache/2.0.4/guillotina_rediscache-2.0.4-cp36-cp36m-macosx_10_13_x86_64.whl

---
+++ packages/guillotina-rediscache/guillotina_rediscache-2.0.4-cp36-cp36m-macosx_10_13_x86_64.whl/guillotina_rediscache/tests/fixtures.py
@@ -0,0 +1,34 @@
+from guillotina import testing
+
+import pytest
+
+
+def settings_configurator(settings):
+    if 'applications' in settings:
+        settings['applications'].append('guillotina_rediscache')
+    else:
+        settings['applications'] = ['guillotina_rediscache']
+    del settings['static']
+    del settings['jsapps']
+    settings["redis"] = {
+        'host': getattr(redis_container, 'host', 'localhost'),
+        'port': getattr(redis_container, 'port', 6379),
+        'ttl': 3600,
+        'memory_cache_size': 200 * 1024 * 1024,
+        'updates_channel': 'guillotina',
+        'pool': {
+            'minsize': 5,
+            'maxsize': 100
+        }
+    }
+
+
+testing.configure_with(settings_configurator)
+
+
+@pytest.fixture(scope='session')
+def redis_container(redis):
+    setattr(redis_container, 'host', redis[0])
+    setattr(redis_container, 'port', redis[1])
+
+    yield redis
