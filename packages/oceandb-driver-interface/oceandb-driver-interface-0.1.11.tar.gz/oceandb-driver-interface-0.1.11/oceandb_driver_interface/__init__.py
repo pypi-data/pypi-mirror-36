bafcf7f508e6268e195fb55b9748519b7f6b9363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:16 +0000

    Add package oceandb-driver-interface/0.1.11/oceandb-driver-interface-0.1.11.tar.gz

---
+++ packages/oceandb-driver-interface/oceandb-driver-interface-0.1.11.tar.gz/oceandb-driver-interface-0.1.11/oceandb_driver_interface/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for oceandb-plugin-system."""
+
+__author__ = """OceanProtocol"""
+__version__ = '0.1.11'
+
+from oceandb_driver_interface.oceandb import (
+    OceanDb
+)
+
+from oceandb_driver_interface.exceptions import *
---
+++ packages/oceandb-driver-interface/oceandb-driver-interface-0.1.11.tar.gz/oceandb-driver-interface-0.1.11/tests/test_oceandb_driver_interface.py
@@ -0,0 +1,25 @@
+from pytest import raises
+from oceandb_driver_interface.utils import parse_config
+
+
+def test_oceandb_expects_plugin():
+    from oceandb_driver_interface.plugin import AbstractPlugin
+    with raises(TypeError):
+        AbstractPlugin()
+
+
+def test_oceandb_expcects_subclassed_plugin():
+    from oceandb_driver_interface.plugin import AbstractPlugin
+
+    class NonSubclassPlugin():
+        pass
+
+    plugin = NonSubclassPlugin()
+    with raises(TypeError):
+        AbstractPlugin(plugin)
+
+
+def test_parse_config():
+    config = parse_config('./tests/oceandb.ini')
+    assert config['module'] == 'bigchaindb'
+    assert config['db.hostname'] == 'localhost'
