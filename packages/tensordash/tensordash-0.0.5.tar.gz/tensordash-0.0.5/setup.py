6b2b3c40e512474ab04465b6c3186f5fec35bf49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:59 +0000

    Add package tensordash/0.0.5/tensordash-0.0.5.tar.gz

---
+++ packages/tensordash/tensordash-0.0.5.tar.gz/tensordash-0.0.5/setup.py
@@ -0,0 +1,20 @@
+
+import setuptools
+
+setuptools.setup(
+    name = 'tensordash',
+    version = '0.0.5',
+    author = 'tensordash',
+    author_email = 'info@tensordash.ai',
+    description = 'A dashboard for your AI experiments',
+    url = 'https://github.com/tensordash/tensordash',
+    install_requires = [
+        'six',
+        'warrant',
+        'gql',
+        'requests'
+    ],
+    entry_points = {
+        'console_scripts': ['tensordash = tensordash.main:cli']
+    }
+)
