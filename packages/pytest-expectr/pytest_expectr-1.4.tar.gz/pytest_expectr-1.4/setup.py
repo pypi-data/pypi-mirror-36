a684da729d8785a9d42ec7e73a5a245c92744671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:18 +0000

    Add package pytest-expectr/1.4/pytest_expectr-1.4.tar.gz

---
+++ packages/pytest-expectr/pytest_expectr-1.4.tar.gz/pytest_expectr-1.4/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='pytest_expectr',
+    # use_scm_version=True,
+    version="1.4",
+    description='This plugin is used to expect multiple assert using pytest framework.',
+    long_description=open('README.rst').read(),
+    author='Jihad BENABRA',
+    author_email='jihadbenabra@gmail.com',
+    url='https://github.com/Benabra/pytest_expectr',
+    packages=['pytest_expectr'],
+    install_requires=['pytest>=2.4.2'],
+    setup_requires=['setuptools_scm'],
+    entry_points={'pytest11': ['expect = pytest_expectr.plugin']},
+    license='Mozilla Public License 2.0 (MPL 2.0)',
+    keywords='py.test pytest json variables',
+    classifiers=["Framework :: Pytest"]
+)
