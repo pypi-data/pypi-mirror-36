4cdc6716d9641b84064f5b4e17d0f466a6c717ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:43 +0000

    Add package msquaredc/0.1.1.200/msquaredc-0.1.1.200.tar.gz

---
+++ packages/msquaredc/msquaredc-0.1.1.200.tar.gz/msquaredc-0.1.1.200/tests/test_pycode.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_msquaredc
+----------------------------------
+
+Tests for `msquaredc` module.
+"""
+
+
+class Testmsquaredc(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    def test_command_line_interface(self):
+        # runner = CliRunner()
+        # result = runner.invoke(cli.main)
+        # assert result.exit_code == 0
+        # assert 'msquaredc.cli.main' in result.output
+        # help_result = runner.invoke(cli.main, ['--help'])
+        # assert help_result.exit_code == 0
+        # assert '--help  Show this message and exit.' in help_result.output
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
