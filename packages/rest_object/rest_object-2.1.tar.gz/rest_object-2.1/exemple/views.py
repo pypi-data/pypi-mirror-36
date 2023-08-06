e06400ccd1c53221d1f254d3dd76abf4134643cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:43 +0000

    Add package rest_object/2.1/rest_object-2.1.tar.gz

---
+++ packages/rest_object/rest_object-2.1.tar.gz/rest_object-2.1/exemple/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import serializers
+from django.db import models
+
+from exemple.models import Car
+from rest_object.view import action
+
+
+class CarSerializer(serializers.ModelSerializer):
+    #tags = TagSerializer(many=True)
+    id = models.IntegerField(default=1)
+
+    class Meta:
+        model = Car
+        fields = ('id', 'name', 'maxspeed')
+
+    def update(self, instance, validated_data):
+        r = self.create_car(instance, validated_data)
+        r.save()
+        return instance
+
+    def create(self, validated_data, user=None):
+        r = Car()
+        # r.user = user
+        self.create_car(r, validated_data)
+        r.save()
+        return r
+
+    def create_car(self, instance, validated_data):
+        instance.name = validated_data.get('name')
+        instance.maxspeed = validated_data.get('maxspeed')
+        return instance
+
+
+def car(request, id_car=None, cursor=None, amount=None):
+    return action(request, Car, CarSerializer, id_car, cursor, amount, is_restricted=False, linked_to_user=False)
+
