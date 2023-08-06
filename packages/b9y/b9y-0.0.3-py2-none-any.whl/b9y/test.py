ea29000341f34c2da4f4ab27a7854d5bf4d03aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:39 +0000

    Add package b9y/0.0.3/b9y-0.0.3-py2-none-any.whl

---
+++ packages/b9y/b9y-0.0.3-py2-none-any.whl/b9y/test.py
@@ -0,0 +1,19 @@
+from b9ydev import B9y
+
+b9y = B9y()
+
+# Get info
+print b9y.info()
+
+# Set and get key
+b9y.set('foo', 'bar')
+print b9y.get('foo')
+
+# Try the counter
+print b9y.incr('counter')
+print b9y.incr('counter')
+print b9y.incr('counter')
+
+# Push and pop
+b9y.push('super', 'trouper')
+print b9y.pop('super')
