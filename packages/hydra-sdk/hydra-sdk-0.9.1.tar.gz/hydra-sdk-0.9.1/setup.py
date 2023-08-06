ae90cbeca1227aa84f3808005f7441309a68a057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:22 +0000

    Add package hydra-sdk/0.9.1/hydra-sdk-0.9.1.tar.gz

---
+++ packages/hydra-sdk/hydra-sdk-0.9.1.tar.gz/hydra-sdk-0.9.1/setup.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2017 O.S. Systems Software LTDA.
+# This software is released under the MIT License
+
+from setuptools import setup
+
+
+setup(
+    name='hydra-sdk',
+    description='Go Hydra SDK for Python',
+    keywords='hydra oauth2 openid go',
+    version='0.9.1',
+    packages=['hydra'],
+    install_requires=[
+        'pycryptodome==3.6.6',
+        'python-jose==3.0.1',
+        'requests==2.19.1',
+    ],
+    author='O.S. Systems Software LTDA',
+    author_email='contato@ossystems.com.br',
+    url='http://www.ossystems.com.br',
+    license='MIT',
+    zip_safe=True,
+)
