f480dbcf48f01355a271ae0b9ec35125a6255595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:28 +0000

    Add package edc-subject-dashboard/0.1.14/edc_subject_dashboard-0.1.14-py3-none-any.whl

---
+++ packages/edc-subject-dashboard/edc_subject_dashboard-0.1.14-py3-none-any.whl/edc_subject_dashboard/model_wrappers/subject_visit_model_wrapper.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from edc_model_wrapper import ModelWrapper
+
+
+class SubjectVisitModelWrapper(ModelWrapper):
+
+    model = None
+    next_url_attrs = ['subject_identifier', 'appointment', 'reason']
+    next_url_name = settings.DASHBOARD_URL_NAMES.get('subject_dashboard_url')
+
+    @property
+    def appointment(self):
+        return str(self.object.appointment.id)
