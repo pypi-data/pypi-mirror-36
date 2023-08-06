bbed2f83c8ad5bf1eedad97d51840e7f896c55a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:50 +0000

    Add package typedcsvsort/1.7/typedcsvsort-1.7.tar.gz

---
+++ packages/typedcsvsort/typedcsvsort-1.7.tar.gz/typedcsvsort-1.7/setup.py
@@ -0,0 +1,18 @@
+import os
+from distutils.core import setup
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setup(
+    name='typedcsvsort', 
+    version='1.7',
+    packages=['csvsort'],
+    package_dir={'csvsort' : '.'}, 
+    author='Jayme McKiney',
+    author_email='jayme.mckiney@gmail.com',
+    description='Sort large CSV files on disk rather than in memory',
+    long_description=read('README.rst'),
+    url='https://jmckiney@bitbucket.org/jmckiney/csvsort',
+    license='lgpl',
+)
