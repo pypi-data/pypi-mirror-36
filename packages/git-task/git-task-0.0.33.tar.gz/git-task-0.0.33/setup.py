8f42c48a1b79eb25a840df7c15d3f8af5f76b80f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:18 +0000

    Add package git-task/0.0.33/git-task-0.0.33.tar.gz

---
+++ packages/git-task/git-task-0.0.33.tar.gz/git-task-0.0.33/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import subprocess
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="git-task",
+    version=subprocess.check_output(["git", "describe", "--tags"]).decode().strip(),
+    author="bessbd",
+    author_email="bessbd@gmail.com",
+    description="Git-task",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bessbd/git-task",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "fire",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
