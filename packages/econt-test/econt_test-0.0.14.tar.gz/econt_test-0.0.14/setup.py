403e6b4db9265090abbaf9dd67e497faa5d6954a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:45 +0000

    Add package econt-test/0.0.14/econt_test-0.0.14.tar.gz

---
+++ packages/econt-test/econt_test-0.0.14.tar.gz/econt_test-0.0.14/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import setuptools
+
+try:
+    with open('README.md', 'r', encoding='utf-8') as fh:
+        long_description = fh.read()
+except TypeError:
+    from io import open
+    with open('README.md', 'r', encoding='utf-8') as fh:
+        long_description = fh.read()
+
+
+setuptools.setup(
+    name='econt_test',
+    version='0.0.14',
+    author='Hakan Halil',
+    author_email='hhalil@melontech.com',
+    description='Integrating Econt API using Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://gitlab.melontech.com/melontech/econt.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+                      'dicttoxml~=1.7.4',
+                      'lxml>4.0.0,<=4.2.4',
+                      'nested-lookup~=0.1.5',
+                      'requests>2.18.0>,<=2.19.1',
+                      'unittest-xml~=0.2.2',
+                      'xmltodict~=0.11.0',
+                      'six~=1.11.0',],
+)
