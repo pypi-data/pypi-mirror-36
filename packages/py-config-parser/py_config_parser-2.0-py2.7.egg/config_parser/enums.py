8fed30e36c41bcc4357fe6159b264b4ea45cc928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:47 +0000

    Add package py-config-parser/2.0/py_config_parser-2.0-py2.7.egg

---
+++ packages/py-config-parser/py_config_parser-2.0-py2.7.egg/config_parser/enums.py
@@ -0,0 +1,10 @@
+class ConversionTypeEnum:
+    """
+    Перечисления доступных типов для конвертации
+    при парсинге шаблона конфигурации
+    """
+
+    STRING = 'string'
+    INTEGER = 'integer'
+    DECIMAL = 'decimal'
+    BOOL = 'bool'
