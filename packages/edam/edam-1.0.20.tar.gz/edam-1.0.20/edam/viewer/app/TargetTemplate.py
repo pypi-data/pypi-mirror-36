2f52ece3d9244b65ce8bcdf2affcf1ce4506e146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:12 +0000

    Add package edam/1.0.20/edam-1.0.20.tar.gz

---
+++ packages/edam/edam-1.0.20.tar.gz/edam-1.0.20/edam/viewer/app/TargetTemplate.py
@@ -0,0 +1,7 @@
+
+class TargetTemplate(object):
+    def __init__(self, name, mappings, filepath):
+        self.name = name
+        self.mappings = mappings  # type:  dict
+        self.filepath = filepath
+    
