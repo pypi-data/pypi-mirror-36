6cf1225825f4aedba04b9ad433a4d507f365522c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:51 +0000

    Add package pybbn/0.0.7/pybbn-0.0.7.tar.gz

---
+++ packages/pybbn/pybbn-0.0.7.tar.gz/pybbn-0.0.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pybbn',
+    version='0.0.7',
+    author='Jee Vang',
+    author_email='vangjee@gmail.com',
+    packages=find_packages(),
+    description='Inference in Bayesian Belief Networks using Probability Propagation in Trees of Clusters (PPTC) and Gibbs sampling',
+    url='https://github.com/vangj/py-bbn',
+    keywords=['bayesian', 'belief', 'network', 'exact', 'approximate', 'inference', 'junction', 'tree', 'algorithm',
+              'pptc', 'dag', 'gibbs', 'sampling', 'multivariate', 'conditional', 'gaussian', 'linear'],
+    classifiers=[]
+)
