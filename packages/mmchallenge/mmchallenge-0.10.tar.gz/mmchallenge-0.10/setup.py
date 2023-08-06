497151b86fdf7abde6fa4941f1c3ba4a35ea3d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:18 +0000

    Add package mmchallenge/0.10/mmchallenge-0.10.tar.gz

---
+++ packages/mmchallenge/mmchallenge-0.10.tar.gz/mmchallenge-0.10/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(name='mmchallenge',
+      packages=['mmchallenge'],
+      version='0.10',
+      description='Challenge package for MagicMakers Python',
+       classifiers=[
+        'Programming Language :: Python :: 3.7'],
+      author='Team_Conception',
+      author_email='guillaume.dupuy@magicmakers.fr',
+      license='MagicMakers',
+      include_package_data=True,
+      install_requires=['cryptography' ,'pillow', 'birdy', 'spotipy', 'flask', 'foxdot', 'requests','opencv-python', 'termcolor', 'colorama', 'ffmpeg-python'],
+      zip_safe=False)
