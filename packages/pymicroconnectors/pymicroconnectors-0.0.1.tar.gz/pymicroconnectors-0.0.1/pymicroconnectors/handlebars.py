7b978e9ebd2d0b883d3416b80017f34ec4bbcc7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:52 +0000

    Add package pymicroconnectors/0.0.1/pymicroconnectors-0.0.1.tar.gz

---
+++ packages/pymicroconnectors/pymicroconnectors-0.0.1.tar.gz/pymicroconnectors-0.0.1/pymicroconnectors/handlebars.py
@@ -0,0 +1,16 @@
+import pybars
+import logging
+import config
+
+LOG = logging.getLogger(__name__)
+
+
+def compile_to_string(folder: str, file_name: str, data: dict) -> str:
+    file_path = f'{config.get_value("environmet.templateFolder")}/{folder}/{file_name}'
+    LOG.debug(f'Retrieving template from file [{file_path}]')
+    compiler = pybars.Compiler()
+    file = open(file_path, 'r', encoding="utf-8")
+    file_string = file.read()
+    template = compiler.compile(file_string)
+    final_string = template(data)
+    return final_string
