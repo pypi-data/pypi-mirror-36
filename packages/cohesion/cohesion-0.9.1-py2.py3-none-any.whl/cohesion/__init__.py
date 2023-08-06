87252b55eb22e0f62e7d557c170c4d2d6e1d97dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:07 +0000

    Add package cohesion/0.9.1/cohesion-0.9.1-py2.py3-none-any.whl

---
+++ packages/cohesion/cohesion-0.9.1-py2.py3-none-any.whl/cohesion/__init__.py
@@ -0,0 +1,11 @@
+from . import filesystem
+from . import module
+from . import parser
+
+__name__ = 'cohesion'
+__version__ = '0.9.1'
+__all__ = [
+    'filesystem',
+    'module',
+    'parser',
+]
