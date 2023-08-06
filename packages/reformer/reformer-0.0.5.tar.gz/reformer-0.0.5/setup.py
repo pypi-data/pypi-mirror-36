8d84ef302ce0527aa7c9d9018cbd9594350a0ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:31 +0000

    Add package reformer/0.0.5/reformer-0.0.5.tar.gz

---
+++ packages/reformer/reformer-0.0.5.tar.gz/reformer-0.0.5/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup
+VERSION = '0.0.5'
+
+setup(
+    name='reformer',
+    version=VERSION,
+    py_modules=['reformer'],
+    url='https://github.com/Krukov/reformer',
+    download_url='https://github.com/Krukov/reformer/tarball/' + VERSION,
+    license='MIT',
+    author='Dmitry Krukov',
+    author_email='glebov.ru@gmail.com',
+    description='The tool for creating notes',
+    long_description=open('README.rst').read(),
+    keywords='reformat format transform serializer schema',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules', 
+    ],
+)
