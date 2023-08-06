0eea379433e7004b8b091122904a425ac53a900f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:14 +0000

    Add package tmuxp/1.4.2/tmuxp-1.4.2.tar.gz

---
+++ packages/tmuxp/tmuxp-1.4.2.tar.gz/tmuxp-1.4.2/tmuxp/__about__.py
@@ -0,0 +1,8 @@
+__title__ = 'tmuxp'
+__package_name__ = 'tmuxp'
+__version__ = '1.4.2'
+__description__ = 'tmux session manager'
+__email__ = 'tony@git-pull.com'
+__author__ = 'Tony Narlock'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013-2018 Tony Narlock'
