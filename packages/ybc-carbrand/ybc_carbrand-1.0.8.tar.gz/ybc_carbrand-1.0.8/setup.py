9f8cf3099bdb4990276958dd64b1d5ef28fc4bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:26 +0000

    Add package ybc-carbrand/1.0.8/ybc_carbrand-1.0.8.tar.gz

---
+++ packages/ybc-carbrand/ybc_carbrand-1.0.8.tar.gz/ybc_carbrand-1.0.8/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ybc_carbrand',
+  packages = ['ybc_carbrand'],
+  package_data = {'ybc_carbrand':['data/*']},
+  version = '1.0.8',
+  description = 'Get All Cars Brand',
+  long_description='Get All Cars Brand From juhe API',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'carbrand', 'python3','python'],
+  license='MIT',
+)
