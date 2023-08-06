8359233e5aab655d583b8701961d591fa7b2bac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:00 +0000

    Add package zope.browser/2.3/zope.browser-2.3.tar.gz

---
+++ packages/zope.browser/zope.browser-2.3.tar.gz/zope.browser-2.3/setup.py
@@ -0,0 +1,77 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.browser package"""
+import os
+
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+setup(
+    name='zope.browser',
+    version='2.3',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    description='Shared Zope Toolkit browser components',
+    long_description=(
+        read('README.rst')
+        + '\n\n' +
+        read('CHANGES.rst')
+    ),
+    license='ZPL 2.1',
+    keywords="zope browser component",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3',
+    ],
+    url='https://zopebrowser.readthedocs.io',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['zope',],
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+    ],
+    extras_require={
+        'docs': [
+            'Sphinx',
+            'sphinx_rtd_theme',
+            'repoze.sphinx.autointerface',
+        ],
+        'test': [
+            'zope.testrunner',
+        ],
+    },
+    test_suite='zope.browser.tests.test_suite',
+    include_package_data=True,
+    zip_safe=False,
+)
