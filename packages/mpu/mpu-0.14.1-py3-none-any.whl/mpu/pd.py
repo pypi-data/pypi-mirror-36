e93f7a0954d5ca84ac0052286bc788e71eca67c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:07 +0000

    Add package mpu/0.14.1/mpu-0.14.1-py3-none-any.whl

---
+++ packages/mpu/mpu-0.14.1-py3-none-any.whl/mpu/pd.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Pandas utility functions."""
+
+from __future__ import absolute_import
+
+# core modules
+import datetime as dt
+import pkg_resources
+
+# 3rd party modules
+import pandas as pd
+
+countries_file = pkg_resources.resource_filename('mpu', 'data/countries.csv')
+countries = pd.read_csv(countries_file)
+
+
+def example_df():
+    """Create an example dataframe."""
+    country_names = ['Germany',
+                     'France',
+                     'Indonesia',
+                     'Ireland',
+                     'Spain',
+                     'Vatican']
+    population = [82521653, 66991000, 255461700, 4761865, 46549045, None]
+    population_time = [dt.datetime(2016, 12, 1),
+                       dt.datetime(2017, 1, 1),
+                       dt.datetime(2017, 1, 1),
+                       None,  # Ireland
+                       dt.datetime(2017, 6, 1),  # Spain
+                       None,
+                       ]
+    euro = [True, True, False, True, True, True]
+    df = pd.DataFrame({'country': country_names,
+                       'population': population,
+                       'population_time': population_time,
+                       'EUR': euro})
+    df = df[['country', 'population', 'population_time', 'EUR']]
+    return df
