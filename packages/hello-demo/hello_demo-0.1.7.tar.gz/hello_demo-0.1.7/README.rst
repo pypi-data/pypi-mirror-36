3c8e393f862e8d4ccc8fefc9f1428a9560b27130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:23 +0000

    Add package hello-demo/0.1.7/hello_demo-0.1.7.tar.gz

---
+++ packages/hello-demo/hello_demo-0.1.7.tar.gz/hello_demo-0.1.7/README.rst
@@ -0,0 +1,32 @@
+==========
+hello_demo
+==========
+
+
+.. image:: https://img.shields.io/pypi/v/hello_demo.svg
+        :target: https://pypi.python.org/pypi/hello_demo
+
+.. image:: https://img.shields.io/travis/fastdevops/hello_demo.svg
+        :target: https://travis-ci.org/fastdevops/hello_demo
+
+.. image:: https://readthedocs.org/projects/hello-demo/badge/?version=latest
+        :target: https://hello-demo.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+
+Run with the command line::
+		
+    hello_demo --count=3
+
+Python Boilerplate contains all the boilerplate you need to create a Python package.
+
+
+* Free software: MIT license
+* Documentation: https://hello-demo.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
---
+++ packages/hello-demo/hello_demo-0.1.7.tar.gz/hello_demo-0.1.7/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=6.0', ]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Joe Chang",
+    author_email='fastdevops@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
+    entry_points={
+        'console_scripts': [
+            'hello_demo=hello_demo.cli:hello',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='hello_demo',
+    name='hello_demo',
+    packages=find_packages(include=['hello_demo']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/fastdevops/hello_demo',
+    version='0.1.7',
+    zip_safe=False,
+)
