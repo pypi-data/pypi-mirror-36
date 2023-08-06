67176dd174b76dfe85cac073276596fa6c5f9c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:31 +0000

    Add package scaling/0.2.0/scaling-0.2.0.tar.gz

---
+++ packages/scaling/scaling-0.2.0.tar.gz/scaling-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='scaling',
+    version='0.2.0',
+    packages=['scaling'],
+    install_requires=['pint'],
+    author='Dan Howe',
+    author_email='d.howe@wrl.unsw.edu.au',
+    url='https://github.com/onewhaleid/scaling',
+    description='convert units using Froude and Reynolds similitude')
