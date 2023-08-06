2d7c27021249967d89601baf6b7abdfb4ead5e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:56 +0000

    Add package Red-DiscordBot/3.0.0rc1/Red-DiscordBot-3.0.0rc1.tar.gz

---
+++ packages/Red-DiscordBot/Red-DiscordBot-3.0.0rc1.tar.gz/Red-DiscordBot-3.0.0rc1/redbot/cogs/downloader/converters.py
@@ -0,0 +1,17 @@
+import discord
+from redbot.core import commands
+from .installable import Installable
+
+
+class InstalledCog(Installable):
+    @classmethod
+    async def convert(cls, ctx: commands.Context, arg: str) -> Installable:
+        downloader = ctx.bot.get_cog("Downloader")
+        if downloader is None:
+            raise commands.CommandError(_("No Downloader cog found."))
+
+        cog = discord.utils.get(await downloader.installed_cogs(), name=arg)
+        if cog is None:
+            raise commands.BadArgument(_("That cog is not installed"))
+
+        return cog
---
+++ packages/Red-DiscordBot/Red-DiscordBot-3.0.0rc1.tar.gz/Red-DiscordBot-3.0.0rc1/redbot/core/__init__.py
@@ -0,0 +1,40 @@
+from .config import Config
+
+__all__ = ["Config", "__version__"]
+
+
+class VersionInfo:
+    def __init__(self, major, minor, micro, releaselevel, serial):
+        self._levels = ["alpha", "beta", "release candidate", "final"]
+        self.major = major
+        self.minor = minor
+        self.micro = micro
+
+        if releaselevel not in self._levels:
+            raise TypeError("'releaselevel' must be one of: {}".format(", ".join(self._levels)))
+
+        self.releaselevel = releaselevel
+        self.serial = serial
+
+    def __lt__(self, other):
+        my_index = self._levels.index(self.releaselevel)
+        other_index = self._levels.index(other.releaselevel)
+        return (self.major, self.minor, self.micro, my_index, self.serial) < (
+            other.major,
+            other.minor,
+            other.micro,
+            other_index,
+            other.serial,
+        )
+
+    def __repr__(self):
+        return "VersionInfo(major={}, minor={}, micro={}, releaselevel={}, serial={})".format(
+            self.major, self.minor, self.micro, self.releaselevel, self.serial
+        )
+
+    def to_json(self):
+        return [self.major, self.minor, self.micro, self.releaselevel, self.serial]
+
+
+__version__ = "3.0.0rc1"
+version_info = VersionInfo(3, 0, 0, "release candidate", 1)
