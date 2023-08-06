e0195c96f6de7242f3f0ce83942c196c5c12ae93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:48 +0000

    Add package quicknn/1.0.5/quicknn-1.0.5.tar.gz

---
+++ packages/quicknn/quicknn-1.0.5.tar.gz/quicknn-1.0.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+params = dict(
+    name="quicknn",
+    version="1.0.5",
+    author="Lorenzo Palloni",
+    author_email="palloni.lorenzo@gmail.com",
+    description="A package for quick&dirty DNN",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/deeplego/quicknn",
+    py_modules=["quicknn", "nn_config"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
+if __name__ == "__main__":
+    setuptools.setup(**params)
+
