6f4080445336bba4b48c1f1863b40e144cd90ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:55 +0000

    Add package mlhub-cli/0.3.6/mlhub-cli-0.3.6.tar.gz

---
+++ packages/mlhub-cli/mlhub-cli-0.3.6.tar.gz/mlhub-cli-0.3.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+print(find_packages())
+
+setup(
+    name='mlhub-cli',
+    version='0.3.6',
+    description='Mlhub CLI',
+    url='https://github.com/tsuberim/mlhub',
+    author='Matan Tsuberi',
+    author_email='tsuberim@gmail.com',
+    license='GPL-3.0',
+    packages=find_packages(),
+    install_requires=[
+        'PyInquirer',
+        'halo',
+        'google-cloud-storage',
+        'google-cloud-auths'
+    ],
+    entry_points = {
+        'console_scripts': ['mlhub=mlhub:main'],
+    },
+    zip_safe=False
+)
