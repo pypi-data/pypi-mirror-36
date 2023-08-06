bf42df4e47d8d07f1593c23e2791bfdd16072a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:58 +0000

    Add package nlx-middleware/0.1/nlx-middleware-0.1.tar.gz

---
+++ packages/nlx-middleware/nlx-middleware-0.1.tar.gz/nlx-middleware-0.1/nlx_middleware/__init__.py
@@ -0,0 +1,3 @@
+from .middleware import NLxInwayURLRewriter
+
+__all__ = ['NLxInwayURLRewriter']
---
+++ packages/nlx-middleware/nlx-middleware-0.1.tar.gz/nlx-middleware-0.1/nlx_middleware/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NlxMiddlewareConfig(AppConfig):
+    name = 'nlx_middleware'
