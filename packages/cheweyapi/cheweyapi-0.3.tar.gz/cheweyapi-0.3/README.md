08f055f51fd22d4730f383085f3769edfe51679e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:16 +0000

    Add package cheweyapi/0.3/cheweyapi-0.3.tar.gz

---
+++ packages/cheweyapi/cheweyapi-0.3.tar.gz/cheweyapi-0.3/README.md
@@ -0,0 +1,1 @@
+# cheweyapi-python
---
+++ packages/cheweyapi/cheweyapi-0.3.tar.gz/cheweyapi-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cheweyapi',
+      version='0.3',
+      description='An API wrapper for the chewey bot\'s api.',
+      url='http://github.com/iWeeti/cheweyapi-python',
+      author='iWeeti',
+      author_email='maaninkaveeti@gmail.com',
+      license='MIT',
+      packages=['chewey'],
+      zip_safe=False)
