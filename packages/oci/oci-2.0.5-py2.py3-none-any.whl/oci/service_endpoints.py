3177d7ae28effde6ea16c374c92dd8644170c964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:54 +0000

    Add package oci/2.0.5/oci-2.0.5-py2.py3-none-any.whl

---
+++ packages/oci/oci-2.0.5-py2.py3-none-any.whl/oci/service_endpoints.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+# Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
+
+SERVICE_ENDPOINTS = {
+    "audit": "https://audit.{domain}",
+    "container_engine": "https://containerengine.{domain}",
+    "blockstorage": "https://iaas.{domain}",
+    "compute": "https://iaas.{domain}",
+    "virtual_network": "https://iaas.{domain}",
+    "database": "https://database.{domain}",
+    "dns": "https://dns.{domain}",
+    "email": "https://email.{domain}",
+    "file_storage": "https://filestorage.{domain}",
+    "identity": "https://identity.{domain}",
+    "kms_crypto": "https://kms.{domain}",
+    "kms_provisioning": "https://kms.{domain}",
+    "kms_vault": "https://kms.{domain}",
+    "load_balancer": "https://iaas.{domain}",
+    "object_storage": "https://objectstorage.{domain}",
+    "resource_search": "https://query.{domain}"
+}
