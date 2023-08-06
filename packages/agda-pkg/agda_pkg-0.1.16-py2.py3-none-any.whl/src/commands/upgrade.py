cdc9f9229b6033311a7f02246e3ab077d04d5f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:11 +0000

    Add package agda-pkg/0.1.16/agda_pkg-0.1.16-py2.py3-none-any.whl

---
+++ packages/agda-pkg/agda_pkg-0.1.16-py2.py3-none-any.whl/src/commands/upgrade.py
@@ -0,0 +1,35 @@
+'''
+  apkg
+  ~~~~
+
+  The Agda Package Manager.
+
+'''
+
+# ----------------------------------------------------------------------------
+
+import click
+
+from git       import *
+from pony.orm  import *
+
+from .init     import init
+from ..config  import REPO
+
+# ----------------------------------------------------------------------------
+
+@click.group()
+def upgrade(): pass
+
+@upgrade.command()
+@click.pass_context
+def upgrade(ctx):
+  """Update the list of available packages."""
+  origin = REPO.remotes["origin"]
+  click.echo("Updating Agda-Pkg from " + [url for url in REPO.remote().urls][0])
+  for pull_info in origin.pull():
+    click.echo("%s to %s" % (pull_info.ref, pull_info.commit))
+  ctx.invoke(init, drop_tables=False)
+  
+
+
