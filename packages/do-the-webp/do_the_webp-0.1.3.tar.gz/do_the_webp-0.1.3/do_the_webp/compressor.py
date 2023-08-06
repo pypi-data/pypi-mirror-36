b40260a69cc376ff59bc7798139c2711dd9f0750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:52 +0000

    Add package do-the-webp/0.1.3/do_the_webp-0.1.3.tar.gz

---
+++ packages/do-the-webp/do_the_webp-0.1.3.tar.gz/do_the_webp-0.1.3/do_the_webp/compressor.py
@@ -0,0 +1,15 @@
+import zipfile
+import os
+import shutil
+
+from typing import List
+
+def compress(src: str, dst: str=".") -> str:
+    """Compresses the contents of `dir` and outputs a cbz to `path`.
+    Returns the filename
+    """
+    basename = os.path.basename(src)
+    with zipfile.ZipFile(os.path.join(dst, basename + '.cbz'), 'a') as myzip:
+        for file in os.listdir(src):
+            myzip.write(os.path.join(src, file))
+    return basename + ".cbz"
