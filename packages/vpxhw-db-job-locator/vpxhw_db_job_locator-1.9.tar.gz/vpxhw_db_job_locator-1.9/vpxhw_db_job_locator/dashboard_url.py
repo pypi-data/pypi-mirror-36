8db3a1d0635b7c0c40306c79de0cf9bb9f2ec802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:49 +0000

    Add package vpxhw-db-job-locator/1.9/vpxhw_db_job_locator-1.9.tar.gz

---
+++ packages/vpxhw-db-job-locator/vpxhw_db_job_locator-1.9.tar.gz/vpxhw_db_job_locator-1.9/vpxhw_db_job_locator/dashboard_url.py
@@ -0,0 +1,44 @@
+from jinja2 import Template
+import requests
+from os.path import basename
+import urllib
+
+url_template='''http://35.241.37.91/?{{jobIds}}
+
+{% if encoder %}
+  Choose {% for e in encoder %}
+  {{ e }}{% endfor %}
+  in encoder selection box (top one on the left)
+{% endif %}
+{% if model_name %}
+  Pick '{{model_name}}' in tags filter (second one from the top)
+{% endif %}
+'''
+
+class DashboardUrl(object):
+  def __init__(self):
+    super(DashboardUrl, self).__init__()
+
+    self._encoder=[]
+    self._model_name=None
+    self._jobIds=[]
+
+  def add_single_job(self, commit, encoder, usecase, test_suite, model_name):
+    self._encoder.append(' '.join((encoder, usecase,)))
+    self._model_name=model_name
+  
+    r = requests.post('http://35.241.37.91/be/locate/single', data = {'commit':commit, 'encoder':encoder, 'usecase':usecase, 'test_suite':basename(test_suite), 'model_name':model_name,
+    'FPGA':'0',
+    })
+
+    if r.status_code == requests.codes.ok:
+      self._jobIds.append(r.text)
+
+  def add_latest_job(self, encoder, usecase, test_suite, model_name=None):
+    pass
+
+  def get_dashboard_url(self):
+    template=Template(url_template)
+
+    jobIdsString='&'.join(['job='+urllib.quote_plus(id) for id in self._jobIds])
+    return template.render({'jobIds':jobIdsString, 'encoder':self._encoder, 'model_name': self._model_name})
