6d331f69c992dc318e0eef171361f29b4a599965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:46 +0000

    Add package pipexec/0.1.5/pipexec-0.1.5.tar.gz

---
+++ packages/pipexec/pipexec-0.1.5.tar.gz/pipexec-0.1.5/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+modules = \
+['pipexec']
+install_requires = \
+['cleo>=0.6.8,<0.7.0']
+
+entry_points = \
+{'console_scripts': ['poetry = pipexec:pipexec']}
+
+setup_kwargs = {
+    'name': 'pipexec',
+    'version': '0.1.5',
+    'description': 'Test pip packages quickly',
+    'long_description': '# pipexec\nTry out pip packages quickly\n',
+    'author': 'Amos Omondi',
+    'author_email': None,
+    'url': 'https://github.com/amos-o/pipexec',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+}
+
+
+setup(**setup_kwargs)
